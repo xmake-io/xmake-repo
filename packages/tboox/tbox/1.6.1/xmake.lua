7e9b09d5cf74b502417b2d4ff300137edf96646a
ruki <waruqi@gmail.com>
2017-04-10 11:18:44 +0800

    add tboox.tbox package

---
+++ packages/tboox/tbox/1.6.1/xmake.lua
@@ -0,0 +1,14 @@
+package("tbox")
+
+    -- set description
+    set_description("A glib-like multi-platform c library")
+    
+    -- set homepage
+    set_homepage("http://www.tboox.org")
+
+    -- set url
+    set_url("https://github.com/tboox/tbox/archive/v1.6.1.tar.gz")
+
+    -- set sha256
+    set_sha256("2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8")
+
