61e0b1262f47a31540e8cdec28e7206d745497e1
ruki <waruqi@gmail.com>
2017-04-12 09:36:46 +0800

    modify package api and remove comments

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -1,14 +1,9 @@
-package("tbox")
+package("tboox.tbox")
 
-    -- set description
-    set_description("A glib-like multi-platform c library")
-    
-    -- set homepage
     set_homepage("http://www.tboox.org")
+    set_description("A glib-like multi-platform c library")
 
-    -- set url
-    set_url("https://github.com/tboox/tbox/archive/v1.6.1.tar.gz")
+    set_url("https://github.com/tboox/tbox/archive/$(version).tar.gz")
+    set_git("git@github.com:tboox/tbox.git")
 
-    -- set sha256
-    set_sha256("2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8")
 
--- packages/zlib/xmake.lua
+++ packages/zlib/xmake.lua
@@ -1,26 +1,24 @@
 package("zlib")
 
-    -- set description
+    set_homepage("http://www.zlib.net")
     set_description("A Massively Spiffy Yet Delicately Unobtrusive Compression Library")
     
-    -- set homepage
-    set_homepage("http://www.zlib.net")
+    set_url("http://zlib.net/zlib-$(version).tar.gz")
+    set_mirror("https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz")
 
-    -- set url
-    set_url("http://zlib.net/zlib-1.2.11.tar.gz")
-
-    -- set mirror
-    set_mirror("https://downloads.sourceforge.net/project/libpng/zlib/1.2.11/zlib-1.2.11.tar.gz")
-
-    -- set sha256
-    set_sha256("c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
+    set_versions("1.2.10", "1.2.11")
 
     -- on build
     on_build(function ()
        -- TODO 
     end)
 
-    -- on package
-    on_package(function ()
+    -- on install
+    on_install(function ()
+       -- TODO 
+    end)
+
+    -- on test
+    on_test(function ()
        -- TODO 
     end)
