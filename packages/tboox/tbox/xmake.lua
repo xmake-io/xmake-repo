02dffa0c92383898ddef0af4f05cdc8ee08f52ab
ruki <waruqi@gmail.com>
2017-04-13 09:15:32 +0800

    modify package description style

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -1,9 +1,14 @@
-package("tboox.tbox")
+package "tboox.tbox"
 
-    set_homepage("http://www.tboox.org")
-    set_description("A glib-like multi-platform c library")
-
-    set_url("https://github.com/tboox/tbox/archive/$(version).tar.gz")
-    set_git("git@github.com:tboox/tbox.git")
+    set_homepage    "http://www.tboox.org"
+    set_description "A glib-like multi-platform c library"
+    set_url         "https://github.com/tboox/tbox/archive/v$(version).tar.gz"
+    set_versions    ("1.5.0", "1.5.1", "1.5.2", "1.5.3", "1.6.0", "1.6.1")
+    set_sha256s     ("2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
+                     "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
+                     "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
+                     "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
+                     "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
+                     "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8")
 
 
--- packages/zlib/xmake.lua
+++ packages/zlib/xmake.lua
@@ -1,24 +1,21 @@
-package("zlib")
+package "zlib"
 
-    set_homepage("http://www.zlib.net")
-    set_description("A Massively Spiffy Yet Delicately Unobtrusive Compression Library")
-    
-    set_url("http://zlib.net/zlib-$(version).tar.gz")
-    set_mirror("https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz")
+    set_homepage        "http://www.zlib.net"
+    set_description     "A Massively Spiffy Yet Delicately Unobtrusive Compression Library"
+    set_url             "http://zlib.net/zlib-$(version).tar.gz"
+    set_mirror          "https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz"
+    set_versions        ("1.2.10", "1.2.11")
+    set_sha256s         ("c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1",
+                         "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
 
-    set_versions("1.2.10", "1.2.11")
-
-    -- on build
-    on_build(function ()
-       -- TODO 
+    -- TODO 
+    on_build(function (package)
     end)
 
-    -- on install
-    on_install(function ()
-       -- TODO 
+    -- TODO 
+    on_install(function (package)
     end)
 
-    -- on test
-    on_test(function ()
-       -- TODO 
+    -- TODO 
+    on_test(function (package)
     end)
