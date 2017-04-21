e5283468c51a119708e3b8bb390b59c01eaec91f
ruki <waruqi@gmail.com>
2017-04-21 17:37:28 +0800

    rename set_url and set_mirror to set_urls

--- packages/zlib/xmake.lua
+++ packages/zlib/xmake.lua
@@ -2,11 +2,11 @@ package "zlib"
 
     set_homepage        "http://www.zlib.net"
     set_description     "A Massively Spiffy Yet Delicately Unobtrusive Compression Library"
-    set_url             "http://zlib.net/zlib-$(version).tar.gz"
-    set_mirror          "https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz"
-    set_versions        ("1.2.10", "1.2.11")
-    set_sha256s         ("8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017",
-                         "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
+    set_urls           ("http://zlib.net/zlib-$(version).tar.gz",
+                        "https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz")
+    set_versions       ("1.2.10", "1.2.11")
+    set_sha256s        ("8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017",
+                        "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
 
     -- TODO 
     on_build(function (package)
