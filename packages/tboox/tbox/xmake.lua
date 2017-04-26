7e3ab749b776ee4d4f4445ebcdb25ded1d40ad0f
ruki <waruqi@gmail.com>
2017-04-26 16:24:28 +0800

    remove some old versions of tbox

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -5,12 +5,8 @@ package "tboox.tbox"
     set_urls           ("https://github.com/tboox/tbox/archive/$(version).tar.gz",
                         "git@github.com:tboox/tbox.git", 
                         "git@git.oschina.net:tboox/tbox.git")
-    set_versions       ("v1.5.0", "v1.5.1", "v1.5.2", "v1.5.3", "v1.6.0", "v1.6.1")
-    set_sha256s        ("232b7b1776b708ebbdbd47767caa6ae10154c68a146d92391a366aada0d77559",
-                        "00166848e2602234e0481692b5db0804c007b7e5673cf5a17bd1fc889b4917d7",
-                        "e4356f32ff64887706da3d8592433edb7cd5f8ab37d0304c685c967a36169606",
-                        "b9ead60d52045290e40a3fb6d4a01c9e66104a5e875b56f110806d7b0ed53ea3",
-                        "4a2c97bf57214abc95058bd12fe91f12d987814233caba17cc2390f893c802cf",
+    set_versions       ("v1.6.0", "v1.6.1")
+    set_sha256s        ("4a2c97bf57214abc95058bd12fe91f12d987814233caba17cc2390f893c802cf",
                         "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8")
 
 
