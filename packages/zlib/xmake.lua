3cdaf9b4f64e121b24e47e5131076ff3d47ac147
ruki <waruqi@gmail.com>
2017-05-05 09:45:48 +0800

    modify build script for zlib

--- packages/zlib/xmake.lua
+++ packages/zlib/xmake.lua
@@ -8,14 +8,9 @@ package "zlib"
     set_sha256s        ("8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017",
                         "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
 
-    -- TODO 
     on_build(function (package)
+
+        os.vrun("./configure")
+        os.vrun("$(make)")
     end)
 
-    -- TODO 
-    on_install(function (package)
-    end)
-
-    -- TODO 
-    on_test(function (package)
-    end)
