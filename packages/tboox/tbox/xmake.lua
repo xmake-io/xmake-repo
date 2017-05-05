a7aae5ecadeeffe761f301ee2f91b42d10647069
ruki <waruqi@gmail.com>
2017-05-05 10:54:39 +0800

    modify git url

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -3,8 +3,8 @@ package "tboox.tbox"
     set_homepage        "http://www.tboox.org"
     set_description     "A glib-like multi-platform c library"
     set_urls           ("https://github.com/tboox/tbox/archive/$(version).tar.gz",
-                        "git@github.com:tboox/tbox.git", 
-                        "git@git.oschina.net:tboox/tbox.git")
+                        "https://github.com/tboox/tbox.git", 
+                        "https://git.oschina.net/tboox/tbox.git")
     set_versions       ("v1.6.0", "v1.6.1")
     set_sha256s        ("4a2c97bf57214abc95058bd12fe91f12d987814233caba17cc2390f893c802cf",
                         "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8")
