713dac6d586acf47743c91b28a8540ea94ff21bb
ruki <waruqi@gmail.com>
2017-08-29 16:21:54 +0800

    fix tbox xmake.lua

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -7,7 +7,7 @@ package "tboox.tbox"
                         "https://git.oschina.net/tboox/tbox.git")
     set_versions       ("v1.6.0", "v1.6.1", "v1.6.2")
     set_sha256s        ("4a2c97bf57214abc95058bd12fe91f12d987814233caba17cc2390f893c802cf",
-                        "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8"
+                        "2f4424194444ec3dd98bff20bb4a3eb9d9fdd485f1833dde75482beb2c3088f8",
                         "5236090b80374b812c136c7fe6b8c694418cbfc9c0a820ec2ba35ff553078c7b")
 
 
