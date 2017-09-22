4339097baece7f715571c72d7971b7e36682477b
ruki <waruqi@gmail.com>
2017-09-22 21:03:50 +0800

    modify oscgit to gitee

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -5,7 +5,7 @@ package("tboox.tbox")
 
     set_urls("https://github.com/tboox/tbox/archive/$(version).tar.gz",
              "https://github.com/tboox/tbox.git", 
-             "https://git.oschina.net/tboox/tbox.git")
+             "https://gitee.com/tboox/tbox.git")
 
     set_versions("v1.6.2")
     add_sha256s("v1.6.2", "5236090b80374b812c136c7fe6b8c694418cbfc9c0a820ec2ba35ff553078c7b")
