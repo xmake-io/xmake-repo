50a02c4e5137ca036e875a5aace98271d65388c0
ruki <waruqi@gmail.com>
2017-09-27 13:26:09 +0800

    fix github sha256 for tbox

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -4,13 +4,13 @@ package("tboox.tbox")
     set_description("A glib-like multi-platform c library")
 
     add_urls("https://github.com/tboox/tbox/archive/$(version).tar.gz", {alias = "github"})
-    add_urls("https://coding.net/u/waruqi/p/tbox/git/archive/$(version).zip", {alias = "coding"})
+--    add_urls("https://coding.net/u/waruqi/p/tbox/git/archive/$(version).zip", {alias = "coding"})
     add_urls("https://github.com/tboox/tbox.git")
     add_urls("https://gitee.com/tboox/tbox.git")
 
     set_versions("v1.6.2")
-    add_sha256s("github@v1.6.2", "5236090b80374b812c136c7fe6b8c694418cbfc9c0a820ec2ba35ff553078c7b")
-    add_sha256s("coding@v1.6.2", "0881b08a88722cc35e7613d9785768d4d7ae4656b134da5653f8a125fc72497e")
+    add_sha256s("github@v1.6.2", "26ede7fd61e33c3635bf2d6657ae4040a4a75c82a5da88855fd965db2f834025")
+--    add_sha256s("coding@v1.6.2", "0881b08a88722cc35e7613d9785768d4d7ae4656b134da5653f8a125fc72497e")
 
     on_build(function (package)
         os.vrun("xmake f -p $(plat) -a $(arch) -m $(mode) --demo=n -c")
