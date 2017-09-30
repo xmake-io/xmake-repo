d417d3ab68d0dbfaa7c0d000cdf08508e1bcf747
ruki <waruqi@gmail.com>
2017-09-30 09:48:24 +0800

    remove on_build for tbox

--- packages/tboox/tbox/xmake.lua
+++ packages/tboox/tbox/xmake.lua
@@ -10,8 +10,3 @@ package("tboox.tbox")
     set_versions("v1.6.2")
     add_sha256s("v1.6.2", "26ede7fd61e33c3635bf2d6657ae4040a4a75c82a5da88855fd965db2f834025")
 
-    on_build(function (package)
-        os.vrun("xmake f -p $(plat) -a $(arch) -m $(mode) --demo=n -c")
-        os.vrun("xmake")
-    end)
-
