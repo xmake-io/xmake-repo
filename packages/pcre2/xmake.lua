2ae7c6d84a23140f2cd12ec1d0af334fb7ba7993
ruki <waruqi@gmail.com>
2017-11-28 23:13:37 +0800

    add pcre and pcre2

---
+++ packages/pcre2/xmake.lua
@@ -0,0 +1,13 @@
+package("pcre2")
+
+    set_homepage("https://www.pcre.org/")
+    set_description("A Perl Compatible Regular Expressions Library")
+
+    set_urls("https://ftp.pcre.org/pub/pcre/pcre2-$(version).zip",
+             "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-$(version).zip")
+
+    set_versions("10.23", "10.30")
+    add_sha256s("10.23", "6301a525a8a7e63a5fac0c2fbfa0374d3eb133e511d886771e097e427707094a")
+    add_sha256s("10.30", "3677ce17854fffa68fce6b66442858f48f0de1f537f18439e4bd2771f8b4c7fb")
+
+
