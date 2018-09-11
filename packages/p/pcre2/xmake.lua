cb699c15031a2fae956c07fcef73fb121937829e
ruki <waruqi@gmail.com>
2018-09-12 00:45:13 +0800

    add on_build and on_install

--- packages/p/pcre2/xmake.lua
+++ packages/p/pcre2/xmake.lua
@@ -8,5 +8,13 @@ package("pcre2")
 
     add_versions("10.23", "6301a525a8a7e63a5fac0c2fbfa0374d3eb133e511d886771e097e427707094a")
     add_versions("10.30", "3677ce17854fffa68fce6b66442858f48f0de1f537f18439e4bd2771f8b4c7fb")
+    add_versions("10.31", "b4b40695a5347a770407d492c1749e35ba3970ca03fe83eb2c35d44343a5a444")
 
+    on_build(function (package)
+        os.vrun("./configure --prefix=%s", package:installdir())
+        os.vrun("make")
+    end)
 
+    on_install(function (package)
+        os.vrun("make install")
+    end)
--- packages/t/tbox/xmake.lua
+++ packages/t/tbox/xmake.lua
@@ -3,9 +3,17 @@ package("tbox")
     set_homepage("http://www.tboox.org")
     set_description("A glib-like multi-platform c library")
 
-    add_urls("https://github.com/tboox/tbox/archive/$(version).tar.gz")
+    add_urls("https://github.com/tboox/tbox/archive/$(version).zip")
     add_urls("https://github.com/tboox/tbox.git")
     add_urls("https://gitee.com/tboox/tbox.git")
 
-    add_versions("v1.6.2", "26ede7fd61e33c3635bf2d6657ae4040a4a75c82a5da88855fd965db2f834025")
+    add_versions("v1.6.2", "5236090b80374b812c136c7fe6b8c694418cbfc9c0a820ec2ba35ff553078c7b")
+    add_versions("v1.6.3", "bc5a957cdb1610c19f0cf94497ad114a0e01fd7d569777e9cb2133c513ef6baa")
 
+    on_build(function (package)
+        os.vrun("xmake")
+    end)
+
+    on_install(function (package)
+        os.vrun("xmake install -o %s", package:installdir())
+    end)
