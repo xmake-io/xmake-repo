dfaeec0355351732d8c57d90cc94277aeecfda5c
ruki <waruqi@gmail.com>
2018-09-15 00:03:36 +0800

    add x64 to pcre and mbedtls

--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -8,15 +8,15 @@ package("mbedtls")
     add_versions("2.13.0", "6e747350bc13e8ff51799daa50f74230c6cd8e15977da55dd59f57b23dcf70a6")
     add_versions("2.7.6", "e527d828ab82650102ca8031302e5d4bc68ea887b2d84e43d3da2a80a9e5a2c8")
 
-    if is_host("windows") and is_arch("x86") then
+    if is_host("windows") then
         add_deps("cmake")
     end
 
-    on_build("windows|x86", function (package)
+    on_build("windows", function (package)
         import("package.builder.cmake").build(package)
     end)
 
-    on_install("windows|x86", function (package)
+    on_install("windows", function (package)
         import("package.builder.cmake").install(package)
     end)
 
