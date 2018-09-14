8170508fc9e396d3336ef1f995064024df4f6682
ruki <waruqi@gmail.com>
2018-09-15 00:48:42 +0800

    filter version format in sqlite3

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -3,8 +3,8 @@ package("sqlite3")
     set_homepage("https://sqlite.org/")
     set_description("The most used database engine in the world")
 
-    set_urls("https://sqlite.org/2018/sqlite-autoconf-3240000.tar.gz",
-             "https://sqlite.org/2018/sqlite-autoconf-3230000.tar.gz")
+    set_urls("https://sqlite.org/2018/sqlite-autoconf-$(version)000.tar.gz",
+             {version = function (version) return version:gsub("%.", "") end})
 
     add_versions("3.24.0", "d9d14e88c6fb6d68de9ca0d1f9797477d82fc3aed613558f87ffbdbbc5ceb74a")
     add_versions("3.23.0", "b7711a1800a071674c2bf76898ae8584fc6c9643cfe933cfc1bc54361e3a6e49")
