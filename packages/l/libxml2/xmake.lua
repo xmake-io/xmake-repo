c0f6fc959b68202c33fb73dd7650d73ef187de20
ruki <waruqi@gmail.com>
2018-09-26 00:57:15 +0800

    add libxml2

--- packages/l/libxml2/xmake.lua
+++ packages/l/libxml2/xmake.lua
@@ -9,9 +9,14 @@ package("libxml2")
     add_versions("v2.9.7", "31dd4c0e10fa625b47e27fd6a5295d246c883f214da947b9a4a9e13733905ed9")
 
     on_build("windows", function (package)
+        os.cd("win32")
+        os.vrun("cscript configure.js iso8859x=yes iconv=no compiler=msvc cruntime=/MT debug=%s prefix=\"%s\"", package:debug() and "yes" or "no", package:installdir())
+        os.vrun("nmake /f Makefile.msvc")
     end)
 
     on_install("windows", function (package)
+        os.cd("win32")
+        os.vrun("nmake /f Makefile.msvc install")
     end)
 
     on_build("macosx", "linux", function (package)
