3bd1fcbe42c00da87e56c7f05d9ab6e6ca0701be
ruki <waruqi@gmail.com>
2018-09-28 00:12:41 +0800

    add excludes to libxml2

--- packages/l/libxml2/xmake.lua
+++ packages/l/libxml2/xmake.lua
@@ -3,7 +3,7 @@ package("libxml2")
     set_homepage("http://xmlsoft.org/")
     set_description("The XML C parser and toolkit of Gnome.")
 
-    set_urls("https://github.com/GNOME/libxml2/archive/$(version).zip")
+    set_urls("https://github.com/GNOME/libxml2/archive/$(version).zip", {excludes = {"*/doc/*", "*/result/*", "*/test/*"}})
 
     add_versions("v2.9.8", "c87793e45e66a7aa19200f861873f75195065de786a21c1b469bdb7bfc1230fb")
     add_versions("v2.9.7", "31dd4c0e10fa625b47e27fd6a5295d246c883f214da947b9a4a9e13733905ed9")
