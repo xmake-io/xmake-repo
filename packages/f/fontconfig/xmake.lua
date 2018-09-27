e88aa5dd314dddbdb9c3d1c1df15f6179cd273ff
ruki <waruqi@gmail.com>
2018-09-27 22:41:29 +0800

    add fontconfig

---
+++ packages/f/fontconfig/xmake.lua
@@ -0,0 +1,17 @@
+package("fontconfig")
+
+    set_homepage("https://www.freedesktop.org/wiki/Software/fontconfig/")
+    set_description("A library for configuring and customizing font access.")
+
+    set_urls("https://www.freedesktop.org/software/fontconfig/release/fontconfig-$(version).tar.gz")
+    add_versions("2.13.1", "9f0d852b39d75fc655f9f53850eb32555394f36104a044bb2b2fc9e66dbbfa7f")
+
+    add_deps("freetype")
+
+    on_build("linux", "macosx", function (package)
+        import("package.builder.autoconf").build(package)
+    end)
+
+    on_install("linux", "macosx", function (package)
+        import("package.builder.autoconf").install(package)
+    end)
