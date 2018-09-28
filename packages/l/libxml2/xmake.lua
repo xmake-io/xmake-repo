852112bf5e1cd208a1b4bd9935cb51e8f6ce71e8
ruki <waruqi@gmail.com>
2018-09-28 22:46:35 +0800

    add deps for libxml2

--- packages/l/libxml2/xmake.lua
+++ packages/l/libxml2/xmake.lua
@@ -8,6 +8,10 @@ package("libxml2")
     add_versions("v2.9.8", "c87793e45e66a7aa19200f861873f75195065de786a21c1b469bdb7bfc1230fb")
     add_versions("v2.9.7", "31dd4c0e10fa625b47e27fd6a5295d246c883f214da947b9a4a9e13733905ed9")
 
+    if is_plat("macosx", "linux") then
+        add_deps("libtool", "autoconf", "automake")
+    end
+
     on_build("windows", function (package)
         os.cd("win32")
         os.vrun("cscript configure.js iso8859x=yes iconv=no compiler=msvc cruntime=/MT debug=%s prefix=\"%s\"", package:debug() and "yes" or "no", package:installdir())
