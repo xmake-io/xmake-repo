b7d16b3aace79f5a3df407e37666a41e1bc49d7f
ruki <waruqi@gmail.com>
2018-10-06 00:45:53 +0800

    add zlib deps for libpng

--- packages/l/libpng/xmake.lua
+++ packages/l/libpng/xmake.lua
@@ -8,6 +8,7 @@ package("libpng")
     add_versions("v1.6.35", "3d22d46c566b1761a0e15ea397589b3a5f36ac09b7c785382e6470156c04247f")
     add_versions("v1.6.34", "7ffa5eb8f9f3ed23cf107042e5fec28699718916668bbce48b968600475208d3")
 
+    add_deps("zlib")
     if is_host("windows") then
         add_deps("cmake")
     end
