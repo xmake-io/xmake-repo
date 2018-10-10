d9d09c5eaf69bddfe7e2fbbb46e6a0769abb59a2
ruki <waruqi@gmail.com>
2018-10-10 23:41:28 +0800

    excludes tests for mbedtls

--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -3,7 +3,7 @@ package("mbedtls")
     set_homepage("https://tls.mbed.org")
     set_description("An SSL library")
 
-    set_urls("https://github.com/ARMmbed/mbedtls/archive/mbedtls-$(version).zip")
+    set_urls("https://github.com/ARMmbed/mbedtls/archive/mbedtls-$(version).zip", {excludes = "*/tests/*"})
 
     add_versions("2.13.0", "6e747350bc13e8ff51799daa50f74230c6cd8e15977da55dd59f57b23dcf70a6")
     add_versions("2.7.6", "e527d828ab82650102ca8031302e5d4bc68ea887b2d84e43d3da2a80a9e5a2c8")
