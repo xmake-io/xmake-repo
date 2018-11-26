f6ee70f49dea06218b8ba02049b0e647d392b3e2
ruki <waruqi@gmail.com>
2018-11-26 23:56:12 +0800

    add cflags to ncurses

--- packages/n/ncurses/xmake.lua
+++ packages/n/ncurses/xmake.lua
@@ -13,5 +13,5 @@ package("ncurses")
     end)
 
     on_install("linux", "macosx", function (package)
-        import("package.tools.autoconf").install(package, {"--without-manpages"})
+        import("package.tools.autoconf").install(package, {"--without-manpages", ["with-build-cflags"] = package:config("cflags")})
     end)
