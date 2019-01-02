4e7a9bd9d7c2b78ac07d46f57902b13e4406d901
ruki <waruqi@gmail.com>
2019-01-02 22:46:20 +0800

    improve cjson

--- packages/c/cjson/xmake.lua
+++ packages/c/cjson/xmake.lua
@@ -1,10 +1,23 @@
 package("cjson")
+
     set_homepage("https://github.com/DaveGamble/cJSON")
     set_description("Ultralightweight JSON parser in ANSI C.")
 
     set_urls("https://github.com/DaveGamble/cJSON/archive/v$(version).zip")
     add_versions("1.7.10", "80a0584410656c8d8da2ba703744f44d7535fc4f0778d8bf4f980ce77c6a9f65")
-    on_install("macosx", "linux", function(package)
-        os.vrun("make")
-        os.vrun("make PREFIX=\"%s\" install", package:installdir())
+
+    if is_host("windows") then
+        add_deps("cmake")
+    end
+
+    if is_plat("windows") and winos.version():gt("winxp") then
+        on_install("windows", function (package)
+            import("package.tools.cmake").install(package)
+        end)
+    end
+
+    on_install("macosx", "linux", function (package)
+        os.vrun("make static")
+        os.cp("*.a", package:installdir("lib"))
+        os.cp("*.h", package:installdir("include"))
     end)
