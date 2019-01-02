b0284faf5ebefcc96fa3dd21fd30c703495240ee
ruki <waruqi@gmail.com>
2019-01-02 22:52:41 +0800

    remove windows libs for cjson

--- packages/c/cjson/xmake.lua
+++ packages/c/cjson/xmake.lua
@@ -6,16 +6,6 @@ package("cjson")
     set_urls("https://github.com/DaveGamble/cJSON/archive/v$(version).zip")
     add_versions("1.7.10", "80a0584410656c8d8da2ba703744f44d7535fc4f0778d8bf4f980ce77c6a9f65")
 
-    if is_host("windows") then
-        add_deps("cmake")
-    end
-
-    if is_plat("windows") and winos.version():gt("winxp") then
-        on_install("windows", function (package)
-            import("package.tools.cmake").install(package)
-        end)
-    end
-
     on_install("macosx", "linux", function (package)
         os.vrun("make static")
         os.cp("*.a", package:installdir("lib"))
