10226ee024e7b3f1417c810816b248a898534943
ruki <waruqi@gmail.com>
2019-04-07 20:20:41 +0800

    fix pkg-config urls

--- packages/p/pkg-config/xmake.lua
+++ packages/p/pkg-config/xmake.lua
@@ -5,13 +5,9 @@ package("pkg-config")
     set_description("A helper tool used when compiling applications and libraries.")
 
     if is_host("macosx", "linux") then
-        add_urls("https://pkgconfig.freedesktop.org/releases/pkg-config-$(version).tar.gz", {alias = "freedesktop"})
-        add_urls("https://github.com/xmake-mirror/pkg-config/archive/pkg-config-$(version).tar.gz", {alias = "github"})
-        add_urls("https://gitlab.com/xmake-mirror/pkg-config/-/archive/pkg-config-$(version)/pkg-config-pkg-config-$(version).tar.gz", {alias = "gitlab"})
-        add_urls("https://gitlab.freedesktop.org/pkg-config/pkg-config.git")
-        add_versions("freedesktop:0.29.2", "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591")
-        add_versions("github:0.29.2", "67d89af4777653f81f18a6c42620fb51536fea469d412c932b9c7a411f62458a")
-        add_versions("gitlab:0.29.2", "67d89af4777653f81f18a6c42620fb51536fea469d412c932b9c7a411f62458a")
+        add_urls("https://pkgconfig.freedesktop.org/releases/pkg-config-$(version).tar.gz")
+		add_urls("http://fco.it.distfiles.macports.org/mirrors/macports-distfiles/pkgconfig/pkg-config-$(version).tar.g")
+        add_versions("0.29.2", "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591")
     end
 
     on_install("macosx", "linux", function (package)
