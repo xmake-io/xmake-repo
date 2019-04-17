2a3e49cad34c3ffbf22c31967a8c3c8f1d08a300
ruki <waruqi@gmail.com>
2019-04-17 23:55:21 +0800

    disable nana

--- packages/n/nana/xmake.lua
+++ packages/n/nana/xmake.lua
@@ -7,9 +7,10 @@ package("nana")
              "https://github.com/cnjinhao/nana.git")
     add_versions("1.6.2", "5f5cb791dff292e27bfa29d850b93f809a0d91d6044ea7e22ce7ae76a5d8b24e")
 
+    --[[ TODO
     on_install(function (package)
         import("package.tools.cmake").install(package)
-    end)
+    end)]]
 
     on_test(function (package)
         import("lib.detect.check_cxsnippets")
