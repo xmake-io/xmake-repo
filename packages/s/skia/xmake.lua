5299d65bc0d89269874edb6da36e4d05b8b7c418
ruki <waruqi@gmail.com>
2019-05-09 00:06:55 +0800

    add skia args

--- packages/s/skia/xmake.lua
+++ packages/s/skia/xmake.lua
@@ -11,16 +11,37 @@ package("skia")
     add_deps("python2", "ninja")
 
     on_install("macosx", "linux", "windows", function (package)
-        os.vrun("git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth 1")
+        --os.vrun("git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth 1")
         local pathes = os.getenv("PATH") or ""
         pathes = pathes .. path.envsep() .. path.join(os.curdir(), "depot_tools")
         pathes = pathes .. path.envsep() .. path.join(os.curdir(), "bin")
-        local args = {is_official_build = true, is_debug = package:debug()}
+        local args = {is_official_build = false,
+                      is_component_build = false,
+                      is_debug = package:debug(),
+                      skia_enable_gpu = true,
+                      skia_enable_pdf = false,
+                      skia_enable_nvpr = false,
+                      skia_use_icu = false,
+                      skia_use_sfntly = false,
+                      skia_use_piex = false,
+                      skia_use_freetype = false,
+                      skia_use_harfbuzz = false,
+                      skia_use_libheif = false,
+                      skia_use_expat = false,
+                      skia_use_libjpeg_turbo = false,
+                      skia_use_libpng = false,
+                      skia_use_libwebp = false,
+                      skia_use_zlib = false,
+                      skia_use_system_expat = false,
+                      skia_use_system_libjpeg_turbo = false,
+                      skia_use_system_libpng = false,
+                      skia_use_system_libwebp = false,
+                      skia_use_system_zlib = false}
         local argstr = ""
         for k, v in pairs(args) do
             argstr = argstr .. ' ' .. k .. '=' .. tostring(v)
         end
-        os.vrunv("python2", {"tools/git-sync-deps"}, {envs = {PATH = pathes}})
+        os.vrunv("python2", {"tools/git-sync-deps"})--, {envs = {PATH = pathes}})
         os.vrun("bin/gn gen build --args='%s'", argstr)
         os.vrun("ninja -C build")
     end)
