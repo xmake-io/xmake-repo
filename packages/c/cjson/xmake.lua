50be2d5cbac9cca42d28daddd293ea2713ba57af
ruki <waruqi@gmail.com>
2019-05-10 00:00:18 +0800

    improve cjson package

--- packages/c/cjson/xmake.lua
+++ packages/c/cjson/xmake.lua
@@ -3,11 +3,16 @@ package("cjson")
     set_homepage("https://github.com/DaveGamble/cJSON")
     set_description("Ultralightweight JSON parser in ANSI C.")
 
-    set_urls("https://github.com/DaveGamble/cJSON/archive/v$(version).zip")
+    set_urls("https://github.com/DaveGamble/cJSON/archive/v$(version).zip",
+             "https://github.com/DaveGamble/cJSON.git")
     add_versions("1.7.10", "80a0584410656c8d8da2ba703744f44d7535fc4f0778d8bf4f980ce77c6a9f65")
 
-    on_install("macosx", "linux", function (package)
-        os.vrun("make static")
+    on_install("macosx", "linux", "iphoneos", "android", function (package)
+        import("package.tools.make").build(package, {"static"})
         os.cp("*.a", package:installdir("lib"))
         os.cp("*.h", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("cJSON_malloc", {includes = "cJSON.h"}))
     end)
