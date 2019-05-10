50716af7b085fac5152840204f9c76c4a98e57ad
ruki <waruqi@gmail.com>
2019-05-10 22:36:13 +0800

    disable skia

--- packages/s/skia/xmake.lua
+++ packages/s/skia/xmake.lua
@@ -10,6 +10,7 @@ package("skia")
 
     add_deps("python2", "ninja")
 
+    --[[ TODO
     on_install("macosx", "linux", "windows", function (package)
         local pathes = os.getenv("PATH") or ""
         pathes = pathes .. path.envsep() .. path.join(os.curdir(), "depot_tools")
@@ -43,7 +44,7 @@ package("skia")
         os.vrun("python2 tools/git-sync-deps")
         os.vrun("bin/gn gen build --args='%s'", argstr)
         os.vrun("ninja -C build")
-    end)
+    end)]]
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
