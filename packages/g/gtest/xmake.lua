13a4fc06771b8cb23a768061d43fedd805f5a503
ruki <waruqi@gmail.com>
2019-09-03 22:59:34 +0800

    fix gtest

--- packages/g/gtest/xmake.lua
+++ packages/g/gtest/xmake.lua
@@ -3,13 +3,13 @@ package("gtest")
     set_homepage("https://github.com/google/googletest")
     set_description("Google Testing and Mocking Framework.")
 
-    add_urls("https://github.com/google/googletest/archive/release-$(version).tar.gz", {alias = "archive"})
+    add_urls("https://github.com/google/googletest/archive/release-$(version).zip", {alias = "archive"})
     add_urls("https://github.com/google/googletest.git", {alias = "github"})
     add_versions("github:1.8.1", "release-1.8.1")
-    add_versions("archive:1.8.1", "9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c")
+    add_versions("archive:1.8.1", "927827c183d01734cc5cfef85e0ff3f5a92ffe6188e0d18e909c5efebf28a0c7")
 
     on_install(function (package)
-        os.cp("googletest/include/", package:installdir())
+        os.cp("./googletest/include", package:installdir())
     end)
 
     on_test(function (package)
