75c2617a6c9eb26d421cc8cf0c37a8624eba2afb
ruki <waruqi@gmail.com>
2019-09-05 11:49:22 +0800

    Update xmake.lua

--- packages/s/spdlog/xmake.lua
+++ packages/s/spdlog/xmake.lua
@@ -3,10 +3,10 @@ package("spdlog")
     set_homepage("https://github.com/gabime/spdlog")
     set_description("Fast C++ logging library.")
 
-    set_urls("https://github.com/gabime/spdlog/archive/v$(version).tar.gz",
+    set_urls("https://github.com/gabime/spdlog/archive/v$(version).zip",
              "https://github.com/gabime/spdlog.git")
 
-    add_versions("1.3.1", "160845266e94db1d4922ef755637f6901266731c4cb3b30b45bf41efa0e6ab70")
+    add_versions("1.3.1", "db6986d0141546d4fba5220944cc1f251bd8afdfc434bda173b4b0b6406e3cd0")
 
     on_install(function (package)
         os.cp("include", package:installdir())
