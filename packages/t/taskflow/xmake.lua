baad8ba4a813d4db43a21defb7f72030d5330137
fasiondog <fasiondog@163.com>
2019-09-08 17:08:26 +0800

    add cpp taskflow lib

---
+++ packages/t/taskflow/xmake.lua
@@ -0,0 +1,35 @@
+package("taskflow")
+
+    set_homepage("https://cpp-taskflow.github.io/")
+    set_description("A fast C++ header-only library to help you quickly write parallel programs with complex task dependencies")
+
+    set_urls("https://github.com/cpp-taskflow/cpp-taskflow/archive/v$(version).zip")
+    add_versions("2.2.0", "6b3c3b083e6e93a988cebc8bbf794a78f61904efab21f1e3a667b3cf60d58ca2")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        import("package.tools.cmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <assert.h>
+            static void test(){
+                tf::Executor executor;
+                tf::Taskflow taskflow;
+                std::vector<int> range(10);
+                std::vector<int> out(10);
+                std::iota(range.begin(), range.end(), 0);
+                std::iota(out.begin(), out.end(), 0);
+                taskflow.parallel_for(range.begin(), range.end(), [&] (const int i) { 
+                    out[i] = i;
+                });
+                executor.run(taskflow).get();
+                for (int i = 0; i < 10; i++) {
+                    assert(out[i] == i);
+                }
+            }
+        ]]}, {configs = {languages = "c++1z"}, includes = "taskflow/taskflow.hpp"}))
+    end)    
+    
