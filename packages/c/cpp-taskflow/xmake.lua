176a425635f58afc7703a831cb7044f3e231674c
fasiondog <fasiondog@163.com>
2019-09-08 17:50:01 +0800

    add cpp-taskflow lib

--- packages/c/cpp-taskflow/xmake.lua
+++ packages/c/cpp-taskflow/xmake.lua
@@ -1,9 +1,10 @@
-package("taskflow")
+package("cpp-taskflow")
 
     set_homepage("https://cpp-taskflow.github.io/")
     set_description("A fast C++ header-only library to help you quickly write parallel programs with complex task dependencies")
 
-    set_urls("https://github.com/cpp-taskflow/cpp-taskflow/archive/v$(version).zip")
+    add_urls("https://github.com/cpp-taskflow/cpp-taskflow.git")
+    add_urls("https://github.com/cpp-taskflow/cpp-taskflow/archive/v$(version).zip")
     add_versions("2.2.0", "6b3c3b083e6e93a988cebc8bbf794a78f61904efab21f1e3a667b3cf60d58ca2")
 
     add_deps("cmake")
