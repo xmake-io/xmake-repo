097fa239c1f018c16e76d7d322b5be74c6ac9d94
ruki <waruqi@gmail.com>
2019-09-09 22:40:12 +0800

    improve taskflow

--- packages/c/cpp-taskflow/xmake.lua
+++ packages/c/cpp-taskflow/xmake.lua
@@ -7,16 +7,14 @@ package("cpp-taskflow")
     add_urls("https://github.com/cpp-taskflow/cpp-taskflow/archive/v$(version).zip")
     add_versions("2.2.0", "6b3c3b083e6e93a988cebc8bbf794a78f61904efab21f1e3a667b3cf60d58ca2")
 
-    add_deps("cmake")
-
     on_install(function (package)
-        import("package.tools.cmake").install(package)
+        os.cp("taskflow", package:installdir("include"))
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             #include <assert.h>
-            static void test(){
+            static void test() {
                 tf::Executor executor;
                 tf::Taskflow taskflow;
                 std::vector<int> range(10);
