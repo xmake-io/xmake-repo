b5268672d4e468bcaa0b5f0bf47802564c8a1e7f
ruki <waruqi@gmail.com>
2019-10-04 00:41:22 +0800

    enable nana for linux

--- packages/n/nana/xmake.lua
+++ packages/n/nana/xmake.lua
@@ -6,11 +6,11 @@ package("nana")
     add_urls("https://github.com/cnjinhao/nana/archive/v$(version).tar.gz",
              "https://github.com/cnjinhao/nana.git")
     add_versions("1.6.2", "5f5cb791dff292e27bfa29d850b93f809a0d91d6044ea7e22ce7ae76a5d8b24e")
+    add_versions("1.7.2", "e2efb3b7619e4ef3b6de93f8afc70ff477ec6cabf4f9740f0d786904c790613f")
 
-    --[[ TODO
-    on_install(function (package)
+    on_install("linux", function (package)
         import("package.tools.cmake").install(package)
-    end)]]
+    end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
