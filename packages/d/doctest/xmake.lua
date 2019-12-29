35c59589b435920413030b58f597378d934f8fa7
fasiondog <fasiondog@163.com>
2019-12-30 00:25:20 +0800

    add doctest v2.3.6

--- packages/d/doctest/xmake.lua
+++ packages/d/doctest/xmake.lua
@@ -6,6 +6,7 @@ package("doctest")
     set_urls("https://github.com/onqtam/doctest/archive/$(version).tar.gz",
              "https://github.com/onqtam/doctest.git")
 
+    add_versions("2.3.6", "f63c3c01021ba3fb35a0702127abfaa6fc44aaefd309e2c246e62a083deffa1f")
     add_versions("2.3.1", "b3d3c6133874e3a8c8e319cab33167156b6b1d2ed1ddde08c2655193cdeb58a0")
 
     on_install(function (package)
