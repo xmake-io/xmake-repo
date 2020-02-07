8faea5f44f94cdd6148c68b7f6fe82f23664e7c8
ruki <waruqi@gmail.com>
2020-02-08 00:48:38 +0800

    update mysql

--- packages/m/mysql/xmake.lua
+++ packages/m/mysql/xmake.lua
@@ -5,7 +5,7 @@ package("mysql")
 
     set_urls("https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-$(version).tar.gz")
 
-    add_versions("5.7.23", "d05700ec5c1c6dae9311059dc1713206c29597f09dbd237bf0679b3c6438e87a")
+    add_versions("5.7.29", "00f514124de2bad1ba7b380cbbd46e316cae7fc7bc3a5621456cabf352f27978")
 
     if is_plat("macosx", "linux") then
         add_deps("cmake", "openssl")
