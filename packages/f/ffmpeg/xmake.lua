4f1ac71c06b235adfb5018753fb8f1fd6d74a7b6
ruki <waruqi@gmail.com>
2020-03-12 23:45:07 +0800

    fix url

--- packages/f/ffmpeg/xmake.lua
+++ packages/f/ffmpeg/xmake.lua
@@ -4,7 +4,7 @@ package("ffmpeg")
     set_description("A collection of libraries to process multimedia content such as audio, video, subtitles and related metadata.")
 
     add_urls("https://ffmpeg.org/releases/ffmpeg-$(version).tar.bz2", {alias = "home"})
---    add_urls("https://github.com/FFmpeg/FFmpeg/archive/n$(version).zip", {alias = "github"})
+    add_urls("https://github.com/FFmpeg/FFmpeg/archive/n$(version).zip", {alias = "github"})
     add_urls("https://git.ffmpeg.org/ffmpeg.git", "https://github.com/FFmpeg/FFmpeg.git")
     add_versions("home:4.0.2", "346c51735f42c37e0712e0b3d2f6476c86ac15863e4445d9e823fe396420d056")
     add_versions("github:4.0.2", "4df1ef0bf73b7148caea1270539ef7bd06607e0ea8aa2fbf1bb34062a097f026")
