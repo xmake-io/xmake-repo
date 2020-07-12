aa10c16234a0d04f0354cc8d618bf3316e61c21e
raininfall <lygjcsrq@163.com>
2020-07-12 18:53:56 +0800

    Update xmake.lua

--- packages/g/gtest/xmake.lua
+++ packages/g/gtest/xmake.lua
@@ -18,16 +18,3 @@ package("gtest")
         ]])
         import("package.tools.xmake").install(package)
     end)
-
-    on_test(function (package)
-        assert(package:check_cxxsnippets({test = [[
-            int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
-            TEST(FactorialTest, Zero) {
-              testing::InitGoogleTest(0, (char**)0);
-              EXPECT_EQ(1, factorial(1));
-              EXPECT_EQ(2, factorial(2));
-              EXPECT_EQ(6, factorial(3));
-              EXPECT_EQ(3628800, factorial(10));
-            }
-        ]]}, {configs = {languages = "c++11"}, includes = "gtest/gtest.h"}))
-    end)
