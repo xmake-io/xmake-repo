8b5158f02801879678c907d667aceaeb93d529bc
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2020-08-28 21:20:20 +0200

    Add libsdl_gfx

---
+++ packages/l/libsdl_gfx/xmake.lua
@@ -0,0 +1,54 @@
+package("libsdl_gfx")
+    add_deps("libsdl")
+
+    set_homepage("https://www.ferzkopp.net/wordpress/2016/01/02/sdl_gfx-sdl2_gfx/")
+    set_description("Simple DirectMedia Layer primitives drawing library")
+
+    if is_plat("macosx", "linux") then
+        set_urls("https://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-$(version).tar.gz")
+        add_versions("1.0.4", "63e0e01addedc9df2f85b93a248f06e8a04affa014a835c2ea34bfe34e576262")
+    end
+
+    add_links("SDL2_gfx")
+
+    on_install("macosx", "linux", function (package)
+        local configs = {}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+        else
+            table.insert(configs, "--enable-shared=no")
+        end
+        import("package.tools.autoconf").install(package, configs)
+        local file_name = path.join(package:installdir("include"), "SDL2", "SDL2_framerate.h")
+        local f = io.open(file_name)
+        local content = f:read("*all")
+        f:close()
+
+        content = string.gsub(content, "\"SDL.h\"", "<SDL2/SDL.h>")
+
+        local f = io.open(file_name, "w")
+        f:write(content)
+        f:close()
+
+        local file_name = path.join(package:installdir("include"), "SDL2", "SDL2_gfxPrimitives.h")
+        local f = io.open(file_name)
+        local content = f:read("*all")
+        f:close()
+
+        content = string.gsub(content, "\"SDL.h\"", "<SDL2/SDL.h>")
+
+        local f = io.open(file_name, "w")
+        f:write(content)
+        f:close()
+
+        local file_name = path.join(package:installdir("include"), "SDL2", "SDL2_rotozoom.h")
+        local f = io.open(file_name)
+        local content = f:read("*all")
+        f:close()
+
+        content = string.gsub(content, "\"SDL.h\"", "<SDL2/SDL.h>")
+
+        local f = io.open(file_name, "w")
+        f:write(content)
+        f:close()
+    end)
