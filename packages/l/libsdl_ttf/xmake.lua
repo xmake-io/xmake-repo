9d2afb684b80db9420f0c09d0404f97c0e11fa9d
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2020-08-29 12:11:18 +0200

    Fix libsdl_ttf for linux and macosx

--- packages/l/libsdl_ttf/xmake.lua
+++ packages/l/libsdl_ttf/xmake.lua
@@ -1,5 +1,8 @@
 package("libsdl_ttf")
     add_deps("libsdl")
+    if is_plat("linux") or is_plat("macosx") then
+        add_deps("freetype")
+    end
 
     set_homepage("https://www.libsdl.org/projects/SDL_ttf/")
     set_description("Simple DirectMedia Layer text rendering library")
@@ -7,9 +10,9 @@ package("libsdl_ttf")
     if is_plat("windows", "mingw") then
         set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-$(version)-VC.zip")
         add_versions("2.0.15", "aab0d81f1aa6fe654be412efc85829f2b188165dca6c90eb4b12b673f93e054b")
-    -- else
-    --     set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).zip")
-    --     add_versions("2.0.15", "cdb72b5b1c3b27795fa128af36f369fee5d3e38a96c350855da0b81880555dbc")
+    else
+        set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).zip")
+        add_versions("2.0.15", "cdb72b5b1c3b27795fa128af36f369fee5d3e38a96c350855da0b81880555dbc")
     end
 
     add_links("SDL2_ttf")
@@ -32,23 +35,23 @@ package("libsdl_ttf")
         os.cp(path.join("lib", arch, "*.dll"), package:installdir("lib"))
     end)
 
-    -- on_install("macosx", "linux", function (package)
-    --     local configs = {}
-    --     if package:config("shared") then
-    --         table.insert(configs, "--enable-shared=yes")
-    --     else
-    --         table.insert(configs, "--enable-shared=no")
-    --     end
+    on_install("macosx", "linux", function (package)
+        local configs = {}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+        else
+            table.insert(configs, "--enable-shared=no")
+        end
 
-    --     table.insert(configs, "--with-sdl-prefix=" .. package:dep("libsdl"):installdir())
+        table.insert(configs, "--with-sdl-prefix=" .. package:dep("libsdl"):installdir())
 
-    --     import("package.tools.autoconf").install(package, configs)
-    --     local file_name = path.join(package:installdir("include"), "SDL2", "SDL_ttf.h")
-    --     local content = io.readfile(file_name)
+        import("package.tools.autoconf").install(package, configs)
+        local file_name = path.join(package:installdir("include"), "SDL2", "SDL_ttf.h")
+        local content = io.readfile(file_name)
 
-    --      content = content:gsub("\"SDL.h\"", "<SDL2/SDL.h>")
-    --      content = content:gsub("\"begin_code.h\"", "<SDL2/begin_code.h>")
-    --      content = content:gsub("\"close_code.h\"", "<SDL2/close_code.h>")
+         content = content:gsub("\"SDL.h\"", "<SDL2/SDL.h>")
+         content = content:gsub("\"begin_code.h\"", "<SDL2/begin_code.h>")
+         content = content:gsub("\"close_code.h\"", "<SDL2/close_code.h>")
 
-    --      io.writefile(file_name, content)
-    -- end)
+         io.writefile(file_name, content)
+    end)
