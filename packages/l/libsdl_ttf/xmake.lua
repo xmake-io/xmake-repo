6cf92d67a0ecee7e7c159faa240eb432cf89f9a7
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2020-08-29 11:30:47 +0200

    Add sdl prefix path to all sdl packages

--- packages/l/libsdl_ttf/xmake.lua
+++ packages/l/libsdl_ttf/xmake.lua
@@ -39,6 +39,9 @@ package("libsdl_ttf")
     --     else
     --         table.insert(configs, "--enable-shared=no")
     --     end
+
+    --     table.insert(configs, "--with-sdl-prefix=" .. package:dep("libsdl"):installdir())
+
     --     import("package.tools.autoconf").install(package, configs)
     --     local file_name = path.join(package:installdir("include"), "SDL2", "SDL_ttf.h")
     --     local content = io.readfile(file_name)
