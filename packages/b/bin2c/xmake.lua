dc8b8cc951cd5a71cd64c940cdb38b2ad5e2c01e
ruki <waruqi@gmail.com>
2020-09-16 00:12:06 +0800

    modify bin2c patch path

--- packages/b/bin2c/xmake.lua
+++ packages/b/bin2c/xmake.lua
@@ -7,7 +7,7 @@ package("bin2c")
     set_urls("https://github.com/gwilymk/bin2c.git")
 
     add_versions("0.0.1", "598395c23508bd4a2c916bfdab8c04a101abc62e")
-    add_patches("0.0.1", path.join(os.scriptdir(), "patch", "bin2c-test.patch"), "e3c8b80948dba824d8ffa0c3294f9b32ca3001c77a80b45f02a46a1e8586a7e1")
+    add_patches("0.0.1", path.join(os.scriptdir(), "patches", "0.0.1", "bin2c-test.patch"), "e3c8b80948dba824d8ffa0c3294f9b32ca3001c77a80b45f02a46a1e8586a7e1")
 
     on_install(function (package)
         if is_host("linux", "macosx") then
