a54408042cff3c1ce3d6695906fe036180d2d781
ruki <waruqi@gmail.com>
2020-10-16 22:45:14 +0800

    improve xorgproto

--- packages/x/xorgproto/xmake.lua
+++ packages/x/xorgproto/xmake.lua
@@ -6,7 +6,9 @@ package("xorgproto")
     set_urls("https://xorg.freedesktop.org/archive/individual/proto/xorgproto-$(version).tar.bz2")
     add_versions("2019.2", "46ecd0156c561d41e8aa87ce79340910cdf38373b759e737fcbba5df508e7b8e")
 
-    add_deps("pkg-config", "util-macros")
+    if is_plat("macosx", "linux") then
+        add_deps("pkg-config", "util-macros")
+    end
 
     on_install("macosx", "linux", function (package)
         local configs = {"--sysconfdir=" .. package:installdir("etc"),
