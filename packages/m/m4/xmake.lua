12f38f35452c0424ba38661a544af249607b6189
ruki <waruqi@gmail.com>
2020-10-16 23:23:12 +0800

    add m4 for autoconf

--- packages/m/m4/xmake.lua
+++ packages/m/m4/xmake.lua
@@ -8,13 +8,13 @@ package("m4")
              "https://ftpmirror.gnu.org/m4/m4-$(version).tar.xz")
     add_versions("1.4.18", "f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07")
 
-    if is_plat("macosx") then
+    if is_host("macosx") then
         -- fix crash from usage of %n in dynamic format strings on High Sierra
         -- patch credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
         add_patches("1.4.18", path.join(os.scriptdir(), "patches", "1.4.18", "secure_snprintf.patch"), "c0a408fbffb7255fcc75e26bd8edab116fc81d216bfd18b473668b7739a4158e")
     end
 
-    on_install("macosx", "linux", function (package)
+    on_install("@macosx", "@linux", function (package)
         import("package.tools.autoconf").install(package, {"--disable-dependency-tracking"})
     end)
 
