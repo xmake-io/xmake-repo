ac7f458e27d8481f2626f65a1b58ac3909dec2ec
xq114 <1140735506@qq.com>
2020-10-31 21:37:15 +0800

    test for zlib with -fPIC

--- packages/o/openexr/xmake.lua
+++ packages/o/openexr/xmake.lua
@@ -4,7 +4,7 @@ package("openexr")
     set_description("OpenEXR provides the specification and reference implementation of the EXR file format, the professional-grade image storage format of the motion picture industry.")
 
     add_urls("https://github.com/AcademySoftwareFoundation/openexr/archive/v$(version).tar.gz",
-             "https://github.com/AcademySoftwareFoundation/openexr")
+             "https://github.com/AcademySoftwareFoundation/openexr.git")
 
     add_versions("2.5.3", "6a6525e6e3907715c6a55887716d7e42d09b54d2457323fcee35a0376960bebf")
 
