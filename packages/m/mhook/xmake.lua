cd3d05139b269b212f0849f66d759d1ce0de571b
Maxime Griot <yamashi@live.com>
2020-11-06 14:48:35 +0100

    shared/static and debug/release configuration

--- packages/m/mhook/xmake.lua
+++ packages/m/mhook/xmake.lua
@@ -13,6 +13,8 @@ package("mhook")
 
     on_install("windows", function (package)
         local configs = {}
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
 
