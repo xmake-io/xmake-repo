20fdf65a849773869593b26e176e52ab68615edc
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-11-07 14:02:27 +0100

    Add 2.0.12 version of SDL2

--- packages/l/libsdl/xmake.lua
+++ packages/l/libsdl/xmake.lua
@@ -6,9 +6,11 @@ package("libsdl")
     if is_plat("windows", "mingw") then
         set_urls("https://www.libsdl.org/release/SDL2-devel-$(version)-VC.zip")
         add_versions("2.0.8", "68505e1f7c16d8538e116405411205355a029dcf2df738dbbc768b2fe95d20fd")
+        add_versions("2.0.12", "00c55a597cebdb9a4eb2723f2ad2387a4d7fd605e222c69b46099b15d5d8b32d")
     else
         set_urls("https://www.libsdl.org/release/SDL2-$(version).zip")
         add_versions("2.0.8", "e6a7c71154c3001e318ba7ed4b98582de72ff970aca05abc9f45f7cbdc9088cb")
+        add_versions("2.0.12", "476e84d6fcbc499cd1f4a2d3fd05a924abc165b5d0e0d53522c9604fe5a021aa")
     end
 
     if is_plat("macosx") then
