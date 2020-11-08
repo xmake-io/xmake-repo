9733e0e4051ea8835d795a0f7295254be9ec31da
Jérôme Leclercq <lynix680@gmail.com>
2020-11-08 11:18:12 +0100

    Add chipmunk2d

---
+++ packages/c/chipmunk2d/xmake.lua
@@ -0,0 +1,33 @@
+package("chipmunk2d")
+
+    set_homepage("https://chipmunk-physics.net/")
+    set_description(" A fast and lightweight 2D game physics library.")
+
+    set_urls("https://github.com/slembcke/Chipmunk2D/archive/Chipmunk-$(version).tar.gz",
+             "https://github.com/slembcke/Chipmunk2D.git")
+
+    add_versions("7.0.3", "1e6f093812d6130e45bdf4cb80280cb3c93d1e1833d8cf989d554d7963b7899a")
+
+    add_deps("cmake")
+
+    on_install("windows", "linux", "macosx", "iphoneos", "mingw", "android", function (package)
+        local configs = {"-BUILD_DEMOS=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        if package:config("shared") then
+            table.insert(configs, "-DBUILD_SHARED=ON")
+            table.insert(configs, "-DBUILD_STATIC=OFF")
+        else
+            table.insert(configs, "-DBUILD_SHARED=OFF")
+            table.insert(configs, "-DBUILD_STATIC=ON")
+        end
+       import("package.tools.cmake").install(package, configs)
+   end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                cpSpace* space = cpSpaceNew();
+                cpSpaceFree(space);
+            }
+        ]]}, {includes = {"chipmunk/chipmunk.h"}}))
+    end)
