2c97455538beffe118fbdfb702468e5c8d246e47
Jérôme Leclercq <lynix680@gmail.com>
2020-11-16 16:18:39 +0100

    libcurl: disable LDAP support on Windows

--- packages/l/libcurl/xmake.lua
+++ packages/l/libcurl/xmake.lua
@@ -35,6 +35,7 @@ package("libcurl")
         local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.includes(configs, "-DCURL_DISABLE_LDAP=ON")
         import("package.tools.cmake").install(package, configs)
     end)
 
