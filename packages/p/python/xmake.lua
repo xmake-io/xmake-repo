b543bbf0dec85685f3cdf4c88d1e86012e8ee212
xq114 <1140735506@qq.com>
2020-11-17 12:18:00 +0800

    update python

--- packages/p/python/xmake.lua
+++ packages/p/python/xmake.lua
@@ -4,24 +4,30 @@ package("python")
     set_description("The python programming language.")
 
     if is_host("windows") then
-        if os.arch() == "x64" then
+        if is_arch("x64", "x86_64") then
             add_urls("https://cdn.jsdelivr.net/gh/xmake-mirror/python-releases@$(version)/python-$(version).win64.tar.gz",
                      "https://github.com/xmake-mirror/python-releases/raw/$(version)/python-$(version).win64.tar.gz",
                      "https://gitlab.com/xmake-mirror/python-releases/-/raw/$(version)/python-$(version).win64.tar.gz")
             add_versions("2.7.15", "c81c4604b4176ff26be8d37cf48a2582e71a5e8f475b531c2e5d032a39511acb")
+            add_versions("2.7.18", "a51d27c9f64cd28415ea0a8fdcb2ffda113ce61267f5f05c9af7fd00e27c9376")
             add_versions("3.7.0", "8acd395e64d09b6b33ef78e199ffa48a8fd48f32d4d90d575e72448939a0e4c5")
+            add_versions("3.8.6", "87a3f900dc9cf3a72056198c764704355f2d624120f9cb3f2592c13874a9a479")
         else
             add_urls("https://cdn.jsdelivr.net/gh/xmake-mirror/python-releases@$(version)/python-$(version).win32.tar.gz",
                      "https://github.com/xmake-mirror/python-releases/raw/$(version)/python-$(version).win32.tar.gz",
                      "https://gitlab.com/xmake-mirror/python-releases/-/raw/$(version)/python-$(version).win32.tar.gz")
             add_versions("2.7.15", "4a7be2b440b74776662daaeb6bb6c5574bb6d0f4ddc0ad03ce63571ab2353303")
+            add_versions("2.7.18", "e80770ae6a10e8bccb56b378cb75a1c28c2762926205923b2fd51ce266e4baad")
             add_versions("3.7.0", "6f6dfd3df4b15157a12d06685a6dda450478ca118aa8832f0033093b9ca6329f")
+            add_versions("3.8.6", "999d1810c4f3e1dfc31dd50a3064fd973e041fc5aa2d3832516dd556b43e039c")
         end
     else
         set_urls("https://www.python.org/ftp/python/$(version)/Python-$(version).tgz",
                  "https://github.com/xmake-mirror/cpython/releases/download/v$(version)/Python-$(version).tgz")
         add_versions("2.7.15", "18617d1f15a380a919d517630a9cd85ce17ea602f9bbdc58ddc672df4b0239db")
+        add_versions("2.7.18", "da3080e3b488f648a3d7a4560ddee895284c3380b11d6de75edb986526b9a814")
         add_versions("3.7.0", "85bb9feb6863e04fb1700b018d9d42d1caac178559ffa453d7e6a436e259fd0d")
+        add_versions("3.8.6", "313562ee9986dc369cd678011bdfd9800ef62fbf7b1496228a18f86b36428c21")
     end
 
     if is_host("macosx", "linux") then
@@ -32,54 +38,30 @@ package("python")
         add_syslinks("util", "pthread", "dl")
     end
 
-    add_resources("3.7.0", "setuptools",  "https://files.pythonhosted.org/packages/c2/f7/c7b501b783e5a74cf1768bc174ee4fb0a8a6ee5af6afa92274ff964703e0/setuptools-40.8.0.zip", "6e4eec90337e849ade7103723b9a99631c1f0d19990d6e8412dc42f5ae8b304d")
-    add_resources("3.7.0", "pip",         "https://files.pythonhosted.org/packages/36/fa/51ca4d57392e2f69397cd6e5af23da2a8d37884a605f9e3f2d3bfdc48397/pip-19.0.3.tar.gz", "6e6f197a1abfb45118dbb878b5c859a0edbdd33fd250100bc015b67fded4b9f2")
-    add_resources("3.7.0", "wheel",       "https://files.pythonhosted.org/packages/b7/cf/1ea0f5b3ce55cacde1e84cdde6cee1ebaff51bd9a3e6c7ba4082199af6f6/wheel-0.33.1.tar.gz", "66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d")
+    on_load("@windows", "@msys", "@cygwin", function (package)
+        package:addenv("PATH", "bin")
 
-    add_resources("2.7.15", "setuptools", "https://files.pythonhosted.org/packages/c2/f7/c7b501b783e5a74cf1768bc174ee4fb0a8a6ee5af6afa92274ff964703e0/setuptools-40.8.0.zip", "6e4eec90337e849ade7103723b9a99631c1f0d19990d6e8412dc42f5ae8b304d")
-    add_resources("2.7.15", "pip",        "https://files.pythonhosted.org/packages/36/fa/51ca4d57392e2f69397cd6e5af23da2a8d37884a605f9e3f2d3bfdc48397/pip-19.0.3.tar.gz", "6e6f197a1abfb45118dbb878b5c859a0edbdd33fd250100bc015b67fded4b9f2")
-    add_resources("2.7.15", "wheel",      "https://files.pythonhosted.org/packages/b7/cf/1ea0f5b3ce55cacde1e84cdde6cee1ebaff51bd9a3e6c7ba4082199af6f6/wheel-0.33.1.tar.gz", "66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d")
+        -- set includedirs
+        package:add("includedirs", "include")
 
-    on_load(function (package)
+        -- set python environments
+        local envs = {}
+        envs.PYTHONPATH = package:installdir("Lib", "site-packages")
+        package:addenv("PYTHONPATH", envs.PYTHONPATH)
+    end)
 
-        -- add PATH
+    on_load("macosx", "linux", function (package)
         package:addenv("PATH", "bin")
 
         -- set includedirs
         local version = package:version()
-        if package:is_plat("windows") then
-            package:add("includedirs", "include")
-        elseif version:ge("3.0") then
-            package:add("includedirs", ("include/python%d.%dm"):format(version:major(), version:minor()))
-        else
-            package:add("includedirs", ("include/python%d.%d"):format(version:major(), version:minor()))
-        end
+        local pyver = ("python%d.%d"):format(version:major(), version:minor())
+        package:add("includedirs", path.join("include", pyver))
 
-        -- define install_resources()
-        package:data_set("install_resources", function()
-
-            -- imports
-            import("lib.detect.find_file")
-
-            -- set python environments
-            local envs = {}
-            if is_host("windows") and version:ge("3.0") then
-                envs.PYTHONPATH = package:installdir("Lib", "site-packages")
-            else
-                envs.PYTHONPATH = package:installdir("lib", "python" .. version:major() .. "." .. version:minor(), "site-packages")
-            end
-            package:addenv("PYTHONPATH", envs.PYTHONPATH)
-
-            -- install resources
-            local python = path.join(package:installdir("bin"), "python" .. (is_host("windows") and ".exe" or ""))
-            for _, name in ipairs({"setuptools", "pip", "wheel"}) do
-                local resourcedir = assert(package:resourcedir(name), "resource(%s): not found!", name)
-                local setupfile = assert(find_file("setup.py", path.join(resourcedir, "*")), "resource(%s): setup.py not found!", name)
-                local oldir = os.cd(path.directory(setupfile))
-                os.vrunv(python, {"setup.py", "install", "--prefix=" .. package:installdir()}, {envs = envs})
-                os.cd(oldir)
-            end
-        end)
+        -- set python environments
+        local envs = {}
+        envs.PYTHONPATH = package:installdir("lib", pyver, "site-packages")
+        package:addenv("PYTHONPATH", envs.PYTHONPATH)
     end)
 
     on_install("@windows", "@msys", "@cygwin", function (package)
@@ -90,15 +72,17 @@ package("python")
         end
         os.mv("*.exe", package:installdir("bin"))
         os.mv("*.dll", package:installdir("bin"))
+        os.mv("Lib", package:installdir())
         os.mv("libs/*", package:installdir("lib"))
-        os.cp("*", package:installdir())
-        package:data("install_resources")()
+        os.cp("**|libs", package:installdir())
+        local python = path.join(package:installdir("bin"), "python.exe")
+        os.vrunv(python, {"-m", "pip", "install", "wheel"})
     end)
 
-    on_install("@macosx", "@linux", function (package)
+    on_install("macosx", "linux", function (package)
 
         -- init configs
-        local configs = {"--enable-ipv6", "--without-ensurepip"}
+        local configs = {"--enable-ipv6", "--with-ensurepip"}
         table.insert(configs, "--datadir=" .. package:installdir("share"))
         table.insert(configs, "--datarootdir=" .. package:installdir("share"))
 
@@ -156,8 +140,8 @@ package("python")
             os.cp(path.join(package:installdir("bin"), "python3-config"), path.join(package:installdir("bin"), "python-config"))
         end
 
-        -- install resources
-        package:data("install_resources")()
+        local python = path.join(package:installdir("bin"), "python")
+        os.vrunv(python, {"-m", "pip", "install", "wheel"})
     end)
 
     on_test(function (package)
