428e1105c9eb973bb9764e104df47ff231f8a95b
MoAlyousef <may642_2000@hotmail.com>
2020-11-25 22:48:24 +0300

    fix option_shared_libs

--- packages/f/fltk/xmake.lua
+++ packages/f/fltk/xmake.lua
@@ -19,7 +19,7 @@ package("fltk")
 
     on_install("macosx", "windows", "mingw", "linux", function (package)
         local configs = {}
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DOPTION_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DFLTK_BUILD_TEST=OFF")
         if package:is_plat("linux") then
