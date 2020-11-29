8e17335f1a0ee7bc4124f097e820373a6e227c45
Hoildkv <42310255+xq114@users.noreply.github.com>
2020-11-29 17:55:31 +0800

    add nettle, gmp, sfntly and libtasn1 (#170)
    * add nettle
    
    * add gmp
    
    * add sfntly
    
    * update libxcb
    
    * add libtasn1
    
    * add license for nettle

---
+++ packages/l/libtasn1/xmake.lua
@@ -0,0 +1,24 @@
+package("libtasn1")
+
+    set_homepage("https://www.gnu.org/software/libtasn1/")
+    set_description("Libtasn1 is the ASN.1 library used by GnuTLS, p11-kit and some other packages.")
+    set_license("LGPL-2.1")
+
+    add_urls("https://ftp.gnu.org/gnu/libtasn1/libtasn1-$(version).tar.gz")
+    add_versions("4.15.0", "dd77509fe8f5304deafbca654dc7f0ea57f5841f41ba530cff9a5bf71382739e")
+
+    on_install("macosx", "linux", function (package)
+        local configs = {"--disable-doc", "--disable-dependency-tracking", "--with-pic"}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared")
+            table.insert(configs, "--disable-static")
+        else
+            table.insert(configs, "--disable-shared")
+            table.insert(configs, "--enable-static")
+        end
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("asn1_create_element", {includes = "libtasn1.h"}))
+    end)
--- packages/x/xcb-proto/xmake.lua
+++ packages/x/xcb-proto/xmake.lua
@@ -3,8 +3,9 @@ package("xcb-proto")
     set_homepage("https://www.x.org/")
     set_description("X.Org: XML-XCB protocol descriptions for libxcb code generation")
 
-    set_urls("https://xcb.freedesktop.org/dist/xcb-proto-$(version).tar.bz2")
-    add_versions("1.13", "7b98721e669be80284e9bbfeab02d2d0d54cd11172b72271e47a2fe875e2bde1")
+    set_urls("https://xcb.freedesktop.org/dist/xcb-proto-$(version).tar.gz")
+    add_versions("1.13", "0698e8f596e4c0dbad71d3dc754d95eb0edbb42df5464e0f782621216fa33ba7")
+    add_versions("1.14", "1c3fa23d091fb5e4f1e9bf145a902161cec00d260fabf880a7a248b02ab27031")
 
     if is_plat("macosx", "linux") then
         add_deps("pkg-config", "python 3.x")
