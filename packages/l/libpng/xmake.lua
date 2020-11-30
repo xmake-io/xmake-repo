472b94ec31e9a5f9dd7c6c96cde3d90e5240c2ef
Hoildkv <42310255+xq114@users.noreply.github.com>
2020-11-30 22:55:11 +0800

    add lcms, lzma as well as update to previous packages (#175)
    * add lcms
    
    * add lzma
    
    * update libpng
    
    * modify vulkan-headers support non-stable versions
    
    * update libtiff
    
    * update libpng versions

--- packages/l/libpng/xmake.lua
+++ packages/l/libpng/xmake.lua
@@ -5,6 +5,7 @@ package("libpng")
 
     set_urls("https://github.com/glennrp/libpng/archive/$(version).zip",
              "https://github.com/glennrp/libpng.git")
+    add_versions("v1.6.37", "c2c50c13a727af73ecd3fc0167d78592cf5e0bca9611058ca414b6493339c784")
     add_versions("v1.6.36", "6274d3f761cc80f7f6e2cde6c07bed10c00bc4ddd24c4f86e25eb51affa1664d")
     add_versions("v1.6.35", "3d22d46c566b1761a0e15ea397589b3a5f36ac09b7c785382e6470156c04247f")
     add_versions("v1.6.34", "7ffa5eb8f9f3ed23cf107042e5fec28699718916668bbce48b968600475208d3")
@@ -15,21 +16,24 @@ package("libpng")
         add_deps("cmake")
     end
 
-    if is_plat("windows") then
-        add_links("libpng16_static")
-    else
-        add_links("png")
-    end
-
     on_install("windows", function (package)
         local configs = {"-DPNG_TESTS=OFF",
-                         "-DPNG_SHARED=OFF",
+                         "-DPNG_SHARED=" .. (package:config("shared") and "ON" or "OFF"),
+                         "-DPNG_STATIC=" .. (package:config("shared") and "OFF" or "ON"),
                          "-DPNG_DEBUG=" .. (package:debug() and "ON" or "OFF")}
         import("package.tools.cmake").install(package, configs)
     end)
 
     on_install("macosx", "linux", function (package)
-        import("package.tools.autoconf").install(package, {"--disable-dependency-tracking", "--disable-silent-rules", "--enable-shared=no"})
+        local configs = {"--disable-dependency-tracking", "--disable-silent-rules"}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+            table.insert(configs, "--enable-static=no")
+        else
+            table.insert(configs, "--enable-static=yes")
+            table.insert(configs, "--enable-shared=no")
+        end
+        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_install("iphoneos", "android@linux,macosx", function (package)
@@ -40,7 +44,15 @@ package("libpng")
             envs.CPPFLAGS = (envs.CPPFLAGS or "") .. " -I" .. os.args(path.join(zlib:installdir(), "include"))
             envs.LDFLAGS = (envs.LDFLAGS or "") .. " -L" .. os.args(path.join(zlib:installdir(), "lib"))
         end
-        autoconf.install(package, {"--disable-dependency-tracking", "--disable-silent-rules", "--enable-shared=no"}, {envs = envs})
+        local configs = {"--disable-dependency-tracking", "--disable-silent-rules"}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+            table.insert(configs, "--enable-static=no")
+        else
+            table.insert(configs, "--enable-static=yes")
+            table.insert(configs, "--enable-shared=no")
+        end
+        autoconf.install(package, configs, {envs = envs})
     end)
 
     on_test(function (package)
---
+++ packages/l/lzma/xmake.lua
@@ -0,0 +1,29 @@
+package("lzma")
+
+    set_homepage("https://www.7-zip.org/sdk.html")
+    set_description("LZMA SDK")
+
+    add_urls("https://www.7-zip.org/a/lzma1900.7z", {version = function (version) return version:gsub("%.", "") end})
+    add_versions("19.00", "00f569e624b3d9ed89cf8d40136662c4c5207eaceb92a70b1044c77f84234bad")
+
+    on_install("windows", "linux", "macosx", function (package)
+        os.cd("C")
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            target("lzma")
+                set_kind("$(kind)")
+                add_files("Alloc.c", "LzFind.c",  "Lzma2Dec.c", "Lzma2Enc.c", "LzmaDec.c", "LzmaEnc.c", "LzmaLib.c")
+                add_headerfiles("7zTypes.h", "LzFind.h", "LzHash.h", "Lzma2Dec.h", "Lzma2Enc.h", "LzmaDec.h", "LzmaEnc.h", "LzmaLib.h")
+                if is_plat("windows") then
+                    add_files("LzFindMt.c", "MtCoder.c", "MtDec.c", "Threads.c", "DllSecur.c", "Lzma2DecMt.c")
+                    add_headerfiles("LzFindMt.h", "Lzma2DecMt.h")
+                else
+                    add_defines("_7ZIP_ST")
+                end
+        ]])
+        import("package.tools.xmake").install(package, {kind = package:config("shared") and "shared" or "static"})
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("LzmaCompress", {includes = "LzmaLib.h"}))
+    end)
