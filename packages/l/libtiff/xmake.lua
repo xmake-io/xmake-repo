565bd052f9d0d34c91c5291cc848e63e71e6b515
xq114 <1140735506@qq.com>
2020-11-30 16:54:46 +0800

    update libtiff

--- packages/l/libtiff/xmake.lua
+++ packages/l/libtiff/xmake.lua
@@ -7,18 +7,26 @@ package("libtiff")
              "https://fossies.org/linux/misc/tiff-$(version).tar.gz")
     add_versions("4.1.0", "5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634")
 
-    add_links("tiff")
-
+    add_deps("zlib")
     if is_plat("windows") then
         add_deps("cmake")
     end
 
     on_install("windows", function (package)
-        import("package.tools.cmake").install(package)
+        local configs = {}
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {"--disable-dependency-tracking", "--disable-lzma", "--without-x"}
+        local configs = {"--disable-dependency-tracking", "--disable-lzma", "--disable-webp", "--disable-jpeg", "--disable-zstd", "--disable-old-jpeg", "--disable-jbig", "--disable-pixarlog", "--without-x"}
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+            table.insert(configs, "--enable-static=no")
+        else
+            table.insert(configs, "--enable-static=yes")
+            table.insert(configs, "--enable-shared=no")
+        end
         import("package.tools.autoconf").install(package, configs)
     end)
 
