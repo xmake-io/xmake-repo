ad804370780f97f59ac2b31d3a5677df18824501
yecate <yecate@qq.com>
2020-11-30 15:12:30 +0800

    add lzo and modify xz support windows

--- packages/x/xz/xmake.lua
+++ packages/x/xz/xmake.lua
@@ -5,12 +5,24 @@ package("xz")
 
     set_urls("https://downloads.sourceforge.net/project/lzmautils/xz-$(version).tar.gz",
              "https://tukaani.org/xz/xz-$(version).tar.gz")
-    add_versions("5.2.4", "b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145")
+    add_versions("5.2.5", "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10")
 
     on_load(function (package)
         if is_plat(os.host()) then
             package:addenv("PATH", "bin")
         end
+        if package:is_plat("windows") and not package:config("shared") then
+            package:add("defines", "LZMA_API_STATIC")
+        end
+    end)
+
+    on_install("windows", "mingw@windows", function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, configs)
     end)
 
     on_install("macosx", "linux", "mingw@linux,macosx", function (package)
@@ -27,8 +39,5 @@ package("xz")
     end)
 
     on_test(function (package)
-        if is_plat(os.host()) then
-            os.vrun("xz --version")
-        end
         assert(package:has_cfuncs("lzma_code", {includes = "lzma.h"}))
     end)
