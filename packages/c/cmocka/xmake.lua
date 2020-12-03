568a0f57c5a6886e980395c3f20f814be933373c
Hoildkv <42310255+xq114@users.noreply.github.com>
2020-12-04 00:27:19 +0800

    add cereal, libunistring, cmocka (#191)
    * fix yaml-cpp msvc runtime
    
    * update libiconv
    
    * add cereal
    
    * add libunistring
    
    * add cmocka

---
+++ packages/c/cmocka/xmake.lua
@@ -0,0 +1,19 @@
+package("cmocka")
+
+    set_homepage("https://cmocka.org/")
+    set_description("cmocka is an elegant unit testing framework for C with support for mock objects.")
+    set_license("Apache-2.0")
+
+    add_urls("https://cmocka.org/files/$(version).tar.xz", {version = function (version) return table.concat(table.slice(version:split('%.'), 1, 2), '.') .. "/cmocka-" .. version end})
+    add_versions("1.1.5", "f0ccd8242d55e2fd74b16ba518359151f6f8383ff8aef4976e48393f77bba8b6")
+
+    add_deps("cmake")
+    on_install("windows", "linux", "macosx", "mingw", function (package)
+        local configs = {"-DUNIT_TESTING=OFF", "-DWITH_EXAMPLES=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("cmocka_set_test_filter", {includes = {"stdarg.h", "stddef.h", "setjmp.h", "stdint.h", "cmocka.h"}}))
+    end)
--- packages/l/libiconv/xmake.lua
+++ packages/l/libiconv/xmake.lua
@@ -5,6 +5,7 @@ package("libiconv")
 
     set_urls("https://ftp.gnu.org/gnu/libiconv/libiconv-$(version).tar.gz",
              "https://ftpmirror.gnu.org/libiconv/libiconv-$(version).tar.gz")
+    add_versions("1.16", "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04")
     add_versions("1.15", "ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178")
 
     if is_plat("macosx") then
@@ -17,11 +18,18 @@ package("libiconv")
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {"--enable-static", "--enable-shared=no", "--disable-dependency-tracking", "--enable-extra-encodings"}
+        local configs = {"--disable-dependency-tracking", "--enable-extra-encodings"}
         if package:debug() then
             table.insert(configs, "--enable-debug")
         else
             table.insert(configs, "--disable-debug")
+        end
+        if package:config("shared") then
+            table.insert(configs, "--enable-shared=yes")
+            table.insert(configs, "--enable-static=no")
+        else
+            table.insert(configs, "--enable-static=yes")
+            table.insert(configs, "--enable-shared=no")
         end
         os.vrunv("make", {"-f", "Makefile.devel", "CFLAGS=" .. (package:config("cflags") or "")})
         import("package.tools.autoconf").install(package, configs)
