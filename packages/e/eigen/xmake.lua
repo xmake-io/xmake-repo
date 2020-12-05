9221efb44debc11428f7fbf80c4d9b901c479746
xq114 <1140735506@qq.com>
2020-12-05 23:48:50 +0800

    update eigen

--- packages/e/eigen/xmake.lua
+++ packages/e/eigen/xmake.lua
@@ -5,8 +5,9 @@ package("eigen")
 
     add_urls("https://gitlab.com/libeigen/eigen/-/archive/$(version)/eigen-$(version).tar.bz2",
              "https://gitlab.com/libeigen/eigen")
-    add_versions("3.3.8", "0215c6593c4ee9f1f7f28238c4e8995584ebf3b556e9dbf933d84feb98d5b9ef")
     add_versions("3.3.7", "685adf14bd8e9c015b78097c1dc22f2f01343756f196acdc76a678e1ae352e11")
+    add_versions("3.3.8", "0215c6593c4ee9f1f7f28238c4e8995584ebf3b556e9dbf933d84feb98d5b9ef")
+    add_versions("3.3.9", "0fa5cafe78f66d2b501b43016858070d52ba47bd9b1016b0165a7b8e04675677")
 
     add_deps("cmake")
     add_includedirs("include")
