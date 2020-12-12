99e2537b8529fee730a86cf37ece63d040f8eebf
Jérôme Leclercq <lynix680@gmail.com>
2020-12-12 14:28:02 +0100

    libflac: Remove CMAKE_POSITION_INDEPENDENT_CODE

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -29,12 +29,7 @@ package("libflac")
         table.insert(configs, "-DBUILD_EXAMPLES=OFF")
         table.insert(configs, "-DBUILD_TESTING=OFF")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        if package:config("shared") then
-            table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
-            table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
-        else
-            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
-        end
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
    end)
 
