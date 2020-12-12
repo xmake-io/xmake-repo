6de38af4ab26fb2706c6cca74f202eaa15c3d3c4
Jérôme Leclercq <lynix680@gmail.com>
2020-12-12 14:55:18 +0100

    libvorbis: Add libogg as a dependency

--- packages/l/libvorbis/xmake.lua
+++ packages/l/libvorbis/xmake.lua
@@ -9,7 +9,7 @@ package("libvorbis")
 
     add_versions("1.3.7", "0e982409a9c3fc82ee06e08205b1355e5c6aa4c36bca58146ef399621b0ce5ab")
 
-    add_deps("cmake")
+    add_deps("cmake", "libogg")
 
     on_install("windows", "linux", "macosx", "iphoneos", "mingw", "android", function (package)
         local configs = {}
