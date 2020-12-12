a60aecf5e94d005bec72c98baf7a53a53355cc21
Jérôme Leclercq <lynix680@gmail.com>
2020-12-12 13:55:51 +0100

    libogg: Always enable -fPIC

--- packages/l/libogg/xmake.lua
+++ packages/l/libogg/xmake.lua
@@ -12,12 +12,8 @@ package("libogg")
     on_install("windows", "macosx", "linux", "mingw", "iphoneos", "android", "cross", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        if package:config("shared") then
-            table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
-            table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
-        else
-            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
-        end
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
         import("package.tools.cmake").install(package, configs)
     end)
 
