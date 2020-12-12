153c56487f50a4262602602b3cf01d92bd235660
Jérôme Leclercq <lynix680@gmail.com>
2020-12-12 21:50:38 +0100

    libflac: Fix static lib building on Windows

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -8,7 +8,7 @@ package("libflac")
              "https://github.com/xiph/flac.git")
 
     add_versions("1.3.3", "668cdeab898a7dd43cf84739f7e1f3ed6b35ece2ef9968a5c7079fe9adfe1689")
-    add_patches("1.3.3", path.join(os.scriptdir(), "patches", "1.3.3", "cmake.patch"), "41fb73c034dbb6f4e963b50c4d55294492a401a8a77f2bccefd1b2bab002dab2")
+    add_patches("1.3.3", path.join(os.scriptdir(), "patches", "1.3.3", "cmake.patch"), "480e1645aa80d64526ba45c73a00703843a6b1764e4e3444b6115d1ead0a90c0")
 
     add_deps("cmake", "libogg")
 
