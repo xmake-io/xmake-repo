7f4d10e8f3a5099c4085c838b221bf8febf2693f
Jérôme Leclercq <lynix680@gmail.com>
2020-12-14 15:28:33 +0100

    Fix replacement

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -38,7 +38,7 @@ package("libflac")
         local libogg = package:dep("libogg"):fetch()
         if libogg then
             local links = table.concat(table.wrap(libogg.links), " ")
-            io.replace("CMakeLists.txt", "find_package(Ogg REQUIRED)", "", {plain = true})
+            io.replace("CMakeLists.txt", "find_package(OGG REQUIRED)", "", {plain = true})
             io.replace("src/libFLAC/CMakeLists.txt", 
             [[
 if(TARGET Ogg::ogg)
