5c7c68a089dd47f84dce15ffd641b185a3a6f499
yamashi <yamashi@users.noreply.github.com>
2020-12-22 15:14:02 +0100

    Add TiltedCore

---
+++ packages/t/TiltedCore/xmake.lua
@@ -0,0 +1,29 @@
+package("TiltedCore")
+
+    set_homepage("https://github.com/tiltedphoques/TiltedCore")
+    set_description("Core library from Tilted Phoques")
+
+    add_urls("https://github.com/tiltedphoques/TiltedCore/archive/$(version).zip")
+    add_urls("https://github.com/tiltedphoques/TiltedCore.git")
+
+    add_versions("v0.1.0", "7ce69086a3d4275a68877f443e682781275447ca0cc89b495428659410813655")
+
+    add_includedirs("Code/core/include")
+    add_links("mimalloc")
+
+    on_load(function (package)
+        package:add("mimalloc")
+    end)
+
+    on_install("linux", "macosx", "windows", "msys", function (package)
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test(int args, char** argv) {
+                Outcome<int, float> outcome;
+            }
+        ]]}, {includes = {"Outcome.hpp"}}))
+    end)
