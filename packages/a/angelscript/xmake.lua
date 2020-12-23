72d9e68bc02c35bd6984efe9eff42847244a3060
yecate <yecate@qq.com>
2020-12-23 19:56:42 +0800

    upgrade angelscript version to 2.35.0 (#226)
    * upgrade version to 2.35.0
    
    * remove iphoneos/macos

--- packages/a/angelscript/xmake.lua
+++ packages/a/angelscript/xmake.lua
@@ -1,8 +1,11 @@
 package("angelscript")
+
     set_homepage("http://angelcode.com/angelscript/")
     set_description("Extremely flexible cross-platform scripting library designed to allow applications to extend their functionality through external scripts")
-    add_urls("http://angelcode.com/angelscript/sdk/files/angelscript_$(version).zip")
+    set_license("zlib")
 
+    add_urls("http://angelcode.com/angelscript/sdk/files/angelscript_$(version).zip")
+    add_versions("2.35.0", "010dd45e23e734d46f5891d70e268607a12cb9ab12503dda42f842d9db7e8857")
     add_versions("2.34.0", "6faa043717522ae0fb2677d907ca5b0e35a79d28e5f83294565e6c6229bfbdf7")
 
     if is_plat("linux") then
@@ -18,7 +21,7 @@ package("angelscript")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         if package:is_plat("android") then
             io.gsub("CMakeLists.txt", "set_property", "#set_property")
-        end    
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
