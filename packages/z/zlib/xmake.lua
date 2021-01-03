1860289d691db91009dcac07973fb369e009faf8
ruki <waruqi@gmail.com>
2021-01-03 21:39:32 +0800

    fix zlib

--- packages/z/zlib/xmake.lua
+++ packages/z/zlib/xmake.lua
@@ -3,14 +3,10 @@ package("zlib")
     set_homepage("http://www.zlib.net")
     set_description("A Massively Spiffy Yet Delicately Unobtrusive Compression Library")
 
-    add_urls("https://github.com/madler/zlib/archive/v$(version).tar.gz", {alias = "github"})
-    add_urls("http://zlib.net/zlib-$(version).tar.gz", {alias = "homepage"})
+    add_urls("https://github.com/madler/zlib/archive/v$(version).tar.gz")
 
-    add_versions("homepage:1.2.10", "8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017")
-    add_versions("homepage:1.2.11", "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
-
-    add_versions("github:1.2.10", "42cd7b2bdaf1c4570e0877e61f2fdc0bce8019492431d054d3d86925e5058dc5")
-    add_versions("github:1.2.11", "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff")
+    add_versions("1.2.10", "42cd7b2bdaf1c4570e0877e61f2fdc0bce8019492431d054d3d86925e5058dc5")
+    add_versions("1.2.11", "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff")
 
     on_install("windows", function (package)
         io.gsub("win32/Makefile.msc", "%-MD", "-" .. package:config("vs_runtime"))
