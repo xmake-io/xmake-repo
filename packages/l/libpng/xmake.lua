bb971a0ae5c9230cbc7f7d3868a08a37d2041f59
ruki <waruqi@gmail.com>
2021-01-06 00:58:39 +0800

    add monkey ci

--- packages/l/libpng/xmake.lua
+++ packages/l/libpng/xmake.lua
@@ -13,7 +13,7 @@ package("libpng")
 
     add_deps("zlib")
     if is_plat("windows", "mingw") then
-        add_deps("cmake >3.12")
+        add_deps("cmake >=3.13")
     end
 
     on_install("windows", "mingw", function (package)
