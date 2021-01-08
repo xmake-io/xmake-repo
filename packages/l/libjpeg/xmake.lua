3a8ce31db4ebb29357ec0c41e3846c428247a812
ruki <waruqi@gmail.com>
2021-01-08 22:36:46 +0800

    fix libjpeg url

--- packages/l/libjpeg/xmake.lua
+++ packages/l/libjpeg/xmake.lua
@@ -3,7 +3,7 @@ package("libjpeg")
     set_homepage("http://ijg.org/")
     set_description("A widely used C library for reading and writing JPEG image files.")
 
-    set_urls("http://www.ijg.org/files/jpegsrc.$(version).tar.gz")
+    set_urls("https://www.ijg.org/files/jpegsrc.$(version).tar.gz")
 
     add_versions("v9c", "650250979303a649e21f87b5ccd02672af1ea6954b911342ea491f351ceb7122")
     add_versions("v9b", "240fd398da741669bf3c90366f58452ea59041cacc741a489b99f2f6a0bad052")
