9f921c0b6f1f42dc4007f10ba0990721a3af6e98
yamashi <yamashi@users.noreply.github.com>
2021-01-22 17:07:34 +0100

    Create xmake.lua

---
+++ packages/m/mem/xmake.lua
@@ -0,0 +1,18 @@
+package("mem")
+
+    set_homepage("https://github.com/0x1F9F1/mem")
+    set_description("A collection of C++11 headers useful for reverse engineering")
+
+    set_urls("https://github.com/0x1F9F1/mem.git")
+    
+    add_versions("0.1.0", "2be6647b8f4c5cdbbd6799eaca80ad6bda07cb2097a0ddd705acff748507f615")
+
+    on_install(function (package)
+        local configs = {}
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxfuncs("mem::module::main()", {includes = "mem/module.h", configs = {languages = "c++11"}}))
+    end)
