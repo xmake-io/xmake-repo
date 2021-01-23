e9aee0494c2a4072ef1dabc1d8a0cea6aaff4858
yamashi <yamashi@users.noreply.github.com>
2021-01-23 01:29:02 +0100

    Fix mem again (#246)
    * Fix mem again
    
    * Update xmake.lua

--- packages/m/mem/xmake.lua
+++ packages/m/mem/xmake.lua
@@ -3,17 +3,12 @@ package("mem")
     set_homepage("https://github.com/0x1F9F1/mem")
     set_description("A collection of C++11 headers useful for reverse engineering")
 
-    set_urls("https://github.com/0x1F9F1/mem")
+    set_urls("https://github.com/0x1F9F1/mem.git")
 
     add_versions("0.1.0", "db0289a50da77101c4e827b92b39f06ba2e90f76")
 
-    if is_plat("windows") then
-        add_deps("cmake")
-    end
-
     on_install("windows", function (package)
-        local configs = {}
-        import("package.tools.cmake").install(package, configs)
+        os.cp("include/mem", package:installdir("include"))
     end)
 
     on_test(function (package)
