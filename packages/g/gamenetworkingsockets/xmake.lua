dffa5702ded2c2b47a3a2f4175bfc7acc8329201
yamashi <yamashi@live.com>
2021-01-25 17:06:57 +0100

    Update xmake.lua

--- packages/g/gamenetworkingsockets/xmake.lua
+++ packages/g/gamenetworkingsockets/xmake.lua
@@ -15,11 +15,15 @@ package("gamenetworkingsockets")
             if is_plat("windows") then
                 package:add("deps", "libsodium", "protobuf-cpp")
                 package:add("syslinks", "ws2_32")
-                package:add("defines", "_WINDOWS", "WIN32")
             else
                 package:add("deps", "openssl", "protobuf-cpp", {configs = {cxflags = "-fpic"}})
-                package:add("defines", "POSIX", "LINUX")
             end
+        end
+
+        if is_plat("windows") then
+            package:add("defines", "_WINDOWS", "WIN32")
+        else
+            package:add("defines", "POSIX", "LINUX")
         end
     end)
 
