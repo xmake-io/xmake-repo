481c78c867207df951a16e6a854a1f3698384400
yamashi <yamashi@live.com>
2021-01-25 15:06:07 +0100

    Add steam's GameNetworkingSockets

---
+++ packages/g/gamenetworkingsockets/port/xmake.lua
@@ -0,0 +1,87 @@
+set_xmakever("2.5.1")
+
+set_languages("cxx11")
+
+add_rules("mode.debug", "mode.release")
+if is_mode("release") then
+    add_ldflags("/LTCG", "/OPT:REF")
+    add_cxflags("/Ot", "/GL", "/Ob2", "/Oi", "/GS-")
+    add_defines("NDEBUG")
+    set_optimize("fastest")
+end
+
+add_requires("protobuf-cpp")
+if is_plat("windows") then
+    add_requires("libsodium")
+else
+    add_requires("openssl")
+end
+
+target("gamenetworkingsockets")
+    set_kind("$(kind)")
+
+    add_vectorexts("sse2")
+    add_packages("protobuf-cpp")
+
+    if is_plat("windows") then
+        add_packages("libsodium")
+        add_syslinks("ws2_32")
+        add_defines("WIN32", "_WINDOWS", "STEAMNETWORKINGSOCKETS_CRYPTO_LIBSODIUM", "STEAMNETWORKINGSOCKETS_CRYPTO_25519_LIBSODIUM")
+        add_files(  "src/common/crypto_libsodium.cpp",
+                    "src/common/crypto_25519_libsodium.cpp")
+    else
+        add_packages("openssl")
+        add_defines("STEAMNETWORKINGSOCKETS_CRYPTO_25519_OPENSSL", "STEAMNETWORKINGSOCKETS_CRYPTO_VALVEOPENSSL", "OPENSSL_HAS_25519_RAW")
+        add_defines("POSIX", "LINUX", "GNUC", "GNU_COMPILER")
+        add_files(  "src/common/crypto_openssl.cpp",
+                    "src/common/crypto_25519_openssl.cpp",
+                    "src/common/opensslwrapper.cpp")
+    end
+
+    if not is_kind("shared") then
+        add_defines("STEAMNETWORKINGSOCKETS_STATIC_LINK")
+    end
+
+    add_defines("VALVE_CRYPTO_ENABLE_25519",
+                "GOOGLE_PROTOBUF_NO_RTTI",
+                "CRYPTO_DISABLE_ENCRYPT_WITH_PASSWORD",
+                "ENABLE_OPENSSLCONNECTION")
+
+    add_includedirs("include",
+                    "src",
+                    "src/common",
+                    "src/tier0",
+                    "src/tier1",
+                    "src/vstdlib",
+                    "src/steamnetworkingsockets",
+                    "src/steamnetworkingsockets/clientlib",
+                    "src/public")
+
+    add_headerfiles("src/**.h")
+
+    add_files(  "src/common/*.proto", {rules = "protobuf.cpp"})
+    add_files(  "src/common/crypto.cpp",
+                "src/common/crypto_textencode.cpp",
+                "src/common/keypair.cpp",
+                "src/common/steamid.cpp",
+                "src/vstdlib/strtools.cpp",
+                "src/tier0/dbg.cpp",
+                "src/tier0/platformtime.cpp",
+                "src/tier1/ipv6text.c",
+                "src/tier1/netadr.cpp",
+                "src/tier1/utlbuffer.cpp",
+                "src/tier1/utlmemory.cpp",
+                "src/steamnetworkingsockets/steamnetworkingsockets_certs.cpp",
+                "src/steamnetworkingsockets/steamnetworkingsockets_thinker.cpp",
+                "src/steamnetworkingsockets/steamnetworkingsockets_certstore.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_connections.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_flat.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_lowlevel.cpp",
+                "src/steamnetworkingsockets/steamnetworkingsockets_shared.cpp",
+                "src/steamnetworkingsockets/steamnetworkingsockets_stats.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_snp.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_udp.cpp",
+                "src/steamnetworkingsockets/clientlib/csteamnetworkingmessages.cpp",
+                "src/steamnetworkingsockets/clientlib/csteamnetworkingsockets.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_p2p.cpp",
+                "src/steamnetworkingsockets/clientlib/steamnetworkingsockets_p2p_ice.cpp")
---
+++ packages/g/gamenetworkingsockets/xmake.lua
@@ -0,0 +1,23 @@
+package("gamenetworkingsockets")
+
+    set_homepage("https://github.com/ValveSoftware/GameNetworkingSockets")
+    set_description("Reliable & unreliable messages over UDP. Robust message fragmentation & reassembly. P2P networking / NAT traversal. Encryption. ")
+    set_license("BSD-3-Clause")
+
+    set_urls("https://github.com/ValveSoftware/GameNetworkingSockets/archive/$(version).tar.gz",
+             "https://github.com/ValveSoftware/GameNetworkingSockets.git")
+
+    add_versions("1.2.0", "768a7cec2491e34c824204c4858351af2866618ceb13a024336dc1df8076bef3")
+
+    on_install("windows", "linux", function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("GameNetworkingSockets_Init", {includes = "steam/steamnetworkingsockets.h"}))
+    end)
