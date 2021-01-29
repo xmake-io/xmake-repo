903b48c6be23c3fcb5f09655cb56840f5f5a8ba2
ruki <waruqi@gmail.com>
2021-01-29 22:34:14 +0800

    add linux-headers 5.0.8

--- packages/l/linux-headers/xmake.lua
+++ packages/l/linux-headers/xmake.lua
@@ -10,6 +10,7 @@ package("linux-headers")
                  return "v" .. version:major() .. ".x/linux-" .. version
              end})
     add_versions("4.20.9", "b5de28fd594a01edacd06e53491ad0890293e5fbf98329346426cf6030ef1ea6")
+    add_versions("5.0.8",  "11908044e8cce1e093141f8da594708d45d05d0381676ae9aa3d8aeaf7c85435")
     add_versions("5.9.16", "b0d7abae88e5f91893627c645e680a95c818defd1b4fcaf3e2afb4b2b6b4ab86")
 
     on_install("linux", function (package)
