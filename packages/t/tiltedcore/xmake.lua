46ea7fa93ccc1bc2c7654d2932a19435d137cb0d
yamashi <yamashi@users.noreply.github.com>
2021-02-06 17:09:40 +0100

    Update tiltedcore to v0.1.6 - add linux support

--- packages/t/tiltedcore/xmake.lua
+++ packages/t/tiltedcore/xmake.lua
@@ -6,13 +6,14 @@ package("tiltedcore")
     add_urls("https://github.com/tiltedphoques/TiltedCore/releases/download/$(version)/release.zip")
     add_urls("https://github.com/tiltedphoques/TiltedCore.git")
 
+    add_versions("v0.1.6", "d29ee14db2015644fecf6410d28f823151986f15bea1dc9ec4251e605ab8461b")
     add_versions("v0.1.5", "8bd6826ba63ddb16137e54383f95997377409d2a7263acdbdf94bed05b50c9c9")
     add_versions("v0.1.4", "c50213b6814267ccfa24212ca0fbc922c162ac97d234ff50de2e05463115e9b4")
     add_versions("v0.1.3", "e6bc279a436e32c187341af9a47a64977d00d354eda66237804aada51d1884e3")
 
     add_deps("mimalloc", {configs = {rltgenrandom = true}})
 
-    on_install("windows", "msys", function (package)
+    on_install("windows", "msys", "linux", function (package)
         import("package.tools.xmake").install(package)
     end)
 
