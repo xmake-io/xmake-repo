77cede9690e01bf92f403de290a7d43ba9c72f62
ruki <waruqi@gmail.com>
2021-02-12 21:00:53 +0800

    use xmake.lua to port libsoundio

--- packages/l/libsoundio/xmake.lua
+++ packages/l/libsoundio/xmake.lua
@@ -9,20 +9,50 @@ package("libsoundio")
     add_versions("2.0.0", "67a8fc1c9bef2b3704381bfb3fb3ce99e3952bc4fea2817729a7180fddf4a71e")
     add_patches("2.0.0", path.join(os.scriptdir(), "patches", "2.0.0", "extern.patch"), "c1c9084012cf39b203cdf7c59d46a8e6362b6a1e7203700d77faef529d49d401")
 
-    add_deps("cmake")
-
     add_includedirs("include", "include/soundio")
 
-    on_install("windows", "linux", "macosx", function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        if is_plat("windows") then
-            -- compile as C++ because C11 is not widely supported with MSVC
-            import("package.tools.cmake").install(package, configs, {cxflags = {"/TP", "/std:c++11"}})
-        else
-            import("package.tools.cmake").install(package, configs)
+    if is_plat("windows") then
+        add_syslinks("ole32")
+    end
+
+    on_load("windows", function (package)
+        if not package:config("shared") then
+            package:add("defines", "SOUNDIO_STATIC_LIBRARY")
         end
+    end)
+
+    on_install(function (package)
+        io.gsub("src/config.h.in", "#cmakedefine SOUNDIO_HAVE_JACK", "${define SOUNDIO_HAVE_JACK}")
+        io.gsub("src/config.h.in", "#cmakedefine SOUNDIO_HAVE_PULSEAUDIO", "${define SOUNDIO_HAVE_PULSEAUDIO}")
+        io.gsub("src/config.h.in", "#cmakedefine SOUNDIO_HAVE_ALSA", "${define SOUNDIO_HAVE_ALSA}")
+        io.gsub("src/config.h.in", "#cmakedefine SOUNDIO_HAVE_COREAUDIO", "${define SOUNDIO_HAVE_COREAUDIO}")
+        io.gsub("src/config.h.in", "#cmakedefine SOUNDIO_HAVE_WASAPI", "${define SOUNDIO_HAVE_WASAPI}")
+        io.gsub("src/config.h.in", "@LIBSOUNDIO_VERSION_MAJOR@", "${VERSION_MAJOR}")
+        io.gsub("src/config.h.in", "@LIBSOUNDIO_VERSION_MINOR@", "${VERSION_MINOR}")
+        io.gsub("src/config.h.in", "@LIBSOUNDIO_VERSION_PATCH@", "${VERSION_ALTER}")
+        io.gsub("src/config.h.in", "@LIBSOUNDIO_VERSION@",  package:version_str())
+        io.writefile("xmake.lua", ([[
+            set_version("%s")
+            add_rules("mode.debug", "mode.release")
+            target("soundio")
+                set_kind("$(kind)")
+                add_files("src/*.c|alsa.c|jack.c|wasapi.c|pulseaudio.c|coreaudio.c")
+                add_includedirs(".", "soundio")
+                set_configdir("soundio")
+                add_configfiles("src/config.h.in")
+                add_headerfiles("(soundio/*.h)")
+                if is_plat("windows") then
+                    add_cflags("/TP") -- fix missing stdatomic.h
+                    add_syslinks("ole32")
+                end
+        ]]):format(package:version_str()))
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        elseif not package:is_plat("windows", "mingw") and package:config("pic") ~= false then
+            configs.cxflags = "-fPIC"
+        end
+        import("package.tools.xmake").install(package, configs)
     end)
 
     on_test(function (package)
