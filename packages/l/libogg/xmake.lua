8e1cc95342994025e06373a427da91d2619bb093
ruki <waruqi@gmail.com>
2021-02-15 23:30:50 +0800

    disable testing for libogg

--- packages/l/libogg/xmake.lua
+++ packages/l/libogg/xmake.lua
@@ -14,7 +14,7 @@ package("libogg")
     end
 
     on_install("windows", "macosx", "linux", "mingw", "iphoneos", "android", "cross", function (package)
-        local configs = {}
+        local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
