fc2b97ad3e528c9063e4cc9843ef55b05e1eabd5
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-02-19 13:16:58 +0100

    Move patch of pqp into version folder

--- packages/p/pqp/xmake.lua
+++ packages/p/pqp/xmake.lua
@@ -7,7 +7,7 @@ package("pqp")
              "https://github.com/GammaUNC/PQP.git")
 
     add_versions("1.3", "00bdcb380206393349ed7ee9a773f894bf796059c66cec0d97c0a93649eb012f")
-    add_patches("1.3", path.join(os.scriptdir(), "patches", "fix.patch"), "9160f8bc35d23dd5a84af0891410fafa07b8fd9329389724627a68e96b4c270f")
+    add_patches("1.3", path.join(os.scriptdir(), "patches", "1.3", "fix.patch"), "9160f8bc35d23dd5a84af0891410fafa07b8fd9329389724627a68e96b4c270f")
 
     on_install(function (package)
         io.writefile("xmake.lua", [[
