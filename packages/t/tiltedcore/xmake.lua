d13242437476cb111aa486e007b31e841619c639
yamashi <yamashi@users.noreply.github.com>
2021-02-23 15:49:44 +0100

    Add v0.2.1 for tiltedcore

--- packages/t/tiltedcore/xmake.lua
+++ packages/t/tiltedcore/xmake.lua
@@ -3,9 +3,10 @@ package("tiltedcore")
     set_homepage("https://github.com/tiltedphoques/TiltedCore")
     set_description("Core library from Tilted Phoques")
 
-    add_urls("https://github.com/tiltedphoques/TiltedCore/releases/download/$(version)/release.zip")
+    add_urls("https://github.com/tiltedphoques/TiltedCore/archive/$(version).zip")
     add_urls("https://github.com/tiltedphoques/TiltedCore.git")
 
+    add_versions("v0.2.1", "5cf7aab7f548c7dc49349af321d4e96286cea83177a4b779a2b8504e86f1ff3b")
     add_versions("v0.2.0", "c08096df42542add9ced163de4784a998fa08e343da3fcd9ffa42fc5393f8f93")
     add_versions("v0.1.6", "d29ee14db2015644fecf6410d28f823151986f15bea1dc9ec4251e605ab8461b")
     add_versions("v0.1.5", "8bd6826ba63ddb16137e54383f95997377409d2a7263acdbdf94bed05b50c9c9")
