e7f48c05b8c1a7e95abe8701b1304c882808e3b5
yamashi <yamashi@users.noreply.github.com>
2021-02-28 13:11:32 +0100

    Update xmake.lua

--- packages/c/cef/xmake.lua
+++ packages/c/cef/xmake.lua
@@ -4,11 +4,14 @@ package("cef")
     set_description("Chromium Embedded Framework (CEF). A simple framework for embedding Chromium-based browsers in other applications.")
     set_license("BSD-3-Clause")
 
+    add_versions("88.2.9", "86c01e38e7b7d59fed8a1e1ab2c3bfbcc1db42e21f8a6e6feb4061b2af7b1b7d")
     add_versions("88.2.1", "8ed01da6327258536c61ada46e14157149ce727e7729ec35a30b91b3ad3cf555")
 
     set_urls("https://cef-builds.spotifycdn.com/cef_binary_$(version).tar.bz2", {version = function (version)
         if version:eq("88.2.1") then
-            return "88.2.1+g0b18d0b+chromium-88.0.4324.146_windows64"
+            return "88.2.1+g0b18d0b+chromium-88.0.4324.146_windows" .. (is_arch("x64") and "64" or "32")
+        elseif version:eq("88.2.9") then
+            return "88.2.9+g5c8711a+chromium-88.0.4324.182_windows" .. (is_arch("x64") and "64" or "32")
         end
         return ""
     end})
@@ -19,7 +22,7 @@ package("cef")
         add_syslinks("user32", "advapi32")
     end
 
-    on_install("windows|x64", function (package)
+    on_install("windows", function (package)
         assert(package:config("vs_runtime") == "MT" and not package:config("shared"), "only support static library with MT")
         package:addenv("PATH", "bin")
         local distrib_type = package:debug() and "Debug" or "Release"
