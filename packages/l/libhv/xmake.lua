a160187730d7fabd0ef1414a8353973820fef2ce
fasiondog <fasiondog@163.com>
2021-03-02 23:33:44 +0800

    update for libhv

--- packages/l/libhv/xmake.lua
+++ packages/l/libhv/xmake.lua
@@ -6,36 +6,54 @@ package("libhv")
     add_urls("https://github.com/ithewei/libhv/archive/v$(version).zip")
     add_versions("1.0.0", "39adb77cc7addaba82b69fa9a433041c8288f3d9c773fa360162e3391dcf6a7b")
 
-    add_configs("BUILD_SHARED", {description = "build shared library", default = false, type = "boolean"})
-    add_configs("BUILD_STATIC", {description = "build static library", default = true, type = "boolean"})
-    add_configs("WITH_PROTOCOL", {description = "compile protocol", default = false, type = "boolean"})
-    add_configs("WITH_HTTP", {description = "compile http", default = true, type = "boolean"})
-    add_configs("WITH_HTTP_SERVER", {description = "compile http/server", default = true, type = "boolean"})
-    add_configs("WITH_HTTP_CLIENT", {description = "compile http/client", default = true, type = "boolean"})
-    add_configs("WITH_CONSUL", {description = "compile consul", default = false, type = "boolean"})
-    add_configs("ENABLE_IPV6", {description = "ipv6", default = false, type = "boolean"})
-    add_configs("ENABLE_UDS", {description = "Unix Domain Socket", default = false, type = "boolean"})
-    add_configs("ENABLE_WINDUMP", {description = "Windows MiniDumpWriteDump", default = false, type = "boolean"})
-    add_configs("USE_MULTIMAP", {description = "MultiMap", default = false, type = "boolean"})
-    add_configs("WITH_CURL", {description = "with curl library", default = false, type = "boolean"})
-    add_configs("WITH_NGHTTP2", {description = "with nghttp2 library", default = false, type = "boolean"})
-    add_configs("WITH_OPENSSL", {description = "with openssl library", default = false, type = "boolean"})
-    add_configs("WITH_MBEDTLS", {description = "with mbedtls library", default = false, type = "boolean"})
+    add_configs("protocol", {description = "compile protocol", default = false, type = "boolean"})
+    add_configs("http", {description = "compile http", default = true, type = "boolean"})
+    add_configs("http_server", {description = "compile http/server", default = true, type = "boolean"})
+    add_configs("http_client", {description = "compile http/client", default = true, type = "boolean"})
+    add_configs("consul", {description = "compile consul", default = false, type = "boolean"})
+    add_configs("ipv6", {description = "ipv6", default = false, type = "boolean"})
+    add_configs("uds", {description = "Unix Domain Socket", default = false, type = "boolean"})
+    add_configs("windump", {description = "Windows MiniDumpWriteDump", default = false, type = "boolean"})
+    add_configs("multimap", {description = "MultiMap", default = false, type = "boolean"})
+    add_configs("curl", {description = "with curl library", default = false, type = "boolean"})
+    add_configs("nghttp2", {description = "with nghttp2 library", default = false, type = "boolean"})
+    add_configs("openssl", {description = "with openssl library", default = false, type = "boolean"})
+    add_configs("mbedtls", {description = "with mbedtls library", default = false, type = "boolean"})
 
     if is_plat("linux") then
         add_syslinks("pthread")
     end
 
+    on_load("windows", "linux", "macosx", "android", "iphoneos", function (package)
+        if package:config("curl") then
+            package:add("deps", "libcurl")
+        end
+        if package:config("openssl") then
+            package:add("deps", "openssl")
+        end
+        if package:config("mbedtls") then
+            package:add("deps", "mbedtls")
+        end
+    end)
+
     add_deps("cmake")
     on_install("windows", "linux", "macosx", "android", "iphoneos", function(package)
         local configs = {"-DBUILD_EXAMPLES=OFF", "-DBUILD_UNITTEST=OFF"}
-        table.insert(configs, "-DBUILD_SHARED=" .. ((package:config("shared") or package:config("BUILD_SHARED")) and "ON" or "OFF"))
-        table.insert(configs, "-DBUILD_STATIC=" .. ((package:config("static") or package:config("BUILD_STATIC")) and "ON" or "OFF"))
-        for name, enabled in pairs(package:configs()) do
-            if not package:extraconf("configs", name, "builtin") then
-                table.insert(configs, "-D" .. name .. "=" .. (enabled and "ON" or "OFF"))
-            end
-        end
+        table.insert(configs, "-DBUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DBUILD_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
+        table.insert(configs, "-DWITH_PROTOCOL=" .. (package:config("protocol") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_HTTP=" .. (package:config("http") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_HTTP_SERVER=" .. (package:config("http_server") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_HTTP_CLIENT=" .. (package:config("http_client") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_CONSUL=" .. (package:config("consul") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_IPV6=" .. (package:config("ipv6") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_UDS=" .. (package:config("uds") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_WINDUMP=" .. (package:config("windump") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_MULTIMAP=" .. (package:config("multimap") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_CURL=" .. (package:config("curl") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_NGHTTP2=" .. (package:config("nghttp2") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_OPENSSL=" .. (package:config("openssl") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_MBEDTLS=" .. (package:config("mbedtls") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
