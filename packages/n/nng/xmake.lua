b8318ce2aaca585b1b0a5ba102b93ae105800076
fasiondog <fasiondog@163.com>
2021-03-12 23:56:57 +0800

    add nngpp and add nng new version 1.4.0 (#320)
    * add pybind11
    
    * add pybind11
    
    * add pybind11
    
    * add nng
    
    * update for add nng
    
    * update for add nng
    
    * update for add nng
    
    * update for add nng, remove MingW
    
    * update protobuf-cpp
    
    * update for protobuf-cpp
    
    * add libhv
    
    * update for libhv
    
    * nng add new version 1.4.0
    
    * add nngpp
    
    * update nngpp
    
    * add nngpp version

--- packages/n/nng/xmake.lua
+++ packages/n/nng/xmake.lua
@@ -4,6 +4,7 @@ package("nng")
     set_description("NNG, like its predecessors nanomsg (and to some extent ZeroMQ), is a lightweight, broker-less library, offering a simple API to solve common recurring messaging problems.")
 
     add_urls("https://github.com/nanomsg/nng/archive/v$(version).zip")
+    add_versions("1.4.0", "43674bb15d0f3810cf3602d2662cc91b6576b914492710244125e32b29f546b8")
     add_versions("1.3.2", "2616110016c89ed3cbd458022ba41f4f545ab17f807546d2fdd0789b55d64471")
 
     -- default is false
---
+++ packages/n/nngpp/xmake.lua
@@ -0,0 +1,22 @@
+package("nngpp")
+
+    set_homepage("https://github.com/cwzx/nngpp")
+    set_description("C++ wrapper around the nanomsg NNG API.")
+
+    add_urls("https://github.com/cwzx/nngpp.git")
+    add_versions("v2020.10.30", "8da8c026bd551b7685a8a140909ff96cfe91bf90")
+
+    add_deps("nng")
+    add_deps("cmake")
+    on_install("windows", "linux", "macosx", "android", "iphoneos", function (package)
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <nngpp/nngpp.h>
+            static void test() {
+                nng::aio aio = nng::make_aio();
+            }
+        ]]}, {includes = "nngpp/nngpp.h",configs = {languages = "c++11"}}))
+    end)
