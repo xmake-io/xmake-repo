c39cc35d5a13c34d0501213e96b18e56bb6d9f09
ruki <waruqi@gmail.com>
2021-03-17 23:18:25 +0800

    update ndk

--- packages/n/ndk/xmake.lua
+++ packages/n/ndk/xmake.lua
@@ -4,22 +4,25 @@ package("ndk")
     set_homepage("https://developer.android.com/ndk")
     set_description("Android NDK toolchain.")
 
-    local versions = {["21"] = "r21e"}
+    local versions = {["21"] = "r21e", ["22"] = "r22"}
     if is_host("windows") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-windows-x86_64.zip", {version = function (version)
             return versions[tostring(version)]
         end})
         add_versions("21", "f71307c5c572e2c163d602b3704b8bc024bec0c43ba2800de36bd10f3a21492b")
+        add_versions("22", "5a0eafa83c8bba3c76e8427aa3d83d169215f62963a277b1914a3651aa47f751")
     elseif is_host("linux") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-linux-x86_64.zip", {version = function (version)
             return versions[tostring(version)]
         end})
         add_versions("21", "ad7ce5467e18d40050dc51b8e7affc3e635c85bd8c59be62de32352328ed467e")
+        add_versions("22", "d37fc69cd81e5660234a686e20adef39bc0244086e4d66525a40af771c020718")
     elseif is_host("macosx") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-darwin-x86_64.zip", {version = function (version)
             return versions[tostring(version)]
         end})
         add_versions("21", "437278103a3db12632c05b1be5c41bbb8522791a67e415cc54411a65366f499d")
+        add_versions("22", "14fce4dea7fb3facbc0e3d20270007bffec3ba383aec02e8b0e0dad8d8782892")
     end
 
     on_install("@windows", "@msys", "@linux", "@macosx", function (package)
