c1d330c85fc47e85cd49ef0cac032efc567ecb86
Jérôme Leclercq <lynix680@gmail.com>
2021-03-18 12:04:17 +0100

    Add some more frameworks

--- packages/l/libfreenect2/xmake.lua
+++ packages/l/libfreenect2/xmake.lua
@@ -10,7 +10,7 @@ package("libfreenect2")
     if is_plat("linux") then
         add_syslinks("pthread")
     elseif is_plat("macosx") then
-        add_frameworks("CoreMedia", "CoreFoundation", "Foundation", "IOKit", "OpenCL")
+        add_frameworks("CoreMedia", "CoreFoundation", "CoreVideo", "Foundation", "IOKit", "OpenCL", "ImageIO")
     end
 
     on_install("windows", "linux", "macosx", function (package)
