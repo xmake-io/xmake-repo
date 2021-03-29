88a74d8946d4831d0d830afbe88f312d5c0f438b
Andrej Redeky <andrej.redeky@outlook.com>
2021-03-29 15:55:24 +0200

    Prepend v to tags and remove it from url

--- packages/s/spdlog/xmake.lua
+++ packages/s/spdlog/xmake.lua
@@ -3,16 +3,16 @@ package("spdlog")
     set_homepage("https://github.com/gabime/spdlog")
     set_description("Fast C++ logging library.")
 
-    set_urls("https://github.com/gabime/spdlog/archive/v$(version).zip",
+    set_urls("https://github.com/gabime/spdlog/archive/$(version).zip",
              "https://github.com/gabime/spdlog.git")
              
-    add_versions("1.8.5", "944d0bd7c763ac721398dca2bb0f3b5ed16f67cef36810ede5061f35a543b4b8")
-    add_versions("1.8.2", "f0410b12b526065802b40db01304783550d3d20b4b6fe2f8da55f9d08ed2035d")
-    add_versions("1.8.1", "eed0095a1d52d08a0834feda146d4f9148fa4125620cd04d8ea57e0238fa39cd")
-    add_versions("1.8.0", "3cc41508fcd79e5141a6ef350506ef82982ca42a875e0588c02c19350ac3702e")
-    add_versions("1.5.0", "87e87c989f15d6b9f5379385aec1001c89a42941341ebaa09ec895b98a00efb4")
-    add_versions("1.4.2", "56b90f0bd5b126cf1b623eeb19bf4369516fa68f036bbc22d9729d2da511fb5a")
-    add_versions("1.3.1", "db6986d0141546d4fba5220944cc1f251bd8afdfc434bda173b4b0b6406e3cd0")
+    add_versions("v1.8.5", "944d0bd7c763ac721398dca2bb0f3b5ed16f67cef36810ede5061f35a543b4b8")
+    add_versions("v1.8.2", "f0410b12b526065802b40db01304783550d3d20b4b6fe2f8da55f9d08ed2035d")
+    add_versions("v1.8.1", "eed0095a1d52d08a0834feda146d4f9148fa4125620cd04d8ea57e0238fa39cd")
+    add_versions("v1.8.0", "3cc41508fcd79e5141a6ef350506ef82982ca42a875e0588c02c19350ac3702e")
+    add_versions("v1.5.0", "87e87c989f15d6b9f5379385aec1001c89a42941341ebaa09ec895b98a00efb4")
+    add_versions("v1.4.2", "56b90f0bd5b126cf1b623eeb19bf4369516fa68f036bbc22d9729d2da511fb5a")
+    add_versions("v1.3.1", "db6986d0141546d4fba5220944cc1f251bd8afdfc434bda173b4b0b6406e3cd0")
 
     add_configs("header_only",  { description = "Use header only", default = true, type = "boolean"})
     add_configs("fmt_external", { description = "Use external fmt library instead of bundled", default = false, type = "boolean"})
