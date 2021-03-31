0b2d611a05adfe13c67d2cd27718a013720b9245
xq114 <1140735506@qq.com>
2021-03-31 09:38:12 +0800

    update to 1.8.1

--- packages/l/libtorch/xmake.lua
+++ packages/l/libtorch/xmake.lua
@@ -6,6 +6,7 @@ package("libtorch")
 
     add_urls("https://github.com/pytorch/pytorch.git")
     add_versions("v1.8.0", "37c1f4a7fef115d719104e871d0cf39434aa9d56")
+    add_versions("v1.8.1", "56b43f4fec1f76953f15a627694d4bba34588969")
 
     add_configs("python", {description = "Build python interface.", default = false, type = "boolean"})
     add_configs("ninja", {description = "Use ninja as build tool.", default = false, type = "boolean"})
