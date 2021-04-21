93c9827d9a03f1c4484d096ad61edd5a82d287d5
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-04-21 11:27:07 +0200

    Use commit date as version

--- packages/f/fastcppcsvparser/xmake.lua
+++ packages/f/fastcppcsvparser/xmake.lua
@@ -4,7 +4,7 @@ package("fastcppcsvparser")
     set_description("This is a small, easy-to-use and fast header-only library for reading comma separated value (CSV) files (by ben-strasser)")
 
     add_urls("https://github.com/ben-strasser/fast-cpp-csv-parser.git")
-    add_versions(os.date("%Y.%m.%d"), "75600d0b77448e6c410893830df0aec1dbacf8e3")
+    add_versions("2021.01.03", "75600d0b77448e6c410893830df0aec1dbacf8e3")
 
     on_install(function (package)
         if package:is_plat("macosx", "iphoneos") then
