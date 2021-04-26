6c7a6a66e857fd8d7c574d282962072b4eb10ce8
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-04-26 11:23:39 +0200

    Add patch to fix lrint at libsdl_gfx

--- packages/l/libsdl_gfx/xmake.lua
+++ packages/l/libsdl_gfx/xmake.lua
@@ -10,6 +10,7 @@ package("libsdl_gfx")
         add_versions("sourceforge:1.0.4", "63e0e01addedc9df2f85b93a248f06e8a04affa014a835c2ea34bfe34e576262")
 
         add_patches("1.0.4", path.join(os.scriptdir(), "patches", "1.0.4", "add-x64-support.patch"), "623ed5796c2771dc959ef0249b46a07762981a98dd25a534977f2614791d61a0")
+        add_patches("1.0.4", path.join(os.scriptdir(), "patches", "1.0.4", "lrint_fix.patch"), "9fb928306fb25293720214377bff2f605f60ea26f43ea5346cf1268c504aff1a")
     elseif is_plat("macosx", "linux") then
         set_urls("https://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-$(version).tar.gz")
         add_urls("https://ufpr.dl.sourceforge.net/project/sdl2gfx/SDL2_gfx-$(version).tar.gz")
