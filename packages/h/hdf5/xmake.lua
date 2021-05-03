57e27170f42e020d6cad16ce671be5f73125709f
xq114 <1140735506@qq.com>
2021-05-03 22:31:03 +0800

    fix hdf5

--- packages/h/hdf5/xmake.lua
+++ packages/h/hdf5/xmake.lua
@@ -8,16 +8,24 @@ package("hdf5")
     add_versions("1.12.0", "a62dcb276658cb78e6795dd29bf926ed7a9bc4edf6e77025cd2c689a8f97c17a")
 
     add_deps("cmake")
+    if is_plat("linux") then
+        add_syslinks("dl")
+    end
     on_install("windows", "macosx", "linux", function (package)
         local configs = {"-DHDF5_GENERATE_HEADERS=OFF", "-DBUILD_TESTING=OFF", "-DHDF5_BUILD_EXAMPLES=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DONLY_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
         import("package.tools.cmake").install(package, configs)
         package:addenv("PATH", "bin")
     end)
 
     on_test(function (package)
-        os.vrun("h5diff --version")
+        if package:config("shared") then
+            os.vrun("h5diff-shared --version")
+        else
+            os.vrun("h5diff --version")
+        end
         assert(package:has_cfuncs("H5open", {includes = "H5public.h"}))
     end)
