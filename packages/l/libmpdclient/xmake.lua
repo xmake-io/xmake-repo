6e60e606819c3b753147b1f570fa49192a53b7ce
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-06 10:13:30 -0400

    make sure libmpdclient is a shared lib

--- packages/l/libmpdclient/xmake.lua
+++ packages/l/libmpdclient/xmake.lua
@@ -1,12 +1,15 @@
 package("libmpdclient")
     set_homepage("https://musicpd.org/libs/libmpdclient/")
     set_description("A stable, documented, asynchronous API library for interfacing MPD in the C, C++ & Objective C languages.")
+    set_kind("shared")
     add_urls("https://musicpd.org/download/libmpdclient/2/libmpdclient-$(version).tar.xz")
     add_versions("2.19", "158aad4c2278ab08e76a3f2b0166c99b39fae00ee17231bd225c5a36e977a189")
     add_deps("meson", "ninja")
 
     on_install("linux", function (package)
-        import("package.tools.meson").install(package)
+        local configs = {}
+        table.insert(configs, "-Ddefault_library=shared")
+        import("package.tools.meson").install(package, configs)
         os.cp("include", package:installdir())
         os.cp("build_*/version.h", package:installdir() .. "/include/mpd")
         os.rm(package:installdir() .. "/include/mpd/version.h.in")
