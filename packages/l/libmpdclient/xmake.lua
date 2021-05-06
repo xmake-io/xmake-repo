fe5c4b90ad3ec545c8f5d34e50fc55adba8f5426
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-06 00:51:06 -0400

    add test function, move version under url

--- packages/l/libmpdclient/xmake.lua
+++ packages/l/libmpdclient/xmake.lua
@@ -1,10 +1,15 @@
 package("libmpdclient")
     set_homepage("https://musicpd.org/libs/libmpdclient/")
     set_description("A stable, documented, asynchronous API library for interfacing MPD in the C, C++ & Objective C languages.")
-    add_versions("2.19", "158aad4c2278ab08e76a3f2b0166c99b39fae00ee17231bd225c5a36e977a189")
     add_urls("https://musicpd.org/download/libmpdclient/2/libmpdclient-$(version).tar.xz")
+    add_versions("2.19", "158aad4c2278ab08e76a3f2b0166c99b39fae00ee17231bd225c5a36e977a189")
     add_deps("meson", "ninja")
+
     on_install(function (package)
         import("package.tools.meson").install(package, {"-Dxxx=ON"})
         os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("mpd_connection_new", {includes = "mpd/connection.h"}))
     end)
