89a910bbcd6154a7fc9c6fa2af454b50eb927bd2
Hoildkv <42310255+xq114@users.noreply.github.com>
2021-05-06 15:41:26 +0800

    add nanoflann, tinyfiledialogs and filament (#400)
    * add nanoflann
    
    * add tinyfiledialogs
    
    * add filament
    
    * fix name issue

---
+++ packages/t/tinyfiledialogs/xmake.lua
@@ -0,0 +1,34 @@
+package("tinyfiledialogs")
+
+    set_homepage("https://sourceforge.net/projects/tinyfiledialogs/")
+    set_description("Native dialog library for WINDOWS MAC OSX GTK+ QT CONSOLE")
+    set_license("zlib")
+
+    add_urls("https://git.code.sf.net/p/tinyfiledialogs/code.git")
+    add_versions("3.8.8", "d89567798fb1b989c6fc46a61698e4734760e0bf")
+
+    if is_plat("windows") then
+        add_syslinks("comdlg32", "ole32", "user32", "shell32")
+    end
+    on_install("windows", "linux", "macosx", function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            target("tinyfiledialogs")
+                set_kind("static")
+                add_files("tinyfiledialogs.c")
+                add_headerfiles("tinyfiledialogs.h")
+        ]])
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_csnippets({test = [[
+            #include <stdio.h>
+            #include <string.h>
+            #include "tinyfiledialogs.h"
+            void test() {
+                char const * lWillBeGraphicMode;
+                lWillBeGraphicMode = tinyfd_inputBox("tinyfd_query", NULL, NULL);
+            }
+        ]]}))
+    end)
