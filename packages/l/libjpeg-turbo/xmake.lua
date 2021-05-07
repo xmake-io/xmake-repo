b354ecee5dfc44c2ee38147ceac3c68a4d19fbc4
xq114 <1140735506@qq.com>
2021-05-07 14:35:33 +0800

    fix libjpeg-turbo

--- packages/l/libjpeg-turbo/xmake.lua
+++ packages/l/libjpeg-turbo/xmake.lua
@@ -31,7 +31,7 @@ package("libjpeg-turbo")
             table.insert(configs, "-DENABLE_SHARED=OFF")
             table.insert(configs, "-DENABLE_STATIC=ON")
         end
-        if package:config("vs_runtime"):startswith("MD") then
+        if package:is_plat("windows") and package:config("vs_runtime"):startswith("MD") then
             table.insert(configs, "-DWITH_CRT_DLL=ON")
         end
         if package:config("pic") ~= false then
