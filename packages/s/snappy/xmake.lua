df7ed4aca2c28c9e5366101b159391d10be00f74
xq114 <1140735506@qq.com>
2021-05-17 12:01:19 +0800

    update leveldb and snappy

--- packages/s/snappy/xmake.lua
+++ packages/s/snappy/xmake.lua
@@ -7,6 +7,7 @@ package("snappy")
              "https://github.com/google/snappy.git")
 
     add_versions("1.1.8", "16b677f07832a612b0836178db7f374e414f94657c138e6993cbfc5dcc58651f")
+    add_versions("1.1.9", "75c1fbb3d618dd3a0483bff0e26d0a92b495bbe5059c8b4f1c962b478b6e06e7")
 
     add_deps("cmake")
 
@@ -14,7 +15,7 @@ package("snappy")
     add_configs("avx2", {description = "Use the AVX2 instruction set", default = false, type = "boolean"})
 
     on_install(function (package)
-        local configs = {"-DSNAPPY_BUILD_TESTS=OFF"}
+        local configs = {"-DSNAPPY_BUILD_TESTS=OFF", "-DSNAPPY_BUILD_BENCHMARKS=OFF"}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DSNAPPY_REQUIRE_AVX=" .. (package:config("avx") and "ON" or "OFF"))
         table.insert(configs, "-DSNAPPY_REQUIRE_AVX2=" .. (package:config("avx2") and "ON" or "OFF"))
