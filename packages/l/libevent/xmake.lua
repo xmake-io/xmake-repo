ee7ec5421de38b6e0fe3a78d39b56ce47198d11d
xq114 <1140735506@qq.com>
2021-05-17 23:03:14 +0800

    improve libevent

--- packages/l/libevent/xmake.lua
+++ packages/l/libevent/xmake.lua
@@ -8,12 +8,25 @@ package("libevent")
     add_urls("https://github.com/libevent/libevent.git")
     add_versions("2.1.12", "92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb")
 
+    add_configs("with_openssl", {description = "Build with OpenSSL library.", default = false, type = "boolean"})
+    add_configs("with_mbedtls", {description = "Build with mbedtls library.", default = false, type = "boolean"})
+
     add_deps("cmake")
+    on_load(function (package)
+        if package:config("with_openssl") then
+            package:add("deps", "openssl")
+        end
+        if package:config("with_mbedtls") then
+            package:add("deps", "mbedtls")
+        end
+    end)
 
     on_install("windows", "linux", "macosx", function (package)
-        local configs = {"-DEVENT__DISABLE_TESTS=ON", "-DEVENT__DISABLE_REGRESS=ON", "-DEVENT__DISABLE_SAMPLES=ON", "-DEVENT__DISABLE_OPENSSL=ON", "-DEVENT__DISABLE_MBEDTLS=ON"}
+        local configs = {"-DEVENT__DISABLE_TESTS=ON", "-DEVENT__DISABLE_REGRESS=ON", "-DEVENT__DISABLE_SAMPLES=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DEVENT__DISABLE_OPENSSL=" .. (package:config("with_openssl") and "OFF" or "ON"))
+        table.insert(configs, "-DEVENT__DISABLE_MBEDTLS=" .. (package:config("with_mbedtls") and "OFF" or "ON"))
         import("package.tools.cmake").install(package, configs)
     end)
 
