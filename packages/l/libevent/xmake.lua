e9ffbfe3500f58d95bce324aa36a87b01dbf5e83
xq114 <1140735506@qq.com>
2021-05-18 09:34:01 +0800

    improve config format

--- packages/l/libevent/xmake.lua
+++ packages/l/libevent/xmake.lua
@@ -8,15 +8,15 @@ package("libevent")
     add_urls("https://github.com/libevent/libevent.git")
     add_versions("2.1.12", "92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb")
 
-    add_configs("with_openssl", {description = "Build with OpenSSL library.", default = false, type = "boolean"})
-    add_configs("with_mbedtls", {description = "Build with mbedtls library.", default = false, type = "boolean"})
+    add_configs("openssl", {description = "Build with OpenSSL library.", default = false, type = "boolean"})
+    add_configs("mbedtls", {description = "Build with mbedtls library.", default = false, type = "boolean"})
 
     add_deps("cmake")
     on_load(function (package)
-        if package:config("with_openssl") then
+        if package:config("openssl") then
             package:add("deps", "openssl")
         end
-        if package:config("with_mbedtls") then
+        if package:config("mbedtls") then
             package:add("deps", "mbedtls")
         end
     end)
@@ -25,8 +25,8 @@ package("libevent")
         local configs = {"-DEVENT__DISABLE_TESTS=ON", "-DEVENT__DISABLE_REGRESS=ON", "-DEVENT__DISABLE_SAMPLES=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DEVENT__DISABLE_OPENSSL=" .. (package:config("with_openssl") and "OFF" or "ON"))
-        table.insert(configs, "-DEVENT__DISABLE_MBEDTLS=" .. (package:config("with_mbedtls") and "OFF" or "ON"))
+        table.insert(configs, "-DEVENT__DISABLE_OPENSSL=" .. (package:config("openssl") and "OFF" or "ON"))
+        table.insert(configs, "-DEVENT__DISABLE_MBEDTLS=" .. (package:config("mbedtls") and "OFF" or "ON"))
         import("package.tools.cmake").install(package, configs)
     end)
 
