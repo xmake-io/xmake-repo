41d5b0d0c0314cde1352f8ad513d2114324274d0
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-19 11:48:59 -0400

    remove some deps that couldn't be disabled, move more deps into some config options, disable deps that aren't installed

--- packages/i/imagemagick/xmake.lua
+++ packages/i/imagemagick/xmake.lua
@@ -3,27 +3,34 @@ package("imagemagick")
     set_description("ImageMagick is a FOSS software suite for modifying images. This does NOT provide any of the utilities. It installs the C/C++ Libraries.")
     add_urls("https://download.imagemagick.org/ImageMagick/download/ImageMagick-$(version).tar.gz")
     add_versions("7.0.11-13", "6c162d7cbd7b80968a6d083d39eb18d9c9bbd49f500f7f49c9a5dcc0fc36a03b")
-    add_deps("bzip2", "freetype", "fontconfig", "libxml2", "lzma", "pkg-config")
+    add_deps("pkg-config")
+    add_configs("bzlib", {description = "Enable bzip2 support.", default = false, type = "boolean"})
     add_configs("exr", {description = "Enable exr support.", default = false, type = "boolean"})
-    add_configs("ffmpeg", {description = "Enable ffmpeg support.", default = false, type = "boolean"})
     add_configs("fftw", {description = "Enable fftw support.", default = false, type = "boolean"})
-    add_configs("jasper", {description = "Enable jasper support.", default = false, type = "boolean"})
-    add_configs("jpeg_turbo", {description = "Enable jpeg support through libjpeg-turbo.", default = false, type = "boolean"})
+    add_configs("fontconfig", {description = "Enable fontconfig support.", default = false, type = "boolean"})
+    add_configs("freetype", {description = "Enable freetype support.", default = false, type = "boolean"})
+    add_configs("jpeg", {description = "Enable jpeg support through libjpeg-turbo.", default = false, type = "boolean"})
+    add_configs("lzma", {description = "Enable LZMA support.", default = false, type = "boolean"})
     add_configs("openjpeg", {description = "Enable jpeg support through openjpeg.", default = false, type = "boolean"})
     add_configs("png", {description = "Enable png support.", default = true, type = "boolean"})
     add_configs("tiff", {description = "Enable tiff support.", default = false, type = "boolean"})
+    add_configs("xml", {description = "Enable XML support.", default = false, type = "boolean"})
     add_configs("webp", {description = "Enable webp support.", default = false, type = "boolean"})
     add_includedirs("include/ImageMagick-7")
 
     on_load(function(package)
-        local configdeps = {exr        = "openexr",
+        local configdeps = {bzlib      = "bzip2",
+                            exr        = "openexr",
                             ffmpeg     = "ffmpeg",
+                            fontconfig = "fontconfig",
+                            freetype   = "freetype",
                             fftw       = "fftw",
-                            jasper     = "jasper",
-                            jpeg_turbo = "libjpeg-turbo",
+                            jpeg       = "libjpeg-turbo",
+                            lzma       = "lzma",
                             openjpeg   = "openjpeg",
                             png        = "libpng",
                             tiff       = "libtiff",
+                            xml        = "libxml2",
                             webp       = "libwebp"}
         for name, dep in pairs(configdeps) do
             if package:config(name) then
@@ -36,7 +43,28 @@ package("imagemagick")
     end)
 
     on_install("bsd", "linux", "macosx", function(package)
-        local configs = {"--with-utilities=no", "--with-x=no", "--without-djvu", "--without-jbig", "--disable-openmp"}
+        local configs = {"--without-utilities", "--without-x", "--without-djvu", "--without-jbig", "--disable-openmp", "--without-perl"}
+        for name, enabled in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                if enabled then
+                    if name == "openjpeg" then
+                        table.insert(configs, "--with-" .. "openjp2")
+                    elseif name == "exr" then
+                        table.insert(configs, "--with-" .. "openexr")
+                    else
+                        table.insert(configs, "--with-" .. name)
+                    end
+                else
+                    if name == "openjpeg" then
+                        table.insert(configs, "--without-" .. "openjp2")
+                    elseif name == "exr" then
+                        table.insert(configs, "--with-" .. "openexr")
+                    else
+                        table.insert(configs, "--without-" .. name)
+                    end
+                end
+            end
+        end
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no")) 
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes")) 
         if package:config("pic") ~= false then
