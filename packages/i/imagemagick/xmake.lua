d261cd978ef2b3c918ae9ec3cf36d2c1408b3cc6
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-19 21:58:45 -0400

    update with some of the reviews, remove bsd until pkg-config has bsd enabled

--- packages/i/imagemagick/xmake.lua
+++ packages/i/imagemagick/xmake.lua
@@ -3,7 +3,7 @@ package("imagemagick")
     set_description("ImageMagick is a FOSS software suite for modifying images. This does NOT provide any of the utilities. It installs the C/C++ Libraries.")
     add_urls("https://download.imagemagick.org/ImageMagick/download/ImageMagick-$(version).tar.gz")
     add_versions("7.0.11-13", "6c162d7cbd7b80968a6d083d39eb18d9c9bbd49f500f7f49c9a5dcc0fc36a03b")
-    add_deps("pkg-config")
+
     add_configs("bzlib", {description = "Enable bzip2 support.", default = false, type = "boolean"})
     add_configs("exr", {description = "Enable exr support.", default = false, type = "boolean"})
     add_configs("fftw", {description = "Enable fftw support.", default = false, type = "boolean"})
@@ -18,6 +18,8 @@ package("imagemagick")
     add_configs("threads", {description = "Enable threading support.", default = false})
     add_configs("xml", {description = "Enable XML support.", default = false, type = "boolean"})
     add_configs("webp", {description = "Enable webp support.", default = false, type = "boolean"})
+
+    add_deps("pkg-config")
     add_includedirs("include/ImageMagick-7/")
     add_links("MagickWand-7.Q16", "MagickCore-7.Q16", "Magick++-7.Q16")
 
@@ -44,12 +46,12 @@ package("imagemagick")
                 end
             end
         end
-        if package:config("threads") and is_plat("linux") then
+        if package:config("threads") and package:is_plat("linux") then
             add_syslinks("pthread")
         end
     end)
 
-    on_install("bsd", "linux", "macosx", function(package)
+    on_install("linux", "macosx", function(package)
         local configs = {"--without-utilities",
                          "--without-x",
                          "--without-djvu",
