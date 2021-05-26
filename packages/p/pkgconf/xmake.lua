36ad454740aa066ed2d474232e9279914515371a
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-25 23:25:22 -0400

    set the package kind, add test function

--- packages/p/pkgconf/xmake.lua
+++ packages/p/pkgconf/xmake.lua
@@ -1,9 +1,10 @@
 package("pkgconf")
+    set_kind("binary")
     set_homepage("http://pkgconf.org")
     set_description("A program which helps to configure compiler and linker flags for development frameworks.")
     add_urls("https://distfiles.dereferenced.org/pkgconf/pkgconf-$(version).tar.xz")
     add_versions("1.1.0", "5f1ef65d73a880fa5e7012102a17f7b32010e5e46139aed85851a541ba828a63")
-    
+
     on_install("linux", "bsd", function(package)
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no")) 
@@ -13,4 +14,8 @@ package("pkgconf")
         end
 
         import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_test(function (package)
+        os.vrun("pkgconf --version")
     end)
