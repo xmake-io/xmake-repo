c6c242546a374c1bb343caf553a446a5979a566c
xq114 <1140735506@qq.com>
2021-05-26 14:02:44 +0800

    fix libavif

--- packages/l/libavif/xmake.lua
+++ packages/l/libavif/xmake.lua
@@ -18,7 +18,7 @@ package("libavif")
     end)
 
     on_install("windows", "linux", "macosx", function (package)
-        local configs = {}
+        local configs = {"-DAVIF_ENABLE_WERROR=OFF", "-DAVIF_BUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:config("dav1d") then
