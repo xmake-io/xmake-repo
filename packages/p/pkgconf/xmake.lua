bbab8de61330061ce28d6c9d32eb28a76a31d338
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-25 23:28:49 -0400

    initial work on adding windows support

--- packages/p/pkgconf/xmake.lua
+++ packages/p/pkgconf/xmake.lua
@@ -5,6 +5,10 @@ package("pkgconf")
     add_urls("https://distfiles.dereferenced.org/pkgconf/pkgconf-$(version).tar.xz")
     add_versions("1.1.0", "5f1ef65d73a880fa5e7012102a17f7b32010e5e46139aed85851a541ba828a63")
 
+    on_load("windows", function(package)
+        add_deps("cmake")
+    end)
+
     on_install("linux", "bsd", function(package)
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no")) 
@@ -14,6 +18,10 @@ package("pkgconf")
         end
 
         import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_install("windows", function(package)
+        import("package.tools.cmake").install(package)
     end)
 
     on_test(function (package)
