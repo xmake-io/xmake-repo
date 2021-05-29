6e8fb1e49fb8c1d2285c9a50a5362d095c481d7e
Jérôme Leclercq <lynix680@gmail.com>
2021-05-29 17:29:28 +0200

    Add dr_libs (dr_flac, dr_mp3 and dr_wav)

---
+++ packages/d/dr_flac/xmake.lua
@@ -0,0 +1,15 @@
+package("dr_mp3")
+    set_homepage("https://github.com/mackron/dr_libs")
+    set_description("Single file audio decoding libraries for C/C++.")
+    set_license("MIT")
+
+    set_urls("https://github.com/mackron/dr_libs.git")
+    add_versions("0.6.27", "f357ade3aae55ced341aa7c83b4e7f628f948e51")
+
+    on_install(function (package)
+        os.cp("dr_mp3.h", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("drmp3_init", {includes = "dr_mp3.h"}))
+    end)
