30e6efbbfb178d285d7031d25222bd7ee0898dbc
yamashi <yamashi@users.noreply.github.com>
2021-05-30 16:01:38 +0200

    Add recastnavigation (#440)
    * Add recastnavigation
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua

---
+++ packages/r/recastnavigation/xmake.lua
@@ -0,0 +1,23 @@
+package("recastnavigation")
+
+    set_homepage("https://github.com/recastnavigation/recastnavigation")
+    set_description("Navigation-mesh Toolset for Games")
+    set_license("zlib")
+
+    set_urls("https://github.com/recastnavigation/recastnavigation/archive/refs/tags/$(version).zip",
+             "https://github.com/recastnavigation/recastnavigation.git")
+
+    add_versions("1.5.1", "c541b56bab7543d7c741a3153af9a9024165b607de21503b90c9a399e626947a")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxfuncs("rcCreateHeightfield", {includes = "Recast.h"}))
+    end)
