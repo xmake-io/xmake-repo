f8f6d3f89b8c964034972b9b35dc89c31f4ae6cf
Jérôme Leclercq <lynix680@gmail.com>
2021-06-03 14:19:08 +0200

    Add OpenAL-Soft

---
+++ packages/o/openal-soft/xmake.lua
@@ -0,0 +1,44 @@
+package("openal-soft")
+
+    set_homepage("https://github.com/paceholder/nodeeditor")
+    set_description("OpenAL Soft is a software implementation of the OpenAL 3D audio API.")
+    set_license("LGPL-2.0")
+
+    add_urls("https://github.com/kcat/openal-soft/archive/refs/tags/$(version).tar.gz", {version = function (version)
+        if version:ge("1.21.0") then
+            if version:startswith("openal-soft-") then
+                return version:sub(13)
+            else
+                return version
+            end
+        else
+            return "openal-soft-" .. version
+        end
+    end})
+    add_urls("https://github.com/kcat/openal-soft.git")
+
+    add_versions("1.21.1", "8ac17e4e3b32c1af3d5508acfffb838640669b4274606b7892aa796ca9d7467f")
+    add_deps("cmake")
+
+    on_load(function (package)
+        if not package:config("shared") then
+            package:add("defines", "AL_LIBTYPE_STATIC")
+        end
+    end)
+
+    on_install("windows", "linux", "mingw", "macosx", "android", "iphoneos", function (package)
+        local configs = {"-DALSOFT_EXAMPLES=OFF", "-DALSOFT_UTILS=OFF"}
+        if package:config("shared") then
+            table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
+            table.insert(configs, "-DLIBTYPE=SHARED")
+        else
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+            table.insert(configs, "-DLIBTYPE=STATIC")
+        end
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("alGetProcAddress", {includes = "AL/al.h"}))
+    end)
