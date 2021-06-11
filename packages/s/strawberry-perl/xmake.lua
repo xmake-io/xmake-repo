7e26ab1473213d447f2e6f5f99e55a327bf3c5a5
ruki <waruqi@gmail.com>
2021-06-12 00:09:57 +0800

    improve mirror url for perl

--- packages/s/strawberry-perl/xmake.lua
+++ packages/s/strawberry-perl/xmake.lua
@@ -5,15 +5,15 @@ package("strawberry-perl")
     set_description("Strawberry Perl is a perl environment for MS Windows containing all you need to run and develop perl applications.")
 
     if os.arch() == "x64" or os.arch() == "x86_64" then
-        add_urls("http://strawberryperl.com/download/$(version)/strawberry-perl-$(version)-64bit.zip",
-            {version = function (version) return version:gsub("%+", ".") end})
         add_urls("https://github.com/xmake-mirror/strawberry-perl/releases/download/$(version)/strawberry-perl-$(version)-64bit.zip",
+            {version = function (version) return version:gsub("%+", ".") end})
+        add_urls("http://strawberryperl.com/download/$(version)/strawberry-perl-$(version)-64bit.zip",
             {version = function (version) return version:gsub("%+", ".") end})
         add_versions("5.32.0+1", "24601fdadd25f921501f04505895d2061a8d8ccfbe515241ceddbd2c372fe78e")
     else
-        add_urls("http://strawberryperl.com/download/$(version)/strawberry-perl-$(version)-32bit.zip",
-            {version = function (version) return version:gsub("%+", ".") end})
         add_urls("https://github.com/xmake-mirror/strawberry-perl/releases/download/$(version)/strawberry-perl-$(version)-32bit.zip",
+            {version = function (version) return version:gsub("%+", ".") end})
+        add_urls("http://strawberryperl.com/download/$(version)/strawberry-perl-$(version)-32bit.zip",
             {version = function (version) return version:gsub("%+", ".") end})
         add_versions("5.32.0+1", "0888c87cb99e42a209f7d6b03fd3a72eda53c647b1c27060913e224f644ab866")
     end
