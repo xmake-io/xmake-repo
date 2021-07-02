6be5859f53020f2b90c844d5e41d6033bfdc934b
ruki <waruqi@gmail.com>
2021-07-02 22:42:43 +0800

    fix benchmark

--- packages/b/benchmark/xmake.lua
+++ packages/b/benchmark/xmake.lua
@@ -6,12 +6,15 @@ package("benchmark")
     add_urls("https://github.com/google/benchmark/archive/v$(version).tar.gz",
              "https://github.com/google/benchmark.git")
     add_versions("1.5.2", "dccbdab796baa1043f04982147e67bb6e118fe610da2c65f88912d73987e700c")
+    add_versions("1.5.5", "3bff5f237c317ddfd8d5a9b96b3eede7c0802e799db520d38ce756a2a46a18a0")
 
     if is_plat("linux") then
         add_syslinks("pthread")
     elseif is_plat("windows") then
         add_syslinks("shlwapi")
     end
+
+    add_deps("cmake")
 
     on_install("macosx", "linux", "windows", function (package)
         local configs = {"-DBENCHMARK_ENABLE_TESTING=OFF"}
