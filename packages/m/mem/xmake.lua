09619bb90bf16ebb0e2f0130e81bde5283d5e4eb
ruki <waruqi@gmail.com>
2021-07-03 19:57:46 +0800

    add tar and fix sha256

--- packages/m/mem/xmake.lua
+++ packages/m/mem/xmake.lua
@@ -3,9 +3,10 @@ package("mem")
     set_homepage("https://github.com/0x1F9F1/mem")
     set_description("A collection of C++11 headers useful for reverse engineering")
 
-    set_urls("https://github.com/0x1F9F1/mem.git")
+    add_urls("https://github.com/0x1F9F1/mem/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/0x1F9F1/mem.git")
 
-    add_versions("1.0.0")
+    add_versions("1.0.0", "db1e58b040ea39ec5794fc1dcc6749c81b062579d9f6b086d035266456bccaf3")
 
     on_install("windows", function (package)
         os.cp("include/mem", package:installdir("include"))
