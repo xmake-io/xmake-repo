c40512633f4dbc134823c7d9083943f086c291ea
xq114 <1140735506@qq.com>
2021-07-07 21:59:07 +0800

    update embree

--- packages/e/embree/xmake.lua
+++ packages/e/embree/xmake.lua
@@ -4,14 +4,20 @@ package("embree")
     set_description("Intel® Embree is a collection of high-performance ray tracing kernels, developed at Intel.")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/embree/embree/archive/v$(version).tar.gz")
-    add_versions("3.12.1", "0c9e760b06e178197dd29c9a54f08ff7b184b0487b5ba8b8be058e219e23336e")
+    add_urls("https://github.com/embree/embree/archive/$(version).tar.gz",
+             "https://github.com/embree/embree.git")
+    add_versions("v3.12.1", "0c9e760b06e178197dd29c9a54f08ff7b184b0487b5ba8b8be058e219e23336e")
+    add_versions("v3.13.0", "4d86a69508a7e2eb8710d571096ad024b5174834b84454a8020d3a910af46f4f")
+
+    -- Not recommanded to build embree as a static library.
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean"})
 
     add_deps("cmake", "tbb")
 
     on_install("macosx", "linux", "windows", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DBUILD_DOC=OFF", "-DEMBREE_TUTORIALS=OFF", "-DEMBREE_ISPC_SUPPORT=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DEMBREE_STATIC_LIB=" .. (package:config("shared") and "OFF" or "ON"))
         if package:is_plat("windows") then
             table.insert(configs, "-DUSE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
         end
