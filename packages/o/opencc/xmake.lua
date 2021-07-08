80f927f3bca4f980f15975a88781420c7e19eeb5
wsw0108 <wsw0108@gmail.com>
2021-07-08 17:39:06 +0800

    update opencc

--- packages/o/opencc/xmake.lua
+++ packages/o/opencc/xmake.lua
@@ -8,8 +8,10 @@ package("opencc")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "bsd", function (package)
         local configs = {"-DBUILD_DOCUMENTATION=OFF", "-DENABLE_GTEST=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
