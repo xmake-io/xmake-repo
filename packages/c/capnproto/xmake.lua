129365b677a896844fe94674d9bc6fb59903ee0f
wsw0108 <wsw0108@gmail.com>
2021-07-08 17:44:18 +0800

    update capnproto

--- packages/c/capnproto/xmake.lua
+++ packages/c/capnproto/xmake.lua
@@ -7,16 +7,17 @@ package("capnproto")
     add_versions("0.8.0", "9a5e090b1f3ad39bb47fed5fd03672169493674ce273418b76c868393fced2e4")
     add_versions("0.7.0", "1054a879e174b8f797f1b506fedb14ecba5556c656e33ac51bd0a62bd90f925f")
 
-    add_deps("zlib")
+    add_deps("cmake", "zlib")
 
-    add_deps("cmake")
-
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "bsd", function (package)
         local configs = {}
         table.insert(configs, "-DBUILD_TESTING=OFF")
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
+        package:addenv("PATH", "bin")
     end)
 
     on_test(function (package)
-        assert(package:has_cxxtypes("capnp::MallocMessageBuilder", {configs = {languages = "c++11"}, includes = "capnp/message.h"}))
+        assert(package:has_cxxtypes("capnp::MallocMessageBuilder", {configs = {languages = "c++14"}, includes = "capnp/message.h"}))
     end)
