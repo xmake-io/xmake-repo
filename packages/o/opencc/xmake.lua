1faeb86be10bff3cf4c383f04bf01c9fec548c2f
wsw0108 <wsw0108@gmail.com>
2021-07-09 17:32:11 +0800

    opencc: fix static build

--- packages/o/opencc/xmake.lua
+++ packages/o/opencc/xmake.lua
@@ -6,37 +6,21 @@ package("opencc")
     set_urls("https://github.com/BYVoid/OpenCC/archive/ver.$(version).zip")
     add_versions("1.1.2", "b4a53564d0de446bf28c8539a8a17005a3e2b1877647b68003039e77f8f7d9c2")
 
+    add_patches("1.1.2", path.join(os.scriptdir(), "patches", "1.1.2", "fix-static.patch"), "8f6369e2133a8415bcdb73128814dda900a0833a8aaf6f6fb164e0aaf621adac")
+
     add_deps("cmake")
-    
+
     on_load(function (package)
         package:addenv("PATH", "bin")
     end)
 
-    on_install("linux", "macosx", "bsd", function (package)
+    on_install("windows", "mingw", "linux", "macosx", "bsd", function (package)
         local configs = {"-DBUILD_DOCUMENTATION=OFF", "-DENABLE_GTEST=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
-        if package:config("static") then
-            os.cp("build/deps/marisa-0.2.6/*.a", package:installdir("lib"))
-        end
-    end)
-
-    on_install("windows", "mingw", function (package)
-        local configs = {"-DBUILD_DOCUMENTATION=OFF", "-DENABLE_GTEST=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
-        package:addenv("PATH", "bin")
-        if package:config("static") then
-            os.cp("build/deps/marisa-0.2.6/" .. (package:debug() and "Debug" or "Release") .. "/*.lib", package:installdir("lib"))
-        end
     end)
 
     on_test("windows", "mingw", "linux", "macosx", "bsd", function (package)
-        local configs = {}
-        if package:config("static") then
-            configs.ldflags = "-lmarisa"
-        end
-        assert(package:has_cfuncs("opencc_open", {includes = "opencc/opencc.h", configs = configs}))
+        assert(package:has_cfuncs("opencc_open", {includes = "opencc/opencc.h"}))
     end)
