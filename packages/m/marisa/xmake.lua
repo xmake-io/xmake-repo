e74c087dc574701659d8780d29d64d4ef6af28e5
ruki <waruqi@gmail.com>
2021-07-10 18:12:19 +0800

    Update xmake.lua

--- packages/m/marisa/xmake.lua
+++ packages/m/marisa/xmake.lua
@@ -3,9 +3,9 @@ package("marisa")
     set_homepage("https://github.com/s-yata/marisa-trie")
     set_description("Matching Algorithm with Recursively Implemented StorAge.")
 
-    add_urls("https://github.com/s-yata/marisa-trie/archive/v$(version).zip")
+    add_urls("https://github.com/s-yata/marisa-trie/archive/$(version).zip")
     add_urls("https://github.com/s-yata/marisa-trie.git")
-    add_versions("0.2.6", "8dc0b79ff9948be80fd09df6d2cc70134367339ec7d6496857bc47cf421df1af")
+    add_versions("v0.2.6", "8dc0b79ff9948be80fd09df6d2cc70134367339ec7d6496857bc47cf421df1af")
 
     add_deps("cmake")
 
