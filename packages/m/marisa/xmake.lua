12d3124297863a20c16a9585c42639906c60a77c
wsw0108 <wsw0108@gmail.com>
2021-07-10 17:27:01 +0800

    marisa: update

--- packages/m/marisa/xmake.lua
+++ packages/m/marisa/xmake.lua
@@ -3,8 +3,11 @@ package("marisa")
     set_homepage("https://github.com/s-yata/marisa-trie")
     set_description("Matching Algorithm with Recursively Implemented StorAge.")
 
-    set_urls("https://github.com/s-yata/marisa-trie/archive/v$(version).zip")
+    add_urls("https://github.com/s-yata/marisa-trie/archive/v$(version).zip")
+    add_urls("https://github.com/s-yata/marisa-trie.git")
     add_versions("0.2.6", "8dc0b79ff9948be80fd09df6d2cc70134367339ec7d6496857bc47cf421df1af")
+
+    add_deps("cmake")
 
     on_install(function (package)
         os.cp(path.join(package:scriptdir(), "port", "CMakeLists.txt"), "CMakeLists.txt")
