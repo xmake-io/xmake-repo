3b8d54a9dfae8a5f02cc766fd5901cc45029f29a
jj683 <jj683@protonmail.com>
2021-07-11 16:06:04 +0300

    Initial mFAST package try

---
+++ packages/m/mfast/xmake.lua
@@ -0,0 +1,35 @@
+package("mfast")
+
+    set_homepage("https://github.com/objectcomputing/mFAST")
+    set_description("High performance C++ encoding/decoding library for FAST (FIX Adapted for STreaming) protocol.")
+    set_license("BSD-3-Clause")
+
+    add_urls("https://github.com/objectcomputing/mFAST/archive/refs/tags/$(version).zip",
+             "https://github.com/objectcomputing/mFAST.git")
+    add_versions("v1.2.2", "bcfde8de2a621021841e330438f404041cd285bf10b4dc041f164876f3d8b692")
+
+    add_patches("v1.2.2", path.join(os.scriptdir(), "patches", "v1.2.2", "tinyxml2.patch"), "0b87c087d0d5fd38f681acee71594c2f7e8211800f16b7245d8a2c6a63cba1d1")
+
+    add_configs("sqlite", {description = "Build with SQLite support.", default = true, type = "boolean"})
+
+    add_deps("cmake")
+    add_deps("boost", {configs = {container = true}})
+    add_deps("tinyxml2")
+
+    on_load(function (package)
+        if package:config("sqlite") then
+            package:add("deps", "sqlite3")
+        end
+    end)
+
+    on_install("linux", "macosx", "windows", function (package)
+        local configs = {"-DBUILD_TESTS=OFF",
+                         "-DBUILD_EXAMPLES=OFF",
+                         "-DBUILD_PACKAGES=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:config("sqlite") then
+            table.insert(configs, "-DBUILD_SQLITE3=ON")
+        end
+        import("package.tools.cmake").install(package, configs)
+    end)
