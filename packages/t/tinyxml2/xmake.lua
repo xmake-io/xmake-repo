3dafd241e265fcc3b7282d9b7ca1b2aeff83d4f6
jj683 <87266000+jj683@users.noreply.github.com>
2021-07-11 16:49:43 +0300

    tinyxml2 9.0.0 (#491)

--- packages/t/tinyxml2/xmake.lua
+++ packages/t/tinyxml2/xmake.lua
@@ -6,6 +6,7 @@ package("tinyxml2")
     add_urls("https://github.com/leethomason/tinyxml2/archive/$(version).tar.gz")
     add_urls("https://github.com/leethomason/tinyxml2.git")
     add_versions("8.0.0", "6ce574fbb46751842d23089485ae73d3db12c1b6639cda7721bf3a7ee862012c")
+    add_versions("9.0.0", "cc2f1417c308b1f6acc54f88eb70771a0bf65f76282ce5c40e54cfe52952702c")
 
     if is_plat("linux", "macosx", "windows") then
         add_deps("cmake")
