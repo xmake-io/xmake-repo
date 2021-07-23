82a406acb2b31424cc4b90ccac2398e1d23199a7
xq114 <1140735506@qq.com>
2021-07-23 18:42:45 +0800

    update sqlite3

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -20,6 +20,7 @@ package("sqlite3")
     add_versions("3.34.0+100", "2a3bca581117b3b88e5361d0ef3803ba6d8da604b1c1a47d902ef785c1b53e89")
     add_versions("3.35.0+300", "ecbccdd440bdf32c0e1bb3611d635239e3b5af268248d130d0445a32daf0274b")
     add_versions("3.35.0+400", "7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d")
+    add_versions("3.36.0+000", "bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3")
 
     if is_plat("macosx", "linux", "bsd") then
         add_syslinks("pthread", "dl")
