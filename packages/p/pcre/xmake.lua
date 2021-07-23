b6781d154b96d4fc9f425576214a8f2169343ccf
Hoildkv <42310255+xq114@users.noreply.github.com>
2021-07-23 20:50:27 +0800

    update pcre, expat and sqlite3 (#513)
    * update pcre
    
    * update expat
    
    * update sqlite3

--- packages/p/pcre/xmake.lua
+++ packages/p/pcre/xmake.lua
@@ -5,6 +5,7 @@ package("pcre")
 
     set_urls("https://ftp.pcre.org/pub/pcre/pcre-$(version).zip",
              "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$(version).zip")
+    add_versions("8.45", "5b709aa45ea3b8bb73052947200ad187f651a2049158fb5bbfed329e4322a977")
     add_versions("8.44", "3464cc5effda458e1c80882e53fc710195bf0d6d923c0866cafea43c7f321d25")
     add_versions("8.40", "99e19194fa57d37c38e897d07ecb3366b18e8c395b36c6d555706a7f1df0a5d4")
     add_versions("8.41", "0e914a3a5eb3387cad6ffac591c44b24bc384c4e828643643ebac991b57dfcc5")
