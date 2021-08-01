9b64d4885415e59445c49bfdbc36f8e151905769
ruki <waruqi@gmail.com>
2021-08-01 16:00:29 +0800

    add opengl, glu and glut

---
+++ packages/o/opengl/xmake.lua
@@ -0,0 +1,18 @@
+package("opengl")
+
+    set_homepage("https://opengl.org/")
+    set_description("OpenGL - The Industry Standard for High Performance Graphics")
+
+    on_fetch(function (package, opt)
+        -- we always get it from compiler toolchain
+        if package:is_plat("macosx") then
+            return {frameworks = "OpenGL", defines = "GL_SILENCE_DEPRECATION"}
+        elseif package:is_plat("windows", "mingw") then
+            return {links = "opengl32"}
+        end
+        if opt.system then
+            if package:is_plat("linux") and package.find_package then
+                return package:find_package("opengl", opt) or package:find_package("libgl", opt)
+            end
+        end
+    end)
