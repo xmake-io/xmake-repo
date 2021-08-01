9a5f9218c9af1c00bb589153033a2c6e5021914d
xq114 <1140735506@qq.com>
2021-08-01 15:49:53 +0800

    update taskflow

--- packages/t/taskflow/xmake.lua
+++ packages/t/taskflow/xmake.lua
@@ -2,9 +2,11 @@ package("taskflow")
 
     set_homepage("https://taskflow.github.io/")
     set_description("A fast C++ header-only library to help you quickly write parallel programs with complex task dependencies")
+    set_license("MIT")
 
     add_urls("https://github.com/taskflow/taskflow.git")
     add_urls("https://github.com/taskflow/taskflow/archive/$(version).tar.gz")
+    add_versions("v3.2.0", "26c37a494789fedc5de8d1f8452dc8a7774a220d02c14d5b19efe0dfe0359c0c")
     add_versions("v3.1.0", "17b56e23312d20c4ad5cc497b9f42cd0ad4451dbd2df0160a0a692fd16d47143")
     add_versions("v3.0.0", "553c88a6e56e115d29ac1520b8a0fea4557a5fcda1af1427bd3ba454926d03a2")
 
