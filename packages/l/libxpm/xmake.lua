fc6896f0da355ba33982042d35b46fd26e4465c0
xq114 <1140735506@qq.com>
2021-08-03 00:35:02 +0800

    add libxpm

---
+++ packages/l/libxpm/xmake.lua
@@ -0,0 +1,17 @@
+package("libxpm")
+
+    set_homepage("https://gitlab.freedesktop.org/xorg/lib/libxpm")
+    set_description("X.Org: X Pixmap (XPM) image file format library")
+
+    add_urls("https://www.x.org/archive/individual/lib/libXpm-$(version).tar.gz")
+    add_versions("3.5.13", "e3dfb0fb8c1f127432f2a498c7856b37ce78a61e8da73f1aab165a73dd97ad00")
+
+    add_deps("libx11", "xorgproto", "gettext")
+
+    on_install("macosx", "linux", function (package)
+        import("package.tools.autoconf").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("XpmCreatePixmapFromData", {includes = "X11/xpm.h"}))
+    end)
