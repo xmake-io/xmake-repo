fa3aef299c28f03d97fd7083eea16829f39b76e2
xq114 <1140735506@qq.com>
2021-08-04 17:34:00 +0800

    fix easyloggingpp

--- packages/e/easyloggingpp/xmake.lua
+++ packages/e/easyloggingpp/xmake.lua
@@ -8,24 +8,18 @@ package("easyloggingpp")
              "https://github.com/amrayn/easyloggingpp.git")
     add_versions("v9.97.0", "9110638e21ef02428254af8688bf9e766483db8cc2624144aa3c59006907ce22")
 
-    on_load("windows", function (package)
-        if package:config("shared") then
-            package:add("defines", "ELPP_AS_DLL")
-        end
-    end)
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
     on_install(function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("easyloggingpp")
-                set_kind("$(kind)")
+                set_kind("static")
+                set_languages("c++11")
                 add_files("src/easylogging++.cc")
                 add_headerfiles("src/easylogging++.h")
-                if is_plat("windows") and is_kind("shared") then
-                    add_defines("ELPP_AS_DLL", "ELPP_EXPORT_SYMBOLS")
-                end
         ]])
-        import("package.tools.xmake").install(package, {kind = package:config("shared") and "shared" or "static"})
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
