963c987a3b4f2c6cb9bdd3b0a5ef9d90e96232e7
xq114 <1140735506@qq.com>
2021-08-13 20:19:00 +0800

    improve sourceforge urls

--- packages/i/irrxml/xmake.lua
+++ packages/i/irrxml/xmake.lua
@@ -3,7 +3,7 @@ package("irrxml")
     set_homepage("https://sourceforge.net/projects/irrlicht/")
     set_description("High speed and easy-to-use XML Parser for C++")
 
-    set_urls("https://deac-fra.dl.sourceforge.net/project/irrlicht/irrXML%20SDK/$(version)/irrxml-$(version).zip")
+    set_urls("https://sourceforge.net/projects/irrlicht/files/irrXML%20SDK/$(version)/irrxml-$(version).zip")
     add_versions("1.2", "9b4f80639b2dee3caddbf75862389de684747df27bea7d25f96c7330606d7079")
 
     on_install(function (package)
