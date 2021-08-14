327b5dd6a61e5345c3e10d758e342a73a8e6c54e
yamashi <yamashi@users.noreply.github.com>
2021-08-14 16:38:08 +0200

    Add mimalloc 2.0.2 and 1.7.2 (#562)
    * Add mimalloc 2.0.2 and 1.7.2
    
    * Update xmake.lua
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/m/mimalloc/xmake.lua
+++ packages/m/mimalloc/xmake.lua
@@ -5,7 +5,9 @@ package("mimalloc")
     set_license("MIT")
 
     set_urls("https://github.com/microsoft/mimalloc/archive/v$(version).zip")
+    add_versions("2.0.2", "6ccba822e251b8d10f8a63d5d7767bc0cbfae689756a4047cdf3d1e4a9fd33d0")
     add_versions("2.0.1", "59c1fe79933e0ac9837a9ca4d954e4887dccd80a84281a6f849681b89a8b8876")
+    add_versions("1.7.2", "2c432e44803d9f4f017323be705f194db5d1452f9a60e38896605e7cfe8b332f")
     add_versions("1.7.1", "dc3219066b4fd50c7f23d60c13fa15ae269a2b10b7dd45b046d5c52c9addb477")
     add_versions("1.7.0", "13f3c82bca3a95233c5e29adb5675ab2b772f0ade23184d822079578c9d6c698")
     add_versions("1.6.7", "5a12aac020650876615a2ce3dd8adc8b208cdcee4d9e6bcfc33b3fbe307f0dbf")
@@ -31,8 +33,8 @@ package("mimalloc")
         table.insert(configs, "-DMI_BUILD_TESTS=OFF")
         table.insert(configs, "-DMI_BUILD_OBJECT=OFF")
         --x64:mimalloc-redirect.lib/dll x86:mimalloc-redirect32.lib/dll
-        if package:config("shared") and package:is_plat("windows") and package:is_arch("x86") then
-            io.gsub("CMakeLists.txt", "-redirect", "-redirect32")
+        if package:version():le("2.0.1") and package:config("shared") and package:is_plat("windows") and package:is_arch("x86") then
+            io.replace("CMakeLists.txt", "-redirect.", "-redirect32.", {plain = true})
         end
         local cxflags
         if package:config("rltgenrandom") then
