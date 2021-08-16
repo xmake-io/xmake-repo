7dd1d3516e0d1cd0ec123da73514a8f8b562ae20
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-16 11:55:18 +0200

    Add extsources to zlib

--- packages/z/zlib/xmake.lua
+++ packages/z/zlib/xmake.lua
@@ -8,6 +8,12 @@ package("zlib")
     add_versions("1.2.10", "42cd7b2bdaf1c4570e0877e61f2fdc0bce8019492431d054d3d86925e5058dc5")
     add_versions("1.2.11", "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff")
 
+    if is_plat("linux") then
+        add_extsources("pacman::zlib", "apt::zlib1g-dev")
+    elseif is_plat("macosx") then
+        add_extsources("brew::zlib")
+    end
+
     on_install(function (package)
         io.writefile("xmake.lua", [[
             includes("check_cincludes.lua")
