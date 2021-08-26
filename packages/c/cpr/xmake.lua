9c53310b8fdbd138f703a3767c61aa62751e28eb
ruki <waruqi@gmail.com>
2021-08-26 10:25:27 +0800

    Update xmake.lua

--- packages/c/cpr/xmake.lua
+++ packages/c/cpr/xmake.lua
@@ -3,14 +3,15 @@ package('cpr')
     set_homepage("https://whoshuu.github.io/cpr/")
     set_description("C++ Requests is a simple wrapper around libcurl inspired by the excellent Python Requests project.")
 
-    set_urls("https://github.com/whoshuu/cpr/archive/refs/tags/$(version).tar.gz")
+    set_urls("https://github.com/whoshuu/cpr/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/whoshuu/cpr.git")
     add_versions("1.6.2", "c45f9c55797380c6ba44060f0c73713fbd7989eeb1147aedb8723aa14f3afaa3")
 
     add_deps("libcurl")
 
     add_deps("cmake")
     on_install("linux", "macosx", "windows", function (package)
-        local configs = {"-DCPR_BUILD_TESTS=OFF"}
+        local configs = {"-DCPR_BUILD_TESTS=OFF", "-DCPR_FORCE_USE_SYSTEM_CURL=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release")) 
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF")) 
         import("package.tools.cmake").install(package, configs)
