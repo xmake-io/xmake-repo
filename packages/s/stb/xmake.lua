233d354aac320ec02e217abe67ed17d0801d04c6
ImperatorS79 <fevrier.simon@gmail.com>
2021-08-27 18:38:19 +0200

    Add extsources to stb

--- packages/s/stb/xmake.lua
+++ packages/s/stb/xmake.lua
@@ -6,6 +6,12 @@ package("stb")
 
     add_urls("https://github.com/nothings/stb.git")
     add_versions("0.0", "3a1174060a7dd4eb652d4e6854bc4cd98c159200")
+    
+    if is_plat("mingw") and is_subhost("msys") then
+        add_extsources("pacman::stb")
+    elseif is_host("linux") then
+        add_extsources("pacman::stb", "apt::libstb-dev")
+    end
 
     on_install(function (package)
         os.cp("*.h", package:installdir("include"))
