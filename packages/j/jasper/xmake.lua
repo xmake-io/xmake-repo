7df3b03369d8dacca67060a7011630804653c416
xq114 <1140735506@qq.com>
2021-08-29 19:15:11 +0800

    update jasper

--- packages/j/jasper/xmake.lua
+++ packages/j/jasper/xmake.lua
@@ -7,11 +7,17 @@ package("jasper")
     add_urls("https://github.com/jasper-software/jasper/archive/refs/tags/version-$(version).tar.gz")
     add_versions("2.0.28", "6b4e5f682be0ab1a5acb0eeb6bf41d6ce17a658bb8e2dbda95de40100939cc88")
     add_versions("2.0.32", "a3583a06698a6d6106f2fc413aa42d65d86bedf9a988d60e5cfa38bf72bc64b9")
+    add_versions("2.0.33", "38b8f74565ee9e7fec44657e69adb5c9b2a966ca5947ced5717cde18a7d2eca6")
 
-    add_deps("cmake", "libjpeg")
-    if not is_plat("macosx") then
-        add_deps("freeglut")
-    end
+    add_configs("opengl", {description = "Enable the use of the OpenGL/GLUT Library.", default = false, type = "boolean"})
+
+    add_deps("cmake", "libjpeg-turbo")
+    on_load("windows", "macosx", "linux", function (package)
+        if package:config("opengl") then
+            package:add("deps", "freeglut")
+        end
+    end)
+
     on_install("windows", "macosx", "linux", function (package)
         io.replace("build/cmake/modules/JasOpenGL.cmake", "find_package(GLUT", "find_package(FreeGLUT", {plain = true})
         local configs = {"-DJAS_ENABLE_PROGRAMS=OFF", "-DJAS_ENABLE_DOC=OFF"}
@@ -24,6 +30,7 @@ package("jasper")
         end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DJAS_ENABLE_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DJAS_ENABLE_OPENGL=" .. (package:config("opengl") and "ON" or "OFF"))
         if package:config("pic") ~= false then
             table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
         end
