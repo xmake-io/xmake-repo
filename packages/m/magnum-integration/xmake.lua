d93848fac80ac03fd78e8f77ce7059dd0f96b8a7
Hoildkv <42310255+xq114@users.noreply.github.com>
2021-08-30 07:51:36 +0800

    improve magnum (#610)

---
+++ packages/m/magnum-integration/xmake.lua
@@ -0,0 +1,46 @@
+package("magnum-integration")
+
+    set_homepage("https://magnum.graphics/")
+    set_description("Integration libraries for magnum, Light­weight and mod­u­lar C++11/C++14 graph­ics mid­dle­ware for games and data visu­al­iz­a­tion.")
+    set_license("MIT")
+
+    add_urls("https://github.com/mosra/magnum-integration/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/mosra/magnum-integration.git")
+    add_versions("v2020.06", "4eb461e0a38d7be69a52b8faf7664493da4e4cabc2c4fa86bd672d2e8f0a9311")
+
+    local intergrations = {"bullet", "dart", "eigen", "glm", "imgui", "ovr"}
+    for _, integration in ipairs(integrations) do
+        add_configs(integration, {description = "Build " .. integration .. " integration library.", default = false, type = "boolean"})
+    end
+
+    add_deps("cmake", "magnum")
+    on_load("windows", "linux", "macosx", function (package)
+        local configdeps = {bullet = "bullet3",
+                            eigen = "eigen3",
+                            glm = "glm",
+                            imgui = "imgui"}
+        for config, dep in pairs(configdeps) do
+            if package:config(config) then
+                package:add("deps", dep)
+            end
+        end
+    end)
+
+    on_install("windows", "linux", "macosx", function (package)
+        local configs = {"-DBUILD_TESTS=OFF", "-DLIB_SUFFIX="}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
+        for _, integration in ipairs(integrations) do
+            table.insert(configs, "-DWITH_" .. integration:upper() .. "=" .. (package:config(integration) and "ON" or "OFF"))
+        end
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                auto year = MAGNUMINTEGRATION_VERSION_YEAR;
+                auto month = MAGNUMINTEGRATION_VERSION_MONTH;
+            }
+        ]]}, {configs = {languages = "c++14"}, includes = "Magnum/versionIntegration.h"}))
+    end)
