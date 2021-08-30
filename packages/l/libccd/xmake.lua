1487ea642c1e56464cf26ca232694979741d1d80
xq114 <1140735506@qq.com>
2021-08-30 19:50:34 +0800

    add octomap

--- packages/l/libccd/xmake.lua
+++ packages/l/libccd/xmake.lua
@@ -4,21 +4,20 @@ package("libccd")
     set_description("libccd is library for a collision detection between two convex shapes.")
     set_license("BSD-3-Clause")
 
-    add_urls("https://github.com/danfis/libccd/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://github.com/danfis/libccd/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/danfis/libccd.git")
     add_versions("v2.1", "542b6c47f522d581fbf39e51df32c7d1256ac0c626e7c2b41f1040d4b9d50d1e")
 
     add_configs("double_precision", {description = "Enable double precision floating-point arithmetic.", default = false, type = "boolean"})
 
     on_load("windows", "macosx", "linux", function (package)
-        if not package:config("shared") then
-            package:add("defines", "CCD_STATIC_DEFINE")
-        end
         if not package.is_built or package:is_built() then
             package:add("deps", "cmake")
         end
     end)
 
     on_install("windows", "macosx", "linux", function (package)
+        io.replace("src/ccd/ccd_export.h", "def CCD_STATIC_DEFINE", package:config("shared") and " 0" or " 1", {plain = true})
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
