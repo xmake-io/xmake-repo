974213ea26fd480e8d4c67e6d28b19424afca39c
idealvin <idealvin@qq.com>
2021-09-14 12:22:46 +0800

    add package libbacktrace

---
+++ packages/l/libbacktrace/xmake.lua
@@ -0,0 +1,12 @@
+package("libbacktrace")
+
+    set_homepage("https://github.com/ianlancetaylor/libbacktrace")
+    set_description("A C library that may be linked into a C/C++ program to produce symbolic backtraces")
+
+    add_urls("https://github.com/ianlancetaylor/libbacktrace.git")
+    add_versions("v1.0", "d0f5e95a87a4d3e0a1ed6c069b5dae7cbab3ed2a")
+
+    add_deps("make")
+    on_install("linux", "macosx", function (package)
+        import("package.tools.make").install(package)
+    end)
