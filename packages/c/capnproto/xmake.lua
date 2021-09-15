0a74fda5b80a4977069dbf8e579c1ae13a83c8b2
Hoildkv <42310255+xq114@users.noreply.github.com>
2021-09-15 17:30:19 +0800

    improve opencv (#640)
    * update capnproto
    
    * improve opencv
    
    * fix opencv
    
    * restrict capnproto for mingw
    
    * fix opencv

--- packages/c/capnproto/xmake.lua
+++ packages/c/capnproto/xmake.lua
@@ -2,8 +2,10 @@ package("capnproto")
 
     set_homepage("https://github.com/capnproto/capnproto")
     set_description("Cap'n Proto serialization/RPC system - core tools and C++ library.")
+    set_license("MIT")
 
     set_urls("https://github.com/capnproto/capnproto/archive/v$(version).zip")
+    add_versions("0.9.0", "18cf46aa4e05446a3d34bad8d56f9d0c73e72020a2b7548b6ec7cb7b1a828d5b")
     add_versions("0.8.0", "9a5e090b1f3ad39bb47fed5fd03672169493674ce273418b76c868393fced2e4")
     add_versions("0.7.0", "1054a879e174b8f797f1b506fedb14ecba5556c656e33ac51bd0a62bd90f925f")
 
@@ -12,10 +14,8 @@ package("capnproto")
     end
 
     add_deps("cmake", "zlib")
-
-    on_install("windows", "mingw", "linux", "macosx", "bsd", function (package)
-        local configs = {}
-        table.insert(configs, "-DBUILD_TESTING=OFF")
+    on_install("windows", "mingw@windows,msys", "linux", "macosx", "bsd", function (package)
+        local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
