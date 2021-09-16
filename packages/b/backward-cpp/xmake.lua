d94af9f1500472b8984f010d891455b3bbb03122
jj683 <jj683@protonmail.com>
2021-09-16 12:37:33 +0000

    Fixes

--- packages/b/backward-cpp/xmake.lua
+++ packages/b/backward-cpp/xmake.lua
@@ -8,16 +8,20 @@ package("backward-cpp")
              "https://github.com/bombela/backward-cpp.git")
     add_versions("v1.6", "9b07e12656ab9af8779a84e06865233b9e30fadbb063bf94dd81d318081db8c2")
 
+    add_deps("cmake")
+
     on_install("linux", "macosx", "windows", function (package)
         local configs = {"-DBACKWARD_TESTS=OFF"}
         table.insert(configs, "-DBACKWARD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").build(package, configs, {buildir = "build_xmake"})
+        import("package.tools.cmake").build(package, configs, {buildir = "build"})
 
         os.cp("backward.hpp", package:installdir("include/backward"))
         if package:is_plat("windows") then
-            os.trycp(path.join("build_xmake", "*", "*.lib"), package:installdir("lib"))
+            os.trycp("build/**.dll", package:installdir("bin"))
+            os.trycp("build/**.lib", package:installdir("lib"))
         else
-            os.trycp(path.join("build_xmake", "*.a"), package:installdir("lib"))
+            os.trycp("build/*.so", package:installdir("lib"))
+            os.trycp("build/*.a", package:installdir("lib"))
         end
     end)
 
