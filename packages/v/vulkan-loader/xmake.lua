3eb99e43f695bc86756a537e7b90c445b58042f2
ruki <waruqi@gmail.com>
2021-09-25 00:08:54 +0800

    improve vulkan-loader

--- packages/v/vulkan-loader/xmake.lua
+++ packages/v/vulkan-loader/xmake.lua
@@ -28,7 +28,7 @@ package("vulkan-loader")
         if package:is_plat("linux") then
             local includes = {}
             local linkdirs = {}
-            for _, lib in ipairs({"wayland", "libxrandr", "libxcb", "libxkbcommon"}) do
+            for _, lib in ipairs({"wayland", "libxrandr", "libxrender", "libxcb", "libxkbcommon"}) do
                 local fetchinfo = package:dep(lib):fetch()
                 for _, dir in ipairs(fetchinfo.sysincludedirs or fetchinfo.includedirs) do
                     table.insert(includes, dir)
