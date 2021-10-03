24787009c9b2c951bd09597115900293e65e3907
ruki <waruqi@gmail.com>
2021-10-03 22:25:49 +0800

    improve libisl (#648)

--- packages/l/libisl/xmake.lua
+++ packages/l/libisl/xmake.lua
@@ -3,11 +3,13 @@ package("libisl")
     set_homepage("http://isl.gforge.inria.fr/")
     set_description("Integer Set Library")
 
-    set_urls("http://isl.gforge.inria.fr/isl-$(version).tar.xz")
-    add_versions("0.23", "5efc53efaef151301f4e7dde3856b66812d8153dede24fab17673f801c8698f2")
-    add_versions("0.22", "6c8bc56c477affecba9c59e2c9f026967ac8bad01b51bdd07916db40a517b9fa")
+    set_urls("https://github.com/xmake-mirror/isl/archive/refs/tags/isl-$(version).tar.gz",
+             "git@github.com:xmake-mirror/isl.git")
+    add_versions("0.24", "63eda93f0a79d812a5f051fa57279b8216f32a68204a1a81caa212a392887b7f")
+    add_versions("0.23", "d9fc290a330227c77fc7f58e4532064b946b55027cd1513b48be62f402361d79")
+    add_versions("0.22", "f9785afcd207683ff1822d79f04cf879e350d55dc75658513642e23bcf42886d")
 
-    add_deps("autoconf", "gmp")
+    add_deps("autoconf", "automake", "libtool", "gmp")
 
     on_install("linux", "macosx", function (package)
         local configs = {}
