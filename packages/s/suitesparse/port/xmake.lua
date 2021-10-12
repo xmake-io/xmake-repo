89d3453ebfc5d932b175fcbc8ff5ebbbf17b58ee
xq114 <1140735506@qq.com>
2021-10-12 21:51:11 +0800

    improve format

--- packages/s/suitesparse/port/xmake.lua
+++ packages/s/suitesparse/port/xmake.lua
@@ -5,16 +5,16 @@ set_languages("cxx14")
 add_rules("mode.debug", "mode.release")
 
 -- initialize options
-option("with_cuda", {default=false, showmenu=true, description="Build with CUDA support."})
-option("with_blas", {default="mkl", showmenu=true, description="Choose BLAS vendor.", values={"mkl", "openblas"}})
-option("complex",   {default=false, showmenu=true, description="Build with complex number support."})
-option("graphblas", {default=false, showmenu=true, description="Build GraphBLAS module."})
+option("with_cuda", {default = false, showmenu = true, description = "Build with CUDA support."})
+option("with_blas", {default = "mkl", showmenu = true, description = "Choose BLAS vendor.", values={"mkl", "openblas"}})
+option("complex",   {default = false, showmenu = true, description = "Build with complex number support."})
+option("graphblas", {default = false, showmenu = true, description = "Build GraphBLAS module."})
 
 -- set dependencies
 add_requires("metis")
 add_requires(get_config("with_blas"))
 if has_config("with_cuda") then
-    add_requires("cuda", {system=true, configs={utils={"cublas"}}})
+    add_requires("cuda", {system = true, configs = {utils = {"cublas"}}})
     add_defines("GPU_BLAS")
 end
 if not has_config("complex") then
@@ -30,7 +30,6 @@ if is_plat("windows") and is_kind("shared") then
 end
 
 -- rule for compiling source files with multiple definitions
---   final set: cauchy product of each set in prod_defs
 rule("c.def_gen")
     set_extensions(".c")
     on_buildcmd_file(function (target, batchcmds, sourcefile, opt)
@@ -43,7 +42,6 @@ rule("c.def_gen")
         end
 
         local objectfile
-        -- compiling
         for _, res in ipairs(prod_defs) do
             local rootdir = path.join(target:autogendir(), "rules", "def_gen")
             local filename = table.concat(table.join({path.basename(sourcefile)}, res), "_") .. ".c"
@@ -54,7 +52,6 @@ rule("c.def_gen")
             table.insert(target:objectfiles(), objectfile)
 
             -- add commands
-            -- batchcmds:mkdir(sourcefile_dir)
             if res ~= "none" then
                 batchcmds:show_progress(opt.progress, "${color.build.object}compiling.%s %s with define %s", get_config("mode"), sourcefile, res)
                 batchcmds:compile(sourcefile, objectfile, {configs = {defines = res}})
@@ -71,21 +68,21 @@ rule("c.def_gen")
     end)
 rule_end()
 
-add_rules("c.def_gen", {override=true})
+add_rules("c.def_gen", {override = true})
 
 target("SuiteSparse_config")
     set_kind("$(kind)")
     add_files("SuiteSparse_config/SuiteSparse_config.c")
     add_headerfiles("SuiteSparse_config/SuiteSparse_config.h")
-    add_includedirs("SuiteSparse_config", {public=true})
+    add_includedirs("SuiteSparse_config", {public = true})
 target_end()
 
 for _, libname in ipairs({"AMD", "BTF", "CAMD", "CCOLAMD", "COLAMD"}) do
 target(libname)
     set_kind("$(kind)")
     add_deps("SuiteSparse_config")
-    add_files(libname .. "/Source/*.c", {prod_defs={"none", "DLONG"}})
-    add_includedirs(libname .. "/Include", {public=true})
+    add_files(libname .. "/Source/*.c", {prod_defs = {"none", "DLONG"}})
+    add_includedirs(libname .. "/Include", {public = true})
     add_headerfiles(libname .. "/Include/*.h")
 target_end()
 end
@@ -95,24 +92,24 @@ target("CHOLMOD")
     add_deps("SuiteSparse_config")
     add_deps("AMD", "CAMD", "COLAMD", "CCOLAMD")
     for _, module in ipairs({"Core", "Check", "Cholesky", "MatrixOps", "Partition", "Modify", "Supernodal"}) do
-        add_files("CHOLMOD/" .. module .. "/cholmod*.c", {prod_defs={"none", "DLONG"}})
+        add_files("CHOLMOD/" .. module .. "/cholmod*.c", {prod_defs = {"none", "DLONG"}})
     end
-    add_includedirs("CHOLMOD/Include", {public=true})
+    add_includedirs("CHOLMOD/Include", {public = true})
     add_headerfiles("CHOLMOD/Include/*.h")
     add_packages("metis")
     add_packages(get_config("with_blas"))
     if has_config("with_cuda") then
         add_files("CHOLMOD/GPU/cholmod*.cu")
-        add_files("CHOLMOD/GPU/cholmod*.c", {prod_defs={"none", "DLONG"}})
-        add_packages("cuda", {public=true})
+        add_files("CHOLMOD/GPU/cholmod*.c", {prod_defs = {"none", "DLONG"}})
+        add_packages("cuda", {public = true})
     end
 target_end()
 
 target("CXSparse")
     set_kind("$(kind)")
     add_deps("SuiteSparse_config")
-    add_files("CXSparse/Source/*.c|cs_convert.c", {prod_defs={"none", "CS_LONG"}})
-    add_includedirs("CXSparse/Include", {public=true})
+    add_files("CXSparse/Source/*.c|cs_convert.c", {prod_defs = {"none", "CS_LONG"}})
+    add_includedirs("CXSparse/Include", {public = true})
     add_headerfiles("CXSparse/Include/*.h")
 target_end()
 
@@ -120,16 +117,16 @@ target("KLU")
     set_kind("$(kind)")
     add_deps("SuiteSparse_config")
     add_deps("AMD", "COLAMD", "BTF")
-    add_files("KLU/Source/*.c", {prod_defs={"none", "DLONG"}})
-    add_includedirs("KLU/Include", {public=true})
+    add_files("KLU/Source/*.c", {prod_defs = {"none", "DLONG"}})
+    add_includedirs("KLU/Include", {public = true})
     add_headerfiles("KLU/Include/*.h")
 target_end()
 
 target("LDL")
     set_kind("$(kind)")
     add_deps("SuiteSparse_config")
-    add_files("LDL/Source/*.c", {prod_defs={"none", "LDL_LONG"}})
-    add_includedirs("LDL/Include", {public=true})
+    add_files("LDL/Source/*.c", {prod_defs = {"none", "LDL_LONG"}})
+    add_includedirs("LDL/Include", {public = true})
     add_headerfiles("LDL/Include/*.h")
 target_end()
 
@@ -140,10 +137,10 @@ target("UMFPACK")
     add_files("UMFPACK/Source/umfpack_timer.c", "UMFPACK/Source/umfpack_tictoc.c")
     local doubledefs = {"umf_analyze.c", "umf_apply_order.c", "umf_cholmod.c", "umf_colamd.c", "umf_free.c", "umf_fsize.c", "umf_is_permutation.c", "umf_malloc.c", "umf_realloc.c", "umf_report_perm.c", "umf_singletons.c"}
     for _, file in ipairs(doubledefs) do
-        add_files("UMFPACK/Source/" .. file, {prod_defs={"DINT", "DLONG"}})
+        add_files("UMFPACK/Source/" .. file, {prod_defs = {"DINT", "DLONG"}})
     end
-    add_files("UMFPACK/Source/*.c|umfpack_timer.c|umfpack_tictoc.c|" .. table.concat(doubledefs, "|"), {prod_defs={"DINT", "DLONG", "ZINT", "ZLONG"}})
-    add_includedirs("UMFPACK/Include", {public=true})
+    add_files("UMFPACK/Source/*.c|umfpack_timer.c|umfpack_tictoc.c|" .. table.concat(doubledefs, "|"), {prod_defs = {"DINT", "DLONG", "ZINT", "ZLONG"}})
+    add_includedirs("UMFPACK/Include", {public = true})
     add_headerfiles("UMFPACK/Include/*.h")
     add_packages(get_config("with_blas"), "metis")
 target_end()
@@ -153,7 +150,7 @@ target("SPQR")
     add_deps("SuiteSparse_config")
     add_deps("CHOLMOD")
     add_files("SPQR/Source/*.cpp")
-    add_includedirs("SPQR/Include", {public=true})
+    add_includedirs("SPQR/Include", {public = true})
     add_headerfiles("SPQR/Include/*.h", "SPQR/Include/*.hpp")
     add_packages(get_config("with_blas"), "metis")
     if has_config("with_cuda") then
@@ -178,7 +175,7 @@ target("SuiteSparse_GPURuntime")
     add_deps("SuiteSparse_config")
     add_packages("cuda")
     add_files("SuiteSparse_GPURuntime/Source/*.cpp")
-    add_includedirs("SuiteSparse_GPURuntime/Include", {public=true})
+    add_includedirs("SuiteSparse_GPURuntime/Include", {public = true})
     add_headerfiles("SuiteSparse_GPURuntime/Include/*.hpp")
 target_end()
 
@@ -186,7 +183,7 @@ target("GPUQREngine")
     set_kind("$(kind)")
     add_deps("SuiteSparse_config", "SuiteSparse_GPURuntime")
     add_files("GPUQREngine/Source/**.cpp", "GPUQREngine/Source/*.cu")
-    add_includedirs("GPUQREngine/Include", {public=true})
+    add_includedirs("GPUQREngine/Include", {public = true})
     add_headerfiles("GPUQREngine/Include/(**.hpp)", "GPUQREngine/Include/(**.cu)")
 target_end()
 end
