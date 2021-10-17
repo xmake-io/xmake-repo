cb9cff32fc5b05109f0a869f4dcdbc8b859a3f73
vkensou <boywanyx@163.com>
2021-10-17 10:47:38 +0800

    add vulkan-headers 1.2.182

--- packages/v/vulkan-headers/xmake.lua
+++ packages/v/vulkan-headers/xmake.lua
@@ -7,6 +7,7 @@ package("vulkan-headers")
 
     add_urls("https://github.com/KhronosGroup/Vulkan-Headers/archive/$(version).tar.gz", {version = function (version) return version:startswith("v") and version or "sdk-" .. version:gsub("%+", ".") end})
     add_versions("1.2.189+1", "ce2eb5995dddd8ff2cee897ab91c30a35d6096d5996fc91cec42bfb37112d3f8")
+    add_versions("1.2.182+0", "61c05dc8a24d5a9104ca2cd233cb9febc3455d69a64e404c3535293f3a463d02")
     add_versions("1.2.162+0", "eb0f6a79ac38e137f55a0e13641140e63b765c8ec717a65bf3904614ef754365")
     add_versions("1.2.154+0", "a0528ade4dd3bd826b960ba4ccabc62e92ecedc3c70331b291e0a7671b3520f9")
 
