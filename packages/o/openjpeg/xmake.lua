d33b554f7f6c32ad897cb535ae879f5e0ef82871
xq114 <1140735506@qq.com>
2021-10-21 20:06:51 +0800

    improve openjpeg

--- packages/o/openjpeg/xmake.lua
+++ packages/o/openjpeg/xmake.lua
@@ -11,15 +11,23 @@ package("openjpeg")
 
     add_deps("cmake")
     add_deps("lcms", "libtiff", "libpng")
+    if is_plat("linux") then
+        add_syslinks("pthread")
+    end
 
     on_load("windows", "linux", "macosx", function (package)
         local ver = package:version():major() .. "." .. package:version():minor()
         package:add("includedirs", "include/openjpeg-" .. ver)
+        if package:is_plat("windows") and not package:config("shared") then
+            package:add("defines", "OPJ_STATIC")
+        end
     end)
 
     on_install("windows", "linux", "macosx", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DBUILD_DOC=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
         import("package.tools.cmake").install(package, configs)
         package:add("PATH", "bin")
         
