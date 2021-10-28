39985abf18074802222ba4c7b791e9e4a619493d
xq114 <1140735506@qq.com>
2021-10-28 19:17:12 +0800

    update libffi

--- packages/l/libffi/xmake.lua
+++ packages/l/libffi/xmake.lua
@@ -3,22 +3,36 @@ package("libffi")
     set_homepage("https://sourceware.org/libffi/")
     set_description("Portable Foreign Function Interface library.")
 
-    set_urls("https://sourceware.org/pub/libffi/libffi-$(version).tar.gz",
-             "https://github.com/libffi/libffi/releases/download/v$(version)/libffi-$(version).tar.gz",
+    set_urls("https://github.com/libffi/libffi/releases/download/v$(version)/libffi-$(version).tar.gz",
              "https://github.com/libffi/libffi.git")
     add_versions("3.2.1", "d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37")
     add_versions("3.3", "72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056")
+    add_versions("3.4.2", "540fb721619a6aba3bdeef7d940d8e9e0e6d2c193595bc243241b77ff9e93620")
 
     if is_plat("linux") then
         add_extsources("apt::libffi-dev")
     end
 
-    on_load(function (package)
+    on_load("windows", function (package)
+        if not package:config("shared") then
+            package:add("defines", "FFI_BUILDING")
+        end
+    end)
+
+    on_load("macosx", "linux", "iphoneos", function (package)
         if package:gitref() then
             package:add("deps", "autoconf", "automake", "libtool")
         elseif package:version():le("3.2.1") then
             package:add("includedirs", "lib/libffi-" .. package:version_str() .. "/include")
         end
+    end)
+
+    on_install("windows", function (package)
+        io.gsub("fficonfig.h.in", "# *undef (.-)\n", "${define %1}\n")
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, {
+            vers = package:version_str()
+        })
     end)
 
     on_install("macosx", "linux", "iphoneos", function (package)
