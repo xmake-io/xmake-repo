ab9a79bbda0a411f02817583fe95ad0924b3ea1f
ruki <waruqi@gmail.com>
2021-11-05 15:38:24 +0800

    fix pcre2 url

--- packages/p/pcre2/xmake.lua
+++ packages/p/pcre2/xmake.lua
@@ -3,12 +3,9 @@ package("pcre2")
     set_homepage("https://www.pcre.org/")
     set_description("A Perl Compatible Regular Expressions Library")
 
-    set_urls("https://ftp.pcre.org/pub/pcre/pcre2-$(version).zip",
-             "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-$(version).zip")
+    set_urls("https://github.com/PhilipHazel/pcre2/releases/download/pcre2-$(version)/pcre2-$(version).tar.gz")
 
-    add_versions("10.23", "6301a525a8a7e63a5fac0c2fbfa0374d3eb133e511d886771e097e427707094a")
-    add_versions("10.30", "3677ce17854fffa68fce6b66442858f48f0de1f537f18439e4bd2771f8b4c7fb")
-    add_versions("10.31", "b4b40695a5347a770407d492c1749e35ba3970ca03fe83eb2c35d44343a5a444")
+    add_versions("10.39", "0781bd2536ef5279b1943471fdcdbd9961a2845e1d2c9ad849b9bd98ba1a9bd4")
 
     if is_host("windows") then
         add_deps("cmake")
