1c759cf020e6e7128482f11f11a4876270b610c5
Hoildkv <42310255+xq114@users.noreply.github.com>
2021-11-11 17:35:31 +0800

    add gnu-gsl (#684)
    * add gnu-gsl
    
    * update libarchive
    
    * update some packages

--- packages/b/bison/xmake.lua
+++ packages/b/bison/xmake.lua
@@ -8,6 +8,7 @@ package("bison")
     add_urls("http://ftp.gnu.org/gnu/bison/bison-$(version).tar.gz")
     add_versions("3.7.4", "fbabc7359ccd8b4b36d47bfe37ebbce44805c052526d5558b95eda125d1677e2")
     add_versions("3.7.6", "69dc0bb46ea8fc307d4ca1e0b61c8c355eb207d0b0c69f4f8462328e74d7b9ea")
+    add_versions("3.8.2", "06c9e13bdf7eb24d4ceb6b59205a4f67c2c7e7213119644430fe82fbd14a0abb")
 
     if is_plat("linux") then
         add_deps("m4")
--- packages/p/pybind11/xmake.lua
+++ packages/p/pybind11/xmake.lua
@@ -1,13 +1,16 @@
 package("pybind11")
 
+    set_kind("library", {headeronly = true})
     set_homepage("https://github.com/pybind/pybind11")
     set_description("Seamless operability between C++11 and Python.")
+    set_license("BSD-3-Clause")
 
     add_urls("https://github.com/pybind/pybind11/archive/$(version).zip",
              "https://github.com/pybind/pybind11.git")
     add_versions("v2.5.0", "1859f121837f6c41b0c6223d617b85a63f2f72132bae3135a2aa290582d61520")
     add_versions("v2.6.2", "0bdb5fd9616fcfa20918d043501883bf912502843d5afc5bc7329a8bceb157b3")
     add_versions("v2.7.1", "350ebf8f4c025687503a80350897c95d8271bf536d98261f0b8ed2c1a697070f")
+    add_versions("v2.8.1", "90907e50b76c8e04f1b99e751958d18e72c4cffa750474b5395a93042035e4a3")
 
     add_deps("cmake", "python 3.x")
 
