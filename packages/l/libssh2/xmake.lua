b197821701792a33c78d9cca1885aff3a2efa94e
ruki <waruqi@gmail.com>
2021-11-13 22:44:12 +0800

    improve libssh2

--- packages/l/libssh2/xmake.lua
+++ packages/l/libssh2/xmake.lua
@@ -9,7 +9,7 @@ package("libssh2")
              "https://github.com/libssh2/libssh2.git")
     add_versions("1.10.0", "2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51")
 
-    add_deps("cmake", "libgcrypt", "libgpg-error")
+    add_deps("cmake", "libgcrypt", "libgpg-error", "gettext")
     if is_plat("linux") then
         add_deps("openssl")
     end
@@ -18,7 +18,7 @@ package("libssh2")
         local configs = {"-DBUILD_EXAMPLES=OFF", "-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = "libgpg-error"})
+        import("package.tools.cmake").install(package, configs, {packagedeps = "libgpg-error", "gettext"})
     end)
 
     on_test(function (package)
