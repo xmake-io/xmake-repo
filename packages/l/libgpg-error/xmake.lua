838e13c0e424c15f1abb0b07e9ddd8322ee3c8a7
ruki <waruqi@gmail.com>
2021-11-13 21:02:50 +0800

    improve gpg-error

--- packages/l/libgpg-error/xmake.lua
+++ packages/l/libgpg-error/xmake.lua
@@ -7,7 +7,7 @@ package("libgpg-error")
     add_urls("https://github.com/gpg/libgpg-error/archive/refs/tags/libgpg-error-$(version).tar.gz")
     add_versions("1.39", "fff17f17928bc6efa2775b16d2ea986a9b82c128ab64dc877325cce468d9b4de")
 
-    add_deps("gettext")
+    add_deps("automake", "autoconf", "gettext")
 
     on_install("linux", "macosx", function (package)
         local configs = {"--disable-doc", "--disable-tests", "--with-pic"}
