6c365f4ee10dee08e669eb8641e731ba6788ec0d
ruki <waruqi@gmail.com>
2021-11-13 20:39:43 +0800

    remove windows

--- packages/l/libssh2/xmake.lua
+++ packages/l/libssh2/xmake.lua
@@ -11,7 +11,7 @@ package("libssh2")
 
     add_deps("cmake", "libgcrypt")
 
-    on_install("macosx", "linux", "windows", function (package)
+    on_install("macosx", "linux", function (package)
         local configs = {"-DBUILD_EXAMPLES=OFF", "-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
