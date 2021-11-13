294129025c8c7abc5518457d818dc85f39b9ad7c
ruki <waruqi@gmail.com>
2021-11-13 23:25:11 +0800

    use autoconf

--- packages/l/libssh2/xmake.lua
+++ packages/l/libssh2/xmake.lua
@@ -9,16 +9,35 @@ package("libssh2")
              "https://github.com/libssh2/libssh2.git")
     add_versions("1.10.0", "2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51")
 
-    add_deps("cmake", "libgcrypt", "libgpg-error", "gettext")
+    add_deps("libgcrypt")
     if is_plat("linux") then
         add_deps("openssl")
     end
 
+    on_load(function (package)
+        if package:gitref() then
+            package:add("deps", "automake", "autoconf")
+        end
+    end)
+
     on_install("macosx", "linux", function (package)
-        local configs = {"-DBUILD_EXAMPLES=OFF", "-DBUILD_TESTING=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = "libgpg-error", "gettext"})
+        local configs = {
+        "--disable-silent-rules",
+        "--disable-examples-build",
+        "--with-openssl",
+        "--with-libz"}
+        local openssl = package:dep("openssl")
+        if openssl then
+            table.insert(configs, "--with-libssl-prefix=" .. openssl:installdir())
+        end
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        if package:is_plat("linux") and package:config("pic") ~= false then
+            table.insert(configs, "--with-pic")
+        end
+        if package:gitref() then
+            os.vrunv("sh", {"./buildconf"})
+        end
+        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_test(function (package)
