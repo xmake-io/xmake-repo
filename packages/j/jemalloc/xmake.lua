f4aa2f86e55135e72e62b47c41a58c3da50f5ce6
ruki <waruqi@gmail.com>
2021-11-14 11:14:30 +0800

    improve jemalloc

--- packages/j/jemalloc/xmake.lua
+++ packages/j/jemalloc/xmake.lua
@@ -11,13 +11,22 @@ fragmentation avoidance and scalable concurrency support]])
 
     add_patches("5.2.1", path.join(os.scriptdir(), "patches", "5.2.1", "fix_nothrow_type.patch"), "d79f5c8767695059ff541f291db3fbc57c9b67299dc129848dd365c2f51b214a")
 
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread", "dl")
+    end
+
     on_load(function (package)
         if package:gitref() then
             package:add("deps", "automake", "autoconf")
         end
+        if package:is_plat("linux") and package:config("pic") ~= false then
+            package:add("links", "jemalloc_pic")
+        else
+            package:add("links", "jemalloc")
+        end
     end)
 
-    on_install(function(package)
+    on_install("linux", "macosx", "bsd", "mingw@macosx", function(package)
         local configs = {"--disable-debug",
                          "--with-jemalloc-prefix="}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
@@ -32,7 +41,7 @@ fragmentation avoidance and scalable concurrency support]])
             test = [[
               #include <stdlib.h>
               #include <jemalloc/jemalloc.h>
-              int main(void) {
+              static void test() {
                 for (size_t i = 0; i < 1000; i++) {
                     malloc(i * 100);
                 }
