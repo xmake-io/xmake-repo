c95147fafbd6bb81bb87b9962822dbb0657508ff
ruki <waruqi@gmail.com>
2021-12-08 11:36:55 +0800

    Update xmake.lua

--- packages/e/ed/xmake.lua
+++ packages/e/ed/xmake.lua
@@ -5,10 +5,9 @@ package("ed")
     set_description("Classic UNIX line editor")
     set_license("GPL-3.0-or-later")
 
-    set_urls("https://ftp.gnu.org/gnu/ed/ed-$(version).tar.lz",
-             "https://ftpmirror.gnu.org/ed/ed-$(version).tar.lz")
-    add_versions("1.17", "71de39883c25b6fab44add80635382a10c9bf154515b94729f4a6529ddcc5e54")
-
+    set_urls("https://ftp.gnu.org/gnu/ed/ed-$(version).tar.gz",
+             "https://ftpmirror.gnu.org/ed/ed-$(version).tar.gz")
+    add_versions("1.17", "db36da85ee1a9d8bafb4b041bd4c8c11becba0c43ec446353b67045de1634fda")
 
     on_install("linux", "macosx", function (package)
         import("package.tools.autoconf").install(package)
