8793291fa65f76162a20a66f66fcaa38159352c2
ruki <waruqi@gmail.com>
2021-12-11 16:53:41 +0800

    Update xmake.lua

--- packages/l/libusbmuxd/xmake.lua
+++ packages/l/libusbmuxd/xmake.lua
@@ -8,12 +8,31 @@ package("libusbmuxd")
              "https://github.com/libimobiledevice/libusbmuxd.git")
     add_versions("2.0.2", "8ae3e1d9340177f8f3a785be276435869363de79f491d05d8a84a59efc8a8fdc")
 
+    if is_plat("windows", "mingw") then
+        add_syslinks("ws2_32")
+    end
+
     add_deps("libplist")
     on_install("macosx", "linux", "mingw@macosx", function (package)
-        local configs = {"--with-pic"}
+        local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-        import("package.tools.autoconf").install(package, configs)
+        if package:is_plat("linux") and package:config("pic") ~= false then
+            table.insert(configs, "--with-pic")
+        end
+        local cflags
+        if package:is_plat("mingw") then
+            -- disable ifaddrs for mingw
+            cflags = {"-DWIN32", "-D_WIN32_WINNT=0x0600"}
+            io.replace("common/socket.c", "AF_INET6", "AF_INET6_")
+        end
+        -- disable tools
+        io.replace("tools/Makefile.am", "bin_PROGRAMS = iproxy inetcat", "bin_PROGRAMS =")
+        -- fix multiple definition with libplist
+        io.replace("common/thread.c", "thread_once", "thread_once_")
+        io.replace("common/thread.h", "thread_once", "thread_once_")
+        io.replace("src/libusbmuxd.c", "thread_once", "thread_once_")
+        import("package.tools.autoconf").install(package, configs, {cflags = cflags})
     end)
 
     on_test(function (package)
