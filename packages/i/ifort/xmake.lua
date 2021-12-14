4e57a2937927f2fd80ec7fe8ee38246e55f7323b
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-12-14 15:05:47 +0100

    Add windows support to ifort

--- packages/i/ifort/xmake.lua
+++ packages/i/ifort/xmake.lua
@@ -19,7 +19,7 @@ package("ifort")
     end)
     
     on_install("@linux", function(package)
-	local version = package:version_str()
+    local version = package:version_str()
         os.execv("curl", {"https://registrationcenter-download.intel.com/akdlm/irc_nas/18210/l_fortran-compiler_p_" .. version .. ".3224.sh", "-o", path.join(package:cachedir(), "install_ifort.sh")})
         os.cd(package:cachedir())
         
@@ -30,12 +30,36 @@ package("ifort")
         table.insert(argv, "-s")
         table.insert(argv, "--eula")
         table.insert(argv, "accept")
+        -- table.insert(argv, "--install-dir")
+        -- table.insert(argv, package:installdir())
 
         os.execv(script_path, argv)
 
         local arch = package:arch()
         package:addenv("PATH", path.join("~/intel/oneapi/compiler", version, "linux/bin", arch == "x86_64" and "intel64" or "ia32"))
         package:addenv("LD_LIBRARY_PATH", path.join("~/intel/oneapi/compiler", version, "linux/compiler/lib", arch == "x86_64" and "intel64" or "ia32"))
+    end)
+
+    on_install("@windows", function(package)
+    local version = package:version_str()
+        os.execv("curl", {"https://registrationcenter-download.intel.com/akdlm/irc_nas/18215/w_fortran-compiler_p_" .. version .. ".3208.exe", "-o", path.join(package:cachedir(), "install_ifort.exe")})
+        os.cd(package:cachedir())
+        
+        local exe_path = "./install_ifort.exe"
+        local argv = {}
+        table.insert(argv, "-a")
+        table.insert(argv, "-s")
+        table.insert(argv, "--eula")
+        table.insert(argv, "accept")
+        table.insert(argv, "-p=NEED_VS2017_INTEGRATION=0")
+        -- table.insert(argv, "--install-dir")
+        -- table.insert(argv, package:installdir())
+
+        os.execv(script_path, argv)
+
+        local arch = package:arch()
+        local compiler_path = vformat("$(env IFORT_COMPILER21)")
+        package:addenv("PATH", path.join(compiler_path, "bin", arch == "x64" and "intel64" or "ia32"))
     end)
 
     on_test(function (package)
