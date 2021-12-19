f518b7a5e6d2a57e287e626d1cfdc74573a6933d
ruki <waruqi@gmail.com>
2021-12-19 13:07:13 +0800

    Create xmake.lua

---
+++ packages/i/imgui-sfml/xmake.lua
@@ -0,0 +1,44 @@
+package("imgui-sfml")
+
+    set_homepage("https://github.com/eliasdaler/imgui-sfml")
+    set_description("Dear ImGui binding for use with SFML")
+
+    add_urls("https://github.com/eliasdaler/imgui-sfml/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/eliasdaler/imgui-sfml.git")
+
+    add_versions("v2.5", "3775c9303f656297f2392e91ffae2021e874ee319b4139c60076d6f757ede109")
+
+    add_deps("imgui", {system = false, private = true})
+    add_deps("sfml")
+    add_deps("opengl", {optional = true})
+
+    on_install("macosx", "linux", "windows", "mingw", function (package)
+        local configs = {"-DIMGUI_SFML_FIND_SFML=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        local imgui_sourcedir = package:dep("imgui"):cachedir()
+        if imgui_sourcedir then
+            local imguidir = import("lib.detect.find_path")("imgui.h", path.join(imgui_sourcedir, "source", "*"))
+            if imguidir then
+                table.insert(configs, "-DIMGUI_DIR=" .. imguidir)
+            end
+        end
+        import("package.tools.cmake").install(package, configs, {packagedeps = "sfml"})
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "imgui.h"
+            #include "imgui-SFML.h"
+
+            #include <SFML/Graphics/CircleShape.hpp>
+            #include <SFML/Graphics/RenderWindow.hpp>
+            #include <SFML/System/Clock.hpp>
+            #include <SFML/Window/Event.hpp>
+            void test() {
+                sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+                window.setFramerateLimit(60);
+                ImGui::SFML::Init(window);
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
