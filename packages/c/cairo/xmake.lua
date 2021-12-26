56b6f106775f52acdc47c0274cbba7853506880e
Kelvin Zhang <zhangxp1998@gmail.com>
2021-12-26 18:35:43 -0500

    Use meson to build cairo

--- packages/c/cairo/xmake.lua
+++ packages/c/cairo/xmake.lua
@@ -3,13 +3,13 @@ package("cairo")
     set_homepage("https://cairographics.org/")
     set_description("Vector graphics library with cross-device output support.")
 
-    set_urls("https://cairographics.org/releases/cairo-$(version).tar.xz")
-    add_versions("1.16.0", "5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331")
+    set_urls("https://gitlab.freedesktop.org/cairo/cairo/-/archive/$(version)/cairo-$(version).tar.gz",
+             "https://gitlab.freedesktop.org/cairo/cairo/-/archive/a04786b9330109ce54bf7f65c7068281419cec6a/cairo-a04786b9330109ce54bf7f65c7068281419cec6a.tar.gz")
+    add_versions("2021.10.07", "8fc7e374a2de1d975171b58c7d43e4d430a28da082c0536ad6e2b178a9863d03")
 
-    add_deps("libpng", "pixman", "zlib", "freetype")
-    if is_plat("linux") then
-        add_deps("fontconfig")
-    end
+
+    add_deps("libpng", "pixman", "zlib", "freetype", "fontconfig")
+
 
     if is_plat("windows") then
         add_syslinks("gdi32", "msimg32", "user32")
@@ -26,10 +26,19 @@ package("cairo")
     end)
 
     on_install("windows", "macosx", "linux", function (package)
-        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        os.cp(path.join(package:scriptdir(), "port", "cairo-features.h.in"), "cairo-features.h")
-        io.replace("cairo-features.h", "${FC_ON}", (package:is_plat("linux") and "1" or "0"), {plain = true})
-        import("package.tools.xmake").install(package, {kind = package:config("shared") and "shared" or "static"})
+        import("package.tools.meson")
+        local configs = {}
+        
+        table.insert(configs, "-Ddebug=" .. (package:debug() and "true" or "false"))
+        table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
+        io.gsub("meson.build", "subdir%('test'%)", "")
+        io.gsub("meson.build", "subdir%('fuzzing'%)", "")
+        io.gsub("meson.build", "subdir%('docs'%)", "")
+        io.replace("src/meson.build", ", subdir: 'cairo'", "", {plain = true})
+        io.replace("util/cairo-gobject/meson.build", ", subdir: 'cairo'", "", {plain = true})
+        io.replace("util/cairo-script/meson.build", ", subdir: 'cairo'", "", {plain = true})
+
+        meson.install(package, configs, {packagedeps = {"glib", "gettext"}})
     end)
 
     on_test(function (package)
