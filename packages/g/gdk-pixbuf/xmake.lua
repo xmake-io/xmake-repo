fada308f9ccf8340ab168ed92bebcf75096e100d
ruki <waruqi@gmail.com>
2022-01-06 13:31:41 +0800

    Update xmake.lua

--- packages/g/gdk-pixbuf/xmake.lua
+++ packages/g/gdk-pixbuf/xmake.lua
@@ -4,8 +4,9 @@ package("gdk-pixbuf")
     set_description("GdkPixbuf is a library that loads image data in various formats and stores it as linear buffers in memory. The buffers can then be scaled, composited, modified, saved, or rendered.")
     set_license("LGPL-2.0")
 
-    set_urls("https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/archive/$(version)/gdk-pixbuf-$(version).tar.gz")
-    add_urls("https://gitlab.gnome.org/GNOME/gdk-pixbuf.git")
+    add_urls("https://github.com/GNOME/gdk-pixbuf/archive/refs/tags/$(version).tar.gz",
+             "https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/archive/$(version)/gdk-pixbuf-$(version).tar.gz",
+             "https://gitlab.gnome.org/GNOME/gdk-pixbuf.git")
 
     add_versions("2.42.6", "c4f3a84a04bc7c5f4fbd97dce7976ab648c60628f72ad4c7b79edce2bbdb494d")
     add_includedirs("include", "include/gdk-pixbuf-2.0")
@@ -20,7 +21,6 @@ package("gdk-pixbuf")
     end
 
     on_install("macosx", "linux", function (package)
-        import("package.tools.meson")
         local configs = {"-Dman=false",
                          "-Dgtk_doc=false",
                          "-Dpng=true",
@@ -30,14 +30,12 @@ package("gdk-pixbuf")
                          "-Drelocatable=true",
                          "-Djpeg=true",
                          "-Dinstalled_tests=false"}
-        
         table.insert(configs, "-Ddebug=" .. (package:debug() and "true" or "false"))
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         io.gsub("meson.build", "subdir%('tests'%)", "")
         io.gsub("meson.build", "subdir%('fuzzing'%)", "")
         io.gsub("meson.build", "subdir%('docs'%)", "")
-
-        meson.install(package, configs, {packagedeps = {"libpng", "libjpeg", "glib"}})
+        import("package.tools.meson").install(package, configs, {packagedeps = {"libpng", "libjpeg", "glib"}})
     end)
 
     on_test(function (package)
