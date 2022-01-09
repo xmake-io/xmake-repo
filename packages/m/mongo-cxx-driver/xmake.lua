73ddca1e42932cc2a4c89ee0311ae52c8bf37cf4
fasiondog <fasiondog@sina.com>
2022-01-10 00:03:30 +0800

    add mongo-cxx-driver

--- packages/m/mongo-cxx-driver/xmake.lua
+++ packages/m/mongo-cxx-driver/xmake.lua
@@ -18,7 +18,7 @@ package("mongo-cxx-driver")
         package:add("includedirs", "/include/mongocxx/v_noabi")
     end)
 
-    on_install("windows", "macosx", function (package)
+    on_install("windows", "macosx", "linux", function (package)
         local configs = {"-DBUILD_SHARED_LIBS=ON", 
                          "-DBUILD_SHARED_AND_STATIC_LIBS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
@@ -27,14 +27,3 @@ package("mongo-cxx-driver")
         end
         import("package.tools.cmake").install(package, configs, {cmake_build = true, config = (package:debug() and "Debug" or "Release")})
     end)
-
-    on_install("linux", function (package)
-        -- unbuntu 18.04 下，使用 gcc 编译失败，指定 clang++ 编译
-        local configs = {"-DCMAKE_CXX_COMPILER=clang++",
-                         "-DBUILD_SHARED_LIBS=ON", 
-                         "-DBUILD_SHARED_AND_STATIC_LIBS=OFF"}
-        if package:version() then
-            table.insert(configs, "-DBUILD_VERSION=" .. package:version())
-        end
-        import("package.tools.cmake").install(package, configs)
-    end)        
