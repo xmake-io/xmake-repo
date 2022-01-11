40efe1142ceba9dc78c43382347d4dd7cc2b4c3f
ruki <waruqi@gmail.com>
2022-01-11 22:35:39 +0800

    improve promise-cpp

--- packages/p/promise-cpp/xmake.lua
+++ packages/p/promise-cpp/xmake.lua
@@ -14,14 +14,31 @@ package("promise-cpp")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DPROMISE_BUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        io.replace("CMakeLists.txt", "add_subdirectory(./example/mfc_timer)", {plain = true})
+        io.replace("CMakeLists.txt", [[add_executable(asio_benchmark_test ${my_headers} example/asio_benchmark_test.cpp)
+    target_compile_definitions(asio_benchmark_test PRIVATE BOOST_ALL_NO_LIB)  
+    target_link_libraries(asio_benchmark_test PRIVATE promise)
+
+    add_executable(asio_timer ${my_headers} example/asio_timer.cpp)
+    target_compile_definitions(asio_timer PRIVATE BOOST_ALL_NO_LIB)  
+    target_link_libraries(asio_timer PRIVATE promise)
+
+    add_executable(asio_http_client ${my_headers} example/asio_http_client.cpp)
+    target_compile_definitions(asio_http_client PRIVATE BOOST_ALL_NO_LIB)  
+    target_link_libraries(asio_http_client PRIVATE promise)
+
+    add_executable(asio_http_server ${my_headers} example/asio_http_server.cpp)
+    target_compile_definitions(asio_http_server PRIVATE BOOST_ALL_NO_LIB)  
+    target_link_libraries(asio_http_server PRIVATE promise)]], "", {plain = true})
         import("package.tools.cmake").install(package, configs, {buildir = "build"})
         os.cp("include", package:installdir())
         os.cp("add_ons", package:installdir("include"))
         print(os.files(package:installdir("lib") .. "/**.a"))
+        print(os.files(package:installdir("lib") .. "/**.so"))
         print(os.files(package:installdir("lib") .. "/**.lib"))
         print(os.files("build/**.a"))
+        print(os.files("build/**.so"))
         print(os.files("build/**.lib"))
-
     end)
 
     on_test(function (package)
@@ -64,3 +81,4 @@ package("promise-cpp")
         }
         ]]}, {configs = {languages = "c++14"}}))
     end)
+
