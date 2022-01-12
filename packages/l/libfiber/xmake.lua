ea735dd54cbb0ab8ec7381bfd6e033e14e75eb07
ruki <waruqi@gmail.com>
2022-01-12 09:05:06 +0800

    Update xmake.lua

--- packages/l/libfiber/xmake.lua
+++ packages/l/libfiber/xmake.lua
@@ -9,8 +9,8 @@ package("libfiber")
     add_versions("v0.9.0+0", "9359a7ebc8d9c48cfaa4c7d1445b3a3e1c392a238574f1d4f7c1191ec8242af2")
 
     add_deps("cmake")
-    if is_plat("linux") then
-        add_syslinks("pthread", "dl", "m")
+    if is_plat("macosx", "linux") then
+        add_syslinks("pthread")
     end
 
     on_load(function (package)
@@ -23,20 +23,21 @@ package("libfiber")
         end
     end)
 
-    on_install("linux", "macosx", "windows", "android", function (package)
-        local configs = {}
+    on_install("linux", "macosx", "android", function (package)
+        local configs = {"-Wno-dev"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        os.cd("c")
-        io.replace("CMakeLists.txt", "-Werror", "", {plain = true})
+        io.writefile("CMakeLists.txt", [[
+        cmake_minimum_required(VERSION 2.8)
+        project(libfiber)
+        add_subdirectory("c")
+        add_subdirectory("cpp")]])
+        io.replace("c/CMakeLists.txt", "-Werror", "", {plain = true})
+        io.replace("cpp/CMakeLists.txt", "-Werror", "", {plain = true})
         import("package.tools.cmake").install(package, configs, {buildir = "build"})
-        os.cp("include", package:installdir())
+        os.cp("c/include", package:installdir())
+        os.cp("cpp/include/**.hpp", package:installdir("include/fiber"))
         os.cp("build/lib", package:installdir())
-        os.cd("../cpp")
-        io.replace("CMakeLists.txt", "-Werror", "", {plain = true})
-        import("package.tools.cmake").install(package, configs, {buildir = "build"})
-        os.cp("include/**.hpp", package:installdir("include/fiber"))
-        os.cp("build/lib/*", package:installdir("lib"))
     end)
 
     on_test(function (package)
