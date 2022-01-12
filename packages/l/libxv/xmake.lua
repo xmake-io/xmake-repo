b42edf46b924c3d9a4a46380624fb7160cda97ac
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-01-12 20:17:19 +0100

    Add linux pacman add_extsources for libxv

--- packages/l/libxv/xmake.lua
+++ packages/l/libxv/xmake.lua
@@ -7,7 +7,7 @@ package("libxv")
     add_versions("1.0.11", "d26c13eac99ac4504c532e8e76a1c8e4bd526471eb8a0a4ff2a88db60cb0b088")
 
     if is_plat("linux") then
-        add_extsources("apt::libxv-dev")
+        add_extsources("apt::libxv-dev", "pacman::libxv")
     end
 
     if is_plat("macosx", "linux") then
