26a26a5af372ee411cee44a7adf79eb1a7d15cec
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-01-12 20:16:53 +0100

    Add linux pacman add_extsources for libsm

--- packages/l/libsm/xmake.lua
+++ packages/l/libsm/xmake.lua
@@ -7,7 +7,7 @@ package("libsm")
     add_versions("1.2.3", "1e92408417cb6c6c477a8a6104291001a40b3bb56a4a60608fdd9cd2c5a0f320")
 
     if is_plat("linux") then
-        add_extsources("apt::libsm-dev")
+        add_extsources("apt::libsm-dev", "pacman::libsm")
     end
 
     add_deps("libice")
