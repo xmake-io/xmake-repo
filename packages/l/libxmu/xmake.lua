39d06cacdb0a1e33bac6a9b69b16c211e45e670a
Arthapz <arthur.laurent4@gmail.com>
2022-01-13 02:53:39 +0100

    Add linux pacman add_extsources for libxmu (#883)

--- packages/l/libxmu/xmake.lua
+++ packages/l/libxmu/xmake.lua
@@ -7,7 +7,7 @@ package("libxmu")
     add_versions("1.1.3", "5bd9d4ed1ceaac9ea023d86bf1c1632cd3b172dce4a193a72a94e1d9df87a62e")
 
     if is_plat("linux") then
-        add_extsources("apt::libxmu-dev")
+        add_extsources("apt::libxmu-dev", "pacman::libxmu")
     end
 
     add_deps("libxt", "libxext")
