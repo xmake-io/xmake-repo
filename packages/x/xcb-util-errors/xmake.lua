7325fefe889cc1ff941b9e984758714eead25f34
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-01-14 18:33:39 +0100

    Add xcb-util-errors addon

---
+++ packages/x/xcb-util-errors/xmake.lua
@@ -0,0 +1,29 @@
+
+package("xcb-util-errors")
+    set_homepage("https://xcb.freedesktop.org")
+    set_description("X.Org: XML-XCB protocol descriptions for libxcb code generation")
+
+    set_urls("https://xcb.freedesktop.org/dist/xcb-util-errors-$(version).tar.gz")
+    add_versions("1.0", "7752a722e580efdbada30632cb23aed35c18757399ac3b547b59fd7257cf5e33")
+
+    if is_plat("linux") then
+        add_extsources("apt::APT_PACKAGE", "pacman::xcb-util-errors")
+    end
+
+    if is_plat("macosx", "linux") then
+        add_deps("pkg-config", {kind = "binary"})
+        add_deps("xcb-proto", "libxcb")
+    end
+
+    on_install("macosx", "linux", function (package)
+        local configs = {"--sysconfdir=" .. package:installdir("etc"), 
+                         "--localstatedir=" .. package:installdir("var"), 
+                         "--disable-silent-rules"}
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_test(function (package)
+        local envs = {PKG_CONFIG_PATH = path.join(package:installdir(), "lib", "pkgconfig")}
+        os.vrunv("pkg-config", {"--exists", "xcb-util"}, {envs = envs})
+    end)
+
