84d27a2ef67e1cf20322094da36f67e6755c705b
Arthapz <arthur.laurent4@gmail.com>
2022-01-15 04:08:54 +0100

    Add xcb addons (#910)
    * Add xcb-util-cursor addon
    
    * Add xcb-util-image addon
    
    * Add xcb-util-errors addon
    
    * Add xcb-util-keysyms addon
    
    * Add xcb-util addon
    
    * Add xcb-util-renderutil addon
    
    * Add xcb-util-wm addon
    
    * Add missing apt packages
    
    * Fix dependencies
    
    * Fix xcb-util-renderutil missing dependency
    
    * Fix installation checking
    
    * Fix m4 dependency
    
    * Cleanup
    
    * More cleanup

---
+++ packages/x/xcb-util-image/xmake.lua
@@ -0,0 +1,30 @@
+
+package("xcb-util-image")
+    set_homepage("https://xcb.freedesktop.org")
+    set_description("X.Org: XML-XCB protocol descriptions for libxcb code generation")
+
+    set_urls("https://xcb.freedesktop.org/dist/xcb-util-image-$(version).tar.gz")
+    add_versions("0.3.8", "3d7f6e3e7e73ca0d42154d360ad61a1e16fc62f6bb000f4e69c0d00305d1e00b")
+    add_versions("0.3.9", "ac7fa09eddc9ecda6fd872d32b6dc23d451a1c6c201873dfe7cac8362c87acf3")
+    add_versions("0.4.0", "cb2c86190cf6216260b7357a57d9100811bb6f78c24576a3a5bfef6ad3740a42")
+
+    if is_plat("linux") then
+        add_extsources("apt::libxcb-image0-dev", "pacman::xcb-util-image")
+    end
+
+    if is_plat("macosx", "linux") then
+        add_deps("m4", "pkg-config")
+        add_deps("xcb-proto", "libxcb", "xcb-util")
+    end
+
+    on_install("macosx", "linux", function (package)
+        local configs = {"--sysconfdir=" .. package:installdir("etc"), 
+                         "--localstatedir=" .. package:installdir("var"), 
+                         "--disable-silent-rules"}
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("xcb_image_create", {includes = "xcb/xcb_image.h"}))
+    end)
+
