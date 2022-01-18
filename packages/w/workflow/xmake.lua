c46717bd7f3768c8db4bdac791500c0d3042aa15
Bradford Zhang <skyteamwater@qq.com>
2022-01-18 19:06:46 +0800

    add workflow

---
+++ packages/w/workflow/xmake.lua
@@ -0,0 +1,21 @@
+package("workflow")
+    set_homepage("https://github.com/sogou/workflow")
+    set_description("C++ Parallel Computing and Asynchronous Networking Engine")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/sogou/workflow/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/sogou/workflow.git")
+    add_versions("v0.9.9", "309775e74e9f22bead08147380be4a69072e8f603d7216992f5b73510643cbe1")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
+    end)
