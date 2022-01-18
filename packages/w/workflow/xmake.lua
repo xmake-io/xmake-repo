bf428f4310d28e1dfe1a27caa5b573957e086f52
Bradford Zhang <skyteamwater@qq.com>
2022-01-18 20:47:35 +0800

    fix on_load

--- packages/w/workflow/xmake.lua
+++ packages/w/workflow/xmake.lua
@@ -33,7 +33,6 @@ package("workflow")
                 task->get_resp()->append_output_body("<html>Hello World!</html>");
             });
             if (server.start(8888) == 0) {  // start server on port 8888
-                getchar(); // press "Enter" to end.
                 server.stop();
             }
         }
