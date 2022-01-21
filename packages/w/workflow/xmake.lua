328773a2f0d01474b191da7aa8d23ef4f46eb1e3
Bradford Zhang <skyteamwater@qq.com>
2022-01-21 10:12:05 +0800

    update workflow

--- packages/w/workflow/xmake.lua
+++ packages/w/workflow/xmake.lua
@@ -4,7 +4,7 @@ package("workflow")
     set_license("Apache-2.0")
 
     add_urls("https://github.com/sogou/workflow.git")
-    add_versions("v0.9.9", "d7d29d68f1e94020f2e31a08e76fa644fd604bd7")
+    add_versions("v0.9.9", "0ec98afdde75511717704f4217c69a32eaa65410")
 
     add_configs("kafka", {description = "Use kafka protocol", default = false})
 
