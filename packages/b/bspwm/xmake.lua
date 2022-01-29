ea86d425e03b61496c348bcbf85e7fcc9929dd59
ruki <waruqi@gmail.com>
2022-01-29 16:50:47 +0800

    improve xcb

--- packages/b/bspwm/xmake.lua
+++ packages/b/bspwm/xmake.lua
@@ -5,18 +5,19 @@ package("bspwm")
     add_urls("https://github.com/baskerville/bspwm.git")
     add_versions("2021.06.23", "e22d0fad23e0e85b401be69f2360a1c3a0767921")
 
-    add_deps("libxcb", "xcb-util-wm")
+    add_deps("libxcb", "xcb-util", "xcb-util-wm", "xcb-util-keysyms")
 
     on_install("linux", "macosx", function (package)
         local configs = {}
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
-            add_requires("libxcb", "xcb-util-wm")
+            add_requires("libxcb", "xcb-util", "xcb-util-wm", "xcb-util-keysyms")
             target("bspwm")
                set_kind("$(kind)")
                add_files("src/*.c")
                add_headerfiles("src/*.h")
-               add_packages("libxcb", "xcb-util-wm")
+               add_defines("JSMN_STRICT", "_POSIX_C_SOURCE=200809L")
+               add_packages("libxcb", "xcb-util", "xcb-util-wm", "xcb-util-keysyms")
         ]])
         if package:config("shared") then
             configs.kind = "shared"
