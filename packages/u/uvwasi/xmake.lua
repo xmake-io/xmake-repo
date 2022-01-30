2c1faa4c7b4097b987f33a28e31470118212c773
ruki <waruqi@gmail.com>
2022-01-30 23:20:40 +0800

    update platforms

--- packages/u/uvwasi/xmake.lua
+++ packages/u/uvwasi/xmake.lua
@@ -9,7 +9,7 @@ package("uvwasi")
 
     add_deps("cmake", "libuv")
 
-    on_install(function (package)
+    on_install("linux", "windows", "macosx", function (package)
         local configs = {"-DWITH_SYSTEM_LIBUV=ON", "-DUVWASI_BUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
