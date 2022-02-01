121100de2b1bc7f06240bc81ba2d199ffd5ea33b
ruki <waruqi@gmail.com>
2022-02-02 00:55:02 +0800

    add depot_tools

---
+++ packages/d/depot_tools/xmake.lua
@@ -0,0 +1,23 @@
+package("depot_tools")
+    set_kind("binary")
+    set_homepage("https://chromium.googlesource.com/chromium/tools/depot_tools")
+    set_description("Tools for working with Chromium development")
+
+    add_urls("https://github.com/xmake-mirror/depot_tools.git",
+             "https://chromium.googlesource.com/chromium/tools/depot_tools.git")
+    add_versions("2022.2.1", "8a6d00f116d6de9d5c4e92acb519fd0859c6449a")
+
+    add_deps("python")
+
+    on_load(function (package)
+        package:addenv("PATH", ".")
+        package:addenv("DEPOT_TOOLS_UPDATE", "0")
+    end)
+
+    on_install(function (package)
+        os.cp("*", package:installdir())
+    end)
+
+    on_test(function (package)
+        os.vrun("gclient --version")
+    end)
