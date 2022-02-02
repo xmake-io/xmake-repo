c3f682ba87307be916cf706a698d1d6967d77156
ruki <waruqi@gmail.com>
2022-02-02 21:05:50 +0800

    improve py

--- packages/d/depot_tools/xmake.lua
+++ packages/d/depot_tools/xmake.lua
@@ -7,7 +7,7 @@ package("depot_tools")
              "https://chromium.googlesource.com/chromium/tools/depot_tools.git")
     add_versions("2022.2.1", "8a6d00f116d6de9d5c4e92acb519fd0859c6449a")
 
-    add_deps("python")
+    add_deps("python 3.x")
 
     on_load(function (package)
         package:addenv("PATH", ".")
@@ -17,6 +17,7 @@ package("depot_tools")
 
     on_install("linux", "macosx", "windows", function (package)
         os.cp("*", package:installdir())
+        io.writefile("python-bin/python3", 'python3 "$@"')
     end)
 
     on_test(function (package)
