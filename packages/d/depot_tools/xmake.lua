88ed452e98f08a7aab1e2f0869eb8a7af51eb3ad
ruki <waruqi@gmail.com>
2022-02-02 12:36:40 +0800

    add depot_tools (#968)
    * Update xmake.lua
    
    * add depot_tools
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * improve py
    
    * Update xmake.lua
    
    * improve tools
    
    * add py
    
    * Update xmake.lua

---
+++ packages/d/depot_tools/xmake.lua
@@ -0,0 +1,28 @@
+package("depot_tools")
+    set_kind("binary")
+    set_homepage("https://chromium.googlesource.com/chromium/tools/depot_tools")
+    set_description("Tools for working with Chromium development")
+
+    add_urls("https://github.com/xmake-mirror/depot_tools.git",
+             "https://chromium.googlesource.com/chromium/tools/depot_tools.git")
+    add_versions("2022.2.1", "8a6d00f116d6de9d5c4e92acb519fd0859c6449a")
+
+    on_load(function (package)
+        package:addenv("PATH", ".")
+        package:addenv("DEPOT_TOOLS_UPDATE", "0")
+        package:addenv("DEPOT_TOOLS_METRICS", "0")
+    end)
+
+    on_install("linux", "macosx", "windows", function (package)
+        os.cp("*", package:installdir())
+        os.cd(package:installdir())
+        if package:is_plat("windows") then
+            os.vrunv("gclient.bat")
+        else
+            os.vrunv("sh", {"./gclient"})
+        end
+  end)
+
+    on_test(function (package)
+        os.vrunv(package:is_plat("windows") and "gclient.bat" or "gclient", {"--version"})
+    end)
