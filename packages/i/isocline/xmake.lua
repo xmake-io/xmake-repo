ab8668c73e9708bdee3a73b98c9203965ca06861
ruki <waruqi@gmail.com>
2022-02-07 11:16:08 +0800

    add isocline (#983)

---
+++ packages/i/isocline/xmake.lua
@@ -0,0 +1,29 @@
+package("isocline")
+    set_homepage("https://github.com/daanx/isocline")
+    set_description("Isocline is a portable GNU readline alternative ")
+    set_license("MIT")
+
+    add_urls("https://github.com/daanx/isocline.git")
+    add_versions("2022.01.16", "762717b5acc7d8baf64faeb5320ae4b85cf98aac")
+
+    add_deps("cmake")
+
+    on_install("linux", "macosx", "bsd", "mingw", "windows", function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("windows") and package:config("shared") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+        end
+        import("package.tools.cmake").build(package, configs, {buildir = "build"})
+        os.cp("include", package:installdir())
+        os.trycp("build/*.a", package:installdir("lib"))
+        os.trycp("build/*.dylib", package:installdir("lib"))
+        os.trycp("build/*.so", package:installdir("lib"))
+        os.trycp("build/*.lib", package:installdir("lib"))
+        os.trycp("build/*.dll", package:installdir("bin"))
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("ic_set_history", {includes = "isocline.h"}))
+    end)
