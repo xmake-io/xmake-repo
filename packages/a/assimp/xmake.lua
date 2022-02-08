f6732a561b74ffb79967d2c9f9acbb4e4dfd0622
xq114 <1140735506@qq.com>
2022-02-08 21:55:54 +0800

    fix indent

--- packages/a/assimp/xmake.lua
+++ packages/a/assimp/xmake.lua
@@ -62,7 +62,7 @@ package("assimp")
             local fetchinfo = zlib:fetch()
             io.replace("CMakeLists.txt", "FIND_PACKAGE(ZLIB)", "", {plain = true})
             io.replace("CMakeLists.txt", [[INSTALL( TARGETS zlib zlibstatic
-    EXPORT "${TARGETS_EXPORT_NAME}")]], "", {plain = true})
+        EXPORT "${TARGETS_EXPORT_NAME}")]], "", {plain = true})
             table.insert(configs, "-DZLIB_FOUND=TRUE")
             table.insert(configs, "-DZLIB_INCLUDE_DIR=" .. table.concat(fetchinfo.includedirs or fetchinfo.sysincludedirs, ";"))
             table.insert(configs, "-DZLIB_LIBRARIES=" .. table.concat(fetchinfo.libfiles, ";"))
