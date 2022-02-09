3a5b678eddabf509680d6c3eb15721722d3e9a9e
ruki <waruqi@gmail.com>
2022-02-09 09:57:57 +0800

    Update xmake.lua

--- packages/l/libdivide/xmake.lua
+++ packages/l/libdivide/xmake.lua
@@ -7,11 +7,9 @@ package("libdivide")
              "https://github.com/ridiculousfish/libdivide.git")
     add_versions("5.0", "01ffdf90bc475e42170741d381eb9cfb631d9d7ddac7337368bcd80df8c98356")
 
-    add_deps("cmake")
-
     on_install(function (package)
-        local configs = {"-DBUILD_TESTS=OFF"}
-        import("package.tools.cmake").install(package, configs)
+        os.cp("*.h", package:installdir("include"))
+        os.cp("*.hpp", package:installdir("include"))
     end)
 
     on_test(function (package)
