71d21879a48c55e648949a0421d0be03dc5f9d2b
Charles Seizilles <17192695+WubiCookie@users.noreply.github.com>
2022-02-09 20:39:00 +0100

    Add benchmark versions

--- packages/b/benchmark/xmake.lua
+++ packages/b/benchmark/xmake.lua
@@ -6,7 +6,12 @@ package("benchmark")
     add_urls("https://github.com/google/benchmark/archive/v$(version).tar.gz",
              "https://github.com/google/benchmark.git")
     add_versions("1.5.2", "dccbdab796baa1043f04982147e67bb6e118fe610da2c65f88912d73987e700c")
+    add_versions("1.5.3", "e4fbb85eec69e6668ad397ec71a3a3ab165903abe98a8327db920b94508f720e")
+    add_versions("1.5.4", "e3adf8c98bb38a198822725c0fc6c0ae4711f16fbbf6aeb311d5ad11e5a081b5")
     add_versions("1.5.5", "3bff5f237c317ddfd8d5a9b96b3eede7c0802e799db520d38ce756a2a46a18a0")
+    add_versions("1.5.6", "789f85b4810d13ff803834ea75999e41b326405d83d6a538baf01499eda96102")
+    add_versions("1.6.0", "1f71c72ce08d2c1310011ea6436b31e39ccab8c2db94186d26657d41747c85d6")
+    add_versions("1.6.1", "6132883bc8c9b0df5375b16ab520fac1a85dc9e4cf5be59480448ece74b278d4")
     
     if is_plat("mingw") and is_subhost("msys") then
         add_extsources("pacman::benchmark")
