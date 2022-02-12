6fab421cd7b9fb0e7fd5168d8bde47987c000b33
Chen Yufei <cyfdecyf@gmail.com>
2022-02-12 21:23:18 +0800

    civetweb: use CMAKE_HOST_WIN32 in FindWinSock.

--- packages/c/civetweb/xmake.lua
+++ packages/c/civetweb/xmake.lua
@@ -7,7 +7,7 @@ package("civetweb")
     add_urls("https://github.com/civetweb/civetweb/archive/refs/tags/$(version).tar.gz",
              "https://github.com/civetweb/civetweb.git")
     add_versions('v1.15', '90a533422944ab327a4fbb9969f0845d0dba05354f9cacce3a5005fa59f593b9')
-    add_patches("v1.15", path.join(os.scriptdir(), "patches", "v1.15", "find-winsock.patch"), "19f0008f13d7a7eceb026c51452c8cac2b3f1841f7ac7c558552e0522cdb1edc")
+    add_patches("v1.15", path.join(os.scriptdir(), "patches", "v1.15", "find-winsock.patch"), "4d8355169fb20ede0fdae2a8bc8561238a088cf810e36107ba267f273989d9ee")
 
     add_configs("openssl", {description = "with openssl library", default = false, type = "boolean"})
     add_configs("zlib",    {description = "Enable zlib support.", default = false, type = "boolean"})
@@ -48,7 +48,7 @@ package("civetweb")
         table.insert(configs, "-DCIVETWEB_ENABLE_SSL=" .. (package:config("openssl") and "ON" or "OFF"))
         table.insert(configs, "-DCIVETWEB_ENABLE_ZLIB=" .. (package:config("zlib") and "ON" or "OFF"))
 
-        import("package.tools.cmake").install(package, configs, {shflags = shflags})
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
