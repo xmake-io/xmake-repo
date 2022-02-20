b211946848131ecb4f441009e70555b04b2d5be2
Hoildkv <42310255+xq114@users.noreply.github.com>
2022-02-20 20:36:43 +0800

    update expat, ezc3d and embree (#1007)
    * update expat
    
    * update ezc3d
    
    * update embree

--- packages/e/ezc3d/xmake.lua
+++ packages/e/ezc3d/xmake.lua
@@ -6,6 +6,7 @@ package("ezc3d")
 
     add_urls("https://github.com/pyomeca/ezc3d/archive/refs/tags/Release_$(version).tar.gz")
     add_versions("1.4.5", "01602b2ccfc0edd88089e89d249e10086022f7ed7ef40caa3eb3b048ccfa40aa")
+    add_versions("1.4.7", "b11921ecd9ff5716f19b4a4eeede36f8cfa5ff08e6fd2c9c12e55f83e9d782bd")
 
     add_deps("cmake")
     add_includedirs("include/ezc3d")
