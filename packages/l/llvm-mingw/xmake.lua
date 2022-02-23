2f7f1e445a6ef9b30631819e00ef6f662aec4786
xq114 <1140735506@qq.com>
2022-02-23 16:06:18 +0800

    use os.arch instead of is_arch

--- packages/l/llvm-mingw/xmake.lua
+++ packages/l/llvm-mingw/xmake.lua
@@ -5,15 +5,15 @@ package("llvm-mingw")
     set_description("An LLVM/Clang/LLD based mingw-w64 toolchain")
 
     if is_host("windows") then
-        if is_arch("x86") then
+        if os.arch() == "x86" then
             set_urls("https://github.com/mstorsjo/llvm-mingw/releases/download/$(version)/llvm-mingw-$(version)-ucrt-i686.zip")
             add_versions("20201020", "4f07721a81a3ba0980fc089e839d1f1a5784bbc8cee1332c15cf1b6ba24525d6")
             add_versions("20211002", "e4faaea995c980f4b0808cc4ec17d5ea9fc2c83449f0cb3a8af07e52abe26679")
-        elseif is_arch("arm64") then
+        elseif os.arch() == "arm64" then
             set_urls("https://github.com/mstorsjo/llvm-mingw/releases/download/$(version)/llvm-mingw-$(version)-ucrt-aarch64.zip")
             add_versions("20201020", "57d6e0fff94774ccd958a3d0174686189d3ec1cb5981eb4ea37fc82a007cc674")
             add_versions("20211002", "1f618c4323a7e64df8a97d4fe8a933e6c8bdc131c91f90b89888927ebd179f83  ")
-        elseif is_arch("arm.*") then
+        elseif os.arch():find("arm.*") then
             set_urls("https://github.com/mstorsjo/llvm-mingw/releases/download/$(version)/llvm-mingw-$(version)-ucrt-armv7.zip")
             add_versions("20201020", "c086562124fc79e157d2cb01eacd7bd3e937d488bacdac138ee45ed6a39d3b6c")
             add_versions("20211002", "a37c4cbd4b7c53f7c931d4ca84e1f9847315b528129310fefeafae48edd65407")
@@ -24,16 +24,16 @@ package("llvm-mingw")
         end
     elseif is_host("linux") then
         -- Built on Ubuntu but hopefully run on other distributions
-        if is_arch("x86_64") then
+        if os.arch() == "x86_64" then
             set_urls("https://github.com/mstorsjo/llvm-mingw/releases/download/$(version)/llvm-mingw-$(version)-ucrt-ubuntu-18.04-x86_64.tar.xz")
             add_versions("20211002", "30e9400783652091d9278ce21e5c170d01a5f44e4f1a25717b63cd9ad9fbe13b")
-        elseif is_arch("arm64") then
+        elseif os.arch() == "arm64" then
             set_urls("https://github.com/mstorsjo/llvm-mingw/releases/download/$(version)/llvm-mingw-$(version)-ucrt-ubuntu-18.04-aarch64.tar.xz")
             add_versions("20211002", "9a26079af16713894e8a11c77e38896c4040b98daceca4408333bd1053c1a3d5")
         end
     end
 
-    on_install("@windows", "@linux", function (package)
+    on_install("@windows", "@linux|x86_64,arm64", function (package)
         os.cp("*", package:installdir())
     end)
 
