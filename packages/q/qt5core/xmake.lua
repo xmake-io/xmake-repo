d27f16bcf2d76e3869d1532b05164db641d44e32
ruki <waruqi@gmail.com>
2022-03-04 09:55:20 +0800

    Update xmake.lua

--- packages/q/qt5core/xmake.lua
+++ packages/q/qt5core/xmake.lua
@@ -23,32 +23,42 @@ package("qt5core")
             return
         end
 
-        local libname = "Qt5Core"
         local syslinks
+        local linkname
+        local frameworks
+        local includedirs = {qt.includedir}
         if package:is_plat("windows") then
+            linkname = "Qt5Core"
             if package:is_debug() then
-                libname = libname .. "d"
+                linkname = linkname .. "d"
             end
+            table.insert(includedirs, path.join(qt.includedir, "QtCore"))
         elseif package:is_plat("android") then
+            linkname = "Qt5Core"
             if package:is_arch("x86_64", "x64") then
-                libname = libname .. "_x86_64"
+                linkname = linkname .. "_x86_64"
             elseif package:is_arch("arm64", "arm64-v8a") then
-                libname = libname .. "_arm64-v8a"
+                linkname = linkname .. "_arm64-v8a"
             elseif package:is_arch("armv7", "armeabi-v7a", "armeabi", "armv7-a", "armv5te") then
-                libname = libname .. "_armeabi-v7a"
+                linkname = linkname .. "_armeabi-v7a"
             elseif package:is_arch("x86") then
-                libname = libname .. "_x86"
+                linkname = linkname .. "_x86"
             end
-
             syslinks = "z"
+            table.insert(includedirs, path.join(qt.includedir, "QtCore"))
+        elseif package:is_plat("macosx", "iphoneos") then
+            table.insert(includedirs, path.join(qt.libdir, "QtCore.framework/Versions/5/Headers"))
+            frameworks = "QtCore"
         end
 
         return {
             qtdir = qt,
             version = qt.version,
-            includedirs = {qt.includedir, path.join(qt.includedir, "QtCore")},
-            links = libname,
+            includedirs = includedirs,
+            links = linkname,
             linkdirs = qt.libdir,
+            frameworks = frameworks,
+            frameworkdirs = qt.libdir,
             syslinks = syslinks
         }
     end)
