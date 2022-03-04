202d4fbc6cfc483ec909f4176786db695a9e5372
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-03-04 12:11:47 +0100

    Use env = installdir

--- packages/s/scons/xmake.lua
+++ packages/s/scons/xmake.lua
@@ -27,7 +27,7 @@ package("scons")
         io.writefile("build/doc/man/scons-time.1", "")
         io.writefile("build/doc/man/sconsign.1", "")
 
-        os.vrunv("python", {"setup.py", "install", "--prefix", package:installdir()}, {envs = {PYTHONPATH = package:getenv("PYTHONPATH")}})
+        os.vrunv("python", {"setup.py", "install", "--prefix", package:installdir()}, {envs = {PYTHONPATH = package:installdir()}})
         if package:is_plat("windows") then
             os.mv(package:installdir("Scripts", "*"), package:installdir("bin"))
             os.rmdir(package:installdir("Scripts"))
