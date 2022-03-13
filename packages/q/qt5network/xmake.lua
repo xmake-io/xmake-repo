c5ca084afd36ae8502aade215163463d43855790
Jérôme Leclercq <lynix680@gmail.com>
2022-03-13 11:15:22 +0100

    Add qt5network

---
+++ packages/q/qt5network/xmake.lua
@@ -0,0 +1,29 @@
+package("qt5network")
+    set_base("qt5lib")
+    set_kind("library")
+
+    on_load(function (package)
+        package:add("deps", "qt5core", {debug = package:is_debug(), version = package:version_str()})
+        package:data_set("libname", "Network")
+
+        package:base():script("load")(package)
+        package:set("kind", "library")
+    end)
+
+    on_test(function (package)
+        local cxflags
+        if not package:is_plat("windows") then
+            cxflags = "-fPIC"
+        end
+        assert(package:check_cxxsnippets({test = [[
+            int test(int argc, char** argv) {
+                QCoreApplication app(argc, argv);
+          
+                QByteArray datagram = "Hello from xmake!";
+                QUdpSocket udpSocket;
+                udpSocket.writeDatagram(datagram, QHostAddress::Broadcast, 45454);
+
+                return app.exec();
+            }
+        ]]}, {configs = {languages = "c++14", cxflags = cxflags}, includes = {"QCoreApplication", "QByteArray", "QUdpSocket"}}))
+    end)
