e06194db15785544514ae5183b5c7389dc867382
ruki <waruqi@gmail.com>
2022-03-16 21:20:25 +0800

    Update xmake.lua

--- packages/q/qt5network/xmake.lua
+++ packages/q/qt5network/xmake.lua
@@ -7,11 +7,17 @@ package("qt5network")
         package:data_set("libname", "Network")
 
         if package:is_plat("linux") then
-            package:add("deps", "openssl 1.1.1b")
+            -- we need system openssl with evp-kdf
+            -- @see https://github.com/xmake-io/xmake-repo/pull/1057#issuecomment-1069006866
+            if linuxos.name() == "fedora" then
+                package:add("deps", "openssl", {system = true})
+            else
+                package:add("deps", "openssl")
+            end
         elseif package:is_plat("iphoneos") then
             package:data_set("frameworks", {"GSS", "IOKit", "Security", "SystemConfiguration"})
         end
-    
+
         package:base():script("load")(package)
         package:set("kind", "library")
     end)
@@ -24,7 +30,7 @@ package("qt5network")
         assert(package:check_cxxsnippets({test = [[
             int test(int argc, char** argv) {
                 QCoreApplication app(argc, argv);
-          
+
                 QByteArray datagram = "Hello from xmake!";
                 QUdpSocket udpSocket;
                 udpSocket.writeDatagram(datagram, QHostAddress::Broadcast, 45454);
