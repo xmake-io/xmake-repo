18bbd36ec460ca84b56d9f5bc470cdcf9f23471a
Hoildkv <42310255+xq114@users.noreply.github.com>
2022-03-23 18:39:02 +0800

    add onedpl (#1092)
    * add onedpl
    
    * improve openblas
    
    * improve magnum
    
    * improve zstd
    
    * add asm back

--- packages/z/zstd/xmake.lua
+++ packages/z/zstd/xmake.lua
@@ -2,12 +2,19 @@ package("zstd")
 
     set_homepage("https://www.zstd.net/")
     set_description("Zstandard - Fast real-time compression algorithm")
+    set_license("BSD-3-Clause")
 
     set_urls("https://github.com/facebook/zstd/archive/$(version).tar.gz",
              "https://github.com/facebook/zstd.git")
     add_versions("v1.4.5", "734d1f565c42f691f8420c8d06783ad818060fc390dee43ae0a89f86d0a4f8c2")
     add_versions("v1.5.0", "0d9ade222c64e912d6957b11c923e214e2e010a18f39bec102f572e693ba2867")
     add_versions("v1.5.2", "f7de13462f7a82c29ab865820149e778cbfe01087b3a55b5332707abf9db4a6e")
+
+    on_load("windows", function (package)
+        if package:config("shared") then
+            package:add("defines", "ZSTD_DLL_IMPORT=1")
+        end
+    end)
 
     on_install(function (package)
         io.writefile("xmake.lua", ([[
@@ -19,10 +26,11 @@ package("zstd")
                 add_files("lib/common/*.c")
                 add_files("lib/compress/*.c")
                 add_files("lib/decompress/*.c")
+                add_files("lib/dictBuilder/*.c")
                 add_headerfiles("lib/*.h")
                 add_defines("XXH_NAMESPACE=ZSTD_")
                 if is_kind("shared") and is_plat("windows") then
-                    add_defines("ZSTD_DLL_EXPORT")
+                    add_defines("ZSTD_DLL_EXPORT=1")
                 end
                 on_config(function (target)
                     if target:is_arch("x64", "x86_64") and target:has_tool("cc", "clang", "gcc") then
@@ -32,7 +40,6 @@ package("zstd")
                     end
                 end)
         ]]):format(package:version_str()))
-
         import("package.tools.xmake").install(package)
     end)
 
