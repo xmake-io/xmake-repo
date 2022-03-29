bc1dd54cd78d02568b6a83d8981f6c3053886213
xq114 <1140735506@qq.com>
2022-03-29 20:33:22 +0800

    update util-linux

--- packages/u/util-linux/xmake.lua
+++ packages/u/util-linux/xmake.lua
@@ -8,36 +8,34 @@ package("util-linux")
     end})
     add_versions("2.32.1", "86e6707a379c7ff5489c218cfaf1e3464b0b95acf7817db0bc5f179e356a67b2")
     add_versions("2.36.2", "f7516ba9d8689343594356f0e5e1a5f0da34adfbc89023437735872bb5024c5f")
+    add_versions("2.38", "6d111cbe4d55b336db2f1fbeffbc65b89908704c01136371d32aa9bec373eb64")
 
     add_patches("2.36.2", path.join(os.scriptdir(), "patches", "2.36.2", "includes.patch"), "7274762cac2810b5f0d17ecb5ac69c7069e7ff2b880df663b7072628df0867f3")
 
     if is_plat("macosx") then
         add_extsources("brew::util-linux")
-    else
+    elseif is_plat("linux") then
+        add_extsources("apt::util-linux", "pacman::util-linux")
         add_deps("ncurses", "zlib")
     end
 
-    if is_plat("linux") then
-        add_extsources("apt::util-linux", "pacman::util-linux")
-    end
-
-    add_configs("ipcs",               { description = "Enable ipcs.", default = false, type = "boolean"})
-    add_configs("ipcrm",              { description = "Enable ipcrm.", default = false, type = "boolean"})
-    add_configs("wall",               { description = "Enable wall.", default = false, type = "boolean"})
-    add_configs("libuuid",            { description = "Enable libuuid.", default = false, type = "boolean"})
-    add_configs("libmount",           { description = "Enable libmount.", default = false, type = "boolean"})
-    add_configs("libsmartcols",       { description = "Enable libsmartcols.", default = false, type = "boolean"})
-    add_configs("use-tty-group",      { description = "Enable use-tty-group.", default = false, type = "boolean"})
-    add_configs("kill",               { description = "Enable kill.", default = false, type = "boolean"})
-    add_configs("cal",                { description = "Enable cal.", default = false, type = "boolean"})
-    add_configs("systemd",            { description = "Enable systemd.", default = false, type = "boolean"})
-    add_configs("chfn-chsh",          { description = "Enable chfn-chsh.", default = false, type = "boolean"})
-    add_configs("login",              { description = "Enable login.", default = false, type = "boolean"})
-    add_configs("su",                 { description = "Enable su.", default = false, type = "boolean"})
-    add_configs("mount",              { description = "Enable mount.", default = false, type = "boolean"})
-    add_configs("runuser",            { description = "Enable runuser.", default = false, type = "boolean"})
-    add_configs("makeinstall-chown",  { description = "Enable makeinstall-chown.", default = false, type = "boolean"})
-    add_configs("makeinstall-setuid", { description = "Enable makeinstall-setuid.", default = false, type = "boolean"})
+    add_configs("ipcs",               {description = "Enable ipcs.", default = false, type = "boolean"})
+    add_configs("ipcrm",              {description = "Enable ipcrm.", default = false, type = "boolean"})
+    add_configs("wall",               {description = "Enable wall.", default = false, type = "boolean"})
+    add_configs("libuuid",            {description = "Enable libuuid.", default = false, type = "boolean"})
+    add_configs("libmount",           {description = "Enable libmount.", default = false, type = "boolean"})
+    add_configs("libsmartcols",       {description = "Enable libsmartcols.", default = false, type = "boolean"})
+    add_configs("use-tty-group",      {description = "Enable use-tty-group.", default = false, type = "boolean"})
+    add_configs("kill",               {description = "Enable kill.", default = false, type = "boolean"})
+    add_configs("cal",                {description = "Enable cal.", default = false, type = "boolean"})
+    add_configs("systemd",            {description = "Enable systemd.", default = false, type = "boolean"})
+    add_configs("chfn-chsh",          {description = "Enable chfn-chsh.", default = false, type = "boolean"})
+    add_configs("login",              {description = "Enable login.", default = false, type = "boolean"})
+    add_configs("su",                 {description = "Enable su.", default = false, type = "boolean"})
+    add_configs("mount",              {description = "Enable mount.", default = false, type = "boolean"})
+    add_configs("runuser",            {description = "Enable runuser.", default = false, type = "boolean"})
+    add_configs("makeinstall-chown",  {description = "Enable makeinstall-chown.", default = false, type = "boolean"})
+    add_configs("makeinstall-setuid", {description = "Enable makeinstall-setuid.", default = false, type = "boolean"})
 
     on_load(function (package)
         package:addenv("PATH", "bin")
@@ -68,4 +66,3 @@ package("util-linux")
         end
         import("package.tools.autoconf").install(package, configs)
     end)
-
