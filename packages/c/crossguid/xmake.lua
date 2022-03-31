5a230dd46a69ab05b07f5110f2868935a0e8f722
L <zover.v@gmail.com>
2022-03-31 16:04:49 +0800

    trying to fix macOS build failures

--- packages/c/crossguid/xmake.lua
+++ packages/c/crossguid/xmake.lua
@@ -6,8 +6,14 @@ package("crossguid")
     add_urls("https://github.com/graeme-hill/crossguid.git")
     add_versions("2019.3.29", "ca1bf4b810e2d188d04cb6286f957008ee1b7681")
 
+    -- Apply patch
     if is_plat("macosx", "iphoneos") then
         add_patches("2019.3.29", path.join(os.scriptdir(), "patches", "warnings.patch"), "9953cb5ef68bdffa7cc9cb138a2119dd3460f3055db14a13aecc4dd256104c09")
+    end
+
+    -- Add necessary lib
+    if is_plat("macosx") then
+        add_frameworks("CoreFoundation")
     elseif is_plat("linux") then
         add_deps("libuuid")
     elseif is_plat("windows", "mingw") then
@@ -16,7 +22,7 @@ package("crossguid")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "iphoneos", "android", "mingw", function (package)
         local configs = {"-DCROSSGUID_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs, {buildir = "build"})
