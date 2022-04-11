e16afaa7fe6ce4dee99272ef28abbe9aad9278d0
Romain Chardiny <romain.chardiny@gmail.com>
2022-04-11 18:04:33 +0200

    simplify xmake.lua

--- packages/l/lexy/xmake.lua
+++ packages/l/lexy/xmake.lua
@@ -1,20 +1,13 @@
 package("lexy")
+    set_kind("library", {headeronly = trye})
     set_homepage("https://lexy.foonathan.net")
     set_description("C++ parsing DSL")
 
     add_urls("https://github.com/foonathan/lexy.git")
     add_versions("2022.03.21", "10342c6b1a03cbc6254c64064b419799a7993e0e")
 
-    add_deps("cmake")
-
     on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DLEXY_BUILD_EXAMPLES=off")
-        table.insert(configs, "-DLEXY_BUILD_TESTS=off")
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
-        os.cp("include/*", package:installdir("include"))
+        os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
