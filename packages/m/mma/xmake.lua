0b362464c03e8f67de17f9199d8f4581f04bcf10
xq114 <1140735506@qq.com>
2022-04-26 16:13:05 +0800

    port to xmake

--- packages/m/mma/xmake.lua
+++ packages/m/mma/xmake.lua
@@ -11,12 +11,20 @@ package("mma")
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
 
-    add_deps("cmake")
-    on_install("windows", "macosx", "linux", "mingw", function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
+    on_install(function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            set_languages("cxx11")
+            target("mma")
+                set_kind("$(kind)")
+                add_files("src/mma/MMASolver.cpp")
+                add_headerfiles("src/(mma/MMASolver.h)")
+            target("gcmma")
+                set_kind("$(kind)")
+                add_files("src/gcmma/GCMMASolver.cpp")
+                add_headerfiles("src/(gcmma/GCMMASolver.h)")
+        ]])
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
