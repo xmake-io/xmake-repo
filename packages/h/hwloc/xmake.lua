96448a6808271daa573f47d94f1c269de99fbc25
xq114 <1140735506@qq.com>
2022-04-26 15:57:12 +0800

    hwloc 2.7.1

--- packages/h/hwloc/xmake.lua
+++ packages/h/hwloc/xmake.lua
@@ -10,17 +10,20 @@ package("hwloc")
                 return format("%d.%d/hwloc-win32-build-%s", version:major(), version:minor(), version)
             end})
             add_versions("2.5.0", "0ff33ef99b727a96fcca8fd510e41f73444c5e9ea2b6c475a64a2d9a294f2973")
+            add_versions("2.7.1", "217d508f715d42932c6d52e5cf5eb3559d9691d6bb77c34f00b3dcb6517c58e5")
         elseif is_arch("x64") then
             add_urls("https://download.open-mpi.org/release/hwloc/v$(version).zip", {version = function (version)
                 return format("%d.%d/hwloc-win64-build-%s", version:major(), version:minor(), version)
             end})
             add_versions("2.5.0", "b64f5ebe534d1ad57cdd4b18ab4035389b68802a97464c1295005043075309ea")
+            add_versions("2.7.1", "31031eb09f7d8bfaaa069e537ec26374269dddd5b1f1a368c1ed6593849be5b1")
         end
     else
         add_urls("https://download.open-mpi.org/release/hwloc/v$(version).tar.gz", {version = function (version)
             return format("%d.%d/hwloc-%s", version:major(), version:minor(), version)
         end})
         add_versions("2.5.0", "38aa8102faec302791f6b4f0d23960a3ffa25af3af6af006c64dbecac23f852c")
+        add_versions("2.7.1", "4cb0a781ed980b03ad8c48beb57407aa67c4b908e45722954b9730379bc7f6d5")
     end
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
