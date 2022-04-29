b6fbc9e019c82c91cb4b21035b4d2ff752feb111
ruki <waruqi@gmail.com>
2022-04-29 08:41:21 +0800

    Update xmake.lua

--- packages/c/cli11/xmake.lua
+++ packages/c/cli11/xmake.lua
@@ -1,4 +1,5 @@
 package("cli11")
+    set_kind("library", {headeronly = true})
     set_homepage("https://github.com/CLIUtils/CLI11")
     set_description("CLI11 is a command line parser for C++11 and beyond that provides a rich feature set with a simple and intuitive interface.")
     set_license("BSD")
@@ -9,7 +10,7 @@ package("cli11")
     add_versions("v2.2.0", "d60440dc4d43255f872d174e416705f56ba40589f6eb07727f76376fb8378fd6")
 
     on_install("windows", "linux", "macosx", function (package)
-        os.cp("include/**.hpp", package:installdir(path.join("include", "CLI")))
+        os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
