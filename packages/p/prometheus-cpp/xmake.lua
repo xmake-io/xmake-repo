a1375629f11201e3c2724b791273fab2a5a10c04
ruki <waruqi@gmail.com>
2022-05-24 11:56:35 +0800

    Update xmake.lua

--- packages/p/prometheus-cpp/xmake.lua
+++ packages/p/prometheus-cpp/xmake.lua
@@ -7,29 +7,9 @@ package("prometheus-cpp")
              "https://github.com/jupp0r/prometheus-cpp.git")
     add_versions("v1.0.0", "07018db604ea3e61f5078583e87c80932ea10c300d979061490ee1b7dc8e3a41")
 
-    local configdeps = {
-        pull = {
-            description = "Enable pull.",
-            deps = {["civetweb"] = {version = "v1.15"}},
-            links = {"prometheus-cpp-pull"},
-            default = true,
-        },
-        push = {
-            description = "Enable push.",
-            deps = {["libcurl"] = {}},
-            links = {"prometheus-cpp-push"},
-            default = true,
-        },
-        compression = {
-            description = "Enable compression.",
-            deps = {["zlib"] = {}},
-            links = nil,
-            default = true,
-        },
-    }
-    for c, o in pairs(configdeps) do
-        add_configs(c, {description = o.description, default = o.default, type = type(o.default)})
-    end
+    add_configs("pull",        {description = "Enable Pull.", default = true, type = "boolean"})
+    add_configs("push",        {description = "Enable push.", default = true, type = "boolean"})
+    add_configs("compression", {description = "Enable compression.", default = true, type = "boolean"})
 
     add_deps("cmake")
 
@@ -38,26 +18,27 @@ package("prometheus-cpp")
     end
 
     on_load("linux", function (package)
-        for c, o in pairs(configdeps) do
-            if package:config(c) then
-                for dep, depcfg in pairs(o.deps) do
-                    package:add("deps", dep, depcfg)
-                end
-                if o.links then
-                    package:add("links", o.links)
-                end
-            end
+        if package:config("pull") then
+            package:add("deps", "civetweb v1.15")
+            package:add("links", "prometheus-cpp-pull")
         end
-        package:add("links", {"prometheus-cpp-core"})
+        if package:config("push") then
+            package:add("deps", "libcurl")
+            package:add("links", "prometheus-cpp-push")
+        end
+        if package:config("compression") then
+            package:add("deps", "zlib")
+        end
+        package:add("links", "prometheus-cpp-core")
     end)
 
     on_install("linux", function (package)
         local configs = {}
-
-        for c, v in pairs(configdeps) do
-            table.insert(configs, "-DENABLE_" .. string.upper(c) .. "=" .. (package:config(c) and "ON" or "OFF"))
+        for name, enabled in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                table.insert(configs, "-DENABLE_" .. name:upper() .. "=" .. (enabled and "ON" or "OFF"))
+            end
         end
-
         table.insert(configs, "-DGENERATE_PKGCONFIG=ON")
         table.insert(configs, "-DENABLE_TESTING=OFF")
         table.insert(configs, "-DUSE_THIRDPARTY_LIBRARIES=OFF")
