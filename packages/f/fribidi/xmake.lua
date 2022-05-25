49ca4777d58d4e2b8842cfa1938171b2542cba41
xq114 <1140735506@qq.com>
2022-05-25 19:32:10 +0800

    update fribidi

--- packages/f/fribidi/xmake.lua
+++ packages/f/fribidi/xmake.lua
@@ -7,6 +7,7 @@ package("fribidi")
     add_urls("https://github.com/fribidi/fribidi/releases/download/v$(version)/fribidi-$(version).tar.xz")
     add_versions("1.0.10", "7f1c687c7831499bcacae5e8675945a39bacbad16ecaa945e9454a32df653c01")
     add_versions("1.0.11", "30f93e9c63ee627d1a2cedcf59ac34d45bf30240982f99e44c6e015466b4e73d")
+    add_versions("1.0.12", "0cd233f97fc8c67bb3ac27ce8440def5d3ffacf516765b91c2cc654498293495")
 
     if is_plat("windows") then
         add_deps("meson", "ninja")
