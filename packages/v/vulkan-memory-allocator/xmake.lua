b9bc6459cceefc190ebae2dc92eb080298c98f42
ruki <waruqi@gmail.com>
2022-06-07 23:44:21 +0800

    Update xmake.lua

--- packages/v/vulkan-memory-allocator/xmake.lua
+++ packages/v/vulkan-memory-allocator/xmake.lua
@@ -1,11 +1,12 @@
 package("vulkan-memory-allocator")
-
+    set_kind("library", {headeronly = true})
     set_homepage("https://gpuopen-librariesandsdks.github.io/VulkanMemoryAllocator/html/")
     set_description("Easy to integrate Vulkan memory allocation library.")
     set_license("MIT")
 
-    add_urls("https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator.git")
-    add_urls("https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/archive/refs/tags/$(version).tar.gz")
+
+    add_urls("https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator.git")
     add_versions("v3.0.0", 'dbb621a7a13fb70b8c34fef62fbe5128cc5193c7179c9edacead9f110df79a2f')
 
     add_deps("vulkan-headers")
