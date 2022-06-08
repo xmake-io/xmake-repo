06e89f5d64f406a96fffd23757ce8d536ff71a6b
DragonJoker <dragonjoker59@hotmail.com>
2022-06-08 03:32:34 +0200

    ShaderWriter: Reverted use of tar.gz due to missing submodule.

--- packages/s/shaderwriter/xmake.lua
+++ packages/s/shaderwriter/xmake.lua
@@ -3,14 +3,14 @@ package("shaderwriter")
     set_homepage("https://github.com/DragonJoker/ShaderWriter")
     set_description("Library used to write shaders from C++, and export them in either GLSL, HLSL or SPIR-V.")
 
-    set_urls("https://github.com/DragonJoker/ShaderWriter/archive/refs/tags/$(version).tar.gz",
-        "https://github.com/DragonJoker/ShaderWriter.git")
-    add_versions("v1.0.0", "30729d7f610c4ff24e9b46702c9729b3f68b637fc72d38bd1a1664ba39dd9329")
-    add_versions("v1.1.0", "02e25288c238792f79b32ec402e514951709116023277b3dc02072add0352134")
-    add_versions("v2.0.0", "79315247617ebb5a6b15fa3062c3d3bff48b83692a9f13760d53ce98c722fff3")
-    add_versions("v2.1.0", "2821ec26b5969f652c2746ff145d93b2981355d7db417db544b41d6710521059")
-    add_versions("v2.2.0", "a9fca4453be9f8d2b57a6deb5af4679eb10af518bb4db0426292e95238e93c79")
-    add_versions("v2.3.0", "d88ecccb74e02d302bcd40cde4ccf706e249b352494253312f21cca2fedd0113")
+    set_urls("https://github.com/DragonJoker/ShaderWriter.git")
+    add_versions("0.1", "a5ef99ff141693ef28cee0e464500888cabc65ad")
+    add_versions("1.0", "7d506b6864edb5f357ed8993512f5a3618a4ddc1")
+    add_versions("1.1", "e7ddabe2b9ec6279951f4dcfe6a803d42d0e9052")
+    add_versions("2.0", "9e7488290713f88149038ef56fdc4034c3a1dd7f")
+    add_versions("2.1", "eec963d7c0d9a88741ed357bcc931a8de763ddb7")
+    add_versions("2.2", "eec963d7c0d9a88741ed357bcc931a8de763ddb7")
+    add_versions("2.3", "8e7769ddf4b008b0c7de3140126fcfb30607879e")
 
     add_deps("cmake")
 
