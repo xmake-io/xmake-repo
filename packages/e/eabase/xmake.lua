b9c160ece27ad77e3e27ec67e47946a6ab5b9f7e
ruki <waruqi@gmail.com>
2022-06-09 19:38:29 +0800

    fix eabase (#1259)

--- packages/e/eabase/xmake.lua
+++ packages/e/eabase/xmake.lua
@@ -1,11 +1,11 @@
 package("eabase")
-
+    set_kind("library", {headeronly = true})
     set_homepage("https://github.com/electronicarts/EABase")
     set_description("EABase is a small set of header files that define platform-independent data types and platform feature macros.")
     set_license("BSD-3-Clause")
 
     add_urls("https://github.com/electronicarts/EABase/archive/$(version).tar.gz")
-    add_versions("2.09.06", "981f922441617152b841585c0fc0bd205bd898c758016fa4985599a63c5a6e16")
+    add_versions("2.09.05", "f85b98c96f1976aa013c8d6016c5e37a409633ac3fce843cd4ec0b89c67e6b7a")
 
     on_install("windows", "linux", "macosx", function (package)
         os.cp("include/Common/EABase", package:installdir("include"))
