3e4e12e3fcb92ae8d2a827858859ff99e0d39f50
yszhou <987912102@qq.com>
2022-06-19 16:42:40 +0800

    add package zydis

---
+++ packages/z/zydis/xmake.lua
@@ -0,0 +1,30 @@
+package("zydis")
+    set_homepage("https://zydis.re")
+    set_description("Fast and lightweight x86/x86-64 disassembler and code generation library")
+    set_license("MIT")
+
+    add_urls("https://github.com/zyantific/zydis/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/zyantific/zydis.git")
+    add_versions("v3.2.1", "349a2d27270e54499b427051dd45f7b6064811b615588414b096cdeeaeb730ad")
+    add_patches("v3.2.1", path.join(os.scriptdir(), "patches", "v3.2.1", "cmake.patch"), "24ad483ca6b432359f7996d4b0b0ccc40f5d6d79cfeb752d4980f7e177056186")
+
+    add_deps("cmake")
+    add_deps("zycore-c")
+    
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DZYDIS_BUILD_EXAMPLES=OFF")
+        table.insert(configs, "-DZYDIS_BUILD_SHARED_LIB=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <Zydis/Zydis.h>
+            #include <Zycore/LibC.h>
+            void test() {
+                ZyanU8 encoded_instruction[ZYDIS_MAX_INSTRUCTION_LENGTH];
+                ZyanUSize encoded_length = sizeof(encoded_instruction);
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
