05fc8a227d12f09eee9da6122c46b421b6a69a71
yszhou <987912102@qq.com>
2022-06-21 10:03:36 +0800

    complement format

--- packages/w/wolfssl/xmake.lua
+++ packages/w/wolfssl/xmake.lua
@@ -1,11 +1,11 @@
 package("wolfssl")
     set_homepage("https://www.wolfssl.com")
     set_description("The wolfSSL library is a small, fast, portable implementation of TLS/SSL for embedded devices to the cloud.  wolfSSL supports up to TLS 1.3!")
+    set_license("GPL-2.0")
 
     add_urls("https://github.com/wolfSSL/wolfssl/archive/refs/tags/$(version).tar.gz",
              "https://github.com/wolfSSL/wolfssl.git")
     add_versions("v5.3.0-stable", "1a3bb310dc01d3e73d9ad91b6ea8249d081016f8eef4ae8f21d3421f91ef1de9")
-    set_license("GPL-2.0")
 
     add_deps("cmake")
 
