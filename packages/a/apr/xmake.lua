c9c33cada29d0196a6150aa69cd925be6552a5de
yszhou <987912102@qq.com>
2022-06-22 19:53:03 +0800

    win & linux to fix

--- packages/a/apr/xmake.lua
+++ packages/a/apr/xmake.lua
@@ -7,27 +7,27 @@ package("apr")
              "https://github.com/apache/apr.git")
     add_versions("1.7.0", "a7e2c5e6d60f6c7b1611b31a2f914a3e58f44eded5b064f0bae43ff30b16a4e6")
 
-    on_load(function (package)
-        if package:is_plat("macosx", "linux") then
-            package:add("deps", "autoconf", "libtool")
-        end
-    end)
-
+    if is_plat("macosx", "linux") then 
+        add_deps("autoconf", "libtool", "python")
+    end
+    
     on_install("linux", "macosx", "windows", function (package)
-        if package:is_plat("macosx", "linux") then 
+        if package:is_plat("linux") then 
             os.vrunv("sh", {"./buildconf"})
-            local configs = {"--prefix=" .. package:installdir()}
-            os.vrunv("./configure", configs)
+            os.vrunv("./configure", {"--prefix=" .. package:installdir()})
+            import("package.tools.make").install(package)
+        elseif package:is_plat("macosx") then 
+            os.exec("sed -i -e 's/#error .* pid_t/#define APR_PID_T_FMT \"d\"/' configure.in")
+            os.vrunv("sh", {"./buildconf"})
+            os.exec("./configure CFLAGS=-DAPR_IOVEC_DEFINED --prefix=" .. package:installdir())
             import("package.tools.make").install(package)
         elseif package:is_plat("windows") then
-            local configs = {"-f", "Makefile.win"}
-            import("package.tools.nmake").install(package, configs)
-            table.insert(configs, "PREFIX=" .. package:installdir(), "install")
+            local configs = {"-f", "Makefile.win", "PREFIX=" .. package:installdir()}
             import("package.tools.nmake").install(package, configs)
         end
-        os.mv(package:installdir("include/apr-2/*"), package:installdir("include"))
+        os.mv(package:installdir("include/apr-1/*"), package:installdir("include"))
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("apr_date_checkmask", {includes = "apr_date.h"}))
+        assert(package:has_cincludes("apr.h"))
     end)
