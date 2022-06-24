0396ad7d882773cd9fa552466867592cef26ead8
yszhou <987912102@qq.com>
2022-06-24 13:30:58 +0800

    temp save

--- packages/l/lbuild/xmake.lua
+++ packages/l/lbuild/xmake.lua
@@ -11,21 +11,21 @@ package("lbuild")
     on_install(function (package)
         local python_version = package:dep("python"):version()
         local lbuild_version = package:version()
-        local lbuild_egg = "lbuild-" .. lbuild_version:major() .. "." .. lbuild_version:minor() .. "." .. lbuild_version:patch() .. "-py" .. python_version:major() .. "." .. python_version:minor() .. ".egg"
         local pyver = ("python%d.%d"):format(python_version:major(), python_version:minor())
         local PYTHONPATH = package:installdir("lib")
         local PYTHONPATH1 = path.join(PYTHONPATH, pyver)
-        PYTHONPATH = path.join(PYTHONPATH, "site-packages", lbuild_egg)
-        PYTHONPATH2 = path.join(PYTHONPATH1, "site-packages", "lxml-4.9.0-py3.9-macosx-12-x86_64.egg")
-        PYTHONPATH1 = path.join(PYTHONPATH1, "site-packages", lbuild_egg)
-        package:addenv("PYTHONPATH", PYTHONPATH, PYTHONPATH1, PYTHONPATH2)
-
-        io.writefile("build/doc/man/lbuild.1", "")
-        io.writefile("build/doc/man/lbuild-time.1", "")
-        io.writefile("build/doc/man/lbuildign.1", "")
+        PYTHONPATH = path.join(PYTHONPATH, "site-packages", "*")
+        PYTHONPATH1 = path.join(PYTHONPATH1, "site-packages", "*")
+        
         io.replace("setup.py", "from lbuild.__init__ import __version__", format("__version__ = '%s'", lbuild_version))
 
         os.vrunv("python", {"setup.py", "install", "--prefix", package:installdir()})
+        for _, path in ipairs(os.dirs(PYTHONPATH)) do
+            package:addenv("PYTHONPATH", path)
+        end
+        for _, path in ipairs(os.dirs(PYTHONPATH1)) do 
+            package:addenv("PYTHONPATH", path)
+        end
         if package:is_plat("windows") then
             os.mv(package:installdir("Scripts", "*"), package:installdir("bin"))
             os.rmdir(package:installdir("Scripts"))
