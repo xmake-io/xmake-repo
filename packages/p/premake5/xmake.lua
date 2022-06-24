1d0535c7d44d8739e350aa657f1160377a31e278
yszhou <987912102@qq.com>
2022-06-24 23:03:54 +0800

    format according to package:mold

--- packages/p/premake5/xmake.lua
+++ packages/p/premake5/xmake.lua
@@ -14,11 +14,26 @@ package("premake5")
         local configs = {"-f", "Bootstrap.mak"}
         table.insert(configs, package:plat())
         if package:is_plat("linux", "macosx") then
-            local deps = {}
-            if package:is_plat("linux") and linuxos().name == "fedora" then 
-                table.insert(deps, "libuuid")
+            local cflags = {}
+            local ldflags = {}
+            local dep = package:dep("libuuid")
+            if dep then 
+                local depinfo = dep:fetch()
+                for _, includedir in ipairs(depinfo.includedirs or depinfo.sysincludedirs) do 
+                    table.insert(cflags, "-I" .. includedir)
+                end
+                for _, linkdir in ipairs(depinfo.linkdirs) do 
+                    table.insert(ldflags, "-L" .. linkdir)
+                end
             end
-            import("package.tools.make").build(package, configs, {packagedeps = deps})
+            if #cflags > 0 then 
+                table.insert(configs, "EXTRA_CFLAGS=" .. table.concat(cflags, " ")) 
+                table.insert(configs, "EXTRA_CXXFLAGS=" .. table.concat(cflags, " "))
+            end
+            if #ldflags > 0 then
+                table.insert(configs, "EXTRA_LDFLAGS=" .. table.concat(ldflags, " "))
+            end
+            import("package.tools.make").build(package, configs)
         else
             import("package.tools.nmake").build(package, configs)
         end
