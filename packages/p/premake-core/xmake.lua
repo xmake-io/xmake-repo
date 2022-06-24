185a19632a1f30f38d45f427e22aa4d200de302f
yszhou <987912102@qq.com>
2022-06-24 20:21:08 +0800

    set binary kind; rename to premake5

--- packages/p/premake-core/xmake.lua
+++ packages/p/premake-core/xmake.lua
@@ -1,16 +1,11 @@
-package("premake-core")
+package("premake5")
+    set_kind("binary")
     set_homepage("https://premake.github.io/")
     set_description("Premake")
 
-    set_urls("")
     set_urls("https://github.com/premake/premake-core/archive/refs/tags/$(version).zip", 
              "https://github.com/premake/premake-core.git")
-    
     add_versions("v5.0.0-beta1", "f4d1be74c514f39f2743698d66da92814aa120ccca9b1f0b2a8c63d8fbb57090")
-
-    on_load("linux", "macosx", "windows", function (package)
-        package:addenv("PATH", "bin")
-    end)
 
     on_install("linux", "macosx", "windows", function (package)
         local configs = {"-f", "Bootstrap.mak"}
