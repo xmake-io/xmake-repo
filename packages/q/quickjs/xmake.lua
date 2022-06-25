5c92159295f0a089d94276d9414a0a7d11d12f0b
yszhou <987912102@qq.com>
2022-06-25 16:49:08 +0800

    update quickjs msvc

--- packages/q/quickjs/xmake.lua
+++ packages/q/quickjs/xmake.lua
@@ -10,8 +10,13 @@ package("quickjs")
         add_syslinks("pthread", "dl", "m")
     elseif is_plat("android") then
         add_syslinks("dl", "m")
+    elseif is_plat("windows") then
+        add_deps("premake5")
+        add_syslinks("ws2_32")
+        -- add_syslinks("libucrt")
+        add_patches("2021.03.27", path.join(os.scriptdir(), "patches", "2021.03.27", "fix_msvc.patch"), "17377b926d4d9c95566c17cc1d02c388b3113f2a318dfe4f4e806b313cda07c5")
     end
-
+    
     on_install("linux", "macosx", "iphoneos", "android", "mingw", "cross", function (package)
         io.writefile("xmake.lua", ([[
             add_rules("mode.debug", "mode.release")
@@ -36,6 +41,140 @@ package("quickjs")
             io.replace("quickjs.c", "#define CONFIG_PRINTF_RNDN", "")
         end
         import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_install("windows", function (package)
+        local vs = import("core.tool.toolchain").load("msvc"):config("vs")
+        local msvc_version
+        if tonumber(vs) == 2019 then
+            msvc_version = "vs2019"
+        elseif tonumber(vs) == 2022 then
+            msvc_version = "vs2022"
+        else
+            raise("unsupported msvc version " .. msvc_version)
+        end
+        local kind = package:config("shared") and "SharedLib" or "StaticLib"
+        local arch = package:is_arch("x86") and "x86_64" or "x64"
+        -- premake5.lua
+        io.writefile("premake5.lua", ([[
+            function GenerateCArray(srcFile, dstFile, arrayName, arraySizeName)
+                local src = io.readfile(srcFile)
+                if src == nil then return false end
+            
+                if arrayName == nil then
+                    arrayName = path.getbasename(dstFile)
+                end
+            
+                if arraySizeName == nil then
+                    arraySizeName = arrayName .. "_size"
+                end
+            
+                local dst = "const unsigned char " .. arrayName .. "[] = {\n"
+                local line = ""
+            
+                for i = 1, #src do
+                    local byte = src:byte(i, i)
+                    line = line .. string.format("0x%%02x, ", byte)
+            
+                    if (#line >= 80) or (i == #src) then
+                        if i == #src then line = line:sub(1, #line - 2) end
+            
+                        dst = dst .. "\t" .. line .. "\n"
+                        line = ""
+                    end
+                end
+            
+                dst = dst .. "};\n\n"
+                dst = dst .. string.format("const unsigned " .. arraySizeName .. " = %%d;\n\n", #src)
+            
+                local oldDst = ""
+                if os.isfile(dstFile) then oldDst = io.readfile(dstFile) end
+            
+                if oldDst ~= dst then
+                    io.writefile(dstFile, dst)
+                end
+            
+                return true
+            end
+            
+            -----------------------------------------------------------------------------------------------------------------------
+            
+            workspace "quickjs"
+                -- Premake output folder
+                location(path.join(".build", _ACTION))
+            
+                -- Target architecture
+                architecture "%s"
+            
+                -- Configuration settings
+                configurations { "Debug", "Release" }
+            
+                -- Debug configuration
+                filter { "configurations:Debug" }
+                    defines { "DEBUG" }
+                    symbols "On"
+                    optimize "Off"
+            
+                -- Release configuration
+                filter { "configurations:Release" }
+                    defines { "NDEBUG" }
+                    optimize "Speed"
+                    inlining "Auto"
+            
+                filter { "language:not C#" }
+                    defines { "_CRT_SECURE_NO_WARNINGS" }
+                    characterset ("MBCS")
+                    buildoptions { "/std:c++latest" }
+            
+                    if _ACTION == "vs2017" then
+                        systemversion("10.0.17763.0")
+                    end
+            
+                filter { }
+                    targetdir "bin/%%{cfg.longname}/"
+                    defines { "WIN32", "_AMD64_", "__x86_64__" }
+                    exceptionhandling "Off"
+                    rtti "Off"
+                    vectorextensions "AVX2"
+            
+            -----------------------------------------------------------------------------------------------------------------------
+            
+            project "quickjs"
+                language "C"
+                kind "%s"
+                files {
+                    "cutils.c",
+                    "cutils.h",
+                    "libregexp.c",
+                    "libregexp.h",
+                    "libregexp-opcode.h",
+                    "libunicode.c",
+                    "libunicode.h",
+                    "libunicode-table.h",
+                    "list.h",
+                    "quickjs.c",
+                    "quickjs.h",
+                    "quickjs-atom.h",
+                    "quickjs-libc.c",
+                    "quickjs-libc.h",
+                    "quickjs-opcode.h"
+                }
+            
+        ]]):format(arch, kind))
+        -- premake generate sln 
+        os.exec("premake5 " .. msvc_version)
+        local configuration = package:debug() and "Debug" or "Release"
+        -- msvc compile and build
+        local configs = {".build/" .. msvc_version .. "/quickjs.sln"}
+        table.insert(configs, "/p:platform=" .. os.arch())
+        table.insert(configs, "/p:configuration=" .. configuration)
+        import("package.tools.msbuild").build(package, configs)
+        os.mv("quickjs.h", package:installdir("include"))
+        os.trymv("bin/" .. configuration .. "/*.lib", package:installdir("lib"))
+        os.trymv("bin/" .. configuration .. "/*.dll", package:installdir("lib"))
+        if not package:config("shared") then 
+            os.tryrm(package:installdir("bin/*.dll"))
+        end
     end)
 
     on_test(function (package)
