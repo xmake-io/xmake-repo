49b90b2b1b4fb01e2914e83a86ea09f9a5a15fb9
yszhou <987912102@qq.com>
2022-06-27 13:11:17 +0800

    set mirror image instead of patch

--- packages/q/quickjs/xmake.lua
+++ packages/q/quickjs/xmake.lua
@@ -3,15 +3,18 @@ package("quickjs")
     set_homepage("https://bellard.org/quickjs/")
     set_description("QuickJS is a small and embeddable Javascript engine")
 
-    add_urls("https://github.com/bellard/quickjs.git")
-    add_versions("2021.03.27", "b5e62895c619d4ffc75c9d822c8d85f1ece77e5b")
+    if is_plat("windows") then
+        add_urls("https://github.com/xmake-mirror/quickjs.git")
+        add_versions("2021.03.27", "e330c2fd6e30a7f3d1bcb88e12273b76c0585025")
+    else
+        add_urls("https://github.com/bellard/quickjs.git")
+        add_versions("2021.03.27", "b5e62895c619d4ffc75c9d822c8d85f1ece77e5b")
+    end
 
     if is_plat("linux", "macosx", "iphoneos", "cross") then
         add_syslinks("pthread", "dl", "m")
     elseif is_plat("android") then
         add_syslinks("dl", "m")
-    elseif is_plat("windows") then
-        add_patches("2021.03.27", path.join(os.scriptdir(), "patches", "2021.03.27", "fix_msvc.patch"), "17377b926d4d9c95566c17cc1d02c388b3113f2a318dfe4f4e806b313cda07c5")
     end
     
     on_install("linux", "macosx", "iphoneos", "android", "mingw", "cross", function (package)
@@ -41,13 +44,6 @@ package("quickjs")
     end)
 
     on_install("windows", function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("quickjs")
-                set_kind("$(kind)")
-                add_files("quickjs.c", "cutils.c", "libregexp.c", "libunicode.c", "quickjs-libc.c")
-                add_headerfiles("quickjs.h")
-        ]])
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
