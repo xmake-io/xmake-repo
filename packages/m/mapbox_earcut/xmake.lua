f941260e3e9a8f6c273506c20778d8e7929558d8
wsw0108 <wsw0108@gmail.com>
2022-07-04 13:14:31 +0800

    try fix patch for mapbox_earcut

--- packages/m/mapbox_earcut/xmake.lua
+++ packages/m/mapbox_earcut/xmake.lua
@@ -7,7 +7,7 @@ package("mapbox_earcut")
     add_urls("https://github.com/mapbox/earcut.hpp/archive/refs/tags/v$(version).zip",
              "https://github.com/mapbox/earcut.hpp.git")
     add_versions("2.2.3", "010d2fe35938744960dcc0b25076eb541b07bb314a92afbcab14f7f887ceb98d")
-    add_patches("2.2.3", path.join(os.scriptdir(), "patches", "2.2.3", "mingw.patch"), "2128177fc505a9c8229d12406c6d7335ea7e5cf0a04e448a7496d185bda9aa5a")
+    add_patches("2.2.3", path.join(os.scriptdir(), "patches", "2.2.3", "mingw.patch"), "20a83bb8fdbc98f5ef034172d7b71120c59899bca0c81e9ccaee6d31ecf760d5")
 
     on_install(function (package)
         os.cp("include/mapbox", package:installdir("include"))
