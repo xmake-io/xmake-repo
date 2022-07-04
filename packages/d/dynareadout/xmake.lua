2dc32d582399f188f11894d8761a1399a4c71ffa
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-07-04 21:51:58 +0200

    Add dynareadout

---
+++ packages/d/dynareadout/xmake.lua
@@ -0,0 +1,25 @@
+package("dynareadout")
+
+    set_homepage("https://github.com/PucklaMotzer09/dynareadout")
+    set_description("Ansi C library for parsing binary output files of LS Dyna (d3plot, binout)")
+
+    add_urls("https://github.com/PucklaMotzer09/dynareadout/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/PucklaMotzer09/dynareadout.git")
+    add_versions("0.1", "833c8516c77ab57c56e942692e1fea2c96c50b7adfebd7f6f633ed43aaf46a56")
+
+    add_configs("cpp", {description = "Build the C++ bindings", default = true, type = "boolean"})
+
+    on_install("windows", "linux", "macosx", "mingw", function (package)
+        local configs = {}
+        configs.build_test = "n"
+        configs.build_cpp = package:config("cpp") and "y" or "n"
+        configs.kind = package:config("shared") and "shared" or "static"
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("binout_open", {includes = "binout.h"}))
+        if package:config("cpp") then
+            assert(package:has_cxxtypes("dro::Binout", {includes = "binout.hpp"}))
+        end
+    end)
