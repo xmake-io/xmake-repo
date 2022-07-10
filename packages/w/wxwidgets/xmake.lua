fc6cbec0b170cc1a824688a18c7cc9f1d3fe8f0c
ruki <waruqi@gmail.com>
2022-07-10 11:59:42 +0800

    use win binaries

--- packages/w/wxwidgets/xmake.lua
+++ packages/w/wxwidgets/xmake.lua
@@ -2,18 +2,48 @@ package("wxwidgets")
     set_homepage("https://www.wxwidgets.org/")
     set_description("Cross-Platform C++ GUI Library")
 
-    add_urls("https://github.com/wxWidgets/wxWidgets/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/wxWidgets/wxWidgets.git")
-    add_versions("v3.2.0", "43480e3887f32924246eb439520a3a2bc04d7947712de1ea0590c5b58dedadd9")
+    if is_plat("windows") then
+        if is_plat("x64") then
+            add_urls("https://github.com/wxWidgets/wxWidgets/releases/download/v$(version)/wxMSW-$(version)_vc14x_x64_Dev.7z")
+            add_versions("3.2.0", "02b7227916b98324f73ae9bed0f1cf27ae3157b4e3a3ded40ee8c0d570f0fd10")
+        else
+            add_urls("https://github.com/wxWidgets/wxWidgets/releases/download/v$(version)/wxMSW-$(version)_vc14x_Dev.7z")
+            add_versions("3.2.0", "0cd2387edcf1f26924d59efcc3ea4c8a00783ee01bf396756dabdd7967e4b37b")
+        end
+        add_resources("3.2.0", "headers",
+            "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.0/wxWidgets-3.2.0-headers.7z",
+            "bd847e20050c52d127f4afe9b00ffe29d87c2f907749bd6bc732c0db05bce4b1")
+    else
+        add_urls("https://github.com/wxWidgets/wxWidgets/archive/refs/tags/$(version).tar.gz",
+                 "https://github.com/wxWidgets/wxWidgets.git")
+        add_versions("v3.2.0", "43480e3887f32924246eb439520a3a2bc04d7947712de1ea0590c5b58dedadd9")
 
-    add_deps("cmake")
-    add_deps("libjpeg", "libpng", "libtiff", "nanosvg", "expat", "zlib")
-    if is_plat("linux") then
-        add_deps("pkg-config")
-        add_deps("gtk+3")
+        add_deps("cmake")
+        add_deps("libjpeg", "libpng", "libtiff", "nanosvg", "expat", "zlib")
+        if is_plat("linux") then
+            add_deps("pkg-config")
+            add_deps("gtk+3")
+        end
     end
 
-    on_install("macosx", "linux", "windows", function (package)
+    on_load(function (package)
+        if package:is_plat("macosx", "linux") then
+            local version = package:version()
+            package:add("includedirs", "include/wx-" .. version:major() .. "." .. version:minor())
+        end
+    end)
+
+    on_install("windows", function (package)
+        local dlldir = package:is_plat("x64") and "vc14x_x64_dll" or "vc14x_dll"
+        os.cp(path.join("lib", dlldir, "*.lib"), package:installdir("lib"))
+        os.cp(path.join("lib", dlldir, "*.dll"), package:installdir("bin"))
+        if package:debug() then
+            os.cp(path.join("lib", dlldir, "*.pdb"), package:installdir("lib"))
+        end
+        os.cp(path.join(package:resourcefile("headers"), "include"), package:installdir())
+    end)
+
+    on_install("macosx", "linux", function (package)
         local configs = {"-DwxBUILD_TESTS=OFF",
                          "-DwxBUILD_SAMPLES=OFF",
                          "-DwxBUILD_DEMOS=OFF",
