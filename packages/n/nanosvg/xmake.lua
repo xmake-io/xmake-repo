c1b4771a29d55ed686a049b5007ba276203da98f
ruki <waruqi@gmail.com>
2022-07-11 22:59:51 +0800

    fix nanosvg for win

--- packages/n/nanosvg/xmake.lua
+++ packages/n/nanosvg/xmake.lua
@@ -14,6 +14,9 @@ package("nanosvg")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("windows") and package:config("shared") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
