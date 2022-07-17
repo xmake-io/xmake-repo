8cd8df930c79c86f7d699c04db5b8a50311ccd70
SirLynix <lynix680@gmail.com>
2022-07-17 11:24:02 +0200

    Update xmake.lua

--- packages/t/tiny-process-library/xmake.lua
+++ packages/t/tiny-process-library/xmake.lua
@@ -12,7 +12,14 @@ package("tiny-process-library")
     on_install(function (package)
         local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:config("shared") then
+            table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
+            if package:is_plat("windows", "mingw") then
+                table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+            end
+        else
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
