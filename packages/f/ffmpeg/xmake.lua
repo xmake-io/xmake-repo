1f824d04572f7371d938231bc560cdcb683f91cd
ImperatorS79 <fevrier.simon@gmail.com>
2022-07-19 11:12:00 +0200

    Update xmake.lua

--- packages/f/ffmpeg/xmake.lua
+++ packages/f/ffmpeg/xmake.lua
@@ -60,7 +60,7 @@ package("ffmpeg")
                             result = result .. pkginfo
                         end
                     else
-                        return {}
+                        return nil
                     end
                 end
                 local ffmpeg = find_tool("ffmpeg", {check = "-help", version = true, command = "-version", parse = "%d+%.?%d+%.?%d+", force = true})
