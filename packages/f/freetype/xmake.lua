0c4853d8cca38ea63428123de3151b22216d8d36
Jérôme Leclercq <lynix680@gmail.com>
2022-07-27 22:49:17 +0200

    Temporary enable all platforms

--- packages/f/freetype/xmake.lua
+++ packages/f/freetype/xmake.lua
@@ -48,7 +48,7 @@ package("freetype")
         add_dep("woff2", "brotli")
     end)
 
-    on_install("windows", "mingw", "linux", "macosx", function (package)
+    on_install(function (package)
         local configs = {"-DCMAKE_INSTALL_LIBDIR=lib"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
