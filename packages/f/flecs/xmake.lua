d9221a294a693433dff9354e3f6778f9e3ba7da1
ruki <waruqi@gmail.com>
2022-08-03 16:54:19 +0800

    Update xmake.lua

--- packages/f/flecs/xmake.lua
+++ packages/f/flecs/xmake.lua
@@ -4,9 +4,10 @@ package("flecs")
     set_description("A fast entity component system (ECS) for C & C++")
     set_license("MIT")
 
-    add_urls("https://github.com/SanderMertens/flecs.git")
-    add_versions("v3.0.0", "8715faf3276f0970b80c28c2a8911f4ac86633d25ebab3d3c69521942769d7d4")
-    add_versions("v2.4.8", "8b24c11b0513ee6ad3904165ed0927d59cbff2acc9bce0f8f989f493891d7deb")
+    add_urls("https://github.com/SanderMertens/flecs/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/SanderMertens/flecs.git")
+    add_versions("v3.0.0", "ae88da6abc5612d16ab2e6aa4041b035491fc1f2")
+    add_versions("v2.4.8", "9a8040a197e4b5e032524bc7183f68faa7b2f759c67b983b40018a7726561cac")
 
     add_deps("cmake")
 
@@ -21,7 +22,7 @@ package("flecs")
         table.insert(configs, "-DFLECS_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
             if package:config("shared") then
-                package:add("defines", "flecs_EXPORTS")
+                package:add("defines", "flecs_EXPORTS=1")
             else
                 package:add("defines", "flecs_STATIC")
             end
@@ -34,5 +35,5 @@ package("flecs")
             void test() {
                 flecs::world ecs;
             }
-        ]]}, {configs = {languages = "c++11"}, includes = "flecs.h"}))
+        ]]}, {configs = {languages = "c++14"}, includes = "flecs.h"}))
     end)
