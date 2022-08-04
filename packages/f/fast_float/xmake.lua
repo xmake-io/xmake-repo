511998034371f24b68575794de61fffb5d3a3434
SirLynix <lynix680@gmail.com>
2022-08-04 18:06:27 +0200

    fast_float: fix wasm support

--- packages/f/fast_float/xmake.lua
+++ packages/f/fast_float/xmake.lua
@@ -8,6 +8,10 @@ package("fast_float")
              "https://github.com/fastfloat/fast_float.git")
     add_versions("v3.4.0", "a242877d2fae81ca412033f5ebf5dbc43cb029c56b4af78e33106b9a69f8f58e")
 
+    if is_plat("wasm") then
+        add_patches("v3.4.0", path.join(os.scriptdir(), "patches", "emscripten_fix.patch"), "026a958df1fd8fdef6ce62bd55fdde7e5dd408eb4cb9dcfebd78eaed6f9339f2")
+    end
+
     on_install(function (package)
         os.vcp("include/fast_float", package:installdir("include"))
     end)
