a3801dbb92e2f4bce6ba54e10c93f721f146c46d
SirLynix <lynix680@gmail.com>
2022-08-04 18:08:00 +0200

    Fix parenthesis

--- packages/f/fast_float/xmake.lua
+++ packages/f/fast_float/xmake.lua
@@ -9,7 +9,7 @@ package("fast_float")
     add_versions("v3.4.0", "a242877d2fae81ca412033f5ebf5dbc43cb029c56b4af78e33106b9a69f8f58e")
 
     if is_plat("wasm") then
-        add_patches("v3.4.0", path.join(os.scriptdir(), "patches", "emscripten_fix.patch"), "026a958df1fd8fdef6ce62bd55fdde7e5dd408eb4cb9dcfebd78eaed6f9339f2")
+        add_patches("v3.4.0", path.join(os.scriptdir(), "patches", "emscripten_fix.patch"), "482705431f67e6f0a375ed7bfe87d6856e7d13f071db6157e1d5659834b0eb50")
     end
 
     on_install(function (package)
