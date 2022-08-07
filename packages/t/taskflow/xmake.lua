466b48cc06c535780ae2d265bb3674f432dafdb3
Bradford Zhang <zyc@zyc.name>
2022-08-07 23:44:17 +0800

    Update taskflow v3.3.0&v3.4.0

--- packages/t/taskflow/xmake.lua
+++ packages/t/taskflow/xmake.lua
@@ -7,6 +7,8 @@ package("taskflow")
 
     add_urls("https://github.com/taskflow/taskflow.git")
     add_urls("https://github.com/taskflow/taskflow/archive/$(version).tar.gz")
+    add_versions("v3.4.0", "8f449137d3f642b43e905aeacdf1d7c5365037d5e1586103ed4f459f87cecf89")
+    add_versions("v3.3.0", "66b891f706ba99a5ca5ed239d520ad6943ebe94728d1c89e07a939615a6488ef")
     add_versions("v3.2.0", "26c37a494789fedc5de8d1f8452dc8a7774a220d02c14d5b19efe0dfe0359c0c")
     add_versions("v3.1.0", "17b56e23312d20c4ad5cc497b9f42cd0ad4451dbd2df0160a0a692fd16d47143")
     add_versions("v3.0.0", "553c88a6e56e115d29ac1520b8a0fea4557a5fcda1af1427bd3ba454926d03a2")
