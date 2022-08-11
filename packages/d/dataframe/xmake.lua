e029bd24978a0b234564f474fd83bed31833d10d
ruki <waruqi@gmail.com>
2022-08-11 21:32:38 +0800

    Update xmake.lua

--- packages/d/dataframe/xmake.lua
+++ packages/d/dataframe/xmake.lua
@@ -1,12 +1,12 @@
-package("DataFrame")
-
+package("dataframe")
     set_homepage("https://github.com/hosseinmoein/DataFrame")
     set_description("This is a C++ analytical library that provides interface and functionality similar to packages/libraries in Python and R.")
     set_license("MIT")
-    add_urls("https://github.com/hosseinmoein/DataFrame/archive/refs/tags/$(version).zip",
+
+    add_urls("https://github.com/hosseinmoein/DataFrame/archive/refs/tags/$(version).tar.gz",
              "https://github.com/hosseinmoein/DataFrame.git")
 
-    add_versions("v1.20.0", "efe74cd0af9e71bf139327c2b8b7a6917c6a2ed3")
+    add_versions("1.21.0", "a6b07eaaf628225a34e4402c1a6e311430e8431455669ac03691d92f44081172")
 
     add_deps("cmake")
 
@@ -17,3 +17,20 @@ package("DataFrame")
         import("package.tools.cmake").install(package, configs)
     end)
 
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <DataFrame/DataFrame.h>
+            #include <DataFrame/DataFrameFinancialVisitors.h>
+            #include <DataFrame/DataFrameMLVisitors.h>
+            #include <DataFrame/DataFrameStatsVisitors.h>
+            #include <DataFrame/Utils/DateTime.h>
+            #include <iostream>
+            using namespace hmdf;
+            using ULDataFrame = StdDataFrame<unsigned long>;
+            void test() {
+                std::vector<unsigned long> idx_col1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                ULDataFrame ul_df1;
+                ul_df1.load_index(std::move(idx_col1));
+            }
+        ]]}, {configs = {languages = "c++17"}}))
+    end)
