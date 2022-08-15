4cb3b830e8c05b08eabe93a5a53951d592acb740
xq114 <1140735506@qq.com>
2022-08-16 01:30:52 +0800

    update sqlite3

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -5,11 +5,10 @@ package("sqlite3")
 
     set_urls("https://sqlite.org/$(version)", {version = function (version)
         local year = "2022"
-        if version:le("3.36") then
-            year = "2021"
-        end
         if version:le("3.24") then
             year = "2018"
+        elseif version:le("3.36") then
+            year = "2021"
         end
         local version_str = version:gsub("[.+]", "")
         if #version_str < 7 then
@@ -25,6 +24,7 @@ package("sqlite3")
     add_versions("3.35.0+400", "7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d")
     add_versions("3.36.0+0", "bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3")
     add_versions("3.37.0+200", "4089a8d9b467537b3f246f217b84cd76e00b1d1a971fe5aca1e30e230e46b2d8")
+    add_versions("3.39.0+200", "852be8a6183a17ba47cee0bbff7400b7aa5affd283bf3beefc34fcd088a239de")
 
     if is_plat("macosx", "linux", "bsd") then
         add_syslinks("pthread", "dl")
