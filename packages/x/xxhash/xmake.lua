273c329c693cd6e2be0527228553683a04c1bb3d
Longhao Li <longhao_li@outlook.com>
2022-08-16 10:04:03 +0800

    add xxhash v0.8.1

--- packages/x/xxhash/xmake.lua
+++ packages/x/xxhash/xmake.lua
@@ -6,6 +6,7 @@ package("xxhash")
 
     add_urls("https://github.com/Cyan4973/xxHash/archive/refs/tags/$(version).tar.gz",
              "https://github.com/Cyan4973/xxHash.git")
+    add_versions("v0.8.1", "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c")
     add_versions("v0.8.0", "7054c3ebd169c97b64a92d7b994ab63c70dd53a06974f1f630ab782c28db0f4f")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
