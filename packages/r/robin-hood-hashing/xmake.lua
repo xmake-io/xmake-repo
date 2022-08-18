56dd9c6daf764e925f89db127416f0bb745332a1
ruki <waruqi@gmail.com>
2022-08-18 09:11:43 +0800

    Update xmake.lua

--- packages/r/robin-hood-hashing/xmake.lua
+++ packages/r/robin-hood-hashing/xmake.lua
@@ -11,7 +11,7 @@ package("robin-hood-hashing")
     add_versions("3.11.5", "7aa183252527ded7f46186c1e2f4efe7d6139a3b7c0869c1b6051bd7260587ed")
 
     on_load(function (package)
-        cprint("${yellow} Package deprecated, author reason:\nUnfortunately I do not have time to continue development for this hashmap. I have a worthy successor though, please head over to: ankerl::unordered_dense::{map, set}${clear}")
+        wprint("Package deprecated, author reason:\nUnfortunately I do not have time to continue development for this hashmap. I have a worthy successor though, please head over to: ankerl::unordered_dense::{map, set}${clear}")
     end)
 
     add_deps("cmake")
