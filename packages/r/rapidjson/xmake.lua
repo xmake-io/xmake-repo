cbea7651ced3c7e2a727b03a4e5c3bafcc0a065c
Longhao Li <longhao_li@outlook.com>
2022-08-18 16:58:26 +0800

    update rapidjson to master branch

--- packages/r/rapidjson/xmake.lua
+++ packages/r/rapidjson/xmake.lua
@@ -3,12 +3,7 @@ package("rapidjson")
     set_homepage("https://github.com/Tencent/rapidjson")
     set_description("RapidJSON is a JSON parser and generator for C++.")
 
-    set_urls("https://github.com/Tencent/rapidjson/archive/$(version).zip",
-             "https://github.com/Tencent/rapidjson.git")
-
-    add_versions("v1.1.0", "8e00c38829d6785a2dfb951bb87c6974fa07dfe488aa5b25deec4b8bc0f6a3ab")
-    -- This commit is used in arrow 7.0.0 https://github.com/apache/arrow/blob/release-7.0.0/cpp/thirdparty/versions.txt#L80
-    add_versions("v1.1.0-arrow", "1a803826f1197b5e30703afe4b9c0e7dd48074f5")
+    set_urls("https://github.com/Tencent/rapidjson.git")
 
     on_install(function (package)
         os.cp(path.join("include", "*"), package:installdir("include"))
