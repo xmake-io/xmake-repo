97429e13ef0d072e74a8a4ffd65c0fba45b043b9
Longhao Li <longhao_li@outlook.com>
2022-08-18 21:22:38 +0800

    update rapidjson to master branch (#1419)
    * update rapidjson to master branch
    
    * update rapidjson to 2022.7.20 git commit

--- packages/r/rapidjson/xmake.lua
+++ packages/r/rapidjson/xmake.lua
@@ -6,6 +6,7 @@ package("rapidjson")
     set_urls("https://github.com/Tencent/rapidjson/archive/$(version).zip",
              "https://github.com/Tencent/rapidjson.git")
 
+    add_versions("2022.7.20", "27c3a8dc0e2c9218fe94986d249a12b5ed838f1d")
     add_versions("v1.1.0", "8e00c38829d6785a2dfb951bb87c6974fa07dfe488aa5b25deec4b8bc0f6a3ab")
     -- This commit is used in arrow 7.0.0 https://github.com/apache/arrow/blob/release-7.0.0/cpp/thirdparty/versions.txt#L80
     add_versions("v1.1.0-arrow", "1a803826f1197b5e30703afe4b9c0e7dd48074f5")
