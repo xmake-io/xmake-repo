1962e97841f86917a382f54d73e28fe7bada900c
xq114 <1140735506@qq.com>
2022-08-20 20:53:24 +0800

    add liblas

---
+++ packages/l/liblas/xmake.lua
@@ -0,0 +1,37 @@
+package("liblas")
+
+    set_homepage("https://liblas.org/index.html")
+    set_description("libLAS - LAS 1.0/1.1/1.2 ASPRS LiDAR data translation toolset")
+    set_license("BSD-3-Clause")
+
+    add_urls("http://download.osgeo.org/liblas/libLAS-$(version).tar.bz2")
+    add_versions("1.8.1", "9adb4a98c63b461ed2bc82e214ae522cbd809cff578f28511122efe6c7ea4e76")
+
+    add_deps("cmake", "libgeotiff")
+    add_deps("boost", {configs = {program_options = true,
+                                  thread = true,
+                                  system = true,
+                                  iostreams = true,
+                                  filesystem = true}})
+    on_install("windows", "macosx", "linux", function (package)
+        local configs = {"-DWITH_TESTS=OFF", "-DWITH_LASZIP=OFF", "-DWITH_GDAL=OFF", "-DBoost_USE_STATIC_LIBS=ON"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("windows") then
+            table.insert(configs, "-DBoost_USE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+        end
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <liblas/liblas.hpp>
+            #include <fstream>
+            void test() {
+                std::ifstream ifs;
+                ifs.open("file.las", std::ios::in | std::ios::binary);
+                liblas::ReaderFactory f;
+                liblas::Reader reader = f.CreateWithStream(ifs);
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
