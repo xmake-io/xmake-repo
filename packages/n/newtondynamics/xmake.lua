02841d999be2ebf8de5b6f74fcb2385b83159f4a
SirLynix <lynix680@gmail.com>
2022-08-21 12:20:02 +0200

    Update xmake.lua

--- packages/n/newtondynamics/xmake.lua
+++ packages/n/newtondynamics/xmake.lua
@@ -5,3 +5,9 @@ package("newtondynamics")
         wprint("newtondynamics package has been renamed to newtondynamics3 due to release of v4, please update your dependency to newtondynamics3 or newtondynamics4")
         package:base():script("load")(package)
     end)
+
+    on_install("windows", "linux", "macosx", "mingw", function (package)
+        -- fixes package:scriptdir() from parent
+        local base = package:base()
+        base:script("load")(base)
+    end)
