d394747536127e1769e7b88937fd9257b074c9d1
xq114 <1140735506@qq.com>
2022-08-25 11:00:15 +0800

    improve shaderc

--- packages/s/shaderc/xmake.lua
+++ packages/s/shaderc/xmake.lua
@@ -4,10 +4,9 @@ package("shaderc")
     set_description("A collection of tools, libraries, and tests for Vulkan shader compilation.")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/google/shaderc.git")
-    add_versions("2021.11.22", "657c5ed2ba1714c0430895a274a94d6f2aeeab85")
-
-    add_configs("binaryonly", { description = "Only use binary program.", default = false, type = "boolean"})
+    add_urls("https://github.com/google/shaderc/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/google/shaderc.git")
+    add_versions("v2022.2", "517d36937c406858164673db696dc1d9c7be7ef0960fbf2965bfef768f46b8c0")
 
     add_deps("cmake", "python 3.x", {kind = "binary"})
 
@@ -16,7 +15,7 @@ package("shaderc")
     end
 
     on_load(function (package)
-        if package:config("binaryonly") then
+        if package:is_binary() then
             package:set("kind", "binary")
         end
         if package:config("shared") then
@@ -27,7 +26,7 @@ package("shaderc")
     end)
 
     on_fetch(function (package, opt)
-        if opt.system and package:config("binaryonly") then
+        if opt.system and package:is_binary() then
             return package:find_tool("glslc")
         end
     end)
@@ -43,7 +42,7 @@ package("shaderc")
 
     on_test(function (package)
         os.vrun("glslc --version")
-        if not package:config("binaryonly") then
+        if not package:is_binary() then
             assert(package:has_cfuncs("shaderc_compiler_initialize", {includes = "shaderc/shaderc.h"}))
         end
     end)
