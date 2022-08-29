7bdbb7b422492f7ecddf6df5cc6ef5294427f265
yszhou <987912102@qq.com>
2022-08-29 20:55:11 +0800

    temp save

--- packages/g/grpc/xmake.lua
+++ packages/g/grpc/xmake.lua
@@ -3,12 +3,14 @@ package("grpc")
     set_description("The C based gRPC (C++, Python, Ruby, Objective-C, PHP, C#)")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/grpc/grpc/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/grpc/grpc.git")
-    add_versions("v1.46.3", "d6cbf22cb5007af71b61c6be316a79397469c58c82a942552a62e708bce60964")
+    -- add_urls("https://github.com/grpc/grpc/archive/refs/tags/$(version).tar.gz",
+            --  "https://github.com/grpc/grpc.git")
+    add_urls("https://github.com/grpc/grpc.git")
+    -- add_versions("v1.46.3", "d6cbf22cb5007af71b61c6be316a79397469c58c82a942552a62e708bce60964")
+    add_versions("v1.46.3", "53d69cc581c5b7305708587f4f1939278477c28a")
 
     add_deps("cmake")
-    add_deps("abseil", "benchmark", "boringssl", "gtest", "libuv", "protobuf-cpp" "re2", "zlib")
+    add_deps("abseil", "benchmark", "boringssl", "gtest", "libuv", "protobuf-cpp", "re2", "zlib")
     -- TODO bloaty envoy-api googleapis opencensus-proto opentelemetry xds
     if is_plat("linux") then
         add_deps("autoconf", "libtool", "pkg-config")
