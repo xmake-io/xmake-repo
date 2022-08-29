3212845a310b16f25b837ea8c94221dc5c322f22
ruki <waruqi@gmail.com>
2022-08-29 23:32:11 +0800

    Update xmake.lua

--- packages/l/libsdl_image/xmake.lua
+++ packages/l/libsdl_image/xmake.lua
@@ -58,7 +58,7 @@ package("libsdl_image")
         if libsdl and not libsdl:is_system() then
             table.insert(configs, "--with-sdl-prefix=" .. libsdl:installdir())
         end
-        if libsdl and is_plat("macosx") then
+        if libsdl and package:is_plat("macosx") then
             table.insert(configs, "-DSDL2_DIR=" .. libsdl:installdir())
         end
         io.replace("Makefile.am", "noinst_PROGRAMS = showimage.-\n", "\n")
