338512fb70973481db5c5bb2295a864bed4a4832
ruki <waruqi@gmail.com>
2022-08-29 10:25:18 +0800

    Update xmake.lua

--- packages/l/libsdl_image/xmake.lua
+++ packages/l/libsdl_image/xmake.lua
@@ -50,22 +50,23 @@ package("libsdl_image")
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
-        else
-            table.insert(configs, "--enable-shared=no")
-        end
-        if package:is_plat("linux") and package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
-        end
+        local exflags
+        local configs = {"--disable-dependency-tracking"}
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         local libsdl = package:dep("libsdl")
         if libsdl and not libsdl:is_system() then
             table.insert(configs, "--with-sdl-prefix=" .. libsdl:installdir())
         end
-        io.replace("Makefile.am", "noinst_PROGRAMS = showimage", "")
+        if package:version():eq("2.0.5") then
+            io.replace("Makefile.am", "noinst_PROGRAMS = showimage", "")
+        elseif package:version():gt("2.6") then
+            exflags = {"-framework", "CoreFoundation",
+                       "-framework", "CoreGraphics",
+                       "-framework", "ImageIO",
+                       "-framework", "CoreServices"}
+        end
         os.rm("./configure")
-        import("package.tools.autoconf").install(package, configs)
+        import("package.tools.autoconf").install(package, configs, {ldflags = exflags, shflags = exflags})
     end)
 
     on_test(function (package)
