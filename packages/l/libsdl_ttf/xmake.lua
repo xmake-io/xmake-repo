d1e05c4decec3e4df01918bbe0502c4b703510ec
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2022-08-30 08:14:48 +0200

    Add github link and update libsdl* (#1444)
    * Add github link and update libsdl*
    
    * Update xmake.lua
    
    * Make sdl_image xmake like sdl xmake
    
    * Make sdl_ttf xmake like sdl xmake
    
    * Readd sdl_image compat flags
    
    * Update xmake.lua
    
    * Re-remove the configure file for sdl_image
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Attempt at `-DSDL2_DIR` for macos
    
    * Update xmake.lua
    
    * correct config parameter
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/l/libsdl_ttf/xmake.lua
+++ packages/l/libsdl_ttf/xmake.lua
@@ -2,14 +2,7 @@ package("libsdl_ttf")
 
     set_homepage("https://www.libsdl.org/projects/SDL_ttf/")
     set_description("Simple DirectMedia Layer text rendering library")
-
-    if is_plat("windows", "mingw") then
-        set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-$(version)-VC.zip")
-        add_versions("2.0.15", "aab0d81f1aa6fe654be412efc85829f2b188165dca6c90eb4b12b673f93e054b")
-    else
-        set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).zip")
-        add_versions("2.0.15", "cdb72b5b1c3b27795fa128af36f369fee5d3e38a96c350855da0b81880555dbc")
-    end
+    set_license("zlib")
 
     if is_plat("mingw") and is_subhost("msys") then
         add_extsources("pacman::SDL2_ttf")
@@ -19,14 +12,37 @@ package("libsdl_ttf")
         add_extsources("brew::sdl2_ttf")
     end
 
+    if is_plat("windows", "mingw") then
+        set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-$(version)-VC.zip")
+        add_urls("https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-devel-$(version)-VC.zip")
+        add_versions("2.0.15", "aab0d81f1aa6fe654be412efc85829f2b188165dca6c90eb4b12b673f93e054b")
+        add_versions("2.0.18", "4e1404ac6095bbcd4cb133eb644a765885aa283ae45f36cfbc3cf5a77d7e1cbd")
+        add_versions("2.20.0", "bc206392a74d2b32f74d770a3a1e623e87c72374781c115478277ab4c722358b")
+        add_versions("2.20.1", "2facfa180f77bac381776376c7598ad504a84d99414418049bb106cb1705fe22")
+    else
+        set_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).zip")
+        add_urls("https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-$(version).zip")
+        add_versions("2.0.15", "cdb72b5b1c3b27795fa128af36f369fee5d3e38a96c350855da0b81880555dbc")
+        add_versions("2.0.18", "64e6a93c7542aba1e32e1418413898dfde82be95fdd0c73ba265fbdada189b5f")
+        add_versions("2.20.0", "04e94fc5ecac3475ab35c1d5cf52650df691867e7e4befcc861bf982a747111a")
+        add_versions("2.20.1", "18d81ab399c8e39adababe8918691830ba6e0d6448e5baa141ee0ddf87ede2dc")
+    end
+
     add_deps("libsdl")
     if is_plat("linux", "macosx") then
         add_deps("freetype")
     end
 
     add_links("SDL2_ttf")
-
     add_includedirs("include", "include/SDL2")
+
+    on_load("macosx", "linux", function (package)
+        if package:version():ge("2.20") then
+            package:add("deps", "cmake")
+        else
+            package:add("deps", "automake", "autoconf")
+        end
+    end)
 
     on_install("windows", "mingw", function (package)
         local arch = package:arch()
@@ -39,20 +55,24 @@ package("libsdl_ttf")
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
+        if package:version():ge("2.20") then
+            local configs = {"-DSDL2TTF_SAMPLES=OFF"}
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+            table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+            if libsdl and not libsdl:is_system() then
+                table.insert(configs, "-DSDL2_DIR=" .. libsdl:installdir())
+            end
+            import("package.tools.cmake").install(package, configs)
         else
-            table.insert(configs, "--enable-shared=no")
+            local configs = {}
+            table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+            table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+            local libsdl = package:dep("libsdl")
+            if libsdl and not libsdl:is_system() then
+                table.insert(configs, "--with-sdl-prefix=" .. libsdl:installdir())
+            end
+            import("package.tools.autoconf").install(package, configs)
         end
-        if package:is_plat("linux") and package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
-        end
-        local libsdl = package:dep("libsdl")
-        if libsdl and not libsdl:is_system() then
-            table.insert(configs, "--with-sdl-prefix=" .. libsdl:installdir())
-        end
-        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_test(function (package)
