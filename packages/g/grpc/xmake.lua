66ef64a434fc9fe974987b7d0d453510f7d8ef55
yszhou <987912102@qq.com>
2022-09-01 01:10:35 +0800

    update grpc

--- packages/g/grpc/xmake.lua
+++ packages/g/grpc/xmake.lua
@@ -3,15 +3,10 @@ package("grpc")
     set_description("The C based gRPC (C++, Python, Ruby, Objective-C, PHP, C#)")
     set_license("Apache-2.0")
 
-    -- add_urls("https://github.com/grpc/grpc/archive/refs/tags/$(version).tar.gz",
-            --  "https://github.com/grpc/grpc.git")
     add_urls("https://github.com/grpc/grpc.git")
-    -- add_versions("v1.46.3", "d6cbf22cb5007af71b61c6be316a79397469c58c82a942552a62e708bce60964")
     add_versions("v1.46.3", "53d69cc581c5b7305708587f4f1939278477c28a")
 
     add_deps("cmake")
-    add_deps("abseil", "benchmark", "boringssl", "gtest", "libuv", "protobuf-cpp", "re2", "zlib")
-    -- TODO bloaty envoy-api googleapis opencensus-proto opentelemetry xds
     if is_plat("linux") then
         add_deps("autoconf", "libtool", "pkg-config")
         add_extsources("apt::build-essential")
@@ -30,5 +25,10 @@ package("grpc")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
+        assert(package:check_cxxsnippets({test = [[
+            using grpc::Server;
+            using grpc::ServerBuilder;
+            using grpc::ServerContext;
+            using grpc::Status;
+        ]]}, {configs = {languages = "c++11"}, includes = "grpcpp/grpcpp.h"}))
     end)
