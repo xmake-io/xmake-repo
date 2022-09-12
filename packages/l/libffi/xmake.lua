00fdb9a2a99fa9fd49321313224128a1081500df
ruki <waruqi@gmail.com>
2022-09-13 00:55:18 +0800

    support bsd for libffi

--- packages/l/libffi/xmake.lua
+++ packages/l/libffi/xmake.lua
@@ -16,7 +16,7 @@ package("libffi")
     end
 
 
-    on_load("macosx", "linux", "iphoneos", function (package)
+    on_load("macosx", "linux", "iphoneos", "bsd", function (package)
         if package:gitref() then
             package:add("deps", "autoconf", "automake", "libtool")
         elseif package:version():le("3.2.1") then
@@ -32,15 +32,12 @@ package("libffi")
         })
     end)
 
-    on_install("macosx", "linux", "iphoneos", function (package)
+    on_install("macosx", "linux", "iphoneos", "bsd", function (package)
         local configs = {"--disable-silent-rules", "--disable-dependency-tracking"}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
         if package:debug() then
             table.insert(configs, "--enable-debug")
-        end
-        if package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
         end
         import("package.tools.autoconf").install(package, configs)
         if package:is_plat("linux") and package:is_arch("x86_64") then
