6c65c5399481ed8ef28f8a198cb676c64b8b8f60
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-09-16 19:02:25 +0200

    Add WIL

---
+++ packages/w/wil/xmake.lua
@@ -0,0 +1,22 @@
+package("wil")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/microsoft/wil")
+    set_description("The Windows Implementation Libraries (WIL) is a header-only C++ library created to make life easier for developers on Windows through readable type-safe C++ interfaces for common Windows coding patterns.")
+    set_license("MIT")
+
+    add_urls("https://github.com/microsoft/wil.git")
+    add_versions("2022.09.16", "5f4caba4e7a9017816e47becdd918fcc872039ba")
+
+    add_deps("cmake")
+    on_install(function (package)
+        import("package.tools.cmake").install(package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                auto foo = GetModuleInstanceHandle();
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "wil/win32_helpers.h"}))
+    end)
