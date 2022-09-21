987d8470bd9b9eab1869cf61c604ee0632b476ef
L-Sun <zover.v@gmail.com>
2022-09-21 21:10:51 +0800

    directx-headers

---
+++ packages/d/directx-headers/xmake.lua
@@ -0,0 +1,16 @@
+package("directx-headers")
+    set_homepage("https://devblogs.microsoft.com/directx/")
+    set_description("Official DirectX headers available under an open source license")
+    set_license("MIT")
+
+    add_urls("https://github.com/microsoft/DirectX-Headers/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/microsoft/DirectX-Headers.git")
+    add_versions("v1.606.4", "fa95f9ab7cd6111a4cbff91f5b440bf79b38179403c9eef22438065b8b08d303")
+
+    add_deps("cmake")
+
+    on_install("windows", "linux", function (package)
+        local configs = {"-DDXHEADERS_BUILD_TEST=OFF", "-DDXHEADERS_BUILD_GOOGLE_TEST=OFF"}
+        import("package.tools.cmake").install(package, configs)
+    end)
+
