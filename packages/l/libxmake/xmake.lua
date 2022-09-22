8905e0616cd417dd4d773ac83ff155c9f17d164d
ruki <waruqi@gmail.com>
2022-09-23 00:38:50 +0800

    fix format error

--- packages/l/libxmake/xmake.lua
+++ packages/l/libxmake/xmake.lua
@@ -62,7 +62,7 @@ package("libxmake")
         end
         table.insert(configs, "--readline=" .. (package:config("readline") and "y" or "n"))
         os.cd("core")
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package, configs, {cxflags = "-Wno-error=format"})
         os.cp("../xmake", package:installdir("share"))
     end)
 
