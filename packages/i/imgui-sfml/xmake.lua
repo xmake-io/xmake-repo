42d52fd516ad4c2e81bfe7a603016d6943233fa4
PoloNX <57038157+PoloNX@users.noreply.github.com>
2022-10-06 16:18:39 +0200

    Fix ImGui-SFML on macosx (#1512)
    * Fix ImGui-SFML on macosx
    
    * added the add_patches()
    
    * fix in the .patch
    
    * Add "" around the hash
    
    * Try to fix the error with the patch
    
    * fix the checksum
    
    * Update xmake.lua
    
    * fix the patch
    
    * Update macosx.patch
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/i/imgui-sfml/xmake.lua
+++ packages/i/imgui-sfml/xmake.lua
@@ -7,6 +7,7 @@ package("imgui-sfml")
              "https://github.com/eliasdaler/imgui-sfml.git")
 
     add_versions("v2.5", "3775c9303f656297f2392e91ffae2021e874ee319b4139c60076d6f757ede109")
+    add_patches("v2.5", path.join(os.scriptdir(), "patches", "v2.5", "macosx.patch"), "1bb74614926dd34034e34a4052794e0b13caa861a780f813ad8a082aac74588f")
 
     add_deps("cmake")
     add_deps("imgui", {system = false, private = true})
