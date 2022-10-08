334035a74a1e47d8fb66a67873114baff41720a3
滴凡 <38903632+ZZLinvec@users.noreply.github.com>
2022-10-08 18:55:45 +0800

    arrange add_includedirs

--- packages/d/dbus/xmake.lua
+++ packages/d/dbus/xmake.lua
@@ -8,13 +8,14 @@ package("dbus")
     add_versions("1.14.2", "9ec5aad6310f79149aa04e8c6bd9e5e2cdca47cf3acec2d23ee9fe06ac7e7a62")
 
     add_deps("expat")
+    add_includedirs("include/dbus-1.0", "lib/dbus-1.0/include")
 
     on_install("windows", "linux", "cross", function (package)
         local configs = {"--enable-tests=no"}
         import("package.tools.autoconf").install(package, configs)
     end)
 
-    add_includedirs("include/dbus-1.0", "lib/dbus-1.0/include")
+    
     
     on_test(function (package)
         assert(package:has_cfuncs("dbus_get_local_machine_id()", {includes = "dbus/dbus.h"}))
