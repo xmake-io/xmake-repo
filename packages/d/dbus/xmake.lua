4075517cc3b1eb9d074a232a710b448ebe900ae4
滴凡 <38903632+ZZLinvec@users.noreply.github.com>
2022-10-08 18:37:34 +0800

    add dbus

---
+++ packages/d/dbus/xmake.lua
@@ -0,0 +1,21 @@
+package("dbus")
+ 
+    set_homepage("https://www.freedesktop.org/wiki/Software/dbus/")
+    set_description("D-Bus is a message bus system, a simple way for applications to talk to one another.")
+    set_license("MIT")
+
+    add_urls("https://gitlab.freedesktop.org/dbus/dbus/-/archive/dbus-1.14.2/dbus-dbus-$(version).tar.gz")
+    add_versions("1.14.2", "9ec5aad6310f79149aa04e8c6bd9e5e2cdca47cf3acec2d23ee9fe06ac7e7a62")
+
+    add_deps("expat")
+
+    on_install("windows", "linux", "cross", function (package)
+        local configs = {"--enable-tests=no"}
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
+    add_includedirs("include/dbus-1.0", "lib/dbus-1.0/include")
+    
+    on_test(function (package)
+        assert(package:has_cfuncs("dbus_get_local_machine_id()", {includes = "dbus/dbus.h"}))
+    end)
