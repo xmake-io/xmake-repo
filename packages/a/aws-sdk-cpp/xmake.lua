b7afbb6ad6e4f731b56d1777d9591fc67720de64
Gavin <gavintang@proton.me>
2022-10-11 01:39:01 +0800

    add on_test function

--- packages/a/aws-sdk-cpp/xmake.lua
+++ packages/a/aws-sdk-cpp/xmake.lua
@@ -5,12 +5,26 @@ package("aws-sdk-cpp")
     add_urls("https://github.com/aws/aws-sdk-cpp.git")
     add_versions("1.9.333", "6ec095fa11379174da20bf1523c93c92503eb8e2")
 
-    add_configs("build_only", {description = 'By default, all SDKS are built, if AWS S3 is required, then set build_only="s3", with multiple SDKS separated by commas.'})
+    add_configs("build_only", {description = 'By default, all SDKS are built, if only AWS S3 is required, then set build_only="s3", with multiple SDKS separated by commas.'})
     add_deps("cmake")
+
     on_install("linux", "macosx", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DBUILD_ONLY=" .. (package:config("build_only") or ""))
+        if package:config("build_only") then
+            table.insert(configs, "-DBUILD_ONLY=" .. package:config("build_only"))
+        end
         import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({
+            test = [[
+              #include <aws/core/Aws.h>
+              static void test() {
+                Aws::SDKOptions options;
+              }
+            ]]
+        }, {configs = {languages = "c++11"}}))
     end)
