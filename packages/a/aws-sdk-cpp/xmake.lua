2470ef387cdf17335bf319e7da31224d199d8f5a
Gavin <gavintang@proton.me>
2022-10-11 15:37:54 +0800

    Add openssl dependencies and test options

--- packages/a/aws-sdk-cpp/xmake.lua
+++ packages/a/aws-sdk-cpp/xmake.lua
@@ -6,14 +6,15 @@ package("aws-sdk-cpp")
     add_versions("1.9.333", "6ec095fa11379174da20bf1523c93c92503eb8e2")
 
     add_configs("build_only", {description = 'By default, all SDKS are built, if only AWS S3 is required, then set build_only="s3", with multiple SDKS separated by commas.'})
-    add_deps("zlib")
-    add_deps("libcurl")
+    add_deps("libcurl", "openssl", "zlib")
     add_deps("cmake")
 
     on_install("linux", "macosx", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_TESTING=" .. (package:config("enable_testing") and "ON" or "OFF"))
+        table.insert(configs, "-DAUTORUN_UNIT_TESTS=" .. (package:config("autorun_unit_tests") and "ON" or "OFF"))
         if package:config("build_only") then
             table.insert(configs, "-DBUILD_ONLY=" .. package:config("build_only"))
         end
