fd791f8900ce44b4a11cd9196678985aae4e6434
ruki <waruqi@gmail.com>
2022-10-12 09:24:43 +0800

    Update xmake.lua

--- packages/a/aws-sdk-cpp/xmake.lua
+++ packages/a/aws-sdk-cpp/xmake.lua
@@ -6,17 +6,15 @@ package("aws-sdk-cpp")
     add_versions("1.9.362", "e9372218a2c8fab756ecaa6e4fefcdb33c3670c1")
 
     add_configs("build_only", {description = 'By default, all SDKS are built, if only AWS S3 is required, then set build_only="s3", with multiple SDKS separated by commas.'})
+
     add_deps("libcurl", "openssl", "zlib")
     add_deps("cmake")
 
     on_install("linux", "macosx", function (package)
-        local configs = {}
+        local configs = {"-DENABLE_TESTING=OFF", "-DAUTORUN_UNIT_TESTS=OFF"}
         table.insert(configs, "-DMINIMIZE_SIZE=ON")
-        table.insert(configs, "-DCMAKE_PREFIX_PATH=" .. package:installdir())
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DENABLE_TESTING=" .. (package:config("enable_testing") and "ON" or "OFF"))
-        table.insert(configs, "-DAUTORUN_UNIT_TESTS=" .. (package:config("autorun_unit_tests") and "ON" or "OFF"))
         if package:config("build_only") then
             table.insert(configs, "-DBUILD_ONLY=" .. package:config("build_only"))
         end
