c961b3611f30b0f5837ef3e27647c68d377c72ca
igavintang <9823338+igavintang@users.noreply.github.com>
2022-10-14 23:59:12 +0800

    Update xmake.lua

--- packages/a/aws-sdk-cpp/xmake.lua
+++ packages/a/aws-sdk-cpp/xmake.lua
@@ -5,13 +5,9 @@ package("aws-sdk-cpp")
     add_urls("https://github.com/aws/aws-sdk-cpp.git")
     add_versions("1.9.362", "e9372218a2c8fab756ecaa6e4fefcdb33c3670c1")
 
-    if is_plat("macosx") then
-        add_frameworks("Foundation", "CoreFoundation", "Security", "SystemConfiguration")
-    end
-
     add_configs("build_only",  {description = 'By default, all SDKS are built, if only AWS S3 is required, then set build_only="s3", with multiple SDKS separated by commas.'})
     add_configs("http_client", {description = 'If disabled, no platform-default http client will be included in the library.', default = true, type = "boolean"})
-    add_configs("encryption",  {description = 'If disabled, no platform-default encryption will be included in the library.', default = true, type = "boolean"})
+    add_configs("encryption",  {description = 'If disabled, no platform-default encryption will be included in the library.', default = false, type = "boolean"})
 
     add_deps("zlib")
     add_deps("cmake")
@@ -26,16 +22,6 @@ package("aws-sdk-cpp")
     end)
 
     on_install("linux", "macosx", function (package)
-        local configs = {"-DENABLE_TESTING=OFF", "-DAUTORUN_UNIT_TESTS=OFF"}
-        table.insert(configs, "-DMINIMIZE_SIZE=ON")
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DNO_HTTP_CLIENT=" .. (package:config("http_client") and "OFF" or "ON"))
-        table.insert(configs, "-DNO_ENCRYPTION=" .. (package:config("encryption") and "OFF" or "ON"))
-        if package:config("build_only") then
-            table.insert(configs, "-DBUILD_ONLY=" .. package:config("build_only"))
-        end
-
         io.replace("cmake/Findcrypto.cmake",
             "if (BUILD_SHARED_LIBS)\n            set(crypto_LIBRARY ${crypto_SHARED_LIBRARY})",
             [[
@@ -46,7 +32,21 @@ package("aws-sdk-cpp")
                         set(crypto_LIBRARY ${crypto_STATIC_LIBRARY})
                     endif()
             ]], {plain = true})
-        import("package.tools.cmake").install(package, configs)
+        local configs = {"-DENABLE_TESTING=OFF", "-DAUTORUN_UNIT_TESTS=OFF"}
+        table.insert(configs, "-DMINIMIZE_SIZE=ON")
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DNO_HTTP_CLIENT=" .. (package:config("http_client") and "OFF" or "ON"))
+        table.insert(configs, "-DNO_ENCRYPTION=" .. (package:config("encryption") and "OFF" or "ON"))
+        if package:config("build_only") then
+            table.insert(configs, "-DBUILD_ONLY=" .. package:config("build_only"))
+        end
+        if package:config("http_client") and package:is_plat("macosx") then
+            local exflags = {"-framework", "CoreFoundation", "-framework", "Security", "-framework", "SystemConfiguration"}
+            import("package.tools.cmake").install(package, configs, {shflags = exflags, ldflags = exflags})
+        else
+            import("package.tools.cmake").install(package, configs)
+        end
     end)
 
     on_test(function (package)
