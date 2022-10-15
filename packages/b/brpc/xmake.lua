0d50c70b7521a94a448fd41b8cc79784c51359ee
Gavin <gavintang@proton.me>
2022-10-15 20:14:22 +0800

    Add brpc

---
+++ packages/b/brpc/xmake.lua
@@ -0,0 +1,39 @@
+package("brpc")
+    set_homepage("https://github.com/apache/incubator-brpc")
+    set_description("brpc is an Industrial-grade RPC framework using C++ Language, which is often used in high performance system such as Search, Storage, Machine learning, Advertisement, Recommendation etc.")
+
+    add_urls("https://github.com/apache/incubator-brpc.git")
+    add_versions("1.3.0", "a90cf60714941632b2986826336a7c50cbd3d530")
+
+    add_deps("leveldb 1.23")
+    add_deps("gflags 2.2.2")
+    add_deps("protobuf-cpp 3.19.4")
+    add_deps("openssl", {system = false})
+    add_deps("cmake")
+
+    on_load(function (package)
+        if package:is_plat("macosx") then
+            package:add("frameworks", "Foundation", "CoreFoundation", "Security", "CoreGraphics", "CoreText")
+            package:add("ldflags", "-Wl,-U,_ProfilerStart", "-Wl,-U,_ProfilerStop")
+        elseif package:is_plat("linux") then
+            package:add("syslinks", "rt")
+            package:add("ldflags", "-static-libstdc++")
+        end
+    end)
+
+    on_install("linux", "macosx", function (package)
+        local configs = {"-DWITH_DEBUG_SYMBOLS=OFF", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"}
+        table.insert(configs, "-DENABLE_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({
+            test = [[
+              #include <brpc/server.h>
+              static void test() {
+                brpc::Server server;
+              }
+            ]]
+        }, {configs = {languages = "c++11"}}))
+    end)
