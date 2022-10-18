0f9b400b654e3c36a5f5d630338dcf066fe3352c
ruki <waruqi@gmail.com>
2022-10-18 17:14:58 +0800

    Update xmake.lua

--- packages/w/websocketpp/xmake.lua
+++ packages/w/websocketpp/xmake.lua
@@ -10,7 +10,7 @@ package("websocketpp")
     add_deps("cmake")
     add_deps("boost")
 
-    on_install(function (package)
+    on_install("linux", "macosx", "windows", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
