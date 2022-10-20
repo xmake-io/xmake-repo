e1837338feef84ebfc69ceb74e0a1c86e877e74a
A2va <49582555+A2va@users.noreply.github.com>
2022-10-20 22:07:36 +0200

    Apply fix from code review

--- packages/s/sentencepiece/xmake.lua
+++ packages/s/sentencepiece/xmake.lua
@@ -9,11 +9,11 @@ package("sentencepiece")
 
     add_versions("v0.1.97", "41c3a07f315e3ac87605460c8bb8d739955bc8e7f478caec4017ef9b7d78669b")
 
-
     add_deps("cmake", "gperftools")
 
     add_configs("external_abseil", {description = "Use external abseil.", default = false, type = "boolean"})
     add_configs("builtin_protobuf", {description = "Use built-in protobuf.", default = true, type = "boolean"})
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true}) 
 
     on_load("windows", "linux", "macosx",function (package) 
         if package:config("external_abseil") then
@@ -23,28 +23,27 @@ package("sentencepiece")
         if not package:config("builtin_protobuf") then 
             package:add("deps", "protobuf-cpp")
         end
-
     end)
 
     on_install("windows", "linux", "macosx", function (package)
         local configs = {}
-
-        if package:is_plat("windows") and package:config("shared") then
-            -- Shared build is impossible on windows
-            -- https://github.com/google/sentencepiece/issues/785
-            print("Shared build is impossible on windows. Roll back to static")
-            package:config_set("shared", false)
-        end
-
-
+        
         table.insert(configs, "-DSPM_ENABLE_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DSPM_USE_EXTERNAL_ABSL=" .. (package:config("external_abseil") and "ON" or "OFF"))
         table.insert(configs, "-DSPM_USE_BUILTIN_PROTOBUF=" .. (package:config("builtin_protobuf") and "ON" or "OFF"))
-
 
         import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:has_cxxtypes("sentencepiece::SentencePieceProcessor", {includes = "sentencepiece_processor.h",configs = {languages = "cxx17"}}))
+        assert(package:check_cxxsnippets({test = [[
+            #include <iostream>
+            void test(int args, char** argv) {
+                sentencepiece::SentencePieceProcessor processor;
+                const auto status = processor.Load("//path/to/model.model");
+                if (!status.ok()) {
+                    std::cerr << status.ToString() << std::endl;
+                }
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "sentencepiece_processor.h"}))
     end)
