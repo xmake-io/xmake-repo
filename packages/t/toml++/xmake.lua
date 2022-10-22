e2be4c843d7df310484dbe8356874fb7917f40b2
Qudix <17361645+Qudix@users.noreply.github.com>
2022-10-22 13:28:10 -0500

    [toml++] Add `v3.2.0`

--- packages/t/toml++/xmake.lua
+++ packages/t/toml++/xmake.lua
@@ -8,6 +8,7 @@ package("toml++")
     add_versions("v2.5.0", "2e246ee126cfb7bd68edd7285d5bb5c8c5296121ce809306ee71cfd6127c76a6")
     add_versions("v3.0.0", "934ad62e82ae5ee67bdef512b39d24ddba45e012fb94e22b39fa1fb192bdabab")
     add_versions("v3.1.0", "dae72714fc356ca1b019298d9e6275cc41ba95546ae722ccdb6795e92f47762e")
+    add_versions("v3.2.0", "aeba776441df4ac32e4d4db9d835532db3f90fd530a28b74e4751a2915a55565")
 
     add_deps("cmake")
 
