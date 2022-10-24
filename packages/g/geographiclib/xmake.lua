80c8ed98ee70eb3babe05561c573bb856105799c
Scially <imhwang@126.com>
2022-10-24 14:31:01 +0000

    add package geographiclib

--- packages/g/geographiclib/xmake.lua
+++ packages/g/geographiclib/xmake.lua
@@ -1,14 +1,12 @@
 package("geographiclib")
-    set_kind("shared")
+    
     set_homepage("https://geographiclib.sourceforge.io/C++/doc/index.html")
-    set_description([[GeographicLib is a small C++ library for
-    1. geodesic and rhumb line calculations;
-    2. conversions between geographic, UTM, UPS, MGRS, geocentric, and local cartesian coordinates;
-    3. gravity (e.g., EGM2008) and geomagnetic field (e.g., WMM2020) calculations.]])
+    set_description("GeographicLib is a small C++ library for geodesic and rhumb line calculations and conversions between geographic, UTM, UPS, MGRS, geocentric")
     set_license("MIT License")
 
     add_urls("https://sourceforge.net/projects/geographiclib/files/distrib-C++/GeographicLib-$(version).zip")
     add_versions("2.1.1", "7887143ae2a6cae08f14f2018508d5a1636273f7f935b1a17d74b56b0252c341")
+    
     add_deps("cmake")
     
     on_install("windows", "macosx", "linux", function (package)
@@ -26,7 +24,6 @@ package("geographiclib")
  
             void test() {
                 const Geodesic& geod = Geodesic::WGS84();
-                // Distance from JFK to LHR
                 double
                     lat1 = 40.6, lon1 = -73.8, // JFK Airport
                     lat2 = 51.6, lon2 = -0.5;  // LHR Airport
