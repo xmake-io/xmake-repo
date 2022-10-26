ea0f64c6b55be05b56d6016d4bbd5db57d5b4a29
L-Sun <zover.v@gmail.com>
2022-10-26 21:04:17 +0800

    update install arch

--- packages/t/tracy/xmake.lua
+++ packages/t/tracy/xmake.lua
@@ -8,7 +8,7 @@ package("tracy")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows|x64", "macosx", "linux|x86_64", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
