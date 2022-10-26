d2415a230042337b62626fa1695abbd59397af90
L-Sun <zover.v@gmail.com>
2022-10-26 21:21:50 +0800

    new package tracy (#1578)
    * new package tracy
    
    * update install arch

---
+++ packages/t/tracy/xmake.lua
@@ -0,0 +1,25 @@
+package("tracy")
+    set_homepage("https://github.com/wolfpld/tracy")
+    set_description("C++ frame profiler")
+
+    add_urls("https://github.com/wolfpld/tracy/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/wolfpld/tracy.git")
+    add_versions("v0.8.2", "4784eddd89c17a5fa030d408392992b3da3c503c872800e9d3746d985cfcc92a")
+
+    add_deps("cmake")
+
+    on_install("windows|x64", "macosx", "linux|x86_64", function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            static void test() {
+                FrameMarkStart("Test start");
+                FrameMarkEnd("Test end");
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = {"Tracy.hpp"}}))
+    end)
