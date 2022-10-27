5325586616a3e2c2e758c119e0c177a537f27851
xq114 <1140735506@qq.com>
2022-10-27 08:49:47 +0800

    check log on macos

--- packages/l/leptonica/xmake.lua
+++ packages/l/leptonica/xmake.lua
@@ -21,6 +21,7 @@ package("leptonica")
 
     on_install("windows", "macosx", "linux", function (package)
         io.replace("CMakeLists.txt", "NOT JP2K", "FALSE", {plain = true})
+        print(io.readfile("CMakeLists.txt"))
         local configs = {"-DSW_BUILD=OFF", "-DCMAKE_FIND_FRAMEWORK=LAST"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
