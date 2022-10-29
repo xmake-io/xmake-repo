baf7271a5e1dc826cc11385c6fc7b77b42c0a3bc
xq114 <1140735506@qq.com>
2022-10-29 14:39:30 +0800

    see log

--- packages/s/spirv-tools/xmake.lua
+++ packages/s/spirv-tools/xmake.lua
@@ -61,6 +61,8 @@ package("spirv-tools")
     end)
 
     on_test(function (package)
+        print(os.host())
+        print(os.arch())
         if not (package:is_plat(os.host()) and package:is_arch(os.arch())) then
             os.runv("spirv-as --help")
             os.runv("spirv-opt --help")
