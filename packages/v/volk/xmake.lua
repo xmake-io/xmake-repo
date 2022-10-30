bd052ebbdb5b6c74a1447252b907b4008a4ebd42
xq114 <1140735506@qq.com>
2022-10-30 15:40:22 +0800

    update volk

--- packages/v/volk/xmake.lua
+++ packages/v/volk/xmake.lua
@@ -4,11 +4,14 @@ package("volk")
     set_description("volk is a meta-loader for Vulkan")
     set_license("MIT")
 
-    add_urls("https://github.com/zeux/volk/archive/$(version).tar.gz",
-            "https://github.com/zeux/volk.git")
+    add_urls("https://github.com/zeux/volk/archive/$(version).tar.gz", {version = function (version)
+        return version:ge("1.3.226") and format("sdk-%s", version:gsub("%+", ".")) or version
+    end})
+    add_urls("https://github.com/zeux/volk.git")
     add_versions("1.2.190", "07f03720b8c70a626c98cc9545350538122bca9f853e6ed20ccad5a25d55fa4b")
     add_versions("1.2.162", "ac4d9d6e88dee5a83ad176e2da57f1989ca2c6df155a0aeb5e18e9471aa4d777")
     add_versions("1.3.204", "7776e7f3c70f199579da33d2ccd7152ca8b96182fa98c31fbe80880cef0fdf70")
+    add_versions("1.3.231+1", "fac8d3d295e88bcc6bfb2b729d2c4babb2ea04ccb39fd918a3471b2d756789b9")
 
     add_deps("vulkan-headers")
 
