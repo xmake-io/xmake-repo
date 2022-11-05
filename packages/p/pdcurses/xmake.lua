343684dbc265f8ea7d034bec1f5cb9df00e3d997
Guyutongxue <guyutongxue@163.com>
2022-11-05 12:57:02 +0800

    Add Windows Console port for pdcurses

--- packages/p/pdcurses/xmake.lua
+++ packages/p/pdcurses/xmake.lua
@@ -9,20 +9,42 @@ package("pdcurses")
     if not is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
+
+    add_configs("port", {description = "Set the target port.", default = "sdl2", values = {"sdl2", "wincon"}})
+
+    on_load(function (package)
+        if package:config("port") == "sdl2" then
+            package:add("deps", "libsdl")
+        else
+            package:add("syslinks", "user32", "advapi32")
+        end
+    end)
     
-    add_deps("libsdl")
 
     on_install("linux", "macosx", "mingw", "windows", function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            add_requires("libsdl")
-            target("pdcurses")
-                set_kind("$(kind)")
-                add_files("pdcurses/*.c", "sdl2/*.c")
-                add_includedirs(".", "sdl2")
-                add_headerfiles("*.h", "sdl2/*.h")
-                add_packages("libsdl")
-        ]])
+        if package:config("port") == "wincon" then
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                add_requires("libsdl")
+                target("pdcurses")
+                    set_kind("$(kind)")
+                    add_files("pdcurses/*.c", "wincon/*.c")
+                    add_includedirs(".", "wincon")
+                    add_headerfiles("*.h", "wincon/*.h")
+                    add_defines("PDC_WIDE", "PDC_FORCE_UTF8")
+            ]])
+        else
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                add_requires("libsdl")
+                target("pdcurses")
+                    set_kind("$(kind)")
+                    add_files("pdcurses/*.c", "sdl2/*.c")
+                    add_includedirs(".", "sdl2")
+                    add_headerfiles("*.h", "sdl2/*.h")
+                    add_packages("libsdl")
+            ]])
+        end
         local configs = {}
         if package:config("shared") then 
             configs.kind = "shared"
