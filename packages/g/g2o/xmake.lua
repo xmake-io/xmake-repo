8caccd7c1adb3b9b1e58e7bf5255bf2fc8d59d62
ruki <waruqi@gmail.com>
2022-11-10 19:00:25 +0800

    Update xmake.lua

--- packages/g/g2o/xmake.lua
+++ packages/g/g2o/xmake.lua
@@ -19,20 +19,13 @@ package("g2o")
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <g2o/core/sparse_optimizer.h>
-            #include <g2o/core/block_solver.h>
-            #include <g2o/core/robust_kernel.h>
-            #include <g2o/core/robust_kernel_impl.h>
-            #include <g2o/core/optimization_algorithm_levenberg.h>
-            #include <g2o/solvers/cholmod/linear_solver_cholmod.h>
-            #include <g2o/types/slam3d/se3quat.h>
-            #include <g2o/types/sba/types_six_dof_expmap.h>
+            #include "g2o/core/factory.h"
+            #include "g2o/core/optimization_algorithm_factory.h"
+            #include "g2o/core/sparse_optimizer.h"
+            #include "g2o/stuff/command_args.h"
+
             void test() {
                 g2o::SparseOptimizer optimizer;
-                g2o::BlockSolver_6_3::LinearSolverType* linearSolver = new g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType> ();
-                g2o::BlockSolver_6_3* block_solver = new g2o::BlockSolver_6_3(linearSolver);
-                g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg(block_solver);
-                optimizer.setAlgorithm(algorithm);
                 optimizer.setVerbose(false);
             }
         ]]}, {configs = {languages = "c++14"}}))
