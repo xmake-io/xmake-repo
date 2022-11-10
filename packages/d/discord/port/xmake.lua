4a1c0ebddcbdc8565ad6b239669eed9949051c03
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-11-10 21:25:21 +0100

    Update discord game sdk to 3.2.1
    support linux
    support macos

--- packages/d/discord/port/xmake.lua
+++ packages/d/discord/port/xmake.lua
@@ -1,10 +1,10 @@
 set_xmakever("2.5.1")
-set_languages("cxx11")
+set_languages("cxx14")
 
 add_rules("mode.debug", "mode.release")
 
 target("discordcpp")
-    set_kind("static")
+    set_kind("$(kind)")
 
     add_includedirs("cpp")
     add_headerfiles("cpp/(*.h)")
--- packages/d/discord/xmake.lua
+++ packages/d/discord/xmake.lua
@@ -4,22 +4,46 @@ package("discord")
     set_description("Whether you’re part of a school club, gaming group, worldwide art community, or just a handful of friends that want to spend time together, Discord makes it easy to talk every day and hang out more often.")
 
     add_versions("2.5.6", "426eb5fa70647d884f461c63825b63668349efb4bc68a16e70bc4a24e119b92e")
+    add_versions("3.2.1", "6757bb4a1f5b42aa7b6707cbf2158420278760ac5d80d40ca708bb01d20ae6b4")
+
+    add_patches("2.5.6", path.join(os.scriptdir(), "patches", "2.5.6", "add_include_cstdint_to_typeh.patch"), "2d36408167da601b5bb19066a951dbcac4e9783dd3b7ef8bc5ea9c3e48391d1c ")
+    add_patches("3.2.1", path.join(os.scriptdir(), "patches", "3.2.1", "add_include_cstdint_to_typeh.patch"), "0ae6618dd5bf2e0149bbb3959dcd2f6df5b2a8e7295b7153eea2fd1e6d389ba0")
 
     set_urls("https://dl-game-sdk.discordapp.net/$(version)/discord_game_sdk.zip")
 
-    on_install("windows", function (package)
+    on_install("windows", "linux", "macosx", function (package)
         os.cp("cpp/*.h", package:installdir("include"))
         os.cp("c/*.h", package:installdir("include"))
-        os.cp("Release/*.lib", package:installdir("lib"))
-        if package:is_arch("x64") then
-            os.cp("lib/x86_64/discord_game_sdk.dll", package:installdir("bin"))
-            os.cp("lib/x86_64/discord_game_sdk.dll.lib", package:installdir("lib"))
-        else
-            os.cp("lib/x86/discord_game_sdk.dll", package:installdir("bin"))
-            os.cp("lib/x86/discord_game_sdk.dll.lib", package:installdir("lib"))
-        end
 
         local configs = {}
+        if package:is_plat("windows") then
+            if package:is_arch("x64") then
+                os.cp("lib/x86_64/discord_game_sdk.dll", package:installdir("bin"))
+                os.cp("lib/x86_64/discord_game_sdk.dll.lib", package:installdir("lib"))
+                configs.linkdirs = "lib/x86_64"
+            else
+                os.cp("lib/x86/discord_game_sdk.dll", package:installdir("bin"))
+                os.cp("lib/x86/discord_game_sdk.dll.lib", package:installdir("lib"))
+                configs.linkdirs = "lib/x86"
+            end
+            configs.links = "discord_game_sdk.dll"
+        elseif package:is_plat("linux") then
+                os.cp("lib/x86_64/discord_game_sdk.so", package:installdir("lib"))
+                configs.linkdirs = "lib/x86_64"
+                configs.links = "discord_game_sdk"
+        elseif package:is_plat("macosx") then
+            local version = package:version()
+            if ((version:major() > 3) or (version:major() == 3 and version:minor() >= 2)) and package:is_arch("arm64") then
+                os.cp("lib/aarch64/discord_game_sdk.dylib", package:installdir("lib"))
+                configs.linkdirs = "lib/aarch64"
+
+            else
+                os.cp("lib/x86_64/discord_game_sdk.dylib", package:installdir("lib"))
+                configs.linkdirs = "lib/x86_64"
+            end
+            configs.links = "discord_game_sdk"
+        end
+
         if package:config("shared") then
             configs.kind = "shared"
         end
