c43c5952c01b347b1c733b1cad2f7e3ac60ac9fc
jingkaimori <jingkaimori@gmail.com>
2022-11-16 13:53:35 +0800

    change multiple typos

--- packages/p/pdfhummus/xmake.lua
+++ packages/p/pdfhummus/xmake.lua
@@ -5,23 +5,32 @@ package("pdfhummus")
     set_description("High performance library for creating, modiyfing and parsing PDF files in C++ ")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/galkahana/PDF-Write/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/galkahana/PDF-Write.git")
+    add_urls("https://github.com/galkahana/PDF-Writer/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/galkahana/PDF-Writer.git")
     add_versions("4.1", "0C0D860B0ECEA928709B9E4642FA21926EB2F626F702699C3B87AFA2965B4857")
     
     add_deps("cmake")
     add_deps("zlib")
     add_deps("freetype")
-    add_deps("libtiff")
-    add_deps("libpng")
-    add_deps("libjpeg-turbo")
+    
+    add_configs("libtiff", {description = "Supporting tiff image", default = true, type = "boolean"})
+    add_configs("libjpeg", {description = "Support DCT encoding", default = true, type = "boolean"})
+    add_configs("bzip2", {description = "Support png image", default = true, type = "boolean"})
+
+    on_load(function (package)
+        local function add_dep(conf, pkg)
+            if package:config(conf) then
+                package:add("deps", pkg or conf)
+            end
+        end
+        add_dep("libtiff")
+        add_dep("libpng")
+        add_dep("libjpeg","libjpeg-turbo")
+    end)
 
     on_install(function (package)
-        local configs = {"-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release")}
+        local configs = {"-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"), "-DUSE_BUNDLED=FALSE"}
         import("package.tools.cmake").install(package, configs)
-        if not package:config("shared") then 
-            os.rm(package:installdir("bin/*.dll"))
-        end
     end)
 
     on_test(function (package)
