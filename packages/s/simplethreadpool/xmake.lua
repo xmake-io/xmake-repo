37558dde1a0c122093d6598d850f0de330e76bd6
ruki <waruqi@gmail.com>
2022-11-22 10:08:44 +0800

    Update xmake.lua

--- packages/s/simplethreadpool/xmake.lua
+++ packages/s/simplethreadpool/xmake.lua
@@ -5,6 +5,12 @@ package("simplethreadpool")
 
     add_urls("https://github.com/romch007/simplethreadpool.git")
 
+    add_versions("2022.11.18", "e0eabdf732394a810f1dd1eeec0efee4954bf5b7")
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
+
     on_load(function (package)
         if not package:config("shared") then
             package:add("defines", "SIMPLETHREADPOOL_STATIC")
@@ -30,3 +36,4 @@ package("simplethreadpool")
           }
         ]]}, {configs = {languages = "c++17"}, includes = "simplethreadpool/pool.hpp"}))
     end)
+
