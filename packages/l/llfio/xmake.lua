2a737c1e7bf59d7e24fb91dfc404af99adf8a1b9
ruki <waruqi@gmail.com>
2022-11-22 09:53:55 +0800

    Update xmake.lua

--- packages/l/llfio/xmake.lua
+++ packages/l/llfio/xmake.lua
@@ -20,6 +20,7 @@ package("llfio")
     on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
     
