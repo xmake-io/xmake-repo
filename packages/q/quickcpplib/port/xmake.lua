b6c8f964bb3b2f416b9190cc649f25c6dd53f54c
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-11-24 13:56:06 +0100

    Improve quickcpplib
    Convert quickcpplib to XMake

---
+++ packages/q/quickcpplib/port/xmake.lua
@@ -0,0 +1,5 @@
+target("quickcpplib")
+    set_kind("headeronly")
+    add_headerfiles("include/(quickcpplib/**.hpp)")
+    add_headerfiles("include/(quickcpplib/**.h)")
+    add_includedirs("include")
