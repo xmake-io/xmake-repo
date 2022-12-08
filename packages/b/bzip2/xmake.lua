e6cebae23e71816e94554a968775ae795bae9547
SirLynix <lynix680@gmail.com>
2022-12-08 08:20:35 +0100

    Fix bzip2 tests

--- packages/b/bzip2/xmake.lua
+++ packages/b/bzip2/xmake.lua
@@ -16,8 +16,8 @@ package("bzip2")
 
     on_install(function (package)
         local configs = {}
-        if not package:is_plat("cross", "iphoneos", "android") then
-            configs.enable_tools = true
+        configs.enable_tools = true
+        if not package:is_plat("iphoneos", "android") then
             package:addenv("PATH", "bin")
         end
 
@@ -26,7 +26,7 @@ package("bzip2")
     end)
 
     on_test(function (package)
-        if not package:is_plat("cross", "iphoneos", "android") then
+        if not package:is_cross() then
             os.vrun("bunzip2 --help")
             os.vrun("bzcat --help")
             os.vrun("bzip2 --help")
