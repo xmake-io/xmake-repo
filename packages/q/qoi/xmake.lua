0de894d4b0c8b3c67e6f1580d8c2867d44c200c0
Hoildkv <42310255+xq114@users.noreply.github.com>
2022-12-12 17:14:18 +0800

    update qoi, utf8proc and libdeflate (#1674)
    * update qoi
    
    * update utf8proc
    
    * update libdeflate
    
    * add constraint

--- packages/q/qoi/xmake.lua
+++ packages/q/qoi/xmake.lua
@@ -1,11 +1,13 @@
 package("qoi")
 
     set_kind("library", {headeronly = true})
-    set_homepage("https://github.com/phoboslab/qoi")
+    set_homepage("https://qoiformat.org/")
     set_description("The Quite OK Image Format for fast, lossless image compression")
+    set_license("MIT")
 
     add_urls("https://github.com/phoboslab/qoi.git")
     add_versions("2021.12.22", "44fe081388c60e7618f49486865b992e08ce4de4")
+    add_versions("2022.11.17", "660839cb2c51d6b5f62221f8ef98662fd40e42d2")
 
     on_install(function (package)
         os.cp("qoi.h", package:installdir("include"))
--- packages/u/utf8proc/xmake.lua
+++ packages/u/utf8proc/xmake.lua
@@ -6,19 +6,19 @@ package("utf8proc")
 
     add_urls("https://github.com/JuliaStrings/utf8proc/archive/refs/tags/$(version).tar.gz",
              "https://github.com/JuliaStrings/utf8proc.git")
-    add_versions('v2.7.0', '4bb121e297293c0fd55f08f83afab6d35d48f0af4ecc07523ad8ec99aa2b12a1')
-
-    if is_plat("windows", "mingw") then
-        add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
-        add_configs("vs_runtime", {description = "Set vs compiler runtime.", default = "MD", readonly = true})
-    end
+    add_versions("v2.7.0", "4bb121e297293c0fd55f08f83afab6d35d48f0af4ecc07523ad8ec99aa2b12a1")
+    add_versions("v2.8.0", "a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc")
 
     add_deps("cmake")
+    on_load("windows", "macosx", "linux", "android", "mingw", "bsd", function (package)
+        if not package:config("shared") then
+            package:add("defines", "UTF8PROC_STATIC")
+        end
+    end)
 
-    on_install(function (package)
-        local configs = {
-            "-DUTF8PROC_ENABLE_TESTING=OFF",
-        }
+    on_install("windows", "macosx", "linux", "android", "mingw", "bsd", function (package)
+        local configs = {"-DUTF8PROC_ENABLE_TESTING=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
