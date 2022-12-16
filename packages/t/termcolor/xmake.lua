d8f6dac899ca2c60c4c3bc44a91c9f62efc494dd
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-12-16 00:11:02 +0000

    add termcolor

---
+++ packages/t/termcolor/xmake.lua
@@ -0,0 +1,25 @@
+local parent_url = "https://github.com/ikalnytskyi/termcolor"
+
+package("termcolor")
+	set_kind("library", { headeronly = true })
+	set_homepage(parent_url)
+	set_description("Termcolor is a header-only C++ library for printing colored messages to the terminal. Written just for fun with a help of the Force.")
+	set_license("BSD-3-Clause")
+
+	add_urls(parent_url.."/archive/refs/tags/v$(version).zip",
+					 parent_url..".git")
+	add_versions("2.1.0","435994c32557674469404cb1527c283fdcf45746f7df75fd2996bb200d6a759f")
+
+	on_install(function(package)
+		os.cp("include/termcolor/termcolor.hpp", package:installdir("include/termcolor"))
+	end)
+
+	on_test(function(package)
+		assert(package:check_cxxsnippets({ test = [[
+		void test() {
+			std::cout << termcolor::red
+				<< "Hello World!"
+				<< termcolor::reset;
+		}
+		]] }, { includes = "termcolor/termcolor.hpp" }))
+	end)
