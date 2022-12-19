35d1c2802a85b202576b74c31d4dec609108e6d3
L-Sun <zover.v@gmail.com>
2022-12-19 22:14:37 +0800

    fix typo

--- packages/u/usd/xmake.lua
+++ packages/u/usd/xmake.lua
@@ -6,15 +6,17 @@ package("usd")
              "https://github.com/PixarAnimationStudios/USD.git")
     add_versions("v22.11", "f34826475bb9385a9e94e2fe272cc713f517b987cbea15ee6bbc6b21db19aaae")
 
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean")
+
     add_deps("cmake", "boost")
     -- usd only support tbb 2022 now https://github.com/PixarAnimationStudios/USD/issues/1471
     add_deps("tbb 2020.3")
 
-    on_load("windows", function (package)
-        package:add("defines", "NOMINMAX")
-    end)
+    if is_plat("windows") then
+        add_defines("NOMINMAX")
+    end
 
-    on_install("linux", "macosx", "windows", function (package)
+    on_install("linux", "macosx", "windows|x64", function (package)
         local configs = {
             "-DPXR_BUILD_ALEMBIC_PLUGIN=OFF",
             "-DBoost_NO_BOOST_CMAKE=OFF",
@@ -28,12 +30,8 @@ package("usd")
             "-DPXR_BUILD_TUTORIALS=OFF",
             "-DPXR_BUILD_USD_TOOLS=OFF",
         }
-        if is_plat("windows") then
-            table.insert(configs, "PXR_USD_WINDOWS_DLL_PATH")
-        end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-
         import("package.tools.cmake").install(package, configs)
     end)
 
@@ -42,5 +40,5 @@ package("usd")
             void test () {
                 auto stage = pxr::UsdStage::CreateInMemory();
             }
-        ]]}, {configs = {languages = "c++17"}, includes="pxr/usd/usd/stage.h"}))
+        ]]}, {configs = {languages = "c++17"}, includes = "pxr/usd/usd/stage.h"}))
     end)
