ad7d8c15d057b52b94cec129af400d0886e26bf2
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-12-19 08:35:15 +0000

    finishing

--- packages/a/argparse/xmake.lua
+++ packages/a/argparse/xmake.lua
@@ -4,7 +4,7 @@ package("argparse")
     set_description("A single header argument parser for C++17")
     set_license("MIT")
 
-    add_urls("https://github.com/p-ranav/argparse/archive/refs/tags/v.zip",
+    add_urls("https://github.com/p-ranav/argparse/archive/refs/tags/v$(version).zip",
              "https://github.com/p-ranav/argparse.git")
     add_versions("2.6", "ce4e58d527b83679bdcc4adfa852af7ec9df16b76c11637823ef642cb02d2618")
     add_versions("2.7", "58cf098fd195895aeb9b9120d96f1e310994b2f44d72934c438ec91bf2191f46  -")
