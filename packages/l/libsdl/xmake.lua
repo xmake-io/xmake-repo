56a635f51d2359d1f56d679a2c7102060f7d1875
Jérôme Leclercq <lynix680@gmail.com>
2022-12-22 15:28:13 +0100

    Remove debug line

--- packages/l/libsdl/xmake.lua
+++ packages/l/libsdl/xmake.lua
@@ -177,7 +177,6 @@ package("libsdl")
             opt = opt or {}
             opt.cflags = {"-sUSE_SDL=0"}
         end
-        table.insert(configs, "--trace-expand")
         import("package.tools.cmake").install(package, configs, opt)
     end)
 
