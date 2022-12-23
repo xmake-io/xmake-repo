5d0fef6fb4ca8e9f9bc323044da80a1ab965d36b
A2va <49582555+A2va@users.noreply.github.com>
2022-12-23 14:42:20 +0100

    Disable shared build

--- packages/c/cpuinfo/xmake.lua
+++ packages/c/cpuinfo/xmake.lua
@@ -11,7 +11,6 @@ package("cpuinfo")
     on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
