2a450c068f1d0d675f210a2c4d16376337079033
ruki <waruqi@gmail.com>
2023-01-03 22:48:01 +0800

    format code

--- packages/p/pdfhummus/xmake.lua
+++ packages/p/pdfhummus/xmake.lua
@@ -77,7 +77,7 @@ package("pdfhummus")
             using namespace std;
             using namespace PDFHummus;
             void test() {
-	 	        PDFWriter pdfWriter;
+                PDFWriter pdfWriter;
                 pdfWriter.Reset();
             }
         ]]}, {configs = {languages = "c++11"}}))
