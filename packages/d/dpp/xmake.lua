4198865479e35b54ef2dfe034824289596b3b4b8
Louis Bailleau <louis.bailleau@free.fr>
2023-01-04 16:18:52 +0100

    dpp: bump version to v10.0.22

--- packages/d/dpp/xmake.lua
+++ packages/d/dpp/xmake.lua
@@ -10,6 +10,8 @@ package("dpp")
     -- Keep all versions with the same major number. If D++ switch to a new major version,
     -- keep only the latest previous versions.
     -- i.e. if D++ switch to v11.x.x, keep only the latest v10.x.x and delete the rest.
+    add_versions("v10.0.22", "a777e0aecc2e13325bae5088ba34bd7dc8ec4f97ed7d456fb8412e891ddd9d2d")
+
     add_versions("v10.0.21", "8ef2bb75f16b80d742a99c3a18ab5a2a57bce74238518af9b9aca670c2d7034b")
 
     add_versions("v10.0.20", "c4a7481c714c27d9c1411c668212e433fa5f6631a933676269c866295bd4ef73")
