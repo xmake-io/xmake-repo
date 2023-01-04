e0afa6fd0830d690d05beea3974e2ac1c6899c90
ruki <waruqi@gmail.com>
2023-01-04 14:37:54 +0800

    Update xmake.lua

--- packages/j/json.h/xmake.lua
+++ packages/j/json.h/xmake.lua
@@ -1,7 +1,7 @@
 package("json.h")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/sheredom/json.h")
-    set_description("🗄️ single header json parser for C and C++")
+    set_description("single header json parser for C and C++")
 
     add_urls("https://github.com/sheredom/json.h.git")
     add_versions("2022.11.27", "06aa5782d650e7b46c6444c2d0a048c0a1b3a072")
