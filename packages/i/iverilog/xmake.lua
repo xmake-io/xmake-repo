87865926676d4a6039da310acc74cf03bd90e573
ruki <waruqi@gmail.com>
2023-01-10 23:02:36 +0800

    add iverilog for windows

--- packages/i/iverilog/xmake.lua
+++ packages/i/iverilog/xmake.lua
@@ -3,10 +3,18 @@ package("iverilog")
     set_homepage("https://steveicarus.github.io/iverilog/")
     set_description("Icarus Verilog")
 
-    add_urls("https://github.com/steveicarus/iverilog.git")
-    add_versions("2023.1.7", "45bd0968c3d6d5b96a7ac7c2c1b0557cc229e568")
+    if is_plat("windows") then
+        add_urls("https://github.com/xmake-mirror/iverilog-windows/releases/download/$(version)/iverilog-$(version).zip")
+        add_versions("2022.6.11", "6ce9411bf9468c43df70ae777e6727d47ae4f56413e772455fa93356597b6b7b")
+    else
+        add_urls("https://github.com/steveicarus/iverilog.git")
+        add_versions("2023.1.7", "45bd0968c3d6d5b96a7ac7c2c1b0557cc229e568")
+        add_deps("autoconf", "automake", "libtool", "flex", "bison", "gperf")
+    end
 
-    add_deps("autoconf", "automake", "libtool", "flex", "bison", "gperf")
+    on_install("windows", "mingw", function (package)
+        os.cp("*", package:installdir())
+    end)
 
     on_install("macosx", "linux", function (package)
         local configs = {}
