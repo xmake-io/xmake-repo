2ac0231882855dff56950c7b5f31552ff64ad288
ruki <waruqi@gmail.com>
2023-01-13 22:38:16 +0800

    improve flex deps for windows

--- packages/f/flex/xmake.lua
+++ packages/f/flex/xmake.lua
@@ -10,19 +10,19 @@ package("flex")
     add_versions("2.6.4", "e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995")
 
     if is_plat("windows") then
-        add_deps("winflexbison")
+        add_deps("winflexbison", {private = true})
     elseif is_plat("linux") then
         add_deps("m4")
     end
 
-    on_load("macosx", "linux", "bsd", function (package)
+    on_load("macosx", "linux", "bsd", "windows", function (package)
         -- we always set it, because flex may be modified as library
         -- by add_deps("flex", {kind = "library"})
         package:addenv("PATH", "bin")
     end)
 
     on_install("windows", function (package)
-        -- handled by winflexbison
+        os.cp(path.join(package:dep("winflexbison"):installdir(), "*"), package:installdir())
     end)
 
     on_install("macosx", "linux", "bsd", "android", "iphoneos", "cross", function (package)
