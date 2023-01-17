4d278a1b9d12e74fd2a4204909659d7701bca382
ruki <waruqi@gmail.com>
2023-01-17 23:49:36 +0800

    disable wasm

--- packages/s/sparsepp/xmake.lua
+++ packages/s/sparsepp/xmake.lua
@@ -7,7 +7,7 @@ package("sparsepp")
              "https://github.com/greg7mdp/sparsepp.git")
     add_versions("1.22", "5516c814fe56c692aaa36f49e696f4a6292f04b5ae79f4ab7bd121e2cc48b917")
 
-    on_install("linux", "macosx", "bsd", "windows", "android", "iphoneos", "cross", "mingw" function (package)
+    on_install("linux", "macosx", "bsd", "windows", "android", "iphoneos", "cross", "mingw", function (package)
         os.cp("sparsepp/*.h", package:installdir("include/sparsepp"))
     end)
 
