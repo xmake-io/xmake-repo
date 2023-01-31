b4c202b37673412511528bd2bd8b1032fd700773
xq114 <1140735506@qq.com>
2023-01-31 13:45:17 +0800

    fix test again

--- packages/m/meshoptimizer/xmake.lua
+++ packages/m/meshoptimizer/xmake.lua
@@ -24,6 +24,7 @@ package("meshoptimizer")
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
+            #include <vector>
             struct Vertex
             {
                 float px, py, pz;
