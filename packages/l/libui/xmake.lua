ddeb8514ffe5ed71948b7f548ac7ffbd257a15bf
ruki <waruqi@gmail.com>
2023-01-31 11:00:31 +0800

    Update xmake.lua

--- packages/l/libui/xmake.lua
+++ packages/l/libui/xmake.lua
@@ -6,49 +6,17 @@ package("libui")
     add_versions("2022.12.3", "8c82e737eea2f8ab3667e227142abd2fd221f038")
 
     add_deps("meson", "ninja")
+    
+    if is_plat("macosx") then
+        add_frameworks("CoreGraphics", "CoreText", "Foundation", "AppKit")
+    end
 
-    on_install(function (package)
-        local config = { "-Dexamples=false", "-Dtests=false" }
-        table.insert(config, "--default-library=" .. (package:config("shared") and "shared" or "static"))
-
-        import("package.tools.meson").install(package, config)
+    on_install("linux", "macosx", "windows", function (package)
+        local configs = {"-Dexamples=false", "-Dtests=false"}
+        table.insert(configs, "--default-library=" .. (package:config("shared") and "shared" or "static"))
+        import("package.tools.meson").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:check_csnippets({test = [[
-        #include <stdio.h>
-        #include <ui.h>
-
-        int onClosing(uiWindow *w, void *data)
-        {
-            uiQuit();
-            return 1;
-        }
-
-        void test(void)
-        {
-            uiInitOptions o = {0};
-            const char *err;
-            uiWindow *w;
-            uiLabel *l;
-
-            err = uiInit(&o);
-            if (err != NULL) {
-                fprintf(stderr, "Error initializing libui-ng: %s\n", err);
-                uiFreeInitError(err);
-                return;
-            }
-
-            // Create a new window
-            w = uiNewWindow("Hello World!", 300, 30, 0);
-            uiWindowOnClosing(w, onClosing, NULL);
-
-            l = uiNewLabel("Hello, World!");
-            uiWindowSetChild(w, uiControl(l));
-
-            uiControlShow(uiControl(w));
-            uiMain();
-            uiUninit();
-        }
-        ]]}, { configs = { languages = "c99" }, includes = "ui.h" }))
+        assert(package:has_cfuncs("uiInit", {includes = "ui.h"}))
     end)
