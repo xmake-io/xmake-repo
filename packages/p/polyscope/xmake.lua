bd72f171fb9122c4a6e25b0280738d331daed9ed
xq114 <1140735506@qq.com>
2023-02-01 16:10:30 +0800

    fix polyscope

--- packages/p/polyscope/xmake.lua
+++ packages/p/polyscope/xmake.lua
@@ -8,7 +8,7 @@ package("polyscope")
              "https://github.com/nmwsharp/polyscope.git")
     add_versions("v1.3.0", "c0d3594b1c818c6e7efe2c2589d71f5e254db383d36a92555aa909a2114f12d4")
 
-    add_patches("1.3.0", path.join(os.scriptdir(), "patches", "v1.3.0", "deps.patch"), "be68f899c68450d761d2db5658df57d718953e4b98b9fa6eee0e30bd2cd6aa5f")
+    add_patches("1.3.0", path.join(os.scriptdir(), "patches", "v1.3.0", "deps.patch"), "2d183713be791deddcfb48e4546793f584071eb1aa7a045e03c7ceed841470c8")
     add_patches("1.3.0", path.join(os.scriptdir(), "patches", "v1.3.0", "imgui.patch"), "e7865d6de85103098cdc25ab8a0e94dd26fb5f718609f20093b91891cc8f21e1")
 
     if is_plat("windows") then
@@ -17,14 +17,13 @@ package("polyscope")
 
     add_deps("cmake")
     add_deps("imgui <=1.86", {configs = {glfw_opengl3 = true}})
-    add_deps("glad", "glfw", "glm", "happly", "nlohmann_json")
+    add_deps("glad", "glfw", "glm", "happly", "nlohmann_json", "stb")
     on_install("windows", "macosx", "linux", function (package)
-        io.replace("deps/CMakeLists.txt", "add_subdirectory(glfw)", "find_package(GLFW REQUIRED)", {plain = true})
-        io.replace("deps/CMakeLists.txt", "add_subdirectory(imgui)", "find_package(imgui REQUIRED)", {plain = true})
+        os.mv("deps/stb/stb_impl.cpp", "deps/stb_impl.cpp")
         local configs = {"-DPOLYSCOPE_BACKEND_OPENGL_MOCK=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = {"imgui", "glad", "glfw", "glm", "happly", "nlohmann_json"}})
+        import("package.tools.cmake").install(package, configs, {packagedeps = {"imgui", "glad", "glfw", "glm", "happly", "nlohmann_json", "stb"}})
         os.cp("include/polyscope", package:installdir("include"))
     end)
 
@@ -35,5 +34,5 @@ package("polyscope")
                 polyscope::init();
                 polyscope::show();
             }
-        ]]}, {configs = {language = "cxx17"}}))
+        ]]}, {configs = {languages = "c++17"}}))
     end)
