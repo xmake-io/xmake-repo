08874bc1ac5bb55e4de67c0d336f5d4edca676f0
xq114 <1140735506@qq.com>
2023-02-02 00:21:21 +0800

    update cli11

--- packages/c/cli11/xmake.lua
+++ packages/c/cli11/xmake.lua
@@ -6,7 +6,7 @@ package("cli11")
 
     add_urls("https://github.com/CLIUtils/CLI11/archive/refs/tags/$(version).tar.gz",
              "https://github.com/CLIUtils/CLI11.git")
-
+    add_versions("v2.3.2", "aac0ab42108131ac5d3344a9db0fdf25c4db652296641955720a4fbe52334e22")
     add_versions("v2.2.0", "d60440dc4d43255f872d174e416705f56ba40589f6eb07727f76376fb8378fd6")
 
     on_install("windows", "linux", "macosx", function (package)
