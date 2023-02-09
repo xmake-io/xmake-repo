9383e50317cc6e182a0e811dc43591fdca309c69
Hoildkv <42310255+xq114@users.noreply.github.com>
2023-02-09 18:00:46 +0800

    update flatbuffers, openssl and libfabric (#1800)
    * update flatbuffers
    
    * update openssl
    
    * update libfabric
    
    * add patches

--- packages/f/flatbuffers/xmake.lua
+++ packages/f/flatbuffers/xmake.lua
@@ -2,17 +2,20 @@ package("flatbuffers")
 
     set_homepage("http://google.github.io/flatbuffers/")
     set_description("FlatBuffers is a cross platform serialization library architected for maximum memory efficiency.")
+    set_license("Apache-2.0")
 
-    add_urls("https://github.com/google/flatbuffers/archive/v$(version).zip")
-    add_versions("1.12.0", "4b8b21adbfe8a74b90604161afcf87c125a26b86c99327e7a04525080606536c")
-    add_versions("2.0.0", "ffd68aebdfb300c9e82582ea38bf4aa9ce65c77344c94d5047f3be754cc756ea")
+    add_urls("https://github.com/google/flatbuffers/archive/$(version).zip",
+             "https://github.com/google/flatbuffers.git")
+    add_versions("v1.12.0", "4b8b21adbfe8a74b90604161afcf87c125a26b86c99327e7a04525080606536c")
+    add_versions("v2.0.0", "ffd68aebdfb300c9e82582ea38bf4aa9ce65c77344c94d5047f3be754cc756ea")
+    add_versions("v23.1.21", "48597d6a6f8ca67a02ae8d8494b3bfc9136eb93da60a538d5bfc024f7c564f97")
 
     add_deps("cmake")
     on_install("windows", "linux", "macosx", "mingw", "android", "iphoneos", function(package)
         local configs = {"-DFLATBUFFERS_BUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DFLATBUFFERS_BUILD_SHAREDLIB=" .. (package:config("shared") and "ON" or "OFF"))
-        if is_plat("android", "iphoneos") then
+        if package:is_cross() then
             table.insert(configs, "-DFLATBUFFERS_BUILD_FLATC=OFF")
             table.insert(configs, "-DFLATBUFFERS_BUILD_FLATHASH=OFF")
         end
@@ -21,7 +24,7 @@ package("flatbuffers")
     end)
 
     on_test(function(package)
-        if is_plat("windows", "linux", "macosx", "mingw@windows") then
+        if not package:is_cross() then
             os.vrun("flatc --version")
         end
         assert(package:check_cxxsnippets({test = [[
