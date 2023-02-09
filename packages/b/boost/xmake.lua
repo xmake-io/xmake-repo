7cd6b4bd2c4c9571f324a64f510ae59fd2ef8920
ruki <waruqi@gmail.com>
2023-02-10 00:54:06 +0800

    improve boost with python

--- packages/b/boost/xmake.lua
+++ packages/b/boost/xmake.lua
@@ -72,7 +72,7 @@ package("boost")
     add_configs("all",          { description = "Enable all library modules support.",  default = false, type = "boolean"})
     add_configs("multi",        { description = "Enable multi-thread support.",  default = true, type = "boolean"})
     for _, libname in ipairs(libnames) do
-        add_configs(libname,    { description = "Enable " .. libname .. " library.", default = (libname == "filesystem"), type = "boolean"})
+        add_configs(libname,    { description = "Enable " .. libname .. " library.", default = (libname == "filesystem" or libname == "python"), type = "boolean"})
     end
 
     on_load(function (package)
