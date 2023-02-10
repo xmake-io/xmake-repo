33bee021c57c1dd52d23bbd78d73998d60d3ff40
ruki <waruqi@gmail.com>
2023-02-11 00:35:44 +0800

    improve llvm fetch

--- packages/l/llvm/components/libunwind.lua
+++ packages/l/llvm/components/libunwind.lua
@@ -1,9 +1,12 @@
-local links = {
-    "unwind"
-}
+function get_links(package)
+    local links = {
+        "unwind"
+    }
+    return links
+end
 
 function main(package, component)
-    component:add("links", links)
+    component:add("links", get_links(package))
 end
 
 
