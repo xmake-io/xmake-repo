a138fc518bee0da3b562330e7f316295e49d769a
ruki <waruqi@gmail.com>
2023-02-10 23:00:35 +0800

    improve apr

--- packages/a/apr/xmake.lua
+++ packages/a/apr/xmake.lua
@@ -26,6 +26,12 @@ package("apr")
             table.insert(configs, "CFLAGS=-DAPR_IOVEC_DEFINED")
         end
         import("package.tools.autoconf").install(package, configs)
+        if package:config("shared") then
+            os.rm(package:installdir("lib/*.a"))
+        else
+            os.tryrm(package:installdir("lib/*.so"))
+            os.tryrm(package:installdir("lib/*.dylib"))
+        end
         package:add("includedirs", "include/apr-1")
     end)
 
