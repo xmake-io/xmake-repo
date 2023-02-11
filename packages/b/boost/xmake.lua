260ac3bbb1320fc215461147b9b097facff0945b
ruki <waruqi@gmail.com>
2023-02-11 20:28:19 +0800

    improve boost

--- packages/b/boost/xmake.lua
+++ packages/b/boost/xmake.lua
@@ -153,8 +153,9 @@ package("boost")
             import("core.tool.toolchain")
             local runenvs = toolchain.load("msvc"):runenvs()
             os.vrunv("bootstrap.bat", bootstrap_argv, {envs = runenvs})
-        elseif package:is_plat("mingw") and is_subhost("windows") then
-            os.vrunv("bootstrap.bat", bootstrap_argv)
+        elseif package:is_plat("mingw") and is_host("windows") then
+            os.vrunv("sh", table.join("./bootstrap.sh", bootstrap_argv))
+            os.cp("./tools/build/src/engine/b2.exe", ".")
         else
             os.vrunv("./bootstrap.sh", bootstrap_argv)
         end
