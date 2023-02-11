95f85c09c3cf6e20f4822a635d80ef571f12eb07
ruki <waruqi@gmail.com>
2023-02-11 23:53:22 +0800

    Update xmake.lua

--- packages/c/cpp-lazy/xmake.lua
+++ packages/c/cpp-lazy/xmake.lua
@@ -2,9 +2,10 @@ package("cpp-lazy")
     set_homepage("https://github.com/MarcDirven/cpp-lazy")
     set_description("A fast C++11/14/17/20 header only library for lazy evaluation and function tools")
     
-    set_urls("https://github.com/MarcDirven/cpp-lazy/archive/refs/tags/v$(version).tar.gz", "https://github.com/MarcDirven/cpp-lazy.git")
+    set_urls("https://github.com/MarcDirven/cpp-lazy/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/MarcDirven/cpp-lazy.git")
 
-    add_versions("7.0.2", "aced554a828ab8d2c426b9a9cc0a0d576752d83c5a8e77a28506575126c3e40e")
+    add_versions("v7.0.2", "7a5c2a42ce5c98343676d09761959b9821ec125dbff7b9f2028792c117de0b09")
 
     add_deps("fmt")
 
