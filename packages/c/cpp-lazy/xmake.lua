e193ecfa22fa70cd7874cf43cad8c33f262579a0
Marc Dirven <marcdirven1996@outlook.com>
2023-02-11 16:33:27 +0100

    Add tar gz and add c++11

--- packages/c/cpp-lazy/xmake.lua
+++ packages/c/cpp-lazy/xmake.lua
@@ -2,7 +2,7 @@ package("cpp-lazy")
     set_homepage("https://github.com/MarcDirven/cpp-lazy")
     set_description("A fast C++11/14/17/20 header only library for lazy evaluation and function tools")
     
-    set_urls("https://github.com/MarcDirven/cpp-lazy/archive/refs/tags/v$(version).zip")
+    set_urls("https://github.com/MarcDirven/cpp-lazy/archive/refs/tags/v$(version).tar.gz", "https://github.com/MarcDirven/cpp-lazy.git")
 
     add_versions("7.0.2", "aced554a828ab8d2c426b9a9cc0a0d576752d83c5a8e77a28506575126c3e40e")
 
@@ -18,5 +18,5 @@ package("cpp-lazy")
             int arr[]{1, 2, 3, 4};
             auto mapper{lz::map(arr, [](const int i) { return i + 1; })};
         }
-    ]]}, {includes = "Lz/Map.hpp"}))
+    ]]}, {includes = "Lz/Map.hpp", configs = {languages = "c++11"}}))
     end)
