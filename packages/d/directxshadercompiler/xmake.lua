86034161f2c9c02043abb0256d61c85bc85e077a
L-Sun <zover.v@gmail.com>
2023-02-28 23:19:38 +0800

    fix typo

--- packages/d/directxshadercompiler/xmake.lua
+++ packages/d/directxshadercompiler/xmake.lua
@@ -14,10 +14,9 @@ package("directxshadercompiler")
         add_versions("1.6.2104", "ee5e96d58134957443ded04be132e2e19240c534d7602e3ab8fd5adc5156014a")
         add_versions("1.6.2106", "053b2d90c227cae84e7ce636bc4f7c25acd224c31c11a324885acbf5dd8b7aac")
         add_versions("1.7.2212", "ed77c7775fcf1e117bec8b5bb4de6735af101b733d3920dda083496dceef130f")
-    elseif is_plat("linux") and is_arch() == "x86_64" then 
+    elseif is_plat("linux") and is_arch("x86_64") then 
         add_urls("https://github.com/microsoft/DirectXShaderCompiler/releases/download/v$(version).tar.gz", {version = function (version) return version .. "/linux_dxc_" .. date[tostring(version)] end})
         add_versions("1.7.2212", "bfb453bd844d52575d2fe0db477701c33db4507e14a89e85128aca8608b5c359")
-        add_extsources("pacman::directx-shader-compiler")
     end
     
     on_install("windows|x64", "linux|x86_64", function (package)
@@ -25,7 +24,7 @@ package("directxshadercompiler")
         os.cp("inc/*", package:installdir("include"))
         os.cp("lib/x64/*", package:installdir("lib"))
 
-        if is_plat("linux") then 
+        if package:is_plat("linux") then 
             os.mv(package:installdir("include/WinAdapter.h"), package:installdir("include/dxc/Support"))
             os.vrunv("chmod", {"+x", path.join(package:installdir("bin"), "dxc")})
             os.vrunv("ln", {"-s", path.join(package:installdir("lib"), "libdxcompiler.so"), path.join(package:installdir("lib"), "libdxcompiler.so.3.7")})
