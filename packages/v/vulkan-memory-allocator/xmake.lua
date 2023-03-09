155e65c0468f69a9d94bc502abd7c11fc1c0a9bc
L-Sun <zover.v@gmail.com>
2023-03-09 16:18:18 +0800

    fix vma build

--- packages/v/vulkan-memory-allocator/xmake.lua
+++ packages/v/vulkan-memory-allocator/xmake.lua
@@ -1,5 +1,5 @@
 package("vulkan-memory-allocator")
-    set_kind("library", {headeronly = true})
+    set_kind("library")
     set_homepage("https://gpuopen-librariesandsdks.github.io/VulkanMemoryAllocator/html/")
     set_description("Easy to integrate Vulkan memory allocation library.")
     set_license("MIT")
@@ -9,10 +9,15 @@ package("vulkan-memory-allocator")
     add_versions("v3.0.0", 'dbb621a7a13fb70b8c34fef62fbe5128cc5193c7179c9edacead9f110df79a2f')
     add_versions("v3.0.1", '2a84762b2d10bf540b9dc1802a198aca8ad1f3d795a4ae144212c595696a360c')
 
-    add_deps("vulkan-headers")
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean"})
 
+    add_deps("vulkan-headers", "cmake")
+    
     on_install("windows", "linux", "mingw", "macosx", "iphoneos", "android", function (package)
-        os.cp("include/vk_mem_alloc.h", package:installdir("include"))
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
