8a72886df7c67ff00a62737fdd6177b5f5c276ab
star-hengxing <hengxings783@gmail.com>
2023-03-11 20:34:10 +0800

    fix config name

--- packages/s/snitch/xmake.lua
+++ packages/s/snitch/xmake.lua
@@ -8,7 +8,7 @@ package("snitch")
              "https://github.com/cschreib/snitch.git")
     add_versions("1.0.0", "9616a854e5d7d26b9003d1bb0fa1a1e4dba03a6e380b0f71ac989648e452a994")
 
-    add_configs("use_main", {description = "Using your own main function", default = false, type = "boolean"})
+    add_configs("main", {description = "Using your own main function", default = false, type = "boolean"})
 
     add_deps("cmake")
 
@@ -16,13 +16,13 @@ package("snitch")
         local configs = {"-DSNITCH_DO_TEST=OFF", "-DSNITCH_CREATE_HEADER_ONLY=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DSNITCH_DEFINE_MAIN=" .. (package:config("use_main") and "OFF" or "ON"))
+        table.insert(configs, "-DSNITCH_DEFINE_MAIN=" .. (package:config("main") and "OFF" or "ON"))
         import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         -- xmake always add main function
-        if not package:config("use_main") then
+        if not package:config("main") then
             return
         end
 
