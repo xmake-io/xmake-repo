692e8440f03774215c28e258ce60cfa98b3f7812
ruki <waruqi@gmail.com>
2023-03-16 23:04:50 +0800

    test

--- packages/t/tclsh/xmake.lua
+++ packages/t/tclsh/xmake.lua
@@ -18,6 +18,7 @@ package("tclsh")
         -- TODO
         io.replace("makefile.vc", "libtclzip:  core dlls $(TCLSCRIPTZIP)", "libtclzip:  core dlls", {plain = true})
         import("package.tools.nmake").build(package, {"-f", "makefile.vc", "release"})
+        print(os.files("**"))
         import("package.tools.nmake").build(package, {"-f", "makefile.vc", "install", "INSTALLDIR=" .. package:installdir()})
         print(os.files(path.join(package:installdir(), "**")))
         os.cp(path.join(package:installdir("bin"), "tclsh90.exe"), path.join(package:installdir("bin"), "tclsh.exe"))
