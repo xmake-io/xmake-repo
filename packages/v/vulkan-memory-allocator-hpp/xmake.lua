40e72b67c664aee2282a95587d7196bf030cd76d
Latias94 <superfrankie621@gmail.com>
2023-03-18 18:10:53 +0800

    modify support platform

--- packages/v/vulkan-memory-allocator-hpp/xmake.lua
+++ packages/v/vulkan-memory-allocator-hpp/xmake.lua
@@ -12,7 +12,7 @@ package("vulkan-memory-allocator-hpp")
     add_deps("vulkan-hpp")
     add_deps("vulkan-memory-allocator")
 
-    on_install("windows", "linux", "mingw", "macosx", "iphoneos", "android", function (package)
+    on_install("windows|x86", "windows|x64", "linux", "macosx", "mingw", function (package)
         os.cp("include", package:installdir())
     end)
 
