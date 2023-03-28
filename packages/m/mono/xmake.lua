4f9f40f3cd6115b8068ca7c75bbfe70f41cc9f06
yamashi <yamashi@users.noreply.github.com>
2023-03-28 14:24:06 +0200

    Update xmake.lua

--- packages/m/mono/xmake.lua
+++ packages/m/mono/xmake.lua
@@ -7,6 +7,7 @@ package("mono")
              {version = function (version) return version:gsub("%+", ".") end})
 
     add_versions("6.8.0+123", "e2e42d36e19f083fc0d82f6c02f7db80611d69767112af353df2f279744a2ac5")
+    add_versions("6.12.0+182", "57366a6ab4f3b5ecf111d48548031615b3a100db87c679fc006e8c8a4efd9424")
 
     add_includedirs("include/mono-2.0")
 
