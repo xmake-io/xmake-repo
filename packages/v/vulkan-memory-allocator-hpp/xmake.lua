fe4f42bc26f1f7a69b660776fc98e7b49bfbd2c6
Latias94 <superfrankie621@gmail.com>
2023-03-31 17:34:36 +0800

    update vma-hpp version

--- packages/v/vulkan-memory-allocator-hpp/xmake.lua
+++ packages/v/vulkan-memory-allocator-hpp/xmake.lua
@@ -7,13 +7,14 @@ package("vulkan-memory-allocator-hpp")
     add_urls("https://github.com/YaaZ/VulkanMemoryAllocator-Hpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/YaaZ/VulkanMemoryAllocator-Hpp.git")
     add_versions("v3.0.0", '2f062b1631af64519d09e7b319c2ba06d7de3c9c5589fb7109a3f4e341cee2b7')
-    add_versions("v3.0.1", '4b50f38f2f6246e6ee23e046a430f5a17df932a9f0c2137d16c4a26a472ec99b')
+    add_versions("v3.0.1", '58aef30d992fea986a8ab3bf3b4c5ffece3679fb585af5700269b3d627bc6760')
+    add_versions("v3.0.1-1", '0e0c374751d5ca6123d0ae0df756693f0674412d7c758ec4a39a5a9dcc412911')
 
     add_deps("vulkan-memory-allocator")
 
     on_install("windows|x86", "windows|x64", "linux", "macosx", "mingw", "android", "iphoneos", function (package)
         os.cp("include", package:installdir())
-        if package:gitref() or package:version():ge("3.0.1") then
+        if package:gitref() or package:version():ge("3.0.1-1") then
             package:add("deps", "vulkan-hpp >= 1.3.234")
         else
             package:add("deps", "vulkan-hpp < 1.3.234")
