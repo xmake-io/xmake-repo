f4ea44b6d94395fc5196a7e1f60086bb5de57226
Qudix <17361645+Qudix@users.noreply.github.com>
2023-04-06 00:14:05 -0500

    [convertutf] Date

--- packages/c/convertutf/xmake.lua
+++ packages/c/convertutf/xmake.lua
@@ -1,6 +1,6 @@
 package("convertutf")
     set_homepage("https://home.unicode.org/")
-    set_description("Conversions between UTF-32, UTF-16, and UTF-8. Circa 2001.")
+    set_description("Conversions between UTF-32, UTF-16, and UTF-8. Circa 2004.")
 
     set_urls("https://github.com/xmake-mirror/convertutf/archive/refs/tags/$(version).tar.gz",
              "https://github.com/xmake-mirror/convertutf.git")
