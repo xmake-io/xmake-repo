48d13148d5bddec5a77e86fff6320456e0ab9e7f
KH <59428912+Wkh3@users.noreply.github.com>
2023-04-09 21:48:09 +0800

    update abseil version (#1978)

--- packages/a/abseil/xmake.lua
+++ packages/a/abseil/xmake.lua
@@ -11,6 +11,7 @@ package("abseil")
     add_versions("20210324.2", "59b862f50e710277f8ede96f083a5bb8d7c9595376146838b9580be90374ee1f")
     add_versions("20211102.0", "dcf71b9cba8dc0ca9940c4b316a0c796be8fab42b070bb6b7cab62b48f0e66c4")
     add_versions("20220623.0", "4208129b49006089ba1d6710845a45e31c59b0ab6bff9e5788a87f55c5abd602")
+    add_versions("20230125.2", "9a2b5752d7bfade0bdeee2701de17c9480620f8b237e1964c1b9967c75374906")
 
     add_deps("cmake")
 
