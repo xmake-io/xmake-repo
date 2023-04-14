16ccf6fc5bd9a7139d1d6db4b5aab2750a3be155
jingkaimori <jingkaimori@gmail.com>
2023-04-14 11:41:01 +0800

    adjust compiling command for android

--- packages/s/s7/port/xmake.lua
+++ packages/s/s7/port/xmake.lua
@@ -36,9 +36,7 @@ target("s7") do
     if is_mode("debug") then
         add_defines("S7_DEBUGGING")
     end
-    if not is_plat("macosx", "android") then
+    if not is_plat("macosx") then
         add_ldflags("-static", "-static-libgcc", {force = true})
-    elseif is_plat("android") then
-        add_ldflags("-lmath", {force = true})
     end
 end
