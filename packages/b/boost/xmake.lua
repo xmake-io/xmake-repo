edd3c488aa3f6f5e27cd35bae46c73fb892cd70e
wanzzhehe <wanzzmicro@outlook.com>
2023-04-17 16:09:33 +0800

    fix bootstrap_argv for bootstrap.bat and conflict between project-config.jam and user-config.jam

--- packages/b/boost/xmake.lua
+++ packages/b/boost/xmake.lua
@@ -152,10 +152,14 @@ package("boost")
         if package:is_plat("windows") then
             import("core.tool.toolchain")
             local runenvs = toolchain.load("msvc"):runenvs()
+            -- for bootstrap.bat, all other arguments are useless
+            bootstrap_argv = { "msvc" }
             os.vrunv("bootstrap.bat", bootstrap_argv, {envs = runenvs})
         elseif package:is_plat("mingw") and is_host("windows") then
-            os.vrunv("sh", table.join("./bootstrap.sh", bootstrap_argv))
-            os.cp("./tools/build/src/engine/b2.exe", ".")
+            bootstrap_argv = { "gcc" }
+            os.vrunv("bootstrap.bat", bootstrap_argv)
+            -- todo looking for better solution to fix the confict between user-config.jam and project-config.jam
+            io.replace("project-config.jam", "using[^\n]+", "")
         else
             os.vrunv("./bootstrap.sh", bootstrap_argv)
         end
