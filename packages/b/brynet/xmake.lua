b6d730bc21227760179a0661207b9260cd140a1e
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-04-18 15:06:32 +0800

    update brynet to 1.12.2

--- packages/b/brynet/xmake.lua
+++ packages/b/brynet/xmake.lua
@@ -6,6 +6,7 @@ package("brynet")
     set_urls("https://github.com/IronsDu/brynet/archive/v$(version).zip")
     add_urls("https://github.com/IronsDu/brynet.git")
     add_versions("1.0.9", "a264a6aaf3ec9fd5aa4029a8857be813be203ee7b93997b0c1c5c5e2c5f89a2a")
+    add_versions("1.12.2", "7c789c1a9a9c3f2cac73e98a96d18338d876b0bcd100c8e71d755a6bf53125ab")
 
     if is_plat("windows") then
         add_syslinks("Ws2_32")
