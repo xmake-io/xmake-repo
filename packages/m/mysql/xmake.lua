f5c89b846f43dcb664464012f42fba4cbd64626c
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-04-19 15:18:11 +0800

    split on_install to windows/others

--- packages/m/mysql/xmake.lua
+++ packages/m/mysql/xmake.lua
@@ -45,53 +45,53 @@ package("mysql")
         end
     end)
 
-    on_install("macosx", "linux", "windows", function (package)
-        -- This is because only windows is tested, so judge if
-        if package:is_plat("windows") then
-            io.gsub("CMakeLists.txt", "ADD_SUBDIRECTORY%(storage/ndb%)", "")
-            local configs = { "-DCOMPILATION_COMMENT=XMake",
-                          "-DDEFAULT_CHARSET=utf8",
-                          "-DDEFAULT_COLLATION=utf8_general_ci",
-                          "-DINSTALL_DOCDIR=share/doc/#{name}",
-                          "-DINSTALL_INCLUDEDIR=include/mysql",
-                          "-DINSTALL_INFODIR=share/info",
-                          "-DINSTALL_MANDIR=share/man",
-                          "-DINSTALL_MYSQLSHAREDIR=share/mysql",
-                          "-DWITH_EDITLINE=" .. (is_plat("macosx") and "system" or "bundled"),
-                          "-DWITH_UNIT_TESTS=OFF",
-                          "-DDISABLE_SHARED=" .. (package:config("shared") and "OFF" or "ON"),
-                          "-DWITH_LZ4='system'",
-                          "-DWITH_ZSTD='system'",
-                          "-DWITH_ZLIB='system'",
-                          "-DWINDOWS_RUNTIME_MD=" .. (package:config("vs_runtime"):startswith("MD") and "ON" or "OFF"),
-                          "-DWITHOUT_SERVER=ON"}
-            io.replace("cmake/ssl.cmake","IF(NOT OPENSSL_APPLINK_C)","IF(FALSE AND NOT OPENSSL_APPLINK_C)", {plain = true})
-            for _, removelib in ipairs({"icu", "libevent", "re2", "rapidjson", "protobuf", "libedit"}) do
-                io.replace("CMakeLists.txt", "MYSQL_CHECK_" .. string.upper(removelib) .. "()\n", "", {plain = true})
-                io.replace("CMakeLists.txt", "INCLUDE(" .. removelib .. ")\n", "", {plain = true})
-                io.replace("CMakeLists.txt", "WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
-                io.replace("CMakeLists.txt", "SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
-            end
-            os.rmdir("extra")
-            for _, folder in ipairs({"client", "man", "mysql-test", "libbinlogstandalone"}) do
-                os.rmdir(folder)
-                io.replace("CMakeLists.txt", "ADD_SUBDIRECTORY(" .. folder .. ")\n", "", {plain = true})
-            end
-            os.rmdir("storage/ndb")
-            for _, line in ipairs({"INCLUDE(cmake/boost.cmake)\n", "MYSQL_CHECK_EDITLINE()\n"}) do
-                io.replace("CMakeLists.txt", line, "", {plain = true})
-            end
-            io.replace("libbinlogevents/CMakeLists.txt", "INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", "MY_INCLUDE_SYSTEM_DIRECTORIES(LZ4)\nINCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", {plain = true})
-            io.replace("cmake/install_macros.cmake", "  INSTALL_DEBUG_SYMBOLS(","  # INSTALL_DEBUG_SYMBOLS(", {plain = true})
-            import("package.tools.cmake").install(package, configs)
-            if package:config("shared") then
-                os.rm(package:installdir("lib/mysqlclient.lib"))
-                os.cp(package:installdir("lib/libmysql.dll"), package:installdir("bin"))
-            else
-                os.rm(package:installdir("lib/libmysql.lib"))
-                os.rm(package:installdir("bin/libmysql.dll"))
-            end
+    on_install("windows", function (package)
+        io.gsub("CMakeLists.txt", "ADD_SUBDIRECTORY%(storage/ndb%)", "")
+        local configs = { "-DCOMPILATION_COMMENT=XMake",
+                        "-DDEFAULT_CHARSET=utf8",
+                        "-DDEFAULT_COLLATION=utf8_general_ci",
+                        "-DINSTALL_DOCDIR=share/doc/#{name}",
+                        "-DINSTALL_INCLUDEDIR=include/mysql",
+                        "-DINSTALL_INFODIR=share/info",
+                        "-DINSTALL_MANDIR=share/man",
+                        "-DINSTALL_MYSQLSHAREDIR=share/mysql",
+                        "-DWITH_EDITLINE=" .. (is_plat("macosx") and "system" or "bundled"),
+                        "-DWITH_UNIT_TESTS=OFF",
+                        "-DDISABLE_SHARED=" .. (package:config("shared") and "OFF" or "ON"),
+                        "-DWITH_LZ4='system'",
+                        "-DWITH_ZSTD='system'",
+                        "-DWITH_ZLIB='system'",
+                        "-DWINDOWS_RUNTIME_MD=" .. (package:config("vs_runtime"):startswith("MD") and "ON" or "OFF"),
+                        "-DWITHOUT_SERVER=ON"}
+        io.replace("cmake/ssl.cmake","IF(NOT OPENSSL_APPLINK_C)","IF(FALSE AND NOT OPENSSL_APPLINK_C)", {plain = true})
+        for _, removelib in ipairs({"icu", "libevent", "re2", "rapidjson", "protobuf", "libedit"}) do
+            io.replace("CMakeLists.txt", "MYSQL_CHECK_" .. string.upper(removelib) .. "()\n", "", {plain = true})
+            io.replace("CMakeLists.txt", "INCLUDE(" .. removelib .. ")\n", "", {plain = true})
+            io.replace("CMakeLists.txt", "WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
+            io.replace("CMakeLists.txt", "SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
+        end
+        os.rmdir("extra")
+        for _, folder in ipairs({"client", "man", "mysql-test", "libbinlogstandalone"}) do
+            os.rmdir(folder)
+            io.replace("CMakeLists.txt", "ADD_SUBDIRECTORY(" .. folder .. ")\n", "", {plain = true})
+        end
+        os.rmdir("storage/ndb")
+        for _, line in ipairs({"INCLUDE(cmake/boost.cmake)\n", "MYSQL_CHECK_EDITLINE()\n"}) do
+            io.replace("CMakeLists.txt", line, "", {plain = true})
+        end
+        io.replace("libbinlogevents/CMakeLists.txt", "INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", "MY_INCLUDE_SYSTEM_DIRECTORIES(LZ4)\nINCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", {plain = true})
+        io.replace("cmake/install_macros.cmake", "  INSTALL_DEBUG_SYMBOLS(","  # INSTALL_DEBUG_SYMBOLS(", {plain = true})
+        import("package.tools.cmake").install(package, configs)
+        if package:config("shared") then
+            os.rm(package:installdir("lib/mysqlclient.lib"))
+            os.cp(package:installdir("lib/libmysql.dll"), package:installdir("bin"))
         else
+            os.rm(package:installdir("lib/libmysql.lib"))
+            os.rm(package:installdir("bin/libmysql.dll"))
+        end
+    end
+
+    on_install("macosx", "linux", function (package)
             -- https://bugs.mysql.com/bug.php?id=87348
             -- Fixes: "ADD_SUBDIRECTORY given source
             -- 'storage/ndb' which is not an existing"
