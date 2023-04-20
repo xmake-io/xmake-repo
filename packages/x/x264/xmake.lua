5a4e2adbe7bbc0fc2529e8b4f54ad6ebf0afa433
DreamOfIce <dreamofice@outlook.com>
2023-04-21 00:11:31 +0800

    feat: add more options for x265

--- packages/x/x264/xmake.lua
+++ packages/x/x264/xmake.lua
@@ -10,19 +10,54 @@ package("x264")
     add_versions("v2018.09.25", "545de2ffec6ae9a80738de1b2c8cf820249a2530")
 
     add_deps("nasm")
+
+    add_configs("cli",            {description = "enable cli", default = false, type = "boolean"})
+    add_configs("bashcompletion", {description = "enable installation of bash-completion script", default = false, type = "boolean"})
+    add_configs("opencl",         {description = "enable OpenCL features", default = true, type = "boolean"})
+    add_configs("gpl",            {description = "enable GPL-only features", default = false, type = "boolean"})
+    add_configs("thread",         {description = "enable multithreaded encoding", default = true, type = "boolean"})
+    add_configs("interlaced",     {description = "enable interlaced encoding support", default = true, type = "boolean"})
+    add_configs("bit-depth",      {description = "set output bit depth", default = "all", value = {8, 10, "all"}})
+    add_configs("chroma-format",  {description = "output chroma format", default = "all", value = {400, 420, 422, 444, "all"}})
+
+    -- Advanced options
+    add_configs("asm", {description = "enable platform-specific assembly optimizations", default = true, type = "boolean"})
+    add_configs("lto", {description = "enable link-time optimization", default = false, type = "boolean"})
+    add_configs("pic", {description = "build position-independent code", default = false, type = "boolean"})
+
+    -- External library support
+    add_configs("avs",     {description = "enable avisynth support", default = false, type = "boolean"})
+    add_configs("swscale", {description = "enable swscale support", default = false, type = "boolean"})
+    add_configs("lavf",    {description = "enable libavformat support", default = false, type = "boolean"})
+    add_configs("ffms",    {description = "enable ffmpegsource support", default = false, type = "boolean"})
+    add_configs("gpac",    {description = "enable gpac support", default = false, type = "boolean"})
+    add_configs("lsmash",  {description = "enable lsmash support", default = false, type = "boolean"})
+
     add_configs("toolchains", {readonly = true, description = "Set package toolchains only for cross-compilation."})
 
     add_syslinks("pthread", "dl")
-    on_install("linux", "macosx", "wasm", function (package)
-        local configs = {"--disable-avs", "--disable-lsmash", "--disable-lavf", "--disable-bashcompletion"}
+
+    if is_plat("wasm") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
+    on_install("windows", "linux", "macosx", "wasm", function (package)
+        local configs = {}
         table.insert(configs, "--enable-" .. (package:config("shared") and "shared" or "static"))
-        if package:config("pic") ~= false then
-            table.insert(configs, "--enable-pic")
-        end
         if package:is_plat("wasm") then
             table.insert(configs, "--host=i686-gnu")
-            table.insert(configs, "--disable-asm")
-            table.insert(configs, "--disable-cli")
+            package:config_set("asm", false)
+            package:config_set("cli", false)
+        end
+
+        for name, value in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                if type(value) == "boolean" then
+                    table.insert(configs, "--" .. (value and "enable" or "disable") .. "-" .. name)
+                else
+                    table.insert(configs, "--" .. name .. "=" .. value)
+                end
+            end
         end
         import("package.tools.autoconf").install(package, configs)
     end)
