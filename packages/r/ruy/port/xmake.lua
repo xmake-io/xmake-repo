96d1b456d0f26cf215c64b24fd40422c9468854b
A2va <49582555+A2va@users.noreply.github.com>
2023-04-29 11:59:02 +0200

    Add ruy

---
+++ packages/r/ruy/port/xmake.lua
@@ -0,0 +1,39 @@
+add_rules("mode.debug", "mode.releasedbg", "mode.release")
+add_requires("cpuinfo")
+
+option("profiler", { default = false, description = "Enable ruy's built-in profiler (harms performance)" })
+
+
+target("ruy")
+    set_kind("static")
+
+    add_files("ruy/**.cc")
+    remove_files("ruy/test*.cc", "ruy/*test.cc")
+    remove_files("ruy/profiler/test*.cc")
+    remove_files("ruy/benchmark.cc")
+
+    remove_files("ruy/profiler/test*.cc")
+    remove_files("ruy/profiler/test.cc")
+
+    add_headerfiles("(ruy/**.h)")
+    remove_headerfiles("ruy/gtest_wrapper.h")
+    remove_headerfiles("ruy/profiler/test*.h")
+
+    add_includedirs(".")
+    set_optimize("fastest")
+
+    add_packages("cpuinfo")
+
+    on_load(function (target) 
+        if is_arch("arm.*") then
+            target:add("cxflags", "-mfpu=neon")
+        end
+
+        if not is_plat("windows") then 
+            target:add("cxflags", "-Wall", "-Wextra", "-Wc++14-compat", "-Wundef")
+        end
+
+        if has_config("profiler") then
+            target:add("defines", "RUY_PROFILE")
+        end
+    end)
