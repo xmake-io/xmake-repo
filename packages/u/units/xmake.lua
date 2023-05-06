b57c388251362d9e744646c1f952b79e2703d60d
Adrien <0x2adr1@gmail.com>
2023-05-06 12:10:25 -0500

    Address comments

--- packages/u/units/xmake.lua
+++ packages/u/units/xmake.lua
@@ -3,17 +3,24 @@ package("units")
     set_homepage("https://nholthaus.github.io/units/")
     set_description("A compile-time, header-only, dimensional analysis library built on c++14 with no dependencies.")
 
-    add_urls("https://github.com/nholthaus/units/archive/refs/tags/v2.3.3.tar.gz", "https://github.com/nholthaus/units.git")
+    add_urls("https://github.com/nholthaus/units/archive/refs/tags/$(version).tar.gz", "https://github.com/nholthaus/units.git")
     add_versions("v2.3.3", "b1f3c1dd11afa2710a179563845ce79f13ebf0c8c090d6aa68465b18bd8bd5fc")
 
-    on_install(function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("units")
-                set_kind("headeronly")
-                add_includedirs("include", {public = true})
-                add_headerfiles("include/units.h")
-        ]])
+    set_kind("library", {headeronly = true})
 
-        os.cp("include/*.h", package:installdir("include"))
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <units.h>
+            static void test() {
+                constexpr units::angle::degree_t deg1{90};
+                constexpr units::angle::degree_t deg2{60};
+            
+                assert(deg1 > deg2);
+                assert(deg1 + deg2 == units::angle::degree_t{150});
+            }
+        ]]}, {configs = {languages = "c++14"}}))
     end)
