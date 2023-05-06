b45ed14f55d4e9de5af48f1e109f4dc5d55370b4
jingkaimori <jingkaimori@gmail.com>
2023-05-06 13:09:45 +0800

    run reconfigure on mingw

--- packages/i/icu4c/xmake.lua
+++ packages/i/icu4c/xmake.lua
@@ -17,7 +17,7 @@ package("icu4c")
     add_patches("69.1", path.join(os.scriptdir(), "patches", "69.1", "replace-py-3.patch"), "ae27a55b0e79a8420024d6d349a7bae850e1dd403a8e1131e711c405ddb099b9")
     add_patches("70.1", path.join(os.scriptdir(), "patches", "70.1", "replace-py-3.patch"), "6469739da001721122b62af513370ed62901caf43af127de3f27ea2128830e35")
     if is_plat("mingw") then
-        add_patches("72.1", path.join(os.scriptdir(), "patches", "72.1", "mingw.patch"), "3a2d31b1024b4231006c953fce0f9dce455eae335d84263bd013dd0b5dfafd58")
+        add_patches("72.1", path.join(os.scriptdir(), "patches", "72.1", "mingw.patch"), "818d55c3114328e9a93932c81a78602e33888f90caed83ce094e776af0e97b09")
     end
 
     add_links("icuuc", "icutu", "icui18n", "icuio")
