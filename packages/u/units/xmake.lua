1f2ad93623fe946abfebcd414d2c8f3f229bb50e
ruki <waruqi@gmail.com>
2023-05-07 10:29:31 +0800

    Update xmake.lua

--- packages/u/units/xmake.lua
+++ packages/u/units/xmake.lua
@@ -1,12 +1,10 @@
 package("units")
-
+    set_kind("library", {headeronly = true})
     set_homepage("https://nholthaus.github.io/units/")
     set_description("A compile-time, header-only, dimensional analysis library built on c++14 with no dependencies.")
 
     add_urls("https://github.com/nholthaus/units/archive/refs/tags/$(version).tar.gz", "https://github.com/nholthaus/units.git")
     add_versions("v2.3.3", "b1f3c1dd11afa2710a179563845ce79f13ebf0c8c090d6aa68465b18bd8bd5fc")
-
-    set_kind("library", {headeronly = true})
 
     on_install(function (package)
         os.cp("include", package:installdir())
