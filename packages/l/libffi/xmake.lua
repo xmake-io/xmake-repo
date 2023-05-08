123c0b54565571402683bb4efcf0d5cbaee53115
xq114 <1140735506@qq.com>
2023-05-08 23:49:18 +0800

    fix libffi

--- packages/l/libffi/xmake.lua
+++ packages/l/libffi/xmake.lua
@@ -35,7 +35,7 @@ package("libffi")
 
     on_install("macosx", "linux", "bsd", "mingw", function (package)
         local configs = {"--disable-silent-rules", "--disable-dependency-tracking"}
-        table.insert(configs, "--libdir=" .. package:installdir("lib"))
+        table.insert(configs, "--libdir=" .. package:installdir("lib"):gsub("\\", "/"))
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
         if package:debug() then
