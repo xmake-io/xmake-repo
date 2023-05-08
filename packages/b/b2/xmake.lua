3f8d18344b6ac18578621b565fc589ad6516df96
xq114 <1140735506@qq.com>
2023-05-08 12:13:14 +0800

    add b2

---
+++ packages/b/b2/xmake.lua
@@ -0,0 +1,25 @@
+package("b2")
+
+    set_kind("binary")
+    set_homepage("https://www.bfgroup.xyz/b2/")
+    set_description("B2 makes it easy to build C++ projects, everywhere.")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/bfgroup/b2/releases/download/$(version)/b2-$(version).zip")
+    add_versions("4.9.6", "a049f7fdfae4b62353a3f76f34a72c8c87324d1c026cf87febe6c563311bf687")
+
+    on_install("@windows", function (package)
+        os.vrun("bootstrap.bat")
+        os.vrunv("b2.exe", {"install", "--prefix=" .. package:installdir()})
+        package:addenv("PATH", ".")
+    end)
+
+    on_install("@macosx", "@linux", "@bsd", "@msys", "@cygwin", function (package)
+        os.vrun("./bootstrap.sh")
+        os.vrunv("./b2", {"install", "--prefix=" .. package:installdir()})
+        package:addenv("PATH", ".")
+    end)
+
+    on_test(function (package)
+        os.vrun("b2 --help")
+    end)
