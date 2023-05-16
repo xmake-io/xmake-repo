8fa89a4715540771d83fba2a49231c1c26b2260b
Qudix <17361645+Qudix@users.noreply.github.com>
2023-05-16 14:17:53 -0500

    [commonlibsse-ng] Add `v3.7.0`

--- packages/c/commonlibsse-ng/xmake.lua
+++ packages/c/commonlibsse-ng/xmake.lua
@@ -5,6 +5,7 @@ package("commonlibsse-ng")
 
     add_urls("https://github.com/CharmedBaryon/CommonLibSSE-NG/archive/$(version).zip",
              "https://github.com/CharmedBaryon/CommonLibSSE-NG.git")
+    add_versions("v3.7.0", "c2fc2b5ac1c67ad4779bd0570c95fe494e0724f957a61880fa42cf74d1edce14")
     add_versions("v3.6.0", "6f84c36f5747cff73d6a95bc7b9de84b11601648b218fd3e69edc884cc94a5a8")
     add_versions("v3.5.6", "a3e1d4ec7496adca8310613fe75a2e08a9dbf562a9febec584b4e79aacc92bd3")
     add_versions("v3.5.5", "5b00de66b9b8bc300244f14f1a281f26961931ba28ed0f4c9cce3a30a77c784a")
