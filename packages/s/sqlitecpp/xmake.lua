b3e133ef00c624e4b6135fcbbc0a63667bc52013
star-hengxing <hengxings783@gmail.com>
2023-05-19 23:11:41 +0800

    add sqlitecpp

---
+++ packages/s/sqlitecpp/xmake.lua
@@ -0,0 +1,46 @@
+package("sqlitecpp")
+
+    set_homepage("http://srombauts.github.io/SQLiteCpp")
+    set_description("SQLiteC++ (SQLiteCpp) is a smart and easy to use C++ SQLite3 wrapper.")
+
+    set_urls("https://github.com/SRombauts/SQLiteCpp/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/SRombauts/SQLiteCpp.git")
+
+    add_versions("3.2.1", "70c67d5680c47460f82a7abf8e6b0329bf2fb10795a982a6d8abc06adb42d693")
+
+    add_deps("cmake")
+
+    add_configs("column_metadata", { description = "Enable Column::getColumnOriginName(). Require support from sqlite3 library.", default = false, type = "boolean"})
+    add_configs("assert_handled", { description = "Enable the user definition of a assertion_failed() handler.", default = false, type = "boolean"})
+    add_configs("has_codec", { description = "Enable database encryption API. Not available in the public release of SQLite.", default = false, type = "boolean"})
+    add_configs("legacy_struct", { description = "EFallback to forward declaration of legacy struct sqlite3_value (pre SQLite 3.19)", default = false, type = "boolean"})
+    add_configs("ommit_load_extension", { description = "Enable ommit load extension.", default = false, type = "boolean"})
+    add_configs("filesystem", { description = "Enable the support for std::filesystem (C++17)", default = true, type = "boolean"})
+    if is_plat("mingw") then
+        add_configs("stack_protection", { description = "Enable stack protection for MySQL.", default = true, type = "boolean", readonly = true})
+    else
+        add_configs("stack_protection", { description = "Enable stack protection for MySQL.", default = true, type = "boolean"})
+    end
+
+    on_install(function (package)
+        local configs = {"-DSQLITECPP_BUILD_EXAMPLES=OFF", "-DSQLITECPP_BUILD_TESTS=OFF"}
+        table.insert(configs, "-DSQLITE_ENABLE_COLUMN_METADATA=" .. (package:config("column_metadata") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITE_ENABLE_ASSERT_HANDLER=" .. (package:config("assert_handled") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITE_HAS_CODEC=" .. (package:config("has_codec") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITE_USE_LEGACY_STRUCT=" .. (package:config("legacy_struct") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITE_OMMIT_LOAD_EXTENSION=" .. (package:config("ommit_load_extension") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITECPP_DISABLE_STD_FILESYSTEM=" .. (package:config("filesystem") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITECPP_USE_STACK_PROTECTION=" .. (package:config("stack_protection") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DSQLITECPP_USE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                SQLite::Database db("example.db3");
+            }
+        ]]}, {includes = {"SQLiteCpp/Database.h"}}))
+    end)
