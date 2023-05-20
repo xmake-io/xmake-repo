24406915e9c57e7e3d8e7cb204f84277ddbef8c5
star-hengxing <hengxings783@gmail.com>
2023-05-20 20:43:47 +0800

    improve rules

--- packages/y/yy-thunks/rules/link.lua
+++ packages/y/yy-thunks/rules/link.lua
@@ -1,30 +1,32 @@
 rule("xp")
     on_config(function (target)
-        import("core.project.project")
-
         local objectfile = "YY_Thunks_for_WinXP.obj"
-        local installdir = project.required_package("yy-thunks"):installdir()
-        table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        local thunks = target:pkg("yy-thunks")
+        if thunks then
+            local installdir = thunks:installdir()
+            table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        end
     end)
 
 rule("vista")
     on_config(function (target)
-        import("core.project.project")
-
         local objectfile = "YY_Thunks_for_Vista.obj"
-        local installdir = project.required_package("yy-thunks"):installdir()
-        table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        local thunks = target:pkg("yy-thunks")
+        if thunks then
+            local installdir = thunks:installdir()
+            table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        end
     end)
 
 rule("2k")
     on_config(function (target)
-        import("core.project.project")
-
         if not target:is_arch("x86") then
             raise("Win2K only supports x86 architecture")
         end
 
-        local objectfile = "YY_Thunks_for_Win2K.obj"
-        local installdir = project.required_package("yy-thunks"):installdir()
-        table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        local thunks = target:pkg("yy-thunks")
+        if thunks then
+            local installdir = thunks:installdir()
+            table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
+        end
     end)
