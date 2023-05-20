a0dc041d6dde750355c20aee99169ea6e2f9bccd
star-hengxing <hengxings783@gmail.com>
2023-05-20 15:13:30 +0800

    fix shared

--- packages/s/sqlitecpp/xmake.lua
+++ packages/s/sqlitecpp/xmake.lua
@@ -32,8 +32,14 @@ package("sqlitecpp")
         table.insert(configs, "-DSQLITECPP_DISABLE_STD_FILESYSTEM=" .. (package:config("filesystem") and "ON" or "OFF"))
         table.insert(configs, "-DSQLITECPP_USE_STACK_PROTECTION=" .. (package:config("stack_protection") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if is_plat("windows") then
+            if package:config("shared") then
+                if package:version():gt("3.2.1") then
+                    table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
+                else
+                    table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+                end
+            end
             table.insert(configs, "-DSQLITECPP_USE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
         end
         import("package.tools.cmake").install(package, configs)
