5ea8806d248d5234dfcad52d5149396c4ef1a0f6
star-hengxing <hengxings783@gmail.com>
2023-05-23 01:12:45 +0800

    improve yy-thunks

--- packages/y/yy-thunks/rules/link.lua
+++ packages/y/yy-thunks/rules/link.lua
@@ -24,9 +24,27 @@ rule("2k")
             raise("Win2K only supports x86 architecture")
         end
 
+        local objectfile = "YY_Thunks_for_Win2K.obj"
         local thunks = target:pkg("yy-thunks")
         if thunks then
             local installdir = thunks:installdir()
             table.insert(target:objectfiles(), path.join(installdir, "lib", objectfile))
         end
+    end)
+
+    after_link(function (target, opt)
+        import("core.project.depend")
+        import("lib.detect.find_tool")
+        import("core.tool.toolchain")
+        import("utils.progress")
+
+        depend.on_changed(function()
+            local msvc = toolchain.load("msvc", {plat = target:plat(), arch = target:arch()})
+            local editbin = assert(find_tool("editbin", {envs = msvc:runenvs()}), "editbin not found!")
+
+            -- osversion -> Major/Minor OperatingSystemVersion
+            -- subsystem -> Major/Minor SubsystemVersion
+            os.iorunv(editbin.program, {"/osversion:5.0", "/subsystem:console,5.0", target:targetfile()})
+            progress.show(opt.progress, "${color.build.target}editing.$(mode) %s", target:filename())
+        end, {files = {target:targetfile()}})
     end)
