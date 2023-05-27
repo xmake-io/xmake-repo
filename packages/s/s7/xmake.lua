0cf486b6db30edf41ee3624776f6558c2fcb0903
沈浪熊猫儿 <shenda@ustc.edu>
2023-05-27 16:00:27 +0800

    Revert "S7: fix link on_test"
    This reverts commit 1ae9ee87404578447243e6a3cf70b2b7f1c5d0d8.

--- packages/s/s7/xmake.lua
+++ packages/s/s7/xmake.lua
@@ -48,5 +48,5 @@ package("s7")
                     return(s7_apply_function(sc, old_string_append, args));
                 return(s7_apply_function(sc, old_add, args));
             }
-        ]], {configs = {links = {"dl", "s7"}}, includes = "s7.h"}))
+        ]], {includes = "s7.h"}))
     end)
