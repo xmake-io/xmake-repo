b117685058ba530b7d69903ce42d52886d097974
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-05-30 09:37:51 +0200

    update sha256 of tarballs

--- packages/m/miniaudio/xmake.lua
+++ packages/m/miniaudio/xmake.lua
@@ -5,9 +5,9 @@ package("miniaudio")
 
     set_urls("https://github.com/mackron/miniaudio/archive/refs/tags/${version}.tar.gz",
              "https://github.com/mackron/miniaudio.git")
-    add_versions("0.11.15", "26b0a9ffc0b2e9d0528106f2f5e4dfc90b0a0d6b")
-    add_versions("0.11.16", "ea205fb7b0b63613f7586a4082ec9c42a0381920")
-    add_versions("0.11.17", "d76b9a1ac424b5b259c2faeea0dc83d215df522a")
+    add_versions("0.11.15", "24a6d38fe69cd42d91f6c1ad211bb559f6c89768c4671fa05b8027f5601d5457")
+    add_versions("0.11.16", "13320464820491c61bd178b95818fecb7cd0e68f9677d61e1345df6be8d4d77e")
+    add_versions("0.11.17", "4b139065f7068588b73d507d24e865060e942eb731f988ee5a8f1828155b9480")
 
 
     on_install(function (package)
