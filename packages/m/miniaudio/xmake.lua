8d45f18caaec174fd2519ab29dc852c77be8e586
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-05-30 09:33:39 +0200

    add tar urls

--- packages/m/miniaudio/xmake.lua
+++ packages/m/miniaudio/xmake.lua
@@ -3,7 +3,8 @@ package("miniaudio")
     set_homepage("https://miniaud.io")
     set_description("Single file audio playback and capture library written in C.")
 
-    add_urls("https://github.com/mackron/miniaudio.git")
+    set_urls("https://github.com/mackron/miniaudio/archive/refs/tags/${version}.tar.gz",
+             "https://github.com/mackron/miniaudio.git")
     add_versions("0.11.15", "26b0a9ffc0b2e9d0528106f2f5e4dfc90b0a0d6b")
     add_versions("0.11.16", "ea205fb7b0b63613f7586a4082ec9c42a0381920")
     add_versions("0.11.17", "d76b9a1ac424b5b259c2faeea0dc83d215df522a")
