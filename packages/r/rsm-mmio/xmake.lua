f9a736dc4f51b5afb23ae64a8ef031e633d714ca
Qudix <17361645+Qudix@users.noreply.github.com>
2023-06-07 17:10:16 -0500

    [rsm-mmio] License

--- packages/r/rsm-mmio/xmake.lua
+++ packages/r/rsm-mmio/xmake.lua
@@ -1,6 +1,7 @@
 package("rsm-mmio")
     set_homepage("https://github.com/Ryan-rsm-McKenzie/mmio")
     set_description("A cross-platform memory-mapped io library for C++")
+    set_license("MIT")
 
     add_urls("https://github.com/Ryan-rsm-McKenzie/mmio/archive/refs/tags/$(version).tar.gz")
     add_versions("2.0.0", "360dddf74a97bd0a7eb41378cc59f2a69871dabfd36c55bf027429ac54930d5b")
