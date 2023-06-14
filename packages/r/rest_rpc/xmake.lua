46238a36d1c27c044351f2de3b9520dc539e484b
cat <wacshuiguo@163.com>
2023-06-14 21:40:41 +0800

    add rest_rpc

---
+++ packages/r/rest_rpc/xmake.lua
@@ -0,0 +1,27 @@
+package("rest_rpc")
+
+    set_homepage("https://github.com/qicosmos/rest_rpc")
+    set_description("c++11, high performance, cross platform, easy to use rpc framework.")
+
+    add_urls("https://github.com/qicosmos/rest_rpc.git")
+    add_versions("2023.6.14", "8782f1d341e1dd18f9fe3a77b8335fd17a5ba585")
+
+    add_deps("asio", "msgpack-cxx")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "rest_rpc.hpp"
+            int main(){
+                rest_rpc::rpc_client client("127.0.0.1", 9000);
+                client.connect();
+
+                int result = client.call<int>("add", 1, 2);
+
+                client.run();
+            }
+        ]]}))
+    end)
