141b6d35abb51decccd2641123b48a33cd6d6173
SirLynix <lynix680@gmail.com>
2023-06-18 20:49:11 +0200

    libflac: Add 1.4.2 patch

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -1,5 +1,4 @@
 package("libflac")
-
     set_homepage("https://xiph.org/flac")
     set_description("Free Lossless Audio Codec")
     set_license("BSD")
@@ -9,6 +8,7 @@ package("libflac")
 
     add_versions("1.4.2", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
     add_versions("1.3.3", "668cdeab898a7dd43cf84739f7e1f3ed6b35ece2ef9968a5c7079fe9adfe1689")
+    add_patches("1.4.2", path.join(os.scriptdir(), "patches", "1.4.2", "cmake.patch"), "0a99382d5d7bd33078572b6cc3af08ee7e5e3618c80754a5fdc400bd69f4e470")
     add_patches("1.3.3", path.join(os.scriptdir(), "patches", "1.3.3", "cmake.patch"), "49baa40ab70d63e74cfc3f0cc2f13824545a618ceaeffdd51d3333d90b37fd32")
     
     if is_plat("mingw") and is_subhost("msys") then
