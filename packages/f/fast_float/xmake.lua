3e2a796ad932d4663550c87e48dee3ceb7f57390
Jérôme Leclercq <lynix680@gmail.com>
2023-06-19 05:35:17 +0200

    Update fast_float to v5.2.0 (#2190)

--- packages/f/fast_float/xmake.lua
+++ packages/f/fast_float/xmake.lua
@@ -9,6 +9,7 @@ package("fast_float")
     add_versions("v3.5.1", "8558bf9c66ccd2f7d03c94461a107f49ad9cf6e4f6c0c84e148fec0aa32b4dd9")
     add_versions("v3.4.0", "a242877d2fae81ca412033f5ebf5dbc43cb029c56b4af78e33106b9a69f8f58e")
     add_versions("v3.10.1", "d162c21c1dc538dbc6b3bb6d1317a7808f2eccef78638445630533f5bed902ee")
+    add_versions("v5.2.0", "172dbbf468f2d83896081a22c4d65964cd0fa3dfda7926bfbd6da8b7d4d91d75")
 
     if is_plat("wasm") then
         add_patches("v3.4.0", path.join(os.scriptdir(), "patches", "emscripten_fix.patch"), "482705431f67e6f0a375ed7bfe87d6856e7d13f071db6157e1d5659834b0eb50")
