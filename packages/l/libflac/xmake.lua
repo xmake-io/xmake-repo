ec719aefa5d971378b01d556c0e9408baae9f115
Jérôme Leclercq <lynix680@gmail.com>
2023-06-19 08:55:55 +0200

    Fix version hash

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -6,7 +6,7 @@ package("libflac")
     set_urls("https://github.com/xiph/flac/archive/$(version).tar.gz",
              "https://github.com/xiph/flac.git")
 
-    add_versions("1.4.2", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
+    add_versions("1.4.2", "8e8e0406fb9e1d177bb4ba8cfed3ca3935d37144eac8f0219a03e8c1ed5cc18e")
     add_versions("1.3.3", "668cdeab898a7dd43cf84739f7e1f3ed6b35ece2ef9968a5c7079fe9adfe1689")
     add_patches("1.4.2", path.join(os.scriptdir(), "patches", "1.4.2", "cmake.patch"), "0a99382d5d7bd33078572b6cc3af08ee7e5e3618c80754a5fdc400bd69f4e470")
     add_patches("1.3.3", path.join(os.scriptdir(), "patches", "1.3.3", "cmake.patch"), "49baa40ab70d63e74cfc3f0cc2f13824545a618ceaeffdd51d3333d90b37fd32")
