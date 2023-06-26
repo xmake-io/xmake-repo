e10a597aeec2bd66e635f29402aee6fbd7d4cd41
Jérôme Leclercq <lynix680@gmail.com>
2023-06-26 13:54:49 +0200

    Update xmake.lua

--- packages/n/nuklear/xmake.lua
+++ packages/n/nuklear/xmake.lua
@@ -10,6 +10,11 @@ package("nuklear")
 
     add_versions("4.10.5", "6c80cbd0612447421fa02ad92f4207da2cd019a14d94885dfccac1aadc57926a")
 
+    on_load("mingw", function (package)
+        -- see https://github.com/Immediate-Mode-UI/Nuklear/issues/320
+        package:add("defines", "NK_INCLUDE_FIXED_TYPES")
+    end)
+
     on_install(function (package)
         os.cp("nuklear.h", package:installdir("include"))
     end)
