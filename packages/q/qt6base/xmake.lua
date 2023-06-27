5a21f81a5efd01fe74611f15d80c0f57322b9af3
Jérôme Leclercq <lynix680@gmail.com>
2023-06-27 08:57:49 +0200

    Update xmake.lua

--- packages/q/qt6base/xmake.lua
+++ packages/q/qt6base/xmake.lua
@@ -3,6 +3,8 @@ package("qt6base")
     set_base("qtbase")
 
     add_versions("6.3.0", "dummy")
+    add_versions("6.5.1", "dummy")
+    add_versions("6.6.0", "dummy")
 
     on_install("windows|x64", "linux|x86_64", "macosx|x86_64", "mingw|x86_64", "android", "iphoneos", function (package)
         package:base():script("install")(package)
