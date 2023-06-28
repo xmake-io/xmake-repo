13522c542053386c4e22dce53f3cf55c5c392af0
ruki <waruqi@gmail.com>
2023-06-28 23:07:40 +0800

    fix readline

--- packages/l/libxmake/xmake.lua
+++ packages/l/libxmake/xmake.lua
@@ -8,8 +8,6 @@ package("libxmake")
              "https://github.com/xmake-io/xmake.git",
              "https://gitlab.com/tboox/xmake.git")
 
-    add_versions("v2.5.9", "5b50e3f28956cabcaa153624c91781730387ceb7c056f3f9b5306b1c77460d8f")
-    add_versions("v2.7.1", "e44085090641547d8814afcc345d641d8ce1e38b6e05fee7375fc88150c0803d")
     add_versions("v2.7.9", "9b42d8634833f4885b05b89429dd60044dca99232f6096320b8d857fb33d2aef")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
@@ -24,10 +22,10 @@ package("libxmake")
     elseif is_plat("android") then
         add_syslinks("m", "c")
     elseif is_plat("macosx") then
-        add_ldflags("-all_load", "-pagezero_size 10000", "-image_base 100000000")
         add_frameworks("CoreFoundation", "CoreServices")
+        add_syslinks("readline")
     else
-        add_syslinks("pthread", "dl", "m", "c")
+        add_syslinks("readline", "pthread", "dl", "m", "c")
     end
     add_defines("LUA_COMPAT_5_1", "LUA_COMPAT_5_2", "LUA_COMPAT_5_3")
 
@@ -37,9 +35,6 @@ package("libxmake")
             package:add("links", "pdcurses")
         else
             package:add("deps", "ncurses")
-        end
-        if package:config("readline") then
-            package:add("links", "readline")
         end
         if package:debug() then
             package:add("defines", "__tb_debug__")
