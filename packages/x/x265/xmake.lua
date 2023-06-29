4c388ed05f3b35697ca74df911a585d3641c2d27
冰雪殇璃陌梦 <dreamofice@outlook.com>
2023-06-29 18:29:16 +0800

    feat(x265): add more versions

--- packages/x/x265/xmake.lua
+++ packages/x/x265/xmake.lua
@@ -4,10 +4,13 @@ package("x265")
     set_description("A free software library and application for encoding video streams into the H.265/MPEG-H HEVC compression format.")
 
     add_urls("https://github.com/videolan/x265/archive/$(version).tar.gz",
-             "https://github.com/videolan/x265.git")
+             "https://github.com/videolan/x265.git",
+             "https://bitbucket.org/multicoreware/x265_git")
 
+    add_versions("3.2", "4dd707648ea90b96bf1f8ea6a36ed21c11fe3a9048923909c5b629755ca8d8f3")
+    add_versions("3.2.1", "b5ee7ea796a664d6e2763f9c0ae281fac5d25892fc2cb134698547103466a06a")
+    add_versions("3.3", "ca25a38772fc6b49e5f1aa88733bc1dc92da7dc18f02a85cc3e99d76ba85b0a9")
     add_versions("3.4", "544d147bf146f8994a7bf8521ed878c93067ea1c7c6e93ab602389be3117eaaf")
-
     add_deps("cmake", "nasm")
 
     if is_plat("macosx") then
