c758101b9eadae663cf15be36b1e1a90e8d168f4
冰雪殇璃陌梦 <dreamofice@outlook.com>
2023-07-01 21:50:24 +0800

    feat(libvpx): add configs

--- packages/l/libvpx/xmake.lua
+++ packages/l/libvpx/xmake.lua
@@ -1,6 +1,7 @@
 package("libvpx")
     set_homepage("http://www.webmproject.org/code/")
     set_description("VP8/VP9 Codec SDK")
+    set_license("BSD-3")
 
     add_urls("https://github.com/webmproject/libvpx.git",
         "https://chromium.googlesource.com/webm/libvpx.git",
@@ -11,36 +12,50 @@ package("libvpx")
     add_versions("1.12.0", "f1acc15d0fd0cb431f4bf6eac32d5e932e40ea1186fe78e074254d6d003957bb")
     add_versions("1.13.0", "cb2a393c9c1fae7aba76b950bb0ad393ba105409fe1a147ccd61b0aaa1501066")
 
-    if is_plat("wasm") then
-        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-    end
     if is_arch("x86.*") then
         add_deps("yasm")
     end
 
-    on_install("linux", "macosx", "mingw", "freebsd", "cross", function (package)
+    add_configs("examples",         {description = "examples", default = false, type = "boolean"})
+    add_configs("tools",            {description = "tools", default = false, type = "boolean"})
+    add_configs("docs",             {description = "documentation", default = false, type = "boolean"})
+    add_configs("unit-tests",       {description = "unit tests", default = false, type = "boolean"})
+
+    add_configs("vp8",              {description = "VP8 codec support", default = true, type = "boolean"})
+    add_configs("vp9",              {description = "VP9 codec support", default = true, type = "boolean"})
+    add_configs("multithread",      {description = "multithreaded encoding and decoding", default = true, type = "boolean"})
+    add_configs("webm-io",          {description = "enable input from and output to WebM container", default = false, type = "boolean"})
+    add_configs("libyuv",           {description = "enable libyuv", default = false, type = "boolean"})
+    add_configs("postproc",         {description = "postprocessing", default = false, type = "boolean"})
+    add_configs("vp9-postproc",     {description = "vp9 specific postprocessing", default = false, type = "boolean"})
+    add_configs("vp9-highbitdepth", {description = "use VP9 high bit depth (10/12) profiles", default = false, type = "boolean"})
+
+    if is_plat("wasm") then
+        add_configs("shared",  {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
+    on_install("linux", "macosx", "mingw", "freebsd", "wasm", "cross", function (package)
         local configs = {}
-        table.insert(configs, "--enable-" .. (package:config("shared") and "shared" or "static"))
-        if package:is_cross() then
+        table.insert(configs, "--prefix=" .. package:installdir())
+        for name, enabled in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                table.insert(configs, "--" .. (enabled and "enable" or "disable") .. "-" .. name)
+            end
+        end
+
+        if package:is_plat("wasm") then
+            table.join2(configs, {"--target=generic-gnu", "--disable-install-bins"})
+        elseif package:is_cross() then
             table.insert(configs, "--target=" .. package:targetarch() .. "-" .. package:targetos())
         end
-        table.insert(configs, "--prefix=" .. package:installdir())
 
         local source_dir = os.curdir()
         os.cd("$(buildir)")
-        os.vrunv(path.join(source_dir, "configure"), configs)
-        import("package.tools.make").install(package)
-    end)
-
-    on_install("wasm", function (package)
-        local configs = {}
-        table.join2(configs, {"--target=generic-gnu", "--disable-install-bins"})
-        table.insert(configs, "--prefix=" .. package:installdir())
-        
-        local source_dir = os.curdir()
-        os.cd("$(buildir)")
-        os.vrunv("emconfigure " .. path.join(source_dir, "/configure"), configs)
-
+        if package:is_plat("wasm") then
+            os.vrunv("emconfigure " .. path.join(source_dir, "/configure"), configs)
+        else
+            os.vrunv(path.join(source_dir, "configure"), configs)
+        end
         import("package.tools.make").install(package)
     end)
 
