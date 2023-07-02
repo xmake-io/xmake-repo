bc15a8a57e35da36b60c60d205f8ba14d506795c
冰雪殇璃陌梦 <dreamofice@outlook.com>
2023-07-03 07:59:26 +0800

    fix: fix typos

--- packages/l/libvpx/xmake.lua
+++ packages/l/libvpx/xmake.lua
@@ -33,9 +33,9 @@ package("libvpx")
     on_load(function (package)
         if is_arch("x86.*") then
             if package:is_plat("freebsd") or (package:is_plat("linux") and linuxos.name == "fedora") then
-                add_deps("nasm")
+                package:add("deps", "nasm")
             else
-                add_deps("yasm")
+                package:add("deps", "yasm")
             end
         end
     end)
