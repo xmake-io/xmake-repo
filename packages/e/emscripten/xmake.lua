cb1d455dd73c1a6e2d32a0558c37d56535c3edd5
A2va <49582555+A2va@users.noreply.github.com>
2023-07-03 16:34:45 +0200

    Use find_directory

--- packages/e/emscripten/xmake.lua
+++ packages/e/emscripten/xmake.lua
@@ -11,6 +11,7 @@ package("emscripten")
     add_deps("python")
 
     on_install("windows", "macosx", "linux", function (package)
+        import("lib.detect.find_directory")
 		-- copy to installdir
         os.cp("*", package:installdir())
 	
@@ -18,28 +19,25 @@ package("emscripten")
         local version = package:version():rawstr()
         local installdir = package:installdir()
         local py = package:is_plat("windows") and "python" or "python3"
-
         os.vrunv(py, {path.join(installdir, "emsdk.py"), "install", version})
 
         -- activation
         os.vrunv(py, {path.join(installdir, "emsdk.py"), "activate", version})
 
         -- setup env
-        local node = os.dirs(path.join(installdir, "node/**"))[1]
+        local node = find_directory("bin", {path.join(installdir, "node", "**")})
         package:addenv("PATH", node)
-
         package:addenv("PATH", path.join(installdir, "upstream", "emscripten"))
         package:addenv("PATH", installdir)
         package:addenv("EMSDK", installdir)
-
+    
         local exe = package:is_plat("windows") and ".exe" or ""
 
-        package:addenv("EMSDK_NODE", path.join(node, "bin", "node"..exe))
+        package:addenv("EMSDK_NODE", path.join(node, "node" .. exe))
         if package:is_plat("windows") then
-            local python = os.dirs(path.join(installdir, "python/**"))[1]
-            package:addenv("EMSDK_PYTHON", path.join(python, "python"..exe))
-
-            local java = os.dirs(path.join(installdir, "java/**"))[1]
+            local python = find_directory("*", path.join(installdir, "python"))
+            package:addenv("EMSDK_PYTHON", path.join(python, "python" .. exe))
+            local java = find_directory("*", path.join(installdir, "java"))
             package:addenv("JAVA_HOME", java)
         end
     end)
