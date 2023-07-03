a7192e46acf5a0eaf96e7e2e19f4ce1943bd4a30
沈浪熊猫儿 <shenda@ustc.edu>
2023-07-03 22:59:42 +0800

    libpng: update url and use version without v

--- packages/l/libpng/xmake.lua
+++ packages/l/libpng/xmake.lua
@@ -3,13 +3,13 @@ package("libpng")
     set_description("The official PNG reference library")
     set_license("libpng-2.0")
 
-    set_urls("https://github.com/glennrp/libpng/archive/$(version).zip",
+    set_urls("https://github.com/glennrp/libpng/archive/refs/tags/v$(version).tar.gz",
              "https://github.com/glennrp/libpng.git")
-    add_versions("v1.6.40", "ab3f88779f0661bbb07c60e778fda782216bff70355d86848fbf6a327084563a")
-    add_versions("v1.6.37", "c2c50c13a727af73ecd3fc0167d78592cf5e0bca9611058ca414b6493339c784")
-    add_versions("v1.6.36", "6274d3f761cc80f7f6e2cde6c07bed10c00bc4ddd24c4f86e25eb51affa1664d")
-    add_versions("v1.6.35", "3d22d46c566b1761a0e15ea397589b3a5f36ac09b7c785382e6470156c04247f")
-    add_versions("v1.6.34", "7ffa5eb8f9f3ed23cf107042e5fec28699718916668bbce48b968600475208d3")
+    add_versions("1.6.40", "ab3f88779f0661bbb07c60e778fda782216bff70355d86848fbf6a327084563a")
+    add_versions("1.6.37", "c2c50c13a727af73ecd3fc0167d78592cf5e0bca9611058ca414b6493339c784")
+    add_versions("1.6.36", "6274d3f761cc80f7f6e2cde6c07bed10c00bc4ddd24c4f86e25eb51affa1664d")
+    add_versions("1.6.35", "3d22d46c566b1761a0e15ea397589b3a5f36ac09b7c785382e6470156c04247f")
+    add_versions("1.6.34", "7ffa5eb8f9f3ed23cf107042e5fec28699718916668bbce48b968600475208d3")
 
     add_deps("zlib")
 
