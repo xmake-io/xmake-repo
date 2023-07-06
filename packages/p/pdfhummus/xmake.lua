225eb44863db91f21e02e3426e9dbe4bd3694f31
charonxin <1656746727@qq.com>
2023-07-06 18:30:18 +0800

    modify some details

--- packages/p/pdfhummus/xmake.lua
+++ packages/p/pdfhummus/xmake.lua
@@ -3,9 +3,12 @@ package("pdfhummus")
     set_description("High performance library for creating, modiyfing and parsing PDF files in C++ ")
     set_license("Apache-2.0")
 
-    set_urls("https://gitee.com/XmacsLabs/pdfhummus.git")
-    add_versions("v4.5.7", "191f39dcc61522c50a3bee96b3f0ccef208c27968fd22d5ab474acb512c24d10")
-    add_versions("v4.5.6", "72a4f3fbbc39d4f169911454e9977ef8dd882bd5c0a305bd3133347903cff705")
+    add_urls("https://github.com/galkahana/PDF-Writer/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/galkahana/PDF-Writer/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/galkahana/PDF-Writer.git")
+    add_versions("4.1", "0c0d860b0ecea928709b9e4642fa21926eb2f626f702699c3b87afa2965b4857")
+    add_versions("4.5.6", "72a4f3fbbc39d4f169911454e9977ef8dd882bd5c0a305bd3133347903cff705")
+    add_versions("4.5.7", "191f39dcc61522c50a3bee96b3f0ccef208c27968fd22d5ab474acb512c24d10")
 
     add_deps("freetype", "zlib", "libaesgm")
 
