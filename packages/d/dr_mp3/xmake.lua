c1e0f0e080beaeaa7448074507679cc5739e89ba
Jérôme Leclercq <lynix680@gmail.com>
2023-07-08 16:44:47 +0200

    Update dr_mp3 to 0.6.37 (#2291)

--- packages/d/dr_mp3/xmake.lua
+++ packages/d/dr_mp3/xmake.lua
@@ -5,6 +5,7 @@ package("dr_mp3")
     set_license("MIT")
 
     set_urls("https://github.com/mackron/dr_libs.git")
+    add_versions("0.6.37", "1b0bc87c6b9b04052e6ef0117396dab8482c250e")
     add_versions("0.6.36", "b7f4c04e77b4c14347b74503e4ce93494e314283")
     add_versions("0.6.34", "dd762b861ecadf5ddd5fb03e9ca1db6707b54fbb")
     add_versions("0.6.27", "f357ade3aae55ced341aa7c83b4e7f628f948e51")
