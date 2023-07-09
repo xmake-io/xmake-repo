2c51c2c1a3fa5befe1bbb84a75ecaa7df389eebb
jj683 <jj683@protonmail.com>
2023-07-09 13:07:36 +0300

    Add missing add_configs

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -17,6 +17,8 @@ package("folly")
     add_patches("<=2023.07.03", path.join(os.scriptdir(), "patches", "2021.06.28", "reorder.patch"), "9a6bf283881580474040cfc7a8e89d461d68b89bae5583d89fff0a3198739980")
     add_patches("<=2022.08.29", path.join(os.scriptdir(), "patches", "2021.06.28", "regex.patch"), "6a77ade9f48dd9966d3f7154e66ca8a5c030ae2b6d335cbe3315784aefd8f495")
 
+    add_configs("no_exception_tracer", {description = "Exclude exception tracer, which is necessary to statically link libstdc++.", default = false, type = "boolean"})
+
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
