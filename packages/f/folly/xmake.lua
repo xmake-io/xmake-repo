88a221c68c8dc23039fb2ebd2ca0354bacac3294
Aleksei Safronov <asafrnv@artis.team>
2023-07-11 20:52:18 +0300

    Disable pkgconfig file generation on windows

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -14,8 +14,12 @@ package("folly")
     add_versions("2022.08.29", "3adac6d4b203c2917185fd190fc04d615051cb2a4f6b988ddf3c42034efc8d4d")
     add_versions("2023.07.03", "e49901ac87bca18c0a2f71af340d63f5573bae463fdeb0c32092c6bf45621474")
 
-    add_patches("<=2023.07.03", path.join(os.scriptdir(), "patches", "2021.06.28", "reorder.patch"), "9a6bf283881580474040cfc7a8e89d461d68b89bae5583d89fff0a3198739980")
     add_patches("<=2022.08.29", path.join(os.scriptdir(), "patches", "2021.06.28", "regex.patch"), "6a77ade9f48dd9966d3f7154e66ca8a5c030ae2b6d335cbe3315784aefd8f495")
+    add_patches("<=2023.07.03", path.join(os.scriptdir(), "patches", "2021.06.28", "reorder.patch"), "9a6bf283881580474040cfc7a8e89d461d68b89bae5583d89fff0a3198739980")
+
+    if is_plat("windows") then
+        add_patches("2023.07.03", path.join(os.scriptdir(), "patches", "2023.07.03", "pkgconfig.patch"), "b95c114b91519d21e62fd95508e3ded70204d74f60d06f2f68cd77bd1c9a6c9e")
+    end
 
     add_configs("no_exception_tracer", {description = "Exclude exception tracer, which is necessary to statically link libstdc++.", default = false, type = "boolean"})
 
