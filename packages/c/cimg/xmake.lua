879e98dc67537957c76c5a769c1776b6f46e64c6
Qudix <17361645+Qudix@users.noreply.github.com>
2023-07-12 13:19:47 -0500

    [cimg] Exclude `iphoneos`

--- packages/c/cimg/xmake.lua
+++ packages/c/cimg/xmake.lua
@@ -10,7 +10,7 @@ package("cimg")
     add_urls("https://github.com/greyclab/cimg.git")
     add_versions("v3.2.6", "1fcca9a7a453aa278660c10d54c6db9b4c614b6a29250adeb231e95a0be209e7")
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "android", "mingw", "cygwin", "bsd", "cross", function (package)
         os.cp("CImg.h", package:installdir("include"))
     end)
 
