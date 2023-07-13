2078b387df1dc2e443f919ed0e546610eccd3671
冰雪殇璃陌梦 <dreamofice@outlook.com>
2023-07-13 12:45:55 +0800

    fix(wavpack): fix cross compile error

--- packages/w/wavpack/xmake.lua
+++ packages/w/wavpack/xmake.lua
@@ -17,6 +17,9 @@ package("wavpack")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_cross() then
+            table.insert(configs, "-DCMAKE_FIND_ROOT_PATH=ONLY")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
