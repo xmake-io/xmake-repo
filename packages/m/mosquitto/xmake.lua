c20d23b84d792fa158edf8571d29b53eaae4dedc
air <2713651268@qq.com>
2023-07-14 13:16:07 +0800

    Optimize mosquitto pacakage

--- packages/m/mosquitto/xmake.lua
+++ packages/m/mosquitto/xmake.lua
@@ -6,13 +6,15 @@ package("mosquitto")
     add_urls("https://github.com/eclipse/mosquitto/archive/refs/tags/$(version).tar.gz",
              "https://github.com/eclipse/mosquitto.git")
  
-    add_versions("v2.0.15", "26dc3f1758b00c1725a0e4dd32f40c61f374375717f09b6af2bac62c5b44f1eb")
+    add_versions("v2.0.15", "547f98acd2e4668c8f3b86ef61e71c755366d180565b6e7537813876467d04d9")
 
-    add_patches("v2.0.15", path.join(os.scriptdir(), "patches", "cmake.patch"),"d81634251dec42facea2bd5c50b3d1988426003a1a4b7e57543e65a92eb051f1")
- 
-    add_configs("ssl", {description = "Include SSL/TLS support", default = true, type = "boolean"})
+    if is_plat("windows") then
+        add_patches("v2.0.15", path.join(os.scriptdir(), "patches", "cmake.patch"),"b241fb965f3d00bad1fddf060fe9b99cba83df32c373a4eaee2289e05abd26b6")
+    end 
+
+    add_configs("tls", {description = "Include SSL/TLS support", default = true, type = "boolean"})
     add_configs("cjson", {description = "Build with cJSON support (required for dynamic security plugin and useful for mosquitto_sub)", default = true, type = "boolean"})
-    add_configs("bundled", {description = "Build with bundled dependencies", default = true, type = "boolean"})
+    add_configs("bundled_deps", {description = "Build with bundled dependencies", default = true, type = "boolean"})
     add_configs("websockets", {description = "Include websockets support", default = false, type = "boolean"})
  
     add_configs("tls_psk", {description = "Include TLS-PSK support (requires WITH_TLS)", default = true, type = "boolean"})
@@ -26,9 +28,9 @@ package("mosquitto")
     add_configs("dlt", {description = "Include DLT support", default = false, type = "boolean"})
  
     add_configs("cpp", {description = "Build C++ library", default = true, type = "boolean"})
-    add_configs("client", {description = "Build clients", default = false, type = "boolean"})
-    add_configs("broker", {description = "Build broker", default = true, type = "boolean"})
-    add_configs("apps", {description = "Build apps", default = true, type = "boolean"})
+    add_configs("clients", {description = "Build clients", default = false, type = "boolean"})
+    add_configs("broker", {description = "Build broker", default = false, type = "boolean"})
+    add_configs("apps", {description = "Build apps", default = false, type = "boolean"})
     add_configs("plugins", {description = "Build plugins", default = false , type = "boolean"})
  
     add_deps("cmake")
@@ -39,11 +41,11 @@ package("mosquitto")
  
     local deps_map =
     {
-        ssl        = "openssl",
-        cjson      = "cjson",
-        bundled    = "uthash",
-        srv        = "c-ares",
-        websockets = "libwebsockets",
+        tls             = "openssl",
+        cjson           = "cjson",
+        bundled_deps    = "uthash",
+        srv             = "c-ares",
+        websockets      = "libwebsockets",
     }
 
     if is_plat("linux") then
@@ -66,38 +68,31 @@ package("mosquitto")
         local configs ={"-DDOCUMENTATION=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DWITH_STATIC_LIBRARIES=" .. (package:config("shared") and "OFF" or "ON"))
-        if is_plat("linux") then
-            table.insert(configs, "-DWITH_THREADING=" .. (package:config("threading") and "ON" or "OFF"))
-            table.insert(configs, "-DWITH_SRV=" .. (package:config("srv") and "ON" or "OFF"))
-        else
-            table.insert(configs, "-DWITH_THREADING=OFF")
-            table.insert(configs, "-DWITH_SRV=OFF")
-        end
  
-        local configs_map =
+        local configs_list =
         {
-            ssl     = "WITH_TLS",
-            cjson   = "WITH_CJSON",
-            bundled = "WITH_BUNDLED_DEPS",
-            websockets = "WITH_WEBSOCKETS",
+            "tls",
+            "cjson",
+            "bundled_deps",
+            "websockets",
  
-            tls_psk      = "WITH_TLS_PSK",
-            ec           = "WITH_EC",
-            unix_sockets = "WITH_UNIX_SOCKETS",
-            socks        = "WITH_SOCKS",
-            srv          = "WITH_SRV",
-            threading    = "WITH_THREADING",
-            dlt          = "WITH_DLT",
+            "tls_psk",
+            "ec",
+            "unix_sockets",
+            "socks",
+            "srv",
+            "threading",
+            "dlt",
  
-            cpp     = "WITH_LIB_CPP",
-            client  = "WITH_CLIENTS",
-            broker  = "WITH_BROKER",
-            apps    = "WITH_APPS",
-            plugins = "WITH_PLUGINS",
+            "cpp",
+            "clients",
+            "broker", 
+            "apps",  
+            "plugins", 
         }
- 
-        for key, value in pairs(configs_map) do
-            table.insert(configs, "-D" .. value .. "=" .. (package:config(key) and "ON" or "OFF"))
+
+        for _,value in pairs(configs_list) do
+            table.insert(configs, "-DWITH_" .. value:upper() .. "=" .. (package:config(value) and "ON" or "OFF"))
         end
  
         local packagedeps = {}
@@ -108,7 +103,8 @@ package("mosquitto")
         end
         import("package.tools.cmake").install(package, configs, {packagedeps = packagedeps})
     end)
-
+    
+ 
     on_test(function (package)
         assert(package:check_csnippets({test = [[
             #include <mosquitto.h>
