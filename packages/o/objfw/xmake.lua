fd9010ba6ab2e0cfcddf964ee3f2c11f4bd0c27f
ruki <waruqi@gmail.com>
2023-07-17 10:43:35 +0800

    Update xmake.lua

--- packages/o/objfw/xmake.lua
+++ packages/o/objfw/xmake.lua
@@ -12,27 +12,26 @@ package("objfw")
         add_frameworks("CoreFoundation")
     end
 
-    add_configs("tls", { description = "Enable TLS support.", default = true, values = { true, false, "yes", "no", "openssl", "gnutls", "securetransport" } })
+    add_configs("tls", { description = "Enable TLS support.", default = true, values = { true, false, "openssl", "gnutls", "securetransport" } })
     add_configs("rpath", { description = "Enable rpath.", default = true, type = "boolean" })
-    --On macOS, you must use the system's runtime
     add_configs("runtime", { description = "Use the included runtime, not recommended for macOS!", default = not is_plat("macosx"), type = "boolean" })
     add_configs("seluid24", { description = "Use 24 bit instead of 16 bit for selector UIDs.", default = false, type = "boolean" })
-    add_configs("unicode-tables", { description = "Enable Unicode tables.", default = true, type = "boolean" })
+    add_configs("unicode_tables", { description = "Enable Unicode tables.", default = true, type = "boolean" })
 
-    add_configs("codepage-437", { description = "Enable codepage 437 support.", default = true, type = "boolean" })
-    add_configs("codepage-850", { description = "Enable codepage 850 support.", default = true, type = "boolean" })
+    add_configs("codepage_437", { description = "Enable codepage 437 support.", default = true, type = "boolean" })
+    add_configs("codepage_850", { description = "Enable codepage 850 support.", default = true, type = "boolean" })
     add_configs("codepage-858", { description = "Enable codepage 858 support.", default = true, type = "boolean" })
-    add_configs("iso-8859-2", { description = "Enable ISO-8859-2 support.", default = true, type = "boolean" })
-    add_configs("iso-8859-3", { description = "Enable ISO-8859-3 support.", default = true, type = "boolean" })
-    add_configs("iso-8859-15", { description = "Enable ISO-8859-15 support.", default = true, type = "boolean" })
-    add_configs("koi8-r", { description = "Enable KOI8-R support.", default = true, type = "boolean" })
-    add_configs("koi8-u", { description = "Enable KOI8-U support.", default = true, type = "boolean" })
-    add_configs("mac-roman", { description = "Enable Mac Roman encoding support.", default = true, type = "boolean" })
-    add_configs("windows-1251", { description = "Enable windows 1251 support.", default = true, type = "boolean" })
-    add_configs("windows-1252", { description = "Enable windows 1252 support.", default = true, type = "boolean" })
+    add_configs("iso_8859_2", { description = "Enable ISO-8859-2 support.", default = true, type = "boolean" })
+    add_configs("iso_8859_3", { description = "Enable ISO-8859-3 support.", default = true, type = "boolean" })
+    add_configs("iso_8859_15", { description = "Enable ISO-8859-15 support.", default = true, type = "boolean" })
+    add_configs("koi8_r", { description = "Enable KOI8-R support.", default = true, type = "boolean" })
+    add_configs("koi8_u", { description = "Enable KOI8-U support.", default = true, type = "boolean" })
+    add_configs("mac_roman", { description = "Enable Mac Roman encoding support.", default = true, type = "boolean" })
+    add_configs("windows_1251", { description = "Enable windows 1251 support.", default = true, type = "boolean" })
+    add_configs("windows_1252", { description = "Enable windows 1252 support.", default = true, type = "boolean" })
 
     add_configs("threads", { description = "Enable threads.", default = true, type = "boolean" })
-    add_configs("compiler-tls", { description = "Enable compiler thread local storage (TLS).", default = true, type = "boolean" })
+    add_configs("compiler_tls", { description = "Enable compiler thread local storage (TLS).", default = true, type = "boolean" })
     add_configs("files", { description = "Enable files.", default = true, type = "boolean" })
     add_configs("sockets", { description = "Enable sockets.", default = true, type = "boolean" })
 
@@ -40,64 +39,39 @@ package("objfw")
 
     on_install("linux", "macosx", "cygwin", "mingw", function (package)
         local configs = {}
-
         local tls = package:config("tls")
-        tls = tls == true and "yes" or tls == false and "no" or tls
-        table.insert(configs, "--enable-tls=" .. tls)
-
-        local function config(cfg)
-            if package:config(cfg) then
-                table.insert(configs, "--enable-" .. cfg)
-            else
-                table.insert(configs, "--disable-" .. cfg)
-            end
+        if type(tls) == "boolean" then
+            tls = tls and "yes" or "no"
         end
-
-        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-
-        config("threads")
-        config("compiler-tls")
-        config("files")
-        config("sockets")
-        config("runtime")
-        config("seluid24")
-        config("unicode-tables")
-        config("rpath")
-
-        config("codepage-437")
-        config("codepage-858")
-        config("codepage-850")
-        config("iso-8859-3")
-        config("iso-8859-2")
-        config("koi8-r")
-        config("koi8-u")
-        config("iso-8859-15")
-        config("mac-roman")
-        config("windows-1251")
-        config("windows-1252")
-
+        table.insert(configs, "--enable-tls=" .. tls)
         if package:is_debug() then
             table.insert(configs, "--enable-debug")
         end
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+        for name, enabled in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                name = name:gsub("_", "-")
+                if enabled then
+                    table.insert(configs, "--enable-" .. name)
+                else
+                    table.insert(configs, "--disable-" .. name)
+                end
+            end
+        end
         import("package.tools.autoconf").install(package, configs)
 
-        local objfwcfg = import("lib.detect.find_tool")("objfw-config", { paths = { package:installdir("bin") } }).program
-
-        local objcflags_str = os.iorunv(objfwcfg, { "--objcflags", (package:config("arc") and "--arc" or "") })
-        local ldflags_str = os.iorunv(objfwcfg, { "--ldflags" })
-
-        local objcflags = {}
         local ldflags = {}
-
-        for flag in objcflags_str:gmatch("%S+") do
+        local objcflags = {}
+        local objfwcfg = path.join(package:installdir("bin"), "objfw-config")
+        local objcflags_str = os.iorunv(objfwcfg, {"--objcflags", (package:config("arc") and "--arc" or "")})
+        local ldflags_str = os.iorunv(objfwcfg, {"--ldflags"})
+        for _, flag in ipairs(objcflags_str:split("%s+")) do
             table.insert(objcflags, flag)
         end
-
-        for flag in ldflags_str:gmatch("%S+") do
+        for _, flag in ipairs(ldflags_str:split("%s+")) do
             table.insert(ldflags, flag)
         end
-
         package:add("mflags", objcflags)
         package:add("ldflags", ldflags)
     end)
