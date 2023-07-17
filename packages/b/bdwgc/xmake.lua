0d956570f5877d68da975b1b69498e75244f9d7f
Faisal Alghamdi <80383169+PunchCakee@users.noreply.github.com>
2023-07-17 18:16:08 +0300

    fix stupid with the help of lynix

--- packages/b/bdwgc/xmake.lua
+++ packages/b/bdwgc/xmake.lua
@@ -7,24 +7,27 @@ package("bdwgc")
 
     add_versions("v8.2.4", "18e63ab1428bd52e691da107a6a56651c161210b11fbe22e2aa3c31f7fa00ca5")
 
-    add_deps("autoconf", "automake", "libtool")
-
     on_install(function (package)
         local configs = {}
-        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        if package:is_debug() then
-            table.insert(configs, "--enable-debug")
-        end
-        import("package.tools.autoconf").install(package, configs)
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:check_cxxsnippets({test=[[
-    
-        int main() {
+        assert(package:has_cfuncs({test=[[
+        
+        int main()
+        {
             GC_INIT();
+        
+            int *ptr = GC_MALLOC(sizeof(int));
+            *ptr = 42;
+        
+            printf("Value: %d\n", *ptr);
+        
             return 0;
         }
 
-        ]]},{configs = {includes = "gc"}}))
+        ]]}),{configs = {includes = "gc"}})
     end)
