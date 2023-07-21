1f87b241235c7de11a03e7018f80e2b6ed875769
xq114 <1140735506@qq.com>
2023-07-21 14:44:29 +0800

    fix uvatlas

--- packages/u/uvatlas/xmake.lua
+++ packages/u/uvatlas/xmake.lua
@@ -15,8 +15,15 @@ package("uvatlas")
     add_configs("tools",  {description = "Build UVAtlasTool", default = false, type = "boolean"})
 
     add_deps("cmake")
-    on_install("windows", "mingw", function (package)
+    on_install("windows", function (package)
         local configs = {"-DUVATLAS_USE_OPENMP=OFF", "-DBUILD_TESTING=OFF"}
+        local vs_sdkver = import("core.tool.toolchain").load("msvc"):config("vs_sdkver")
+        if vs_sdkver then
+            local build_ver = string.match(vs_sdkver, "%d+%.%d+%.(%d+)%.?%d*")
+            assert(tonumber(build_ver) >= 18362, "UVAtlas requires Windows SDK to be at least 10.0.18362.0")
+            table.insert(configs, "-DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION=" .. vs_sdkver)
+            table.insert(configs, "-DCMAKE_SYSTEM_VERSION=" .. vs_sdkver)
+        end
         table.insert(configs, "-DBUILD_TOOLS=" .. (package:config("tools") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
