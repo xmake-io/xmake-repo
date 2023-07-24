6afb38bc55ff05e0d409f692426c50221ceb7bb9
xq114 <1140735506@qq.com>
2023-07-24 21:49:35 +0800

    update cpuinfo

--- packages/c/cpuinfo/xmake.lua
+++ packages/c/cpuinfo/xmake.lua
@@ -1,19 +1,17 @@
 package("cpuinfo")
     set_homepage("https://github.com/pytorch/cpuinfo")
     set_description("CPU INFOrmation library (x86/x86-64/ARM/ARM64, Linux/Windows/Android/macOS/iOS)")
-    set_license("BSD 2-Clause")
+    set_license("BSD-2-Clause")
 
     add_urls("https://github.com/pytorch/cpuinfo.git")
     add_versions("2022.09.15", "de2fa78ebb431db98489e78603e4f77c1f6c5c57")
-
-    add_deps("cmake")
+    add_versions("2023.07.21", "60480b7098c8ddc73d611285fc478dec66e4edf9")
 
     if is_plat("windows") then
-        add_configs("shared",     {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-        add_configs("vs_runtime", {description = "Set vs compiler runtime.", default = "MT", readonly = true})
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
 
-    add_links("cpuinfo", "clog")
+    add_deps("cmake")
     if is_plat("linux", "macosx", "bsd") then
         add_syslinks("pthread")
     end
@@ -25,7 +23,10 @@ package("cpuinfo")
                          "-DCPUINFO_BUILD_BENCHMARKS=OFF",
                          "-DCPUINFO_BUILD_PKG_CONFIG=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCPUINFO_LIBRARY_TYPE=" .. (package:config("shared") and "shared" or "static"))
+        if package:is_plat("windows") then
+            table.insert(configs, "-DCPUINFO_RUNTIME_TYPE=" .. (package:config("vs_runtime"):startswith("MT") and "static" or "shared"))
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
