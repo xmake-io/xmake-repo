8a32735ce9d0cbd9e07d6070fcef96e78f66f223
DanielKui <kuibing@126.com>
2023-07-25 08:18:48 +0800

    Update xmake.lua
    Add protobuf-cpp 3.12.0

--- packages/p/protobuf-cpp/xmake.lua
+++ packages/p/protobuf-cpp/xmake.lua
@@ -12,6 +12,7 @@ package("protobuf-cpp")
     add_versions("3.15.8", "093e0dca5277b377c36a48a3633325dca3d92d68ac17d5700a1f7e1c3eca2793")
     add_versions("3.17.3", "fe65f4bfbd6cbb8c23de052f218cbe4ebfeb72c630847e0cca63eb27616c952a")
     add_versions("3.19.4", "a11a262a395f999f9dca83e195cc15b6c23b6d5e74133f8e3250ad0950485da1")
+    add_versions("3.12.0", "4094a9e89753b164e7dfcb8bcb64bb2a00df7ccc3875f54f7384e5a03c2300bc")
 
     add_patches("3.17.3", path.join(os.scriptdir(), "patches", "3.17.3", "field_access_listener.patch"), "ac9bdf49611b01e563fe74b2aaf1398214129454c3e18f1198245549eb281e85")
     add_patches("3.19.4", path.join(os.scriptdir(), "patches", "3.19.4", "vs_runtime.patch"), "8e73e585d29f3b9dca3c279df0b11b3ee7651728c07f51381a69e5899b93c367")
