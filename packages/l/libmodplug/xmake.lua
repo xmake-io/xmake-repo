cd1daf8e6d8fc8694f77790dd843025918446879
star9029 <hengxings783@gmail.com>
2023-07-27 21:59:00 +0800

    libmodplug: fix static on windows

--- packages/l/libmodplug/xmake.lua
+++ packages/l/libmodplug/xmake.lua
@@ -6,12 +6,21 @@ package("libmodplug")
     add_urls("https://github.com/Konstanty/libmodplug.git")
     add_versions("2022.1.31", "d1b97ed0020bc620a059d3675d1854b40bd2608d")
 
+    if is_plat("windows") then
+        add_syslinks("user32")
+    end
+
     add_deps("cmake")
 
     on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:config("shared") then
+            table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
+        else
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+            package:add("defines", "MODPLUG_STATIC")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
