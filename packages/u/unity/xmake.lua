18068b0725c72266ff3a5affd8e3dd0703c3f7c7
fwqaaq <fwqaaq@gmail.com>
2023-07-28 13:19:31 +0800

    update postions and version

--- packages/u/unity/xmake.lua
+++ packages/u/unity/xmake.lua
@@ -6,9 +6,12 @@ package("unity")
     add_urls("https://github.com/ThrowTheSwitch/Unity/archive/refs/tags/$(version).tar.gz", "https://github.com/ThrowTheSwitch/Unity")
 
     add_versions("v2.5.2", "3786de6c8f389be3894feae4f7d8680a02e70ed4dbcce36109c8f8646da2671a")
+    add_versions("v2.5.1", "5ce08ef62f5f64d18f8137b3eaa6d29199ee81d1fc952cef0eea96660a2caf47")
+    add_versions("v2.5.0", "d470165dc46652cf73fda54e2650b483b94a7e24ccf793ded28615729b2f41ed")
+    add_versions("v2.4.3", "a8c5e384f511a03c603bbecc9edc24d2cb4a916998d51a29cf2e3a2896920d03")
 
     on_install(function (package)
-        os.cp("src/*.c", package:installdir("include"))
+        os.cp("src/*.c", package:installdir("lib"))
         os.cp("src/*.h", package:installdir("include"))
     end)
 
