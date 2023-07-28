c9c6823633424193e3a9f848ffda07eacb5e7d83
ruki <waruqi@gmail.com>
2023-07-28 23:41:50 +0800

    add phnt

--- packages/p/phnt/xmake.lua
+++ packages/p/phnt/xmake.lua
@@ -1,18 +1,19 @@
 package("phnt")
-
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/winsiderss/systeminformer")
     set_description("Native API header files for the System Informer project.")
 
-    set_urls("https://github.com/winsiderss/phnt.git")
-    add_versions("2023.6.18", "7c1adb8a7391939dfd684f27a37e31f18d303944")
+    add_urls("git@github.com:winsiderss/phnt.git")
+    add_versions("2022.10.13", "7c1adb8a7391939dfd684f27a37e31f18d303944")
 
-    add_syslinks("ntdll")
+    if is_plat("windows") then
+        add_syslinks("ntdll")
+    end
 
     on_install("windows", function (package)
         os.cp("*.h", package:installdir("include"))
     end)
 
     on_test(function (package)
-        assert(package:has_cincludes("phnt_windows.h"))
+        assert(package:has_cfuncs("NtConnectPort", {includes = {"phnt_windows.h", "phnt.h"}}))
     end)
