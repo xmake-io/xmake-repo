383c9eebb22a0537c44ba9579b229da33c05711c
star9029 <hengxings783@gmail.com>
2023-07-30 15:35:13 +0800

    fix static on windows

--- packages/l/libmodbus/xmake.lua
+++ packages/l/libmodbus/xmake.lua
@@ -12,16 +12,16 @@ package("libmodbus")
     end
 
     on_load(function (package)
-        if (not is_host("windows")) or is_subhost("msys", "cygwin") then
-            package:add("deps", "autoconf")
+        if not is_plat("windows", "mingw") then
+            package:add("deps", "autoconf", "automake", "libtool")
         end
     end)
 
     on_install(function (package)
-        if (not is_host("windows")) or is_subhost("msys", "cygwin") then
-            os.vrun("./configure")
-        end
+        import("package.tools.autoconf").install(package, {"--disable-tests"})
+    end)
 
+    on_install("windows", "mingw", function (package)
         local configs = {}
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
@@ -30,31 +30,29 @@ package("libmodbus")
                 add_files("src/*.c")
                 add_headerfiles("src/*.h")
 
-                if is_plat("windows", "mingw") then
-                    add_syslinks("ws2_32")
-                    add_includedirs("src/win32")
-                    if is_plat("windows") then
-                        add_files("src/win32/modbus.rc")
-                        add_defines("HAVE_CONFIG_H", "_CRT_SECURE_NO_DEPRECATE=1", "_CRT_NONSTDC_NO_DEPRECATE=1")
-                        if is_mode("debug") then
-                            add_defines("W32DEBUG")
-                        end
-                        if is_kind("shared") then
-                            add_defines("DLLBUILD")
-                        end
+                add_syslinks("ws2_32")
+                add_includedirs("src/win32")
+                if is_plat("windows") then
+                    add_files("src/win32/modbus.rc")
+                    add_defines("HAVE_CONFIG_H", "_CRT_SECURE_NO_DEPRECATE=1", "_CRT_NONSTDC_NO_DEPRECATE=1")
+                    if is_mode("debug") then
+                        add_defines("W32DEBUG")
+                    end
+                    if is_kind("shared") then
+                        add_defines("DLLBUILD")
                     end
                 end
 
                 before_build(function (target)
-                    if is_host("windows") and (not is_subhost("msys", "cygwin")) then
-                        local old = os.cd("src/win32")
-                        os.exec("cscript /nologo configure.js")
-                        os.cd(old)
-                    end
+                    local old = os.cd("src/win32")
+                    os.exec("cscript /nologo configure.js")
+                    os.cd(old)
                 end)
         ]])
         if package:config("shared") then
             configs.kind = "shared"
+        else
+            io.replace("src/modbus.h", "#  define MODBUS_API __declspec(dllimport)", "#  define MODBUS_API", {plain = true})
         end
         import("package.tools.xmake").install(package, configs)
     end)
