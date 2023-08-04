320fcd124f137fa3df9a80b0a11fd94d2ec9a40a
SirLynix <lynix680@gmail.com>
2023-08-04 17:55:58 +0200

    freetype: Add version 2.13.1 and fix git tag names

--- packages/f/freetype/xmake.lua
+++ packages/f/freetype/xmake.lua
@@ -1,17 +1,27 @@
 package("freetype")
-
     set_homepage("https://www.freetype.org")
     set_description("A freely available software library to render fonts.")
+    set_license("BSD") -- FreeType License (FTL) is a BSD-style license
 
-    set_urls("https://downloads.sourceforge.net/project/freetype/freetype2/$(version)/freetype-$(version).tar.gz",
-             "https://download.savannah.gnu.org/releases/freetype/freetype-$(version).tar.gz",
-             "https://gitlab.freedesktop.org/freetype/freetype.git")
-    add_versions("2.13.0", "a7aca0e532a276ea8d85bd31149f0a74c33d19c8d287116ef8f5f8357b4f1f80")
-    add_versions("2.12.1", "efe71fd4b8246f1b0b1b9bfca13cfff1c9ad85930340c27df469733bbb620938")
-    add_versions("2.11.1", "f8db94d307e9c54961b39a1cc799a67d46681480696ed72ecf78d4473770f09b")
-    add_versions("2.11.0", "a45c6b403413abd5706f3582f04c8339d26397c4304b78fa552f2215df64101f")
-    add_versions("2.10.4", "5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac")
-    add_versions("2.9.1", "ec391504e55498adceb30baceebd147a6e963f636eb617424bcfc47a169898ce")
+    add_urls("https://downloads.sourceforge.net/project/freetype/freetype2/$(version)/freetype-$(version).tar.gz",
+             "https://download.savannah.gnu.org/releases/freetype/freetype-$(version).tar.gz", {alias="archive"})
+    add_urls("https://gitlab.freedesktop.org/freetype/freetype.git", {alias = "git"})
+
+    add_versions("archive:2.13.1", "0b109c59914f25b4411a8de2a506fdd18fa8457eb86eca6c7b15c19110a92fa5")
+    add_versions("archive:2.13.0", "a7aca0e532a276ea8d85bd31149f0a74c33d19c8d287116ef8f5f8357b4f1f80")
+    add_versions("archive:2.12.1", "efe71fd4b8246f1b0b1b9bfca13cfff1c9ad85930340c27df469733bbb620938")
+    add_versions("archive:2.11.1", "f8db94d307e9c54961b39a1cc799a67d46681480696ed72ecf78d4473770f09b")
+    add_versions("archive:2.11.0", "a45c6b403413abd5706f3582f04c8339d26397c4304b78fa552f2215df64101f")
+    add_versions("archive:2.10.4", "5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac")
+    add_versions("archive:2.9.1",  "ec391504e55498adceb30baceebd147a6e963f636eb617424bcfc47a169898ce")
+    add_versions("git:2.13.1", "VER-2-13-1")
+    add_versions("git:2.13.0", "VER-2-13-0")
+    add_versions("git:2.12.1", "VER-2-12-1")
+    add_versions("git:2.12.0", "VER-2-12-0")
+    add_versions("git:2.11.1", "VER-2-11-1")
+    add_versions("git:2.11.0", "VER-2-11-0")
+    add_versions("git:2.10.4", "VER-2-10-4")
+    add_versions("git:2.9.1",  "VER-2-9-1")
 
     add_patches("2.11.0", path.join(os.scriptdir(), "patches", "2.11.0", "writing_system.patch"), "3172cf1e50501fc7455d9bb04ef4d5bb35b9712bb635f217f90ae6b2f7532eef")
 
