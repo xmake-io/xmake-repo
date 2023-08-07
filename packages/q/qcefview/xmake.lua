67a66db6f5545ddeecc7165b96e7fa1e1d6438f1
air <271651268@qq.com>
2023-08-07 14:28:46 +0800

    modify patch file

--- packages/q/qcefview/xmake.lua
+++ packages/q/qcefview/xmake.lua
@@ -2,16 +2,17 @@ package("qcefview")
     set_homepage("https://cefview.github.io/QCefView/")
     set_description("A QWidget-based Web View Component Integrated with CEF")
     add_urls("https://github.com/CefView/QCefView.git")
+    add_versions("2023.08.03", "b9dd0cf2fa0fcd5b6349cc7a5e3d1d178357a04f")
     
     add_configs("sandbox", {description = "Enable sandbox mode.", default = false, type = "boolean"})
     add_configs("debug", {description = "Enable debug mode.", default = false, type = "boolean"})
     add_configs("demo", {description = "Build demo.", default = false, type = "boolean"})
     --只提供选项,如果版本编译不能使用,请自行修改CefViewCore
     add_configs("cef_version", {description = "CEF version.", default = "113.3.1+g525fa10+chromium-113.0.5672.128", type = "string"})
-    add_patches("@default", path.join(os.scriptdir(), "patches", "CefConfig.cmake.patch"),"f26ddcf689572618548d028e9b84d02247cd435536a620a548ceb0895a2dbf69")
+    add_patches("@default", path.join(os.scriptdir(), "patches", "cefconfig.cmake.patch"),"f26ddcf689572618548d028e9b84d02247cd435536a620a548ceb0895a2dbf69")
 
     add_deps("cmake")
-   
+
     on_install("windows", "linux", function (package)
         local configs = {}
         import("detect.sdks.find_qt")
@@ -39,6 +40,6 @@ package("qcefview")
     end)
 
     -- on_test(function (package)
-    --     assert(package:has_cxxfuncs("QCefView::QCefView(QWidget*)", 
-    --     {includes = "QCefView.h", configs={vs_runtime="MD",rules = "qt.widgetapp"}}))
+    --     package:add("deps", "qt5core", "qt5gui", "qt5widget", {debug = package:is_debug(), version = package:version_str()})
+    --     assert(package:has_cxxfuncs("QCefView::QCefView(QWidget*)", {includes = "QCefView.h", configs={languages = "c++14"}}))
     -- end)
