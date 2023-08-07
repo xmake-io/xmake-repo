c1280a594c03f8cc239674265cb293460895ea4d
ruki <waruqi@gmail.com>
2023-08-07 21:18:54 +0800

    Update xmake.lua

--- packages/l/lolly/xmake.lua
+++ packages/l/lolly/xmake.lua
@@ -7,10 +7,13 @@ package("lolly")
 
     add_versions("v1.0.1", "69ebde6df3e5b4b9473f018d105f48f4abb179ff")
 
-    if is_plat("mingw", "windows") then
-        add_deps("nowide_standalone")
-        add_configs("nowide_standalone", {description = "nowide", default = true, type = "boolean"})
-    end
+    add_configs("nowide_standalone", {description = "nowide", default = true, type = "boolean"})
+
+    on_load(function (package)
+        if package:is_plat("mingw", "windows") and package:config("nowide_standalone") then
+            package:add("deps", "nowide_standalone")
+        end
+    end)
 
     on_install("linux", "macosx", "mingw", "wasm", function (package)
         local configs = {}
@@ -19,7 +22,7 @@ package("lolly")
         end
         import("package.tools.xmake").install(package, configs)
     end)
-    
+
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             #include "string.hpp"
@@ -28,5 +31,3 @@ package("lolly")
             }
         ]]}, {configs = {languages = "c++11"}}))
     end)
-
-package_end()
