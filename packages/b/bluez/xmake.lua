2e1709d5ce6232cfad97340299797a3631ffae19
Aidan Sun <aidansun05@gmail.com>
2023-08-07 15:43:43 -0400

    Add BlueZ

---
+++ packages/b/bluez/xmake.lua
@@ -0,0 +1,27 @@
+package("bluez")
+    set_homepage("http://www.bluez.org")
+    set_description("BlueZ Bluetooth library for Linux")
+    set_license("GPL-2.0")
+
+    add_urls("https://git.kernel.org/pub/scm/bluetooth/bluez.git")
+    add_versions("5.68", "d764f78f27653bc1df71c462e9aca7a18bc75f9f")
+
+    add_includedirs("include")
+
+    on_install("linux", function (package)
+        os.cp("lib/*.h", package:installdir("include", "bluetooth"))
+
+        -- Includes in the library have the formats "bluetooth.h" and <bluetooth/bluetooth.h>
+        -- So includedirs must be set using the package install location
+        io.writefile("xmake.lua", format([[
+            target("bluez2")
+                set_kind("$(kind)")
+                add_files("lib/*.c")
+                add_includedirs("%s", ".")
+        ]], package:installdir("include")))
+
+        local configs = {}
+        configs.kind = package:config("shared") and "shared" or "static"
+
+        import("package.tools.xmake").install(package, configs)
+    end)
