76afe08320666f150109048f477e97814e513ace
charonxin <1656746727@qq.com>
2023-08-07 14:41:57 +0800

    add package lolly

---
+++ packages/l/lolly/xmake.lua
@@ -0,0 +1,17 @@
+package("lolly")
+    set_homepage("https://github.com/XmacsLabs/lolly")
+    set_description("Lolly is a C++ library")
+
+    add_urls("https://github.com/XmacsLabs/lolly.git")
+    add_urls("https://gitee.com/XmacsLabs/lolly.git")
+
+    add_versions("v1.0.1", "69ebde6df3e5b4b9473f018d105f48f4abb179ff")
+
+    on_install("linux", "macosx", "mingw", "wasm", function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+package_end()
