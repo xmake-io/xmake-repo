454e0e97d207a4209ad6485d2ca43dddaf618b7f
charonxin <1656746727@qq.com>
2023-08-07 18:11:04 +0800

    add nowide_standalone

--- packages/l/lolly/xmake.lua
+++ packages/l/lolly/xmake.lua
@@ -7,7 +7,29 @@ package("lolly")
 
     add_versions("v1.0.1", "69ebde6df3e5b4b9473f018d105f48f4abb179ff")
 
+    add_deps("nowide_standalone")
+    if is_plat("mingw", "windows") then
+        add_configs("nowide_standalone", {description = "nowide", default = true, type = "boolean"})
+    end
+
+    on_load(function (package)
+        for _, dep in ipairs({"nowide_standalone"}) do
+            if package:config(dep) then
+                package:add("deps", dep)
+            end
+        end
+    end)
+
     on_install("linux", "macosx", "mingw", "wasm", function (package)
+        io.writefile("xmake.lua", [[
+            option("nowide_standalone", {description = "nowide", default = false})
+            add_rules("mode.debug", "mode.release")
+            if has_config("nowide_standalone") then
+                add_requires("nowide_standalone")
+            end
+            target("lolly")
+                set_kind("static")
+        ]])
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
