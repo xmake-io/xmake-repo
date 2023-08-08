abca8a0a25a9446c1df2d0d6b3aec9fcbb354705
ruki <waruqi@gmail.com>
2023-08-08 08:40:44 +0800

    Update xmake.lua

--- packages/b/bluez/xmake.lua
+++ packages/b/bluez/xmake.lua
@@ -6,23 +6,17 @@ package("bluez")
     add_urls("https://git.kernel.org/pub/scm/bluetooth/bluez.git")
     add_versions("5.68", "d764f78f27653bc1df71c462e9aca7a18bc75f9f")
 
-    add_includedirs("include")
-
     on_install("linux", function (package)
-        os.cp("lib/*.h", package:installdir("include", "bluetooth"))
-
-        -- Includes in the library have the formats "bluetooth.h" and <bluetooth/bluetooth.h>
-        -- So includedirs must be set using the package install location
         io.writefile("xmake.lua", format([[
             target("bluez2")
                 set_kind("$(kind)")
                 add_files("lib/*.c")
                 add_includedirs("%s", ".")
+                add_headerfiles("lib/(*.h)", {prefixdir = "bluetooth"})
         ]], package:installdir("include")))
 
         local configs = {}
         configs.kind = package:config("shared") and "shared" or "static"
-
         import("package.tools.xmake").install(package, configs)
     end)
 
