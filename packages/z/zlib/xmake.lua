2174c55ba3b3baf5ad7e3ca8d6e45884f73f1e90
ruki <waruqi@gmail.com>
2023-08-09 23:20:58 +0800

    remove invalid url for zlib

--- packages/z/zlib/xmake.lua
+++ packages/z/zlib/xmake.lua
@@ -4,9 +4,6 @@ package("zlib")
     set_description("A Massively Spiffy Yet Delicately Unobtrusive Compression Library")
 
     add_urls("https://github.com/madler/zlib/archive/$(version).tar.gz")
-    add_urls("https://mirrors.ustc.edu.cn/debian/pool/main/z/zlib/zlib_$(version).dfsg.orig.tar.gz", {
-        version = function (version) return version:sub(2) end
-    })
     add_urls("https://github.com/madler/zlib.git")
     add_versions("v1.2.10", "42cd7b2bdaf1c4570e0877e61f2fdc0bce8019492431d054d3d86925e5058dc5")
     add_versions("v1.2.11", "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff")
