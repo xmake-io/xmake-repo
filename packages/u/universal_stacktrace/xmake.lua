96462d244b95e6a4f0a6632aab71c510305e978a
ruki <waruqi@gmail.com>
2023-08-09 23:27:42 +0800

    fix windows

--- packages/u/universal_stacktrace/xmake.lua
+++ packages/u/universal_stacktrace/xmake.lua
@@ -4,7 +4,7 @@ package("universal_stacktrace")
     set_description("C++ Stacktrace for windows linux and os/x")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/MisterTea/UniversalStacktrace.git")
+    add_urls("https://github.com/MisterTea/UniversalStacktrace.git", {submodules = false})
     add_versions("2022.11.06", "28f5230d75e677ce8e4e140b2f3e0b8550195c85")
 
     if is_plat("windows") then
@@ -13,6 +13,7 @@ package("universal_stacktrace")
 
     on_install("linux", "macosx", "windows", function (package)
         io.replace("ust/ust.hpp", "#include <vector>", "#include <vector>\n#include <cstring>", {plain = true})
+        io.replace("ust/ust.hpp", "#include <DbgHelp.h>\n#include <windows.h>", "#include <windows.h>\n#include <DbgHelp.h>", {plain = true})
         os.cp("ust", package:installdir("include"))
     end)
 
