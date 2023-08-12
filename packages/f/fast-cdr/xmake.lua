3c10e6930f705faa43e3b891c8d613310792b48e
star9029 <hengxings783@gmail.com>
2023-08-12 23:04:00 +0800

    fast-cdr: add package

---
+++ packages/f/fast-cdr/xmake.lua
@@ -0,0 +1,27 @@
+package("fast-cdr")
+    set_homepage("https://www.eprosima.com")
+    set_description("eProsima FastCDR library provides two serialization mechanisms. One is the standard CDR serialization mechanism, while the other is a faster implementation of it.")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/eProsima/Fast-CDR/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/eProsima/Fast-CDR.git")
+
+    add_versions("v1.1.0", "5c4b2ad5493abd30b9475b14856641a8944c98077a36bd0760c1d83c65216e67")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <fastcdr/FastBuffer.h>
+            void test() {
+                auto buffer = eprosima::fastcdr::FastBuffer();
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
