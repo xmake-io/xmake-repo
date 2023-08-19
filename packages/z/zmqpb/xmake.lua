b5d9231f61d804a06340870ab45ee6872ff2cbd0
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-08-19 11:59:50 +0200

    add dependencies of library

--- packages/z/zmqpb/xmake.lua
+++ packages/z/zmqpb/xmake.lua
@@ -7,6 +7,10 @@ package("zmqpb")
              "https://github.com/SFGrenade/ZmqPb.git")
     add_versions("0.1", "4a34ec92faa381306356e84e2a2000093d8f76cfa037db1f4cd0adb0205faebb")
 
+    add_deps("cppzmq")
+    add_deps("fmt")
+    add_deps("protobuf-cpp")
+
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
