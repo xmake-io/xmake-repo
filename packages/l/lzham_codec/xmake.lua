2e09115a844b98efb65c8274b74ebe33f1cb1ab4
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-08-23 16:41:58 +0100

    Added patches from lzham_codec pull requests

--- packages/l/lzham_codec/xmake.lua
+++ packages/l/lzham_codec/xmake.lua
@@ -5,14 +5,15 @@ package("lzham_codec")
     set_urls("https://github.com/richgel999/lzham_codec.git")
 
     add_versions("2023.01.03", "d379b1f9121e2197881c61cfc4713c78848bdfe7")
+    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_linux.patch"), "f43db780903b98567d5039bcc99c08d3ed871a979333b3387e8416536b3ba925")
+    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "use_lzham_prefixed_max_int_values.patch"), "bf8dd1bf584fb0e8a7dcdb846e009ef52c1fcc0fbee7158635322b69883874a6")
+    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_macosx.patch"), "8c5ec78e9381215f0283dde82059775c3f8406c444013c04bb55b578415ff7ef")
 
     on_install(function (package)
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        io.replace("lzhamdecomp/lzham_symbol_codec.h", [[#include "lzham_prefix_coding.h"]], [[#include "lzham_prefix_coding.h"
-#include <cstdint>]], {plain = true})
-        import("package.tools.xmake").install(package, {kind = package:config("shared") and "shared" or "static"})
+        import("package.tools.xmake").install(package)
     end)
-
+    
     on_test(function (package)
         assert(package:has_cfuncs("lzham_compress_init", {includes = "lzham_static_lib.h"}))
         assert(package:check_cxxsnippets({test = [[
