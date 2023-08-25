e4b37adf073341832e3c4e78e73eba598cd10e76
ruki <waruqi@gmail.com>
2023-08-25 22:33:57 +0800

    improve lua deps

--- packages/e/emmylua_debugger/xmake.lua
+++ packages/e/emmylua_debugger/xmake.lua
@@ -7,26 +7,39 @@ package("emmylua_debugger")
              "https://github.com/EmmyLua/EmmyLuaDebugger.git")
 
     add_versions("1.6.2", "80bfee98542a0ffe41459c5c77137e3628e931b5912a6b5e13f60b9ca67dd7c7")
-    add_versions("2023.08.25", "602402ffdd430cd9c2c86f1bff4d05688a07d785")
 
     add_configs("luasrc", {description = "Use lua source.", default = true, type = "boolean"})
-    add_configs("luaver", {description = "Set lua version.", default = "5.4", type = "string"})
+    add_configs("luaver", {description = "Set lua version.", default = "5.4.4", type = "string"})
 
     add_deps("cmake")
-    add_deps("lua 5.4.6")
+
+    on_load(function (package)
+        if package:config("luasrc") then
+            local luaver = package:config("luaver")
+            if luaver then
+                package:add("deps", "lua " .. luaver)
+            else
+                package:add("deps", "lua")
+            end
+        end
+    end)
 
     on_install("macosx", "linux", "windows", function (package)
+        import("core.base.semver")
         local configs = {}
+        local cxflags
         if package:config("luasrc") then
-            table.insert(configs, "-DEMMY_USE_LUA_SOURCE=ON")
+            cxflags = "-DEMMY_USE_LUA_SOURCE=1"
+            table.insert(configs, "-DEMMY_COMPILE_AS_LIB=ON")
         end
         local luaver = package:config("luaver")
         if luaver then
-            table.insert(configs, "-DEMMY_LUA_VERSION=" .. (luaver:gsub("%.", "")))
+            local version = semver.new(luaver)
+            table.insert(configs, "-DEMMY_LUA_VERSION=" .. version:major() .. version:minor())
         end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         io.replace("CMakeLists.txt", "set(CMAKE_INSTALL_PREFIX install)", "", {plain = true})
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.cmake").install(package, configs, {packagedeps = "lua", cxflags = cxflags})
     end)
 
     on_test(function (package)
