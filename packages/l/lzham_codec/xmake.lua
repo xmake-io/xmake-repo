0ace0d2016f4295e12d9337f3ce3695a08354d4c
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-08-25 08:27:43 +0100

    Add string.h patch, update fix_arm64.patch

--- packages/l/lzham_codec/xmake.lua
+++ packages/l/lzham_codec/xmake.lua
@@ -8,8 +8,9 @@ package("lzham_codec")
     add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_linux.patch"), "f43db780903b98567d5039bcc99c08d3ed871a979333b3387e8416536b3ba925")
     add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "use_lzham_prefixed_max_int_values.patch"), "bf8dd1bf584fb0e8a7dcdb846e009ef52c1fcc0fbee7158635322b69883874a6")
     add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_macosx.patch"), "8c5ec78e9381215f0283dde82059775c3f8406c444013c04bb55b578415ff7ef")
-    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_arm64.patch"), "fb5b52dd66c5e873e64c1196960bbc8926c531e75bc202cd9bd9299c5a09956d")
-    
+    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_arm64.patch"), "5decfe89970409c5777842441e90283b10f7b1873dae12f3eb8fadf0bdb2c346")
+    add_patches("2023.01.03", path.join(os.scriptdir(), "patches", "2023.01.03", "fix_mingw.patch"), "b35bb937a0fc2ba7a61c7e3253ede9d3e8c0f952a8f312e6fa2fe577987fa9c6")
+
     on_install(function (package)
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         import("package.tools.xmake").install(package)
