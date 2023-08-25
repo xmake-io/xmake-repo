f67bbabfc29dbd08a93abd95c14b85010ce6075b
ruki <waruqi@gmail.com>
2023-08-26 00:41:50 +0800

    fix envs

--- packages/e/emscripten/xmake.lua
+++ packages/e/emscripten/xmake.lua
@@ -1,5 +1,4 @@
 package("emscripten")
-
     set_kind("toolchain")
     set_homepage("https://emscripten.org/")
     set_description("Emscripten: An LLVM-to-WebAssembly Compiler.")
@@ -9,6 +8,18 @@ package("emscripten")
     add_versions("3.1.42", "c2260b4f28f53b411032de0955a6fe6b6bcf3edd")
 
     add_deps("python")
+
+    on_load(function (package)
+        package:addenv("PATH", "upstream/emscripten")
+        package:addenv("PATH", ".")
+        package:addenv("EMSDK", ".")
+        package:mark_as_pathenv("EMSDK")
+        package:mark_as_pathenv("EMSDK_NODE")
+        if package:is_plat("windows") then
+            package:mark_as_pathenv("EMSDK_PYTHON")
+            package:mark_as_pathenv("JAVA_HOME")
+        end
+    end)
 
     on_install("windows", "macosx", "linux", function (package)
         import("lib.detect.find_directory")
@@ -26,32 +37,23 @@ package("emscripten")
         os.vrunv(py, {path.join(installdir, "emsdk.py"), "activate", version})
 
         -- setup env
-        package:addenv("PATH", "upstream/emscripten")
-        package:addenv("PATH", ".")
-        package:addenv("EMSDK", ".")
-        package:mark_as_pathenv("EMSDK")
-
         local exe = package:is_plat("windows") and ".exe" or ""
         local node_bindir = find_directory("bin", {path.join(installdir, "node", "**")})
         if node_bindir then
             node_bindir = path.relative(node_bindir, installdir)
             package:addenv("PATH", node_bindir)
             package:addenv("EMSDK_NODE", path.join(node_bindir, "node" .. exe))
-            package:mark_as_pathenv("EMSDK_NODE")
         end
-
         if package:is_plat("windows") then
             local python = find_directory("*", path.join(installdir, "python"))
             if python then
                 python = path.relative(python, installdir)
                 package:addenv("EMSDK_PYTHON", path.join(python, "python" .. exe))
-                package:mark_as_pathenv("EMSDK_PYTHON")
             end
             local java = find_directory("*", path.join(installdir, "java"))
             if java then
                 java = path.relative(java, installdir)
                 package:addenv("JAVA_HOME", java)
-                package:mark_as_pathenv("JAVA_HOME")
             end
         end
     end)
