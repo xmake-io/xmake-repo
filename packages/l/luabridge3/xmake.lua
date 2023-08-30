9a3ef14b307a369c8e4b4d60f6b87e21837acd11
ruki <waruqi@gmail.com>
2023-08-30 14:11:53 +0800

    Update xmake.lua

--- packages/l/luabridge3/xmake.lua
+++ packages/l/luabridge3/xmake.lua
@@ -1,17 +1,15 @@
 package("luabridge3")
-
+    set_kind("library", {headeronly = true})
     set_homepage("https://kunitoki.github.io/LuaBridge3")
     set_description("LuaBridge3 is a lightweight and dependency-free library for mapping data, functions, and classes back and forth between C++ and Lua.")
     set_license("MIT")
 
     add_urls("https://github.com/kunitoki/LuaBridge3/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/kunitoki/LuaBridge3.git"
-    )
+             "https://github.com/kunitoki/LuaBridge3.git")
     add_versions("3.0-rc3", "842a3803587c42568f6f4e65314f762fb7724c9c7c91efc930282921dbf9a79f")
-    set_kind("library", {headeronly = true})
     
     on_install(function (package)
-        os.cp("Source", package:installdir() .. "/include/luabridge3")
+        os.cp("Source", path.join(package:installdir("include"), "luabridge3"))
     end)
 
     on_test(function (package)
