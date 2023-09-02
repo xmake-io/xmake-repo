f7c4c683df067c729b23caeef3648a17005dde78
ruki <waruqi@gmail.com>
2023-09-02 21:27:36 +0800

    Update xmake.lua

--- packages/f/fruit/xmake.lua
+++ packages/f/fruit/xmake.lua
@@ -28,6 +28,7 @@ package("fruit")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DFRUIT_USES_BOOST=" .. (package:config("boost") and "ON" or "OFF"))
+        io.replace("CMakeLists.txt", "add_subdirectory(tests)", "", {plain = true})
         import("package.tools.cmake").install(package, configs)
     end)
 
