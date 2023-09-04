3dbbb3f650bf6e3fec97996366fa91e5ba9f5be2
Arthur LAURENT <arthur.laurent4@gmail.com>
2023-09-04 13:53:02 +0200

    use before_buildcmd_file to simplify wayland.protocol rule

--- packages/w/wayland-protocols/rules/wayland_protocols.lua
+++ packages/w/wayland-protocols/rules/wayland_protocols.lua
@@ -56,90 +56,30 @@ rule("wayland.protocols")
         end
     end)
 
-    before_build_files(function(target, batchjobs, sourcebatch, opt)
-        sourcebatch.objectfiles = {}
-
-        local outputdir = target:extraconf("rules", "wayland.protocols", "outputdir") or path.join(target:autogendir(), "rules", "wayland.protocols")
-
-        opt.rootjob = batchjobs:group_leave() or opt.rootjob
-        batchjobs:group_enter(target:name() .. "/generate_protocols", {rootjob = opt.rootjob})
-        for _, protocol in ipairs(sourcebatch.sourcefiles) do
-            batchjobs:addjob(target:name() .. "-generate-" .. protocol, function(index, total)
-                import("utils.progress")
-                import("core.base.option")
-                import("core.project.depend")
-                import("lib.detect.find_tool")
-
-                opt.progress = opt.progress or (index * 100) / total
-
-                local dryrun = option.get("dry-run")
-                local dependfile = target:dependfile(protocol)
-                local dependinfo = option.get("rebuild") and {} or (depend.load(dependfile) or {})
-
-                local clientfile = path.join(outputdir, path.basename(protocol) .. ".h")
-                local privatefile = path.join(outputdir, path.basename(protocol) .. ".c")
-
-                local lastmtime = os.exists(privatefile) and os.mtime(dependfile) or 0
-                if not dryrun and not depend.is_changed(dependinfo, {lastmtime = lastmtime}) then
-                    return
-                end
-
-                local client_flag = "client-header"
-                local private_flag = "private-code"
-
-                local wayland_scanner = find_tool("wayland-scanner")
-                assert(wayland_scanner, "wayland-scanner not found! please install wayland package")
-
-                progress.show(opt.progress, "${color.build.object}generating.wayland.protocol.client %s", path.basename(protocol))
-                vprint(wayland_scanner.program, client_flag, protocol, clientfile)
-
-                progress.show(opt.progress, "${color.build.object}generating.wayland.protocol.private %s", path.basename(protocol))
-                vprint(wayland_scanner.program, private_flag, protocol, privatefile)
-
-                if not dryrun then
-                    os.rm(clientfile)
-
-                    dependinfo.files = {}
-
-                    assert(os.execv(wayland_scanner.program, {client_flag, protocol, clientfile}))
-                    assert(os.execv(wayland_scanner.program, {private_flag, protocol, privatefile}))
-
-                    table.join2(dependinfo.files, protocol)
-                    depend.save(dependinfo, dependfile)
-                end
-            end, {rootjob = opt.rootjob})
-        end
-    end, {batch = true})
-
-    -- serial compilation only, usually used to support project generator
-    before_buildcmd_files(function(target, batchcmds, sourcebatch, opt)
+    before_buildcmd_file(function(target, batchcmds, sourcefile, opt)
         import("lib.detect.find_tool")
-
-        sourcebatch.objectfiles = {}
 
         local outputdir = target:extraconf("rules", "wayland.protocols", "outputdir") or path.join(target:autogendir(), "rules", "wayland.protocols")
 
         local wayland_scanner = find_tool("wayland-scanner")
         assert(wayland_scanner, "wayland-scanner not found! please install wayland package")
 
-        for _, protocol in ipairs(sourcebatch.sourcefiles) do
-            local clientfile = path.join(outputdir, path.basename(protocol) .. ".h")
-            local privatefile = path.join(outputdir, path.basename(protocol) .. ".c")
+        local clientfile = path.join(outputdir, path.basename(sourcefile) .. ".h")
+        local privatefile = path.join(outputdir, path.basename(sourcefile) .. ".c")
 
-            local client_flag = "client-header"
-            local private_flag = "private-code"
+        local client_flag = "client-header"
+        local private_flag = "private-code"
 
-            local client_flags = {client_flag, protocol, clientfile}
-            local private_flags = {private_flag, protocol, privatefile}
+        local client_flags = {client_flag, sourcefile, clientfile}
+        local private_flags = {private_flag, sourcefile, privatefile}
 
-            batchcmds:show_progress(opt.progress, "${color.build.object}generating.wayland.protocol.client %s", path.basename(protocol))
-            batchcmds:vexecv(wayland_scanner.program, client_flags)
+        batchcmds:show_progress(opt.progress, "${color.build.object}generating.wayland.protocol.client %s", path.basename(sourcefile))
+        batchcmds:vexecv(wayland_scanner.program, client_flags)
 
-            batchcmds:show_progress(opt.progress, "${color.build.object}generating.wayland.protocol.private %s", path.basename(protocol))
-            batchcmds:vexecv(wayland_scanner.program, private_flags)
+        batchcmds:show_progress(opt.progress, "${color.build.object}generating.wayland.protocol.private %s", path.basename(sourcefile))
+        batchcmds:vexecv(wayland_scanner.program, private_flags)
 
-            batchcmds:add_depfiles(protocol)
-            batchcmds:set_depmtime(os.mtime(privatefile))
-            batchcmds:set_depcache(target:dependfile(privatefile))
-        end
+        batchcmds:add_depfiles(sourcefile)
+        batchcmds:set_depmtime(os.mtime(privatefile))
+        batchcmds:set_depcache(target:dependfile(privatefile))
     end)
