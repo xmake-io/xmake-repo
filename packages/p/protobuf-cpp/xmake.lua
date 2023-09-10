33575e890272f71bddd723c8373ce59520828ff6
Maxime <672982+maximegmd@users.noreply.github.com>
2023-09-10 12:15:55 +0200

    zlib is always required now

--- packages/p/protobuf-cpp/xmake.lua
+++ packages/p/protobuf-cpp/xmake.lua
@@ -25,8 +25,6 @@ package("protobuf-cpp")
     add_patches("3.17.3", path.join(os.scriptdir(), "patches", "3.17.3", "field_access_listener.patch"), "ac9bdf49611b01e563fe74b2aaf1398214129454c3e18f1198245549eb281e85")
     add_patches("3.19.4", path.join(os.scriptdir(), "patches", "3.19.4", "vs_runtime.patch"), "8e73e585d29f3b9dca3c279df0b11b3ee7651728c07f51381a69e5899b93c367")
 
-    add_configs("zlib", {description = "Enable zlib", default = false, type = "boolean"})
-
     add_deps("cmake")
 
     if is_plat("windows") then
@@ -41,9 +39,7 @@ package("protobuf-cpp")
 
     on_load(function (package)
         package:addenv("PATH", "bin")
-        if package:config("zlib") then
-            package:add("deps", "zlib")
-        end
+        package:add("deps", "zlib")
         package:add("deps", "abseil")
     end)
 
@@ -61,9 +57,7 @@ package("protobuf-cpp")
                 package:add("defines", "PROTOBUF_USE_DLLS")
             end
         end
-        if package:config("zlib") then
-            table.insert(configs, "-Dprotobuf_WITH_ZLIB=ON")
-        end
+        table.insert(configs, "-Dprotobuf_WITH_ZLIB=ON")
         import("package.tools.cmake").install(package, configs, {buildir = "build", packagedeps = "abseil"})
         os.trycp("build/Release/protoc.exe", package:installdir("bin"))
     end)
