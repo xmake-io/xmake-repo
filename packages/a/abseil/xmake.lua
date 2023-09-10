63e11811ed81bfe614ced16d83306e095496005f
Maxime <672982+maximegmd@users.noreply.github.com>
2023-09-10 12:27:17 +0200

    abseil 20230802.0

--- packages/a/abseil/xmake.lua
+++ packages/a/abseil/xmake.lua
@@ -12,6 +12,7 @@ package("abseil")
     add_versions("20211102.0", "dcf71b9cba8dc0ca9940c4b316a0c796be8fab42b070bb6b7cab62b48f0e66c4")
     add_versions("20220623.0", "4208129b49006089ba1d6710845a45e31c59b0ab6bff9e5788a87f55c5abd602")
     add_versions("20230125.2", "9a2b5752d7bfade0bdeee2701de17c9480620f8b237e1964c1b9967c75374906")
+    add_versions("20230802.0", "46217fe41739e096dba2f9854f2552de58ba8a8db2cef06cfe42f3437673ebe3")
 
     add_deps("cmake")
 
