4b2df9b05a32619499537fd9adb0d503cdb9052c
star9029 <hengxings783@gmail.com>
2023-09-12 21:10:59 +0800

    aws-c-common: improve package

--- packages/a/aws-c-common/xmake.lua
+++ packages/a/aws-c-common/xmake.lua
@@ -3,21 +3,31 @@ package("aws-c-common")
     set_description("Core c99 package for AWS SDK for C")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/awslabs/aws-c-common/archive/$(version).tar.gz",
+    add_urls("https://github.com/awslabs/aws-c-common/archive/refs/tags/$(version).tar.gz",
              "https://github.com/awslabs/aws-c-common.git")
-    add_versions("v0.6.9", "928a3e36f24d1ee46f9eec360ec5cebfe8b9b8994fe39d4fa74ff51aebb12717")
 
-    if is_plat("linux") then
-        add_deps("cmake")
+    add_versions("v0.9.3", "389eaac7f64d7d5a91ca3decad6810429eb5a65bbba54798b9beffcb4d1d1ed6")
+
+    add_configs("asan", {description = "Enable Address Sanitize.", default = false, type = "boolean"})
+
+    if is_plat("windows") then
+        add_syslinks("bcrypt", "ws2_32", "shlwapi")
+    elseif is_plat("linux", "bsd") then
         add_syslinks("dl", "m", "pthread", "rt")
+    elseif is_plat("macosx") then
+        add_frameworks("CoreFoundation")
     end
 
-    on_install("linux", function (package)
-        local configs = {}
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DPERFORM_HEADER_CHECK=ON")
-        table.insert(configs, "-DENABLE_NET_TESTS=OFF")
+        table.insert(configs, "-DENABLE_SANITIZERS=" .. (package:config("asan") and "ON" or "OFF"))
+        if package:is_plat("windows") then
+            table.insert(configs, "-DAWS_STATIC_MSVC_RUNTIME_LIBRARY=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
