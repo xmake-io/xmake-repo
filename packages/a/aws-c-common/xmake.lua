613df4d1437737c9815bf436646be65638494610
star9029 <hengxings783@gmail.com>
2023-09-12 21:42:07 +0800

    fix syslinks

--- packages/a/aws-c-common/xmake.lua
+++ packages/a/aws-c-common/xmake.lua
@@ -9,8 +9,11 @@ package("aws-c-common")
     add_versions("v0.9.3", "389eaac7f64d7d5a91ca3decad6810429eb5a65bbba54798b9beffcb4d1d1ed6")
 
     add_configs("asan", {description = "Enable Address Sanitize.", default = false, type = "boolean"})
-
     if is_plat("windows") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
+    if is_plat("windows", "mingw") then
         add_syslinks("bcrypt", "ws2_32", "shlwapi")
     elseif is_plat("linux", "bsd") then
         add_syslinks("dl", "m", "pthread", "rt")
@@ -20,9 +23,9 @@ package("aws-c-common")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows|x64", "windows|x86", "linux", "macosx", "bsd", "mingw", "msys", "android", "iphoneos", "cross", "wasm", function (package)
         local configs = {"-DBUILD_TESTING=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DENABLE_SANITIZERS=" .. (package:config("asan") and "ON" or "OFF"))
         if package:is_plat("windows") then
