46acfff17ca18803322090136603f65849356b57
star9029 <hengxings783@gmail.com>
2023-09-17 15:37:09 +0800

    disable plat

--- packages/s/s2n-tls/xmake.lua
+++ packages/s/s2n-tls/xmake.lua
@@ -8,16 +8,27 @@ package("s2n-tls")
 
     add_versions("v1.3.51", "75c650493c42dddafd5dec6a42f2258ab52e501719ee5a337ec580cc958ea67a")
 
+    add_configs("pq", {description = [[Enables all Post Quantum Crypto code. You likely want this
+    for older compilers or uncommon platforms.]], default = false, type = "boolean"})
+    add_configs("pq_asm", {description = [[Turns on the ASM for PQ Crypto even if it's available for the toolchain. You likely want this on older compilers.]], default = false, type = "boolean"})
+    add_configs("stacktrace", {description = [[Enables stacktrace functionality in s2n-tls. Note that this functionality is
+    only available on platforms that support execinfo.]], default = false, type = "boolean"})
+
     if is_plat("linux", "bsd") then
         add_syslinks("m", "pthread")
     end
 
     add_deps("cmake", "openssl")
 
-    on_install("linux", "macosx", "bsd", "mingw", "msys", "cross", "android", function (package)
+    on_install("linux", "bsd", "cross", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DUNSAFE_TREAT_WARNINGS_AS_ERRORS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DS2N_LTO=" .. (package:config("lto") and "ON" or "OFF"))
+
+        table.insert(configs, "-DS2N_NO_PQ=" .. (package:config("pq") and "OFF" or "ON"))
+        table.insert(configs, "-DS2N_NO_PQ_ASM=" .. (package:config("pq_asm") and "OFF" or "ON"))
+        table.insert(configs, "-DS2N_STACKTRACE=" .. (package:config("stacktrace") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
