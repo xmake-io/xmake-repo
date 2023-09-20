a8eaa2c54cad1b81b912c30d6322a46949cf2b99
xq114 <1140735506@qq.com>
2023-09-21 01:04:45 +0800

    add alembic

--- packages/i/imath/xmake.lua
+++ packages/i/imath/xmake.lua
@@ -14,6 +14,7 @@ package("imath")
     add_versions("v3.1.5", "1e9c7c94797cf7b7e61908aed1f80a331088cc7d8873318f70376e4aed5f25fb")
     add_versions("v3.1.9", "f1d8aacd46afed958babfced3190d2d3c8209b66da451f556abd6da94c165cf3")
 
+    add_includedirs("include", "include/Imath")
     on_load(function (package)
         if not package.is_built or package:is_built() then
             package:add("deps", "cmake")
