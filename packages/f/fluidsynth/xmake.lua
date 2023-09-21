2592bd00959d3d23ec7951e6267880880cf65bfa
jingkaimori <jingkaimori@gmail.com>
2023-09-21 16:26:18 +0800

    add package fluidsynth

---
+++ packages/f/fluidsynth/xmake.lua
@@ -0,0 +1,31 @@
+package("fluidsynth")
+
+    set_homepage("https://www.fluidsynth.org/")
+    set_description("FluidSynth is a real-time software synthesizer based on the SoundFont 2 specifications and has reached widespread distribution.")
+    set_license("LGPL-2.1")
+
+    add_urls("https://github.com/FluidSynth/fluidsynth/archive/refs/tags/$(version).zip",
+             "https://github.com/FluidSynth/fluidsynth.git")
+    add_versions("v2.3.3", "0ab6f1aae1c7652b9249de2d98070313f3083046fddd673277556f1cca65568e")
+
+    add_deps("cmake")
+    add_deps("glib")
+    add_deps("libiconv")
+    add_deps("libintl")
+
+    on_install("windows", "linux", "macosx", function (package)
+        io.gsub("cmake_admin/FindGLib2.cmake", "list%(APPEND _glib2_link_libraries \"pcre\"%)", "")
+        import("package.tools.cmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() 
+            {
+                fluid_settings_t* settings = new_fluid_settings();
+                fluid_synth_t* synth = new_fluid_synth(settings);
+                delete_fluid_synth(synth);
+                delete_fluid_settings(settings);
+            }
+        ]]}, {includes = "fluidsynth.h"}))
+    end)
