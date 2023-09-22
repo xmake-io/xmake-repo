4e041baf1f9ce64baf511484a372a8c8d18b3de7
jingkaimori <jingkaimori@gmail.com>
2023-09-22 12:30:06 +0800

    patch ill-formed cmake file

--- packages/f/fluidsynth/xmake.lua
+++ packages/f/fluidsynth/xmake.lua
@@ -7,15 +7,19 @@ package("fluidsynth")
     add_urls("https://github.com/FluidSynth/fluidsynth/archive/refs/tags/$(version).zip",
              "https://github.com/FluidSynth/fluidsynth.git")
     add_versions("v2.3.3", "0ab6f1aae1c7652b9249de2d98070313f3083046fddd673277556f1cca65568e")
+    if not is_plat("linux") then
+        add_patches("v2.3.3", path.join(os.scriptdir(), "patches", "find-intl.patch"), "2A577B9D2F81CDF944AE5B0F90F54015720C2DFA15658F29CC6934ADDF6ED982")
+    end
 
     add_deps("cmake")
     add_deps("glib")
     add_deps("libiconv")
+    if not is_plat("linux") then
+        add_deps("libintl")
+    end
+    add_deps("pkgconf")
 
     on_install("windows", "linux", "macosx", function (package)
-        io.gsub("cmake_admin/FindGLib2.cmake", "\"Intl::Intl\"", "")
-        io.gsub("cmake_admin/FindGLib2.cmake", "find_package%(Intl QUIET%)", "")
-        io.gsub("cmake_admin/FindGLib2.cmake", "list%(APPEND _glib2_link_libraries \"pcre\"%)", "")
         import("package.tools.cmake").install(package)
     end)
 
