8e3d9656ebec86aeddc7a48caf209beb3fe038e8
jingkaimori <jingkaimori@gmail.com>
2023-09-23 11:59:55 +0800

    fix libm 2

--- packages/f/fluidsynth/xmake.lua
+++ packages/f/fluidsynth/xmake.lua
@@ -8,7 +8,7 @@ package("fluidsynth")
              "https://github.com/FluidSynth/fluidsynth.git")
     add_versions("v2.3.3", "0ab6f1aae1c7652b9249de2d98070313f3083046fddd673277556f1cca65568e")
     if not is_plat("linux") then
-        add_patches("v2.3.3", path.join(os.scriptdir(), "patches", "find-intl.patch"), "dcfc119cca07d8ce118c56841e4b855e3e90895b0f3da10760cd33b261a0c210")
+        add_patches("v2.3.3", path.join(os.scriptdir(), "patches", "find-intl.patch"), "afe563d3319e14af0d20ab2802e34b163faa4a96e89b15b84949afd6943e1add")
     end
 
     -- Some libraries are required for build with our default config settings.
