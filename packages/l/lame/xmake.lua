ebe5747fbc2a9b9d715a69a5b01b0bf372ed7c24
Yiheng Wu <36156959+jingkaimori@users.noreply.github.com>
2023-09-26 14:45:37 +0800

    build lame on windows x86 (#2653)
    * build lame with msvc on x86 system
    
    * build static package only

--- packages/l/lame/xmake.lua
+++ packages/l/lame/xmake.lua
@@ -5,6 +5,7 @@ package("lame")
 
     add_urls("https://downloads.sourceforge.net/project/lame/lame/$(version)/lame-$(version).tar.gz")
     add_versions("3.100", "ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e")
+    add_configs("shared", {description = "Build static libraries", default = false, type = "boolean", readonly = true})
 
     add_deps("nasm")
 
@@ -21,6 +22,15 @@ package("lame")
         -- https://sourceforge.net/p/lame/mailman/message/36081038/
         io.replace("include/libmp3lame.sym", "lame_init_old\n", "", {plain = true})
         import("package.tools.autoconf").install(package, configs)
+    end)
+
+    on_install("windows|x86", function (package)
+        os.cp("configMS.h", "config.h")
+        io.gsub("Makefile.MSVC", "nasmw", "nasm")
+        import("package.tools.nmake").build(package, {"-f", "Makefile.MSVC"})
+        os.cp("output/*.lib", package:installdir("lib"))
+        os.cp("output/*.exe", package:installdir("bin"))
+        os.cp("include/*.h", package:installdir("include/lame"))
     end)
 
     on_test(function (package)
