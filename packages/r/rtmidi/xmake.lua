396ee6e6330eea3d4b56c7428a7c8eee0611a40e
jingkaimori <jingkaimori@gmail.com>
2023-09-30 15:44:12 +0800

    fix compilation error under linux

--- packages/r/rtmidi/xmake.lua
+++ packages/r/rtmidi/xmake.lua
@@ -7,6 +7,13 @@ package("rtmidi")
 
     add_versions("6.0.0", "ef7bcda27fee6936b651c29ebe9544c74959d0b1583b716ce80a1c6fea7617f0")
 
+    if is_plat("linux") then
+        add_configs("alsa", {default = false, showmenu = true, description = "Use alsa api on linux.", values={true, false}})
+    end
+    if is_plat("linux", "macosx", "bsd") then
+        add_configs("jack", {default = false, showmenu = true, description = "Use jack api on posix.", values={true, false}})
+    end
+
     if is_plat("windows", "mingw") then
         add_syslinks("winmm")
     elseif is_plat("macosx") then
@@ -21,8 +28,23 @@ package("rtmidi")
 
     add_deps("cmake")
 
+    on_load("linux", "macosx", "bsd", function(package)
+        if package:config("alsa") then
+            package:add("syslinks", "asound")
+        end
+        if package:config("jack") then
+            package:add("syslinks", "jack")
+        end
+    end)
+
     on_install("windows", "linux", "macosx", "bsd", "mingw", "msys", "iphoneos", "cross", "wasm", function (package)
         local configs = {"-DRTMIDI_BUILD_TESTING=OFF"}
+        if package:config("alsa") then
+            table.insert(configs, "-DRTMIDI_API_ALSA=ON")
+        end
+        if package:config("jack") then
+            table.insert(configs, "-DRTMIDI_API_JACK=ON")
+        end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
