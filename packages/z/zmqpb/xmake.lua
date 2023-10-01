9ae0aca7d7bd7a95a7b78ff3aa45f8ac58385c73
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-10-01 15:44:03 +0200

    Add zmqpb v0.2

--- packages/z/zmqpb/xmake.lua
+++ packages/z/zmqpb/xmake.lua
@@ -6,6 +6,7 @@ package("zmqpb")
     set_urls("https://github.com/SFGrenade/ZmqPb/archive/refs/tags/v$(version).tar.gz",
              "https://github.com/SFGrenade/ZmqPb.git")
     add_versions("0.1", "4a34ec92faa381306356e84e2a2000093d8f76cfa037db1f4cd0adb0205faebb")
+    add_versions("0.2", "5dfa4d4cebb10cb7ae03943e18e8d48c8ff215e80371f24c5ade212be7f20721")
 
     add_deps("cppzmq")
     add_deps("fmt")
