d28425055b1f00b348c1fbc96ae13c8a7fcd7843
Darcy Shen <da@liii.pro>
2023-10-03 18:22:51 +0800

    Add more versions for Qt 6.5 LTS (#2668)

--- packages/q/qt6base/xmake.lua
+++ packages/q/qt6base/xmake.lua
@@ -3,7 +3,10 @@ package("qt6base")
     set_base("qtbase")
 
     add_versions("6.3.0", "dummy")
+    add_versions("6.5.0", "dummy")
     add_versions("6.5.1", "dummy")
+    add_versions("6.5.2", "dummy")
+    add_versions("6.5.3", "dummy")
     add_versions("6.6.0", "dummy")
 
     on_install("windows|x64", "linux|x86_64", "macosx|x86_64", "mingw|x86_64", function (package)
