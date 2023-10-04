ec225eb2c40967569c2a3171d7e3b228471688f4
Jérôme Leclercq <lynix680@gmail.com>
2023-10-04 12:39:50 +0200

    Auto-update libsndfile to 1.2.2 (#2657)
    * Update libsndfile to 1.2.2
    
    * Update xmake.lua

--- packages/l/libsndfile/xmake.lua
+++ packages/l/libsndfile/xmake.lua
@@ -6,6 +6,7 @@ package("libsndfile")
 
     add_urls("https://github.com/libsndfile/libsndfile/archive/refs/tags/$(version).tar.gz",
              "https://github.com/libsndfile/libsndfile.git")
+    add_versions("1.2.2", "ffe12ef8add3eaca876f04087734e6e8e029350082f3251f565fa9da55b52121")
     add_versions("1.0.31", "8cdee0acb06bb0a3c1a6ca524575643df8b1f3a55a0893b4dd9f829d08263785")
 
     add_deps("cmake", "libflac", "libopus", "libvorbis", "libogg")
@@ -23,9 +24,9 @@ package("libsndfile")
         table.insert(configs, "-DBUILD_TESTING=OFF")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_MPEG=OFF")
 
         if package:is_plat("windows", "mingw") then
-
             -- libsndfile doesn't build well with a static libFLAC, this fixes it
             if not package:dep("libflac"):config("shared") then
                 local cmake = io.open("CMakeLists.txt", "a")
