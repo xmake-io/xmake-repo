0024cb4db769e62a310fe0bd237963f4b2ac3936
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-10-04 12:32:49 +0200

    add version 0.11.18 of miniaudio (#2674)

--- packages/m/miniaudio/xmake.lua
+++ packages/m/miniaudio/xmake.lua
@@ -8,6 +8,7 @@ package("miniaudio")
     add_versions("0.11.15", "24a6d38fe69cd42d91f6c1ad211bb559f6c89768c4671fa05b8027f5601d5457")
     add_versions("0.11.16", "13320464820491c61bd178b95818fecb7cd0e68f9677d61e1345df6be8d4d77e")
     add_versions("0.11.17", "4b139065f7068588b73d507d24e865060e942eb731f988ee5a8f1828155b9480")
+    add_versions("0.11.18", "85ca916266d809b39902e180a6d16f82caea9c2ea1cea6d374413641b7ba48c3")
 
 
     on_install(function (package)
