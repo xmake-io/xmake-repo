b5423426da3b492319c7591f40a0f8665855a29e
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2023-10-05 22:31:48 +0800

    cppp-reiconv: Remove unused `on_download`.

--- packages/c/cppp-reiconv/xmake.lua
+++ packages/c/cppp-reiconv/xmake.lua
@@ -8,39 +8,6 @@ package("cppp-reiconv")
     add_versions("v2.1.0", "3e539785a437843793c5ce2f8a72cb08f2b543cba11635b06db25cfc6d9cc3a4")
 
     add_deps("cmake")
-    
-    on_download(function (package, opt)
-        import("net.http")
-        import("utils.archive")
-
-        local url = opt.url
-        local sourcedir = opt.sourcedir
-        local packagefile = path.filename(url)
-        local sourcehash = package:sourcehash(opt.url_alias)
-
-        local cached = true
-        if not os.isfile(packagefile) or sourcehash ~= hash.sha256(packagefile) then
-            cached = false
-
-            os.tryrm(packagefile)
-            http.download(url, packagefile)
-
-            if sourcehash and sourcehash ~= hash.sha256(packagefile) then
-                raise("unmatched checksum, current hash(%s) != original hash(%s)", hash.sha256(packagefile):sub(1, 8), sourcehash:sub(1, 8))
-            end
-        end
-
-        local sourcedir_tmp = sourcedir .. ".tmp"
-        os.rm(sourcedir_tmp)
-        if archive.extract(packagefile, sourcedir_tmp) then
-            os.rm(sourcedir)
-            os.mv(sourcedir_tmp, sourcedir)
-        else
-            os.tryrm(sourcedir)
-            os.mkdir(sourcedir)
-        end
-        package:originfile_set(path.absolute(packagefile))
-    end)
 
     on_install(function (package)
         local configs = {}
@@ -69,3 +36,4 @@ package("cppp-reiconv")
         }
     ]]}, {configs = {languages = "c++14"}}))
     end)
+
