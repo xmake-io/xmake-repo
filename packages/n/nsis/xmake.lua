34c74644a304b0c378d114b598463e471f5927bd
ruki <waruqi@gmail.com>
2023-10-09 23:49:32 +0800

    fix typo

--- packages/n/nsis/xmake.lua
+++ packages/n/nsis/xmake.lua
@@ -3,8 +3,12 @@ package("nsis")
     set_homepage("https://nsis.sourceforge.io/")
     set_description("NSIS (Nullsoft Scriptable Install System) is a professional open source system to create Windows installers.")
 
+    add_urls("https://github.com/kichik/nsis/archive/refs/tags/$(version).tar.gz", {version = function (version)
+        return version:gsub("%.", "")
+    end})
     add_urls("https://github.com/kichik/nsis.git")
-    add_versions("2023.09.02", "5907dc38b6257717e6008986fd392d597da910f1")
+
+    add_versions("v3.09", "4d2ce0d2fecc28bd2d8a0108152339f091e9d1f35342dac0eb4594157435292b")
 
     on_load(function (package)
         if not package:is_precompiled() then
@@ -17,7 +21,7 @@ package("nsis")
         local configs = {
             "NSIS_MAX_STRLEN=8192",
             "PREFIX=" .. package:installdir(),
-            "ZLIB_W32=" .. package:dep("zlib"):installdir()
+            "ZLIB_W32=" .. package:dep("zlib"):installdir(),
             "install-compiler", "install-stubs"}
         import("package.tools.scons").build(package, configs)
     end)
