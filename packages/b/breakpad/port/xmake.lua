272cae9b6fefe3c2c914d787d92db4ecdd778d32
star9029 <hengxings783@gmail.com>
2023-10-16 23:56:38 +0800

    fix dwarf files

--- packages/b/breakpad/port/xmake.lua
+++ packages/b/breakpad/port/xmake.lua
@@ -70,10 +70,12 @@ target("breakpad")
                 add_rules("utils.symbols.export_all", {export_classes = true})
             end
         else
-            -- add_files("src/common/dwarf/*.cc")
-            -- remove_files("src/common/dwarf/*test*.cc")
-            -- add_headerfiles("src/(common/dwarf/*.h)")
-            -- remove_headerfiles("src/common/dwarf/*test*.h")
+            add_files("src/common/dwarf/*.cc")
+            remove_files("src/common/dwarf/*test*.cc",
+                         "src/common/dwarf/functioninfo.cc")
+            add_headerfiles("src/(common/dwarf/*.h)")
+            remove_headerfiles("src/common/dwarf/*test*.h",
+                               "src/common/dwarf/functioninfo.h")
 
             if is_plat("macosx") then
                 add_defines("HAVE_MACH_O_NLIST_H")
