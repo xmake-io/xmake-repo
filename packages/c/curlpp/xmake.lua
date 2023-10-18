50e3d79ce2380f2bd5949ca0c3a7cda41ea5a862
star9029 <hengxings783@gmail.com>
2023-10-18 23:02:16 +0800

    curlpp: add package

---
+++ packages/c/curlpp/xmake.lua
@@ -0,0 +1,44 @@
+package("curlpp")
+    set_homepage("http://www.curlpp.org")
+    set_description("C++ wrapper around libcURL")
+    set_license("MIT")
+
+    add_urls("https://github.com/jpbarrette/curlpp.git")
+    add_versions("2023.07.27", "1d8c7876cc81d7d125b663066282b207d9cbfe9a")
+
+    add_deps("libcurl")
+
+    on_install("windows", "linux", "macosx", "mingw", "cross", function (package)
+        local curl = package:dep("libcurl")
+        local curl_configs = curl:requireinfo().configs
+        if curl_configs then
+            curl_configs = string.serialize(curl_configs, {strip = true, indent = false})
+        end
+
+        io.writefile("xmake.lua", ([[
+            set_languages("c++11")
+            add_rules("mode.debug", "mode.release")
+            add_requires("libcurl %s", {configs = %s})
+            target("curlpp")
+                set_kind("$(kind)")
+                add_files("src/**.cpp")
+                add_includedirs("include/")
+                add_headerfiles("include/(**.hpp)", "include/(**.inl)")
+                add_packages("libcurl")
+                if is_plat("windows") and is_kind("shared") then
+                    add_rules("utils.symbols.export_all", {export_classes = true})
+                end
+        ]]):format(curl:version_str(), curl_configs))
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            int main() {
+                curlpp::Cleanup cleaner;
+                curlpp::Easy request;
+                request.setOpt<curlpp::options::Url>("https://example.com");
+            }
+        ]]}, { configs = {languages = "c++11"}, includes = {"curlpp/cURLpp.hpp", "curlpp/Easy.hpp", "curlpp/Options.hpp"}
+        }))
+    end)
