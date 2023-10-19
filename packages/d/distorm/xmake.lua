f40e5cd526f86bac8deb87a843e61a62c61bcf82
star9029 <hengxings783@gmail.com>
2023-10-19 21:34:51 +0800

    distorm: add package

---
+++ packages/d/distorm/xmake.lua
@@ -0,0 +1,24 @@
+package("distorm")
+    set_homepage("https://github.com/gdabah/distorm")
+    set_description("Powerful Disassembler Library For x86/AMD64")
+
+    add_urls("https://github.com/gdabah/distorm.git")
+    add_versions("2021.12.18", "7a02caa1a936f0a653fc75f1aaea9bd3fa654603")
+
+    on_install(function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.release", "mode.debug")
+            target("distorm")
+                set_kind("$(kind)")
+                add_files("src/*.c")
+                add_headerfiles("include/*.h", {prefixdir = "distorm"})
+                if is_plat("windows") and is_kind("shared") then
+                    add_rules("utils.symbols.export_all")
+                end
+        ]])
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("distorm_version", {includes = "distorm/distorm.h"}))
+    end)
