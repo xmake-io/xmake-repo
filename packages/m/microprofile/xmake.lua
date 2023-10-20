3fd7b992d162d3be42d208d9d65810588945eb2a
star9029 <hengxings783@gmail.com>
2023-10-20 22:56:26 +0800

    port

--- packages/m/microprofile/xmake.lua
+++ packages/m/microprofile/xmake.lua
@@ -8,7 +8,7 @@ package("microprofile")
 
     add_versions("v4.0", "59cd3ee7afd3ce5cfeb7599db62ccc0611818985a8e649353bec157122902a5c")
 
-    add_configs("config_file", {description = "Use user provided configuration in microprofile.config.h file.", default = false, type = "boolean"})
+    add_configs("shared", {description = "Build shared binaries.", default = false, type = "boolean", readonly = true})
 
     if is_plat("windows", "mingw") then
         add_syslinks("ws2_32", "advapi32", "shell32")
@@ -16,15 +16,28 @@ package("microprofile")
         add_syslinks("pthread")
     end
 
-    add_deps("cmake")
     add_deps("stb")
 
     on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DMICROPROFILE_USE_CONFIG_FILE=" .. (package:config("config_file") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = "stb"})
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            set_languages("c++11")
+            add_requires("stb")
+            target("microprofile")
+                set_kind("$(kind)")
+                add_files("microprofile.cpp")
+                add_headerfiles("microprofile.h", "microprofile_html.h")
+                if is_plat("windows", "mingw") then
+                    add_syslinks("ws2_32", "advapi32", "shell32")
+                    if is_plat("windows") and is_kind("shared") then
+                        add_rules("utils.symbols.export_all", {export_classes = true})
+                    end
+                elseif is_plat("linux") then
+                    add_syslinks("pthread")
+                end
+                add_packages("stb")
+        ]])
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
