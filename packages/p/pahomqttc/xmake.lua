992107694b9b4ca764176cad8b93c9b284030616
Altyrost <altyrost@funkybox.fr>
2023-10-23 14:42:05 +0200

    add test for pahomqttc

--- packages/p/pahomqttc/xmake.lua
+++ packages/p/pahomqttc/xmake.lua
@@ -20,5 +20,6 @@ package("pahomqttc")
     end)
 
     on_test(function (package)
+        assert(package:has_cxxtypes("MQTTClient_message", {includes = "MQTTClient.h"}))
     end)   
 package_end()
