a627c631bdc3b527b3bc3343593991cd50e58516
ruki <waruqi@gmail.com>
2023-10-25 19:23:34 -0500

    Update xmake.lua

--- packages/s/srtp/xmake.lua
+++ packages/s/srtp/xmake.lua
@@ -3,8 +3,9 @@ package("srtp")
     set_homepage("https://github.com/cisco/libsrtp")
     set_description("Library for SRTP (Secure Realtime Transport Protocol)")
 
-    add_urls("https://github.com/cisco/libsrtp.git")
-    add_versions("v2.5.0", "a566a9cfcd619e8327784aa7cff4a1276dc1e895")
+    add_urls("https://github.com/cisco/libsrtp/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/cisco/libsrtp.git")
+    add_versions("v2.5.0", "8a43ef8e9ae2b665292591af62aa1a4ae41e468b6d98d8258f91478735da4e09")
 
     add_configs("openssl", {description = "Enable OpenSSL crypto engine", default = false, type = "boolean"})
     add_configs("mbedtls", {description = "Enable MbedTLS crypto engine", default = false, type = "boolean"})
@@ -13,17 +14,17 @@ package("srtp")
     add_deps("cmake")
     add_deps("openssl")
 
-    on_install(function (package)
-        local options = {
-            openssl = "ENABLE_OPENSSL",
-            mbedtls = "ENABLE_MBEDTLS",
-            nss = "ENABLE_NSS"
-        }
+    on_install("windows", "linux", "macosx", "android", "cross", "bsd", "mingw", function (package)
         local configs = {"-DLIBSRTP_TEST_APPS=OFF", "-DBUILD_WITH_WARNINGS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         for name, item in pairs(options) do
             table.insert(configs, "-D" .. item .. "=" .. (package:config(name) and "ON" or "OFF"))
+        end
+        for name, enabled in pairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                table.insert(configs, "-DENABLE_" .. name:upper() .. "=" .. (enabled and "ON" or "OFF"))
+            end
         end
         import("package.tools.cmake").install(package, configs)
     end)
