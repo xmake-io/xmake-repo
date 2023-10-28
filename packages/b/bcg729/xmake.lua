fef549a0288bc9c856bf25b916c6e38726d7401b
ping <elkpi@qq.com>
2023-10-28 14:08:49 +0800

    bcg729: add tar.gz url

--- packages/b/bcg729/xmake.lua
+++ packages/b/bcg729/xmake.lua
@@ -2,9 +2,10 @@ package("bcg729")
     set_homepage("http://linphone.org")
     set_description("Bcg729 is an opensource implementation of both encoder and decoder of the ITU G729 Annex A/B speech codec.")
 
-    add_urls("https://github.com/BelledonneCommunications/bcg729.git")
-    add_versions("1.0.4", "9ada79d7ff53815e85432e7442810a2fd49dbd0e")
-    add_versions("1.1.1", "faaa895862165acde6df8add722ba4f85a25007d")
+    add_urls("https://github.com/BelledonneCommunications/bcg729/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/BelledonneCommunications/bcg729.git")
+    add_versions("1.0.4", "94b3542a06cbd96306efc19f959f9febae62806a22599063f82a8c33e989d48b")
+    add_versions("1.1.1", "68599a850535d1b182932b3f86558ac8a76d4b899a548183b062956c5fdc916d")
 
     add_deps("cmake")
 
