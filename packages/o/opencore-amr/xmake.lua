f5fc99732fdeeeac82ee1df39599ad91652f59cd
ping <elkpi@qq.com>
2023-10-29 00:39:00 +0800

    opencore-amr: filter plat

--- packages/o/opencore-amr/xmake.lua
+++ packages/o/opencore-amr/xmake.lua
@@ -8,7 +8,7 @@ package("opencore-amr")
 
     add_deps("autoconf", "automake", "libtool")
 
-    on_install(function (package)
+    on_install("linux", "macosx", "android", "iphoneos", "bsd", "cross", "wasm", function (package)
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         if package:debug() then
