a6d6cf0d0f1d765fb9711beb1de517b9811e9252
KkemChen <kkemchen@qq.com>
2023-10-31 10:44:30 +0800

    libyuv: add package

---
+++ packages/l/libyuv/xmake.lua
@@ -0,0 +1,30 @@
+package("libyuv")
+
+    set_homepage("https://chromium.googlesource.com/libyuv/libyuv/")
+    set_description("libyuv is an open source project that includes YUV scaling and conversion functionality.")
+    set_license("BSD-3-Clause")
+
+    set_urls("https://chromium.googlesource.com/libyuv/libyuv.git")
+    add_versions("2023.10.27", "31e1d6f896615342d5d5b6bde8f7b50b3fd698dc")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {"-DTEST=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        
+        io.replace("CMakeLists.txt", "INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert			DESTINATION bin )", "", {plain = true})
+        import("package.tools.cmake").install(package, configs)
+        
+        if is_plat("macosx", "linux") then
+            if package:config("shared") then 
+                os.tryrm(package:installdir("lib","*.a"))
+            else 
+                os.tryrm(package:installdir("lib","*.so"))
+            end
+        end
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("I420Rotate", {includes = "libyuv/rotate.h"}))
+    end)
