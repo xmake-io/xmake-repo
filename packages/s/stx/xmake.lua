c387eb6b77e0723cdb32dd32b05f3bd0d8599da3
star9029 <hengxings783@gmail.com>
2023-11-04 08:53:14 +0800

    disable plat

--- packages/s/stx/xmake.lua
+++ packages/s/stx/xmake.lua
@@ -9,6 +9,7 @@ package("stx")
     add_versions("v1.0.3", "0118b76a5f2a7a60132edecc9b632d9fc82a187bc232ac1a3fd9200fdd92dc7d")
 
     add_configs("backtrace", {description = "Enable stack backtraces", default = false, type = "boolean"})
+    add_configs("custom_handler", {description = "Override the default panic behaviour by implementing a custom panic handler. The default behavior is to print the panic report and abort the program. (You can read the docs for more details)", default = false, type = "boolean"})
     add_configs("shared", {description = "Build shared binaries", default = false, type = "boolean", readonly = true})
 
     if is_plat("linux", "bsd") then
@@ -17,11 +18,12 @@ package("stx")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "bsd", "mingw", "msys", "iphoneos", "cross", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DSTX_ENABLE_BACKTRACE=" .. (package:config("backtrace") and "ON" or "OFF"))
+        table.insert(configs, "-DSTX_CUSTOM_PANIC_HANDLER=" .. (package:config("custom_handler") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
