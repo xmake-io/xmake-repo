dc875e1cf4c4bf81e4991fc59933b152fa4ad9b8
Theramar <81759060+Theramar@users.noreply.github.com>
2023-11-08 19:47:24 +0100

    changed sha

--- packages/b/boost_interval/xmake.lua
+++ packages/b/boost_interval/xmake.lua
@@ -3,11 +3,10 @@ package("boost_interval")
     set_homepage("http://boost.org/libs/numeric/interval/doc")
     set_description("interval: Boost.org numeric interval library")
 
-    add_urls("https://github.com/boostorg/interval/archive/refs/tags/boost-$(version).zip",
-             "https://github.com/boostorg/interval/archive/refs/tags/boost-$(version).tar.gz",
+    add_urls("https://github.com/boostorg/interval/archive/refs/tags/boost-$(version).tar.gz",
              "https://github.com/boostorg/interval.git")
 
-    add_versions("1.83.0", "2eda7413ac16dd4158005446438daf8a7e435dd9")
+    add_versions("1.83.0", "c0685b68dd44cc46574cce86c4e17c0f611b15e195be9848dfd0769a0a207628")
 
     on_install(function (package)
         os.cp("include", package:installdir())
