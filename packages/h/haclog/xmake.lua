a23f9691960ece25f933e4677928f5ca91e215da
star9029 <hengxings783@gmail.com>
2023-11-09 22:42:59 +0800

    disable plat

--- packages/h/haclog/xmake.lua
+++ packages/h/haclog/xmake.lua
@@ -14,7 +14,7 @@ package("haclog")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "bsd", "mingw@windows,msys", "android", "cross", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
