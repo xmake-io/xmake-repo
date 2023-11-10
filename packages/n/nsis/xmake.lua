a89f3581121dbdc308aba3915242178095e9bc24
ruki <waruqi@gmail.com>
2023-11-11 00:45:48 +0800

    disable examples

--- packages/n/nsis/xmake.lua
+++ packages/n/nsis/xmake.lua
@@ -36,6 +36,8 @@ package("nsis")
             "SKIPUTILS=NSIS Menu",
             "DOCTYPES=web",
             "dist"}
+        io.replace("Examples/SConscript", "env.DistributeExamples(examples)", "", {plain = true})
+        io.replace("Examples/SConscript", "env.DistributeExamples(mui_examples, path='Modern UI')", "", {plain = true})
         os.vrunv("scons", configs)
         os.cp(path.join(package:resourcedir("uac"), "UAC.nsh"), path.join(package:installdir(), "Include"))
     end)
