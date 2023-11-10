c88fa1663f23e0ab8858e6498b43e62bee4ceaca
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-11-10 16:49:27 +0000

    cartographer: update port `xmake.lua`

--- packages/c/cartographer/port/xmake.lua
+++ packages/c/cartographer/port/xmake.lua
@@ -2,31 +2,33 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("boost", {configs = {iostreams = true}})
 add_requires("ceres-solver", {configs = {suitesparse = true}})
-add_requires("cairo", "eigen", "lua", "protobuf-cpp")
+add_requires("abseil", "cairo", "eigen", "glog", "lua", "protobuf-cpp")
 
 target("cartographer")
     set_kind("$(kind)")
-    set_languages("cxx11")
+    set_languages("cxx14")
 
     add_packages(
+        "abseil",
         "boost", 
         "ceres-solver", 
         "cairo", 
-        "eigen", 
+        "eigen",
+        "glog",
         "lua"
     )
+    
     add_packages("protobuf-cpp", {public = true})
     add_rules("protobuf.cpp")
 
-    add_files("cartographer/**.proto", {proto_autogendir = path.join(os.projectdir(), "build", "proto"), proto_public = true})
+    add_files("cartographer/**.proto", {proto_rootdir = "$(scriptdir)", proto_public = true})
     remove_files("cartographer/**_service.proto")
-    
-    add_headerfiles("$(buildir)/proto/cartographer/**.h")
-    add_files("$(buildir)/proto/cartographer/**.cc")
-    add_includedirs("$(buildir)/proto")
 
     add_headerfiles("(cartographer/**.h)")
     add_files("cartographer/**.cc")
+
+    remove_files("cartographer/io/serialization_format_migration.cc")
+    remove_headerfiles("cartographer/io/serialization_format_migration.h")
 
     remove_headerfiles("**/fake_*.h", "**/*test_helpers*.h", "**/mock*.h")
     remove_files("**/fake_*.cc", "**/*test_helpers*.cc", "**/mock*.cc", "**/*_main.cc", "**/*_test.cc")
