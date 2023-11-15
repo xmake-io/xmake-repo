41918aa1bd4cc731a3195e14e885317ba18edfdd
zjyhjqs <zjyhjqs@Hotmail.com>
2023-11-16 00:00:57 +0800

    Update CGAL

--- packages/c/cgal/xmake.lua
+++ packages/c/cgal/xmake.lua
@@ -6,13 +6,19 @@ package("cgal")
 
     add_urls("https://github.com/CGAL/cgal/releases/download/v$(version)/CGAL-$(version)-library.zip")
     add_versions("5.1.1", "ceca7ea896505941878f6c1fb7a7ae86653fdd9b3d87b276da72227f173a9cd2")
+    add_versions("5.1.5", "dd1ec792da32ea40881ac786cea6a7e80755cf275de794b870fdfd02fbab1208")
     add_versions("5.2.1", "ccdea67db79153417504f50c534cea3bb6b0e9754e7f32fb753fc19005114db0")
+    add_versions("5.2.3", "995205202ebff2f2cb614b8831cd35d9bcde768a65c3291975b77faa53c352ca")
     add_versions("5.3", "49ccfb6b72a78d03ab026c6502099ba9358cf604d9d1f51c33e90b314635fe35")
+    add_versions("5.3.2", "f4b3ef432895ded307b4da54e05626ade24fe5cb02e9ba0017feda1ae28b5332")
     add_versions("5.4", "dbca692666866df5979ef14264570b85a00f21cb77a9f9420ec0823ce8fae473")
+    add_versions("5.4.5", "af132d94b9c9d52c780cb09ef3742681946e72b799ef1474883875351c6ed553")
     add_versions("5.5.2", "77b17ba5d43fe844c0eca7fb7eff7d5687174b037290c390f1251ef44cc4909b")
+    add_versions("5.5.3", "7ee94d721b42ba386ff61a08a5b3adc6b399e4064e91abdfa7dd1031388f29f7")
+    add_versions("5.6", "aea66cfa617dfdb7450844969b2e0b2a3e83319fee1c358bc8b027943f64a247")
 
     add_configs("header_only", {description = "Use header only version.", default = true, type = "boolean"})
-    
+
     if is_plat("mingw") and is_subhost("msys") then
         add_extsources("pacman::cgal")
     elseif is_plat("linux") then
