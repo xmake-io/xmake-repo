e37de9e7011e0c2ad218632f430eedb47da3a9db
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-11-17 20:24:30 +0800

    Update xmake.lua

--- packages/a/acl-server/xmake.lua
+++ packages/a/acl-server/xmake.lua
@@ -7,21 +7,8 @@ package("acl-server")
     set_urls("https://github.com/acl-dev/acl.git")
     add_versions("2023.11.17", "1d9386a73d0317850be27d5a94381e8c4b9ffd26")
 
-    add_deps("cmake")
-
-    on_load(function (package)
-        if package:config("shared") then
-            package:add("defines", "ACL_DLL", "HTTP_DLL", "ACL_CPP_DLL")
-        end
-    end)
-
     on_install(function (package)
-        local configs = {"-DACL_BUILD_SHARED =" .. (package:config("shared") and "YES" or "NO")}
-        import("package.tools.cmake").install(package, configs)
-        os.cp(path.join("lib_acl_cpp", "include"), package:installdir())
-        os.cp(path.join("lib_acl", "include"), package:installdir())
-        os.cp(path.join("lib_fiber", "cpp", "include"), package:installdir())
-        os.cp(path.join("lib_protocol", "include"), package:installdir())
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
