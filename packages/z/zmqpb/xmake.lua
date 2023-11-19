21105723cf5ef81a041c5281014b7fab936793d3
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-11-19 20:18:42 +0100

    Add zmqpb v0.4

--- packages/z/zmqpb/xmake.lua
+++ packages/z/zmqpb/xmake.lua
@@ -8,6 +8,7 @@ package("zmqpb")
     add_versions("0.1", "4a34ec92faa381306356e84e2a2000093d8f76cfa037db1f4cd0adb0205faebb")
     add_versions("0.2", "5dfa4d4cebb10cb7ae03943e18e8d48c8ff215e80371f24c5ade212be7f20721")
     add_versions("0.3", "343c57c9f72facca47082422a259ec8c531f5c6e332a3828835080c4a96b9064")
+    add_versions("0.4", "7c0001db73b19e65b007adf6c9c5092c3589f043ab3e95a16b3ec2b2a87fa244")
 
     add_deps("cppzmq")
     add_deps("protobuf-cpp")
