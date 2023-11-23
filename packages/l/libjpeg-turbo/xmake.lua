5017f85bbe6649f7f465198a1ad57bbfc4f82d74
xq114 <1140735506@qq.com>
2023-11-23 23:34:53 +0800

    update libjpeg-turbo

--- packages/l/libjpeg-turbo/xmake.lua
+++ packages/l/libjpeg-turbo/xmake.lua
@@ -12,17 +12,18 @@ package("libjpeg-turbo")
     add_versions("2.1.0",  "d6b7790927d658108dfd3bee2f0c66a2924c51ee7f9dc930f62c452f4a638c52")
     add_versions("2.1.1",  "20e9cd3e5f517950dfb7a300ad344543d88719c254407ffb5ad88d891bf701c4")
     add_versions("2.1.2",  "e7fdc8a255c45bc8fbd9aa11c1a49c23092fcd7379296aeaeb14d3343a3d1bed")
-    add_versions("2.1.3", "dbda0c685942aa3ea908496592491e5ec8160d2cf1ec9d5fd5470e50768e7859")
-    add_versions("2.1.4", "a78b05c0d8427a90eb5b4eb08af25309770c8379592bb0b8a863373128e6143f")
+    add_versions("2.1.3",  "dbda0c685942aa3ea908496592491e5ec8160d2cf1ec9d5fd5470e50768e7859")
+    add_versions("2.1.4",  "a78b05c0d8427a90eb5b4eb08af25309770c8379592bb0b8a863373128e6143f")
+    add_versions("3.0.1",  "5b9bbca2b2a87c6632c821799438d358e27004ab528abf798533c15d50b39f82")
 
     add_configs("jpeg", {description = "libjpeg API/ABI emulation target version.", default = "6", type = "string", values = {"6", "7", "8"}})
 
-    if is_subhost("windows") and is_plat("android") then
+    if is_plat("android") then
         add_deps("make")
     end
 
     on_load(function (package)
-        if not package.is_built or package:is_built() then
+        if package:is_built() then
             package:add("deps", "cmake", "nasm")
         end
     end)
@@ -53,7 +54,6 @@ package("libjpeg-turbo")
             io.replace("CMakeLists.txt", 'message(STATUS "${BITS}-bit build (${CPU_TYPE})")',
                 'set(CPU_TYPE arm64)\nmessage(STATUS "${BITS}-bit build (${CPU_TYPE})")', {plain = true})
         end
-        table.insert(configs, "-DCMAKE_INSTALL_LIBDIR:PATH=lib")
         import("package.tools.cmake").install(package, configs)
     end)
 
