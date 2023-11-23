bb9aa676f0d201a5119df38edf5a3969d827bdd1
xq114 <1140735506@qq.com>
2023-11-23 23:34:58 +0800

    update make

--- packages/m/make/xmake.lua
+++ packages/m/make/xmake.lua
@@ -4,11 +4,13 @@ package("make")
     set_homepage("https://www.gnu.org/software/make/")
     set_description("GNU make tool.")
 
-    add_urls("https://ftp.gnu.org/gnu/make/make-$(version).tar.gz",
+    add_urls("https://ftpmirror.gnu.org/gnu/make/make-$(version).tar.gz",
+             "https://ftp.gnu.org/gnu/make/make-$(version).tar.gz",
              "https://mirrors.ustc.edu.cn/gnu/make/make-$(version).tar.gz",
              "http://mirror.easyname.at/gnu/make/make-$(version).tar.gz")
     add_versions("4.2.1", "e40b8f018c1da64edd1cc9a6fce5fa63b2e707e404e20cad91fbae337c98a5b7")
     add_versions("4.3", "e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19")
+    add_versions("4.4.1", "dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3")
 
     if is_host("linux") then
         add_extsources("pacman::make", "apt::make")
