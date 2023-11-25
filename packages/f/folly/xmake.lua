fd91de01190b03891a59060346a58db2b3fd0a83
jj683 <jj683@protonmail.com>
2023-11-26 00:10:33 +0300

    Enable GLOG_NO_ABBREVIATED_SEVERITIES patch

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -17,6 +17,7 @@ package("folly")
     add_patches("<=2022.08.29", path.join(os.scriptdir(), "patches", "2021.06.28", "regex.patch"), "6a77ade9f48dd9966d3f7154e66ca8a5c030ae2b6d335cbe3315784aefd8f495")
     add_patches("<=2023.11.20", path.join(os.scriptdir(), "patches", "2021.06.28", "reorder.patch"), "9a6bf283881580474040cfc7a8e89d461d68b89bae5583d89fff0a3198739980")
     add_patches("<=2023.11.20", path.join(os.scriptdir(), "patches", "2023.11.20", "pkgconfig.patch"), "6838623d453418569853f62ad97c729e802a120c13d804aabba6d6455997e674")
+    add_patches("<=2023.11.20", path.join(os.scriptdir(), "patches", "2023.11.20", "glog.patch"), "ccdafe3579dd41414f60b8ad36fd071ce08bd5cf73f27d47c3717679ca3f6568")
 
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
