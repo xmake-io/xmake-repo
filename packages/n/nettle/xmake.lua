a57c9af30f7eef0648e253784b23ad68682380b0
xq114 <1140735506@qq.com>
2023-11-25 20:33:19 +0800

    add dep m4

--- packages/n/nettle/xmake.lua
+++ packages/n/nettle/xmake.lua
@@ -9,6 +9,11 @@ package("nettle")
     add_versions("3.6", "d24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1")
     add_versions("3.9.1", "ccfeff981b0ca71bbd6fbcb054f407c60ffb644389a5be80d6716d5b550c6ce3")
 
+    add_deps("m4")
+    if is_plat("linux") then
+        add_extsources("apt::nettle-dev")
+    end
+
     on_install("macosx", "linux", function (package)
         local configs = {"--disable-openssl", "--disable-documentation", "--enable-pic"}
         if package:config("shared") then
