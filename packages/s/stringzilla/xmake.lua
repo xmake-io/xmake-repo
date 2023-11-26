55a2fe604f497dfdf966d9b3da4b394ce338de3b
star9029 <hengxings783@gmail.com>
2023-11-26 14:28:15 +0800

    Update xmake.lua

--- packages/s/stringzilla/xmake.lua
+++ packages/s/stringzilla/xmake.lua
@@ -7,6 +7,7 @@ package("stringzilla")
     add_urls("https://github.com/ashvardanian/StringZilla/archive/refs/tags/$(version).tar.gz",
              "https://github.com/ashvardanian/StringZilla.git")
 
+    add_versions("v1.2.2", "2e17c49965841647a1c371247f53b2f576e5fb32fe4b84a080d425b12f17703c")
     add_versions("v2.0.3", "6b52a7b4eb8383cbcf83608eaa08e5ba588a378449439b73584713a16d8920e3")
 
     on_install(function (package)
@@ -14,5 +15,9 @@ package("stringzilla")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("sz_sort", {includes = "stringzilla.h"}))
+        if package:version():gt("2.0.0") then
+            assert(package:has_cfuncs("sz_sort", {includes = "stringzilla.h"}))
+        else
+            assert(package:has_cxxfuncs("strzl_sort", {includes = "stringzilla.h", configs = {languages = "c++17"}}))
+        end
     end)
