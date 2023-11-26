209ce71f2f8aefbd3bc73a6343d986d4d0f74a15
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-11-26 13:52:30 +0000

    ixwebsocket: add config to use mbedtls

--- packages/i/ixwebsocket/xmake.lua
+++ packages/i/ixwebsocket/xmake.lua
@@ -8,6 +8,8 @@ package("ixwebsocket")
 
     add_versions("v11.4.4", "9ef7fba86a91ce18693451466ddc54b1e0c4a7dc4466c3028d888d6d55dde539")
 
+    add_configs("ssl", {description = "Enable SSL", default = nil, type = "string", values = {"openssl", "mbedtls"}})
+
     add_configs("use_tls", {description = "Use TLS", default = false, type = "boolean"})
 
     add_deps("zlib")
@@ -16,12 +18,17 @@ package("ixwebsocket")
     elseif is_plat("macosx") then
         add_frameworks("Foundation", "Security")
     end
-    if not is_plat("windows") then
-        add_deps("openssl")
-    end
     add_deps("cmake")
 
-    on_install(function (package)
+    on_load(function (package)
+        if package:config("ssl") == "openssl" then
+            package:add("deps", "openssl")
+        elseif package:config("ssl") == "mbedtls" then
+            package:add("deps", "mbedtls")
+        end
+    end)
+
+    on_install("windows", "linux", "macosx", "bsd", function (package)
         local configs = {"-DCMAKE_CXX_STANDARD=11"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
