7d71e1ccb434a209358d92447d2463906b23aa88
star9029 <hengxings783@gmail.com>
2023-12-03 11:25:07 +0800

    revert

--- packages/a/alsa-lib/xmake.lua
+++ packages/a/alsa-lib/xmake.lua
@@ -1,10 +1,12 @@
 package("alsa-lib")
     set_homepage("https://alsa-project.org/wiki/Main_Page")
     set_description("The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system.")
+    set_license("LGPL-2.1")
 
-    set_urls("http://www.alsa-project.org/files/pub/lib/alsa-lib-$(version).tar.bz2")
-
-    add_versions("1.2.10", "c86a45a846331b1b0aa6e6be100be2a7aef92efd405cf6bac7eef8174baa920e")
+    add_urls("http://www.alsa-project.org/files/pub/lib/alsa-lib-$(version).tar.bz2", {alias = "home"})
+    add_urls("https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$(version).tar.gz", {alias = "github"})
+    add_versions("home:1.2.10", "c86a45a846331b1b0aa6e6be100be2a7aef92efd405cf6bac7eef8174baa920e")
+    add_versions("github:1.2.10", "f55749847fd98274501f4691a2d847e89280c07d40a43cdac43d6443f69fc939")
 
     if is_plat("linux") then
         add_extsources("pacman::alsa-lib", "apt::libasound2-dev")
