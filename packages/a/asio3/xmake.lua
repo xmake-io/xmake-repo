285d5b9bccd382011af34205b33d9680fbaefd40
ruki <waruqi@gmail.com>
2023-12-04 23:00:37 +0800

    remove spaces

--- packages/a/asio3/xmake.lua
+++ packages/a/asio3/xmake.lua
@@ -1,5 +1,4 @@
 package("asio3")
-
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/zhllxt/asio3")
     set_description("Header only c++ network library, based on c++ 20 coroutine and asio")
@@ -11,7 +10,6 @@ package("asio3")
     add_deps("asio", "cereal", "fmt", "openssl3", "nlohmann_json")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
 
-
     on_install("windows", "linux", "macosx", "mingw", "bsd", function (package)
         os.cp(path.join("include"), package:installdir())
     end)
@@ -22,7 +20,6 @@ package("asio3")
             namespace net = ::asio;
             void test() {
                 net::io_context ctx;
-
 	            net::tcp_client client(ctx.get_executor());
             }
         ]]}, {configs = {languages = "c++23"}}))
