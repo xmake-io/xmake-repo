ff47b0bbb75b554044a1c3b48beb7de0813991b2
star9029 <hengxings783@gmail.com>
2023-12-10 01:20:52 +0800

    alsa-lib: add config

--- packages/a/alsa-lib/xmake.lua
+++ packages/a/alsa-lib/xmake.lua
@@ -8,6 +8,8 @@ package("alsa-lib")
     add_versions("home:1.2.10", "c86a45a846331b1b0aa6e6be100be2a7aef92efd405cf6bac7eef8174baa920e")
     add_versions("github:1.2.10", "f55749847fd98274501f4691a2d847e89280c07d40a43cdac43d6443f69fc939")
 
+    add_configs("without_versioned", {description = "pass flag --without-versioned", default = true, type = "boolean"})
+
     if is_plat("linux") then
         add_extsources("pacman::alsa-lib", "apt::libasound2-dev")
     end
@@ -18,6 +20,9 @@ package("alsa-lib")
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
         if package:is_debug() then
             table.insert(configs, "--enable-debug")
+        end
+        if package:config("without_versioned")then
+            table.insert(configs, "--without-versioned")
         end
         import("package.tools.autoconf").install(package, configs)
     end)
--- packages/o/openjdk/xmake.lua
+++ packages/o/openjdk/xmake.lua
@@ -27,8 +27,8 @@ package("openjdk")
     add_configs("shared", {description = "Download shared binaries.", default = true, type = "boolean", readonly = true})
 
     if is_plat("linux") then
-        add_deps("alsa-lib", {configs = {shared = true}})
-        -- add_deps("freetype", "libxtst", "libxi", "libxrender")
+        add_deps("alsa-lib", {configs = {without_versioned = false}})
+        add_deps("freetype", "libxtst", "libxi", "libxrender")
         add_extsources("pacman::jdk-openjdk", "apt::default-jdk")
     elseif is_plat("macosx") then
         add_extsources("brew::openjdk")
