d6a56f4d188bfe12cd15851866f82961a2e74286
ruki <waruqi@gmail.com>
2023-12-15 00:49:56 +0800

    disable python

--- packages/r/rpm/xmake.lua
+++ packages/r/rpm/xmake.lua
@@ -8,7 +8,6 @@ package("rpm")
     add_versions("4.19.0", "19083de356ef80f2497686fd6e52011ec2d3e2dfb481e113a9f4dd1b03b47347")
 
     add_deps("cmake", "lua", "doxygen")
-    add_deps("python 3.x", {kind = "binary"})
     add_deps("openssl", "popt", "libcap", "acl", "sqlite3", "libarchive", "libmagic", "lzma", "zstd", "bzip2", "lz4", "zlib")
     add_deps("elfutils", {configs = {libdw = true}})
 
@@ -19,6 +18,7 @@ package("rpm")
             "-DCMAKE_INSTALL_LOCALSTATEDIR=var",
             "-DENABLE_NLS=ON",
             "-DENABLE_PLUGINS=OFF",
+            "-DENABLE_PYTHON=OFF",
             "-DWITH_AUDIT=OFF",
             "-DWITH_INTERNAL_OPENPGP=ON",
             "-DWITH_OPENSSL=ON",
