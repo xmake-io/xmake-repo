28037598c1a8b8808c5b11cd10db31f641b9337a
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-12-15 19:33:25 +0800

    Update xmake.lua

--- packages/a/abseil/xmake.lua
+++ packages/a/abseil/xmake.lua
@@ -16,28 +16,6 @@ package("abseil")
 
     add_deps("cmake")
 
-    add_links(
-        "absl_status", "absl_cord",
-        "absl_flags", "absl_flags_parse", "absl_flags_internal", "absl_flags_reflection", "absl_flags_marshalling",
-        "absl_flags_commandlineflag_internal", "absl_synchronization", "absl_time", "absl_hash", "absl_city", "absl_time_zone",
-        "absl_spinlock_wait", "absl_failure_signal_handler", "absl_bad_optional_access", "absl_flags_commandlineflag",
-        "absl_random_internal_pool_urbg",
-        "absl_cordz_info", "absl_cord_internal", "absl_cordz_functions", "absl_cordz_handle", "absl_cordz_sample_token",
-        "absl_base", "absl_bad_any_cast_impl", "absl_periodic_sampler", "absl_random_distributions",
-        "absl_flags_usage_internal", "absl_random_seed_sequences",
-        "absl_throw_delegate", "absl_stacktrace", "absl_symbolize", "absl_debugging_internal",
-        "absl_flags_private_handle_accessor",
-        "absl_strings", "absl_flags_config", "absl_malloc_internal", "absl_str_format_internal",
-        "absl_flags_usage", "absl_strings_internal", "absl_flags_program_name", "absl_int128",
-        "absl_scoped_set_env", "absl_raw_hash_set", "absl_random_internal_seed_material",
-        "absl_random_internal_randen", "absl_random_internal_randen_slow", "absl_random_internal_randen_hwaes_impl",
-        "absl_random_internal_randen_hwaes",
-        "absl_graphcycles_internal", "absl_exponential_biased", "absl_bad_variant_access", "absl_statusor",
-        "absl_random_internal_distribution_test_util", "absl_random_internal_platform",
-        "absl_hashtablez_sampler", "absl_demangle_internal", "absl_leak_check", "absl_log_severity", "absl_raw_logging_internal",
-        "absl_strerror", "absl_examine_stack", "absl_low_level_hash", "absl_random_seed_gen_exception", "absl_civil_time",
-        "absl_crc_cord_state", "absl_crc32c", "absl_crc_cpu_detect", "absl_crc_internal")
-
     if is_plat("macosx") then
         add_frameworks("CoreFoundation")
     end
@@ -47,8 +25,32 @@ package("abseil")
             package:add("defines", "ABSL_CONSUME_DLL")
             package:add("links", "abseil_dll")
         end
-        if package:version():eq("20230802.1") and package:is_plat("mingw") then
-            raise("abseil 20230802.1 not support mingw")
+        if package:version():eq("20230802.1") then
+            if package:is_plat("mingw") then
+                raise("abseil 20230802.1 not support mingw")
+            end
+        elseif package:version():lt("20230802.1") then
+            add_links(
+            "absl_status", "absl_cord",
+            "absl_flags", "absl_flags_parse", "absl_flags_internal", "absl_flags_reflection", "absl_flags_marshalling",
+            "absl_flags_commandlineflag_internal", "absl_synchronization", "absl_time", "absl_hash", "absl_city", "absl_time_zone",
+            "absl_spinlock_wait", "absl_failure_signal_handler", "absl_bad_optional_access", "absl_flags_commandlineflag",
+            "absl_random_internal_pool_urbg",
+            "absl_cordz_info", "absl_cord_internal", "absl_cordz_functions", "absl_cordz_handle", "absl_cordz_sample_token",
+            "absl_base", "absl_bad_any_cast_impl", "absl_periodic_sampler", "absl_random_distributions",
+            "absl_flags_usage_internal", "absl_random_seed_sequences",
+            "absl_throw_delegate", "absl_stacktrace", "absl_symbolize", "absl_debugging_internal",
+            "absl_flags_private_handle_accessor",
+            "absl_strings", "absl_flags_config", "absl_malloc_internal", "absl_str_format_internal",
+            "absl_flags_usage", "absl_strings_internal", "absl_flags_program_name", "absl_int128",
+            "absl_scoped_set_env", "absl_raw_hash_set", "absl_random_internal_seed_material",
+            "absl_random_internal_randen", "absl_random_internal_randen_slow", "absl_random_internal_randen_hwaes_impl",
+            "absl_random_internal_randen_hwaes",
+            "absl_graphcycles_internal", "absl_exponential_biased", "absl_bad_variant_access", "absl_statusor",
+            "absl_random_internal_distribution_test_util", "absl_random_internal_platform",
+            "absl_hashtablez_sampler", "absl_demangle_internal", "absl_leak_check", "absl_log_severity", "absl_raw_logging_internal",
+            "absl_strerror", "absl_examine_stack", "absl_low_level_hash", "absl_random_seed_gen_exception", "absl_civil_time",
+            "absl_crc_cord_state", "absl_crc32c", "absl_crc_cpu_detect", "absl_crc_internal")
         end
     end)
 
