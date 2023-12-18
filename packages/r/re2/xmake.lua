5a937dd7443d667c5badc560fa744b8a420a8eff
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-12-18 17:27:53 +0800

    Update xmake.lua

--- packages/r/re2/xmake.lua
+++ packages/r/re2/xmake.lua
@@ -23,7 +23,7 @@ package("re2")
             if not package:is_plat("macosx", "linux", "windows", "mingw", "cross") then
                 raise("re2 2023.11.01 only support macosx linux windows mingw cross")
             end
-            package:add("deps", "abseil 20230802.1")
+            package:add("deps", "abseil")
         end
     end)
 
