907c3becfee3859b19b27ecb344171365f0e8507
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-23 20:02:57 +0000

    libgpiod: test older version

--- packages/l/libgpiod/xmake.lua
+++ packages/l/libgpiod/xmake.lua
@@ -4,8 +4,8 @@ package("libgpiod")
 
     add_urls("https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/snapshot/libgpiod-$(version).tar.gz",
              "https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git")
-    add_versions("v2.0.1", "cf0d4db1d94cc99281de142063d0e28f42760c4d918d6b8854e1b27811517c34")
-    add_versions("v2.0", "a0f835c4ca4a2a3ca021090b574235ba58bb9fd612d8a6051fb1350054e04fdd")
+    -- add_versions("v2.0.1", "cf0d4db1d94cc99281de142063d0e28f42760c4d918d6b8854e1b27811517c34")
+    -- add_versions("v2.0", "a0f835c4ca4a2a3ca021090b574235ba58bb9fd612d8a6051fb1350054e04fdd")
     add_versions("v1.6.4", "9f920260c46b155f65cba8796dcf159e4ba56950b85742af357d75a1af709e68")
 
     add_configs("enable_bindings_cxx", {description = "Enable C++ bindings", default = true, type = "boolean"})
