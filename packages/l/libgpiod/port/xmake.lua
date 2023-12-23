8111c4c7b4c9938d7e5df96ab4268fd4c56587b3
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-23 19:20:57 +0000

    libgpiod: use add_defines

--- packages/l/libgpiod/port/xmake.lua
+++ packages/l/libgpiod/port/xmake.lua
@@ -16,7 +16,7 @@ target("libgpiod")
         local configure = io.readfile("configure.ac")
         local version = configure:match("AC_INIT%(%[libgpiod%], %[([0-9%.]+)%]%)")
         print("version: " .. version)
-        package:add("defines", "GPIOD_VERSION_STR=" .. version)
+        add_defines("GPIOD_VERSION_STR=" .. version)
     end)
 
 for _, tool_file in ipairs(os.files("tools/*.c")) do
