6cd95b97b14033dffe68c9c4459cbb9cf8ce50e2
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-23 19:58:55 +0000

    libgpiod: on_test use config

--- packages/l/libgpiod/xmake.lua
+++ packages/l/libgpiod/xmake.lua
@@ -25,17 +25,21 @@ package("libgpiod")
     end)
 
     on_test(function (package)
-        os.runv("gpiodetect")
         assert(package:check_cxxsnippets({test = [[
             #include <gpiod.h>
             void test() {
                 gpiod_api_version();
             }
         ]]}, {configs = {languages = "c++11"}}))
-        assert(package:check_cxxsnippets({test = [[
-            #include <gpiod.hpp>
-            void test() {
-                gpiod::api_version;
-            }
-        ]]}, {configs = {languages = "c++17"}}))
+        if package:config("enable_bindings_cxx") then
+            assert(package:check_cxxsnippets({test = [[
+                #include <gpiod.hpp>
+                void test() {
+                    gpiod::api_version;
+                }
+            ]]}, {configs = {languages = "c++17"}}))
+        end
+        if package:config("enable_tools") then
+            os.runv("gpiodetect")
+        end
     end)
