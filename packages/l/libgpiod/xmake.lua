c7c515116caa18d68fa96bc7ddff7383c2e784fb
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-23 22:22:37 +0000

    libgpiod: set kind to shared if version under v1.6.4

--- packages/l/libgpiod/xmake.lua
+++ packages/l/libgpiod/xmake.lua
@@ -4,18 +4,19 @@ package("libgpiod")
 
     add_urls("https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/snapshot/libgpiod-$(version).tar.gz",
              "https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git")
-    -- add_versions("v2.0.1", "cf0d4db1d94cc99281de142063d0e28f42760c4d918d6b8854e1b27811517c34")
-    -- add_versions("v2.0", "a0f835c4ca4a2a3ca021090b574235ba58bb9fd612d8a6051fb1350054e04fdd")
+    add_versions("v2.0.1", "cf0d4db1d94cc99281de142063d0e28f42760c4d918d6b8854e1b27811517c34")
+    add_versions("v2.0", "a0f835c4ca4a2a3ca021090b574235ba58bb9fd612d8a6051fb1350054e04fdd")
     add_versions("v1.6.4", "9f920260c46b155f65cba8796dcf159e4ba56950b85742af357d75a1af709e68")
    
-    add_patches("v1.6.4", path.join(os.scriptdir(), "patches", "v1.6.4", "fix_cxx.patch"), "5a56708fe2ba98dac0aadac1246d4fd55d8f34870e85fdc3430fc0a7899b1bcf")
-
     add_configs("enable_bindings_cxx", {description = "Enable C++ bindings", default = true, type = "boolean"})
     add_configs("enable_tools", {description = "Enable tools", default = true, type = "boolean"})
 
     on_install("linux", function (package)
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
+        if package:version():le("v1.6.4") then
+            configs.kind = "shared"
+        end
         if package:config("enable_bindings_cxx") then
             configs.enable_bindings_cxx = true
         end
@@ -23,7 +24,6 @@ package("libgpiod")
             configs.enable_tools = true
         end
         import("package.tools.xmake").install(package)
-        package:addenv("PATH", "bin")
     end)
 
     on_test(function (package)
