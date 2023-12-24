de270696aeb174ffa658db25542a7d92c8399892
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-24 00:58:44 +0000

    libgpiod: use same methods for other deps

--- packages/l/libgpiod/port/xmake.lua
+++ packages/l/libgpiod/port/xmake.lua
@@ -22,14 +22,21 @@ if has_config("enable_bindings_cxx") then
         set_kind("$(kind)")
         set_languages("cxx17")
 
+        add_headerfiles("include/(gpiod.h)")
+        add_headerfiles("lib/uapi/*.h")
+        add_files("lib/*.c")
+
         add_headerfiles("bindings/cxx/(gpiod.hpp)")
         add_headerfiles("bindings/cxx/(gpiodcxx/**.hpp)")
         add_files("bindings/cxx/*.cpp")
         
         add_includedirs("bindings/cxx", {public = true})
-        
-        add_deps("libgpiod")
-        add_links("libgpiod")
+
+        before_build(function (target)
+            local configure = io.readfile("configure.ac")
+            local version = configure:match("AC_INIT%(%[libgpiod%], %[?([0-9%.]+)%]?%)")
+            target:add("defines", "GPIOD_VERSION_STR=\"" .. version .. "\"")
+        end)
 end
 
 if has_config("enable_tools") then
@@ -39,13 +46,22 @@ if has_config("enable_tools") then
             target(name)
                 set_kind("binary")
 
+                add_headerfiles("include/(gpiod.h)")
+                add_headerfiles("lib/uapi/*.h")
+                add_files("lib/*.c")
+
                 add_files("tools/" .. name .. ".c")
                 add_headerfiles("tools/tools-common.h")
                 add_files("tools/tools-common.c")
 
                 add_defines("program_invocation_short_name=\"" .. name .. "\"")
                 add_defines("program_invocation_name=\"" .. name .. "\"")
-                add_deps("libgpiod")
+
+                before_build(function (target)
+                    local configure = io.readfile("configure.ac")
+                    local version = configure:match("AC_INIT%(%[libgpiod%], %[?([0-9%.]+)%]?%)")
+                    target:add("defines", "GPIOD_VERSION_STR=\"" .. version .. "\"")
+                end)
         end
     end
 end
