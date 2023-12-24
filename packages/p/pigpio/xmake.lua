89a1fea6bcf4c52c1f87a798d688281115812c22
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-24 23:51:59 +0000

    pigpio: set to only linux

--- packages/p/pigpio/xmake.lua
+++ packages/p/pigpio/xmake.lua
@@ -9,7 +9,7 @@ package("pigpio")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("linux", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
