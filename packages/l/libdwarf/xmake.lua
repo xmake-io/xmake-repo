ae64831e6dab0a4bf2cd35ae51e81adfbea05bfc
NahidaChan <42824603+KawaiiNahida@users.noreply.github.com>
2023-12-28 18:07:40 +0800

    libdwarf: add version 0.9.0, add msvc patch

--- packages/l/libdwarf/xmake.lua
+++ packages/l/libdwarf/xmake.lua
@@ -6,10 +6,11 @@ package("libdwarf")
 
     add_urls("https://github.com/davea42/libdwarf-code/releases/download/v$(version)/libdwarf-$(version).tar.xz")
     add_versions("0.8.0", "771814a66b5aadacd8381b22d8a03b9e197bd35c202d27e19fb990e9b6d27b17")
+    add_versions("0.9.0", "d3cad80a337276a7581bb90ebcddbd743484a99a959157c066dd30f7535db59b")
 
     add_deps("cmake")
     add_deps("zlib", "zstd")
-    if is_plat("mingw") then
+    if is_plat("mingw") or is_plat("windows") then
         add_links("dwarf")
     end
 
@@ -19,8 +20,14 @@ package("libdwarf")
         table.insert(configs, "-DBUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DBUILD_NON_SHARED=" .. (package:config("shared") and "OFF" or "ON"))
 
-        if package:is_plat("mingw") and not package:config("shared") then
+        if (package:is_plat("mingw") or package:is_plat("windows")) and not package:config("shared") then
             package:add("defines", "LIBDWARF_STATIC=1")
+        end
+
+        if package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_INSTALL_PREFIX='./'")
+            io.replace("src/lib/libdwarf/libdwarf_private.h", "typedef long long off_t;", "#include <sys/types.h>", {plain = true})
+            io.replace("src/lib/libdwarf/libdwarf_private.h", "typedef long off_t;", "#include <sys/types.h>", {plain = true})
         end
 
         io.replace("CMakeLists.txt", "add_subdirectory(src/bin/dwarfdump)", "", {plain = true})
