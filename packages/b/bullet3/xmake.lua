fbf991557e5a6c2279984705547e297762bb1b39
Jérôme Leclercq <lynix680@gmail.com>
2024-01-03 22:39:53 +0100

    Update xmake.lua

--- packages/b/bullet3/xmake.lua
+++ packages/b/bullet3/xmake.lua
@@ -37,7 +37,7 @@ package("bullet3")
         table.insert(configs, "-DUSE_DOUBLE_PRECISION=" .. (package:config("double_precision") and "ON" or "OFF"))
         table.insert(configs, "-DBUILD_EXTRAS=" .. (package:config("extras") and "ON" or "OFF"))
         table.insert(configs, "-DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON") -- setting this to ON prevents Bullet from replacing flags
-        if not package:config("vs_runtime"):endswith("d") then
+        if package:is_plat("windows") and not package:config("vs_runtime"):endswith("d") then
             table.insert(configs, "-DUSE_MSVC_RELEASE_RUNTIME_ALWAYS=ON") -- required to remove _DEBUG from cmake flags
         end
         import("package.tools.cmake").install(package, configs)
