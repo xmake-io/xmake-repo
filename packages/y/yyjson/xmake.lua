bba85858231482ae3e532fec1d69953d0c0ff1f9
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-05 16:18:42 +0000

    yyjson: add version 0.8.0 (#3043)

--- packages/y/yyjson/xmake.lua
+++ packages/y/yyjson/xmake.lua
@@ -5,6 +5,7 @@ package("yyjson")
 
     add_urls("https://github.com/ibireme/yyjson/archive/$(version).tar.gz",
              "https://github.com/ibireme/yyjson.git")
+    add_versions("0.8.0", "b2e39ac4c65f9050820c6779e6f7dd3c0d3fed9c6667f91caec0badbedce00f3")
     add_versions("0.5.1", "b484d40b4e20cc3174a6fdc160d0f20f961417f9cb3f6dc1cf6555fffa8359f3")
     add_versions("0.5.0", "1a65c41d25394c979ad26554a0befb8006ecbf9f7f3a5b0130fdae4f2dd03d42")
     add_versions("0.4.0", "061fe713391d7f3f85f13e8bb2752a4cdeb8e70ce20d68e1e9e4332bd0bf64fa")
