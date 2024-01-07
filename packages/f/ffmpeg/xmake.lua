5e77d9a3ff332feea06525695f1b60fdb974cd0c
Jérôme Leclercq <lynix680@gmail.com>
2024-01-07 14:40:40 +0100

    Add macOS ARM64 CI and fix FFMpeg package for macOS ARM64 (#3052)

--- packages/f/ffmpeg/xmake.lua
+++ packages/f/ffmpeg/xmake.lua
@@ -219,6 +219,11 @@ package("ffmpeg")
             os.vrunv("make", argv)
             os.vrun("make install")
         else
+            if package:is_cross() then
+                table.insert(configs, "--enable-cross-compile")
+                table.insert(configs, "--arch=" .. package:targetarch())
+                configs.host = "" -- prevents xmake to add a --host=xx parameter
+            end
             import("package.tools.autoconf").install(package, configs)
         end
         package:addenv("PATH", "bin")
