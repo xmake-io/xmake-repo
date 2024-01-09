61a35bf08260b9607f2b325c56e154174952ea8e
Jérôme Leclercq <lynix680@gmail.com>
2024-01-09 16:06:27 +0100

    libcurl: Use CMake to build

--- packages/l/libcurl/xmake.lua
+++ packages/l/libcurl/xmake.lua
@@ -1,7 +1,6 @@
 includes(path.join(os.scriptdir(), "versions.lua"))
 
 package("libcurl")
-
     set_homepage("https://curl.haxx.se/")
     set_description("The multiprotocol file transfer library.")
     set_license("MIT")
@@ -12,12 +11,13 @@ package("libcurl")
              {version = function (version) return (version:gsub("%.", "_")) .. "/curl-" .. version end})
     add_versions_list()
 
+    add_deps("cmake")
+
     if is_plat("macosx", "iphoneos") then
         add_frameworks("Security", "CoreFoundation", "SystemConfiguration")
     elseif is_plat("linux") then
         add_syslinks("pthread")
     elseif is_plat("windows", "mingw") then
-        add_deps("cmake")
         add_syslinks("advapi32", "crypt32", "wldap32", "winmm", "ws2_32", "user32")
     end
 
@@ -70,7 +70,7 @@ package("libcurl")
         end
     end)
 
-    on_install("windows", "mingw", function (package)
+    on_install("windows", "mingw", "linux", "macosx", "android", "iphoneos", "cross", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DENABLE_MANUAL=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
@@ -95,6 +95,9 @@ package("libcurl")
         for name, opt in pairs(configopts) do
             table.insert(configs, "-D" .. opt .. "=" .. (package:config(name) and "ON" or "OFF"))
         end
+        if package:is_plat("macosx", "iphoneos") then
+            table.insert(configs, (package:version():ge("7.65") and "-DCURL_USE_SECTRANSP" or "-DCMAKE_USE_DARWINSSL"))
+        end
         if package:is_plat("windows") then
             table.insert(configs, "-DCURL_STATIC_CRT=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
         end
@@ -102,44 +105,6 @@ package("libcurl")
             io.replace("src/CMakeLists.txt", 'COMMAND ${CMAKE_COMMAND} -E echo "/* built-in manual is disabled, blank function */" > tool_hugehelp.c', "", {plain = true})
         end
         import("package.tools.cmake").install(package, configs)
-    end)
-
-    on_install("macosx", "linux", "cross", function (package)
-        local configs = {"--disable-silent-rules",
-                         "--disable-dependency-tracking",
-                         "--without-hyper",
-                         "--without-libgsasl",
-                         "--without-librtmp",
-                         "--without-quiche",
-                         "--without-ngtcp2",
-                         "--without-nghttp3"}
-
-        local version = package:version()
-        if (package:is_plat("mingw") and version:ge("7.85")) then
-            package:add("syslinks", "Bcrypt")
-        end
-
-        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-        if package:debug() then
-            table.insert(configs, "--enable-debug")
-        end
-        if package:is_plat("macosx", "iphoneos") then
-            table.insert(configs, (package:version():ge("7.77") and "--with-secure-transport" or "--with-darwinssl"))
-        end
-        for _, name in ipairs({"openssl", "mbedtls", "zlib", "brotli", "zstd", "libssh2", "libidn2", "libpsl", "nghttp2"}) do
-            table.insert(configs, package:config(name) and "--with-" .. name or "--without-" .. name)
-        end
-        table.insert(configs, package:config("cares") and "--enable-ares" or "--disable-ares")
-        table.insert(configs, package:config("openldap") and "--enable-ldap" or "--disable-ldap")
-        if package:is_plat("macosx") then
-            local cares = package:dep("c-ares")
-            if cares and not cares:config("shared") then
-                -- we need fix missing `-lresolv` when checking c-ares
-                io.replace("./configure", "PKGCONFIG --libs-only-l libcares", "PKGCONFIG --libs-only-l --static libcares", {plain = true})
-            end
-        end
-        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_test(function (package)
