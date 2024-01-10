7cdd50406fe6d04950511d8e6e05ebcd18bf69bb
ruki <waruqi@gmail.com>
2024-01-11 00:37:52 +0800

    get shasum of tags

--- scripts/checkupdate.lua
+++ scripts/checkupdate.lua
@@ -1,5 +1,6 @@
 import("core.base.semver")
 import("net.http")
+import("devel.git")
 import("private.action.require.impl.utils.filter")
 
 function shasum_of(package, url, version)
@@ -28,14 +29,48 @@ function _is_valid_version(version)
 end
 
 function _check_version_from_github_tags(package, url)
-    -- TODO
-    -- return version, shasum
+    local repourl = url:match("https://github%.com/.-/.-/")
+    if repourl then
+        print("checking version from github tags %s ..", repourl)
+        local version_latest
+        local tags = git.tags(repourl)
+        for _, tag in ipairs(tags) do
+            if _is_valid_version(tag) and (not version_latest or semver.compare(tag, version_latest) > 0) then
+                version_latest = tag
+            end
+        end
+        if version_latest then
+            local has_prefix_v = false
+            for _, version in ipairs(package:versions()) do
+                if version:startswith("v") then
+                    has_prefix_v = true
+                end
+                if semver.compare(version, version_latest) >= 0 then
+                    version_latest = nil
+                    break
+                end
+            end
+            if version_latest then
+                if has_prefix_v and not version_latest:startswith("v") then
+                    version_latest = "v" .. version_latest
+                elseif not has_prefix_v and version_latest:startswith("v") then
+                    version_latest = version_latest:sub(2)
+                end
+            end
+        end
+        if version_latest then
+            local shasum = shasum_of(package, url, version_latest)
+            if shasum then
+                return version_latest, shasum
+            end
+        end
+    end
 end
 
 function _check_version_from_github_releases(package, url)
     local repourl = url:match("https://github%.com/.-/.-/")
     if repourl then
-        print("checking version from %s ..", repourl)
+        print("checking version from github releases %s ..", repourl)
         local list = try {function() return os.iorunv("gh", {"release", "list", "--exclude-drafts", "--exclude-pre-releases", "-R", repourl}) end}
         if not list then
             list = os.iorunv("gh", {"release", "list", "-R", repourl})
@@ -82,7 +117,7 @@ end
 function main(package)
     local checkers = {
         ["https://github%.com/.-/.-/archive/refs/tags/.*"] = _check_version_from_github_tags,
-        ["https://github%.com/.-/.-/releases/download/.*"] = _check_version_from_github_releases
+--        ["https://github%.com/.-/.-/releases/download/.*"] = _check_version_from_github_releases
     }
     for _, url in ipairs(package:urls()) do
         for pattern, checker in pairs(checkers) do
