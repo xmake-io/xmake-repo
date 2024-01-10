86d73cba02f310252e01ff5dc6790b70046f8ce8
Jérôme Leclercq <lynix680@gmail.com>
2024-01-10 17:33:44 +0100

    Update xmake.lua

--- packages/s/sentry-native/xmake.lua
+++ packages/s/sentry-native/xmake.lua
@@ -38,12 +38,20 @@ package("sentry-native")
             package:add("defines", "SENTRY_BUILD_STATIC")
         end
 
+        local backend
+        if package:is_plat("linux") then -- linux defaults to breakpad before 0.7 and then defaults to crashpad
+            backend = package:version() and package:version():ge("0.7") and "crashpad" or "breakpad"
+        end
         if package:config("backend") == "crashpad" then
+            backend = "crashpad"
+        elseif package:config("backend") == "breakpad" then
+            backend = "breakpad"
+        end
+
+        if backend == "crashpad" then
             package:add("links", "sentry", "crashpad_client", "crashpad_util", "crashpad_minidump", "crashpad_handler_lib", "mini_chromium", "crashpad_tools", "crashpad_compat", "crashpad_snapshot")
             package:add("deps", "zlib")
-        elseif package:config("backend") == "breakpad" then
-            package:add("links", "sentry", "breakpad_client")
-        elseif package:is_plat("linux") then -- linux defaults to breakpad
+        else
             package:add("links", "sentry", "breakpad_client")
         end
     end)
