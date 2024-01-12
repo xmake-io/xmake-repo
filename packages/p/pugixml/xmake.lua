9361782ee6814732ac2dda70e8ae2fd590067528
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-12 23:32:46 +0000

    pugixml: add v1.14 (#3089)

--- packages/p/pugixml/xmake.lua
+++ packages/p/pugixml/xmake.lua
@@ -8,6 +8,7 @@ package("pugixml")
              "https://github.com/zeux/pugixml.git")
     add_versions("v1.11.4", "017139251c122dbff400a507cddc4cb74120a431a50c6c524f30edcc5b331ade")
     add_versions("v1.13", "5c5ad5d7caeb791420408042a7d88c2c6180781bf218feca259fd9d840a888e1")
+    add_versions("v1.14", "2f10e276870c64b1db6809050a75e11a897a8d7456c4be5c6b2e35a11168a015")
 
     add_deps("cmake")
 
