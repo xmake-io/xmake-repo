68f151ecf65a412a1ecfd2db6dc5f97a9e51d536
star9029 <hengxings783@gmail.com>
2024-01-13 07:45:43 +0800

    bytepack: add package

---
+++ packages/b/bytepack/xmake.lua
@@ -0,0 +1,23 @@
+package("bytepack")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/farukeryilmaz/bytepack")
+    set_description("C++ Binary Serialization Made Easy: Header-only, configurable endianness, cross-platform, no IDL, no exceptions, and no macros")
+    set_license("MIT")
+
+    add_urls("https://github.com/farukeryilmaz/bytepack/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/farukeryilmaz/bytepack.git")
+
+    add_versions("v0.1.0", "7761cf51736d4e1a65ca69323182930967846eaed04adddfd316f59a5c2eb244")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <bytepack/bytepack.hpp>
+            void test() {
+                bytepack::binary_stream serializationStream(64);
+            }
+        ]]}, {configs = {languages = "c++20"}}))
+    end)
