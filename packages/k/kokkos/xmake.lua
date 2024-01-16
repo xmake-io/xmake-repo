4a8a2578c96d450fc2a8f365bb38483c7cc251bd
Jérôme Leclercq <lynix680@gmail.com>
2024-01-16 09:58:49 +0100

    Update xmake.lua

--- packages/k/kokkos/xmake.lua
+++ packages/k/kokkos/xmake.lua
@@ -24,7 +24,7 @@ package("kokkos")
         end
     end)
 
-    on_install("windows|x64", "macosx", "linux", function (package)
+    on_install("windows|x64", "macosx|x86_64", "linux", function (package)
         local configs = {"-DKokkos_ENABLE_SERIAL=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
