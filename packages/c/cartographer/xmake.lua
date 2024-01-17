275479cee7197c755d2acb73ddd8cf8ba60122bd
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-17 07:43:57 +0000

    cartographer: disable shared for windows

--- packages/c/cartographer/xmake.lua
+++ packages/c/cartographer/xmake.lua
@@ -9,7 +9,11 @@ package("cartographer")
     add_versions("1.0.0", "474a410bf6457eb8a0fd92ea412d7889fb013051e625d3ee25e8d65e4113fd6c")
 
     add_patches("1.0.0", path.join(os.scriptdir(), "patches", "1.0.0", "fix-build-error.patch"), "a4bb53d6f098c77a397d72c244d4283af1f9eec8a4ca7a7fa28de77b06d1201e")
-        
+
+    if is_plat("windows") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
     add_deps("cmake")
     add_deps("zlib")
     add_deps("boost", {configs = {iostreams = true}})
