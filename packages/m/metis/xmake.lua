348b49b80d640b125ce562559eae8bbd96766d9d
xq114 <1140735506@qq.com>
2024-01-18 18:39:19 +0800

    fix metis

--- packages/m/metis/xmake.lua
+++ packages/m/metis/xmake.lua
@@ -7,7 +7,7 @@ package("metis")
     add_urls("https://github.com/KarypisLab/METIS/archive/refs/tags/$(version).tar.gz")
     add_versions("v5.2.1", "1a4665b2cd07edc2f734e30d7460afb19c1217c2547c2ac7bf6e1848d50aff7a")
 
-    add_patches("5.2.1", "patches/5.2.1/gklib.patch", "5a8067e15681d4a1fd1da0307effcafdaa491a314558df803457dff21602b566")
+    add_patches("5.2.1", "patches/5.2.1/gklib.patch", "63e5035241f23ee664800ec1811ea8baa69895c71ec007ab1a95103a290c11eb")
     add_resources("5.2.1", "gklib", "https://github.com/KarypisLab/GKlib.git", "8bd6bad750b2b0d90800c632cf18e8ee93ad72d7")
 
     add_configs("long_index", {description = "Use 64-bit uint as index.", default = false, type = "boolean"})
@@ -22,6 +22,9 @@ package("metis")
         io.gsub(path.join("include", "metis.h"), "//#define IDXTYPEWIDTH %d+", "#define IDXTYPEWIDTH " .. idx_width)
         local real_width = package:config("double") and 64 or 32
         io.gsub(path.join("include", "metis.h"), "//#define REALTYPEWIDTH %d+", "#define REALTYPEWIDTH " .. real_width)
+        io.replace(path.join(package:resourcefile("gklib"), "gk_arch.h"), "gk_ms_stdint.h", "stdint.h", {plain = true})
+        io.replace(path.join(package:resourcefile("gklib"), "gk_arch.h"), "gk_ms_inttypes.h", "inttypes.h", {plain = true})
+        io.replace(path.join(package:resourcefile("gklib"), "gk_arch.h"), "gk_ms_stat.h", "sys/stat.h", {plain = true})
 
         local configs = {}
         table.insert(configs, "-DDEBUG=" .. (package:debug() and "ON" or "OFF"))
