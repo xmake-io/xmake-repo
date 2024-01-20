516bab5d62357e0d8b378036579beeed248fde5c
xq114 <1140735506@qq.com>
2024-01-20 18:39:53 +0800

    update ceres-solver

--- packages/c/ceres-solver/xmake.lua
+++ packages/c/ceres-solver/xmake.lua
@@ -2,10 +2,12 @@ package("ceres-solver")
 
     set_homepage("http://ceres-solver.org/")
     set_description("Ceres Solver is an open source C++ library for modeling and solving large, complicated optimization problems.")
+    set_license("BSD-3-Clause")
     
     add_urls("http://ceres-solver.org/ceres-solver-$(version).tar.gz")
     add_versions("2.0.0", "10298a1d75ca884aa0507d1abb0e0f04800a92871cd400d4c361b56a777a7603")
     add_versions("2.1.0", "f7d74eecde0aed75bfc51ec48c91d01fe16a6bf16bce1987a7073286701e2fc6")
+    add_versions("2.2.0", "48b2302a7986ece172898477c3bcd6deb8fb5cf19b3327bc49969aad4cede82d")
 
     add_configs("blas",        {description = "Choose BLAS library to use.", default = "openblas", type = "string", values = {"mkl", "openblas"}})
     add_configs("suitesparse", {description = "Enable SuiteSparse.", default = true, type = "boolean"})
@@ -14,10 +16,11 @@ package("ceres-solver")
     on_load("windows", "linux", "macosx", function (package)
         if package:config("suitesparse") then
             package:add("deps", "suitesparse", {configs = {blas = package:config("blas")}})
+            package:add("deps", "openmp")
         end
     end)
 
-    on_install("windows", "linux", "macosx", function (package)
+    on_install("windows|x64", "linux", "macosx", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DBUILD_DOCUMENTATION=OFF", "-DBUILD_EXAMPLES=OFF", "-DBUILD_BENCHMARKS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
@@ -26,9 +29,13 @@ package("ceres-solver")
         end
         table.insert(configs, "-DSUITESPARSE=" .. (package:config("suitesparse") and "ON" or "OFF"))
         table.insert(configs, "-DCXSPARSE=" .. (package:config("suitesparse") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
+        if package:config("suitesparse") then
+            import("package.tools.cmake").install(package, configs, {packagedeps = {"openmp", "libomp"}})
+        else
+            import("package.tools.cmake").install(package, configs)
+        end
     end)
 
     on_test(function (package)
-        assert(package:has_cxxtypes("ceres::Problem", {configs = {languages = "c++14"}, includes = "ceres/ceres.h"}))
+        assert(package:has_cxxtypes("ceres::Problem", {configs = {languages = "c++17"}, includes = "ceres/ceres.h"}))
     end)
