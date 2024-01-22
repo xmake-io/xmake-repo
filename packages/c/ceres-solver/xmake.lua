32084930b0d0b264b56b23a2f253fdbc7f46cda4
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-22 21:07:51 +0000

    ceres-solver & suitesparse: add support for older version

--- packages/c/ceres-solver/xmake.lua
+++ packages/c/ceres-solver/xmake.lua
@@ -7,7 +7,9 @@ package("ceres-solver")
     add_urls("http://ceres-solver.org/ceres-solver-$(version).tar.gz")
     add_versions("2.0.0", "10298a1d75ca884aa0507d1abb0e0f04800a92871cd400d4c361b56a777a7603")
     add_versions("2.1.0", "f7d74eecde0aed75bfc51ec48c91d01fe16a6bf16bce1987a7073286701e2fc6")
-    add_versions("2.2.0", "48b2302a7986ece172898477c3bcd6deb8fb5cf19b3327bc49969aad4cede82d")
+    if not is_plat("windows") or (is_plat("windows") and is_arch("x64")) then
+        add_versions("2.2.0", "48b2302a7986ece172898477c3bcd6deb8fb5cf19b3327bc49969aad4cede82d")
+    end
 
     add_configs("blas",        {description = "Choose BLAS library to use.", default = "openblas", type = "string", values = {"mkl", "openblas"}})
     add_configs("suitesparse", {description = "Enable SuiteSparse.", default = true, type = "boolean"})
@@ -15,12 +17,16 @@ package("ceres-solver")
     add_deps("cmake", "eigen", "glog", "gflags")
     on_load("windows", "linux", "macosx", function (package)
         if package:config("suitesparse") then
-            package:add("deps", "suitesparse", {configs = {blas = package:config("blas")}})
-            package:add("deps", "openmp")
+            if package:version():ge("2.2.0") then
+                package:add("deps", "suitesparse", {configs = {blas = package:config("blas")}})
+                package:add("deps", "openmp")
+            else
+                package:add("deps", "suitesparse 5.x", {configs = {blas = package:config("blas")}})
+            end
         end
     end)
 
-    on_install("windows|x64", "linux", "macosx", function (package)
+    on_install("windows", "linux", "macosx", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DBUILD_DOCUMENTATION=OFF", "-DBUILD_EXAMPLES=OFF", "-DBUILD_BENCHMARKS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
@@ -29,7 +35,7 @@ package("ceres-solver")
         end
         table.insert(configs, "-DSUITESPARSE=" .. (package:config("suitesparse") and "ON" or "OFF"))
         table.insert(configs, "-DCXSPARSE=" .. (package:config("suitesparse") and "ON" or "OFF"))
-        if package:config("suitesparse") then
+        if package:version():ge("2.2.0") and package:config("suitesparse") then
             import("package.tools.cmake").install(package, configs, {packagedeps = {"openmp", "libomp"}})
         else
             import("package.tools.cmake").install(package, configs)
