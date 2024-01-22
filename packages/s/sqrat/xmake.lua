f8901b85a99877c147c4f212bb9efa1a1b5dcb4b
Jérôme Leclercq <lynix680@gmail.com>
2024-01-22 14:05:43 +0100

    Update xmake.lua

--- packages/s/sqrat/xmake.lua
+++ packages/s/sqrat/xmake.lua
@@ -1,12 +1,15 @@
-    package("sqrat")
+package("sqrat")
     set_kind("library", {headeronly = true})
     set_homepage("http://scrat.sourceforge.net/")
     set_description("Sqrat is a C++ library for Squirrel that facilitates exposing classes and other native functionality to Squirrel scripts.")
     set_license("zlib")
+
     add_urls("git://git.code.sf.net/p/scrat/code")
+
     add_deps("squirrel")
 
     add_includedirs("include", "include/sqrat")
+
     on_install(function (package)
         os.cp("include/*.h", package:installdir("include"))
         os.cp("include/sqrat/*.h", package:installdir("include/sqrat/"))
