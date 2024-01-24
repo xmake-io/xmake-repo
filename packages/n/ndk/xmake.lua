84fff858b794cbb0fb3681f44129d61986c17c9f
zebede22 <132909114+zebede22@users.noreply.github.com>
2024-01-24 11:23:28 +0400

    NDK LTS
    Add latest  NDK LTS:
    https://github.com/android/ndk/wiki

--- packages/n/ndk/xmake.lua
+++ packages/n/ndk/xmake.lua
@@ -4,19 +4,21 @@ package("ndk")
     set_homepage("https://developer.android.com/ndk")
     set_description("Android NDK toolchain.")
 
-    local versions = {["21"] = "r21e", ["22"] = "r22"}
+    local versions = {["21"] = "r21e", ["22"] = "r22", ["26"] = "r26b"}
     if is_host("windows") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-windows-x86_64.zip", {version = function (version)
             return versions[tostring(version:major())]
         end})
         add_versions("21.0", "f71307c5c572e2c163d602b3704b8bc024bec0c43ba2800de36bd10f3a21492b")
         add_versions("22.0", "5a0eafa83c8bba3c76e8427aa3d83d169215f62963a277b1914a3651aa47f751")
+        add_versions("26.0", "a478d43d4a45d0d345cda6be50d79642b92fb175868d9dc0dfc86181d80f691e")
     elseif is_host("linux") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-linux-x86_64.zip", {version = function (version)
             return versions[tostring(version:major())]
         end})
         add_versions("21.0", "ad7ce5467e18d40050dc51b8e7affc3e635c85bd8c59be62de32352328ed467e")
         add_versions("22.0", "d37fc69cd81e5660234a686e20adef39bc0244086e4d66525a40af771c020718")
+        add_versions("26.0", "ad73c0370f0b0a87d1671ed2fd5a9ac9acfd1eb5c43a7fbfbd330f85d19dd632")
     elseif is_host("macosx") then
         set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-darwin-x86_64.zip", {version = function (version)
             return versions[tostring(version:major())]
