791ac9d70c383f12abb59117610556adb5ed4e9f
Jérôme Leclercq <lynix680@gmail.com>
2024-01-24 14:25:10 +0100

    Update xmake.lua

--- packages/n/ndk/xmake.lua
+++ packages/n/ndk/xmake.lua
@@ -20,12 +20,13 @@ package("ndk")
         add_versions("22.0", "d37fc69cd81e5660234a686e20adef39bc0244086e4d66525a40af771c020718")
         add_versions("26.0", "ad73c0370f0b0a87d1671ed2fd5a9ac9acfd1eb5c43a7fbfbd330f85d19dd632")
     elseif is_host("macosx") then
-        local versions = {["21"] = "r21e", ["22"] = "r22"}
-        set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-darwin-x86_64.zip", {version = function (version)
+        local versions = {["21"] = "r21e-darwin-x86_64", ["22"] = "r22-darwin-x86_64", ["26"] = "r26b-darwin"}
+        set_urls("https://dl.google.com/android/repository/android-ndk-$(version)-darwin.zip", {version = function (version)
             return versions[tostring(version:major())]
         end})
         add_versions("21.0", "437278103a3db12632c05b1be5c41bbb8522791a67e415cc54411a65366f499d")
         add_versions("22.0", "14fce4dea7fb3facbc0e3d20270007bffec3ba383aec02e8b0e0dad8d8782892")
+        add_versions("26.0", "4b0ea6148a9a2337e62a0c0c7ac59ff1edc38d69b81d9c58251897d23f7fa321")
     end
 
     on_fetch(function (package, opt)
