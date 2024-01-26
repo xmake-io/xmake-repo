63e3e31ea4a506cd313584f6866c0675fb3ad7b3
ruki <waruqi@gmail.com>
2024-01-26 23:47:08 +0800

    improve test script

--- scripts/test.lua
+++ scripts/test.lua
@@ -78,7 +78,11 @@ function _require_packages(argv, packages)
     end
     local runtimes = argv.runtimes or argv.vs_runtime
     if runtimes then
-        table.insert(config_argv, "--vs_runtime=" .. runtimes)
+        if is_host("windows") then
+            table.insert(config_argv, "--vs_runtime=" .. runtimes)
+        else
+            table.insert(config_argv, "--runtimes=" .. runtimes)
+        end
     end
     if argv.xcode_sdkver then
         table.insert(config_argv, "--xcode_sdkver=" .. argv.xcode_sdkver)
