869bff256a0bb0c90da42494bda29a7924bb9fff
ruki <waruqi@gmail.com>
2024-01-29 00:56:10 +0000

    Update libkmod to v31

--- packages/l/libkmod/xmake.lua
+++ packages/l/libkmod/xmake.lua
@@ -5,6 +5,7 @@ package("libkmod")
 
     add_urls("https://github.com/kmod-project/kmod/archive/refs/tags/$(version).tar.gz",
              "https://github.com/kmod-project/kmod.git")
+    add_versions("v31", "16c40aaa50fc953035b4811b29ce3182f220e95f3c9e5eacb4b07b1abf85f003")
     add_versions("v30", "1fa3974abd80b992d61324bcc04fa65ea96cfe2e9e1150f48394833030c4b583")
 
     on_install("linux", "android", function (package)
