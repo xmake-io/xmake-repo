64b8215bfa24f1b5c104216ef2c902d29a3b4359
Zheng Zeng <zheng1995z@gmail.com>
2024-01-30 01:49:51 -0800

    Remove comment; update sha256.

--- packages/l/linalg/xmake.lua
+++ packages/l/linalg/xmake.lua
@@ -6,7 +6,7 @@ package("linalg")
     add_urls("https://github.com/sgorsten/linalg/archive/refs/tags/$(version).tar.gz",
              "https://github.com/sgorsten/linalg.git")
 
-    add_versions("v2.2", "4460f1f5b85ccc81ffcf49aa450d454db58ca90e")
+    add_versions("v2.2", "28640228988186EDAF7729177BFAB4C91170B303AD489407A4228CEB55A73EC2")
 
     on_install("windows", "linux", "macosx", function (package)
         os.cp("linalg.h", package:installdir("include"))
@@ -15,7 +15,6 @@ package("linalg")
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
         using namespace linalg::aliases;
-        // Compute the coefficients of the equation of a plane containing points a, b, and c
         float4 compute_plane(float3 a, float3 b, float3 c)
         {
             float3 n = cross(b-a, c-a);
