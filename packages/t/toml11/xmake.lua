78ad25d0ab79ecc6a86692bc2a898796fd757583
ruki <waruqi@gmail.com>
2024-02-01 00:58:31 +0000

    Update toml11 to v3.8.1

--- packages/t/toml11/xmake.lua
+++ packages/t/toml11/xmake.lua
@@ -6,6 +6,7 @@ package("toml11")
 
     add_urls("https://github.com/ToruNiina/toml11/archive/refs/tags/$(version).tar.gz",
              "https://github.com/ToruNiina/toml11.git")
+    add_versions("v3.8.1", "6a3d20080ecca5ea42102c078d3415bef80920f6c4ea2258e87572876af77849")
     add_versions("v3.7.0", "a0b6bec77c0e418eea7d270a4437510884f2fe8f61e7ab121729624f04c4b58e")
 
     add_deps("cmake")
