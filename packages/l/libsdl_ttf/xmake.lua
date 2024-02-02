8ffc9ae2bfee57cbcc53cc0734d22710e2ce3e8a
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-02-02 23:08:24 +0100

    add more versions for libsdl* mostly github version

--- packages/l/libsdl_ttf/xmake.lua
+++ packages/l/libsdl_ttf/xmake.lua
@@ -13,17 +13,33 @@ package("libsdl_ttf")
 
     if is_plat("windows", "mingw") then
         add_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).zip",
-                 "https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-$(version).zip")
-        add_versions("2.20.0", "04e94fc5ecac3475ab35c1d5cf52650df691867e7e4befcc861bf982a747111a")
-        add_versions("2.20.1", "18d81ab399c8e39adababe8918691830ba6e0d6448e5baa141ee0ddf87ede2dc")
-        add_versions("2.20.2", "aa6256bfcffd8381a75b3a2a2384ac12109b5b148e72722a19b0ede54c4051dc")
+                 "https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-$(version).zip", { alias = "archive" })
+        add_versions("archive:2.20.0", "04e94fc5ecac3475ab35c1d5cf52650df691867e7e4befcc861bf982a747111a")
+        add_versions("archive:2.20.1", "18d81ab399c8e39adababe8918691830ba6e0d6448e5baa141ee0ddf87ede2dc")
+        add_versions("archive:2.20.2", "aa6256bfcffd8381a75b3a2a2384ac12109b5b148e72722a19b0ede54c4051dc")
+        -- todo: add archive versions with their sha256
     else
         add_urls("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$(version).tar.gz",
-                 "https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-$(version).tar.gz")
-        add_versions("2.20.0", "874680232b72839555a558b48d71666b562e280f379e673b6f0c7445ea3b9b8a")
-        add_versions("2.20.1", "78cdad51f3cc3ada6932b1bb6e914b33798ab970a1e817763f22ddbfd97d0c57")
-        add_versions("2.20.2", "9dc71ed93487521b107a2c4a9ca6bf43fb62f6bddd5c26b055e6b91418a22053")
+                 "https://github.com/libsdl-org/SDL_ttf/releases/download/release-$(version)/SDL2_ttf-$(version).tar.gz", { alias = "archive" })
+        add_versions("archive:2.20.0", "874680232b72839555a558b48d71666b562e280f379e673b6f0c7445ea3b9b8a")
+        add_versions("archive:2.20.1", "78cdad51f3cc3ada6932b1bb6e914b33798ab970a1e817763f22ddbfd97d0c57")
+        add_versions("archive:2.20.2", "9dc71ed93487521b107a2c4a9ca6bf43fb62f6bddd5c26b055e6b91418a22053")
+        -- todo: add archive versions with their sha256
     end
+    add_urls("https://github.com/libsdl-org/SDL_ttf.git", { alias = "github" })
+    add_versions("github:2.0.8",  "release-2.0.8")
+    add_versions("github:2.0.9",  "release-2.0.9")
+    add_versions("github:2.0.10",  "release-2.0.10")
+    add_versions("github:2.0.11",  "release-2.0.11")
+    add_versions("github:2.0.12",  "release-2.0.12")
+    add_versions("github:2.0.13",  "release-2.0.13")
+    add_versions("github:2.0.14",  "release-2.0.14")
+    add_versions("github:2.0.15",  "release-2.0.15")
+    add_versions("github:2.0.18",  "release-2.0.18")
+    add_versions("github:2.20.0",  "release-2.20.0")
+    add_versions("github:2.20.1",  "release-2.20.1")
+    add_versions("github:2.20.2",  "release-2.20.2")
+    add_versions("github:2.22.0",  "release-2.22.0")
 
     add_patches(">=2.20.0 <=2.20.1", path.join(os.scriptdir(), "patches", "2.20.1", "cmakelists.patch"), "fe04ada62d9ed70029c0efb3c04bfec22fc7596bd6b73a567beb964e61ebd82c")
 
