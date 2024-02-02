aa2d6d782c0e5f3c1cf755d1c78f932b0c5b4023
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2024-02-02 16:57:37 +0800

    Set libpq to version 16.1

--- packages/l/libpq/xmake.lua
+++ packages/l/libpq/xmake.lua
@@ -1,11 +1,12 @@
 package("libpq")
-    set_homepage("https://www.postgresql.org/docs/14/libpq.html")
+    set_homepage("https://www.postgresql.org/docs/16/libpq.html")
     set_description("Postgres C API library")
     set_license("PostgreSQL")
 
     add_urls("https://github.com/postgres/postgres/archive/refs/tags/REL_$(version).tar.gz", {alias = "github", version = function (version)
         return version:gsub("%.", "_")
     end})
+    add_versions("16.1", "58250fee449f5fd0b5de5c1b0205e8aa8c9a4e3a8cead64ef8a3ca3c9fc2c6e9")
     add_versions("14.1", "14809c9f669851ab89b344a50219e85b77f3e93d9df9e255b9781d8d60fcfbc9")
 
     add_deps("krb5", "openssl", "zlib")
