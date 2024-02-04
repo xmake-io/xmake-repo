07c1126d49612b88bc2732612fdacaef31ab146a
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-02-04 16:47:10 +0100

    add inbetween versions for libsdl_image add 2.22.0 for libsdl_ttf

--- packages/l/libsdl_image/xmake.lua
+++ packages/l/libsdl_image/xmake.lua
@@ -16,6 +16,9 @@ package("libsdl_image")
     add_versions("2.6.0", "2252cdfd5be73cefaf727edc39c2ef3b7682e797acbd3126df117e925d46aaf6")
     add_versions("2.6.1", "cbfea63a46715c63a1db9e41617e550749a95ffd33ef9bd5ba6e58b2bdca6ed3")
     add_versions("2.6.2", "efe3c229853d0d40c35e5a34c3f532d5d9728f0abc623bc62c962bcef8754205")
+    add_versions("2.6.3", "b448a8ca5b7927d9bd1577d393f4d6c59581f87ee525652a27e699941db37b7c")
+    add_versions("2.8.0", "fed33c3fe9f8d38ab4460bdd100c4495be40f8afdac1d44bfcd2b0259b74a123")
+    add_versions("2.8.1", "0c5afef0ac4bc951a46c6790e576c9b3e7ed2c5ab1d2bbfa5e7e9300718f67d2")
     add_versions("2.8.2", "2196ad6665b68fc453a659e172d67fbf18d548277aa07344dfd2deed9d9b84bd")
 
     if is_plat("macosx", "iphoneos") then
