0bd6710b90fbf908674c9a57afc9ab350e3e7378
ruki <waruqi@gmail.com>
2024-02-05 23:04:53 +0800

    add mirror

--- packages/c/cosmocc/xmake.lua
+++ packages/c/cosmocc/xmake.lua
@@ -3,7 +3,8 @@ package("cosmocc")
     set_homepage("https://github.com/jart/cosmopolitan")
     set_description("build-once run-anywhere c library")
 
-    add_urls("https://cosmo.zip/pub/cosmocc/cosmocc-$(version).zip")
+    add_urls("https://cosmo.zip/pub/cosmocc/cosmocc-$(version).zip",
+             "https://github.com/xmake-mirror/cosmopolitan/releases/download/$(version)/cosmocc-$(version).zip")
     add_versions("3.2.4", "d2fa6dbf6f987310494581deff5b915dbdc5ca701f20f7613bb0dcf1de2ee511")
 
     on_install("macosx", "windows", "linux", function (package)
