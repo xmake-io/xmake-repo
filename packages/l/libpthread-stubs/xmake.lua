7b4aeb5ae6ceb94e79e584e906ff7df673f02106
xq114 <1140735506@qq.com>
2024-02-07 18:25:56 +0800

    update libpthread-stubs

--- packages/l/libpthread-stubs/xmake.lua
+++ packages/l/libpthread-stubs/xmake.lua
@@ -3,8 +3,10 @@ package("libpthread-stubs")
     set_homepage("https://www.x.org/")
     set_description("X.Org: pthread-stubs.pc")
 
-    set_urls("https://xcb.freedesktop.org/dist/libpthread-stubs-$(version).tar.bz2")
-    add_versions("0.4", "e4d05911a3165d3b18321cc067fdd2f023f06436e391c6a28dff618a78d2e733")
+    add_urls("https://xcb.freedesktop.org/dist/libpthread-stubs-$(version).tar.gz",
+             "https://www.x.org/archive/individual/lib/libpthread-stubs-$(version).tar.gz")
+    add_versions("0.4", "50d5686b79019ccea08bcbd7b02fe5a40634abcfd4146b6e75c6420cc170e9d9")
+    add_versions("0.5", "593196cc746173d1e25cb54a93a87fd749952df68699aab7e02c085530e87747")
 
     add_deps("pkg-config")
 
