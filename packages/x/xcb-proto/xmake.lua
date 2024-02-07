dbe9da1e150921d864def9da443d31c3073ec1d5
xq114 <1140735506@qq.com>
2024-02-07 18:26:14 +0800

    update xcb-proto

--- packages/x/xcb-proto/xmake.lua
+++ packages/x/xcb-proto/xmake.lua
@@ -3,14 +3,15 @@ package("xcb-proto")
     set_homepage("https://www.x.org/")
     set_description("X.Org: XML-XCB protocol descriptions for libxcb code generation")
 
-    set_urls("https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$(version).tar.gz",
+    add_urls("https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$(version).tar.gz",
              "https://xcb.freedesktop.org/dist/xcb-proto-$(version).tar.gz")
     add_versions("1.13", "0698e8f596e4c0dbad71d3dc754d95eb0edbb42df5464e0f782621216fa33ba7")
     add_versions("1.14", "1c3fa23d091fb5e4f1e9bf145a902161cec00d260fabf880a7a248b02ab27031")
     add_versions("1.14.1", "85cd21e9d9fbc341d0dbf11eace98d55d7db89fda724b0e598855fcddf0944fd")
+    add_versions("1.16.0", "d9c7f010b1105fc3858bf07b5169b2dd8e7493c6652b1fe45f3321d874f291d7")
 
     if is_plat("macosx", "linux") then
-        add_deps("pkg-config", "python 3.x")
+        add_deps("pkg-config", "python 3.x", {kind = "binary"})
     end
 
     on_install("macosx", "linux", function (package)
@@ -19,6 +20,9 @@ package("xcb-proto")
                          "--disable-silent-rules",
                          "PYTHON=python3"}
         import("package.tools.autoconf").install(package, configs)
+        local version = package:dep("python"):version()
+        local pyver = ("python%d.%d"):format(version:major(), version:minor())
+        package:addenv("PYTHONPATH", path.join(package:installdir("lib"), pyver, "site-packages"))
     end)
 
     on_test(function (package)
