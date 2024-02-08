efefb761e20b46b8f2c3fee8f02e71efe5663c26
xq114 <1140735506@qq.com>
2024-02-08 20:11:19 +0800

    update genie

--- packages/g/genie/xmake.lua
+++ packages/g/genie/xmake.lua
@@ -3,22 +3,16 @@ package("genie")
     set_kind("binary")
     set_homepage("https://github.com/bkaradzic/GENie")
     set_description("GENie - Project generator tool")
+    set_license("BSD-3-Clause")
 
-    if is_host("windows") then
-        add_urls("https://raw.githubusercontent.com/bkaradzic/bx/aa5090bbd8c39e84d483f4850a7d9ca7ff9241ed/tools/bin/windows/genie.exe")
-        add_versions("1160.0", "a1226a0098f6ff4895a423df17737af90cc0af9effce7cc81fe904c9b2e02e8f")
-    else
-        add_urls("https://github.com/bkaradzic/GENie.git")
-        add_versions("1160.0", "0f5133b66d103efbcdf7c0d23f6cb6b353282e38")
-    end
+    add_urls("https://github.com/bkaradzic/GENie.git")
+    add_versions("1160", "f9bd455a8439dbcb807816c0be9e4aedf5991bc3")
+    add_versions("1165", "5461c5ab94d0365c40031fef8f459b9c21b4d37a")
+    add_versions("1170", "22cc907a4351db46c55f73e6aa901f1b2f0c52ad")
 
-    on_install("@windows", function (package)
-        os.cp(package:originfile(), package:installdir("bin"))
-    end)
-
-    on_install("@macosx", "@linux", function (package)
-        import("package.tools.make").build(package, configs)
-        os.cp("bin/*/genie", package:installdir("bin"))
+    on_install("@windows", "@msys", "@macosx", "@linux", function (package)
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, {mode = "release"})
     end)
 
     on_test(function (package)
