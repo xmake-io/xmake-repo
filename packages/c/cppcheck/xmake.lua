957cadadb8c75709f42c8c156020a68171f92f98
A2va <49582555+A2va@users.noreply.github.com>
2024-02-09 16:24:05 +0100

    Add tests and cmake dep

--- packages/c/cppcheck/xmake.lua
+++ packages/c/cppcheck/xmake.lua
@@ -1,10 +1,13 @@
 package("cppcheck")
-    set_kind("binary")
-
     set_homepage("https://cppcheck.sourceforge.io/")
     add_urls("https://github.com/danmar/cppcheck/archive/refs/tags/$(version).tar.gz")
     add_versions("2.13.0", "8229afe1dddc3ed893248b8a723b428dc221ea014fbc76e6289840857c03d450")
 
+    add_deps("cmake")
     on_install(function (package)
         import("package.tools.cmake").install(package, {})
+    end)
+
+    on_test(function (package) 
+        os.vrun("cppcheck --version")
     end)
