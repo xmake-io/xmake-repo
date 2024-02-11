8cc5336e0fc9e9f2628c28aa74ec04008987cfee
xq114 <1140735506@qq.com>
2024-02-11 14:54:55 +0800

    revert xtrans

--- packages/x/xtrans/xmake.lua
+++ packages/x/xtrans/xmake.lua
@@ -5,7 +5,7 @@ package("xtrans")
 
     set_urls("https://www.x.org/archive/individual/lib/xtrans-$(version).tar.gz")
     add_versions("1.4.0", "48ed850ce772fef1b44ca23639b0a57e38884045ed2cbb18ab137ef33ec713f9")
-    add_versions("1.5.0", "a806f8a92f879dcd0146f3f1153fdffe845f2fc0df9b1a26c19312b7b0a29c86")
+    -- add_versions("1.5.0", "a806f8a92f879dcd0146f3f1153fdffe845f2fc0df9b1a26c19312b7b0a29c86")
 
     if is_plat("linux") then
         add_extsources("apt::xtrans-dev", "pacman::xtrans")
