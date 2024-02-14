0ee057d7890673d8f62127643dfac7db2eccb930
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 21:52:12 +0000

    pseudo-double-c: remove add_patches

--- packages/p/pseudo-double-c/xmake.lua
+++ packages/p/pseudo-double-c/xmake.lua
@@ -7,8 +7,6 @@ package("pseudo-double-c")
 
     add_versions("2024.01.17", "275b244eee40b987a209927d7942d4bf83d91c95")
 
-    add_patches("2024.01.17", path.join(os.scriptdir(), "patches", "2024.01.17", "fix_build.patch"), "47d9e5b354311ec8004ea8281f18ecdd1641bfbf8668e71c046724c80d95d6f1")
-
     add_configs("pseudo_double_exp_bits", {description = "This sets the number of bits in the exponent, defaulting to 16 if not set.", default = "16", type = "string", values = {"8", "16", "32"}})
     add_configs("pd_error_check", {description = "This enables error checking in the library, defaulting to true if not set.", default = true, type = "boolean"})
 
