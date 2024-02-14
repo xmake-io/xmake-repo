80fb35e69550ffceae21983a7a56001bedc17388
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 20:46:46 +0000

    pseudo-double: add limits

--- packages/p/pseudo-double/xmake.lua
+++ packages/p/pseudo-double/xmake.lua
@@ -7,26 +7,23 @@ package("pseudo-double")
 
     add_versions("2024.01.17", "275b244eee40b987a209927d7942d4bf83d91c95")
 
-    add_patches("2024.01.17", path.join(os.scriptdir(), "patches", "2024.01.17", "fix_build.patch"), "0fa2351c6c2ecc8fbd7e0037c8f1708a3f49153c1ddbb618d302ae256ff32652")
+    add_patches("2024.01.17", path.join(os.scriptdir(), "patches", "2024.01.17", "fix_build.patch"), "47d9e5b354311ec8004ea8281f18ecdd1641bfbf8668e71c046724c80d95d6f1")
 
     add_configs("pseudo_double_exp_bits", {description = "This sets the number of bits in the exponent, defaulting to 16 if not set.", default = "16", type = "string", values = {"8", "16", "32"}})
     add_configs("pd_error_check", {description = "This enables error checking in the library, defaulting to true if not set.", default = true, type = "boolean"})
 
-    on_install(--[["windows|x64", "linux|x86_64", "bsd", "android|arm64*", "cross", ]]function (package)
+    on_install("windows|x64", "linux|x86_64", "bsd", "android|arm64*", "cross", function (package)
         local configs = {}
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
             target("pseudo-double")
                 set_kind("$(kind)")
+                set_languages("c++11")
 
                 if is_plat("windows") then
                     add_defines("_MSC_VER")
                 end
-                if is_plat("macosx") then
-                    add_defines("__APPLE__")
-                end
 
-                set_languages("c++11")
                 add_files("pseudo_double.c", "pseudo_double.cpp")
                 add_headerfiles("(pseudo_double.h)", "(PseudoDouble.h)")
                 
