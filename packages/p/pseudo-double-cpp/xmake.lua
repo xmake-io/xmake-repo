794d2d4ae397f9395be39833026ecbf277f34297
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 21:48:52 +0000

    pseudo-double: separate into 2 packages

--- packages/p/pseudo-double-cpp/xmake.lua
+++ packages/p/pseudo-double-cpp/xmake.lua
@@ -7,29 +7,30 @@ package("pseudo-double")
 
     add_versions("2024.01.17", "275b244eee40b987a209927d7942d4bf83d91c95")
 
-    add_patches("2024.01.17", path.join(os.scriptdir(), "patches", "2024.01.17", "fix_build.patch"), "47d9e5b354311ec8004ea8281f18ecdd1641bfbf8668e71c046724c80d95d6f1")
-
     add_configs("pseudo_double_exp_bits", {description = "This sets the number of bits in the exponent, defaulting to 16 if not set.", default = "16", type = "string", values = {"8", "16", "32"}})
     add_configs("pd_error_check", {description = "This enables error checking in the library, defaulting to true if not set.", default = true, type = "boolean"})
 
-    if is_plat("linux") then
-        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-    end
-    
-    on_install("windows|x64", "linux|x86_64", "bsd", "android|arm64*", "cross", function (package)
+    on_load(function (package)
+        package:add("deps", "pseudo-double-c", {configs = {shared = package:config("shared"), pseudo_double_exp_bits = package:config("pseudo_double_exp_bits"), pd_error_check = package:config("pd_error_check")}})
+    end)
+
+    on_install(--[["windows|x64", "linux|x86_64", "bsd", "android|arm64*", "cross", ]]function (package)
         local configs = {}
+        io.replace("PseudoDouble.h", "#include <stdexcept>", "#include <stdexcept>\n#include <string>", {plain = true})
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
-            target("pseudo-double")
+            add_requires("pseudo-double-c")
+            target("pseudo-double-cpp")
                 set_kind("$(kind)")
                 set_languages("c++11")
+                add_packages("pseudo-double-c")
 
                 if is_plat("windows") then
                     add_defines("_MSC_VER")
                 end
 
-                add_files("pseudo_double.c", "pseudo_double.cpp")
-                add_headerfiles("(pseudo_double.h)", "(PseudoDouble.h)")
+                add_files("pseudo_double.cpp")
+                add_headerfiles("(PseudoDouble.h)")
                 
                 on_config(function (target)
                     if target:has_tool("gcc", "gxx") then
@@ -46,10 +47,8 @@ package("pseudo-double")
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <pseudo_double.h>
             #include <PseudoDouble.h>
             void test() {
-                pseudo_double a_1 = int64fixed10_to_pd(3, -1);
                 PseudoDouble a_2 = PD_create_fixed10(3,-1);
             }
         ]]}))
