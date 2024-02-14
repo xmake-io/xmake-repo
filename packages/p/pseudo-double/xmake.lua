5cf89d19dc0e6b12fefe17900c446600936aa7fd
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 13:42:58 +0000

    pseudo-double: add configs and include stdbool

--- packages/p/pseudo-double/xmake.lua
+++ packages/p/pseudo-double/xmake.lua
@@ -6,16 +6,23 @@ package("pseudo-double")
     add_urls("https://github.com/royward/pseudo-double.git")
     add_versions("2024.01.17", "275b244eee40b987a209927d7942d4bf83d91c95")
 
+    add_configs("pseudo_double_exp_bits", {description = "This sets the number of bits in the exponent, defaulting to 16 if not set.", default = "16", type = "string", values = {"8", "16", "32"}})
+    add_configs("pd_error_check", {description = "This enables error checking in the library, defaulting to 0 if not set.", default = true, type = "boolean"})
+
+
     on_install(function (package)
         local configs = {}
+        io.replace("pseudo_double.h", "#include <stdint.h>", "#include <stdint.h>\n#include <stdbool.h>", {plain = true})
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
             target("pseudo-double")
                 set_kind("$(kind)")
-                set_languages("cxx11")
+                set_languages("c99", "cxx11")
                 add_files("pseudo_double.c", "pseudo_double.cpp")
                 add_headerfiles("(pseudo_double.h)", "(PseudoDouble.h)")
         ]])
+        package:add("defines", "PSEUDO_DOUBLE_EXP_BITS=" .. package:config("pseudo_double_exp_bits"))
+        package:add("defines", "PD_ERROR_CHECK=" .. package:config("pd_error_check") and "1" or "0")
         import("package.tools.xmake").install(package, configs)
     end)
 
