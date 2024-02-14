f9e8454f0467ca6a861685e48392a451e8676969
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 15:24:00 +0000

    pseudo-double: remove empty configs

--- packages/p/pseudo-double/xmake.lua
+++ packages/p/pseudo-double/xmake.lua
@@ -23,7 +23,7 @@ package("pseudo-double")
         ]])
         package:add("defines", "PSEUDO_DOUBLE_EXP_BITS=" .. package:config("pseudo_double_exp_bits"))
         package:add("defines", "PD_ERROR_CHECK=" .. (package:config("pd_error_check") and "1" or "0"))
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
