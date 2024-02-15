70ae4df82f3c565b4056d149783a82cc72fea185
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-15 12:41:55 +0000

    pseudo-double-cpp: change shared to kind

--- packages/p/pseudo-double-cpp/port/xmake.lua
+++ packages/p/pseudo-double-cpp/port/xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release")
 option("pseudo_double_exp_bits", {default = "16", showmenu = true, description = "This sets the number of bits in the exponent, defaulting to 16 if not set."})
 option("pd_error_check", {default = true, showmenu = true, description = "This enables error checking in the library, defaulting to true if not set."})
 
-add_requires("pseudo-double-c", {configs = {shared = get_config("kind"), pseudo_double_exp_bits = get_config("pseudo_double_exp_bits"), pd_error_check = get_config("pd_error_check")}})
+add_requires("pseudo-double-c", {configs = {kind = get_config("kind"), pseudo_double_exp_bits = get_config("pseudo_double_exp_bits"), pd_error_check = get_config("pd_error_check")}})
 
 target("pseudo-double-cpp")
     set_kind("$(kind)")
