89e0e84ced89c88ad621f354d061f4ba420f7180
Latias94 <superfrankie621@gmail.com>
2024-02-17 16:35:45 +0800

    add cpptrace define

--- packages/c/cpptrace/xmake.lua
+++ packages/c/cpptrace/xmake.lua
@@ -21,15 +21,14 @@ package("cpptrace")
         add_syslinks("dbghelp")
     end
 
-    if package:config("static") then
-        add_defines("CPPTRACE_STATIC_DEFINE", {public = true})
-    end
-
     on_install("linux", "macosx", "windows", "mingw", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         package:add("links", "cpptrace")
+        if not package:config("shared") then
+            package:add("defines", "CPPTRACE_STATIC_DEFINE")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
