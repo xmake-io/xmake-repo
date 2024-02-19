6b59c83120275517a98869a4fd9dbcf67db718b7
Jérôme Leclercq <lynix680@gmail.com>
2024-02-19 10:16:55 +0100

    Add 7.9.0

--- packages/l/libpqxx/xmake.lua
+++ packages/l/libpqxx/xmake.lua
@@ -4,6 +4,7 @@ package("libpqxx")
 
     add_urls("https://github.com/jtv/libpqxx/archive/refs/tags/$(version).tar.gz",
              "https://github.com/jtv/libpqxx.git")
+    add_versions("7.9.0", "a1fafd5f6455f6c66241fca1f35f5cb603251580b99f9a0cf1b5d0a586006f16")
     add_versions("7.8.1", "0f4c0762de45a415c9fd7357ce508666fa88b9a4a463f5fb76c235bc80dd6a84")
     add_versions("7.7.0", "2d99de960aa3016915bc69326b369fcee04425e57fbe9dad48dd3fa6203879fb")
 
