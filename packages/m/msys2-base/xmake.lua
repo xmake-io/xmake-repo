5f1460e309eb18b27547753b54efea7a76320a1e
ruki <waruqi@gmail.com>
2024-02-21 23:16:19 +0800

    seperate msys2

---
+++ packages/m/msys2-base/xmake.lua
@@ -0,0 +1,25 @@
+package("msys2-base")
+    set_kind("toolchain")
+    set_homepage("https://www.msys2.org/")
+    set_description("Software Distribution and Building Platform for Windows")
+
+    add_urls("https://github.com/msys2/msys2-installer/releases/download/$(version).tar.xz", {version = function (version)
+            return version:gsub("%.", "-")  .. "/msys2-base-x86_64-" .. version:gsub("%.", "")
+        end})
+    add_versions("2024.01.13", "04456a44a956d3c0b5f9b6c754918bf3a8c3d87c858be7a0c94c9171ab13c58c")
+
+    on_install("@windows|x64", function (package)
+        -- reduce time required to install packages by disabling pacman's disk space checking
+        io.gsub("etc/pacman.conf", "^CheckSpace", "#CheckSpace")
+
+        os.cp("*", package:installdir())
+        package:addenv("PATH", "usr/bin")
+    end)
+
+    on_test(function (package)
+        os.vrun("sh --version")
+        os.vrun("perl --version")
+        os.vrun("ls -l")
+        os.vrun("grep --version")
+        os.vrun("uname -a")
+    end)
--- packages/m/msys2/xmake.lua
+++ packages/m/msys2/xmake.lua
@@ -3,20 +3,11 @@ package("msys2")
     set_homepage("https://www.msys2.org/")
     set_description("Software Distribution and Building Platform for Windows")
 
-    add_urls("https://github.com/msys2/msys2-installer/releases/download/$(version).tar.xz", {version = function (version)
-            return version:gsub("%.", "-")  .. "/msys2-base-x86_64-" .. version:gsub("%.", "")
-        end})
-    add_versions("2024.01.13", "04456a44a956d3c0b5f9b6c754918bf3a8c3d87c858be7a0c94c9171ab13c58c")
+    add_deps("msys2-base")
 
     add_configs("msystem", {description = "Set msys2 system.", type = "string", values = {"MSYS", "MINGW32", "MINGW64", "UCRT64", "CLANG32", "CLANG64", "CLANGARM64"}})
 
     on_install("@windows|x64", function (package)
-        -- reduce time required to install packages by disabling pacman's disk space checking
-        io.gsub("etc/pacman.conf", "^CheckSpace", "#CheckSpace")
-
-        os.cp("*", package:installdir())
-        package:addenv("PATH", "usr/bin")
-
         local msystem = package:config("msystem")
         if msystem then
             package:addenv("MSYSTEM", msystem)
