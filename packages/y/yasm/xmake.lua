975e0fd1f70f2fb6d3a9888985ad35f9e49047eb
Jérôme Leclercq <lynix680@gmail.com>
2024-02-23 09:32:58 +0100

    fix yasm for Windows

--- packages/y/yasm/xmake.lua
+++ packages/y/yasm/xmake.lua
@@ -1,18 +1,17 @@
 package("yasm")
-
     set_kind("binary")
     set_homepage("https://yasm.tortall.net/")
     set_description("Modular BSD reimplementation of NASM.")
 
     if is_host("windows") then
         if os.arch() == "x64" then
-            add_urls("https://github.com/yasm/yasm/releases/download/$(version)/vsyasm-$(version)-win64.zip",
+            add_urls("https://github.com/yasm/yasm/releases/download/v$(version)/vsyasm-$(version)-win64.zip",
                      "http://www.tortall.net/projects/yasm/releases/vsyasm-$(version)-win64.zip")
-            add_versions("1.3.0", "6D991CA77E3827AADE0091C87C89CB4C9FA6AD097AFCEA95EA736482BAE707E2")
+            add_versions("1.3.0", "6d991ca77e3827aade0091c87c89cb4c9fa6ad097afcea95ea736482bae707e2")
         else
-            add_urls("https://github.com/yasm/yasm/releases/download/$(version)/vsyasm-$(version)-win32.zip",
+            add_urls("https://github.com/yasm/yasm/releases/download/v$(version)/vsyasm-$(version)-win32.zip",
                      "http://www.tortall.net/projects/yasm/releases/vsyasm-$(version)-win32.zip")
-            add_versions("1.3.0", "FF4585E2A03E7015B0B1D406D4231267C2D3733968FFC6FC633E586C85C16DA5")
+            add_versions("1.3.0", "ff4585e2a03e7015b0b1d406d4231267c2d3733968ffc6fc633e586c85c16da5")
         end
     else
         add_urls("https://www.tortall.net/projects/yasm/releases/yasm-$(version).tar.gz",
