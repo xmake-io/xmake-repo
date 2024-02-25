52c6f27790c965c3619e202444f9cbac4c48118b
Ren Tatsumoto <tatsu@autistici.org>
2024-02-25 20:50:45 +0300

    add homepage

--- packages/m/mecab/xmake.lua
+++ packages/m/mecab/xmake.lua
@@ -1,5 +1,6 @@
 package("mecab")
     set_description("Yet another part-of-speech and morphological analyzer.")
+    set_homepage("https://taku910.github.io/mecab/")
     set_license("GPL-2.0")
     add_urls("https://github.com/taku910/mecab/archive/05481e751dd5aa536a2bace46715ce54568b972a.zip")
     add_versions("0.996", "d5d3ec4954e969ea93f4f1d778d7dc3ae98848056ee1c8cb99fced8578fd73f3")
