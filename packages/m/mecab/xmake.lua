fae3d64e9f151c2973be5831b1e20ca3ff2bdd18
Ren Tatsumoto <tatsu@autistici.org>
2024-02-25 20:43:55 +0300

    add mecab to the repo

---
+++ packages/m/mecab/xmake.lua
@@ -0,0 +1,17 @@
+package("mecab")
+    set_description("Mecab.")
+    set_license("GPL-2.0")
+    add_urls("https://github.com/taku910/mecab/archive/05481e751dd5aa536a2bace46715ce54568b972a.zip")
+    add_versions("0.996", "d5d3ec4954e969ea93f4f1d778d7dc3ae98848056ee1c8cb99fced8578fd73f3")
+    add_configs("cxx17", {default = true, type = "boolean"})
+    if is_plat("macosx", "linux") then
+        add_deps("make", "autoconf")
+    end
+    if is_plat("linux") then
+        add_extsources("pacman::mecab-git", "apt::mecab")
+    end
+    on_install("macosx", "linux", function (package)
+        os.cd("mecab")
+        import("package.tools.autoconf").install(package, {"--with-charset=utf-8"})
+    end)
+package_end()
