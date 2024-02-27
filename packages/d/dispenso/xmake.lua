fb485760a1a20ffc6097af7ea8612f6b1b0635c2
c8ef <c8ef@outlook.com>
2024-02-27 09:19:41 +0800

    update dispenso to v1.2.0

--- packages/d/dispenso/xmake.lua
+++ packages/d/dispenso/xmake.lua
@@ -7,6 +7,7 @@ package("dispenso")
              "https://github.com/facebookincubator/dispenso.git")
 
     add_versions("v1.1.0", "581f95c16cd479692bc89448d0648f6ce24162454308c544c4d35bf5e9efe5c8")
+    add_versions("v1.2.0", "a44d9cf2f9234f5cbdbe4050fd26e63f3266b64955731651adf04dbb7f6b31df")
 
     if is_plat("linux", "bsd") then
         add_syslinks("pthread")
