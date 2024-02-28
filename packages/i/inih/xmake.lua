067334ff6a293b22a934adcc2c3203220f55ff84
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-28 14:58:30 +0000

    inih: Make tests smaller and remove 'r' from version

--- packages/i/inih/xmake.lua
+++ packages/i/inih/xmake.lua
@@ -2,10 +2,10 @@ package("inih")
     set_homepage("https://github.com/benhoyt/inih")
     set_description("Simple .INI file parser in C, good for embedded systems")
 
-    add_urls("https://github.com/benhoyt/inih/archive/refs/tags/$(version).tar.gz",
+    add_urls("https://github.com/benhoyt/inih/archive/refs/tags/r$(version).tar.gz",
              "https://github.com/benhoyt/inih.git")
 
-    add_versions("r58", "e79216260d5dffe809bda840be48ab0eec7737b2bb9f02d2275c1b46344ea7b7")
+    add_versions("58", "e79216260d5dffe809bda840be48ab0eec7737b2bb9f02d2275c1b46344ea7b7")
 
     add_configs("ini_parser", {description = "compile and (if selected) install INIReader", default = true, type = "boolean"})
     add_configs("heap", {description = "allocate memory on the heap using malloc instead using a fixed-sized line buffer on the stack", default = false, type = "boolean"})
@@ -24,47 +24,18 @@ package("inih")
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <stdio.h>
-            #include <stdlib.h>
-            #include <string.h>
             #include "ini.h"
 
-            typedef struct
-            {
-                int version;
-                const char* name;
-                const char* email;
-            } configuration;
+            typedef struct { } configuration;
 
-            static int handler(void* user, const char* section, const char* name, const char* value)
-            {
-                configuration* pconfig = (configuration*)user;
-                #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-                if (MATCH("protocol", "version")) {
-                    pconfig->version = atoi(value);
-                } else if (MATCH("user", "name")) {
-                    pconfig->name = strdup(value);
-                } else if (MATCH("user", "email")) {
-                    pconfig->email = strdup(value);
-                } else {
-                    return 0;
-                }
-                return 1;
-            }
+            static int handler(void* user, const char* section, const char* name, const char* value) { return 1; }
 
             int test(int argc, char* argv[])
             {
                 configuration config;
-                config.version = 0;
-                config.name = NULL;
-                config.email = NULL;
                 if (ini_parse("test.ini", handler, &config) < 0) {
-                    printf("Can't load 'test.ini'\n");
                     return 1;
                 }
-                int version = config.version;
-                const char* name = config.name;
-                const char* email = config.email;
                 return 0;
             }
         ]]}, {configs = {languages = "cxx11"}}))
@@ -77,13 +48,6 @@ package("inih")
                 int test()
                 {
                     INIReader reader("test.ini");
-                    if (reader.ParseError() < 0) {
-                        std::cout << "Can't load 'test.ini'\n";
-                        return 1;
-                    }
-                    int version = reader.GetInteger("protocol", "version", -1);
-                    std::string name = reader.Get("user", "name", "UNKNOWN");
-                    std::string email = reader.Get("user", "email", "UNKNOWN");
                     return 0;
                 }
             ]]}, {configs = {languages = "cxx11"}}))
