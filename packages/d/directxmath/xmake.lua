34d3f0099e97ad3cbbe7eecf93a71715ed49208e
c8ef <c8ef@outlook.com>
2024-02-28 10:39:51 +0800

    final

--- packages/d/directxmath/xmake.lua
+++ packages/d/directxmath/xmake.lua
@@ -14,10 +14,17 @@ package("directxmath")
     add_versions("2022.12", "2ed0ae7d7fe5d11ad11f6d3d9b31ce686024a551cf82ade723de86aa7b4b57e1")
     add_versions("2024.02", "214d71420107249dfb4bbc37a573f288b0951cc9ffe323dbf662101f3df4d766")
 
+    if is_plat("linux") then
+        add_resources(">=2022.12", "headers", "https://raw.githubusercontent.com/dotnet/runtime/2201016c1e13bdb9abf49e2e38cadf4ee0568df2/src/coreclr/pal/inc/rt/sal.h", "7dae281adc3a09a691291fb90526f05e4f9ef8b16d7f33d716ba690f7241a492")
+    end
+
     add_deps("cmake")
     add_includedirs("include/directxmath")
 
     on_install("windows", "mingw", "linux", function (package)
+        if package:is_plat("linux") then
+            os.cp("../resources/headers/sal.h", package:installdir("include", "directxmath"))
+        end
         import("package.tools.cmake").install(package, {"-DBUILD_TESTING=OFF"})
     end)
 
