1cb9dffc8846bb171314a3a5ff05b277e50b0c89
Ren Tatsumoto <tatsu@autistici.org>
2024-02-29 03:30:12 +0300

    remove unused config

--- packages/m/mecab/xmake.lua
+++ packages/m/mecab/xmake.lua
@@ -4,7 +4,6 @@ package("mecab")
     set_license("GPL-2.0")
     add_urls("https://github.com/taku910/mecab/archive/05481e751dd5aa536a2bace46715ce54568b972a.zip")
     add_versions("0.996", "d5d3ec4954e969ea93f4f1d778d7dc3ae98848056ee1c8cb99fced8578fd73f3")
-    add_configs("cxx17", {default = true, type = "boolean"})
     if is_plat("macosx", "linux") then
         add_deps("make", "autoconf")
     end
