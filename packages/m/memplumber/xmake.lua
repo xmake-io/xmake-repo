e4789bbcc63c80ff7c16cdddecb3320562e5c0f8
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-03-02 02:04:16 +0000

    memplumber: add limits

--- packages/m/memplumber/xmake.lua
+++ packages/m/memplumber/xmake.lua
@@ -6,13 +6,13 @@ package("memplumber")
     add_urls("https://github.com/seladb/MemPlumber.git")
     add_versions("2022.01.27", "ff04d339b034c40f72e09653c6a0340c0bb05d3b")
 
-    if is_plat("android", "linux", "macosx") then
+    if is_plat("linux", "macosx") then
         add_deps("libbacktrace")
     end
 
     add_configs("collect_static_var_data", {description = "Collect data also on static variable memory allocation", default = false, type = "boolean"})
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "mingw", function (package)
         io.replace("memplumber.cpp", "unsigned long", "uintptr_t")
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
