efc55ed2b3c3d7debb3df3931b5099b105039669
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-03-02 02:15:01 +0000

    injector: add license

--- packages/i/injector/xmake.lua
+++ packages/i/injector/xmake.lua
@@ -1,6 +1,7 @@
 package("injector")
     set_homepage("https://github.com/kubo/injector")
     set_description("Library for injecting a shared library into a Linux or Windows process")
+    set_license("LGPL-2.1")
 
     add_urls("https://github.com/kubo/injector.git")
     add_versions("2024.02.18", "c719b4f6b3bde75fd18d4d0c6b752a68dce593aa")
