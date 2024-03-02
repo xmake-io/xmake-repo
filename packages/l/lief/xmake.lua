e7862e6c0d1abc8a6d44cefeb14dad311d73ab20
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-03-02 01:29:18 +0000

    lief: add versions from 0.14.0

--- packages/l/lief/xmake.lua
+++ packages/l/lief/xmake.lua
@@ -8,6 +8,8 @@ package("lief")
              "https://github.com/lief-project/LIEF.git")
     add_versions("0.10.1", "6f30c98a559f137e08b25bcbb376c0259914b33c307b8b901e01ca952241d00a")
     add_versions("0.11.5", "6d6d57304a56850958e4ce54f3da2ea2b9eb856ccbab61c6cde9cba15d7c9da5")
+    add_versions("0.14.0", "400804e38cb5ce8d15fb52a4db6345f02da7b2e5cb773665712283001482b808")
+    add_versions("0.14.1", "92916dcb3178353d863aef4f409186889983c56e025b774741d5316a72ec3a7d")
 
     add_deps("cmake")
 
@@ -19,6 +21,10 @@ package("lief")
     add_configs("vdex",   {description = "Enable Vdex module.", default = false, type = "boolean"})
     add_configs("oat",    {description = "Enable Oat module.", default = false, type = "boolean"})
     add_configs("art",    {description = "Enable Art module.", default = false, type = "boolean"})
+
+    if is_plat("windows") then
+        add_syslinks("advapi32")
+    end
 
     on_install("macosx", "linux", "windows", function (package)
         local configs = {"-DLIEF_PYTHON_API=OFF", "-DLIEF_DOC=OFF", "-DLIEF_TESTS=OFF", "-DLIEF_EXAMPLES=OFF", "-DLIEF_INSTALL_PYTHON=OFF"}
@@ -42,6 +48,12 @@ package("lief")
             parse_entry = "macho_parse"
         end
         if parse_entry then
-            assert(package:has_cfuncs(parse_entry, {includes = "LIEF/LIEF.h"}))
+            assert(package:check_cxxsnippets({test = [[
+                #include <LIEF/LIEF.h>
+                void test() {
+                    ]] .. parse_entry .. [[("");
+                }
+            ]]}, {configs = {languages = "c"}}))
+            assert(package:has_cfuncs(parse_entry, {includes = ""}))
         end
     end)
