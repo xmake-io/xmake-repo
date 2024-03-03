ef9f9c979c8364916726734444cfa5c016c58740
c8ef <c8ef@outlook.com>
2024-03-03 11:36:58 +0800

    update

--- packages/f/fizz/xmake.lua
+++ packages/f/fizz/xmake.lua
@@ -2,7 +2,6 @@ package("fizz")
     set_homepage("https://github.com/facebookincubator/fizz")
     set_description("C++14 implementation of the TLS-1.3 standard ")
     set_license("BSD")
-    set_languages("c++14")
 
     add_urls("https://github.com/facebookincubator/fizz/releases/download/v$(version).00/fizz-v$(version).00.zip",
              "https://github.com/facebookincubator/fizz.git")
@@ -12,7 +11,7 @@ package("fizz")
 
     on_install("linux", "macosx", function (package)
         os.cd("fizz")
-        local configs = { "-DBUILD_TESTS=OFF", "-DBUILD_EXAMPLES=OFF" }
+        local configs = { "-DBUILD_TESTS=OFF", "-DBUILD_EXAMPLES=OFF", "-DCMAKE_CXX_STANDARD=14" }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
