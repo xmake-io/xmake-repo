20913ab3111c70b801003e335ff0238a473bc076
c8ef <c8ef@outlook.com>
2024-03-03 10:46:44 +0800

    update hash

--- packages/f/fizz/xmake.lua
+++ packages/f/fizz/xmake.lua
@@ -4,11 +4,11 @@ package("fizz")
 
     add_urls("https://github.com/facebookincubator/fizz/releases/download/v$(version).00/fizz-v$(version).00.zip",
              "https://github.com/facebookincubator/fizz.git")
-    add_versions("2024.02.26", "418375b9ee968071078da1d1fecb521c495377fbf687299ca7b57fc892858c2a")
+    add_versions("2024.02.26", "fa389dca0c49e14e83e089f07f896bf616757b3c70723ddfac7be2e3fd1f312f")
 
     add_deps("cmake", "folly", "libsodium")
 
-    on_install("linux", "macos", "windows", function (package)
+    on_install("linux", "macosx|x86_64", "windows|x64", function (package)
         local configs = { "-DBUILD_TESTS=OFF", "-DBUILD_EXAMPLES=OFF" }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
