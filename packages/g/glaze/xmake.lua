3799c7cb8c596337aa022ceb3030b1c1de6890d8
c8ef <c8ef@outlook.com>
2024-03-06 09:19:49 +0800

    fix test

--- packages/g/glaze/xmake.lua
+++ packages/g/glaze/xmake.lua
@@ -9,7 +9,7 @@ package("glaze")
     add_versions("v1.3.5", "de5d59cb7f31193d45f67f25d8ced1499df50c0d926a1461432b87f2b2368817")
     add_versions("v2.2.0", "1d6e36029a58bf8c4bdd035819e1ab02b87d8454dd80fa2f5d46c96a1e6d600c")
 
-    on_install(function (package)
+    on_install("linux", "macosx", function (package)
         os.cp("include", package:installdir())
     end)
 
