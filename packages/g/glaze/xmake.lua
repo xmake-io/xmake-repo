cfa35f3760be41c91e326c8a2d86ce9fc25cc49b
c8ef <c8ef@outlook.com>
2024-03-06 08:45:13 +0800

    update glaze to 2.2.0

--- packages/g/glaze/xmake.lua
+++ packages/g/glaze/xmake.lua
@@ -7,6 +7,7 @@ package("glaze")
              "https://github.com/stephenberry/glaze.git")
 
     add_versions("v1.3.5", "de5d59cb7f31193d45f67f25d8ced1499df50c0d926a1461432b87f2b2368817")
+    add_versions("v2.2.0", "1d6e36029a58bf8c4bdd035819e1ab02b87d8454dd80fa2f5d46c96a1e6d600c")
 
     on_install(function (package)
         os.cp("include", package:installdir())
