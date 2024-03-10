2e8517e2f5a98c4c0a3e1543fa3350df94ef2174
ruki <waruqi@gmail.com>
2024-03-10 00:59:03 +0000

    Update rtm to v2.3.0

--- packages/r/rtm/xmake.lua
+++ packages/r/rtm/xmake.lua
@@ -5,6 +5,7 @@ package("rtm")
 
     add_urls("https://github.com/nfrechette/rtm/archive/refs/tags/$(version).tar.gz",
              "https://github.com/nfrechette/rtm.git")
+    add_versions("v2.3.0", "2b5f2c3761bb52ae89802a574e9dc9949aec3b183f7e100b9b66a65adcc6f5ab")
     add_versions("v2.1.5", "afb05cb00b59498756ca197028de291a1960e58d5f6fcad161d8240682481eae")
 
     on_install("linux", "macosx", "windows", function (package)
