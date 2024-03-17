e1ebe29b8291246a8d83c35270de819cfdf7b1b6
ruki <waruqi@gmail.com>
2024-03-17 19:41:34 +0800

    update leptonica

--- packages/l/leptonica/xmake.lua
+++ packages/l/leptonica/xmake.lua
@@ -4,11 +4,12 @@ package("leptonica")
     set_description("Leptonica is a pedagogically-oriented open source site containing software that is broadly useful for image processing and image analysis applications.")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/DanBloomberg/leptonica/archive/$(version).tar.gz",
+    add_urls("https://github.com/DanBloomberg/leptonica/releases/download/$(version)/leptonica-$(version).tar.gz",
              "https://github.com/DanBloomberg/leptonica.git")
-    add_versions("1.80.0", "3952b974ec057d24267aae48c54bca68ead8275604bf084a73a4b953ff79196e")
-    add_versions("1.81.1", "e9dd2100194843a20bbb980ad8b94610558d47f623861bc80ac967f2d2ecb879")
-    add_versions("1.82.0", "40fa9ac1e815b91e0fa73f0737e60c9eec433a95fa123f95f2573dd3127dd669")
+    add_versions("1.80.0", "ec9c46c2aefbb960fb6a6b7f800fe39de48343437b6ce08e30a8d9688ed14ba4")
+    add_versions("1.81.1", "0f4eb315e9bdddd797f4c55fdea4e1f45fca7e3b358a2fc693fd957ce2c43ca9")
+    add_versions("1.82.0", "155302ee914668c27b6fe3ca9ff2da63b245f6d62f3061c8f27563774b8ae2d6")
+    add_versions("1.84.1", "2b3e1254b1cca381e77c819b59ca99774ff43530209b9aeb511e1d46588a64f6")
 
     add_deps("cmake")
     add_deps("libwebp", {configs = {libwebpmux = true}})
