99e29b91b115cf359cd105b456ff40547a336b3f
Seedking <875828815@qq.com>
2024-03-17 23:07:44 +0800

    add simsimd (#3409)
    * add simsimd
    
    * fix: add set_kind
    
    * fix: headonly -> dep cmake
    
    * Revert "fix: headonly -> dep cmake"
    
    This reverts commit ca6b7be88b4826602a5508e5d75e22948ca8ed35.
    
    * fix: on_test
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>
    Co-authored-by: ruki <waruqi@gmail.com>

---
+++ packages/s/simsimd/xmake.lua
@@ -0,0 +1,21 @@
+package("simsimd")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://ashvardanian.com/posts/simsimd-faster-scipy/")
+    set_description("Vector Similarity Functions 3x-200x Faster than SciPy and NumPy")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/ashvardanian/SimSIMD/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/ashvardanian/SimSIMD.git")
+
+    add_versions("v3.9.0", "8e79b628ba89beebc7c4c853323db0e10ebb6f85bcda2641e1ebaf77cfbda7f9")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+        if not package:has_ctypes("_Float16") then
+            package:add("defines", "SIMSIMD_NATIVE_F16=0")
+        end
+    end)
+
+    on_test(function (package)
+        assert(package:has_cincludes("simsimd/simsimd.h"))
+    end)
