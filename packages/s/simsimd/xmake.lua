d8f1a786074bedab8a8b798367ad43a55893e8d8
ruki <waruqi@gmail.com>
2024-03-17 21:44:14 +0800

    Update xmake.lua

--- packages/s/simsimd/xmake.lua
+++ packages/s/simsimd/xmake.lua
@@ -1,7 +1,7 @@
 package("simsimd")
     set_kind("library", {headeronly = true})
     set_homepage("https://ashvardanian.com/posts/simsimd-faster-scipy/")
-    set_description("Vector Similarity Functions 3x-200x Faster than SciPy and NumPy — for Python, JavaScript, Rust, and C 11, supporting f64, f32, f16, i8, and binary vectors using SIMD for both x86 AVX2 & AVX-512 and Arm NEON & SVE 📐")
+    set_description("Vector Similarity Functions 3x-200x Faster than SciPy and NumPy")
     set_license("Apache-2.0")
 
     add_urls("https://github.com/ashvardanian/SimSIMD/archive/refs/tags/$(version).tar.gz",
@@ -10,7 +10,13 @@ package("simsimd")
     add_versions("v3.9.0", "8e79b628ba89beebc7c4c853323db0e10ebb6f85bcda2641e1ebaf77cfbda7f9")
 
     on_install(function (package)
+        if package:is_plat("iphoneos") then
+            io.replace("simsimd/spatial.h", "_vec = vdotq_s32", "_vec = (int32x4_t)vdotq_s32", {plain = true})
+        end
         os.cp("include", package:installdir())
+        if not package:has_ctypes("_Float16") then
+            package:add("defines", "SIMSIMD_NATIVE_F16=0")
+        end
     end)
 
     on_test(function (package)
