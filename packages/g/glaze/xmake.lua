ee70d93bd395118c46114b56e84ad7c4a847c5db
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2024-03-21 17:45:34 +0200

    Update glaze to v.2.3.1 (#3578)

--- packages/g/glaze/xmake.lua
+++ packages/g/glaze/xmake.lua
@@ -8,6 +8,7 @@ package("glaze")
 
     add_versions("v1.3.5", "de5d59cb7f31193d45f67f25d8ced1499df50c0d926a1461432b87f2b2368817")
     add_versions("v2.2.0", "1d6e36029a58bf8c4bdd035819e1ab02b87d8454dd80fa2f5d46c96a1e6d600c")
+    add_versions("v2.3.1", "941bf3f8cea5b6a024895d37dceaaaa82071a9178af63e9935a1d9fd80caa451")
 
     on_install("linux", function (package)
         os.cp("include", package:installdir())
