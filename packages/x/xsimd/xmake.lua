abe41791028530e781f1bf7f02b1f27099d700c5
ruki <waruqi@gmail.com>
2024-03-23 08:10:28 +0800

    Auto-update xtensor to 0.25.0 (#3561)
    * Update xtensor to 0.25.0
    
    * xsimd: add 11.0.0
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/x/xsimd/xmake.lua
+++ packages/x/xsimd/xmake.lua
@@ -1,5 +1,4 @@
 package("xsimd")
-
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/xtensor-stack/xsimd/")
     set_description("C++ wrappers for SIMD intrinsics")
@@ -7,11 +6,13 @@ package("xsimd")
 
     add_urls("https://github.com/xtensor-stack/xsimd/archive/refs/tags/$(version).tar.gz",
              "https://github.com/xtensor-stack/xsimd.git")
+
     add_versions("7.6.0", "eaf47f1a316ef6c3287b266161eeafc5aa61226ce5ac6c13502546435b790252")
     add_versions("8.0.3", "d1d41253c4f82eaf2f369d7fcb4142e35076cf8675b9d94caa06ecf883024344")
     add_versions("8.0.5", "0e1b5d973b63009f06a3885931a37452580dbc8d7ca8ad40d4b8c80d2a0f84d7")
     add_versions("9.0.1", "b1bb5f92167fd3a4f25749db0be7e61ed37e0a5d943490f3accdcd2cd2918cc0")
     add_versions("10.0.0", "73f818368b3a4dad92fab1b2933d93694241bd2365a6181747b2df1768f6afdd")
+    add_versions("11.0.0", "50c31c319c8b36c8946eb954c7cca2e2ece86bf8a66a7ebf321b24cd273e7c47")
 
     if is_plat("windows") then
         add_cxxflags("/arch:AVX2")
