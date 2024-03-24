ec6450d41c276b037d59e50f49e553817f93b51e
star9029 <hengxings783@gmail.com>
2024-03-24 15:01:53 +0800

    add config

--- packages/c/cminpack/xmake.lua
+++ packages/c/cminpack/xmake.lua
@@ -7,6 +7,8 @@ package("cminpack")
 
     add_versions("v1.3.8", "3ea7257914ad55eabc43a997b323ba0dfee0a9b010d648b6d5b0c96425102d0e")
 
+    add_configs("blas", {description = "Compile cminpack using cblas library if possible", default = false, type = "boolean"})
+
     if is_plat("linux") then
         add_syslinks("m")
     end
@@ -15,14 +17,21 @@ package("cminpack")
 
     add_deps("cmake")
 
+    on_load(function (package)
+        if package:config("blas") then
+            package:add("deps", "openblas")
+        end
+    end)
+
     on_install(function (package)
-        if package:is_plat("windows") and (not package:config("shared")) then
+        if package:is_plat("windows", "mingw") and (not package:config("shared")) then
             package:add("defines", "CMINPACK_NO_DLL")
         end
 
         local configs = {"-DBUILD_EXAMPLES=OFF", "-DENABLE_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_BLAS=" .. (package:config("blas") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
