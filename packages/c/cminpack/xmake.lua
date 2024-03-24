f1f8310d182b9ffc3ebea94c39e16c02ba97d51b
star9029 <hengxings783@gmail.com>
2024-03-24 09:35:40 +0800

    cminpack: add package

---
+++ packages/c/cminpack/xmake.lua
@@ -0,0 +1,31 @@
+package("cminpack")
+    set_homepage("http://devernay.free.fr/hacks/cminpack/")
+    set_description("A C/C++ rewrite of the MINPACK software (originally in FORTRAN) for solving nonlinear equations and nonlinear least squares problems")
+
+    add_urls("https://github.com/devernay/cminpack/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/devernay/cminpack.git")
+
+    add_versions("v1.3.8", "3ea7257914ad55eabc43a997b323ba0dfee0a9b010d648b6d5b0c96425102d0e")
+
+    if is_plat("linux") then
+        add_syslinks("m")
+    end
+
+    add_includedirs("include", "include/cminpack-1")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        if package:is_plat("windows") and (not package:config("shared")) then
+            package:add("defines", "CMINPACK_NO_DLL")
+        end
+
+        local configs = {"-DBUILD_EXAMPLES=OFF", "-DENABLE_TESTING=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("lmdif", {includes = "minpack.h"}))
+    end)
