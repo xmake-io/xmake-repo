3329ee3e5aa846a08c3809c489772870b08e943f
star9029 <hengxings783@gmail.com>
2024-03-24 15:59:29 +0800

    shared readonly for gcc

--- packages/f/fastgltf/xmake.lua
+++ packages/f/fastgltf/xmake.lua
@@ -14,11 +14,14 @@ package("fastgltf")
     add_configs("memory_pool", {description = "Disables the memory allocation algorithm based on polymorphic resources", default = false, type = "boolean"})
     add_configs("f64", {description = "Default to 64-bit double precision floats for everything", default = false, type = "boolean"})
     add_configs("cxx_standard", {description = "Select c++ standard to build.", default = "17", type = "string", values = {"17", "20"}})
+    if is_plat("linux", "mingw") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
 
     add_deps("cmake")
     add_deps("simdjson")
 
-    on_install("windows|x64", "mingw|x86_64", "macosx|x86_64", "linux|x86_64", "linux|arm64", "iphoneos|arm64", function (package)
+    on_install("windows|x64", "mingw|x86_64", "macosx|x86_64", "linux|x86_64", "linux|arm64", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
