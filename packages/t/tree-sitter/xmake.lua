8b4fec36103946102973e7798c5306e4333b5d14
ruki <waruqi@gmail.com>
2024-03-24 11:30:51 +0800

    Update tree-sitter to 0.22.2 (#3591)

--- packages/t/tree-sitter/xmake.lua
+++ packages/t/tree-sitter/xmake.lua
@@ -5,6 +5,7 @@ package("tree-sitter")
 
     add_urls("https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v$(version).zip")
 
+    add_versions("0.22.2", "df0cd4aacc53b6feb9519dd4b74a7a6c8b7f3f7381fcf7793250db3e5e63fb80")
     add_versions("0.21.0", "874794e6b3b985f7f9e87dfe29e4bfdbe5c0339e67740f35dfc4fa85804ba708")
 
     on_install(function(package)
