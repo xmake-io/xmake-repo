740b70ec36f8a21d8cb37309c62c6e79df0161db
Jérôme Leclercq <lynix680@gmail.com>
2024-03-27 17:28:24 +0100

    Update xmake.lua

--- packages/p/pugixml/xmake.lua
+++ packages/p/pugixml/xmake.lua
@@ -5,9 +5,10 @@ package("pugixml")
 
     add_urls("https://github.com/zeux/pugixml/archive/$(version).tar.gz",
              "https://github.com/zeux/pugixml.git")
+
     add_versions("v1.11.4", "017139251c122dbff400a507cddc4cb74120a431a50c6c524f30edcc5b331ade")
     add_versions("v1.13", "5c5ad5d7caeb791420408042a7d88c2c6180781bf218feca259fd9d840a888e1")
-    add_versions("v1.14", "2f10e276870c64b1db6809050a75e11a897a8d7456c4be5c6b2e35a11168a015")
+    add_versions("v1.14", "610f98375424b5614754a6f34a491adbddaaec074e9044577d965160ec103d2e")
 
     add_deps("cmake")
 
