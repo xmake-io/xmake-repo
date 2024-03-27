71b1af822b5f1b84c33c454974e26688b17ce439
Jérôme Leclercq <lynix680@gmail.com>
2024-03-27 16:56:24 +0100

    Add tmxlite

---
+++ packages/t/tmxlite/xmake.lua
@@ -0,0 +1,41 @@
+package("tmxlite")
+    set_homepage("https://github.com/fallahn/tmxlite")
+    set_description("lightweight C++14 parser for Tiled tmx files")
+    set_license("zlib")
+
+    add_urls("https://github.com/fallahn/tmxlite/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/fallahn/tmxlite.git")
+
+    add_versions("v1.4.4", "ec8893efc8396308f291c284cb09f007441a15aabbb0e5722096cf79c65c9e58")
+
+    add_deps("cmake", "pugixml", "zlib", "zstd")
+
+    on_install(function (package)
+        os.cd("tmxlite")
+        local configs = {
+            "-DENABLE_ALL_WARNINGS=OFF",
+            "-DINTERPROCEDURAL_OPTIMIZATION=OFF",
+            "-DTARGET_UNIT_TESTS=OFF",
+            "-DTARGET_HELLO_WORLD=OFF",
+            "-DTARGET_PERFORMANCE_TEST=OFF",
+            "-DTARGET_SAMPLES=OFF",
+            "-DTARGET_VIEWER=OFF",
+            "-DUSE_STATIC_MSVC_RUNTIME_LIBRARY=OFF",
+            "-DOVERRIDE_CXX_FLAGS=OFF"
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DTMXLITE_STATIC_LIB=" .. (package:config("shared") and "OFF" or "ON"))
+        table.insert(configs, "-DUSE_EXTLIBS=ON")
+        table.insert(configs, "-DUSE_ZSTD=ON")
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                tmx::Map map;
+                map.load("assets/test.tmx");
+            }
+        ]]}, {configs = {languages = "c++14"}, includes = "tmxlite/Map.hpp"}))
+    end)
