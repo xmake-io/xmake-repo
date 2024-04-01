30b568976cbf76e7b90300f647117a7a583c16e2
c8ef <c8ef@outlook.com>
2024-04-01 23:10:35 +0800

    update folly related to 04.01

--- packages/w/wangle/xmake.lua
+++ packages/w/wangle/xmake.lua
@@ -10,6 +10,7 @@ package("wangle")
     add_versions("2024.03.11", "ad9fc14426c9463b9469848214e095fc1793779045bd9685c2d4d0432cb49674")
     add_versions("2024.03.18", "578986898b3464ed9bd2e392a08d07604b68b2251322518c1f819c965eebe8f2")
     add_versions("2024.03.25", "3de926ff92e59f5185e89e5e9365925530e5e57fd70f8e5107938149ce2fe140")
+    add_versions("2024.04.01", "c21c3616d3017bc7b72f6b2315f81be4fd9be4c0dc6e1fae0266ec545fbc5535")
 
     add_deps("cmake", "folly", "fizz")
 
