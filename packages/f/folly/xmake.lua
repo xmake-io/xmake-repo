8cb9794813e62c1ad3f5a3d7b5ae6dc5723bd621
c8ef <c8ef@outlook.com>
2024-04-02 07:56:26 +0800

    Update xmake.lua

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -38,7 +38,7 @@ package("folly")
         add_syslinks("pthread")
     end
 
-    on_install("linux", "macosx", "windows|x64", function (package)
+    on_install("linux", "macosx", function (package)
         local configs = {"-DBUILD_TESTS=OFF",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_LibDwarf=ON",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_Libiberty=ON",
