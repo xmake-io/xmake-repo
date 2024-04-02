6be5157f06be898fc775dc21546e626b75a92495
ruki <waruqi@gmail.com>
2024-04-02 00:58:56 +0000

    Update thread-pool to v4.1.0

--- packages/t/thread-pool/xmake.lua
+++ packages/t/thread-pool/xmake.lua
@@ -6,6 +6,7 @@ package("thread-pool")
 
     add_urls("https://github.com/bshoshany/thread-pool/archive/refs/tags/$(version).tar.gz",
              "https://github.com/bshoshany/thread-pool.git")
+    add_versions("v4.1.0", "be7abecbc420bb87919eeef729b13ff7c29d5ce547bdae284923296c695415bd")
     add_versions("v3.3.0", "b76c0103c7ed07c137bd5b1988b9c09da280bbbad37588a096d2954c8d996e0f")
 
     on_install(function (package)
