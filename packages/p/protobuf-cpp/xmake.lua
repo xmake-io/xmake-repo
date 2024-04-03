fdc820639badde4e1e967ab1e5e00f97a984782d
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-04-03 17:38:17 +0100

    protobuf-cpp: use default config for abseil

--- packages/p/protobuf-cpp/xmake.lua
+++ packages/p/protobuf-cpp/xmake.lua
@@ -39,7 +39,7 @@ package("protobuf-cpp")
             package:add("deps", "zlib")
         end
         if package:version():gt("3.19.4") then
-            package:add("deps", "abseil", {configs = {shared = package:is_plat("windows")}})
+            package:add("deps", "abseil")
         end
     end)
 
