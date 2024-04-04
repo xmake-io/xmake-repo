dcfc306ef2e6336b0151df0a9dae2bbc3cf05d41
KkemChen <kkemchen@qq.com>
2024-04-04 13:34:12 +0800

    cinatra: add v0.8.9

--- packages/c/cinatra/xmake.lua
+++ packages/c/cinatra/xmake.lua
@@ -8,10 +8,15 @@ package("cinatra")
     add_urls("https://github.com/qicosmos/cinatra/archive/refs/tags/v$(version).tar.gz",
              "https://github.com/qicosmos/cinatra.git")
 
+    add_versions("0.8.9", "007dc38aceedf42d03a9c05dc9aa6d2f303456ae7ce1100800df7a565b83b510")
     add_versions("0.8.0", "4e14d5206408eccb43b3e810d3a1fe228fbc7496ded8a16b041ed12cbcce4479")
 
     add_configs("ssl", {description = "Enable SSL", default = false, type = "boolean"})
     add_configs("gzip", {description = "Enable GZIP", default = false, type = "boolean"})
+    add_configs("sse42",{description = "Enable sse4.2 instruction set", default = false, type = "boolean"})
+    add_configs("avx2",{description = "Enable avx2 instruction set", default = false, type = "boolean"})
+    add_configs("aarch64",{description = "Enable aarch64 instruction set (only arm)", default = false, type = "boolean"})
+
 
     add_deps("asio")
     add_deps("async_simple", {configs = {aio = false}})
@@ -26,6 +31,18 @@ package("cinatra")
             package:add("deps", "zlib")
             package:add("defines", "CINATRA_ENABLE_GZIP")
         end
+
+        local configdeps = {
+            sse42 = "CINATRA_SSE",
+            avx2 = "CINATRA_AVX2",
+            aarch64 = "CINATRA_ARM_OPT"
+        }
+        
+        for name, item in pairs(configdeps) do
+            if package:config(name) then
+                package:add("defines", item)
+            end
+        end
     end)
 
     on_install("windows", "linux", "macosx", function (package)
@@ -37,12 +54,11 @@ package("cinatra")
             #include <cinatra.hpp>
             using namespace cinatra;
             void test() {
-                http_server server(std::thread::hardware_concurrency());
-                server.listen("0.0.0.0", "8080");
-                server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
-                    res.set_status_and_content(status_type::ok, "hello world");
+                coro_http_server server(std::thread::hardware_concurrency(), 8080);
+                server.set_http_handler<GET, POST>("/", [](coro_http_request& req, coro_http_response& res) {
+			        res.set_status_and_content(status_type::ok, "hello world");
                 });
-                server.run();
+                server.sync_start();
             }
         ]]}, {configs = {languages = "c++20"}}))
     end)
