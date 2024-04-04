dee674b0bb7c87f2170c1a31439635874ab87cc2
star9029 <hengxings783@gmail.com>
2024-04-04 14:14:11 +0800

    support windows arm64

--- packages/a/angelscript/xmake.lua
+++ packages/a/angelscript/xmake.lua
@@ -1,5 +1,4 @@
 package("angelscript")
-
     set_homepage("http://angelcode.com/angelscript/")
     set_description("Extremely flexible cross-platform scripting library designed to allow applications to extend their functionality through external scripts")
     set_license("zlib")
@@ -10,17 +9,21 @@ package("angelscript")
     add_versions("2.35.0", "010dd45e23e734d46f5891d70e268607a12cb9ab12503dda42f842d9db7e8857")
     add_versions("2.34.0", "6faa043717522ae0fb2677d907ca5b0e35a79d28e5f83294565e6c6229bfbdf7")
 
+    if is_plat("windows") and is_arch("arm64") then
+        add_patches(">=2.34.0", "patches/cmake-msvc-arm64.patch", "5054f3011ba1886325dbbcf7f7e6cf6d77350b2a51cc65ceed22e7fb8b85f9bb")
+    end
+
     if is_plat("linux") then
         add_syslinks("pthread")
     end
 
     add_deps("cmake")
 
-    on_install("windows|x86", "windows|x64", "linux", "android", "msys", "mingw", function (package)
+    on_install("windows", "linux", "android", "msys", "mingw", function (package)
         os.cd("angelscript/projects/cmake")
         local configs = {}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         if package:is_plat("android") then
             io.gsub("CMakeLists.txt", "set_property", "#set_property")
         end
