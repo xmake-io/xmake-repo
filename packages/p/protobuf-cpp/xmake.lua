6459e530767c1bcc55514b319ebe97bd6927fc67
c8ef <c8ef@outlook.com>
2024-04-06 18:54:26 +0800

    Update xmake.lua

--- packages/p/protobuf-cpp/xmake.lua
+++ packages/p/protobuf-cpp/xmake.lua
@@ -3,7 +3,7 @@ package("protobuf-cpp")
     set_homepage("https://developers.google.com/protocol-buffers/")
     set_description("Google's data interchange format for cpp")
 
-    add_urls("https://github.com/protocolbuffers/protobuf/releases/download/v$(version), {version = function (version)
+    add_urls("https://github.com/protocolbuffers/protobuf/releases/download/v$(version)", {version = function (version)
         if version:le("3.19.4") then
             return "protobuf-cpp-" .. version .. ".zip"
         else
