19b94a56bfc5d90ef602a4cd17d381e9f977475f
star9029 <hengxings783@gmail.com>
2024-04-09 21:12:42 +0800

    improve meson

--- packages/o/openh264/xmake.lua
+++ packages/o/openh264/xmake.lua
@@ -1,29 +1,38 @@
 package("openh264")
-
     set_homepage("http://www.openh264.org/")
     set_description("OpenH264 is a codec library which supports H.264 encoding and decoding.")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/cisco/openh264/archive/refs/tags/$(version).tar.gz")
+    set_urls("https://github.com/cisco/openh264/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/cisco/openh264.git")
+
     add_versions("v2.4.1", "8ffbe944e74043d0d3fb53d4a2a14c94de71f58dbea6a06d0dc92369542958ea")
     add_versions("v2.1.1", "af173e90fce65f80722fa894e1af0d6b07572292e76de7b65273df4c0a8be678")
 
-    add_deps("meson", "ninja", "nasm")
     if is_plat("linux") then
         add_syslinks("pthread", "rt")
     end
+
+    add_deps("meson", "ninja", "nasm")
+
     on_install("windows", "linux", function (package)
-        import("package.tools.meson").build(package, {"-Dtests=disabled"}, {buildir = "out"})
-        import("package.tools.ninja").install(package, {}, {buildir = "out"})
-        if package:config("shared") then
-            os.tryrm(path.join(package:installdir("lib"), "libopenh264.a"))
+        if package:version():ge("2.4.1") then
+            local configs = {"-Dtests=disabled"}
+            table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
+            import("package.tools.meson").install(package, configs)
         else
-            os.tryrm(path.join(package:installdir("lib"), "libopenh264.so*"))
-            os.tryrm(path.join(package:installdir("lib"), "openh264.lib"))
-            os.tryrm(path.join(package:installdir("bin"), "openh264-*.dll"))
-        end
-        if package:is_plat("windows") then
-            os.trymv(path.join(package:installdir("lib"), "libopenh264.a"), path.join(package:installdir("lib"), "openh264.lib"))
+            import("package.tools.meson").build(package, {"-Dtests=disabled"}, {buildir = "out"})
+            import("package.tools.ninja").install(package, {}, {buildir = "out"})
+            if package:config("shared") then
+                os.tryrm(path.join(package:installdir("lib"), "libopenh264.a"))
+            else
+                os.tryrm(path.join(package:installdir("lib"), "libopenh264.so*"))
+                os.tryrm(path.join(package:installdir("lib"), "openh264.lib"))
+                os.tryrm(path.join(package:installdir("bin"), "openh264-*.dll"))
+            end
+            if package:is_plat("windows") then
+                os.trymv(path.join(package:installdir("lib"), "libopenh264.a"), path.join(package:installdir("lib"), "openh264.lib"))
+            end
         end
     end)
 
