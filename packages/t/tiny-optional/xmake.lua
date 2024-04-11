21f07d99a69abe2718207baf66eb075a9392fef1
star9029 <hengxings783@gmail.com>
2024-04-12 00:18:44 +0800

    Update xmake.lua

--- packages/t/tiny-optional/xmake.lua
+++ packages/t/tiny-optional/xmake.lua
@@ -9,14 +9,10 @@ package("tiny-optional")
 
     add_versions("v1.2.0", "d4ce47d0c9c4f89ab41e4e0b96d25bfb98c0cc02da3d7b312337e5e4e6e1c9e8")
 
-    on_install("windows|!arm*", "linux|!arm*", "macosx|!arm*", "bsd|!arm*", "mingw|!arm*", "msys|!arm*", function (package)
+    on_install("windows|!arm*", "linux|!arm*", "macosx|!arm*", "bsd|!arm*", "mingw|!arm*", "msys|!arm*", "android|!arm*", function (package)
         os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
-        assert(package:check_cxxsnippets({test = [[
-            void test() {
-                tiny::optional<double> x;
-            }
-        ]]}, {configs = {languages = "c++17"}, includes = {"tiny/optional.h"}}))
+        assert(package:has_cxxtypes("tiny::optional<int>", {configs = {languages = "c++17"}, includes = "tiny/optional.h"}))
     end)
