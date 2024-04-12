3077e02f02a141e2645fa7e3603289bbea707efd
star9029 <hengxings783@gmail.com>
2024-04-12 23:18:31 +0800

    Update xmake.lua

--- packages/t/tiny-optional/xmake.lua
+++ packages/t/tiny-optional/xmake.lua
@@ -9,7 +9,7 @@ package("tiny-optional")
 
     add_versions("v1.2.0", "d4ce47d0c9c4f89ab41e4e0b96d25bfb98c0cc02da3d7b312337e5e4e6e1c9e8")
 
-    on_install("!cross,!wasm|!arm*", function (package)
+    on_install("windows|!arm*", "linux|!arm*", "macosx|!arm*", "bsd|!arm*", "mingw|!arm*", "msys|!arm*", "android|!arm*", function (package)
         os.cp("include", package:installdir())
     end)
 
