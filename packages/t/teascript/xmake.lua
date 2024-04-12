e4c12c827ffb3645d42d970397cc88e83d44a4ae
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-04-13 00:23:36 +0100

    teascript: fix ignore iphone

--- packages/t/teascript/xmake.lua
+++ packages/t/teascript/xmake.lua
@@ -21,7 +21,7 @@ package("teascript")
         end
     end)
 
-    on_install("!cross,!android,!iphone,!mingw,!wasm"--[[, "!macosx|x86_64"]], function (package)
+    on_install("!cross,!android,!iphon*,!mingw,!wasm"--[[, "!macosx|x86_64"]], function (package)
         os.cp("include", package:installdir())
     end)
 
