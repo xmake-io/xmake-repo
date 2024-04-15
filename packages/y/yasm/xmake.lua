f5cf7b3741c831d306ce93f5c2a2780bcaf0e62e
Jérôme Leclercq <lynix680@gmail.com>
2024-04-15 13:49:42 +0200

    fix

--- packages/y/yasm/xmake.lua
+++ packages/y/yasm/xmake.lua
@@ -4,9 +4,11 @@ package("yasm")
     set_description("Modular BSD reimplementation of NASM.")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/yasm/yasm/archive/refs/tags/$(version.tar.gz",
+    add_urls("https://github.com/yasm/yasm/archive/refs/tags/$(version).tar.gz",
              "https://github.com/yasm/yasm.git")
     add_versions("v1.3.0", "f708be0b7b8c59bc1dbe7134153cd2f31faeebaa8eec48676c10f972a1f13df3")
+
+    add_deps("cmake")
 
     on_install(function (package)
         local configs = {"-DYASM_BUILD_TESTS=OFF"}
