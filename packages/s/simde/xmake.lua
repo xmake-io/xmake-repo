416d832df492b8f828523b10654dedfb34927fab
ruki <waruqi@gmail.com>
2024-04-15 03:19:12 +0000

    Update simde to 0.8.0

--- packages/s/simde/xmake.lua
+++ packages/s/simde/xmake.lua
@@ -6,6 +6,7 @@ package("simde")
 
     set_urls("https://github.com/simd-everywhere/simde/releases/download/v$(version)/simde-amalgamated-$(version).tar.xz")
 
+    add_versions("0.8.0", "7c8dd4d613b18724b7ef3dcd1d58739a91501ed80ace916cbca9b8c13e5b92bb")
     add_versions("0.7.2", "544c8aac764f0e24e444b1a7842d0314fa0231802d3b1b2020a03677b5be6142")
 
     on_install(function (package)
