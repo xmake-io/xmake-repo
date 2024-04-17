624e3eb437769cb4730d245313ff0fa9a0ec4031
Raildex <n@a.com>
2024-04-17 12:42:37 +0200

    glad: Added version 2.0.0 and 2.0.6

--- packages/g/glad/xmake.lua
+++ packages/g/glad/xmake.lua
@@ -8,6 +8,8 @@ package("glad")
              "https://github.com/Dav1dde/glad.git")
     add_versions("v0.1.34", "4be2900ff76ac71a2aab7a8be301eb4c0338491c7e205693435b09aad4969ecd")
     add_versions("v0.1.36", "8470ed1b0e9fbe88e10c34770505c8a1dc8ccb78cadcf673331aaf5224f963d2")
+    add_versions("v2.0.0", "98977626533c8a58550cc358a8859c17f25c303803d9fa9217b7105c3208aa6c")
+    add_versions("v2.0.6", "61a70234dc7da467cceb07fcdd6dec1213d6143a1b6b19ccc5d7b64cc247ea47")
 
     if is_plat("linux") then
         add_syslinks("dl")
