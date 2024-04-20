8af250d6cd006e8693a54def22cc94f4bb31dde8
star9029 <hengxings783@gmail.com>
2024-04-20 15:00:50 +0800

    add pkgconf

--- packages/g/gstreamer/xmake.lua
+++ packages/g/gstreamer/xmake.lua
@@ -4,11 +4,10 @@ package("gstreamer")
     set_license("LGPL-2.0-or-later")
 
     add_urls("https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-$(version).tar.xz", {alias = "home"})
-    -- add_urls("https://github.com/GStreamer/gstreamer/archive/refs/tags/$(version).tar.gz", {alias = "github"})
 
-    add_versions("home:1.22.8", "ad4e3db1771139b1db17b1afa7c05db083ae0100bd4da244b71f162dcce41bfc")
-    -- add_versions("github:1.22.8", "ebe085820a32f135d9a5a3442b2cb2238d8ce1d3bc66f4d6bfbc11d0873dbecc")
+    add_versions("home:1.24.2", "9cafdd23bd180f1681c56cd3a6879a8497ccf24da6f422a6b6f356fa074a8481")
 
+    add_configs("tools", {description = "Build tools.", default = false, type = "boolean"})
     add_configs("libunwind", {description = "Use libunwind to generate backtraces", default = false, type = "boolean"})
 
     if is_plat("linux") then
@@ -20,12 +19,12 @@ package("gstreamer")
     end
 
     add_deps("meson", "ninja")
-    add_deps("glib")
     if is_plat("windows") then
-        add_deps("winflexbison")
+        add_deps("pkgconf","winflexbison")
     else
         add_deps("flex", "bison")
     end
+    add_deps("glib")
 
     add_includedirs("include", "include/gstreamer-1.0")
 
@@ -33,11 +32,13 @@ package("gstreamer")
         if package:config("libunwind") then
             package:add("deps", "libunwind")
         end
+        if not package:config("shared") then
+            package:add("defines", "GST_STATIC_COMPILATION")
+        end
     end)
 
     on_install("windows", "macosx", "linux", "cross", function (package)
         local configs = {
-            "-Dtools=disabled",
             "-Dexamples=disabled",
             "-Dbenchmarks=disabled",
             "-Dtests=disabled",
@@ -45,15 +46,14 @@ package("gstreamer")
         table.insert(configs, "-Dgst_debug=" .. (package:is_debug() and "true" or "false"))
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         table.insert(configs, "-Dlibunwind=" .. (package:config("libunwind") and "enabled" or "disabled"))
+        table.insert(configs, "-Dtools=" .. (package:config("tools") and "enabled" or "disabled"))
+
         local packagedeps = {}
         if not package:dep("glib"):config("shared") then
             table.insert(packagedeps, "libiconv")
         end
-        if package:is_plat("macosx") then
+        if package:is_plat("windows", "macosx") then
             table.insert(packagedeps, "libintl")
-        end
-        if package:is_plat("windows") then
-            table.insert(packagedeps, "glib")
         end
         import("package.tools.meson").install(package, configs, {packagedeps = packagedeps})
     end)
