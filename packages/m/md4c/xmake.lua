3ccfd522356bbcb58f617ad0210a6a98a62728d1
ruki <waruqi@gmail.com>
2024-04-21 22:59:18 +0800

    md4c

---
+++ packages/m/md4c/xmake.lua
@@ -0,0 +1,20 @@
+package("md4c")
+    set_homepage("https://github.com/mity/md4c")
+    set_description("C Markdown parser. Fast. SAX-like interface. Compliant to CommonMark specification.")
+    set_license("MIT")
+
+    add_urls("https://github.com/mity/md4c.git")
+    add_versions("2024.02.25", "481fbfbdf72daab2912380d62bb5f2187d438408")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("md_html", {includes = "md4c-html.h"}))
+    end)
