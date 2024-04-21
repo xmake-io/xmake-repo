e66c2ef1144df20e4fec960936661ac02696f005
ruki <waruqi@gmail.com>
2024-04-21 23:18:19 +0800

    fix test

--- packages/m/md4c/xmake.lua
+++ packages/m/md4c/xmake.lua
@@ -20,5 +20,7 @@ package("md4c")
 
     on_test(function (package)
         assert(package:has_cfuncs("md_html", {includes = "md4c-html.h"}))
-        os.exec("md2html --version")
+        if not package:is_cross() then
+            os.exec("md2html --version")
+        end
     end)
