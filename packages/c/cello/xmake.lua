a3aea3ed40d54d6af13b53fd21729d90cbe67ab3
star9029 <hengxings783@gmail.com>
2024-04-25 00:08:33 +0800

    fix links

--- packages/c/cello/xmake.lua
+++ packages/c/cello/xmake.lua
@@ -7,6 +7,12 @@ package("cello")
 
     add_versions("2.1.0", "c138d974325fcb9640307c8b2d5dcc2d7127a1ccc5589d6c0794f86a5cb4001d")
 
+    if is_plat("windows", "mingw") then
+        add_syslinks("dbghelp")
+    elseif is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
+
     on_install(function(package)
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
@@ -16,9 +22,22 @@ package("cello")
                 add_headerfiles("include/Cello.h")
                 add_includedirs("include")
 
-                if is_plat("windows") and is_kind("shared") then
-                    add_rules("utils.symbols.export_all")
+                if is_plat("windows", "mingw") then
+                    add_syslinks("dbghelp")
+                    if is_kind("shared") then
+                        add_rules("utils.symbols.export_all")
+                    end
+                elseif is_plat("linux", "bsd") then
+                    add_syslinks("pthread")
                 end
+
+                on_config(function(target)
+                    if not target:has_cincludes("execinfo.h") then
+                        target:add("defines", "CELLO_NSTRACE")
+                    else
+                        target:add("syslinks", "execinfo")
+                    end
+                end)
         ]])
         import("package.tools.xmake").install(package)
     end)
