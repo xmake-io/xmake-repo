6c3982fa5a31c79f20f8fc2a0bf6308f5ffeb059
star9029 <hengxings783@gmail.com>
2024-04-30 19:34:50 +0800

    iguana: update to 1.0.4

--- packages/i/iguana/xmake.lua
+++ packages/i/iguana/xmake.lua
@@ -7,11 +7,13 @@ package("iguana")
     add_urls("https://github.com/qicosmos/iguana/archive/refs/tags/$(version).tar.gz",
              "https://github.com/qicosmos/iguana.git")
 
+    add_versions("1.0.4", "b584cd26e65902a14a3a349ebc480beb7b4502fd5a5ffa3cb7c6102d857958b1")
     add_versions("v1.0.3", "7dcb21a36bd64a63a9ea857f3563ac61e965c49ec60ad7b99a2bfb9192f3e4c3")
 
+    add_deps("frozen")
+
     on_install(function (package)
-        os.cp("iguana", package:installdir("include"))
-        os.cp("frozen", package:installdir("include"))
+        os.vcp("iguana", package:installdir("include"))
     end)
 
     on_test(function (package)
@@ -27,6 +29,5 @@ package("iguana")
                 std::string_view str = "{\"name\":\"tom\", \"age\":20}";
                 iguana::from_json(obj, str);
             }
-        ]]}, {configs = {languages = "c++17"}}))
+        ]]}, {configs = {languages = "c++20"}}))
     end)
-
