d409a7c0315ba11e8ac2a5b994c97f127603411e
ruki <waruqi@gmail.com>
2024-04-30 09:43:10 +0800

    Update xmake.lua

--- packages/g/gamenetworkingsockets/port/xmake.lua
+++ packages/g/gamenetworkingsockets/port/xmake.lua
@@ -430,7 +430,7 @@ end
 target("gns") -- we need limit path length
     set_kind("$(kind)")
     add_rules("protobuf.cpp")
-    set_languages("gnu17", "c++17")
+    set_languages("gnu17", "gnu++17")
     add_vectorexts("sse2")
     add_packages("protobuf-cpp", "openssl")
     set_basename("gamenetworkingsockets")
