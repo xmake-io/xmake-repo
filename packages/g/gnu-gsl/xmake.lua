1a1c0ad0dd29a3d3a355a5fc3428ce2dcd59524f
huarkiou <46279300+huarkiou@users.noreply.github.com>
2024-04-30 15:52:01 +0800

    Update gnu-gsl to latest 2.7.1

--- packages/g/gnu-gsl/xmake.lua
+++ packages/g/gnu-gsl/xmake.lua
@@ -6,6 +6,10 @@ package("gnu-gsl")
 
     add_urls("https://ftpmirror.gnu.org/gsl/gsl-$(version).tar.gz",
              "https://ftp.gnu.org/gnu/gsl/gsl-$(version).tar.gz")
+    add_versions("2.7.1", "DCB0FBD43048832B757FF9942691A8DD70026D5DA0FF85601E52687F6DEEB34B")
+    add_patches("2.7.1", path.join(os.scriptdir(), "0001-configure.patch"), "50FE9E6A4E68750FA2E21FEBF05471423CC7A0A38E59CF41D5009CD79352B2E6")
+    add_patches("2.7.1", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6C6782327126EA979C5ACEAB3EE022B5DDC7D9D01C244774294572E73427EC4B")
+
     add_versions("2.7", "efbbf3785da0e53038be7907500628b466152dbc3c173a87de1b5eba2e23602b")
     add_patches("2.7", path.join(os.scriptdir(), "0001-configure.patch"), "50FE9E6A4E68750FA2E21FEBF05471423CC7A0A38E59CF41D5009CD79352B2E6")
     add_patches("2.7", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6C6782327126EA979C5ACEAB3EE022B5DDC7D9D01C244774294572E73427EC4B")
