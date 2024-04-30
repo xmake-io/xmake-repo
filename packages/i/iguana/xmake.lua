a47b7a12d11521bf4a2c3938a35f0af573f2a1a7
star9029 <hengxings783@gmail.com>
2024-04-30 20:06:31 +0800

    only c++20 for windows arm

--- packages/i/iguana/xmake.lua
+++ packages/i/iguana/xmake.lua
@@ -13,18 +13,15 @@ package("iguana")
     add_deps("frozen")
 
     on_install(function (package)
-        if package:is_plat("android") and package:is_arch("arm.*") then
-            import("core.tool.toolchain")
-
-            local ndk = toolchain.load("ndk", {plat = package:plat(), arch = package:arch()})
-            local ndk_sdkver = ndk:config("ndk_sdkver")
-            assert(ndk_sdkver and tonumber(ndk_sdkver) > 22, "package(iguana): need c++20 stl for arm android")
-        end
-
         os.vcp("iguana", package:installdir("include"))
     end)
 
     on_test(function (package)
+        local languages = "c++17"
+        if package:is_plat("windows") and package:is_arch("arm.*") then
+            languages = "c++20"
+        end
+
         assert(package:check_cxxsnippets({test = [[
             #include <iguana/json_reader.hpp>
             struct some_obj {
@@ -37,5 +34,5 @@ package("iguana")
                 std::string_view str = "{\"name\":\"tom\", \"age\":20}";
                 iguana::from_json(obj, str);
             }
-        ]]}, {configs = {languages = "c++20"}}))
+        ]]}, {configs = {languages = languages}}))
     end)
