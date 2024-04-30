5531fd03cd885532190afdbdac64b177e128053b
huarkiou <46279300+huarkiou@users.noreply.github.com>
2024-04-30 16:18:30 +0800

    change all sha256 string to lowercase

--- packages/g/gnu-gsl/xmake.lua
+++ packages/g/gnu-gsl/xmake.lua
@@ -7,12 +7,12 @@ package("gnu-gsl")
     add_urls("https://ftpmirror.gnu.org/gsl/gsl-$(version).tar.gz",
              "https://ftp.gnu.org/gnu/gsl/gsl-$(version).tar.gz")
     add_versions("2.7.1", "dcb0fbd43048832b757ff9942691a8dd70026d5da0ff85601e52687f6deeb34b")
-    add_patches("2.7.1", path.join(os.scriptdir(), "0001-configure.patch"), "50FE9E6A4E68750FA2E21FEBF05471423CC7A0A38E59CF41D5009CD79352B2E6")
-    add_patches("2.7.1", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6C6782327126EA979C5ACEAB3EE022B5DDC7D9D01C244774294572E73427EC4B")
+    add_patches("2.7.1", path.join(os.scriptdir(), "0001-configure.patch"), "50fe9e6a4e68750fa2e21febf05471423cc7a0a38e59cf41d5009cd79352b2e6")
+    add_patches("2.7.1", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6c6782327126ea979c5aceab3ee022b5ddc7d9d01c244774294572e73427ec4b")
 
     add_versions("2.7", "efbbf3785da0e53038be7907500628b466152dbc3c173a87de1b5eba2e23602b")
-    add_patches("2.7", path.join(os.scriptdir(), "0001-configure.patch"), "50FE9E6A4E68750FA2E21FEBF05471423CC7A0A38E59CF41D5009CD79352B2E6")
-    add_patches("2.7", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6C6782327126EA979C5ACEAB3EE022B5DDC7D9D01C244774294572E73427EC4B")
+    add_patches("2.7", path.join(os.scriptdir(), "0001-configure.patch"), "50fe9e6a4e68750fa2e21febf05471423cc7a0a38e59cf41d5009cd79352b2e6")
+    add_patches("2.7", path.join(os.scriptdir(), "0002-add-fp-control.patch"), "6c6782327126ea979c5aceab3ee022b5ddc7d9d01c244774294572e73427ec4b")
 
     add_links("gsl", "gslcblas")
     on_install("macosx", "linux", function (package)
