1b21b1d896230e6f51c50f4dfe1e26f2b4326501
Arteiimis <lionelym0987@gmail.com>
2024-04-30 17:44:09 +0800

    Refactor xmake.lua to exclude copying frozen and iguana directories during installation

--- packages/o/ormpp/xmake.lua
+++ packages/o/ormpp/xmake.lua
@@ -30,12 +30,12 @@ package("ormpp")
         end
     end)
 
-    if not is_plat("windows|arm64") then
+    if not (is_plat("windows") and is_arch("arm64")) then
         add_deps("frozen", "iguana")
     end
 
     on_install(function (package)
-        if is_plat("windows|arm64") then
+        if is_plat("windows") and is_arch("arm64") then
             os.cp("frozen/**", package:installdir("include/frozen"), {rootdir = "frozen"})
             os.cp("iguana/**", package:installdir("include/iguana"), {rootdir = "iguana"})   
         end
