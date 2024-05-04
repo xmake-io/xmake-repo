8fd663541795dc9478979edb3cc49010d156031f
c8ef <c8ef@outlook.com>
2024-05-04 14:18:13 +0800

    Update xmake.lua

--- packages/x/xsimd/xmake.lua
+++ packages/x/xsimd/xmake.lua
@@ -30,30 +30,11 @@ package("xsimd")
     on_test(function (package)
         if package:version():ge("8.0") then
             assert(package:check_cxxsnippets({test = [[
-                #include <cstddef>
-                #include <vector>
                 #include "xsimd/xsimd.hpp"
-                
-                namespace xs = xsimd;
-                using vector_type = std::vector<double, xsimd::aligned_allocator<double>>;
-                
-                void mean(const vector_type& a, const vector_type& b, vector_type& res)
+
+                xsimd::batch<int> come_and_get_some(xsimd::batch<int> x, xsimd::batch<int> y)
                 {
-                    std::size_t size = a.size();
-                    constexpr std::size_t simd_size = xsimd::simd_type<double>::size;
-                    std::size_t vec_size = size - size % simd_size;
-                
-                    for(std::size_t i = 0; i < vec_size; i += simd_size)
-                    {
-                        auto ba = xs::load_aligned(&a[i]);
-                        auto bb = xs::load_aligned(&b[i]);
-                        auto bres = (ba + bb) / 2.;
-                        bres.store_aligned(&res[i]);
-                    }
-                    for(std::size_t i = vec_size; i < size; ++i)
-                    {
-                        res[i] = (a[i] + b[i]) / 2.;
-                    }
+                    return x + y;
                 }
                 void test() {}
             ]]}, {configs = {languages = "c++14"}}))
