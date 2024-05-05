9087104f174959776cd89955a118d3e31664e96c
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-05-05 08:14:06 +0200

    add zmqpb v0.8 & v0.9, add zeromq brew source (#3712)
    * add zmqpb v0.5
    also use c++17
    
    * zeromq isn't supported on arm, so zmqpb isn't either
    
    * make shared config
    
    * changed sha
    
    * arm zeromq
    - provided by brew
    
    * add utf8_range dep
    adjust sha of tar.gz
    
    * windows arm is apparently not supported by protoc, which this needs
    
    * new 0.5 release
    add abseil dep
    - needed to compile protobuf apparently
    plain windows
    - instead of excluding arm
    - same as deps
      - even though protobuf doesn't find protoc.exe on arm
    
    * updated sha
    
    * remove config and add configs var
    
    * i somehow missed a space
    
    * add zmqpb v0.6
    
    * abseil is listed as dep of protobuf
    
    * only add v0.8
    
    * add v0.9, with default requireconfs
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/z/zmqpb/xmake.lua
+++ packages/z/zmqpb/xmake.lua
@@ -9,9 +9,13 @@ package("zmqpb")
     add_versions("0.2", "5dfa4d4cebb10cb7ae03943e18e8d48c8ff215e80371f24c5ade212be7f20721")
     add_versions("0.3", "343c57c9f72facca47082422a259ec8c531f5c6e332a3828835080c4a96b9064")
     add_versions("0.4", "7c0001db73b19e65b007adf6c9c5092c3589f043ab3e95a16b3ec2b2a87fa244")
+    add_versions("0.8", "93433dfe60b09add321d5f6fd467724409929211010963ad63be6c68494446ed")
+    add_versions("0.9", "c4192777fd7d62b3624a6389efea68a772c4f2820c3d85128961c3dd5ee94a67")
 
     add_deps("cppzmq")
     add_deps("protobuf-cpp")
+    -- protobuf needs it and somehow just doesn't publicizes the linkage
+    add_deps( "utf8_range" )
 
     on_load("windows", "macosx", "linux", function (package)
         if not package:gitref() and package:version():lt("0.3") then
@@ -19,7 +23,7 @@ package("zmqpb")
         end
     end)
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install("windows|native", "macosx", "linux", function (package)
         local configs = {}
         import("package.tools.xmake").install(package, configs)
     end)
@@ -31,7 +35,7 @@ package("zmqpb")
                     ZmqPb::ReqRep network( "tcp://127.0.0.1:13337", false );
                     network.run();
                 }
-            ]]}, {configs = {languages = "c++14"}, includes = "zmqPb/reqRep.hpp"}))
+            ]]}, {configs = {languages = "c++17"}, includes = "zmqPb/reqRep.hpp"}))
         else
             assert(package:check_cxxsnippets({test = [[
                 void test() {
