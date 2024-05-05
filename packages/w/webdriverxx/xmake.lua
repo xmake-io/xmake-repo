9573cc2b3a37f82e2445103ee90147c3454815cb
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-05-06 00:01:37 +0100

    webdriverxx: use fork instead & exclude wasm

--- packages/w/webdriverxx/xmake.lua
+++ packages/w/webdriverxx/xmake.lua
@@ -1,17 +1,17 @@
 package("webdriverxx")
-    set_homepage("https://github.com/sekogan/webdriverxx")
+    set_homepage("https://github.com/durdyev/webdriverxx")
     set_description("A C++ client library for Selenium Webdriver")
     set_license("MIT")
 
-    add_urls("https://github.com/sekogan/webdriverxx.git")
-    add_versions("2018.03.25", "985d5699e663eeb8fd51cb8af8c40be059364c1a")
+    add_urls("https://github.com/durdyev/webdriverxx.git")
+    add_versions("2018.02.22", "11c4addbb3f791c3d59aecfee2354ba49612d5ca")
 
-    add_patches("2018.03.25", "patches/2018.03.25/add_phantom.patch", "d84fc74252b395795ab45ebf71495c77a0f246fb1484096145252a8fd1931cda")
-    add_patches("2018.03.25", "patches/2018.03.25/fix_firefox.patch", "f492040c0aab9582a3bc455d8fe47aa34c74ee30b926e3595d34ef2852b6aed4")
+    add_patches("2018.03.25", "patches/2018.03.25/fix_picojson_dep.patch", "e3eaacaad4df12429694efba597423bca4083b55f371ced3df2af1f5f009737b")
 
     add_deps("libcurl", "picojson")
 
-    on_install(function (package)
+    on_install("!wasm", function (package)
+        os.rm("include/picojson.h")
         os.cp("include", package:installdir())
     end)
 
