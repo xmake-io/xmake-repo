7408cbb060843258689e3225ebf5b10870c3a01a
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-05-06 15:46:22 +0100

    nmd: add package

---
+++ packages/n/nmd/xmake.lua
@@ -0,0 +1,20 @@
+package("nmd")
+    set_homepage("https://github.com/Nomade040/nmd")
+    set_description("set of single-header libraries for C/C++. The code is far from finished but some parts are quite usable.")
+
+    add_urls("https://github.com/Nomade040/nmd.git")
+    add_versions("2021.03.28", "33ac3b62c7d1eb28ae6b71d4dd78aa133ef96488")
+
+    on_install(function (package)
+        os.cp("nmd_assembly.h", package:installdir("include/nmd"))
+        os.cp("nmd_graphics.h", package:installdir("include/nmd"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <nmd/nmd_assembly.h>
+            void test() {
+                nmd_x86_instruction instruction;
+            }
+        ]]}, {configs = {languages = "c89"}}))
+    end)
