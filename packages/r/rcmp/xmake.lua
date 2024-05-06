193246fae77bef37563774fbae885045a0b532f9
ruki <waruqi@gmail.com>
2024-05-07 00:41:41 +0800

    install headers

--- packages/r/rcmp/xmake.lua
+++ packages/r/rcmp/xmake.lua
@@ -15,6 +15,7 @@ package("rcmp")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
+        os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
