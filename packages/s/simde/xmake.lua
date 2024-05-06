939f017ca2114ed551cd628e36f2d76b1ad3b993
ruki <waruqi@gmail.com>
2024-05-06 15:05:13 +0800

    Auto-update simde to 0.8.2 (#3956)
    * Update simde to 0.8.2
    
    * limit plat
    
    ---------
    
    Co-authored-by: c8ef <c8ef@outlook.com>

--- packages/s/simde/xmake.lua
+++ packages/s/simde/xmake.lua
@@ -6,9 +6,10 @@ package("simde")
 
     set_urls("https://github.com/simd-everywhere/simde/releases/download/v$(version)/simde-amalgamated-$(version).tar.xz")
 
+    add_versions("0.8.2", "59068edc3420e75c5ff85ecfd80a77196fb3a151227a666cc20abb313a5360bf")
     add_versions("0.7.2", "544c8aac764f0e24e444b1a7842d0314fa0231802d3b1b2020a03677b5be6142")
 
-    on_install(function (package)
+    on_install("windows|x86", "windows|x64", "macosx", "linux", "mingw", "iphoneos", "bsd", "wasm", function (package)
         os.cp("*", package:installdir("include"))
     end)
 
