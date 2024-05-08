097da8a2a0f564b8c673a550a4038b90ce27a7ab
0.382 <18322825326@163.com>
2024-05-08 09:13:17 +0800

    improve make

--- packages/w/wigxjpf/xmake.lua
+++ packages/w/wigxjpf/xmake.lua
@@ -7,7 +7,8 @@ package("wigxjpf")
     add_versions("1.13", "90ab9bfd495978ad1fdcbb436e274d6f4586184ae290b99920e5c978d64b3e6a")
 
     on_install("linux", "macosx", function (package)
-        os.vrun("make")
+        local configs = {}
+        import("package.tools.make").build(package, configs)
         os.cp("inc/*.h", package:installdir("include"))
         os.cp("lib/*.a", package:installdir("lib"))
         os.cp("bin/*", package:installdir("bin"))
