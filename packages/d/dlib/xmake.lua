c31336b7cc20d8c371f71464d23e9d16e0a7dcd9
star9029 <hengxings783@gmail.com>
2024-05-08 22:48:55 +0800

    fix windows shared

--- packages/d/dlib/xmake.lua
+++ packages/d/dlib/xmake.lua
@@ -13,10 +13,9 @@ package("dlib")
     add_configs("jpg", {description = "Enable jpg", default = false, type = "boolean"})
     add_configs("gif", {description = "Enable gif", default = false, type = "boolean"})
     add_configs("sqlite3", {description = "Enable sqlite3", default = false, type = "boolean"})
+    add_configs("blas", {description = "Enable blas", default = false, type = "boolean"})
+    add_configs("mkl", {description = "Enable mkl", default = false, type = "boolean"})
     add_configs("webp", {description = "Enable webp", default = false, type = "boolean"})
-    if is_plat("windows") then
-        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-    end
 
     if is_plat("linux", "bsd") then
         add_syslinks("pthread", "nsl")
@@ -37,6 +36,12 @@ package("dlib")
         if package:config("sqlite3") then
             package:add("deps", "sqlite3")
         end
+        if package:config("blas") then
+            package:add("deps", "openblas")
+        end
+        if package:config("mkl") then
+            package:add("deps", "mkl")
+        end
         if package:version():ge("19.24") and package:config("webp") then
             package:add("deps", "libwebp")
         end
@@ -48,8 +53,14 @@ package("dlib")
             "-DDLIB_ISO_CPP_ONLY=OFF",
             "-DDLIB_NO_GUI_SUPPORT=ON"
         }
-        if package:is_debug() then
-            table.insert(configs, "-DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=''")
+        if package:is_plat("windows") then
+            if package:is_debug() then
+                table.insert(configs, "-DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=''")
+            end
+            if package:config("shared") then
+                table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+                io.replace("dlib/CMakeLists.txt", [[message(FATAL_ERROR "Building dlib as a standalone dll is not supported when using Visual Studio.  You are highly encouraged to use static linking instead.  See https://github.com/davisking/dlib/issues/1483 for a discussion.")]], "", {plain = true})
+            end
         end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
@@ -58,11 +69,13 @@ package("dlib")
         table.insert(configs, "-DDLIB_JPEG_SUPPORT=" .. (package:config("jpg") and "ON" or "OFF"))
         table.insert(configs, "-DDLIB_GIF_SUPPORT=" .. (package:config("jpg") and "ON" or "OFF"))
         table.insert(configs, "-DDLIB_LINK_WITH_SQLITE3=" .. (package:config("sqlite3") and "ON" or "OFF"))
+        table.insert(configs, "-DDLIB_LINK_WITH_SQLITE3=" .. (package:config("blas") and "ON" or "OFF"))
+        table.insert(configs, "-DDLIB_LINK_WITH_SQLITE3=" .. (package:config("mkl") and "ON" or "OFF"))
         table.insert(configs, "-DDLIB_WEBP_SUPPORT=" .. (package:config("webp") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:has_cxxtypes("dlib::command_line_parser", {
-            includes = "dlib/cmd_line_parser.h", configs = {languages = "c++11"}}))
+            includes = "dlib/cmd_line_parser.h", configs = {languages = "c++14"}}))
     end)
