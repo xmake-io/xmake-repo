fbd5a3eaf64d906482160f713930b7f0ff011001
star9029 <hengxings783@gmail.com>
2024-05-11 01:00:21 +0800

    fix semver

--- packages/c/cglm/check.lua
+++ packages/c/cglm/check.lua
@@ -3,7 +3,7 @@ import("core.base.semver")
 function main(argv)
     local vs_sdkver = argv.vs_sdkver
     if vs_sdkver then
-        vs_sdkver = semver.new(vs_sdkver)
+        vs_sdkver = semver.match(vs_sdkver)
         if vs_sdkver:le("10.0.17763") then
             wprint(format("vs_sdkver <= 10.0.17763.0, skip package(%s) test", path.basename(os.scriptdir())))
             return false
