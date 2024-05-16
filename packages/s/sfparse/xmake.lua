dee78122273646fdecfa53044f0e4c546f4bd89b
star9029 <hengxings783@gmail.com>
2024-05-16 23:22:16 +0800

    Revert "improve sfparse includedirs"
    This reverts commit 6248342ec5e029659817f9793d6464e6a9313d32.

--- packages/s/sfparse/xmake.lua
+++ packages/s/sfparse/xmake.lua
@@ -1,12 +1,11 @@
 package("sfparse")
+
     set_homepage("https://github.com/ngtcp2/sfparse")
     set_description("Structured Field Values parser")
     set_license("MIT")
 
-    set_urls("https://github.com/ngtcp2/sfparse.git", {submodules = false})
+    set_urls("https://github.com/ngtcp2/sfparse.git")
     add_versions("2024.05.12", "c669673012f9d535ec3bcf679fe911c8c75a479f")
-
-    add_includedirs("include", "include/sfparse")
 
     on_install(function (package)
         io.writefile("xmake.lua", [[
@@ -14,12 +13,16 @@ package("sfparse")
             target("sfparse")
                 set_kind("$(kind)")
                 add_files("sfparse.c")
-                add_headerfiles("sfparse.h", {prefixdir = "sfparse"})
+                add_headerfiles("sfparse.h")
                 if is_plat("windows") and is_kind("shared") then
-                    add_rules("utils.symbols.export_all")
+                    add_rules("utils.symbols.export_all", {export_classes = true})
                 end
         ]])
-        import("package.tools.xmake").install(package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
     end)
 
     on_test(function (package)
