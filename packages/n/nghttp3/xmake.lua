5b6cacf7be801fa13df733ccee12deb9bf9121db
c8ef <c8ef@outlook.com>
2024-05-16 15:35:08 +0800

    vendor sfparse

--- packages/n/nghttp3/xmake.lua
+++ packages/n/nghttp3/xmake.lua
@@ -3,8 +3,11 @@ package("nghttp3")
     set_description("HTTP/3 library written in C")
     set_license("MIT")
 
-    add_urls("https://github.com/ngtcp2/nghttp3.git")
-    add_versions("v1.3.0", "e4f96f9572e4fc27324b4a009d877a4a2bbacc4e")
+    add_urls("https://github.com/ngtcp2/nghttp3/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/ngtcp2/nghttp3.git")
+    add_versions("v1.3.0", "a83c6a4f589ae777a5f967652969d99b3399a85971340b8de9bed79119a11f88")
+
+    add_patches("v1.3.0", "patches/vendor.patch", "51ab785328270b8df854283a8c20403c09813b0586eb84702a9c20241ff14980")
 
     add_deps("cmake")
 
@@ -15,14 +18,16 @@ package("nghttp3")
     end)
 
     on_install("windows", "macosx", "linux", function (package)
-        local configs = {"-DENABLE_LIB_ONLY=ON"}
+        io.replace("lib/nghttp3_http.c", "sfparse/sfparse.h", "sfparse.h", {plain = true})
+
+        local configs = {"-DENABLE_LIB_ONLY=ON", "-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DENABLE_SHARED_LIB=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DENABLE_STATIC_LIB=" .. (package:config("shared") and "OFF" or "ON"))
         if package:is_plat("windows") then
             table.insert(configs, "-DENABLE_STATIC_CRT=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
         end
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.cmake").install(package, configs, {packagedeps = "sfparse"})
     end)
 
     on_test(function (package)
