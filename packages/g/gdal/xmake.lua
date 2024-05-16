e59bfaf2fda142495e378ec759fb125e544d141c
zjyhjqs <zjyhjqs@Hotmail.com>
2024-05-16 23:40:24 +0800

    update `GDAL` to 3.8.5 (since 3.9 c++17 is required)

--- packages/g/gdal/xmake.lua
+++ packages/g/gdal/xmake.lua
@@ -4,6 +4,7 @@ package("gdal")
     set_license("MIT")
 
     add_urls("https://github.com/OSGeo/gdal/releases/download/v$(version)/gdal-$(version).tar.gz")
+    add_versions("3.8.5", "0c865c7931c7e9bb4832f50fb53aec8676cbbaccd6e55945011b737fb89a49c2")
     add_versions("3.5.1", "7c4406ca010dc8632703a0a326f39e9db25d9f1f6ebaaeca64a963e3fac123d1")
 
     add_configs("apps", {description = "Build GDAL applications.", default = false, type = "boolean"})
@@ -12,14 +13,14 @@ package("gdal")
     if is_plat("windows") then
         add_syslinks("wsock32", "ws2_32")
     end
- 
+
     on_install("windows|x86", "windows|x64", "macosx", "linux", function (package)
         local configs = {"-DBUILD_TESTING=OFF", "-DGDAL_USE_EXTERNAL_LIBS=OFF", "-DGDAL_USE_OPENJPEG=ON",
                          "-DBUILD_JAVA_BINDINGS=OFF", "-DBUILD_CSHARP_BINDINGS=OFF", "-DBUILD_PYTHON_BINDINGS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DBUILD_APPS=" .. (package:config("apps") and "ON" or "OFF"))
-        
+
         --fix gdal compile on msvc debug mode
         local cxflags
         if package:debug() and package:is_plat("windows") then
