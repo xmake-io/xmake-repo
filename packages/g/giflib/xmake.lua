35ab7d60bed7df9a14e8765bd4399aa91087bb66
star9029 <hengxings783@gmail.com>
2024-05-22 23:20:10 +0800

    add 5.2.2

--- packages/g/giflib/xmake.lua
+++ packages/g/giflib/xmake.lua
@@ -6,9 +6,10 @@ package("giflib")
     add_urls("https://github.com/xmake-mirror/giflib/releases/download/$(version)/giflib-$(version).tar.gz",
              "https://downloads.sourceforge.net/project/giflib/giflib-$(version).tar.gz")
 
+    add_versions("5.2.2", "be7ffbd057cadebe2aa144542fd90c6838c6a083b5e8a9048b8ee3b66b29d5fb")
     add_versions("5.2.1", "31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd")
 
-    add_patches(">=5.2.1", "patches/5.2.1/unistd.h.patch", "0f1f7b9a295c9965529608f7e9d810b7f7a2642b3b18910402d5d7ce8b86fc13")
+    add_patches("5.2.1", "patches/5.2.1/unistd.h.patch", "0f1f7b9a295c9965529608f7e9d810b7f7a2642b3b18910402d5d7ce8b86fc13")
 
     add_configs("utils", {description = "Build utility binaries.", default = true, type = "boolean"})
 
@@ -20,6 +21,13 @@ package("giflib")
 
     on_install(function (package)
         local utils = package:config("utils")
+        if utils and package:is_plat("windows") then
+            -- fix unresolved external symbol snprintf before vs2013
+            for _, file in ipairs({"gif2rgb.c", "gifbuild.c", "gifclrmp.c", "giffix.c", "giftext.c", "giftool.c"}) do
+                io.replace(file, "snprintf", "_snprintf", {plain = true})
+            end
+        end
+
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         import("package.tools.xmake").install(package, {utils = utils})
         if utils then
