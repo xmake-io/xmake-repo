7aedb06a62911afb1996408cbf3887c671f67471
SanQi <1459998132@.qq.com>
2024-05-26 15:32:52 +0800

    1. add on_test check function 2. modify link sort 3. move add_includedirs position when platform is windows

--- packages/c/crashpad/xmake.lua
+++ packages/c/crashpad/xmake.lua
@@ -22,19 +22,21 @@ package("crashpad")
             add_versions("2021.8.1", "699fdf741f39da1c68069820ce891b6eb8b48ef29ab399fc1bcf210b67ff8547")
             add_versions("2022.4.16", "c3bffb64d1087198946739dfb30d24b2355e49ddfe90d8e2a75ed373ed6e3377")
         end
+        add_includedirs("include", "include/mini_chromium")
     end
 
-    add_includedirs("include", "include/mini_chromium")
-
-
-
+    
+    
     if is_host("linux") then
         local version = "v2024.05.26"
         if is_arch("x64", "x86_64") then
             set_urls("https://github.com/sanqideshi/xmake-repo/releases/download/$(version)/crashpad.tar.gz")
             add_versions("v2024.05.26", "239122ad8c747dc6e12e3636cfc7885ee5f997b10e472a0ee27d65b7777ff8f0")
         end
+        add_includedirs("include","/include/third_party/mini_chromium/mini_chromium","include/out/Default/gen")
+        add_links("common","client","util","base")
     end
+    
 
     on_install("windows", function (package)
         os.cp("include/*", package:installdir("include"))
@@ -44,10 +46,31 @@ package("crashpad")
         else
             os.cp("lib_mt/*", package:installdir("lib"))
         end
-     end)
+    end)
 
-     on_install("linux", function (package)
+    
+    on_install("linux", function (package)
         os.cp("include/*", package:installdir("include"))
         os.cp("bin/*", package:installdir("bin"))
         os.cp("lib/*", package:installdir("lib"))
-     end)
+        package:addenv("PATH", "bin")
+    end)
+
+    on_test(function (package)
+        if is_host("linux") then
+            os.runv("crashpad_handler --help")
+            
+            assert(package:check_cxxsnippets({test = [[
+                #include <stdio.h>
+                #include <unistd.h>
+                #include <string.h>
+                #include "client/crashpad_client.h"
+                #include "client/crash_report_database.h"
+                #include "client/settings.h"
+                using namespace crashpad;
+                void test() {
+                    CrashpadClient *client = new CrashpadClient();
+                }
+            ]]}, {configs = {languages = "cxx17"}}))
+        end
+    end)
