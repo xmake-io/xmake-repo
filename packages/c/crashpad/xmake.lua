5ba244944890474c81f39cbccb536f67bd9dc2dd
star9029 <hengxings783@gmail.com>
2024-05-31 18:59:20 +0800

    Update xmake.lua

--- packages/c/crashpad/xmake.lua
+++ packages/c/crashpad/xmake.lua
@@ -5,23 +5,26 @@ package("crashpad")
 
     add_urls("https://github.com/getsentry/crashpad.git")
     add_versions("2024.04.15", "96e301b7d6b81990a244d7de41a0d36eeb60899e")
-    add_deps("libcurl")
-
-    on_install("linux", "windows|x64", "windows|x86", function(package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {
-            packagedeps = {"libcurl"}
-        })
-        package:addenv("PATH", "bin")
-    end)
 
     add_includedirs("include/crashpad", "include/crashpad/mini_chromium")
     add_links("crashpad_client", "crashpad_util", "mini_chromium")
 
+    add_deps("cmake")
+    add_deps("libcurl")
+
+    on_install("linux", "windows|x64", "windows|x86", function(package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs, {packagedeps = "libcurl"})
+        package:addenv("PATH", "bin")
+    end)
+
     on_test(function(package)
-        os.vrunv("crashpad_handler" .. (package:is_plat("windows") and ".exe" or ""), {"--help"})
+        if not package:is_cross() then
+            os.vrun("crashpad_handler --help")
+        end
+
         assert(package:check_cxxsnippets({test = [[
             #include "client/crashpad_client.h"
             using namespace crashpad;
