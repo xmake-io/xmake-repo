e940109f5a55d15a17c277da812f059bc8f005fa
SanQi <1459998132@.qq.com>
2024-06-02 16:50:19 +0800

    edit

--- packages/v/vcpkg/xmake.lua
+++ packages/v/vcpkg/xmake.lua
@@ -6,9 +6,13 @@ package("vcpkg")
     add_urls("https://github.com/microsoft/vcpkg/archive/refs/tags/$(version).tar.gz")
     add_versions("2024.05.24", "3034e534d4ed13e6e6edad3c331c0e9e3280f579dd4ba86151aa1e2896b85d31")
 
-    add_deps("unzip","libzip","cmake","ninja","libcurl")
+    add_deps("unzip","cmake","ninja","libcurl")
     on_install("linux", "windows|x64", "windows|x86", function(package)
        
+        if linuxos.name == "archlinux" or linuxos.name == "manjaro" then
+            add_deps("pacman::zip","pacman::curl")
+        end
+
         local scriptpath = path.join(".",package:is_plat("linux") and "bootstrap-vcpkg.sh" or "bootstrap-vcpkg.bat")
         local exepath = path.join(".",package:is_plat("linux") and "vcpkg" or "vcpkg.exe")
         print("scriptpath:" .. scriptpath)
