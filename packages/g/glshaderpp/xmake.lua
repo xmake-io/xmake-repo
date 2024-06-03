ac93ad4e19c7e4f085679194936228fd4acf6852
star9029 <hengxings783@gmail.com>
2024-06-03 23:47:03 +0800

    glshaderpp: add package

---
+++ packages/g/glshaderpp/xmake.lua
@@ -0,0 +1,18 @@
+package("glshaderpp")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://gitlab-lepuy.iut.uca.fr/opengl/glshaderpp")
+    set_description("A lightweight header-only library to compile and link OpenGL GLSL shaders.")
+    set_license("LGPL-3.0-or-later")
+
+    add_urls("https://gitlab-lepuy.iut.uca.fr/opengl/glshaderpp/-/archive/$(version)/glshaderpp-$(version).tar.bz2",
+             "https://gitlab-lepuy.iut.uca.fr/opengl/glshaderpp.git")
+
+    add_versions("v1.0.0", "81b47b90e90d8be19d0421d67f4fc735d74d285a5f516b99ee7dc49d7933ecf6")
+
+    on_install(function (package)
+        os.cp("GLShaderPP/public/GLShaderPP", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxtypes("GLShaderPP::CShaderException", {configs = {languages = "c++17"}, includes = "GLShaderPP/ShaderException.h"}))
+    end)
