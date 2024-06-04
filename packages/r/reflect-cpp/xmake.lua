207f8ceb5d769652ea68c5386b669b36ab1283f3
Sean <pengshuhut@outlook.com>
2024-06-04 22:41:37 +0800

    Update xmake.lua

--- packages/r/reflect-cpp/xmake.lua
+++ packages/r/reflect-cpp/xmake.lua
@@ -10,21 +10,18 @@ package("reflect-cpp")
 
     add_versions("v0.9.0", "a64ad16da970da7d66d71a134312c7d0b7de2f4e1448b83d3ea92130dfe0449c")
 
-    add_configs("json", { description = "OnEnable Json Support.", default = true, type = "boolean"})
     --add_configs("with_bson", { description = "OnEnable Bson Support.", default = false, type = "boolean"})
-    add_configs("cbor", { description = "OnEnable Cbor Support.", default = false, type = "boolean"})
+    add_configs("cbor", { description = "OnEnable Cbor Support.", default = true, type = "boolean"})
     add_configs("flatbuffers", { description = "OnEnable Flexbuffers Support.", default = false, type = "boolean"})
     add_configs("msgpack", { description = "OnEnable Msgpack Support.", default = false, type = "boolean"})
-    add_configs("xml", { description = "OnEnable Xml Support.", default = false, type = "boolean"})
+    add_configs("xml", { description = "OnEnable Xml Support.", default = true, type = "boolean"})
     add_configs("toml", { description = "OnEnable Toml Support.", default = false, type = "boolean"})
     add_configs("yaml", { description = "OnEnable Yaml Support.", default = false, type = "boolean"})
 
-    add_deps("cmake")
+    add_deps({"cmake", "yyjson"})
 
     on_load(function (package)
-        if package:config("json") then
-            package:add("deps", "yyjson")
-        end
+        package:add("links", "yyjson")
 
         if package:config("cbor") then
             package:add("deps", "tinycbor")
@@ -37,7 +34,7 @@ package("reflect-cpp")
         if package:config("msgpack") then
             package:add("deps", "msgpack-c")
         end
-        
+
         if package:config("xml") then
             package:add("deps", "pugixml")
         end
@@ -48,9 +45,9 @@ package("reflect-cpp")
 
         if package:config("yaml") then
             package:add("deps", "yaml-cpp")
-        end          
+        end 
+        
     end)
-
 
     on_install(function (package)
          os.cp("include", package:installdir())
@@ -60,17 +57,14 @@ package("reflect-cpp")
         assert(package:check_cxxsnippets({test = [[
             #include <rfl/json.hpp>
             #include <rfl.hpp>
-
             struct Person {
                 std::string first_name;
                 std::string last_name;
                 int age;
             };
-
             const auto homer = Person{.first_name = "Homer",
                                       .last_name = "Simpson",
                                       .age = 45};
-
             void test(int argc, char** argv)
             {
                 const std::string json_string = rfl::json::write(homer);
@@ -78,3 +72,4 @@ package("reflect-cpp")
             }   
         ]]}, {includes = {"rfl.hpp", "rfl/json.hpp"}, configs = {languages = "c++20"}}))
     end)
+
