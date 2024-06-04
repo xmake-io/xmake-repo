c1d56677a2287f09668ef21b91c85d3c2d4a8c29
star9029 <hengxings783@gmail.com>
2024-06-04 19:26:17 +0800

    add dep

--- packages/g/glshaderpp/xmake.lua
+++ packages/g/glshaderpp/xmake.lua
@@ -9,10 +9,22 @@ package("glshaderpp")
 
     add_versions("v1.0.0", "81b47b90e90d8be19d0421d67f4fc735d74d285a5f516b99ee7dc49d7933ecf6")
 
-    on_install(function (package)
+    add_deps("glew")
+
+    on_install("linux", "macosx", "mingw", "windows", function (package)
         os.cp("GLShaderPP/public/GLShaderPP", package:installdir("include"))
     end)
 
     on_test(function (package)
-        assert(package:has_cxxtypes("GLShaderPP::CShaderException", {configs = {languages = "c++17"}, includes = "GLShaderPP/ShaderException.h"}))
+        assert(package:check_cxxsnippets({test = [[
+            #include <GL/glew.h>
+            #include <GLShaderPP/Shader.h>
+            #include <GLShaderPP/ShaderException.h>
+            #include <GLShaderPP/ShaderProgram.h>
+
+            void test() {
+                GLShaderPP::CShaderException e("If you read this, GLShaderPP is happy :)",
+                                            GLShaderPP::CShaderException::ExceptionType::LinkError);
+            }
+        ]]}, {configs = {languages = "c++17"}}))
     end)
