9b81a35e5c2067599e0d565074f757331c51bb8f
Sean <pengshuhut@outlook.com>
2024-06-05 18:32:14 +0800

    remove unuseful code & fix add_urls

--- packages/q/quirc/xmake.lua
+++ packages/q/quirc/xmake.lua
@@ -3,12 +3,11 @@ package("quirc")
     set_description("QR decoder library")
     set_license("ISC")
 
-    add_urls("https://github.com/dlbeer/quirc/archive/refs/tags/v1.2.tar.gz",
+    add_urls("https://github.com/dlbeer/quirc/archive/refs/tags/$(version).tar.gz",
              "https://github.com/dlbeer/quirc.git")
     add_versions("v1.2", "73c12ea33d337ec38fb81218c7674f57dba7ec0570bddd5c7f7a977c0deb64c5")
 
     on_install(function (package)
-        local configs = {}
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
             target("quirc")
