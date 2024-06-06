383dc276b6ef6418727e16282dd5178066dc2fbc
Arteiimis <lionelym0987@gmail.com>
2024-06-06 21:02:46 +0800

    fix xmake.lua

--- packages/y/yalantinglibs/xmake.lua
+++ packages/y/yalantinglibs/xmake.lua
@@ -10,12 +10,12 @@ package("yalantinglibs")
 
     add_deps("cmake")
 
-    add_configs("ENABLE_SSL", {description = "Enable optional ssl support for rpc/http",  default = false, type = "boolean"})
-    add_configs("ENABLE_PMR", {description = "Enable pmr optimization",  default = false, type = "boolean"})
-    add_configs("ENABLE_IO_URING", {description = "Using io_uring as a backend on linux (instead of epoll)",  default = false, type = "boolean"})
-    add_configs("ENABLE_FILE_IO_URING	", {description = "Enable io_uring optimizations",  default = false, type = "boolean"})
-    add_configs("ENABLE_STRUCT_PACK_UNPORTABLE_TYPE", {description = "struct_pack enables support for special types that are not cross-platform (such as wstring, in128_t)",  default = false, type = "boolean"})
-    add_configs("ENABLE_STRUCT_PACK_OPTIMIZE", {description = "struct_pack enables aggressive template expansion optimization (will cost more compilation time)",  default = false, type = "boolean"})
+    add_configs("SSL", {description = "Enable optional ssl support for rpc/http",  default = false, type = "boolean"})
+    add_configs("PMR", {description = "Enable pmr optimization",  default = false, type = "boolean"})
+    add_configs("IO_URING", {description = "Using io_uring as a backend on linux (instead of epoll)",  default = false, type = "boolean"})
+    add_configs("FILE_IO_URING	", {description = "Enable io_uring optimizations",  default = false, type = "boolean"})
+    add_configs("STRUCT_PACK_UNPORTABLE_TYPE", {description = "struct_pack enables support for special types that are not cross-platform (such as wstring, in128_t)",  default = false, type = "boolean"})
+    add_configs("STRUCT_PACK_OPTIMIZE", {description = "struct_pack enables aggressive template expansion optimization (will cost more compilation time)",  default = false, type = "boolean"})
 
     on_install(function (package)
         import("core.tool.compiler")
@@ -30,12 +30,12 @@ package("yalantinglibs")
         table.insert(configs, "-DENABLE_CPP_20=" .. CPP20_ENABLE)
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_SSL=" .. (package:config("ENABLE_SSL") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_PMR=" .. (package:config("ENABLE_PMR") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_IO_URING=" .. (package:config("ENABLE_IO_URING") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_FILE_IO_URING=" .. (package:config("ENABLE_FILE_IO_URING") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_STRUCT_PACK_UNPORTABLE_TYPE=" .. (package:config("ENABLE_STRUCT_PACK_UNPORTABLE_TYPE") and "ON" or "OFF"))
-        table.insert(configs, "-DYLT_ENABLE_STRUCT_PACK_OPTIMIZE=" .. (package:config("ENABLE_STRUCT_PACK_OPTIMIZE") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_SSL=" .. (package:config("SSL") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_PMR=" .. (package:config("PMR") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_IO_URING=" .. (package:config("IO_URING") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_FILE_IO_URING=" .. (package:config("FILE_IO_URING") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_STRUCT_PACK_UNPORTABLE_TYPE=" .. (package:config("STRUCT_PACK_UNPORTABLE_TYPE") and "ON" or "OFF"))
+        table.insert(configs, "-DYLT_ENABLE_STRUCT_PACK_OPTIMIZE=" .. (package:config("STRUCT_PACK_OPTIMIZE") and "ON" or "OFF"))
         
         table.insert(configs, "-DBUILD_EXAMPLES=OFF")
         table.insert(configs, "-DBUILD_BENCHMARK=OFF")
@@ -59,18 +59,18 @@ package("yalantinglibs")
             #include "ylt/struct_json/json_writer.h"
 
             struct person {
-            std::string name;
-            int age;
+                std::string name;
+                int age;
             };
             REFLECTION(person, name, age);
 
             int main() {
-            person p{.name = "tom", .age = 20};
-            std::string str;
-            struct_json::to_json(p, str); // {"name":"tom","age":20}
+                person p{.name = "tom", .age = 20};
+                std::string str;
+                struct_json::to_json(p, str); // {"name":"tom","age":20}
 
-            person p1;
-            struct_json::from_json(p1, str);
+                person p1;
+                struct_json::from_json(p1, str);
             }
         ]]}, {configs = {languages = languages}}))
     end)
