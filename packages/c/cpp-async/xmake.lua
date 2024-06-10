9cd53f0a0f4782c27e0cca5103a556799642833a
star9029 <hengxings783@gmail.com>
2024-06-10 22:42:30 +0800

    Update xmake.lua

--- packages/c/cpp-async/xmake.lua
+++ packages/c/cpp-async/xmake.lua
@@ -4,10 +4,12 @@ package("cpp-async")
     set_description("Support types and functions for C++20 coroutines")
     set_license("MIT")
 
-    add_urls("https://github.com/microsoft/cpp-async.git")
-    add_versions("v1.1.0", "67907d193f4df024b057caef06ee67fbc0a799c7")
+    add_urls("https://github.com/microsoft/cpp-async/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/microsoft/cpp-async.git")
 
-    on_install("windows", "linux", "macosx", "mingw", "msys", "android", "iphoneos", "cross", function (package)
+    add_versions("v1.1.0", "168681dfd999b7546d623f20d18072ce280502cf0bf3a829c2aec58741062a23")
+
+    on_install(function (package)
         os.cp("include/async", package:installdir("include"))
     end)
 
@@ -15,7 +17,6 @@ package("cpp-async")
         assert(package:check_cxxsnippets({test = [[
             #include <async/task.h>
             #include <coroutine>
-
             inline async::task<void> do_async()
             {
                 co_return;
