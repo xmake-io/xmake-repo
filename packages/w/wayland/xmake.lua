f542968aa6853abb898fec027e4cb051229af5fb
Jérôme Leclercq <lynix680@gmail.com>
2024-06-11 11:01:45 +0200

    Update xmake.lua

--- packages/w/wayland/xmake.lua
+++ packages/w/wayland/xmake.lua
@@ -3,13 +3,14 @@ package("wayland")
     set_description("Wayland is a protocol for a compositor to talk to its clients as well as a C library implementation of that protocol.")
     set_license("MIT")
 
-    add_urls("https://wayland.freedesktop.org/releases/wayland-$(version).tar.xz")
---    add_versions("1.18.0", "4675a79f091020817a98fd0484e7208c8762242266967f55a67776936c2e294d")
---    add_versions("1.19.0", "baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15")
-    add_versions("1.23.0", "a00bb4f55e680d6681982b0fe2d64aab1e9569e47e21605f168b9af09b771742")
-    		
+    add_urls("https://gitlab.freedesktop.org/wayland/wayland/-/releases/$(version)/downloads/wayland-$(version).tar.xz")
+    add_versions("1.18.0", "4675a79f091020817a98fd0484e7208c8762242266967f55a67776936c2e294d")
+    add_versions("1.19.0", "baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15")
+    add_versions("1.23.0", "05b3e1574d3e67626b5974f862f36b5b427c7ceeb965cb36a4e6c2d342e45ab2")
+
     add_resources("1.18.0", "protocols", "https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz", "9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7")
     add_resources("1.19.0", "protocols", "https://wayland.freedesktop.org/releases/wayland-protocols-1.21.tar.xz", "b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5")
+    add_resources("1.23.0", "protocols", "https://wayland.freedesktop.org/releases/wayland-protocols-1.36.tar.xz", "71fd4de05e79f9a1ca559fac30c1f8365fa10346422f9fe795f74d77b9ef7e92")
 
     if is_plat("linux") then
         add_extsources("apt::libwayland-dev", "pacman::wayland")
