465294a435da3829a085074226bcdc4ef11b97b2
star9029 <hengxings783@gmail.com>
2024-06-12 00:45:59 +0800

    improve indent

--- packages/m/mlpack/xmake.lua
+++ packages/m/mlpack/xmake.lua
@@ -40,14 +40,14 @@ package("mlpack")
             #include <mlpack/methods/random_forest.hpp>
             using namespace mlpack;
             void test() {
-                    const size_t numClasses = 2;
-                    const size_t minimumLeafSize = 5;
-                    const size_t numTrees = 10;
+                const size_t numClasses = 2;
+                const size_t minimumLeafSize = 5;
+                const size_t numTrees = 10;
 
-                    arma::mat dataset;
-                    arma::Row<size_t> labels, predictions;
-                    RandomForest<GiniGain, RandomDimensionSelect> rf(dataset, labels, numClasses, numTrees, minimumLeafSize);
-                    rf.Classify(dataset, predictions);
+                arma::mat dataset;
+                arma::Row<size_t> labels, predictions;
+                RandomForest<GiniGain, RandomDimensionSelect> rf(dataset, labels, numClasses, numTrees, minimumLeafSize);
+                rf.Classify(dataset, predictions);
             }
         ]]}, {configs = {languages = "cxx17"}}))
     end)
