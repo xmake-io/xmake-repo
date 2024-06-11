4ac6ffeadead6690a5bbfa3e7c34cdab2ba49b9e
Altina Orion <139891033+Altina-oz@users.noreply.github.com>
2024-06-11 12:18:53 +0800

    minor formatting refinement

--- packages/o/onedpl/fetch.lua
+++ packages/o/onedpl/fetch.lua
@@ -10,13 +10,13 @@ function _find_package(package, opt)
     local result = {links = {}, linkdirs = {}, includedirs = {}}
 
     -- find include
-    local includepath = find_path(path.join("oneapi","dpl","algorithm"), paths, {suffixes = "include"})
+    local includepath = find_path(path.join("oneapi", "dpl", "algorithm"), paths, {suffixes = "include"})
     if includepath then
         table.insert(result.includedirs, includepath)
     end
 	
     if #result.includedirs > 0  then
-        local version_file = path.join(includepath, "oneapi","dpl","pstl","onedpl_config.h")
+        local version_file = path.join(includepath, "oneapi", "dpl", "pstl", "onedpl_config.h")
         if os.isfile(version_file) then
             local content = io.readfile(version_file)
             local major = content:match("ONEDPL_VERSION_MAJOR +(%d+)\n")
