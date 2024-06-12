4e3bccc01938c321b168a55d81e06da3cf8a3d1c
c8ef <c8ef@outlook.com>
2024-06-12 10:15:29 +0800

    Update xmake.lua

--- packages/l/liboqs/xmake.lua
+++ packages/l/liboqs/xmake.lua
@@ -9,8 +9,8 @@ package("liboqs")
 
     add_deps("cmake")
 
-    on_install(function (package)
-        local configs = {"-DOQS_BUILD_ONLY_LIB=ON"}
+    on_install("windows", "linux", "macosx", function (package)
+        local configs = {"-DOQS_BUILD_ONLY_LIB=ON", "-DOQS_USE_OPENSSL=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
