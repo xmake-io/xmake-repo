2299b7abfc6f854db837313f3f23f3055aca69da
Jérôme Leclercq <lynix680@gmail.com>
2024-06-12 15:02:30 +0200

    remove test code

--- packages/w/wayland/xmake.lua
+++ packages/w/wayland/xmake.lua
@@ -50,23 +50,7 @@ package("wayland")
         local buildfile = find_file("meson.build", path.join(protocol, "**"))
         if buildfile then
             os.cd(path.directory(buildfile))
-            try
-            {
-                function ()
-                    meson.install(package, {"-Dtests=false"}, {envs = envs})
-                end,
-                catch
-                {
-                    function (errs)
-                        print("catching error")
-                        print("reading logs...")
-                        local logs = find_file("meson-log.txt", "**")
-                        print("found logs at " .. logs)
-                        print(io.readfile(logs))
-                        os.raise(errs)
-                    end
-                }
-            }
+            meson.install(package, {"-Dtests=false"}, {envs = envs})
         else
             import("package.tools.autoconf")
             
