09290ea3393e78df6e12befef757832cb9a4fd2c
c8ef <c8ef@outlook.com>
2024-06-15 17:37:22 +0800

    Update test

--- packages/w/wangle/xmake.lua
+++ packages/w/wangle/xmake.lua
@@ -29,7 +29,7 @@ package("wangle")
         assert(package:check_cxxsnippets({test = [[
             #include "wangle/util/FilePoller.h"
             void test() {
-                FilePoller poller(1);
+                wangle::FilePoller poller(1);
             }
         ]]}, {configs = {languages = "c++17"}}))
     end)
