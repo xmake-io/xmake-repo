69a70b3f4e20ef2e203bc9bc57b08c64ea125d59
star9029 <hengxings783@gmail.com>
2024-06-17 21:10:29 +0800

    quickjs-ng: add package

---
+++ packages/q/quickjs-ng/xmake.lua
@@ -0,0 +1,30 @@
+package("quickjs-ng")
+    set_homepage("https://github.com/quickjs-ng/quickjs")
+    set_description("QuickJS, the Next Generation: a mighty JavaScript engine")
+    set_license("MIT")
+
+    set_urls("https://github.com/quickjs-ng/quickjs/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/quickjs-ng/quickjs.git")
+
+    add_versions("v0.5.0", "41212a6fb84bfe07d61772c02513734b7a06465843ba8f76f1ce1e5df866f489")
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("m")
+    end
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCONFIG_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
+        if package:config("shared") and package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+        end
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("JS_NewRuntime", {includes = "quickjs.h"}))
+    end)
