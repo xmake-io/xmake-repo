b64cfff2c1893bba8bc1167009b17477049d2506
star9029 <hengxings783@gmail.com>
2024-06-17 22:31:29 +0800

    fix bsd pthread

--- packages/q/quickjs-ng/xmake.lua
+++ packages/q/quickjs-ng/xmake.lua
@@ -3,25 +3,26 @@ package("quickjs-ng")
     set_description("QuickJS, the Next Generation: a mighty JavaScript engine")
     set_license("MIT")
 
-    set_urls("https://github.com/quickjs-ng/quickjs/archive/refs/tags/$(version).tar.gz",
+    add_urls("https://github.com/quickjs-ng/quickjs/archive/refs/tags/$(version).tar.gz",
              "https://github.com/quickjs-ng/quickjs.git")
 
     add_versions("v0.5.0", "41212a6fb84bfe07d61772c02513734b7a06465843ba8f76f1ce1e5df866f489")
 
     if is_plat("linux", "bsd") then
-        add_syslinks("m")
+        add_syslinks("m", "pthread")
     end
 
     add_deps("cmake")
 
-    if on_check then
-        on_check("windows", function (package)
-            assert(package:has_cincludes("stdatomic.h", {configs = {languages = "c11"}}),
-             "package(quickjs-ng) Require at least C11 and stdatomic.h")
-        end)
-    end
+    on_check("windows", function (package)
+        local msvc = package:toolchain("msvc")
+        if msvc then
+            local vs = msvc:config("vs")
+            assert(vs and tonumber(vs) >= 2022, "package(quickjs-ng): need vs >= 2022")
+        end
+    end)
 
-    on_install(function (package)
+    on_install("!iphoneos", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
