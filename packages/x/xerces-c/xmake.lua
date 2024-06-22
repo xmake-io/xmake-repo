e590aee99bb108e699c6f0da20d8900d7d24670e
zjyhjqs <zjyhjqs@Hotmail.com>
2024-06-22 19:15:28 +0800

    use `on_check` to limit the API requirements on `android|armeabi-v7a`

--- packages/x/xerces-c/xmake.lua
+++ packages/x/xerces-c/xmake.lua
@@ -20,14 +20,14 @@ package("xerces-c")
             "f58fa2c89e1d4a17d5af193df3e3e5918986b71beb6ce055e9edd1546c20318a")
     end
 
-    on_install("windows", "macosx", "linux", "android", function (package)
-        if package:is_plat("android") and package:is_arch("armeabi-v7a") then
-            import("core.tool.toolchain")
-            local ndk = toolchain.load("ndk", {plat = package:plat(), arch = package:arch()})
-            local ndk_sdkver = ndk:config("ndk_sdkver")
-            assert(ndk_sdkver and tonumber(ndk_sdkver) >= 26, "package(xerces-c): need ndk api level >= 26 for android armeabi-v7a")
-        end
+    on_check("android|armeabi-v7a", function (package)
+        import("core.tool.toolchain")
+        local ndk = toolchain.load("ndk", {plat = package:plat(), arch = package:arch()})
+        local ndk_sdkver = ndk:config("ndk_sdkver")
+        assert(ndk_sdkver and tonumber(ndk_sdkver) >= 26, "package(xerces-c): need ndk api level >= 26 for android armeabi-v7a")
+    end)
 
+    on_install("windows", "macosx", "linux", "android", function (package)
         local configs = {"-Dnetwork=OFF", "-DCMAKE_DISABLE_FIND_PACKAGE_ICU=ON", "-DCMAKE_DISABLE_FIND_PACKAGE_CURL=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
