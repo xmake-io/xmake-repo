18040050634042e57ea51b5657659ec5a7942bc1
star9029 <hengxings783@gmail.com>
2024-06-24 23:34:03 +0800

    pahomqttc: fix syslinks

--- packages/p/pahomqttc/xmake.lua
+++ packages/p/pahomqttc/xmake.lua
@@ -8,18 +8,46 @@ package("pahomqttc")
 
     add_versions("v1.3.13", "5ba7c7ab7ebb1499938fa2e358e6c1f9a926b270f2bf082acf89d59b4771a132")
 
+    add_configs("uuid", {description = "Flag that defines whether libuuid or a custom uuid implementation should be used", default = false, type = "boolean"})
+
+    if is_plat("windows") then
+        add_syslinks("ws2_32", "advapi32", "rpcrt4")
+    elseif is_plat("linux") then
+        add_syslinks("dl", "pthread", "rt")
+    elseif is_plat("android") then
+        add_syslinks("dl")
+    elseif is_plat("bsd") then
+        add_syslinks("compat", "pthread")
+    end
+
     add_deps("cmake")
 
-    on_install("windows", "linux", "macosx", function (package)
-        local configs = {"-DPAHO_BUILD_SAMPLES=FALSE",
-                         "-DPAHO_WITH_SSL=FALSE",
-                         "-DPAHO_BUILD_DOCUMENTATION=FALSE"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+    on_load(function (package)
+        if package:config("uuid") then
+            package:add("deps", "uuid")
+        end
+
+        if package:config("shared") then
+            package:add("defines", "PAHO_MQTT_IMPORTS")
+        end
+    end)
+
+    on_install(function (package)
+        local configs = {
+            "-DPAHO_BUILD_SAMPLES=FALSE",
+            "-DPAHO_ENABLE_TESTING=OFF",
+            "-DPAHO_ENABLE_CPACK=OFF",
+            "-DPAHO_BUILD_DOCUMENTATION=FALSE",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        local shared = package:config("shared")
+        table.insert(configs, "-DPAHO_BUILD_SHARED=" .. (shared and "TRUE" or "FALSE"))
+        table.insert(configs, "-DPAHO_BUILD_STATIC=" .. (shared and "FALSE" or "TRUE"))
+
+        table.insert(configs, "-DPAHO_WITH_LIBUUID=" .. (package:config("uuid") and "TRUE" or "FALSE"))
         import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:has_cfuncs("MQTTClient_connect", {includes = "MQTTClient.h"}))
-    end)   
-package_end()
+    end)
