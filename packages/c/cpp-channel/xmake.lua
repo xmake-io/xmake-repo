20aecd9178a671ca1a326f07e29ae69c78240ad5
star9029 <hengxings783@gmail.com>
2024-06-26 21:57:46 +0800

    Update xmake.lua

--- packages/c/cpp-channel/xmake.lua
+++ packages/c/cpp-channel/xmake.lua
@@ -9,17 +9,14 @@ package("cpp-channel")
 
     add_versions("0.8.2", "36f234c40d59b90356d37b558a8918a86b128030ad5d42d8c6a627cfe81c8624")
 
-    add_deps("cmake")
-
     on_install(function (package)
         os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <msd/channel.hpp>
             void test() {
                 msd::channel<int> chan;
             }
-        ]]}, {configs = {languages = "c++11"}}))
+        ]]}, {configs = {languages = "c++11"}, includes = "msd/channel.hpp"}))
     end)
