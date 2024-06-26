42648fe57e4d1aae74c9eecc2a1b4ad0cdde3b5e
star9029 <hengxings783@gmail.com>
2024-06-26 22:15:39 +0800

    Update xmake.lua

--- packages/c/cpp-channel/xmake.lua
+++ packages/c/cpp-channel/xmake.lua
@@ -4,10 +4,10 @@ package("cpp-channel")
     set_description("Thread-safe container for sharing data between threads")
     set_license("MIT")
 
-    set_urls("https://github.com/andreiavrammsd/cpp-channel/archive/refs/tags/v$(version).zip",
+    add_urls("https://github.com/andreiavrammsd/cpp-channel/archive/refs/tags/$(version).zip",
              "https://github.com/andreiavrammsd/cpp-channel.git")
 
-    add_versions("0.8.2", "36f234c40d59b90356d37b558a8918a86b128030ad5d42d8c6a627cfe81c8624")
+    add_versions("v0.8.2", "36f234c40d59b90356d37b558a8918a86b128030ad5d42d8c6a627cfe81c8624")
 
     on_install(function (package)
         os.cp("include", package:installdir())
