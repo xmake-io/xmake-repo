c3bc970b7a7d28781c2a5448fe8f78f0646a2aba
star9029 <hengxings783@gmail.com>
2024-06-27 00:26:36 +0800

    libmspack: support all platform (#4404)

--- packages/l/libmspack/xmake.lua
+++ packages/l/libmspack/xmake.lua
@@ -1,15 +1,15 @@
 package("libmspack")
-
     set_homepage("https://www.cabextract.org.uk/libmspack/")
     set_description("libmspack is a portable library for some loosely related Microsoft compression formats.")
     set_license("LGPL-2.0")
 
     add_urls("https://github.com/kyz/libmspack/archive/refs/tags/$(version).zip",
              "https://github.com/kyz/libmspack.git")
+
     add_versions("v1.11", "88f8395ff4630d600a0973d7c8c011c21877b80439be5d6cc840afd02f54da20")
     add_versions("v0.10.1alpha", "d51e3b0d42afef91939fb282f7712e0b81c243ffe0aaacafc977d384408b4ab1")
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install(function (package)
         os.cd("libmspack/mspack")
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
@@ -19,7 +19,7 @@ package("libmspack")
                 add_files("*.c|debug.c")
                 add_includedirs(".")
                 add_headerfiles("mspack.h")
-                if is_plat("windows") and is_kind("shared") then
+                if is_plat("windows", "mingw") and is_kind("shared") then
                     add_files("mspack.def")
                 end
         ]])
