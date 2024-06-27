760ab41dbdb337157f7ac59bd8b76bb5572ef065
ruki <waruqi@gmail.com>
2024-06-28 00:13:54 +0800

    Update xmake.lua

--- packages/w/winlamb/xmake.lua
+++ packages/w/winlamb/xmake.lua
@@ -17,11 +17,8 @@ package("winlamb")
             public:
                 My_Window();
             };
-
-            RUN(My_Window) // optional, generate WinMain call and instantiate My_Window
-
-            My_Window::My_Window()
-            {
+            RUN(My_Window)
+            My_Window::My_Window() {
                 setup.wndClassEx.lpszClassName = L"SOME_CLASS_NAME"; // class name to be registered
                 setup.title = L"This is my window";
                 setup.style |= WS_MINIMIZEBOX;
@@ -29,13 +26,6 @@ package("winlamb")
                 on_message(WM_CREATE, [this](wl::wm::create p) -> LRESULT
                 {
                     set_text(L"A new title for the window");
-                    return 0;
-                });
-
-                on_message(WM_LBUTTONDOWN, [](wl::wm::lbuttondown p) -> LRESULT
-                {
-                    bool isCtrlDown = p.has_ctrl();
-                    long xPos = p.pos().x;
                     return 0;
                 });
             }
