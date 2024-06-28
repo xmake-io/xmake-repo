74d3ed8ac6378fe9f2355474d7404bf99e0a34d7
star9029 <hengxings783@gmail.com>
2024-06-28 20:30:46 +0800

    improve xmake.lua

--- packages/a/asio2/xmake.lua
+++ packages/a/asio2/xmake.lua
@@ -1,15 +1,15 @@
 package("asio2")
-
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/zhllxt/asio2")
     set_description("Header only c++ network library, based on asio, support tcp,udp,http,websocket,rpc,ssl,icmp,serial_port.")
     set_license("BSL-1.0")
 
-    add_urls("https://github.com/zhllxt/asio2/archive/refs/tags/v$(version).zip")
-    add_urls("https://github.com/zhllxt/asio2.git")
-    add_versions("2.9", "3ce0b41300954ffc13948bd51af6430e324b1c28e26d2eb5a83e775cf38c12b4")
-    add_patches("2.9", "https://github.com/zhllxt/asio2/commit/36ba0c3d5d3914c4caca2d0448775de5e44a0c6c.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
+    add_urls("https://github.com/zhllxt/asio2/archive/refs/tags/v$(version).zip",
+             "https://github.com/zhllxt/asio2.git")
 
+    add_versions("2.9", "3ce0b41300954ffc13948bd51af6430e324b1c28e26d2eb5a83e775cf38c12b4")
+
+    add_patches("2.9", "patches/2.9/remove-const.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
 
     add_deps("asio", "cereal", "fmt", "openssl3")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
