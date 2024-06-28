ab7ab0dd12dd7b3ef7338cf432da2391af8e2412
xpxz <97490782+heheda123123@users.noreply.github.com>
2024-06-28 09:40:25 +0800

    Update xmake.lua

--- packages/a/asio2/xmake.lua
+++ packages/a/asio2/xmake.lua
@@ -8,6 +8,8 @@ package("asio2")
     add_urls("https://github.com/zhllxt/asio2/archive/refs/tags/v$(version).zip")
     add_urls("https://github.com/zhllxt/asio2.git")
     add_versions("2.9", "3ce0b41300954ffc13948bd51af6430e324b1c28e26d2eb5a83e775cf38c12b4")
+    add_patches("2.9", "https://github.com/zhllxt/asio2/commit/36ba0c3d5d3914c4caca2d0448775de5e44a0c6c.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
+
 
     add_deps("asio", "cereal", "fmt", "openssl3")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
