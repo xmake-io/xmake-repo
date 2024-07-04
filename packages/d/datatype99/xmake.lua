7d000f62d562019df2f1ec7776fed7902ce7846c
ruki <waruqi@gmail.com>
2024-07-04 13:50:21 +0800

    Update xmake.lua

--- packages/d/datatype99/xmake.lua
+++ packages/d/datatype99/xmake.lua
@@ -5,10 +5,10 @@ package("datatype99")
     set_license("MIT")
 
     add_urls("https://github.com/Hirrolot/datatype99/archive/refs/tags/v$(version).tar.gz",
-        "https://github.com/Hirrolot/datatype99.git")
+             "https://github.com/Hirrolot/datatype99.git")
 
     add_versions("1.6.3", "0ddc138eac8db19fa22c482d9a2ec107ff622fd7ce61bb0b1eefb4d8f522e01e")
-    -- add_versions("1.6.4", "f8488decc7ab035e3af77ee62e64fc678d5cb57831457f7270efe003e63d6f09")
+    add_versions("1.6.4", "f8488decc7ab035e3af77ee62e64fc678d5cb57831457f7270efe003e63d6f09")
 
     add_deps("metalang99")
 
