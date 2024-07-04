58e9a75dd4fc0c5fcc022a90d3a460d3a1f3ac19
IMXEren <96839938+IMXEren@users.noreply.github.com>
2024-07-04 07:14:22 +0530

    metalang99: test latest version

--- packages/m/metalang99/xmake.lua
+++ packages/m/metalang99/xmake.lua
@@ -8,10 +8,10 @@ package("metalang99")
         "https://github.com/Hirrolot/metalang99.git")
 
     add_versions("1.13.2", "912c6d91b872d34d2b6580d25afc38faccacf6c57462ab1c862010ff4afbf790")
-    -- add_versions("1.13.3", "91fe8d4edcc2e7f91c5b567a2b90f2e30c2373f1ebbabcf209ea0d74f63bc1e9")
+    add_versions("1.13.3", "91fe8d4edcc2e7f91c5b567a2b90f2e30c2373f1ebbabcf209ea0d74f63bc1e9")
 
     on_install(function(package)
-        os.cp("include/*", package:installdir("include"))
+        os.cp("include", package:installdir())
     end)
 
     on_test(function(package)
