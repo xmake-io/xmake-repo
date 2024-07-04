473a33fd32014b59d63518b3315d7f99d71ea640
star9029 <hengxings783@gmail.com>
2024-07-04 21:40:40 +0800

    fix syslinks

--- packages/s/stfl/xmake.lua
+++ packages/s/stfl/xmake.lua
@@ -3,10 +3,14 @@ package("stfl")
     set_description("stfl with Newsboat-related bugfixes")
     set_license("LGPL-3.0")
 
-    add_urls("https://github.com/newsboat/stfl/archive/c2c10b8a50fef613c0aacdc5d06a0fa610bf79e9.tar.gz"
+    add_urls("https://github.com/newsboat/stfl/archive/c2c10b8a50fef613c0aacdc5d06a0fa610bf79e9.tar.gz",
              "https://github.com/newsboat/stfl.git")
 
     add_versions("0.24", "59d3f43522161bc2252bd806f973ad64c86a081f06a57a6d628b1c7bdfee7551")
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
 
     add_deps("ncurses")
 
@@ -20,6 +24,10 @@ package("stfl")
                 add_files("*.c|example.c")
                 add_files("widgets/*.c")
                 add_headerfiles("stfl.h")
+
+                if is_plat("linux", "bsd") then
+                    add_syslinks("pthread")
+                end
         ]])
         import("package.tools.xmake").install(package)
     end)
