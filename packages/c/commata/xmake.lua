f55e002cd862cbb057a179623a5cecf49fc7d718
benny WU <benny066567@163.com>
2024-07-05 12:00:25 +0800

    add commit url

--- packages/c/commata/xmake.lua
+++ packages/c/commata/xmake.lua
@@ -5,7 +5,8 @@ package("commata")
     set_description("Just another header-only C++17 CSV parser")
     set_license("Unlicense")
 
-    add_urls("https://github.com/furfurylic/commata/archive/refs/tags/v1.0.0-rc.2.zip")
+    add_urls("https://github.com/furfurylic/commata/archive/refs/tags/v1.0.0-rc.2.zip",
+             "https://github.com/furfurylic/commata.git")
     add_versions("v1.0.0", "5f9ef542d10d5d04d296e609ae8931e09a157761c86630d71b2f397c6a205a75")
 
     on_install(function (package)
