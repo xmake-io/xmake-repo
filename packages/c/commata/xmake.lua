ed82d59bfd3bd7bd7a4cc5b046d0d2e15a72568a
benny WU <benny066567@163.com>
2024-07-05 11:40:41 +0800

    add license and change the way of os.cp writing

--- packages/c/commata/xmake.lua
+++ packages/c/commata/xmake.lua
@@ -3,12 +3,13 @@ package("commata")
     set_kind("library", {headeronly = true})
     set_homepage("https://furfurylic.github.io/commata/CommataSpecification.xml")
     set_description("Just another header-only C++17 CSV parser")
+    set_license("Unlicense")
 
     add_urls("https://github.com/furfurylic/commata/archive/refs/tags/v1.0.0-rc.2.zip")
     add_versions("v1.0.0", "5f9ef542d10d5d04d296e609ae8931e09a157761c86630d71b2f397c6a205a75")
 
     on_install(function (package)
-        os.cp("include/", package:installdir("include/"), {rootdir = "include"})
+        os.cp("include/", package:installdir(""))
     end)
 
     on_test(function (package)
