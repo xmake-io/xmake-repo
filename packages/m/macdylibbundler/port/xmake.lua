3dd8c78d58f201bb886e2c65282577d69654a022
ruki <waruqi@gmail.com>
2024-07-08 12:16:56 +0800

    Delete packages/m/macdylibbundler/port/xmake.lua

--- packages/m/macdylibbundler/port/xmake.lua
+++
@@ -1,9 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-target("macdylibbundler")
-    set_kind("$(kind)")
-    set_languages("c++11")
-    add_files("src/*.cpp")
-    add_includedirs("src")
-
-    add_headerfiles("src/*.h")
