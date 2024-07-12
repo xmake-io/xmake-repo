46b9066f3580f8fb50ca78037c700b2b8c3937ad
Zetelias <145657074+Zetelias@users.noreply.github.com>
2024-07-12 19:34:16 +0200

    Make glaze multiplatform

--- packages/g/glaze/xmake.lua
+++ packages/g/glaze/xmake.lua
@@ -16,7 +16,7 @@ package("glaze")
     add_versions("v2.2.0", "1d6e36029a58bf8c4bdd035819e1ab02b87d8454dd80fa2f5d46c96a1e6d600c")
     add_versions("v1.3.5", "de5d59cb7f31193d45f67f25d8ced1499df50c0d926a1461432b87f2b2368817")
 
-    on_install("linux", function (package)
+    on_install(function (package)
         os.cp("include", package:installdir())
     end)
 
