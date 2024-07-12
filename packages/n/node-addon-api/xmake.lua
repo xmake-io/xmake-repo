ef00baf274327b99bded28a94f38ed4971297f59
Enzo Einhorn <enzo.einhorn@akkodis.com>
2024-07-12 17:50:14 +0200

    use has_cxxfunc instead of has_cfunc

--- packages/n/node-addon-api/xmake.lua
+++ packages/n/node-addon-api/xmake.lua
@@ -39,5 +39,5 @@ package("node-addon-api")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("Napi::Just(0)", {includes = "napi.h"}))
+        assert(package:has_cxxfuncs("Napi::Just(0)", {includes = "napi.h"}))
     end)
