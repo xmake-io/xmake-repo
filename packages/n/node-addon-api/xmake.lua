45bce351b1ef56f505efe6fb22deef31bb91c390
Enzo Einhorn <enzo.einhorn@akkodis.com>
2024-07-12 10:38:25 +0200

    switch nodejs dep. to node-api-headers and add napi_version option

--- packages/n/node-addon-api/xmake.lua
+++ packages/n/node-addon-api/xmake.lua
@@ -7,15 +7,16 @@ package("node-addon-api")
 
     add_configs("errors", {description = "Choose error handling method.", default = "except", type = "string", values = {"except", "noexcept", "maybe"}})
     add_configs("disable_deprecated", {description = "Disable deprecated APIs.", default = true, type = "boolean"})
+    add_configs("napi_version", {description = "Target a specific Node-API version.", default = nil, type = "number"})
 
     set_urls("https://github.com/nodejs/node-addon-api/archive/refs/tags/$(version).tar.gz",
         "https://github.com/nodejs/node-addon-api.git")
     add_versions("v8.0.0", "42424c5206b9d67b41af4fcff5d6e3cb22074168035a03b8467852938a281d47")
 
-    add_deps("nodejs")
+    add_deps("node-api-headers")
 
     on_load(function(package)
-        package:add("defines", "NAPI_VERSION=" .. package:version():major())
+        package:add("defines", "NAPI_VERSION=" .. package:config("napi_version") or package:version():major())
         if package:config("disable_deprecated") then
             package:add("defines", "NODE_ADDON_API_DISABLE_DEPRECATED")
         end
