4e385bc5fbf8246c93b891ee732df078d09f71aa
ruki <waruqi@gmail.com>
2024-07-12 22:02:58 +0800

    Update xmake.lua

--- packages/n/node-api-headers/xmake.lua
+++ packages/n/node-api-headers/xmake.lua
@@ -5,7 +5,7 @@ package("node-api-headers")
     set_license("MIT")
 
     set_urls("https://github.com/nodejs/node-api-headers/archive/refs/tags/$(version).tar.gz",
-        "https://github.com/nodejs/node-api-headers.git")
+             "https://github.com/nodejs/node-api-headers.git")
     add_versions("v1.1.0", "70608bc1e6dddce280285f3462f18a106f687c0720a4b90893e1ecd86e5a8bbf")
 
     on_install(function(package)
