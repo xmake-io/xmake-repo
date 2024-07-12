93da33a9adffb1ca5bcdd55629c6c297e7a1d39a
Enzo Einhorn <enzo.einhorn@akkodis.com>
2024-07-12 10:32:37 +0200

    add node_api_headers package

---
+++ packages/n/node-api-headers/xmake.lua
@@ -0,0 +1,17 @@
+package("node-api-headers")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/nodejs/node-api-headers")
+    set_description("C-based Node-API headers")
+    set_license("MIT")
+
+    set_urls("https://github.com/nodejs/node-api-headers/archive/refs/tags/$(version).tar.gz",
+        "https://github.com/nodejs/node-api-headers.git")
+    add_versions("v1.1.0", "70608bc1e6dddce280285f3462f18a106f687c0720a4b90893e1ecd86e5a8bbf")
+
+    on_install(function(package)
+        os.cp("include/*.h", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(os.isfile(path.join(package:installdir("include"), "node_api.h")))
+    end)
