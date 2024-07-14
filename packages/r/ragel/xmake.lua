93fd1b018aed7c225e505f44ec160ef47e857414
c8ef <c8ef@outlook.com>
2024-07-14 08:31:22 +0800

    Update xmake.lua

--- packages/r/ragel/xmake.lua
+++ packages/r/ragel/xmake.lua
@@ -8,7 +8,7 @@ package("ragel")
              "https://github.com/adrian-thurston/ragel.git")
     add_versions("6.10", "5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f")
 
-    on_install("linux", "windows|x64", function (package)
+    on_install("linux", "windows|!arm*", function (package)
         io.replace("ragel/main.cpp", "#include <unistd.h>", "", {plain = true})
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
@@ -19,28 +19,13 @@ package("ragel")
                 add_includedirs("aapl")
         ]])
         io.writefile("ragel/config.h", [[
-            /* Name of package */
             #define PACKAGE "ragel"
-
-            /* Define to the address where bug reports for this package should be sent. */
             #define PACKAGE_BUGREPORT ""
-
-            /* Define to the full name of this package. */
             #define PACKAGE_NAME "ragel"
-
-            /* Define to the full name and version of this package. */
             #define PACKAGE_STRING "ragel 6.10"
-
-            /* Define to the one symbol short name of this package. */
             #define PACKAGE_TARNAME "ragel"
-
-            /* Define to the home page for this package. */
             #define PACKAGE_URL ""
-
-            /* Define to the version of this package. */
             #define PACKAGE_VERSION "6.10"
-
-            /* Version number of package */
             #define VERSION "6.10"
         ]])
         import("package.tools.xmake").install(package)
