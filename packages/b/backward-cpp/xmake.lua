ae545d43a35c5a9bfb6394ec4f929fe9e9692003
star9029 <hengxings783@gmail.com>
2024-07-18 22:00:37 +0800

    only support headeronly kind

--- packages/b/backward-cpp/xmake.lua
+++ packages/b/backward-cpp/xmake.lua
@@ -1,4 +1,5 @@
 package("backward-cpp")
+    set_kind("library", {headeronly = true})
     set_homepage("https://github.com/bombela/backward-cpp")
     set_description("Backward is a beautiful stack trace pretty printer for C++.")
     set_license("MIT")
@@ -18,6 +19,12 @@ package("backward-cpp")
         end
     end
 
+    if is_plat("windows", "mingw") then
+        add_syslinks("psapi", "dbghelp")
+    elseif is_plat("linux", "bsd") then
+        add_syslinks("dl", "m")
+    end
+
     add_deps("cmake")
 
     on_load("linux", "mingw@msys", "macos", function (package)
@@ -29,29 +36,24 @@ package("backward-cpp")
     end)
 
     on_install(function (package)
+        io.replace("backward.hpp", [[#pragma comment(lib, "psapi.lib")]], "", {plain = true})
+        io.replace("backward.hpp", [[#pragma comment(lib, "dbghelp.lib")]], "", {plain = true})
+
         local configs = {"-DBACKWARD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBACKWARD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
             table.insert(configs, "-DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=''")
-            if package:config("shared") then
-                table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
-            end
         end
         import("package.tools.cmake").install(package, configs)
-
         os.cp(package:installdir("include/*.hpp"), package:installdir("include/backward"))
         os.cp(package:installdir("include/*.hpp"), package:installdir("lib/backward"))
-        os.cp("**.a", package:installdir("lib"))
-        os.cp("**.so", package:installdir("lib"))
-        os.cp("**.lib", package:installdir("lib"))
-        os.cp("**.dll", package:installdir("bin"))
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             void test() {
-                backward::SignalHandling x;
+                backward::Printer printer;
+                backward::StackTrace st;
             }
-        ]]}, {configs = {languages = "c++11"}, includes = "backward/backward.hpp"}))
+        ]]}, {configs = {languages = "c++11"}, includes = "backward.hpp"}))
     end)
