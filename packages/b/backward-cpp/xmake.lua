bb663723784bf42d78fc63d8d48cb5ccd78935f6
star9029 <hengxings783@gmail.com>
2024-07-18 22:42:31 +0800

    improve configs and deps

--- packages/b/backward-cpp/xmake.lua
+++ packages/b/backward-cpp/xmake.lua
@@ -9,7 +9,8 @@ package("backward-cpp")
 
     add_versions("v1.6", "9b07e12656ab9af8779a84e06865233b9e30fadbb063bf94dd81d318081db8c2")
 
-    add_configs("bfd", {description = "Get stack trace with details about your sources by using libbfd from binutils.", default = false, type = "boolean"})
+    add_configs("stack_walking", {description = "Choose stack walking library to use.", default = nil, type = "string", values = {"unwind", "libunwind", "backtrace"}})
+    add_configs("stack_details", {description = "Choose stack details library to use.", default = nil, type = "string", values = {"dw", "bfd", "dwarf", "backtrace_symbol"}})
 
     if is_plat("mingw") then
         add_patches("v1.6", "patches/v1.6/link_to_imagehlp.patch", "0a135b6d68970ff6609a3eb4deb2b10c317eee15ba980eb178b93402a97c957c")
@@ -21,29 +22,63 @@ package("backward-cpp")
 
     if is_plat("windows", "mingw") then
         add_syslinks("psapi", "dbghelp")
-    elseif is_plat("linux", "bsd") then
+    elseif is_plat("linux", "bsd", "android") then
         add_syslinks("dl", "m")
     end
 
     add_deps("cmake")
 
-    on_load("linux", "mingw@msys", "macos", function (package)
-        if package:config("bfd") then
+    on_load(function (package)
+        local stack_walking = package:config("stack_walking")
+        if stack_walking == "libunwind" then
+            package:add("deps", "libunwind")
+        end
+
+        local stack_details = package:config("stack_details")
+        if stack_details == "dwarf" then
+            package:add("deps", "libdwarf", "libelf")
+        elseif stack_details == "dw" then
+            package:add("deps", "elfutils")
+        elseif stack_details == "bfd" then
             package:add("deps", "binutils")
             package:add("syslinks", "bfd")
-            package:add("defines", "BACKWARD_HAS_BFD=1")
         end
+
+        package:add("defines", "BACKWARD_HAS_UNWIND=" .. (stack_walking == "unwind" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_LIBUNWIND=" .. (stack_walking == "libunwind" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_BACKTRACE=" .. (stack_walking == "backtrace" and "1" or "0"))
+
+        package:add("defines", "BACKWARD_HAS_BACKTRACE_SYMBOL=" .. (stack_details == "backtrace_symbol" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_DW=" .. (stack_details == "dw" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_BFD=" .. (stack_details == "bfd" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_DWARF=" .. (stack_details == "dwarf" and "1" or "0"))
+        package:add("defines", "BACKWARD_HAS_PDB_SYMBOL=" .. (is_plat("windows") and "1" or "0"))
     end)
 
-    on_install(function (package)
+    on_install("windows|!arm.*", function (package)
         io.replace("backward.hpp", [[#pragma comment(lib, "psapi.lib")]], "", {plain = true})
         io.replace("backward.hpp", [[#pragma comment(lib, "dbghelp.lib")]], "", {plain = true})
 
-        local configs = {"-DBACKWARD_TESTS=OFF"}
+        local configs = {"-DBACKWARD_TESTS=OFF", "-DSTACK_DETAILS_AUTO_DETECT=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBACKWARD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
             table.insert(configs, "-DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=''")
+            if package:config("shared") then
+                table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+            end
         end
+
+        local stack_walking = package:config("stack_walking")
+        local stack_details = package:config("stack_details")
+
+        table.insert(configs, "-DSTACK_WALKING_UNWIND=" .. ((stack_walking == "unwind") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_WALKING_LIBUNWIND=" .. ((stack_walking == "libunwind") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_WALKING_BACKTRACE=" .. ((stack_walking == "backtrace") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_DETAILS_BACKTRACE_SYMBOL=" .. ((stack_walking == "backtrace_symbol") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_DETAILS_DW=" .. ((stack_walking == "dw") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_DETAILS_BFD=" .. ((stack_walking == "bfd") and "ON" or "OFF"))
+        table.insert(configs, "-DSTACK_DETAILS_DWARF=" .. ((stack_walking == "dwarf") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
         os.cp(package:installdir("include/*.hpp"), package:installdir("include/backward"))
         os.cp(package:installdir("include/*.hpp"), package:installdir("lib/backward"))
