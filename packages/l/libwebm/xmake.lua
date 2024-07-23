3f154f50f04ce0b7a4932cee96fa38b7138df1c1
ruki <waruqi@gmail.com>
2024-07-23 21:18:14 +0800

    Update xmake.lua

--- packages/l/libwebm/xmake.lua
+++ packages/l/libwebm/xmake.lua
@@ -3,10 +3,7 @@ package("libwebm")
     set_description("Library for muxing and demuxing WebM media container files")
     set_license("BSD-3-Clause")
 
-    add_urls("https://chromium.googlesource.com/webm/libwebm.git", { version = function (version)
-            return version:gsub("+", ".")
-        end
-    })
+    add_urls("https://chromium.googlesource.com/webm/libwebm.git")
 
     add_versions("1.0.0+31", "6745fd29e0245fc584b0bb9f65018ea2366fe7fb")
 
@@ -24,7 +21,6 @@ package("libwebm")
         local configs = {"-DENABLE_SAMPLE_PROGRAMS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-
         table.insert(configs, "-DENABLE_WEBMTS=" .. (package:config("webmts") and "ON" or "OFF"))
         table.insert(configs, "-DENABLE_WEBMINFO=" .. (package:config("webm_info") and "ON" or "OFF"))
         table.insert(configs, "-DENABLE_WEBM_PARSER=" .. (package:config("parser") and "ON" or "OFF"))
@@ -39,7 +35,7 @@ package("libwebm")
             #else
             #include <webm/mkvmuxer/mkvmuxerutil.h>
             #include <webm/mkvparser/mkvparser.h>
-            #endif // UNPREFIXED_HEADERS
+            #endif
             void test() {
                 int32_t major, minor, build, revision;
                 mkvparser::GetVersion(major, minor, build, revision);
