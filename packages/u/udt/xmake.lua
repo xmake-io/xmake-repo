2dc53fd9b4a60dda91cf249124a83dd33f298543
star9029 <hengxings783@gmail.com>
2024-07-24 14:53:13 +0800

    Update xmake.lua

--- packages/u/udt/xmake.lua
+++ packages/u/udt/xmake.lua
@@ -3,13 +3,16 @@ package("udt")
     set_description("UDP-based Data Transfer Protocol")
     set_license("Apache-2.0")
 
-    set_urls("https://github.com/dorkbox/UDT.git")
+    add_urls("https://github.com/dorkbox/UDT/archive/8272c251deb8bfd7289646b7604f1079b59194d0.tar.gz",
+             "https://github.com/dorkbox/UDT.git")
+
+    add_versions("2017.12.03", "af743ffdb2e40225d7375df1a3d6320127b903322af4021460681f1052b7461e")
 
     if is_plat("windows") then
         add_defines("WINDOWS")
         add_syslinks("Ws2_32")
-    else
-        add_syslinks("pthread")
+    elseif is_plat("linux", "bsd") then
+        add_syslinks("pthread", "m")
     end
 
     on_install(function (package)
@@ -20,23 +23,17 @@ package("udt")
                 if is_mode("debug") then
                     add_defines("_DEBUG")
                 end
-                set_warnings("all")
                 set_languages("cxx11")
                 if is_plat("windows") then
                     add_defines("WINDOWS")
-                    add_links(
-                        "Ws2_32"
-                    )
-                else
-                    add_links(
-                        "pthread",
-                        "m"
-                    )
+                    add_syslinks("Ws2_32")
+                elseif is_plat("linux", "bsd") then
+                    add_syslinks("pthread", "m")
                 end
                 add_files("src/*.cpp")
                 add_headerfiles("src/udt.h")
         ]])
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
