dd88c6288d7117ffd8693dc79ce9ad28b39cfbf1
Denis <43519306+urasssopretty@users.noreply.github.com>
2024-08-01 16:17:00 +0300

    fixed xmake warning for GLFW package (#4807)
    * fixed xmake warning for GLFW package
    
    warning: please use package:runtimes() or package:has_runtime() instead of package:config("vs_runtime")
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/g/glfw/xmake.lua
+++ packages/g/glfw/xmake.lua
@@ -53,7 +53,7 @@ package("glfw")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
-            table.insert(configs, "-DUSE_MSVC_RUNTIME_LIBRARY_DLL=" .. (package:config("vs_runtime"):startswith("MT") and "OFF" or "ON"))
+            table.insert(configs, "-DUSE_MSVC_RUNTIME_LIBRARY_DLL=" .. (package:has_runtime("MT") and "OFF" or "ON"))
         end
         table.insert(configs, "-DGLFW_BUILD_X11=" .. (package:config("x11") and "ON" or "OFF"))
         table.insert(configs, "-DGLFW_BUILD_WAYLAND=" .. (package:config("wayland") and "ON" or "OFF"))
