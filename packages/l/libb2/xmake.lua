8565205f04692e5dac72f031be880fac1e1ca795
star9029 <hengxings783@gmail.com>
2024-08-03 15:33:59 +0800

    libb2: add package

---
+++ packages/l/libb2/xmake.lua
@@ -0,0 +1,26 @@
+package("libb2")
+    set_homepage("https://blake2.net")
+    set_description("BLAKE2 official implementations")
+    set_license("CC0-1.0")
+
+    add_urls("https://github.com/BLAKE2/libb2/archive/643decfbf8ae600c3387686754d74c84144950d1.tar.gz",
+             "https://github.com/BLAKE2/libb2.git")
+
+    add_versions("v0.98.1", "9eb776149c41a34619e801adeae8056ca68faadc7cea3a68a54b2a4d93ef1937")
+
+    on_install(function (package)
+        if package:config("shared") then
+            package:add("defines", "BLAKE2_DLL")
+        end
+
+        io.writefile("src/config.h")
+        io.replace("src/blake2-impl.h",
+            "#define BLAKE2_IMPL_NAME(fun)  BLAKE2_IMPL_EVAL(fun, SUFFIX)",
+            "#define BLAKE2_IMPL_NAME", {plain = true})
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("blake2", {includes = "blake2.h"}))
+    end)
