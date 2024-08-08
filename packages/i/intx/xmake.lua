808e0369ed6864c14215962267814bbe1726de5b
star9029 <hengxings783@gmail.com>
2024-08-08 20:24:33 +0800

    intx: add package

---
+++ packages/i/intx/xmake.lua
@@ -0,0 +1,24 @@
+package("intx")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/chfast/intx")
+    set_description("Extended precision integer C++ library")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/chfast/intx/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/chfast/intx.git")
+
+    add_versions("v0.11.0", "bff2a78e3a9a3b9bbabf50500feae65bc0ec50a2364f4a83768277d6eba7a844")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        import("package.tools.cmake").install(package, {"-DINTX_TESTING=OFF"})
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                auto x = static_cast<int>(intx::uint512{1} / (intx::uint512{1} << 111));
+            }
+        ]]}, {configs = {languages = "c++20"}, includes = {"intx/intx.hpp"}}))
+    end)
