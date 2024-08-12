abf2d34b5bd0492d948e9d723bc802bc69c9880b
star9029 <hengxings783@gmail.com>
2024-08-12 23:55:42 +0800

    usd: improve xmake.lua

--- packages/u/usd/xmake.lua
+++ packages/u/usd/xmake.lua
@@ -39,7 +39,7 @@ package("usd")
             "-DPXR_BUILD_USD_TOOLS=OFF",
             "-DPXR_BUILD_MONOLITHIC=" .. (package:config("monolithic") and "ON" or "OFF")
         }
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
