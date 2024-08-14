c38ca7889842e20cea7563804c4c0856db30aaff
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-08-14 17:20:46 +0100

    yaml-cpp: make shared if mingw

--- packages/y/yaml-cpp/xmake.lua
+++ packages/y/yaml-cpp/xmake.lua
@@ -13,13 +13,18 @@ package("yaml-cpp")
     add_versions("0.8.0", "fbe74bbdcee21d656715688706da3c8becfd946d92cd44705cc6098bb23b3a16")
 
     add_deps("cmake")
+
+    if is_plat("mingw") then
+        add_configs("shared", {description = "Build shared library.", default = true, readonly = true, type = "boolean"})
+    end
+
     on_load("windows", function (package)
         if not package:config("shared") then
             package:add("defines", "YAML_CPP_STATIC_DEFINE")
         end
     end)
 
-    on_install("!mingw", function (package)
+    on_install(function (package)
         local configs = {"-DYAML_CPP_BUILD_TESTS=OFF"}
         table.insert(configs, "-DYAML_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
