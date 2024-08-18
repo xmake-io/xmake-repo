b5ebe34b0f3a107fdb34059c7690d8c5142ea27e
star9029 <hengxings783@gmail.com>
2024-08-18 09:06:31 +0800

    fix asan

--- packages/l/libcbor/xmake.lua
+++ packages/l/libcbor/xmake.lua
@@ -14,7 +14,7 @@ package("libcbor")
         local configs = {"-DWITH_EXAMPLES=OFF", "-DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DSANITIZE =" .. (package:config("asan") and "ON" or "OFF"))
+        table.insert(configs, "-DSANITIZE=" .. (package:config("asan") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
