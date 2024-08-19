9b5715a180af6706e1df1df6230573ac8ca10869
hahahahbenny <1292983376@qq.com>
2024-08-19 09:25:35 +0800

    change version to commit and ban the vapi

--- packages/l/libgda/xmake.lua
+++ packages/l/libgda/xmake.lua
@@ -4,19 +4,15 @@ package("libgda")
     set_description("libgda - an interface to the GDA architecture")
     set_license("LGPL-2.0")
 
-    add_urls("https://github.com/GNOME/libgda/archive/refs/tags/LIBGDA_$(version).tar.gz", {alias = "github", version = function (version) return version:gsub("%.", "_") end})
     add_urls("https://github.com/GNOME/libgda.git")
-    add_versions("github:6.0.0", "232b3012d24533edf07b873b9d40bbb95a30e08a2afcb4e12723866b21f5a437")
+    add_versions("2024.08.12", "97fec3090dc86d4b69f62f09d87db9dbcc864fa2")
 
     add_includedirs("include", "include/libgda-6.0", "include/libgda-6.0/libgda")
 
-    if is_plat("linux") then
-        add_extsources("pacman::intltool", "apt::intltool")
-    end
-
     add_deps("meson", "ninja", "libxml2", "gettext", "sqlite3", "json-glib", "pcre2")
     on_install("linux", function (package)
-        local configs = {"-Dui=false"}
+        local configs = {"-Dui=false", "-Dvapi=false", ""}
+        
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         import("package.tools.meson").install(package, configs, {packagedeps = {"glib"}})
     end)
