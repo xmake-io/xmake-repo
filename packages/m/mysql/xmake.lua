ab4eac124195eef7795460d53e6dc431f8cc821e
star9029 <hengxings783@gmail.com>
2024-08-21 20:46:33 +0800

    fix windows arm

--- packages/m/mysql/xmake.lua
+++ packages/m/mysql/xmake.lua
@@ -39,7 +39,7 @@ package("mysql")
         end
     end)
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install("windows|native", "macosx", "linux", function (package)
         local version = package:version()
         if version:eq("9.0.1") then
             io.replace("cmake/ssl.cmake", "FIND_CUSTOM_OPENSSL()", "FIND_SYSTEM_OPENSSL()", {plain = true})
@@ -49,6 +49,10 @@ package("mysql")
             io.replace("cmake/install_macros.cmake",
                 [[NOT type MATCHES "STATIC_LIBRARY"]],
                 [[NOT type MATCHES "STATIC_LIBRARY" AND CMAKE_BUILD_TYPE STREQUAL "DEBUG"]], {plain = true})
+
+            if package:is_arch("arm.*") then
+                io.replace("cmake/rapidjson.cmake", "IF (NOT HAVE_RAPIDJSON_WITH_STD_REGEX)", "if(FALSE)", {plain = true})
+            end
         end
 
         local configs = {
