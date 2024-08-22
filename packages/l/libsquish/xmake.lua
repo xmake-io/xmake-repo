5ed4c157fcc55a28071105d0141021a8c443c2a0
star9029 <hengxings783@gmail.com>
2024-08-23 00:44:49 +0800

    port xmake

--- packages/l/libsquish/xmake.lua
+++ packages/l/libsquish/xmake.lua
@@ -6,26 +6,26 @@ package("libsquish")
     add_urls("https://sourceforge.net/projects/libsquish/files/libsquish-$(version).tgz")
     add_versions("1.15", "628796eeba608866183a61d080d46967c9dda6723bc0a3ec52324c85d2147269")
 
-    add_configs("openmp", {description = "Enable OpenMP build.", default = false, type = "boolean"})
-
-    add_deps("cmake")
+    add_configs("openmp", {description = "Build with OpenMP.", default = false, type = "boolean"})
+    add_configs("sse2", {description = "Build with SSE2.", default = false, type = "boolean"})
 
     on_load(function (package)
         if package:config("openmp") then
             package:add("deps", "openmp")
+            package:add("defines", "SQUISH_USE_OPENMP")
+        end
+
+        if package:config("sse2") then
+            package:add("defines", "SQUISH_USE_SSE=2")
         end
     end)
 
     on_install(function (package)
-        local configs = {"-DBUILD_SQUISH_EXTRA=ON"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DBUILD_SQUISH_WITH_OPENMP=" .. (package:config("openmp") and "ON" or "OFF"))
-        if package:config("shared") and package:is_plat("windows") then
-            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
-        end
-        import("package.tools.cmake").install(package, configs)
-        os.trycp(package:buildir() .. "/**.dll", package:installdir("bin"))
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, {
+            openmp = package:config("openmp"),
+            sse2 = package:config("sse2")
+        })
     end)
 
     on_test(function (package)
