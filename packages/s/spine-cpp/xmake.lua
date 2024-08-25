b74389efd95440b20b9ad27053d84346ac6c7537
ZhangXiChang <yxrbeigua@outlook.com>
2024-08-25 16:17:33 +0800

    fix issues.But install package error: package(spine-cpp): links not found!

--- packages/s/spine-cpp/xmake.lua
+++ packages/s/spine-cpp/xmake.lua
@@ -3,21 +3,38 @@ package("spine-cpp")
     set_description("Spine runtimes for C++")
     set_license("Spine Runtimes")
 
-    add_urls("https://github.com/EsotericSoftware/spine-runtimes.git")
-
-    add_versions("3.8","d33c10f85634d01efbe4a3ab31dabaeaca41230c")
-    add_patches("3.8","./3.8.patch")
-
     if is_plat("windows") then
         set_policy("platform.longpaths", true)
     end
 
+    add_urls("https://github.com/EsotericSoftware/spine-runtimes.git")
+
+    add_versions("3.8","d33c10f85634d01efbe4a3ab31dabaeaca41230c")
+    add_patches("3.8","./patches/3.8/fix.patch")
+
     add_deps("cmake")
-    on_install(function (package)
-        import("package.tools.cmake").install(package)
-        os.cp("./include", package:installdir())
-        os.cp("./lib", package:installdir())
+    on_install(function(package)
+        local configs={}
+        table.insert(configs,"-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package,configs)
+        --os.cp("./include", package:installdir())
+        --os.cp("./lib", package:installdir())
     end)
     on_test(function (package)
-        assert(package:has_cxxincludes("spine/spine.h"))
+        assert(package:check_cxxsnippets({test=[[
+            #include <spine/spine.h>
+
+            namespace spine
+            {
+                SpineExtension *getDefaultExtension()
+                {
+                    return new DefaultSpineExtension();
+                }
+            }
+
+            static void test()
+            {
+                assert(spine::SpineExtension::getInstance() != nullptr);
+            }
+        ]]},{configs={languages="c++17"}}))
     end)
