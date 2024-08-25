f9b007c4858ba58d707106415f5436ade084800c
ZhangXiChang <yxrbeigua@outlook.com>
2024-08-25 18:54:59 +0800

    Remove the CMAKE_INSTALL_PREFIX from the target repository and let xrepo set this variable itself to solve the problem of source and library files not being copied to the package directory.

--- packages/s/spine-cpp/xmake.lua
+++ packages/s/spine-cpp/xmake.lua
@@ -17,8 +17,6 @@ package("spine-cpp")
         local configs={}
         table.insert(configs,"-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package,configs)
-        --os.cp("./include", package:installdir())
-        --os.cp("./lib", package:installdir())
     end)
     on_test(function (package)
         assert(package:check_cxxsnippets({test=[[
