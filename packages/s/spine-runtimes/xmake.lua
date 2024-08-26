c19cb97db18533881d6374beac7fd6ff4fb5ed10
star9029 <hengxings783@gmail.com>
2024-08-26 15:50:16 +0800

    fix indent

--- packages/s/spine-runtimes/xmake.lua
+++ packages/s/spine-runtimes/xmake.lua
@@ -5,9 +5,9 @@ package("spine-runtimes")
 
     add_urls("https://github.com/EsotericSoftware/spine-runtimes.git")
 
-    add_versions("3.8","d33c10f85634d01efbe4a3ab31dabaeaca41230c")
+    add_versions("3.8", "d33c10f85634d01efbe4a3ab31dabaeaca41230c")
 
-    add_patches("3.8","patches/3.8/cmake.patch", "bbfa70e3e36f8b3beefbc84d8047eb6735e1e75f4dce643d8916e231b13b992c")
+    add_patches("3.8", "patches/3.8/cmake.patch", "bbfa70e3e36f8b3beefbc84d8047eb6735e1e75f4dce643d8916e231b13b992c")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
@@ -19,13 +19,13 @@ package("spine-runtimes")
 
     on_install(function(package)
         local configs = {}
-        table.insert(configs,"-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package,configs)
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:check_cxxsnippets({test=[[
+        assert(package:check_cxxsnippets({test = [[
             namespace spine {
                 SpineExtension *getDefaultExtension() {
                     return new DefaultSpineExtension();
