f9ef211aeaf332075a673fad0efb6e2350164eda
star9029 <hengxings783@gmail.com>
2024-09-01 22:51:45 +0800

    add configs

--- packages/l/libwebsockets/xmake.lua
+++ packages/l/libwebsockets/xmake.lua
@@ -11,7 +11,12 @@ package("libwebsockets")
     add_deps("cmake")
 
     add_configs("ssl", {description = "Enable ssl (default: openssl).", default = false, type = "boolean"})
-    add_configs("cap", {description = "Build with libcap", default = false, type = "boolean"})
+    add_configs("libev", {description = "Build with libev", default = false, type = "boolean"})
+    add_configs("libuv", {description = "Build with libuv", default = false, type = "boolean"})
+    add_configs("libevent", {description = "Build with libevent", default = false, type = "boolean"})
+    add_configs("glib", {description = "Build with glib", default = false, type = "boolean"})
+
+    add_configs("libcap", {description = "Build with libcap", default = false, type = "boolean"})
 
     if is_plat("windows", "mingw") then
         add_syslinks("ws2_32")
@@ -30,26 +35,40 @@ package("libwebsockets")
     end
 
     on_load(function (package)
-        if package:config("ssl") then
-            package:add("deps", "openssl")
-        end
-        if package:config("cap") then
-            package:add("deps", "libcap")
+        local deps_map = {
+            ["ssl"] = "openssl"
+        }
+        for name, enabled in table.orderpairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") then
+                if package:config(name) then
+                    local dep_name = deps_map[name]
+                    if not dep_name then
+                        dep_name = name
+                    end
+                    package:add("deps", dep_name)
+                end
+            end
         end
     end)
 
-    on_install(function (package)
+    on_install("!wasm", function (package)
         io.replace("CMakeLists.txt", "/WX", "", {plain = true})
-        if not package:is_plat("linux") or not package:config("cap") then
+        if not package:is_plat("linux") or not package:config("libcap") then
             io.replace("CMakeLists.txt", [[CHECK_LIBRARY_EXISTS(cap cap_set_flag "" LWS_HAVE_LIBCAP)]], "", {plain = true})
         end
 
         local configs = {"-DDISABLE_WERROR=ON", "-DLWS_WITH_MINIMAL_EXAMPLES=OFF", "-DLWS_WITHOUT_TESTAPPS=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DLWS_WITH_SSL=" .. (package:config("ssl") and "ON" or "OFF"))
         table.insert(configs, "-DLWS_WITH_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DLWS_WITH_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DLWS_WITH_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
+
+        table.insert(configs, "-DLWS_WITH_SSL=" .. (package:config("ssl") and "ON" or "OFF"))
+        table.insert(configs, "-DLWS_WITH_LIBEV=" .. (package:config("libev") and "ON" or "OFF"))
+        table.insert(configs, "-DLWS_WITH_LIBUV=" .. (package:config("libuv") and "ON" or "OFF"))
+        table.insert(configs, "-DLWS_WITH_LIBEVENT=" .. (package:config("libevent") and "ON" or "OFF"))
+        table.insert(configs, "-DLWS_WITH_GLIB=" .. (package:config("glib") and "ON" or "OFF"))
 
         os.mkdir(path.join(package:buildir(), "lib", "pdb"))
         import("package.tools.cmake").install(package, configs)
