66411fa17e0647001c8f4303471264ff190af2ce
star9029 <hengxings783@gmail.com>
2024-09-08 01:18:28 +0800

    port xmake

--- packages/d/duilib_ultimate/xmake.lua
+++ packages/d/duilib_ultimate/xmake.lua
@@ -8,31 +8,22 @@ package("duilib_ultimate")
 
     add_versions("0.3", "4a650267e98d8b19818bdeb7675dcf1403017732b961620678e1d2d81f81db91")
 
-    add_configs("shared", {description = "Build shared binaries.", default = false, type = "boolean", readonly = true})
-    add_configs("runtimes", {description = "Set vs compiler runtime.", default = "MT", type = "string", readonly = true})
+    add_configs("unicode", {description = "Enable Unicode support.", default = true, type = "boolean"})
 
     add_syslinks("gdi32", "comctl32", "imm32", "uuid", "winmm")
 
     on_install("windows|!arm64", function (package)
+        local configs = {unicode = package:config("unicode")}
         if not package:config("shared") then
             package:add("defines", "UILIB_STATIC")
         end
+        if configs.unicode then
+            package:add("defines", "UNICODE", "_UNICODE")
+        end
 
-        local configs = {"DuiLib.sln", "-t:DuiLib"}
-        table.insert(configs, "/p:Configuration=" .. (package:is_debug() and "SDebug" or "SRelease"))
-        table.insert(configs, "/p:Platform=" .. (package:is_arch("x64") and "x64" or "Win32"))
-        import("package.tools.msbuild").build(package, configs, {upgrade = {"DuiLib.sln"}})
-
-        io.replace("DuiLib/Utils/downloadmgr.h", [[#pragma comment(lib,"uuid.lib")]], "", {plain = true})
-        io.replace("DuiLib/UIlib.h", [[#pragma comment( lib, "comctl32.lib" )]], "", {plain = true})
-        io.replace("DuiLib/UIlib.h", [[#pragma comment( lib, "GdiPlus.lib" )]], "", {plain = true})
-        io.replace("DuiLib/UIlib.h", [[#pragma comment( lib, "Imm32.lib" )]], "", {plain = true})
-
-        os.cp("Lib/*", package:installdir("lib"))
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package, configs)
         os.cp("bin", package:installdir())
-        os.cp ("DuiLib/**.h", package:installdir("include"), {rootdir="DuiLib"})
-        os.cp ("DuiLib/Utils/Flash11.tlb", package:installdir("include") .. "/Utils/")
-        os.cp ("DuiLib/Utils/flash11.tlh", package:installdir("include") .. "/Utils/")
     end)
 
     on_test(function (package)
