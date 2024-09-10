20aeaf3d1681afb9e054db8c87157dc341552b7c
xpxz <97490782+heheda123123@users.noreply.github.com>
2024-09-10 18:31:32 +0800

    Update xmake.lua

--- packages/a/asio2/xmake.lua
+++ packages/a/asio2/xmake.lua
@@ -13,7 +13,7 @@ package("asio2")
     add_patches("2.9", "patches/2.9/remove-const.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
 
     add_deps("asio", "cereal")
-    add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
+    add_deps("spdlog", { configs = { header_only = false } })
 
     if is_plat("mingw") then
         add_syslinks("ws2_32")
