4f23e24c8cd0b7f1415247c1558f03257d1f23f9
xpxz <97490782+heheda123123@users.noreply.github.com>
2024-09-10 18:10:16 +0800

    fix ssl support fox asio2

--- packages/a/asio2/xmake.lua
+++ packages/a/asio2/xmake.lua
@@ -8,13 +8,23 @@ package("asio2")
              "https://github.com/zhllxt/asio2.git")
 
     add_versions("v2.9", "d173e83a22f6d4ec8697ac533f4cf71051b7aa5c550d24997d991610206dd534")
+    add_configs("ssl", {description = "Build OpenSSL module", default = false, type = "boolean"})
 
     add_patches("2.9", "patches/2.9/remove-const.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
 
-    add_deps("asio", "cereal", "openssl3")
+    add_deps("asio", "cereal")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
 
+    on_load(function (package)
+        if package:config("ssl") then
+            package:add("deps", "openssl3")
+        end
+    end)
+
     on_install("windows", "linux", "macosx", "mingw", "bsd", "cross", function (package)
+        if package:config("ssl") then
+            package:add("defines", "ASIO2_ENABLE_SSL")
+        end
         os.cp("include/*", package:installdir("include"))
     end)
 
