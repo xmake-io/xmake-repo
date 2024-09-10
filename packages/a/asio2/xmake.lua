8a82e88be9d62129992899e53c3a37beeec524d7
star9029 <hengxings783@gmail.com>
2024-09-10 22:21:37 +0800

    Update xmake.lua

--- packages/a/asio2/xmake.lua
+++ packages/a/asio2/xmake.lua
@@ -8,6 +8,7 @@ package("asio2")
              "https://github.com/zhllxt/asio2.git")
 
     add_versions("v2.9", "d173e83a22f6d4ec8697ac533f4cf71051b7aa5c550d24997d991610206dd534")
+
     add_configs("ssl", {description = "Build OpenSSL module", default = false, type = "boolean"})
 
     add_patches("2.9", "patches/2.9/remove-const.patch", "6326f333ab2d0484c23bb3cd9cfd5a565030b5525d083677565a693f5f8803b6")
@@ -15,20 +16,18 @@ package("asio2")
     add_deps("asio", "cereal")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
 
-    if is_plat("mingw") then
+    if is_plat("windows", "mingw") then
         add_syslinks("ws2_32")
     end
 
     on_load(function (package)
         if package:config("ssl") then
             package:add("deps", "openssl3")
+            package:add("defines", "ASIO2_ENABLE_SSL")
         end
     end)
 
     on_install("windows", "linux", "macosx", "mingw", "bsd", "cross", function (package)
-        if package:config("ssl") then
-            package:add("defines", "ASIO2_ENABLE_SSL")
-        end
         os.cp("include/*", package:installdir("include"))
     end)
 
