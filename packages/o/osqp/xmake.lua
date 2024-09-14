35b28143f9ae86256d62f683af489ce39e692e62
star9029 <hengxings783@gmail.com>
2024-09-14 14:14:43 +0800

    osqp: add v0.6.3 version

--- packages/o/osqp/xmake.lua
+++ packages/o/osqp/xmake.lua
@@ -1,21 +1,26 @@
 package("osqp")
-
     set_homepage("https://osqp.org/")
     set_description("The Operator Splitting QP Solver")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/oxfordcontrol/osqp.git")
-    add_versions("v0.6.2", "f9fc23d3436e4b17dd2cb95f70cfa1f37d122c24")
+    add_urls("https://github.com/osqp/osqp/releases/download/$(version)/osqp-$(version)-src.tar.gz",
+             "https://github.com/osqp/osqp.git")
+
+    add_versions("v0.6.3", "285b2a60f68d113a1090767ec8a9c81a65b3af2d258f8c78a31cc3f98ba58456")
+
+    add_patches("0.6.3", "patches/0.6.3/cmake.patch", "15524f5562ccde458d7f9885b592d982b44b0239dbfbdf31cf720861ea3be6e8")
 
     add_deps("cmake")
-    on_install("windows", "macosx", "linux", "mingw", function (package)
+    add_deps("qdldl")
+
+    on_install(function (package)
         local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        if package:config("pic") ~= false then
-            table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
+        if package:config("shared") and package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
         end
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.cmake").install(package, configs, {packagedeps = "qdldl"})
     end)
 
     on_test(function (package)
