424aeb732c779b69b364b17c8477db51ca4976f7
hahahahbenny <1292983376@qq.com>
2024-09-15 15:27:25 +0800

    change the version and patch name

--- packages/j/jthread/xmake.lua
+++ packages/j/jthread/xmake.lua
@@ -3,16 +3,16 @@ package("jthread")
     set_description("The JThread package provides some classes to make use of threads easy on different platforms")
     set_license("MIT")
 
-    add_urls("https://github.com/j0r1/JThread/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/j0r1/JThread.git")
+    add_urls("https://github.com/j0r1/JThread.git")
 
-    add_versions("v1.3.3", "5e24779acff268989099a1e135f928ea4f366780d9d7466919037a1414451b95")
+    add_versions("2023.08.18", "719413043807b77448df3ba1c749798fb72ee459")
     add_deps("cmake")
 
-    add_patches("1.3.3", "patches/1.3.3/cmakeList.patch", "4c5162f128f31ebee63805db1dbed0a17fc02b2661958b80468fcffc394d7f4e")
+    add_patches("1.3.3", "patches/1.3.3/cmakelist.patch", "4c5162f128f31ebee63805db1dbed0a17fc02b2661958b80468fcffc394d7f4e")
 
     add_includedirs("include", "include/jthread")
-    on_install(function (package)
+    
+    on_install("windows", "linux", "macosx", function(package)
 
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
@@ -21,6 +21,7 @@ package("jthread")
             table.insert(configs, "-DJTHREAD_COMPILE_STATIC_ONLY=OFF")
         else 
             table.insert(configs, "-DJTHREAD_COMPILE_STATIC_ONLY=ON")
+            table.insert(configs, "-DJTHREAD_COMPILE_STATIC=ON")
         end
 
         import("package.tools.cmake").install(package, configs)
