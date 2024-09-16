54eae4a98bfa678c33a5455343d76152a102e95a
bennyWU <1292983376@qq.com>
2024-09-16 19:49:00 +0800

    change the default backend of xtd

--- packages/x/xtd/xmake.lua
+++ packages/x/xtd/xmake.lua
@@ -8,7 +8,7 @@ package("xtd")
 
     add_versions("v0.1.2", "648f7e5e2252d0db4e9432d493cec0682c059605ae3dfded793884cbbf3d1bd5")
 
-    add_configs("graphic_toolkit", {description = "Select xtd graphic toolkit.", default = "wxwidgets", type = "string", values = {"gtk3", "wxwidgets", "fltk", "gtk4", "qt5"}})
+    add_configs("graphic_toolkit", {description = "Select xtd graphic toolkit.", default = "fltk", type = "string", values = {"gtk3", "wxwidgets", "fltk", "gtk4", "qt5"}})
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     -- when building the xtd shared library, it shows Cyclic dependencies error. I have summit a issue to xtd.
     -- https://github.com/gammasoft71/xtd/issues/264#issue-2527671013
