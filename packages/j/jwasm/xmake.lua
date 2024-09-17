025bafded98a43f687dc2e6f23874c5b76f610b7
star9029 <hengxings783@gmail.com>
2024-09-18 00:25:20 +0800

    limit plat

--- packages/j/jwasm/xmake.lua
+++ packages/j/jwasm/xmake.lua
@@ -6,7 +6,7 @@ package("jwasm")
     add_urls("https://github.com/JWasm/JWasm.git")
     add_versions("2022.12.25", "7218960b65d69216693a655d928eb4c2fb6b505c")
 
-    on_install(function (package)
+    on_install("@windows", "@linux", "@macosx", "@bsd", "@mingw", "@msys", function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("jwasm")
