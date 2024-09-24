4f4f080f789ccbf96ee97611478fbf4ba6da9356
bennyWU <1292983376@qq.com>
2024-09-24 14:26:07 +0800

    add lapack manual

--- packages/i/ipopt/xmake.lua
+++ packages/i/ipopt/xmake.lua
@@ -7,7 +7,7 @@ package("ipopt")
     add_versions("3.14.16", "cc8c217991240db7eb14189eee0dff88f20a89bac11958b48625fa512fe8d104")
 
     if is_plat("linux") then
-        -- add_extsources("apt::liblapack-dev")
+        add_extsources("apt::liblapack-dev")
         add_syslinks("gfortran", "pthread")
     end
 
@@ -19,13 +19,21 @@ package("ipopt")
 
     add_includedirs("include", "include/coin-or")
 
-    -- add_links("smumps", "dmumps", "cmumps", "zmumps", "mumps_common", "esmumps", "pord", "mpiseq", "ptesmumps", "esmumps", "scotch", "scotcherr", 
-    --         "scotcherrexit", "scotchmetisv5", "scotchmetisv3", "gfortran", "dl")
-
     on_install("linux", function (package)
-        local fetch_info_mumps = package:dep("mumps"):fetch()
+        io.replace("configure", "ac_name=dsyev", "ac_name=LAPACKE_dsyev", {plain=true})
+        io.replace("configure", "ac_name=DSYEV", "ac_name=LAPACKE_dsyev", {plain=true})
 
-        local configs = {"-with-PACKAGE=yes"}
+        local fetch_info_mumps = package:dep("mumps"):fetch()
+        local fetch_info_lapack = package:dep("lapack"):fetch()
+
+        local configs = {}
+        print(fetch_info_lapack.linkdirs)
+        local lapack_flags = [[--with-lapack-lflags=-L]] .. fetch_info_lapack.linkdirs[1] .. " " 
+        for _, link in ipairs(fetch_info_lapack.links) do 
+            lapack_flags = lapack_flags .. "-l" .. link .. " "
+        end        
+        lapack_flags = lapack_flags .. [[-lgfortran -lm]]
+        table.insert(configs, lapack_flags)
         
         table.insert(configs, [[--with-mumps-cflags="-I]] .. fetch_info_mumps.sysincludedirs[1] .. [["]])
 
@@ -33,7 +41,7 @@ package("ipopt")
         for _, link in ipairs(fetch_info_mumps.links) do 
             mumps_lflags = mumps_lflags .. "-l" .. link .. " "
         end        
-        mumps_lflags = mumps_lflags  .. [[-lesmumps -lscotch -lscotcherr -lscotcherrexit -lscotchmetisv5 -lscotchmetisv3 -lgfortran -lpthread"]]
+        mumps_lflags = mumps_lflags  .. [[-lesmumps -lscotch -lscotcherr -lscotcherrexit -lscotchmetisv5 -lscotchmetisv3 -lpthread"]]
 
         table.insert(configs, mumps_lflags)
 
@@ -42,7 +50,7 @@ package("ipopt")
         if package:is_debug() then
             table.insert(configs, "--enable-debug")
         end
-        import("package.tools.autoconf").install(package, configs, {packagedeps={"mumps", "gfortran", "scotch"}})
+        import("package.tools.autoconf").install(package, configs, {packagedeps={"mumps", "scotch", "lapack", "gfortran"}})
     end)
 
     on_test(function (package)
