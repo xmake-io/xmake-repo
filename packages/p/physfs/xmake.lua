fceea7953d2b05d297b525fc9fd77d4bf895f394
star9029 <hengxings783@gmail.com>
2024-09-27 13:12:22 +0800

    Update xmake.lua

--- packages/p/physfs/xmake.lua
+++ packages/p/physfs/xmake.lua
@@ -3,10 +3,9 @@ package("physfs")
     set_description("A portable, flexible file i/o abstraction")
     set_license("zlib")
 
-    set_urls("https://github.com/icculus/physfs/archive/refs/tags/release-$(version).tar.gz",
-             "https://github.com/icculus/physfs.git")
+    set_urls("https://github.com/icculus/physfs.git")
 
-    add_versions("3.2.0", "1991500eaeb8d5325e3a8361847ff3bf8e03ec89252b7915e1f25b3f8ab5d560")
+    add_versions("2024.09.23", "74c30545031ca8cdb69b2f1ec173e77d79078093")
 
     local archivers = {
         ["zip"] = "ZIP",
@@ -24,13 +23,18 @@ package("physfs")
         add_configs(k, {description = "Enable " .. v .. " support", default = true, type = "boolean"})
     end
 
-    add_deps("cmake")
     if is_plat("windows") then
-        add_syslinks("User32", "Advapi32", "Shell32")
+        add_syslinks("user32", "advapi32", "shell32")
+    elseif is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    elseif is_plat("macosx") then
+        add_frameworks("IOKit", "Foundation")
     end
 
+    add_deps("cmake")
+
     on_install(function (package)
-        local configs = {}
+        local configs = {"-DPHYSFS_BUILD_TEST=OFF", "-DPHYSFS_BUILD_DOCS=OFF"}
         table.insert(configs, "-DPHYSFS_BUILD_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DPHYSFS_BUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
@@ -38,6 +42,11 @@ package("physfs")
             table.insert(configs, "-DPHYSFS_ARCHIVE_" .. v:upper() .. "=" .. (package:config(k) and "ON" or "OFF"))
         end
         import("package.tools.cmake").install(package, configs)
+
+        if package:is_plat("windows") then
+            local dir = package:installdir(package:config("shared") and "bin" or "lib")
+            os.trycp(path.join(package:buildir(), "physfs.pdb"), dir)
+        end
     end)
 
     on_test(function (package)
