c550f35fe98eaef58c1694d10f2f0f1c34277cf7
c8ef <c8ef@outlook.com>
2024-09-28 18:20:03 +0800

    redundant white space

--- packages/p/pcapplusplus/xmake.lua
+++ packages/p/pcapplusplus/xmake.lua
@@ -32,7 +32,7 @@ package("pcapplusplus")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
-    
+
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             #include "pcapplusplus/IPv4Layer.h"
