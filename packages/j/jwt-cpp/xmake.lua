3fcfd0dcda6df22c678a56646c78d03aa866dcb8
star9029 <hengxings783@gmail.com>
2024-09-28 17:41:36 +0800

    Update xmake.lua

--- packages/j/jwt-cpp/xmake.lua
+++ packages/j/jwt-cpp/xmake.lua
@@ -10,24 +10,48 @@ package("jwt-cpp")
     add_versions("v0.7.0", "b9eb270e3ba8221e4b2bc38723c9a1cb4fa6c241a42908b9a334daff31137406")
     add_versions("v0.6.0", "0227bd6e0356b211341075c7997c837f0b388c01379bd256aa525566a5553f03")
 
-    add_configs("picojson", {description = "Use picojson", default = true, type = "boolean"})
+    add_configs("picojson", {description = "Use picojson", default = false, type = "boolean"})
+    add_configs("ssl", {description = "Select ssl library", default = "openssl", type = "string", values = {"openssl", "openssl3", "libressl", "wolfssl"}})
+    add_configs("base64", {description = "Use picojson", default = true, type = "boolean"})
 
-    add_deps("openssl")
+    add_deps("cmake")
 
     on_load(function (package)
+        package:add("deps", "nlohmann_json", {configs = {cmake = true}})
+        package:add("deps", package:config("ssl"))
+
         if package:config("picojson") then
             package:add("deps", "picojson")
         else
             package:add("defines", "JWT_DISABLE_PICOJSON")
         end
+
+        if not package:config("base64") then
+            package:add("defines", "JWT_DISABLE_BASE64")
+        end
     end)
 
-    on_install("windows", "linux", "macosx", "bsd", "mingw", "msys", "android", "cross", function (package)
+    on_install(function (package)
         if package:config("picojson") then
             io.replace("include/jwt-cpp/jwt.h", "picojson/picojson.h", "picojson.h", {plain = true})
             io.replace("include/jwt-cpp/traits/kazuho-picojson/traits.h", "picojson/picojson.h", "picojson.h", {plain = true})
+            io.replace("CMakeLists.txt", "find_package(picojson 1.3.0 REQUIRED)", "", {plain = true})
         end
-        os.cp("include/jwt-cpp", package:installdir("include"))
+
+        local configs = {"-DJWT_BUILD_EXAMPLES=OFF", "-DJWT_EXTERNAL_NLOHMANN_JSON=ON"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DJWT_DISABLE_PICOJSON=" .. (package:config("picojson") and "OFF" or "ON"))
+        table.insert(configs, "-DJWT_EXTERNAL_PICOJSON=" .. (package:config("picojson") and "ON" or "OFF"))
+        table.insert(configs, "-DJWT_DISABLE_BASE64=" .. (package:config("base64") and "OFF" or "ON"))
+
+        local ssl = package:config("ssl")
+        if ssl:startswith("openssl") then
+            local openssl = package:dep(ssl)
+            if not openssl:is_system() then
+                table.insert(configs, "-DOPENSSL_ROOT_DIR=" .. openssl:installdir())
+            end
+        end
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
@@ -39,5 +63,7 @@ package("jwt-cpp")
                     auto decoded = jwt::decode(token);
                 }
             ]]}, {configs = {languages = "c++11"}}))
+        else
+            assert(package:has_cxxincludes("jwt-cpp/jwt.h", {configs = {languages = "c++11"}}))
         end
     end)
