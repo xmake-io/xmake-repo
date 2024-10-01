88636725f2cc71a9a66c03ef02148c8d901de9bd
Doekin <105162544+Doekin@users.noreply.github.com>
2024-10-01 17:10:03 +0800

    `npcap_sdk`: add `mingw` support with workaround for unrecognized SAL (#5390)
    * npcap_sdk: add mingw support with workaround for unrecognized SAL
    
    * npcap_sdk: add previous version 1.12 for compatibility
    
    * correct architecture specifier for mingw
    
    * fix wrong patch

--- packages/n/npcap_sdk/xmake.lua
+++ packages/n/npcap_sdk/xmake.lua
@@ -4,12 +4,17 @@ package("npcap_sdk")
 
     set_urls("https://npcap.com/dist/npcap-sdk-$(version).zip")
     add_versions("1.13", "dad1f2bf1b02b787be08ca4862f99e39a876c1f274bac4ac0cedc9bbc58f94fd")
+    add_versions("1.12", "24c4862723f61d28048a24e10eb31d2269b2152a5762410dd1caffc041871337")
 
-    on_install("windows", function (package)
-        os.cp("Include", package:installdir())
+    on_install("windows", "mingw", function (package)
+        if package:is_plat("mingw") and package:version():eq("1.13") then
+            io.replace("Include/Packet32.h", "_Post_invalid_", "", {plain = true})
+        end
+
+        os.cp("Include/*", package:installdir("include"))
         if package:is_arch("arm64") then
             os.cp("Lib/ARM64/*", package:installdir("lib"))
-        elseif package:is_arch("x86") then
+        elseif package:is_arch("x86") or package:is_arch("i386") then
             os.cp("Lib/*.lib", package:installdir("lib"))
         else
             os.cp("Lib/x64/*.lib", package:installdir("lib"))
