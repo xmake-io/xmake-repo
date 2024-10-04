835feae0af7f8100edc1a046244e1989b1615b1e
star9029 <hengxings783@gmail.com>
2024-10-04 15:52:25 +0800

    pycdc: add 2024.08.12 version

--- packages/p/pycdc/xmake.lua
+++ packages/p/pycdc/xmake.lua
@@ -1,22 +1,16 @@
 package("pycdc")
     set_homepage("https://github.com/zrax/pycdc")
     set_description("C++ python bytecode disassembler and decompiler")
+    set_license("GPL-3.0")
 
     add_urls("https://github.com/zrax/pycdc.git")
+
+    add_versions("2024.08.12", "dc6ca4ae36128f2674b5b4c9b0ce6fdda97d4df0")
     add_versions("2022.10.04", "44a730f3a889503014fec94ae6e62d8401cb75e5")
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-
-    add_deps("cmake")
-    add_deps("python", {kind = "binary"})
-
-    on_install("macosx", "linux", "windows", function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        import("package.tools.cmake").install(package, configs, {buildir = "build"})
-        os.cp("*.h", package:installdir("include"))
-        os.trycp("build/*.a", package:installdir("lib"))
-        os.trycp("build/*.lib", package:installdir("lib"))
+    on_install(function (package)
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package)
         package:addenv("PATH", "bin")
     end)
 
