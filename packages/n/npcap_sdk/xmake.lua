faf165dc71c79be7a276bf727e175e258a9514cd
star9029 <hengxings783@gmail.com>
2024-10-07 23:14:12 +0800

    libpcap: enable  windows

--- packages/n/npcap_sdk/xmake.lua
+++ packages/n/npcap_sdk/xmake.lua
@@ -3,10 +3,11 @@ package("npcap_sdk")
     set_description("Npcap is the Nmap Project's packet capture (and sending) library for Microsoft Windows.")
 
     set_urls("https://npcap.com/dist/npcap-sdk-$(version).zip")
+
     add_versions("1.13", "dad1f2bf1b02b787be08ca4862f99e39a876c1f274bac4ac0cedc9bbc58f94fd")
     add_versions("1.12", "24c4862723f61d28048a24e10eb31d2269b2152a5762410dd1caffc041871337")
 
-    on_install("windows", "mingw", function (package)
+    on_install("windows", "mingw", "msys", function (package)
         if package:is_plat("mingw") and package:version():eq("1.13") then
             io.replace("Include/Packet32.h", "_Post_invalid_", "", {plain = true})
         end
