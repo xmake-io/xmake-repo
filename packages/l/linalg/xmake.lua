28531e67591f09a0f74781cf4770d6b94448856c
star9029 <hengxings783@gmail.com>
2024-10-08 08:15:55 +0800

    remove limit

--- packages/l/linalg/xmake.lua
+++ packages/l/linalg/xmake.lua
@@ -9,16 +9,16 @@ package("linalg")
 
     add_versions("v2.2", "28640228988186edaf7729177bfab4c91170b303ad489407a4228ceb55a73ec2")
 
-    on_install("windows", "linux", "macosx", function (package)
+    on_install(function (package)
         os.cp("linalg.h", package:installdir("include"))
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-        using namespace linalg::aliases;
-        float4 test(float3 a, float3 b, float3 c) {
-            float3 n = cross(b-a, c-a);
-            return {n, -dot(n,a)};
-        }
-    ]]}, {configs = {languages = "c++11"}, includes = "linalg.h"}))
+            using namespace linalg::aliases;
+            float4 test(float3 a, float3 b, float3 c) {
+                float3 n = cross(b-a, c-a);
+                return {n, -dot(n,a)};
+            }
+        ]]}, {configs = {languages = "c++11"}, includes = "linalg.h"}))
     end)
