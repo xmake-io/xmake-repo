c8cd537eca1fc49285c35750d6ef50a2117a795f
Saikari <lin@sz.cn.eu.org>
2024-10-10 23:46:00 +0300

    Update xmake.lua to origin k/keystone/xmake.lua
    Update xmake.lua to origin k/keystone/xmake.lua

--- packages/k/keystone/xmake.lua
+++ packages/k/keystone/xmake.lua
@@ -9,13 +9,17 @@ package("keystone")
     add_versions("0.9.2", "c9b3a343ed3e05ee168d29daf89820aff9effb2c74c6803c2d9e21d55b5b7c24")
 
     add_deps("cmake")
+    add_deps("python 3.x", {kind = "binary"})
 
-    if is_plat("windows") then
-        add_syslinks("shell32")
+    if is_plat("windows", "mingw") then
+        add_syslinks("shell32", "ole32", "uuid")
     end
 
     on_load(function (package)
-        if not package:is_cross() then
+        if package:is_cross() or package:is_plat("mingw") or (package:is_plat("windows") and package:config("shared")) then
+            package:data_set("build_libs_only", true)
+        end
+        if not package:data("build_libs_only") then
             package:addenv("PATH", "bin")
         end
     end)
@@ -24,20 +28,23 @@ package("keystone")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        if package:is_cross() then
+        if package:data("build_libs_only") then
             table.insert(configs, "-DBUILD_LIBS_ONLY=ON")
-        elseif package:is_plat("windows") then
-            table.insert(configs, "-DKEYSTONE_BUILD_STATIC_RUNTIME=" .. (package:has_runtime("MT", "MTd") and "ON" or "OFF"))
         end
+        if package:is_plat("windows") then
+            table.insert(configs, "-DKEYSTONE_BUILD_STATIC_RUNTIME=" .. (package:has_runtime("MT", "MTd") and "ON" or "OFF"))
+        elseif package:is_plat("android") then
+            -- support for ndk >= r27
+            -- https://github.com/android/ndk/issues/2032
+            table.insert(configs, "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW")
+        end
+        io.replace("CMakeLists.txt", "add_subdirectory(suite/fuzz)", "", {plain = true})
         import("package.tools.cmake").install(package, configs)
         os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
-        if not package:is_cross() and not package:is_plat("windows") then
-            os.vrun('kstool -b x64 "mov rax, 1; ret"')
-        end
-        if package:is_plat("windows") and package:config("static") then
+        if not package:data("build_libs_only") then
             os.vrun('kstool -b x64 "mov rax, 1; ret"')
         end
         assert(package:has_cfuncs("ks_version", {includes = "keystone/keystone.h"}))
