697e3b88670ff69536d651718fb047a35eec7ebe
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-10-10 21:48:36 +0100

    libhybris: use ndk dep

--- packages/l/libhybris/xmake.lua
+++ packages/l/libhybris/xmake.lua
@@ -5,15 +5,25 @@ package("libhybris")
     add_urls("https://github.com/libhybris/libhybris.git")
     add_versions("2024.09.01", "936279916605003fba95a0f3629a6bc5e6caa343")
 
-    add_deps("autoconf", "automake", "libtool", "pkgconf")
+    add_deps("autoconf", "automake", "libtool", "pkgconf", "ndk")
 
     on_install("linux", function (package)
-        os.cd("hybris")
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         if package:is_debug() then
             table.insert(configs, "--enable-debug")
         end
+        local ndk_version = package:dep("ndk"):version()
+        local ndk_version_major = ndk_version:major() or 0
+        local ndk_version_minor = ndk_version:minor() or 0
+        local ndk_version_patch = ndk_version:patch() or 0
+        os.run(
+            "sh utils/extract-headers.sh --version %s %s %s", 
+            ndk_version_major .. '.' .. ndk_version_minor .. '.' .. ndk_version_patch,
+            package:dep("ndk"):installdir(),
+            package:installdir("include")
+        )
+        os.cd("hybris")
         import("package.tools.autoconf").install(package, configs)
     end)
 
