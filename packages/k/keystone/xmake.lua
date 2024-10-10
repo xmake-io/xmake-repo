632f5baca6e0bfc9d8dd9d0324fb09896b2aeb6d
ruki <waruqi@gmail.com>
2024-10-11 00:59:41 +0800

    fix ndk

--- packages/k/keystone/xmake.lua
+++ packages/k/keystone/xmake.lua
@@ -30,14 +30,14 @@ package("keystone")
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:data("build_libs_only") then
             table.insert(configs, "-DBUILD_LIBS_ONLY=ON")
-        elseif package:is_plat("windows") then
+        end
+        if package:is_plat("windows") then
             table.insert(configs, "-DKEYSTONE_BUILD_STATIC_RUNTIME=" .. (package:has_runtime("MT", "MTd") and "ON" or "OFF"))
         elseif package:is_plat("android") then
             -- support for ndk >= r27
-            io.insert("CMakeLists.txt", 1, [[if (POLICY CMP0057)
-              cmake_policy(SET CMP0057 NEW)
-            endif()]])
-            end
+            -- https://github.com/android/ndk/issues/2032
+            table.insert(configs, "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW")
+        end
         io.replace("CMakeLists.txt", "add_subdirectory(suite/fuzz)", "", {plain = true})
         import("package.tools.cmake").install(package, configs)
         os.cp("include", package:installdir())
