decd231b50e35288875b64e36318e82109c84d5e
star9029 <hengxings783@gmail.com>
2024-10-12 15:47:39 +0800

    aws-c-io: fix android

--- packages/a/aws-c-io/xmake.lua
+++ packages/a/aws-c-io/xmake.lua
@@ -39,7 +39,11 @@ package("aws-c-io")
 
         local cmakedir = path.unix(package:dep("aws-c-common"):installdir("lib", "cmake"))
 
-        local configs = {"-DBUILD_TESTING=OFF", "-DCMAKE_MODULE_PATH=" .. cmakedir}
+        local configs = {
+            "-DBUILD_TESTING=OFF",
+            "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
+            "-DCMAKE_MODULE_PATH=" .. cmakedir,
+        }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DENABLE_SANITIZERS=" .. (package:config("asan") and "ON" or "OFF"))
