0d1d44b2b55479decda6e487bc02ec49f389b849
star9029 <hengxings783@gmail.com>
2024-10-15 23:24:01 +0800

    patch cmake

--- packages/l/ls-qpack/xmake.lua
+++ packages/l/ls-qpack/xmake.lua
@@ -10,7 +10,8 @@ package("ls-qpack")
     add_versions("v2.5.4", "56b96190a1943d75ef8d384b13cd4592a72e3e2d84284f78d7f8adabbc717f3e")
     add_versions("v2.5.3", "075a05efee27961eac5ac92a12a6e28a61bcd6c122a0276938ef993338577337")
 
-    add_patches("v2.5.3", "patches/v2.5.3/fix-cmake-install.patch", "7d819b620b5e2bd34ef58a91bf20d882883c7525def9f9f80313b64cba5e5239")
+    add_patches("2.5.5", "patches/2.5.5/cmake.patch", "23fd785c3db2e1b43ead464b0ee8d12e9f290fbfdf818c3238cba316df295f08")
+    add_patches("2.5.3", "patches/v2.5.3/fix-cmake-install.patch", "7d819b620b5e2bd34ef58a91bf20d882883c7525def9f9f80313b64cba5e5239")
 
     add_deps("cmake")
     add_deps("xxhash")
@@ -19,6 +20,8 @@ package("ls-qpack")
         if package:gitref() or package:version():ge("2.5.5") then
             if is_subhost("windows") then
                 package:add("deps", "pkgconf")
+            else
+                package:add("deps", "pkg-config")
             end
         end
     end)
@@ -31,7 +34,12 @@ package("ls-qpack")
             opt.packagedeps = "xxhash"
         end
 
-        local configs = {"-DLSQPACK_TESTS=OFF", "-DLSQPACK_BIN=OFF", "-DLSQPACK_XXH=OFF",}
+        local configs = {
+            "-DLSQPACK_TESTS=OFF",
+            "-DLSQPACK_BIN=OFF",
+            "-DLSQPACK_XXH=OFF",
+            "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
+        }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") and package:config("shared") then
