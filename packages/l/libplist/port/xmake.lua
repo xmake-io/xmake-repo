78b66bb73dfdfea4ea0aa99b35909e6edaa4d6ab
star9029 <hengxings783@gmail.com>
2024-10-16 17:29:05 +0800

    remove main files

--- packages/l/libplist/port/xmake.lua
+++ packages/l/libplist/port/xmake.lua
@@ -2,17 +2,21 @@ option("version", {default = "2.6.0"})
 
 add_rules("mode.debug", "mode.release")
 
-if not is_plat("windows") then
+if not is_plat("windows", "mingw", "msys") then
     add_defines("HAVE_STRNDUP")
 end
 
-if has_config("version") then
-    add_defines("PACKAGE_VERSION=\"" .. get_config("version") .. "\"")
+local version = get_config("version")
+if version then
+    set_version(version, {soname = true})
+    add_defines("PACKAGE_VERSION=\"" .. version .. "\"")
 end
+
+set_languages("c++20")
 
 target("plist")
     set_kind("$(kind)")
-    add_files("libcnary/*.c", "src/*.c")
+    add_files("libcnary/*.c|cnary.c", "src/*.c")
     add_includedirs("src", "include", "libcnary/include")
     add_headerfiles("include/(plist/*.h)")
 
