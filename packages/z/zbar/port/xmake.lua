a7e6fb255f5059a2c83df3560caaad2b287b42f7
Doekin <Leetimemp@gmail.com>
2024-10-18 00:45:13 +0800

    Build with xmake port instead of autotools

---
+++ packages/z/zbar/port/xmake.lua
@@ -0,0 +1,45 @@
+add_rules("mode.debug", "mode.release")
+
+add_requires("libiconv")
+
+target("zbar")
+    set_kind("$(kind)")
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
+
+    add_packages("libiconv")
+    
+    add_includedirs("include")
+    add_includedirs("zbar")
+
+    add_headerfiles("include/zbar.h", "include/zbar/Scanner.h", "include/zbar/Decoder.h",
+        "include/zbar/Exception.h", "include/zbar/Symbol.h", "include/zbar/Image.h",
+        "include/zbar/ImageScanner.h", "include/zbar/Video.h", "include/zbar/Window.h",
+        "include/zbar/Processor.h")
+
+    add_files(
+            "zbar/config.c",
+            "zbar/error.c", 
+            "zbar/symbol.c",
+            "zbar/image.c", 
+            "zbar/convert.c",
+            "zbar/processor.c", 
+            "zbar/processor/lock.c",
+            "zbar/refcnt.c", 
+            "zbar/window.c", 
+            "zbar/video.c",
+            "zbar/img_scanner.c", 
+            "zbar/scanner.c",
+            "zbar/decoder.c", 
+            "zbar/misc.c",
+            "zbar/sqcode.c")
+    -- pdf417 is incomplete
+    add_files("zbar/decoder/*.c|pdf417.c")
+    add_files("zbar/qrcode/*.c")
+    if is_plat("windows", "mingw") then
+        add_files("zbar/processor/win.c", "zbar/libzbar.rc")
+    else 
+        add_files("zbar/processor/posix.c")
+    end
--- packages/z/zbar/xmake.lua
+++ packages/z/zbar/xmake.lua
@@ -6,62 +6,20 @@ package("zbar")
     add_urls("https://github.com/mchehab/zbar/archive/refs/tags/$(version).tar.gz")
     add_versions("0.23.93", "212dfab527894b8bcbcc7cd1d43d63f5604a07473d31a5f02889e372614ebe28")
 
-    if is_plat("linux") then
+    if is_plat("linux", "bsd") then
         add_syslinks("pthread")
     end
 
-    add_deps("autoconf", "automake", "libtool", "pkg-config")
-    add_deps("gettext", {host = true , private = true})
     if is_plat("macosx") then
         add_deps("libiconv", {system = true})
     else
         add_deps("libiconv")
     end
 
-    on_install("macosx", "linux", "android@linux,macosx", function (package)
-        local configs = {   "--disable-video",
-                            "--without-gtk",
-                            "--without-python",
-                            "--without-qt",
-                            "--without-java",
-	                        "--without-imagemagick",
-                            "--without-dbus",
-                        }
-        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-        if package:is_debug() then
-            table.insert(configs, "--enable-debug")
-        end
-        if package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
-        end
-        if package:is_plat("android") then
-            table.insert(configs, "--disable-pthread")
-        end
-
-        local cflags = {}
-        local ldflags = {}
-        local depinfo = package:dep("libiconv"):fetch()
-        if depinfo then
-            for _, includedir in ipairs(depinfo.includedirs or depinfo.sysincludedirs) do
-                table.insert(cflags, "-I" .. includedir)
-            end
-            for _, linkdir in ipairs(depinfo.linkdirs) do
-                table.insert(ldflags, "-L" .. linkdir)
-            end
-            for _, link in ipairs(depinfo.links) do
-                table.insert(ldflags, "-l" .. link)
-            end
-        end
-
-        local libtool = package:dep("libtool")
-        local autoconf = package:dep("autoconf")
-        if libtool and autoconf then
-            os.vrun("autoreconf --force --install -I" .. libtool:installdir("share", "aclocal") .. " -I" .. autoconf:installdir("share", "autoconf", "autoconf"))
-        else
-            os.vrun("autoreconf --force --install")
-        end    
-        import("package.tools.autoconf").install(package, configs, {cflags = cflags, ldflags = ldflags})
+    on_install(function (package)
+        os.cp(path.join(package:scriptdir(), "port", "config.h"), "include/config.h")
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
