331dcff4ec709e1e2783b1db129ce5ff5eeeb532
Doekin <Leetimemp@gmail.com>
2024-10-18 10:47:46 +0800

    Disable sys/time.h header inclusion

--- packages/z/zbar/port/config.h
+++ packages/z/zbar/port/config.h
@@ -294,7 +294,7 @@
 #define HAVE_SYS_TIMES_H 1
 
 /* Define to 1 if you have the <sys/time.h> header file. */
-#define HAVE_SYS_TIME_H 1
+/* #undef HAVE_SYS_TIME_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
