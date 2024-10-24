20f8bcdc80bed684ed146f4cb4f29f12e2c6dcc0
Doekin <Leetimemp@gmail.com>
2024-10-24 15:23:55 +0800

    Add checks for Windows on ARM and macosx

--- packages/g/gperftools/xmake.lua
+++ packages/g/gperftools/xmake.lua
@@ -23,6 +23,20 @@ package("gperftools")
 
     add_deps("cmake")
 
+    if on_check then
+        on_check("windows", function (package)
+            assert(package:config("minimal"), "package(gperftools): supports only minimal mode on Windows")
+            if package:is_arch("arm.*") then
+                assert(package:version():ge("2.11"), "package(gperftools): requires version >= 2.11 for Windows on ARM")
+            end
+        end)
+        on_check("macosx", function (package)
+            if macos.version():gt("12") then
+                assert(package:version():ge("2.11"), "package(gperftools): requires version >= 2.11 for macOS 13+")
+            end
+        end)
+    end
+
     on_load(function (package)
         if package:is_plat("linux") and package:config("unwind") then
             package:add("deps", "libunwind")
