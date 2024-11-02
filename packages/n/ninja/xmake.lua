bedf251f00512127f725be00942db142e4294434
xq114 <1140735506@qq.com>
2024-11-02 12:23:02 +0800

    fix ninja url

--- packages/n/ninja/xmake.lua
+++ packages/n/ninja/xmake.lua
@@ -7,10 +7,10 @@ package("ninja")
 
     if is_host("windows") then
         if os.arch() == "arm64" then
-            set_urls("https://github.com/ninja-build/ninja/releases/download/v$(version)/ninja-winarm64.zip")
+            set_urls("https://github.com/ninja-build/ninja/releases/download/$(version)/ninja-winarm64.zip")
             add_versions("v1.12.1", "79c96a50e0deafec212cfa85aa57c6b74003f52d9d1673ddcd1eab1c958c5900")
         else
-            set_urls("https://github.com/ninja-build/ninja/releases/download/v$(version)/ninja-win.zip")
+            set_urls("https://github.com/ninja-build/ninja/releases/download/$(version)/ninja-win.zip")
             add_versions("v1.9.0", "2d70010633ddaacc3af4ffbd21e22fae90d158674a09e132e06424ba3ab036e9")
             add_versions("v1.10.1", "5d1211ea003ec9760ad7f5d313ebf0b659d4ffafa221187d2b4444bc03714a33")
             add_versions("v1.10.2", "bbde850d247d2737c5764c927d1071cbb1f1957dcabda4a130fa8547c12c695f")
@@ -19,7 +19,7 @@ package("ninja")
             add_versions("v1.12.1", "f550fec705b6d6ff58f2db3c374c2277a37691678d6aba463adcbb129108467a")
         end
     elseif is_host("macosx") then
-        set_urls("https://github.com/ninja-build/ninja/releases/download/v$(version)/ninja-mac.zip")
+        set_urls("https://github.com/ninja-build/ninja/releases/download/$(version)/ninja-mac.zip")
         add_versions("v1.9.0", "26d32a79f786cca1004750f59e545199bf110e21e300d3c2424c1fddd78f28ab")
         add_versions("v1.10.1", "0bd650190d4405c15894055e349d9b59d5690b0389551d757c5ed2d3841972d1")
         add_versions("v1.10.2", "6fa359f491fac7e5185273c6421a000eea6a2f0febf0ac03ac900bd4d80ed2a5")
@@ -27,7 +27,7 @@ package("ninja")
         add_versions("v1.11.1", "482ecb23c59ae3d4f158029112de172dd96bb0e97549c4b1ca32d8fad11f873e")
         add_versions("v1.12.1", "89a287444b5b3e98f88a945afa50ce937b8ffd1dcc59c555ad9b1baf855298c9")
     elseif is_host("linux", "bsd") then
-        add_urls("https://github.com/ninja-build/ninja/archive/refs/tags/v$(version).tar.gz",
+        add_urls("https://github.com/ninja-build/ninja/archive/refs/tags/$(version).tar.gz",
                  "https://github.com/ninja-build/ninja.git")
         add_versions("v1.9.0", "5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9")
         add_versions("v1.10.1", "a6b6f7ac360d4aabd54e299cc1d8fa7b234cd81b9401693da21221c62569a23e")
