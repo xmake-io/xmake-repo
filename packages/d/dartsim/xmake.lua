74ed9baa18a46dc49f7f5f5c548a6d0eac646404
Inari0 <42310255+xq114@users.noreply.github.com>
2024-11-04 13:49:59 +0800

    dart 6.14.5 (#5659)

--- packages/d/dartsim/xmake.lua
+++ packages/d/dartsim/xmake.lua
@@ -6,6 +6,7 @@ package("dartsim")
 
     add_urls("https://github.com/dartsim/dart/archive/refs/tags/$(version).tar.gz",
              "https://github.com/dartsim/dart.git")
+    add_versions("v6.14.5", "eb89cc01f4f48c399b055d462d8ecd2a3f846f825a35ffc67f259186b362e136")
     add_versions("v6.14.4", "f5fc7f5cb1269cc127a1ff69be26247b9f3617ce04ff1c80c0f3f6abc7d9ab70")
     add_versions("v6.13.0", "4da3ff8cee056252a558b05625a5ff29b21e71f2995e6d7f789abbf6261895f7")
     add_versions("v6.14.2", "6bbaf452f8182b97bf22adeab6cc7f3dc1cd2733358543131fa130e07c0860fc")
@@ -26,6 +27,8 @@ package("dartsim")
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
         add_cxxflags("/permissive-")
         add_syslinks("user32")
+        -- https://gitlab.kitware.com/cmake/cmake/-/issues/20222
+        set_policy("package.cmake_generator.ninja", false)
     end
 
     add_deps("cmake")
@@ -38,6 +41,9 @@ package("dartsim")
         end
         if package:config("dartpy") then
             package:add("deps", "python 3.x")
+        end
+        if package:config("openscenegraph") then
+            package:add("deps", "imgui")
         end
     end)
 
@@ -52,6 +58,7 @@ package("dartsim")
         io.replace("cmake/DARTFindDependencies.cmake", "dart_check_required_package(fcl \"fcl\")", "dart_check_required_package(fcl \"fcl\")\ntarget_link_libraries(fcl INTERFACE ccd)", {plain = true})
         io.replace("cmake/DARTFindDependencies.cmake", "check_cxx_source_compiles%(.-\".-\".-(ASSIMP.-DEFINED)%)", "set(%1 1)")
         local configs = {
+            "-DDART_USE_SYSTEM_IMGUI=ON",
             "-DDART_SKIP_lz4=ON",
             "-DDART_SKIP_flann=ON",
             "-DDART_SKIP_IPOPT=ON",
@@ -70,6 +77,10 @@ package("dartsim")
         table.insert(configs, "-DDART_BUILD_DARTPY=" .. (package:config("dartpy") and "ON" or "OFF"))
         table.insert(configs, "-DDART_BUILD_GUI_OSG=" .. (package:config("openscenegraph") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
+        local suffix = package:is_debug() and "d" or ""
+        for _, lib in ipairs({"dart-collision-bullet", "dart-collision-ode", "dart-gui-osg", "dart-gui", "dart-optimizer-ipopt", "dart-optimizer-nlopt", "dart-optimizer-pagmo", "dart-utils-urdf", "dart-utils", "dart", "dart-external-odelcpsolver", "dart-external-lodepng"}) do
+            package:add("links", lib .. suffix)
+        end
     end)
 
     on_test(function (package)
