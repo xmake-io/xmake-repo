9c4f3c61dc07d717b6fa98fc5cfd159b4f4fc09e
star9029 <hengxings783@gmail.com>
2024-11-12 19:10:01 +0800

    add check

--- packages/z/zpp_bits/xmake.lua
+++ packages/z/zpp_bits/xmake.lua
@@ -9,15 +9,21 @@ package("zpp_bits")
 
     add_versions("v4.4.25", "d4afb8cf73aec19686928445e912dbbe8d39bffdac43ea69b4781f145195a09e")
 
+    if on_check then
+        on_check("android", function (package)
+            local ndk = package:toolchain("ndk"):config("ndkver")
+            assert(ndk and tonumber(ndk) > 22, "package(zpp_bits) require ndk version > 22")
+        end)
+    end
+
     on_install(function (package)
         os.vcp("zpp_bits.h", package:installdir("include"))
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <zpp_bits.h>
             void test() {
                 auto [data, in, out] = zpp::bits::data_in_out();
             }
-        ]]}, {configs = {languages = "c++20"}}))
+        ]]}, {configs = {languages = "c++20"}, includes = "zpp_bits.h"}))
     end)
