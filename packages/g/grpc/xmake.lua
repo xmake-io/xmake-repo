c4914a50d16f30ac3085b8fcc8c40469a15e9114
ruki <waruqi@gmail.com>
2024-11-20 01:17:00 +0000

    Update grpc to v1.68.0

--- packages/g/grpc/xmake.lua
+++ packages/g/grpc/xmake.lua
@@ -5,6 +5,7 @@ package("grpc")
 
     add_urls("https://github.com/grpc/grpc/archive/refs/tags/$(version).zip",
              "https://github.com/grpc/grpc.git")
+    add_versions("v1.68.0", "e7529cf6ceb5f140af28cae92a699a519b14a3180a579a679cd2e9f24ab8c6cb")
     add_versions("v1.51.3", "17720fd0a690e904a468b4b3dae6fa5ec40b0d1f4d418e2ca092e2f92f06fce0")
     add_versions("v1.62.1", "f672a3a3b370f2853869745110dabfb6c13af93e17ffad4676a0b95b5ec204af")
 
