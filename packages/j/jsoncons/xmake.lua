2c7603b47e8fb62f1b0aef1cae3cc82cf17233b4
star9029 <hengxings783@gmail.com>
2024-11-30 14:19:17 +0800

    fix android

--- packages/j/jsoncons/xmake.lua
+++ packages/j/jsoncons/xmake.lua
@@ -11,7 +11,6 @@ package("jsoncons")
     add_versions("v0.177.0", "a381d58489f143a3a515484f4ad6e32ae4d977033e1a455fecf8cdc4e2c9a49e")
     add_versions("v0.176.0", "2eb50b5cbe204265fef96c052511ed6e3b8808935c6e2c8d28e0aba7b08fda33")
     add_versions("v0.170.2", "0ff0cd407f6b27dea66a3202bc8bc2e043ec1614419e76840eda5b5f8045a43a")
-    add_versions("v0.158.0", "cf49ed3ecc3bf005c07f8e003e838e52adcfd383eb6faf52bffa1caf7bf19d30")
 
     add_configs("cmake", {description = "Use cmake build system", default = true, type = "boolean"})
 
@@ -23,7 +22,10 @@ package("jsoncons")
 
     on_install(function (package)
         if package:config("cmake") then
-            import("package.tools.cmake").install(package, {"-DJSONCONS_BUILD_TESTS=OFF"})
+            import("package.tools.cmake").install(package, {
+                "-DJSONCONS_BUILD_TESTS=OFF",
+                "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
+            })
         else
             os.cp("include", package:installdir())
         end
