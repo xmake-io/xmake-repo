e1b9ad14ffc4855386077641d9c4b87a136c6edc
star9029 <hengxings783@gmail.com>
2024-11-30 14:53:20 +0800

    jsoncons: add v0.178.0 version (#5875)
    * jsoncons: add v0.178.0 version
    
    * fix android

--- packages/j/jsoncons/xmake.lua
+++ packages/j/jsoncons/xmake.lua
@@ -1,20 +1,34 @@
 package("jsoncons")
-
     set_kind("library", {headeronly = true})
     set_homepage("https://danielaparker.github.io/jsoncons/")
     set_description("A C++, header-only library for constructing JSON and JSON-like data formats, with JSON Pointer, JSON Patch, JSONPath, JMESPath, CSV, MessagePack, CBOR, BSON, UBJSON")
     set_license("BSL-1.0")
 
-    set_urls("https://github.com/danielaparker/jsoncons/archive/$(version).zip",
+    set_urls("https://github.com/danielaparker/jsoncons/archive/refs/tags/$(version).tar.gz",
              "https://github.com/danielaparker/jsoncons.git")
 
-    add_versions("v0.177.0", "ce9f0ee1dbcdc67733cf9e50b038f81d36121b800f8d12a3d89ea5232457edd6")
-    add_versions("v0.176.0", "71a618219b62a2bbcc46efac98696574581e343cd98ef33e5e1bd8db182005d9")
-    add_versions("v0.170.2", "81ac768eecb8cf2613a09a9d081294895d7afd294b841166b4e1378f0acfdd6e")
-    add_versions("v0.158.0", "7ad7cc0e9c74df495dd16b818758ec2e2a5b7fef8f1852841087fd5e8bb6a6cb")
+    add_versions("v0.178.0", "c531b4288bb08c9c2b36fba53f568bc800e93656830bcffc18a87a3af1f46290")
+    add_versions("v0.177.0", "a381d58489f143a3a515484f4ad6e32ae4d977033e1a455fecf8cdc4e2c9a49e")
+    add_versions("v0.176.0", "2eb50b5cbe204265fef96c052511ed6e3b8808935c6e2c8d28e0aba7b08fda33")
+    add_versions("v0.170.2", "0ff0cd407f6b27dea66a3202bc8bc2e043ec1614419e76840eda5b5f8045a43a")
+
+    add_configs("cmake", {description = "Use cmake build system", default = true, type = "boolean"})
+
+    on_load(function (package)
+        if package:config("cmake") then
+            package:add("deps", "cmake")
+        end
+    end)
 
     on_install(function (package)
-        os.cp("include", package:installdir())
+        if package:config("cmake") then
+            import("package.tools.cmake").install(package, {
+                "-DJSONCONS_BUILD_TESTS=OFF",
+                "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
+            })
+        else
+            os.cp("include", package:installdir())
+        end
     end)
 
     on_test(function (package)
