8c773d7aa37f324b71f5974614b893630d90eee0
star9029 <hengxings783@gmail.com>
2024-12-02 07:43:15 +0800

    libdicom: support android

--- packages/l/libdicom/xmake.lua
+++ packages/l/libdicom/xmake.lua
@@ -18,7 +18,7 @@ package("libdicom")
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
 
-    on_install("!android and (windows|!arm64 or !windows)", function (package)
+    on_install("windows|!arm64 or !windows", function (package)
         local configs = {"-Dtests=false"}
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         import("package.tools.meson").install(package, configs)
