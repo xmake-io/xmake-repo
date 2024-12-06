0dffd2bbe585df7fd003ef2ebb27bd014f6e9038
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-12-06 08:25:14 +0000

    node-addon-api: use lambda instead

--- packages/n/node-addon-api/xmake.lua
+++ packages/n/node-addon-api/xmake.lua
@@ -16,11 +16,7 @@ package("node-addon-api")
     add_deps("node-api-headers")
 
     on_load(function(package)
-        if package:config("napi_version") then
-            package:add("defines", "NAPI_VERSION=" .. package:config("napi_version"))
-        else
-            package:add("defines", "NAPI_VERSION=" .. package:version():major())
-        end
+        package:add("defines", "NAPI_VERSION=" .. (package:config("napi_version") or package:version():major()))
         if not package:config("deprecated") then
             package:add("defines", "NODE_ADDON_API_DISABLE_DEPRECATED")
         end
