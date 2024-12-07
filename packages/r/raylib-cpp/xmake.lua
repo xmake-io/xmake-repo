dd09f37418b5ef24d2ed4b4caa8ef806a25671b3
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2024-12-07 13:10:15 +0000

    Update Raylib-cpp to v5.5.0 (#5923)
    * add v5.5.0
    
    * fix: add install limits
    
    * fix: use raylib 5.x
    
    * freebsd -> bsd
    
    * fix: raylib not supported on bsd
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/r/raylib-cpp/xmake.lua
+++ packages/r/raylib-cpp/xmake.lua
@@ -7,6 +7,7 @@ package("raylib-cpp")
     add_urls("https://github.com/RobLoach/raylib-cpp/archive/refs/tags/$(version).tar.gz", 
              "https://github.com/RobLoach/raylib-cpp.git")
 
+    add_versions("v5.5.0", "bcb4a4e241a95376e8562aa77c29976ed0921235a9f5326822130a7bcf4860a5")
     add_versions("v5.0.2", "d3a718170882bc873c973a19a824d7fa4bfd9d0087b4778057231409a240920d")
     add_versions("v5.0.1", "6d10469019700fd5993db9a18bdd0ed025105b1bf7dd8916e353eef8bfac6355")
 
