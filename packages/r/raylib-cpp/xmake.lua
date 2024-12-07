d530a58f6a3c42bbcd59cb047cc2f10a89db643f
star9029 <hengxings783@gmail.com>
2024-12-07 13:43:44 +0800

    freebsd -> bsd

--- packages/r/raylib-cpp/xmake.lua
+++ packages/r/raylib-cpp/xmake.lua
@@ -7,13 +7,13 @@ package("raylib-cpp")
     add_urls("https://github.com/RobLoach/raylib-cpp/archive/refs/tags/$(version).tar.gz", 
              "https://github.com/RobLoach/raylib-cpp.git")
 
+    add_versions("v5.5.0", "bcb4a4e241a95376e8562aa77c29976ed0921235a9f5326822130a7bcf4860a5")
     add_versions("v5.0.2", "d3a718170882bc873c973a19a824d7fa4bfd9d0087b4778057231409a240920d")
     add_versions("v5.0.1", "6d10469019700fd5993db9a18bdd0ed025105b1bf7dd8916e353eef8bfac6355")
-    add_versions("v5.5.0", "bcb4a4e241a95376e8562aa77c29976ed0921235a9f5326822130a7bcf4860a5")
 
 	add_deps("raylib 5.x")
 
-    on_install("windows", "linux", "freebsd", "macosx", "mingw", function (package)
+    on_install("windows", "linux", "macosx", "bsd", "mingw", function (package)
         os.cp("include/*.hpp", package:installdir("include/raylib-cpp"))
     end)
 
