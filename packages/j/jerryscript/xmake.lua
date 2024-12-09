aa5512a7c2f2060fc1592ba00f6a9dc3d659537b
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-12-09 20:32:55 +0000

    jerryscript: update patch

--- packages/j/jerryscript/xmake.lua
+++ packages/j/jerryscript/xmake.lua
@@ -7,7 +7,7 @@ package("jerryscript")
 
     add_versions("2024.12.03", "c509a06669bd39301fdf0d36305a69689f51919e")
 
-    add_patches("2024.12.03", "patches/2024.12.03/enum.patch", "2bc6d55d5a0f8cda1775eb4da1377b33455602166cd039c616b0fd65e0cd2c75")
+    add_patches("2024.12.03", "patches/2024.12.03/enum.patch", "ad89e79baf928e678d362cd3a50af70550a40b4d087403f11ec437d50273880d")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
