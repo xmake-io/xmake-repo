91e998f2a72f1c9b1eccb89c8943747279a78137
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-12-09 22:08:01 +0000

    Revert "jerryscript: fix patch"
    This reverts commit 0a6fd56bf0029906721f280fcb2a563f0160e6c4.

--- packages/j/jerryscript/xmake.lua
+++ packages/j/jerryscript/xmake.lua
@@ -7,7 +7,7 @@ package("jerryscript")
 
     add_versions("2024.12.03", "c509a06669bd39301fdf0d36305a69689f51919e")
 
-    add_patches("2024.12.03", "patches/2024.12.03/enum.patch", "c7ba3678204a5edfcf1f5695ce760a2b3a2d84cbcb5bf2fce0547b7d339a947f")
+    add_patches("2024.12.03", "patches/2024.12.03/enum.patch", "d8fe23c305a084e400738e7be830691e0dad62699cfbe747e0366683a7c0fc9b")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
