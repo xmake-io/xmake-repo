f978bb63bf3d81bba1c633bf2a1bd6e48f91c8fc
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-12-10 21:11:05 +0000

    jerryscript: disable clang

--- packages/j/jerryscript/xmake.lua
+++ packages/j/jerryscript/xmake.lua
@@ -15,6 +15,12 @@ package("jerryscript")
 
     add_deps("cmake")
 
+    if on_check then
+        on_check("linux", function (package)
+            assert(not package:has_tool("cxx", "clang", "clangxx"), "package(jerryscript): Linux Clang is not supported.")
+        end)
+    end
+
     on_install("!mingw and !macosx", function (package)
         local configs = {
             "-DJERRY_CMDLINE=" .. (package:config("cli") and "ON" or "OFF"),
