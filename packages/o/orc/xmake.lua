cee786550fb6f098f5bb6d9a8bc3e6f7d399d480
star9029 <hengxings783@gmail.com>
2024-12-17 01:36:16 +0800

    limit plat

--- packages/o/orc/xmake.lua
+++ packages/o/orc/xmake.lua
@@ -6,7 +6,7 @@ package("orc")
     add_urls("https://github.com/apache/orc/archive/refs/tags/$(version).tar.gz",
              "https://github.com/apache/orc.git")
 
-    add_versions("v2.0.3","7920c7c7644f31c5519befa18f8f949cdf53420603b621bd85d214b516e25ff3")
+    add_versions("v2.0.3", "7920c7c7644f31c5519befa18f8f949cdf53420603b621bd85d214b516e25ff3")
 
     add_configs("tools", {description = "build command line tools", default = false, type = "boolean"})
     add_configs("avx512", {description = "build the C++ library with AVX512 enabled if possible", default = false, type = "boolean"})
@@ -15,7 +15,7 @@ package("orc")
         add_extsources("pacman::apache-orc")
     end
 
-    if is_plat("bsd", "linux") then
+    if is_plat("linux", "bsd") then
         add_syslinks("pthread", "m")
     end
 
@@ -24,11 +24,14 @@ package("orc")
 
     on_check(function (package)
         if package:is_arch("arm.*") then
-            raise("package(orc) unsupported arm arch")
+            raise("package(orc) unsupported arm architectures")
+        end
+        if package:is_cross() then
+            raise("package(orc) unsupported cross-compilation")
         end
     end)
 
-    on_install(function (package)
+    on_install("windows", "linux", "macosx", "bsd", function (package)
         local configs = {
             "-DBUILD_JAVA=OFF",
             "-DBUILD_CPP_TESTS=OFF",
