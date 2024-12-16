372703125f9259ddfdc635bcde80bf6ebd259561
Altina Orion <139891033+Altina-oz@users.noreply.github.com>
2024-12-16 23:11:30 +0800

    New package: google-cloud-cpp
    Google Cloud Platform C++ Client Libraries

---
+++ packages/g/google-cloud-cpp/xmake.lua
@@ -0,0 +1,41 @@
+package("google-cloud-cpp")
+    set_homepage("https://github.com/googleapis/google-cloud-cpp")
+    set_description("C++ Client Libraries for Google Cloud Services")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/googleapis/google-cloud-cpp/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/googleapis/google-cloud-cpp.git")
+    add_versions("2.32.0","db69dd73ef4af8b2e816d80ded04950036d0e0dccc274f8c3d3ed1d7f5692a1b")
+
+    add_deps("cmake")
+    add_deps("abseil", "crc32c", "libcurl", "openssl3", "zlib")
+    add_deps("nlohmann_json", {configs = {cmake = true}})
+
+    on_install(function (package)
+        local configs = {
+            "-DBUILD_TESTING=OFF",
+            "-DGOOGLE_CLOUD_CPP_WITH_MOCKS=OFF",
+            "-DGOOGLE_CLOUD_CPP_ENABLE_MACOS_OPENSSL_CHECK=OFF",
+            "-DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF",
+        }
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_load(function (package)
+        if package:config("shared") then
+            package:add("deps","protobuf-cpp",{shared= true})
+            package:add("deps","grpc",{shared= true})
+        end
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "google/cloud/version.h"
+            int test() {
+              google::cloud::version_string();
+              return 0;
+            }
+        ]]}, {configs = {languages = "c++17"}}))
+    end)
+
+
