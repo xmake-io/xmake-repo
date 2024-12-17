74fcb2c9e6c13501798a91db2c3a7195f8158df0
Altina Orion <139891033+Altina-oz@users.noreply.github.com>
2024-12-17 23:20:14 +0800

    Add protocand see if we should patch cmake

--- packages/g/grpc/xmake.lua
+++ packages/g/grpc/xmake.lua
@@ -54,6 +54,9 @@ package("grpc")
             "-DgRPC_PROTOBUF_PACKAGE_TYPE=CONFIG"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_cross() then
+            package:add("deps", "protoc")
+        end
         import("package.tools.cmake").install(package, configs, {packagedeps = {"abseil", "protobuf-cpp"}})
     end)
 
