d1ce9657fddb85b736ea7d6d2162d834249ecae2
Altina Orion <139891033+Altina-oz@users.noreply.github.com>
2024-12-17 13:45:57 +0800

    Update xmake.lua

--- packages/g/grpc/xmake.lua
+++ packages/g/grpc/xmake.lua
@@ -7,10 +7,11 @@ package("grpc")
              "https://github.com/grpc/grpc.git")
     add_versions("v1.51.3", "17720fd0a690e904a468b4b3dae6fa5ec40b0d1f4d418e2ca092e2f92f06fce0")
     add_versions("v1.62.1", "f672a3a3b370f2853869745110dabfb6c13af93e17ffad4676a0b95b5ec204af")
+    add_versions("v1.68.2", "2a17adb0d23768413ca85990dbf800a600863d272fcc37a9f67f3b5e34ae9174")
 
     add_patches("1.51.3", path.join(os.scriptdir(), "patches", "1.51.3", "disable-download-archive.patch"), "90fdd6e4a51cbc9756d1fcdd0f65e665d4b78cfd91fdbeb0228cc4e9c4ba1b73")
     add_patches("1.51.3", path.join(os.scriptdir(), "patches", "1.51.3", "static-linking-in-linux.patch"), "176474919883f93be0c5056098eccad408038663c6c7361f2e049cdf7247a19c")
-
+    add_patches("1.51.3", path.join(os.scriptdir(), "patches", "1.68.2", "fix-NAN-on-Win11.patch"), "3b5c90855adbee29cda36a1d140167a20c96e81c9b39692bb7c77b32da73fb71")
     add_deps("cmake")
     if is_plat("linux") then
         add_deps("autoconf", "libtool", "pkg-config")
