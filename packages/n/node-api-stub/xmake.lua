d5e2d6c3455d0517de115d6b9b32e90acfd3d104
star9029 <hengxings783@gmail.com>
2024-12-23 16:03:27 +0800

    Update xmake.lua

--- packages/n/node-api-stub/xmake.lua
+++ packages/n/node-api-stub/xmake.lua
@@ -1,23 +1,29 @@
 package("node-api-stub")
-
     set_homepage("https://github.com/napi-bindings/node-api-stub")
     set_description("Node-API stub")
     set_license("MIT")
 
-    add_urls("https://github.com/napi-bindings/node-api-stub.git")
-    add_urls("https://github.com/napi-bindings/node-api-stub/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://github.com/napi-bindings/node-api-stub/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/napi-bindings/node-api-stub.git")
 
     add_versions("8.0.0", "7fdf725a3122f4d86443e707227a30c663c7163c6d1b9f883cb2305f18e01740")
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-
-    add_deps("cmake")
-
     on_install(function (package)
+        if package:has_tool("cxx", "cl") then
+            io.replace("node_api.c", "void exit()  __attribute__((noreturn));", "NAPI_NO_RETURN void exit();", {plain = true})
+        end
+        if package:is_plat("windows") then
+            if package:config("shared") then
+                package:add("defines", "NAPI_EXTERN=__declspec(dllimport)")
+            else
+                io.replace("js_native_api.h", "__declspec(dllexport)", "", {plain = true})
+            end
+        end
+
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("node-api-stub")
-                set_kind("static")
+                set_kind("$(kind)")
                 add_files("node_api.c")
                 add_headerfiles("*.h")
         ]])
