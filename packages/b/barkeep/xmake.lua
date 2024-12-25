9e8def714a1938453ca6e40802190998e4f7d476
ruki <waruqi@gmail.com>
2024-12-25 10:18:08 +0800

    Update barkeep to v0.1.4 (#6041)

--- packages/b/barkeep/xmake.lua
+++ packages/b/barkeep/xmake.lua
@@ -7,6 +7,7 @@ package("barkeep")
     add_urls("https://github.com/oir/barkeep/archive/refs/tags/$(version).tar.gz",
              "https://github.com/oir/barkeep.git")
 
+    add_versions("v0.1.4", "2dc1b2cf6f0e0c0de1a0f18a1d31a97bc698ed0cfdf186780daf5a17aa56dfa2")
     add_versions("v0.1.3", "211425e348b570547b49d11edfb6e3750701d97cc89f073771b16d6012530a66")
 
     add_configs("fmt", {description = "Use fmt format", default = true, type = "boolean"})
