eb985c378b1678d6ca6e68586f404cdfa4a483b6
さくらみこ <lin@sz.cn.eu.org>
2024-12-26 21:00:13 +0300

    revise url

--- packages/p/pffft/xmake.lua
+++ packages/p/pffft/xmake.lua
@@ -3,13 +3,9 @@ package("pffft")
     set_homepage("https://bitbucket.org/jpommier/pffft/")
     set_description("PFFFT, a pretty fast Fourier Transform.")
     set_license("BSD-like (FFTPACK license)")
-
-    add_urls("https://bitbucket.org/jpommier/pffft/get/$(version).zip")
-    add_versions("02fe7715a5bf", "81e1b91bad77092681e5ed6c2eef1a3bae7eb2154d3358a10c663867cd947396")
-
+    add_urls("https://bitbucket.org/jpommier/pffft.git")
     add_configs("nosimd", {description = "Build without SIMD support.", default = false, type = "boolean"})
 
-    add_deps("cmake")
     if not is_plat("windows") then
         add_syslinks("m")
     end
