9083e591935e6711ea70205f651f837eaafaa0d3
さくらみこ <lin@sz.cn.eu.org>
2024-12-26 21:16:12 +0300

    check nosimd

--- packages/p/pffft/xmake.lua
+++ packages/p/pffft/xmake.lua
@@ -4,7 +4,7 @@ package("pffft")
     set_description("PFFFT, a pretty fast Fourier Transform.")
     set_license("BSD-like (FFTPACK license)")
     add_urls("https://bitbucket.org/jpommier/pffft.git")
-    add_configs("nosimd", {description = "Build without SIMD support.", default = false, type = "boolean"})
+    add_configs("nosimd", {description = "Build without SIMD support.", default = true, type = "boolean"})
 
     if not is_plat("windows") then
         add_syslinks("m")
@@ -12,10 +12,6 @@ package("pffft")
 
     on_install(function (package)
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        local configs = {}
-        if package:config("nosimd") then
-            table.insert(configs, "-DPFFFT_SIMD_DISABLE=ON")
-        end
         import("package.tools.xmake").install(package, configs)
     end)
 
