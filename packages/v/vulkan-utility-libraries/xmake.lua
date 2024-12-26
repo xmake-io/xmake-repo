b10af7769c9ee3a3898c294afa47bef2d68620b3
ruki <waruqi@gmail.com>
2024-12-26 01:13:18 +0000

    Update vulkan-utility-libraries to v1.4.304

--- packages/v/vulkan-utility-libraries/xmake.lua
+++ packages/v/vulkan-utility-libraries/xmake.lua
@@ -12,6 +12,7 @@ package("vulkan-utility-libraries")
         return version:startswith("v") and version or prefix .. version:gsub("%+", ".")
     end})
 
+    add_versions("v1.4.304", "daaf71220fffe3988e79f229ae70fb0937e5d932cd176bccb6be0f946dd54c03")
     add_versions("v1.3.290", "5173690276d25e51b63132ed6907542b9bc2d64150db0fe057ff59067493e33c")
     add_versions("v1.3.283", "a446616dede2b0168726f4e1b51777ba5c20ec46c475b378e2c07fd4ab4375ee")
     add_versions("v1.3.280", "075e13f2fdeeca3bb6fb39155c8cc345cf216ab93661549b1a33368aa28a9dea")
