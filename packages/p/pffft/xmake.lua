a9a061d75eb9f1c6ac444345a4e25bf902e69a05
さくらみこ <lin@sz.cn.eu.org>
2024-12-27 12:36:57 +0300

    revise versioning

--- packages/p/pffft/xmake.lua
+++ packages/p/pffft/xmake.lua
@@ -4,9 +4,15 @@ package("pffft")
     set_description("PFFFT, a pretty fast Fourier Transform.")
     set_license("BSD-like (FFTPACK license)")
 
-    add_urls("https://bitbucket.org/jpommier/pffft/$(version).tar.gz",
-             "https://bitbucket.org/jpommier/pffft.git")
-    add_versions("02fe7715a5bf8bfd914681c53429600f94e0f536", "9adeb18ac7bb52e9fb921c31c0c6a4e9ae150cc6fcb20a899d4b3a2275176ded")
+    add_urls("https://bitbucket.org/jpommier/pffft/$(version)", {version = function (version)
+        local hash = "02fe7715a5bf8bfd914681c53429600f94e0f536"
+        if version:le("2024+100") then
+            hash = "02fe7715a5bf8bfd914681c53429600f94e0f536"
+        end
+        return "get/" .. hash .. ".tar.gz"
+    end})
+    add_versions("2024+100", "9adeb18ac7bb52e9fb921c31c0c6a4e9ae150cc6fcb20a899d4b3a2275176ded")
+
     add_configs("simd", {description = "Build with SIMD support.", default = true, type = "boolean"})
 
     if not is_plat("windows") then
