4cf22214c82a6ae5960bec9e45f5920cdc386ba9
さくらみこ <lin@sz.cn.eu.org>
2024-12-27 14:01:15 +0300

    revise versioning

--- packages/p/pffft/xmake.lua
+++ packages/p/pffft/xmake.lua
@@ -6,12 +6,12 @@ package("pffft")
 
     add_urls("https://bitbucket.org/jpommier/pffft/$(version)", {version = function (version)
         local hash = "02fe7715a5bf8bfd914681c53429600f94e0f536"
-        if version:le("2024+100") then
+        if version:le("2024.11.29") then
             hash = "02fe7715a5bf8bfd914681c53429600f94e0f536"
         end
         return "get/" .. hash .. ".tar.gz"
     end})
-    add_versions("2024+100", "9adeb18ac7bb52e9fb921c31c0c6a4e9ae150cc6fcb20a899d4b3a2275176ded")
+    add_versions("2024.11.29", "9adeb18ac7bb52e9fb921c31c0c6a4e9ae150cc6fcb20a899d4b3a2275176ded")
 
     add_configs("simd", {description = "Build with SIMD support.", default = true, type = "boolean"})
 
