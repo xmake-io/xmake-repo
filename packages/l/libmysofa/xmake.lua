b9ff09950a09bbdf7171f361d1ea9677fd6e5046
さくらみこ <lin@sz.cn.eu.org>
2024-12-27 19:20:25 +0300

    fixup

--- packages/l/libmysofa/xmake.lua
+++ packages/l/libmysofa/xmake.lua
@@ -11,9 +11,12 @@ package("libmysofa")
 
     on_install(function (package)
         local configs = {"-DBUILD_TESTS=OFF"}
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
-        table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        if package:config("shared") then
+            table.insert(configs, "-DBUILD_STATIC_LIBS=ON")
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+        else
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
