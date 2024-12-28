55411beea9adfad2c9e3d82a856d3124bb0cb156
さくらみこ <lin@sz.cn.eu.org>
2024-12-28 09:53:58 +0300

    check

--- packages/l/libmysofa/xmake.lua
+++ packages/l/libmysofa/xmake.lua
@@ -8,7 +8,7 @@ package("libmysofa")
 
     add_versions("v1.3.2", "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76")
 
-    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "babadc83541ad6d6d37884bf44b6ebcba783d727f075dcc6e4ce01790ca0f5b0")
+    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "beb93eaaea535ca69669f5f77dbf96e42ab35e66f25c6889ac1a53c56da30528")
 
     add_deps("cmake", "zlib")
 
