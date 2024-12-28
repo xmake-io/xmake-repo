0377d7ea599b42769ea7ae23af84b4f80e62fd1e
さくらみこ <lin@sz.cn.eu.org>
2024-12-28 12:04:05 +0300

    export surface

--- packages/l/libmysofa/xmake.lua
+++ packages/l/libmysofa/xmake.lua
@@ -17,6 +17,7 @@ package("libmysofa")
         os.cp("share/MIT_KEMAR_normal_pinna.sofa", "share/default.sofa")
         local configs = {"-DBUILD_TESTS=OFF"}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
 
