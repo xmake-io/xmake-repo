6755319c3851b045f5076e814f7a10453104c005
さくらみこ <lin@sz.cn.eu.org>
2024-12-28 13:45:44 +0300

    test

--- packages/l/libmysofa/xmake.lua
+++ packages/l/libmysofa/xmake.lua
@@ -8,7 +8,7 @@ package("libmysofa")
 
     add_versions("v1.3.2", "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76")
 
-    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "bb799e249773edcff9520856b44d6aaba8d6cb3d88a5371877194ddd580ca3c6")
+    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "dd166f489adf647554f9cc449bb71b8ae1070dc75e6bb8d291fe6d73ede21766")
 
     add_deps("cmake", "zlib")
 
@@ -17,7 +17,8 @@ package("libmysofa")
         os.rm("share/default.sofa")
         os.cp("share/MIT_KEMAR_normal_pinna.sofa", "share/default.sofa")
         local configs = {"-DBUILD_TESTS=OFF", "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
