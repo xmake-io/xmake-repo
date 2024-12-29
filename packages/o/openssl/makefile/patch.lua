79e98d43a2620d29c822f3008e999a87f6badf56
Chan Lee <Leetimemp@gmail.com>
2024-12-29 12:27:12 +0800

    openssl: fix Lua script

--- packages/o/openssl/makefile/patch.lua
+++ packages/o/openssl/makefile/patch.lua
@@ -1,6 +1,6 @@
 function main(package, opt)
     if not package:is_plat("windows") and not opt.perl.use_unix_path then
-        local apps_openssl = io:readfile("Makefile"):match("APPS_OPENSSL=([^\r\n]*)")
+        local apps_openssl = io.readfile("Makefile"):match("APPS_OPENSSL=([^\r\n]*)")
         io.gsub("Makefile", "\\([^%s\"\\])", "/%1")
         io.replace("Makefile", "APPS_OPENSSL=([^\r\n]*)", "APPS_OPENSSL=" .. apps_openssl)
         io.replace("Makefile", "$(APPS_OPENSSL)", [["$(APPS_OPENSSL)"]], {plain = true})
