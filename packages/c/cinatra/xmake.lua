0534af8f2947835ca53f9886c0c7edc2963f8441
xpxz <97490782+heheda123123@users.noreply.github.com>
2024-12-31 20:51:07 +0800

    update cinatra to 0.9.4 (#6012)
    * update cinatra to 0.9.4
    
    * limit patch range
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/c/cinatra/xmake.lua
+++ packages/c/cinatra/xmake.lua
@@ -7,11 +7,12 @@ package("cinatra")
     add_urls("https://github.com/qicosmos/cinatra/archive/refs/tags/$(version).tar.gz",
              "https://github.com/qicosmos/cinatra.git")
 
+    add_versions("0.9.4", "2b8b4e264f8083674554db55ca137998f02c528730cf9565697234fec9de3378")
     add_versions("0.9.1", "d1a8018e41caabbda2c380175b632e3c9c10b519727f6b998eda4e3f4ede84bd")
     add_versions("v0.8.9", "007dc38aceedf42d03a9c05dc9aa6d2f303456ae7ce1100800df7a565b83b510")
     add_versions("v0.8.0", "4e14d5206408eccb43b3e810d3a1fe228fbc7496ded8a16b041ed12cbcce4479")
 
-    add_patches(">=0.8.9", "patches/0.8.9/windows-move.patch", "c913ed0e9044ffc0ced40516245ec0d55262f8eabd30244d9911c3f0427a60f5")
+    add_patches(">=0.8.9 <=0.9.2", "patches/0.8.9/windows-move.patch", "c913ed0e9044ffc0ced40516245ec0d55262f8eabd30244d9911c3f0427a60f5")
 
     add_configs("ssl", {description = "Enable SSL", default = false, type = "boolean"})
     add_configs("gzip", {description = "Enable GZIP", default = false, type = "boolean"})
@@ -22,7 +23,16 @@ package("cinatra")
     add_deps("asio")
     add_deps("async_simple", {configs = {aio = false}})
 
-    on_load("windows", "linux", "macosx", function (package)
+    on_check("windows", function (package)
+        local vs_toolset = package:toolchain("msvc"):config("vs_toolset")
+        if vs_toolset then
+            local vs_toolset_ver = import("core.base.semver").new(vs_toolset)
+            local minor = vs_toolset_ver:minor()
+            assert(minor and minor >= 30, "package(cinatra) require vs_toolset >= 14.3")
+        end
+    end)
+
+    on_load(function (package)
         package:add("defines", "ASIO_STANDALONE")
         if package:config("ssl") then
             package:add("deps", "openssl")
