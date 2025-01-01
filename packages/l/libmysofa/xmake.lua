7db8cf424047e6b70477dcca062de351c05c5177
Saikari <lin@sz.cn.eu.org>
2025-01-01 23:13:53 +0300

    test CI

--- packages/l/libmysofa/xmake.lua
+++ packages/l/libmysofa/xmake.lua
@@ -8,12 +8,12 @@ package("libmysofa")
 
     add_versions("v1.3.2", "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76")
 
-    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "5b78f981ea5fabfdc33625ede95c2b9a5edba0f570e69f063c0a59d94a551a2e")
+    add_patches("v1.3.2", "patches/v1.3.2/fix-build.patch", "34308e9aab3700c2db1bcdc230fc9fbd399b8ff4a849fb94a86c99ebfb49a670")
 
     add_deps("cmake", "zlib")
 
     if is_plat("linux", "bsd") then
-        add_syslinks("m", "pthread")
+        add_syslinks("pthread")
     end
 
     on_install(function (package)
@@ -21,7 +21,6 @@ package("libmysofa")
             io.replace("src/CMakeLists.txt", [[find_library(MATH m)]], [[set(MATH "")]], {plain = true})
         end
         os.rm("windows/third-party/zlib-1.2.11")
-        os.rm("share/default.sofa")
         os.cp("share/MIT_KEMAR_normal_pinna.sofa", "share/default.sofa")
         local configs = {"-DBUILD_TESTS=OFF", "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
         table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
