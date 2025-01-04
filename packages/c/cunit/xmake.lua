94e91939d81f4a49d99a9b196e20dacba35c18cf
Saikari <lin@sz.cn.eu.org>
2025-01-04 22:10:03 +0300

    fixup

--- packages/c/cunit/xmake.lua
+++ packages/c/cunit/xmake.lua
@@ -11,10 +11,8 @@ package("cunit")
     add_deps("cmake")
 
     on_install(function (package)
-        io.replace("CMakeLists.txt", "-Werror=strict-prototypes", "")
-        io.replace("CMakeLists.txt", "-Werror", "")
-        io.replace("CUnit/CMakeLists.txt", "-Werror=strict-prototypes", "")
-        io.replace("CUnit/CMakeLists.txt", "-Werror", "")
+        io.replace("CMakeLists.txt", "-Werror -Werror=strict-prototypes", "")
+        io.replace("CUnit/CMakeLists.txt", "-Werror -Werror=strict-prototypes", "")
         local configs = {}
         table.insert(configs, "-DCUNIT_DISABLE_TESTS=TRUE")
         table.insert(configs, "-DCUNIT_DISABLE_EXAMPLES=TRUE")
