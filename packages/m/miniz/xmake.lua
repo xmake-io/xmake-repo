d9e7408c0ebb318dbcf1c34ffa7aec94a89a7359
Saikari <lin@sz.cn.eu.org>
2025-01-05 23:44:46 +0300

    revise

--- packages/m/miniz/xmake.lua
+++ packages/m/miniz/xmake.lua
@@ -6,24 +6,18 @@ package("miniz")
 
     add_urls("https://github.com/richgel999/miniz/archive/refs/tags/$(version).tar.gz",
              "https://github.com/richgel999/miniz.git")
-    add_versions("2.1.0", "95f9b23c92219ad2670389a23a4ed5723b7329c82c3d933b7047673ecdfc1fea")
-    add_versions("2.2.0", "bd1136d0a1554520dcb527a239655777148d90fd2d51cf02c36540afc552e6ec")
     add_versions("3.0.2", "c4b4c25a4eb81883448ff8924e6dba95c800094a198dc9ce66a292ac2ef8e018")
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-
     on_install(function (package)
-        io.writefile("miniz_export.h", "#define MINIZ_EXPORT")
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("miniz")
-                set_kind("static")
-                add_files("miniz.c", "miniz_zip.c", "miniz_tinfl.c", "miniz_tdef.c")
-                add_headerfiles("miniz.h", "miniz_export.h", "miniz_common.h", "miniz_zip.h", "miniz_tinfl.h", "miniz_tdef.h")
-        ]])
-        import("package.tools.xmake").install(package)
+        configs = {}
+        table.insert(configs, "-DBUILD_EXAMPLES=OFF")
+        table.insert(configs, "-DBUILD_TESTS=OFF")
+        table.insert(configs, "-DINSTALL_PROJECT=ON")
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("mz_compress", {includes = "miniz.h"}))
+        assert(package:has_cfuncs("mz_compress", {includes = "miniz/miniz.h"}))
     end)
