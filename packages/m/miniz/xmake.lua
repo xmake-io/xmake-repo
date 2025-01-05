1b029d68ad4fb00dbeb179fc19405967fe1cee03
Saikari <lin@sz.cn.eu.org>
2025-01-06 01:19:14 +0300

    fmt

--- packages/m/miniz/xmake.lua
+++ packages/m/miniz/xmake.lua
@@ -6,7 +6,10 @@ package("miniz")
 
     add_urls("https://github.com/richgel999/miniz/archive/refs/tags/$(version).tar.gz",
              "https://github.com/richgel999/miniz.git")
+
     add_versions("3.0.2", "c4b4c25a4eb81883448ff8924e6dba95c800094a198dc9ce66a292ac2ef8e018")
+
+    add_deps("cmake")
 
     on_install(function (package)
         local configs = {"-DCMAKE_POLICY_DEFAULT_CMP0057=NEW", "-DBUILD_EXAMPLES=OFF", "-DBUILD_TESTS=OFF", "-DINSTALL_PROJECT=ON"}
