9f75176cdf122d6d7491d8239d75ee670648b209
Saikari <lin@sz.cn.eu.org>
2025-01-06 16:57:09 +0300

    fallback

--- packages/c/cunit/xmake.lua
+++ packages/c/cunit/xmake.lua
@@ -8,6 +8,8 @@ package("cunit")
              "https://gitlab.com/cunity/cunit.git")
     add_versions("3.4.4", "eda4c24afcb2f689b150dadea790a12efb1a0e5e2eb68df7d6417a3ae70a90c7")
 
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+
     add_deps("cmake")
 
     on_install(function (package)
@@ -17,14 +19,6 @@ package("cunit")
         table.insert(configs, "-DCUNIT_DISABLE_TESTS=TRUE")
         table.insert(configs, "-DCUNIT_DISABLE_EXAMPLES=TRUE")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-
-        if package:config("shared") then
-            io.replace("CUnit/CUnit/CUnit.h.in", "ifdef CU_DLL", "if 1", {plain = true})
-            io.replace("CUnit/CUnit/CUnit.h.in", "ifdef CU_BUILD_DLL", "if 1", {plain = true})
-            io.replace("CUnit/CMakeLists.txt", "add_library(cunit STATIC", "add_library(cunit", {plain = true})
-        end
-
         import("package.tools.cmake").install(package, configs)
     end)
 
