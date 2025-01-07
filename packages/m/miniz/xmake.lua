eafe13a440f9c28e532d3043f5cf148b8a4ec35f
Saikari <lin@sz.cn.eu.org>
2025-01-07 09:34:17 +0300

    return old ver

--- packages/m/miniz/xmake.lua
+++ packages/m/miniz/xmake.lua
@@ -8,12 +8,17 @@ package("miniz")
              "https://github.com/richgel999/miniz.git")
 
     add_versions("3.0.2", "c4b4c25a4eb81883448ff8924e6dba95c800094a198dc9ce66a292ac2ef8e018")
+    add_versions("2.2.0", "bd1136d0a1554520dcb527a239655777148d90fd2d51cf02c36540afc552e6ec")
+    add_versions("2.1.0", "95f9b23c92219ad2670389a23a4ed5723b7329c82c3d933b7047673ecdfc1fea")
 
     add_includedirs("include", "include/miniz")
 
     add_deps("cmake")
 
     on_install(function (package)
+        if version:lt("3.0.0") then
+            add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+        end
         local configs = {"-DCMAKE_POLICY_DEFAULT_CMP0057=NEW", "-DBUILD_EXAMPLES=OFF", "-DBUILD_TESTS=OFF", "-DINSTALL_PROJECT=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
@@ -22,5 +27,4 @@ package("miniz")
 
     on_test(function (package)
         assert(package:has_cfuncs("mz_compress", {includes = "miniz.h"}))
-        assert(package:has_cfuncs("mz_compress", {includes = "miniz/miniz.h"}))
     end)
