fd1eaeee9914e0846a1cfd16541a45454800916a
star9029 <hengxings783@gmail.com>
2025-01-08 11:37:53 +0800

    Update littlefs to v2.10.1

--- packages/l/littlefs/xmake.lua
+++ packages/l/littlefs/xmake.lua
@@ -5,6 +5,7 @@ package("littlefs")
 
     add_urls("https://github.com/littlefs-project/littlefs/archive/refs/tags/$(version).tar.gz",
              "https://github.com/littlefs-project/littlefs.git")
+    add_versions("v2.10.1", "620691695d65ad161eed1247122b63ad03e0251d8617864ba086a563afe98216")
     add_versions("v2.9.3", "9cf2e7db673ea27d967a54cdafe8f55a7ffe27c63a2070ff7424fadd559cad67")
     add_versions("v2.9.2", "97675486790a09d335fa0955da105a0a08e1ff336208b77730d92a041b202015")
     add_versions("v2.8.2", "9f46d00d6d6ad0a0d72840455ba748efcad84b8a236bd8b9d3a08e3af7953386")
