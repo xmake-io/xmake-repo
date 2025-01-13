9ece9c43ddf208b3ca8a0a7e8fe44eae89ec5e63
star9029 <hengxings783@gmail.com>
2025-01-13 15:46:56 +0800

    Fix version

--- packages/g/gltf-sdk/xmake.lua
+++ packages/g/gltf-sdk/xmake.lua
@@ -1,34 +1,38 @@
-package("gltf-sdk")
-    set_homepage("https://github.com/microsoft/glTF-SDK")
-    set_description("glTF-SDK is a C++ Software Development Kit for glTF (GL Transmission Format -https://github.com/KhronosGroup/glTF).")
+package("rapidjson")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/Tencent/rapidjson")
+    set_description("RapidJSON is a JSON parser and generator for C++.")
     set_license("MIT")
 
-    add_urls("https://github.com/microsoft/glTF-SDK/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/microsoft/glTF-SDK.git")
+    set_urls("https://github.com/Tencent/rapidjson/archive/$(version).zip",
+             "https://github.com/Tencent/rapidjson.git")
 
-    add_versions("r1.9.6.0", "f3049d2b8ecebb914f634496918ab645f7ce452a67a5b4210e7354dc7c0059ab")
-
-    add_deps("cmake")
+    add_versions("2024.08.16", "7c73dd7de7c4f14379b781418c6e947ad464c818")
+    add_versions("2023.12.6", "6089180ecb704cb2b136777798fa1be303618975")
+    add_versions("2022.7.20", "27c3a8dc0e2c9218fe94986d249a12b5ed838f1d")
+    add_versions("v1.1.0", "8e00c38829d6785a2dfb951bb87c6974fa07dfe488aa5b25deec4b8bc0f6a3ab")
+    -- This commit is used in arrow 7.0.0 https://github.com/apache/arrow/blob/release-7.0.0/cpp/thirdparty/versions.txt#L80
+    add_versions("v1.1.0-arrow", "1a803826f1197b5e30703afe4b9c0e7dd48074f5")
 
     on_install(function (package)
-        local configs = {
-            "-DENABLE_UNIT_TESTS=OFF",
-            "-DENABLE_SAMPLES=OFF",
-            "-DRAPIDJSON_BUILD_DOC=OFF",
-            "-DRAPIDJSON_BUILD_EXAMPLES=OFF",
-            "-DRAPIDJSON_BUILD_TESTS=OFF",
-        }
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
+        if package:is_plat("windows") and package:is_arch("arm.*") then
+            package:add("defines", "RAPIDJSON_ENDIAN=RAPIDJSON_LITTLEENDIAN")
+        end
+
+        os.cp(path.join("include", "*"), package:installdir("include"))
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            void test() {
-                using namespace Microsoft::glTF;
-                const Color3 c1 = { 0.0f, 0.0f, 0.0f };
-                Color3 c = c1.ToGamma();
+            void test()
+            {
+                const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+                rapidjson::Document d;
+                d.Parse(json);
+
+                rapidjson::StringBuffer buffer;
+                rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+                d.Accept(writer);
             }
-        ]]}, {configs = {languages = "c++14"}, includes = {"GLTFSDK/Color.h"}}))
+        ]]}, {configs = {languages = "c++11"}, includes = { "rapidjson/document.h", "rapidjson/stringbuffer.h", "rapidjson/writer.h"} }))
     end)
