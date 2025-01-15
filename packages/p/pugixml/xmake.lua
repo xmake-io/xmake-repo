91ed9f4db4bb7f0e3e4d83a90090e5045ff051be
Saikari <lin@sz.cn.eu.org>
2025-01-15 18:18:39 +0300

    init

--- packages/p/pugixml/xmake.lua
+++ packages/p/pugixml/xmake.lua
@@ -9,6 +9,7 @@ package("pugixml")
     add_versions("v1.11.4", "017139251c122dbff400a507cddc4cb74120a431a50c6c524f30edcc5b331ade")
     add_versions("v1.13", "5c5ad5d7caeb791420408042a7d88c2c6180781bf218feca259fd9d840a888e1")
     add_versions("v1.14", "610f98375424b5614754a6f34a491adbddaaec074e9044577d965160ec103d2e")
+    add_versions("v1.15", "b39647064d9e28297a34278bfb897092bf33b7c487906ddfc094c9e8868bddcb")
 
     add_deps("cmake")
 
