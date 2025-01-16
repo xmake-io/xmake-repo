3be6da92506a0d4a811e427123a1f1aec5345f6c
star9029 <hengxings783@gmail.com>
2025-01-16 18:55:08 +0800

    Support storage

--- packages/g/google-cloud-cpp/xmake.lua
+++ packages/g/google-cloud-cpp/xmake.lua
@@ -8,8 +8,7 @@ package("google-cloud-cpp")
 
     add_versions("v2.33.0", "e53ba3799c052d97acac9a6a6b27af24ce822dbde7bfde973bac9e5da714e6b2")
 
-    -- Enable google_cloud_cpp_common by default
-    add_configs("libraries", {description = "Enable subset of the libraries", default = {}, type = "table"})
+    add_configs("libraries", {description = "Enable subset of the libraries (default: google_cloud_cpp_common)", default = {}, type = "table"})
     add_configs("exceptions", {description = "Enable exceptions", default = true, type = "boolean"})
 
     if is_plat("linux", "bsd") then
@@ -17,9 +16,13 @@ package("google-cloud-cpp")
     end
 
     add_deps("cmake")
-    add_deps("abseil")
 
     on_load(function (package)
+        package:add("deps", "abseil")
+        if not package:is_plat("windows") then
+            package:add("deps", "openssl3")
+        end
+
         -- https://github.com/googleapis/google-cloud-cpp/blob/main/cmake/GoogleCloudCppFeatures.cmake
         local libraries = package:config("libraries")
         if libraries and #libraries ~= 0 then
@@ -52,7 +55,15 @@ package("google-cloud-cpp")
             end
 
             if has_grpc then
-                package:add("deps", "grpc")
+                if package:config("shared") then
+                    package:add("deps", "grpc", {configs = {shared = true}})
+                    wprint([[Build google-cloud-cpp shared library require protobuf shared too, Please use `add_requireconfs("**.protobuf-cpp", {configs = {shared = true}})`]])
+                else
+                    package:add("deps", "grpc")
+                end
+                if package:is_cross() then
+                    package:add("deps", "grpc~binary", {private = true, kind = "binary"})
+                end
                 -- commit hash from https://github.com/googleapis/google-cloud-cpp/blob/main/cmake/GoogleapisConfig.cmake
                 package:add("resources", ">=2.33.0", "googleapis", "https://github.com/googleapis/googleapis.git", "c3556b45dc35a145e04b5692bc72e01a4f58a6b2")
             end
@@ -61,11 +72,11 @@ package("google-cloud-cpp")
                 package:add("deps", "nlohmann_json", {configs = {cmake = true}})
                 package:add("deps", "libcurl", "crc32c")
             end
-            -- TODO: optional deps opentelemetry
+
             local hash_libraries = hashset.from(libraries)
             package:data_set("hash_libraries", hash_libraries)
             if hash_libraries:has("opentelemetry") then
-                package:add("deps", "opentelemetry")
+                package:add("deps", "opentelemetry-cpp")
             end
         end
     end)
@@ -100,10 +111,21 @@ package("google-cloud-cpp")
     end)
 
     on_test(function (package)
+        local languages = "c++" .. package:dep("abseil"):config("cxx_standard")
         assert(package:check_cxxsnippets({test = [[
             #include <google/cloud/version.h>
             void test() {
                 auto version = google::cloud::version_string();
             }
-        ]]}, {configs = {languages = "c++17"}}))
+        ]]}, {configs = {languages = languages}}))
+
+        local hash_libraries = package:data("hash_libraries")
+        if hash_libraries:has("storage") then
+            assert(package:check_cxxsnippets({test = [[
+                #include <google/cloud/storage/client.h>
+                void test() {
+                    auto client = google::cloud::storage::Client();
+                }
+            ]]}, {configs = {languages = languages}}))
+        end
     end)
