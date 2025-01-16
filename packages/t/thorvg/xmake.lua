0a989d105a62a15894aa316ab77701ca07d11fe6
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-01-16 15:38:37 +0800

    update thorvg to latest (#6153)
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Fix test
    
    * Fix test
    
    * Fix test
    
    * Fix checksum
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/t/thorvg/xmake.lua
+++ packages/t/thorvg/xmake.lua
@@ -6,6 +6,8 @@ package("thorvg")
     add_urls("https://github.com/thorvg/thorvg/archive/refs/tags/$(version).tar.gz",
              "https://github.com/thorvg/thorvg.git")
 
+    add_versions("v1.0-pre10", "a8d2cff9c64495b24b4f51730d26d16f5a12136c7a5c38ac18c0e6caa8d4efc6")
+    add_versions("v0.15.8", "bc0d6cf60a49fa760d562e125300d144d9f0436a8499f942ce234bd2acb7a5d5")
     add_versions("v0.15.2", "98fcd73567c003a33fad766a7dbb9244c61e9b4721397d42e7fa04fc2e499dce")
     add_versions("v0.15.1", "4935228bb11e1a5303fc98d2a4b355c94eb82bff10cff581821b0b3c41368049")
     add_versions("v0.14.10", "e11e2e27ef26ed018807e828cce3bca1fb9a7f25683a152c9cd1f7aac9f3b67a")
@@ -106,7 +108,12 @@ package("thorvg")
     end)
 
     on_test(function (package)
-        assert(package:has_cxxfuncs("tvg::Initializer::init(tvg::CanvasEngine::Sw, 0)", {includes = "thorvg.h", configs = {languages = "c++14"}}))
+        assert(package:check_cxxsnippets({test = [[
+            #include <thorvg.h>
+            void test() {
+                auto canvas = tvg::SwCanvas::gen();
+            }
+        ]]}, {configs = {languages = "c++14"}}))
         if package:config("c_api") then
             assert(package:has_cxxfuncs("tvg_engine_init", {includes = "thorvg_capi.h"}))
         end
