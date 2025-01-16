9bda002368d9c3f28a9c858999eae53020c02dc6
star9029 <hengxings783@gmail.com>
2025-01-16 21:48:08 +0800

    Update xmake.lua

--- packages/p/pugixml/xmake.lua
+++ packages/p/pugixml/xmake.lua
@@ -3,7 +3,7 @@ package("pugixml")
     set_description("Light-weight, simple and fast XML parser for C++ with XPath support")
     set_license("MIT")
 
-    add_urls("https://github.com/zeux/pugixml/archive/$(version).tar.gz",
+    add_urls("https://github.com/zeux/pugixml/archive/refs/tags/$(version).tar.gz",
              "https://github.com/zeux/pugixml.git")
 
     add_versions("v1.11.4", "017139251c122dbff400a507cddc4cb74120a431a50c6c524f30edcc5b331ade")
@@ -13,42 +13,40 @@ package("pugixml")
 
     add_configs("wchar", {description = "Enable wchar_t mode", default = true, type = "boolean"})
     add_configs("exceptions", {description = "Enable exceptions", default = true, type = "boolean"})
-    add_configs("exports", {description = "Export symbols for DLL", default = true, type = "boolean"})
-    add_configs("headeronly", {description = "Install headerfiles only", default = true, type = "boolean"})
-
-    add_deps("cmake")
+    add_configs("headeronly", {description = "Install headerfiles only", default = false, type = "boolean"})
 
     on_load(function (package)
         if package:config("headeronly") then
             package:set("kind", "library", {headeronly = true})
+        else
+            package:add("deps", "cmake")
         end
     end)
 
     on_install(function (package)
-        local configs = {}
-        if package:is_plat("wasm") and package:config("shared") then
-            os.cp(path.join(package:scriptdir(), "port", "sharedwasm.cmake"), "sharedwasm.cmake")
-            table.insert(configs, "-DCMAKE_PROJECT_INCLUDE=sharedwasm.cmake")
-        end
         if package:config("wchar") then
             io.replace("src/pugiconfig.hpp", "// #define PUGIXML_WCHAR_MODE", "#define PUGIXML_WCHAR_MODE", {plain = true})
         end
         if not package:config("exceptions") then
             io.replace("src/pugiconfig.hpp", "// #define PUGIXML_NO_EXCEPTIONS", "#define PUGIXML_NO_EXCEPTIONS", {plain = true})
         end
-        if package:is_plat("windows", "mingw") and package:config("exports") then
-            if package:config("shared") then
-                io.replace("src/pugiconfig.hpp", "// #define PUGIXML_API __declspec(dllexport)", "#define PUGIXML_API __declspec(dllexport)", {plain = true})
-            end
+        if package:is_plat("windows") and package:config("shared") then
+            io.replace("src/pugiconfig.hpp", "// #define PUGIXML_API __declspec(dllexport)", "#define PUGIXML_API __declspec(dllexport)", {plain = true})
         end
+
         if package:config("headeronly") then
             io.replace("src/pugiconfig.hpp", "// #define PUGIXML_HEADER_ONLY", "#define PUGIXML_HEADER_ONLY", {plain = true})
-            os.cp("src/*.*", package:installdir("include"))
+            os.cp("src/*", package:installdir("include"))
         else
-            table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+            local configs = {}
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
             table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
             if package:is_plat("windows") then
                 table.insert(configs, "-DSTATIC_CRT=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+            end
+            if package:is_plat("wasm") and package:config("shared") then
+                os.cp(path.join(package:scriptdir(), "port", "sharedwasm.cmake"), "sharedwasm.cmake")
+                table.insert(configs, "-DCMAKE_PROJECT_INCLUDE=sharedwasm.cmake")
             end
             import("package.tools.cmake").install(package, configs)
         end
