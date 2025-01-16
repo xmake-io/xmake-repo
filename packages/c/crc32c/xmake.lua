b9d3d7770f3bd9dd5cd400668aff8abea6ddc306
star9029 <hengxings783@gmail.com>
2025-01-16 21:10:06 +0800

    crc32c: Fix windows shared build (#6170)
    * crc32c: Fix windows shared build
    
    * Update xmake.lua

--- packages/c/crc32c/xmake.lua
+++ packages/c/crc32c/xmake.lua
@@ -1,7 +1,7 @@
 package("crc32c")
-
     set_homepage("https://github.com/google/crc32c")
     set_description("CRC32C implementation with support for CPU-specific acceleration instructions")
+    set_license("BSD-3-Clause")
 
     add_urls("https://github.com/google/crc32c/archive/refs/tags/$(version).tar.gz",
              "https://github.com/google/crc32c.git")
@@ -11,15 +11,13 @@ package("crc32c")
     add_deps("cmake")
 
     on_install(function (package)
-        import("package.tools.cmake")
-        local configs = {"-DCRC32C_BUILD_TESTS=OFF", "-DCRC32C_BUILD_BENCHMARKS=OFF", "-DCRC32C_USE_GLOG=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        if package:config("shared") then
-            table.insert(configs, "-DBUILD_SHARED_LIBS=on")
-        else
-            table.insert(configs, "-DBUILD_SHARED_LIBS=off")
+        local configs = {"-DCRC32C_BUILD_TESTS=OFF", "-DCRC32C_BUILD_BENCHMARKS=OFF", "-DCRC32C_USE_GLOG=OFF", "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("windows") and package:config("shared") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
         end
-        cmake.install(package, configs)
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
