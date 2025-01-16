a0adee10aa449e1a9fc1f9758a480815ef2b8bc0
star9029 <hengxings783@gmail.com>
2025-01-16 18:15:53 +0800

    Update xmake.lua

--- packages/g/google-cloud-cpp/xmake.lua
+++ packages/g/google-cloud-cpp/xmake.lua
@@ -3,72 +3,107 @@ package("google-cloud-cpp")
     set_description("C++ Client Libraries for Google Cloud Services")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/googleapis/google-cloud-cpp/archive/refs/tags/v$(version).tar.gz",
+    add_urls("https://github.com/googleapis/google-cloud-cpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/googleapis/google-cloud-cpp.git")
-    add_versions("2.32.0","db69dd73ef4af8b2e816d80ded04950036d0e0dccc274f8c3d3ed1d7f5692a1b")
 
-    add_configs("grpc", {description = "Enable gRPC", default = true, type = "boolean"})
+    add_versions("v2.33.0", "e53ba3799c052d97acac9a6a6b27af24ce822dbde7bfde973bac9e5da714e6b2")
+
+    -- Enable google_cloud_cpp_common by default
+    add_configs("libraries", {description = "Enable subset of the libraries", default = {}, type = "table"})
+    add_configs("exceptions", {description = "Enable exceptions", default = true, type = "boolean"})
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
 
     add_deps("cmake")
-    add_deps("abseil", "crc32c", "libcurl", "openssl3", "zlib")
-    add_deps("nlohmann_json", {configs = {cmake = true}})
+    add_deps("abseil")
 
-    on_check(function (package)
-        if package:is_plat("android") then
-            raise("package(google-cloud-cpp) unsupported on android due to package(grpc) is unsupported on android yet.")
-        end
-        if package:is_plat("wasm") then
-            raise("package(google-cloud-cpp) unsupported on wasm due to package(grpc) is unsupported on wasm yet.")
-        end
-        if package:is_plat("mingw") then
-            raise("package(google-cloud-cpp) unsupported on mingw due to package(grpc) is unsupported on mingw yet.")
-        end
-        if package:is_plat("iphoneos") then
-            raise("package(google-cloud-cpp) unsupported on iphoneos due to package(grpc) is unsupported on iphoneos yet.")
-        end
-        if package:is_plat("bsd") then
-            raise("package(google-cloud-cpp) unsupported on bsd")
-        end
-        if package:is_arch("arm.*")  then
-            raise("package(google-cloud-cpp) unsupported on arm yet")
+    on_load(function (package)
+        -- https://github.com/googleapis/google-cloud-cpp/blob/main/cmake/GoogleCloudCppFeatures.cmake
+        local libraries = package:config("libraries")
+        if libraries and #libraries ~= 0 then
+            import("core.base.hashset")
+
+            local has_grpc = false
+            local no_grpc_require = hashset.of("experimental-bigquery_rest", "mocks", "oauth2", "storage")
+
+            local has_rest = false
+            local rest_requires = hashset.of(
+                "storage",
+                "bigquerycontrol",
+                "compute",
+                "experimental",
+                "gkeconnect",
+                "oauth2",
+                "opentelemetry",
+                "sql",
+                "universe_domain",
+                "generator"
+            )
+
+            for _, lib in ipairs(libraries) do
+                if not no_grpc_require:has(lib) then
+                    has_grpc = true
+                end
+                if rest_requires:has(lib) then
+                    has_rest = true
+                end
+            end
+
+            if has_grpc then
+                package:add("deps", "grpc")
+                -- commit hash from https://github.com/googleapis/google-cloud-cpp/blob/main/cmake/GoogleapisConfig.cmake
+                package:add("resources", ">=2.33.0", "googleapis", "https://github.com/googleapis/googleapis.git", "c3556b45dc35a145e04b5692bc72e01a4f58a6b2")
+            end
+
+            if has_rest then
+                package:add("deps", "nlohmann_json", {configs = {cmake = true}})
+                package:add("deps", "libcurl", "crc32c")
+            end
+            -- TODO: optional deps opentelemetry
+            local hash_libraries = hashset.from(libraries)
+            package:data_set("hash_libraries", hash_libraries)
+            if hash_libraries:has("opentelemetry") then
+                package:add("deps", "opentelemetry")
+            end
         end
     end)
 
     on_install(function (package)
+        -- https://github.com/googleapis/google-cloud-cpp/blob/main/doc/packaging.md
+        if package:dep("grpc") then
+            local googleapis_dir = path.unix(package:resourcedir("googleapis"))
+            io.replace("external/googleapis/CMakeLists.txt", 
+                [[set(EXTERNAL_GOOGLEAPIS_PREFIX "${PROJECT_BINARY_DIR}/external/googleapis")]],
+                format("set(EXTERNAL_GOOGLEAPIS_PREFIX %s)", googleapis_dir), {plain = true})
+        end
+
         local configs = {
             "-DBUILD_TESTING=OFF",
             "-DGOOGLE_CLOUD_CPP_WITH_MOCKS=OFF",
             "-DGOOGLE_CLOUD_CPP_ENABLE_MACOS_OPENSSL_CHECK=OFF",
             "-DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF",
         }
-        table.insert(configs, "-DGOOGLE_CLOUD_CPP_ENABLE_GRPC=" .. (package:config("grpc") and "ON" or "OFF"))
-        
-        package:add("cxxflags", "-Wno-missing-template-arg-list-after-template-kw")
-        import("package.tools.cmake").install(package, configs)
-    end)
-
-    on_load(function (package)
-        if package:config("shared") then
-            package:add("deps","protobuf-cpp",{shared= true})
-            if package:config("grpc") then
-                package:add("deps","grpc",{shared= true})
-            end
-        else
-            package:add("deps","protobuf-cpp")
-            if package:config("grpc") then
-                package:add("deps","grpc")
-            end
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DGOOGLE_CLOUD_CPP_ENABLE_CXX_EXCEPTIONS=" .. (package:config("exceptions") and "ON" or "OFF"))
+        if package:is_plat("windows") and package:config("shared") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
         end
+
+        local libraries = package:config("libraries")
+        if libraries then
+            table.insert(configs, "-DGOOGLE_CLOUD_CPP_ENABLE=" .. table.concat(libraries, ";"))
+        end
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include "google/cloud/version.h"
-            int test() {
-              google::cloud::version_string();
-              return 0;
+            #include <google/cloud/version.h>
+            void test() {
+                auto version = google::cloud::version_string();
             }
         ]]}, {configs = {languages = "c++17"}}))
     end)
-
-
