effbb786f91981bd62028cc07301ab581a060437
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-19 02:20:50 +0800

    libnyquist: disable building example

--- packages/l/libnyquist/xmake.lua
+++ packages/l/libnyquist/xmake.lua
@@ -6,14 +6,13 @@ package("libnyquist")
     add_urls("https://github.com/ddiakopoulos/libnyquist.git")
     add_versions("2023.02.12", "767efd97cdd7a281d193296586e708490eb6e54f")
 
-    add_patches("2023.02.12", path.join(os.scriptdir(), "patches", "error-deprecated-register.patch"),
-        "1bbf8462e8d3fac5d9b533e59ee475165650c5f7d9439b46da9b6f5cf25dd40f")
-
     add_deps("cmake")
 
     on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        local configs = {
+            "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
+            "-DLIBNYQUIST_BUILD_EXAMPLE=Off",
+        }
         import("package.tools.cmake").install(package, configs)
         os.cp("include/libnyquist/*.h", package:installdir("include/libnyquist"))
     end)
