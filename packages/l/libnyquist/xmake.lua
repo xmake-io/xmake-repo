0aafcd0c15f4b1366413f51a6267ffbae127061d
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-19 11:22:31 +0800

    libnyquist: block non-VS2017 from compiling

--- packages/l/libnyquist/xmake.lua
+++ packages/l/libnyquist/xmake.lua
@@ -9,32 +9,28 @@ package("libnyquist")
     add_deps("cmake")
 
     on_install(function (package)
+        local msvc = package:toolchain("msvc")
+        if package:is_plat("windows") and msvc and msvc:config("vs") ~= "2017" then
+          error("libnyquist: cannot compile with non-VS2017 toolchain")
+        end
+
         local is_byte_order_little = package:check_csnippets({
             test = [[
                 #if !(defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
                 #error not little endian
                 #endif
             ]]
-        }, {})
-        local is_msvc = package:check_csnippets({
-            test = [[
-                #if !(defined(_MSC_VER) && !defined(__clang__) && !defined(__INTEL_COMPILER))
-                #error not msvc
-                #endif
-            ]]
-        })
+        }, { verbose = false })
 
         local configs = {
             "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
             "-DCMAKE_CXX_STANDARD=14",
             "-DCMAKE_CXX_FLAGS=\z
                 -D" .. (
-                    (package:is_targetarch("x86_64", "i%d86") or is_byte_order_little)
+                    (package:is_targetarch("x86_64", "x86", "i%d86") or is_byte_order_little)
                     and "ARCH_CPU_LITTLE_ENDIAN"
                     or "ARCH_CPU_BIG_ENDIAN"
-                ) .. (
-                    (not is_msvc) and " -Wno-implicit-function-declaration" or ""
-                ),
+                )
             "-DLIBNYQUIST_BUILD_EXAMPLE=Off",
         }
 
