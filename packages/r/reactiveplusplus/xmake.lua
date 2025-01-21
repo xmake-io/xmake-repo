51694339e623848597b05d13197ba40b811d7342
ruki <waruqi@gmail.com>
2025-01-21 01:12:02 +0000

    Update reactiveplusplus to v2.2.2

--- packages/r/reactiveplusplus/xmake.lua
+++ packages/r/reactiveplusplus/xmake.lua
@@ -7,6 +7,7 @@ package("reactiveplusplus")
     add_urls("https://github.com/victimsnino/ReactivePlusPlus/archive/refs/tags/$(version).tar.gz",
              "https://github.com/victimsnino/ReactivePlusPlus.git")
 
+    add_versions("v2.2.2", "54b2bf457996bd3d22e3b33fb98a8d0a3867dfa2d5e23991718dcd94ba6e4157")
     add_versions("v2.1.1", "0b962478d7c973a1f74062ce7f8d24c2fdcd2733031b1f014e65d252d59ebe6a",
                  "v2.1.0", "d84a194ef96b92201ea574f81780837c95e8956bbad09b3dc2dc5cef7c2eef98",
                  "v0.2.3", "9542419f8d7da98126ba2c6ae08fab287b4b3798d89cf75ed9bed2a9e3ec1678")
