852a92081f8d8e52a07a430d1b92ff23c82e3be6
ruki <waruqi@gmail.com>
2025-01-22 11:17:18 +0800

    Update verilator to v5.032 (#6213)

--- packages/v/verilator/xmake.lua
+++ packages/v/verilator/xmake.lua
@@ -7,6 +7,7 @@ package("verilator")
     add_urls("https://github.com/verilator/verilator/archive/refs/tags/$(version).tar.gz",
              "https://github.com/verilator/verilator.git")
 
+    add_versions("v5.032", "5a262564b10be8bdb31ff4fb67d77bcf5f52fc1b4e6c88d5ca3264fb481f1e41")
     add_versions("v5.016", "66fc36f65033e5ec904481dd3d0df56500e90c0bfca23b2ae21b4a8d39e05ef1")
 
     add_deps("cmake")
