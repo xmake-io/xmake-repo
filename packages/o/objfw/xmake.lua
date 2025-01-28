f47499005185003aad2c9c5b906180939b0bfadd
Frityet <ambhogal01@gmail.com>
2025-01-28 01:37:05 +0000

    Update ObjFW (#5986)
    * Updated objfw
    
    * update objfw
    
    * use package:is_plat
    
    * Updated objfw
    
    * Update xmake.lua
    
    * remove debug message
    
    * add objfw v1.2-v1.2.2
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/o/objfw/xmake.lua
+++ packages/o/objfw/xmake.lua
@@ -23,8 +23,10 @@ package("objfw")
     add_versions("1.1.4",   "f6bfdbab22008aae3e4b48d77ced1a04c5153961c6f7e5492891f90ae5131a78")
     add_versions("1.1.5",   "9d45d2009a0bb9b1a0918918e454b47b8161670df8016b5f3a85eccea91d8988")
     add_versions("1.1.6",   "c19a97a011e14780fb32cfbdbbd6a699a955b57124e4e079768cb8aad4430e1d")
-	add_versions("1.1.7",	"5107d8a0627e2270d211abf1b4f6c50fd89c8d672d2179b50daa7d3b66d68a70")
-
+	add_versions("1.1.7",   "5107d8a0627e2270d211abf1b4f6c50fd89c8d672d2179b50daa7d3b66d68a70")
+    add_versions("1.2.0",   "f1d92b64f524a1aaf8e8b572a0edf5817d589c3d3c60cab9bb182ccbac3ee405")
+    add_versions("1.2.1",   "637fdeccae149cec236e62c5289450afad542fe930343918856e76594ab3fcfd")
+    add_versions("1.2.2",   "4fe0bed1ec21561a184d804aa577ff630f1e3d20b1c3b973073e23ce829294a1")
     if is_host("linux", "macosx") then
         add_deps("autoconf", "automake", "libtool")
     end
@@ -129,7 +131,6 @@ package("objfw")
             local ssl_incdir = find_path(is_gnu and "gnutls/gnutls.h" or "openssl/ssl.h", { ssl:installdir("include"), "/usr/include/", "/usr/local/include" })
 
             if libssl then
-                print("Using SSL "..ssl:name().." from "..libssl.linkdir..", include dir: "..ssl_incdir)
                 table.insert(configs, "CPPFLAGS=-I"..ssl_incdir)
                 table.insert(configs, "LDFLAGS=-L"..libssl.linkdir)
             else
