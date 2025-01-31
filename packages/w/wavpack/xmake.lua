3265eb42523a0a05b710288e2af2f328c8b0b6c0
ruki <waruqi@gmail.com>
2025-01-31 22:09:57 +0800

    Update xmake.lua

--- packages/w/wavpack/xmake.lua
+++ packages/w/wavpack/xmake.lua
@@ -39,7 +39,9 @@ package("wavpack")
         local configs = {"-DWAVPACK_INSTALL_CMAKE_MODULE=OFF", "-DWAVPACK_INSTALL_DOCS=OFF", "-DWAVPACK_INSTALL_PKGCONFIG_MODULE=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-
+        if package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_STATIC_LIBRARY_SUFFIX=.lib")
+        end
         for name, enabled in pairs(package:configs()) do
             if not package:extraconf("configs", name, "builtin") then
                 table.insert(configs, "-DWAVPACK_ENABLE_" .. name:upper() .. "=" .. (enabled and "ON" or "OFF"))
