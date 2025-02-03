02eda2c3d8e54d746d8ac88fd1a9730293d4f7a7
Jérôme Leclercq <lynix680@gmail.com>
2025-02-03 17:23:13 +0100

    Update xmake.lua

--- packages/l/libxext/xmake.lua
+++ packages/l/libxext/xmake.lua
@@ -25,6 +25,9 @@ package("libxext")
         if package:config("pic") then
             table.insert(configs, "--with-pic")
         end
+        if package:is_plat("cross") then
+            table.insert(configs, "--disable-malloc0returnsnull")
+        end
         import("package.tools.autoconf").install(package, configs)
     end)
 
