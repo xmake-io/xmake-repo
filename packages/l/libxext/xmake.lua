44425f4d9611785863f5602ad1079e383cd330b6
Jérôme Leclercq <lynix680@gmail.com>
2025-02-03 14:38:57 +0100

    Update xmake.lua

--- packages/l/libxext/xmake.lua
+++ packages/l/libxext/xmake.lua
@@ -10,11 +10,11 @@ package("libxext")
         add_extsources("apt::libxext-dev")
     end
 
-    if is_plat("macosx", "linux", "bsd") then
+    if is_plat("macosx", "linux", "bsd", "cross") then
         add_deps("pkg-config", "libx11", "xorgproto")
     end
 
-    on_install("macosx", "linux", "bsd", function (package)
+    on_install("macosx", "linux", "bsd", "cross", function (package)
         local configs = {"--sysconfdir=" .. package:installdir("etc"),
                          "--localstatedir=" .. package:installdir("var"),
                          "--disable-dependency-tracking",
