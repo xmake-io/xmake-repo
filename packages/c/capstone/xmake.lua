720e88af0ed0919687877fa6199f65a34e77d1c0
star9029 <hengxings783@gmail.com>
2025-02-06 10:33:39 +0800

    Remove cpack

--- packages/c/capstone/xmake.lua
+++ packages/c/capstone/xmake.lua
@@ -4,7 +4,7 @@ package("capstone")
     set_license("BSD-3-Clause")
 
     add_urls("https://github.com/capstone-engine/capstone/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/capstone-engine/capstone.git")
+             "https://github.com/capstone-engine/capstone.git", {submodules = false})
 
     add_versions("5.0.5", "3bfd3e7085fbf0fab75fb1454067bf734bb0bebe9b670af7ce775192209348e9")
     add_versions("5.0.3", "3970c63ca1f8755f2c8e69b41432b710ff634f1b45ee4e5351defec4ec8e1753")
@@ -16,14 +16,21 @@ package("capstone")
             package:addenv("PATH", "bin")
         end
 
+        io.replace("CMakeLists.txt", "include(CPackConfig.txt)", "", {plain = true})
+
         local configs = {
             "-DCAPSTONE_BUILD_CSTOOL=ON",
-            "-DCAPSTONE_BUILD_STATIC_RUNTIME=OFF", -- Use our pass CMAKE_MSVC_RUNTIME_LIBRARY
             "-DCAPSTONE_BUILD_LEGACY_TESTS=OFF",
             "-DCAPSTONE_BUILD_TESTS=OFF",
+            -- xmake will pass CMAKE_MSVC_RUNTIME_LIBRARY
+            "-DCAPSTONE_BUILD_STATIC_RUNTIME=OFF",
+            "-DCAPSTONE_BUILD_STATIC_MSVC_RUNTIME=OFF",
         }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCAPSTONE_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DBUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
+        table.insert(configs, "-DCAPSTONE_BUILD_STATIC_LIBS=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DENABLE_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
