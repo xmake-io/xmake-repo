d91975cb577c97cd3ecfdd0c408c4ba35581e46b
Jérôme Leclercq <lynix680@gmail.com>
2025-02-08 21:59:46 +0100

    Update xmake.lua

--- packages/j/johnnyengine/xmake.lua
+++ packages/j/johnnyengine/xmake.lua
@@ -1,16 +1,17 @@
 package("johnnyengine")
-
     set_homepage("https://github.com/PucklaJ/JohnnyEngine")
     set_description("A 2D/3D Engine using OpenGL and SDL for input and the window")
+    set_license("zlib")
 
     add_urls("https://github.com/PucklaJ/JohnnyEngine/archive/refs/tags/$(version).zip",
             "https://github.com/PucklaJ/JohnnyEngine.git")
     add_versions("1.0.1", "53c11b827bea6fe30f9bca27adbd712eec85a0853c0402407930bae78ad54a8f")
     add_patches("1.0.1", path.join(os.scriptdir(), "patches", "1.0.1", "win32_shared_fix.patch"), "fbe22cb5a9f0485982c7755936d14de6da3ce80a42394d48946b14b922847611")
 
-    add_deps("glew", "libsdl2", "libsdl2_ttf", "libsdl2_mixer", "libsdl2_gfx", "box2d", "assimp", "stb", "tmxparser")
+    add_deps("glew", "libsdl2", "libsdl2_ttf", "libsdl2_mixer", "libsdl2_gfx", "box2d 2.x", "assimp", "stb", "tmxparser")
 
     on_install("windows", "linux", "macosx", function (package)
+        io.replace("xmake.lua", [[add_requires("glew", "libsdl", "libsdl_ttf", "libsdl_mixer", "libsdl_gfx", "box2d", "assimp", "stb", "tmxparser")]], [[add_requires("glew", "libsdl2", "libsdl2_ttf", "libsdl2_mixer", "libsdl2_gfx", "box2d 2.x", "assimp", "stb", "tmxparser")]], {plain = true})
         import("package.tools.xmake").install(package, {kind = (package:config("shared") and "shared" or "static")})
     end)
 
