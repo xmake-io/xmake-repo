c449abe5494f219a8f75724662506fa141634e70
star9029 <hengxings783@gmail.com>
2025-02-10 22:38:50 +0800

    Update xmake.lua

--- packages/w/webui/xmake.lua
+++ packages/w/webui/xmake.lua
@@ -17,6 +17,12 @@ package("webui")
         add_syslinks("pthread", "dl")
     end
 
+    if on_check then
+        on_check("mingw", function (package)
+            assert(package:is_arch("x86_64"), "package(webui/mingw) only suport x86_64 arch")
+        end)
+    end
+
     on_install("windows", "linux", "macosx", "mingw", "msys", "android", "cross", function (package)
         if package:is_plat("android") and package:is_arch("armeabi-v7a") then
             import("core.tool.toolchain")
