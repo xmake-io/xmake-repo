909fc0a46f6028044015628002bc95f6fc9aab90
SuniRein <sunirein@qq.com>
2025-02-10 21:41:37 +0800

    fix: syslink missing on linux in webui.

--- packages/w/webui/xmake.lua
+++ packages/w/webui/xmake.lua
@@ -13,6 +13,8 @@ package("webui")
         add_syslinks("user32", "advapi32", "shell32")
     elseif is_plat("mingw") then
         add_syslinks("ws2_32")
+    elseif is_plat("linux") then
+        add_syslinks("pthread", "dl")
     end
 
     on_install("windows", "linux", "macosx", "mingw", "msys", "android", "cross", function (package)
