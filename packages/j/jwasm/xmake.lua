0926ff135a7d2b0cc39e6e820df38b365a197360
star9029 <hengxings783@gmail.com>
2025-02-14 19:53:21 +0800

    Update xmake.lua

--- packages/j/jwasm/xmake.lua
+++ packages/j/jwasm/xmake.lua
@@ -2,22 +2,29 @@ package("jwasm")
     set_kind("binary")
     set_homepage("https://github.com/JWasm/JWasm")
     set_description("JWasm continuation")
+    set_license("JWasm")
 
     add_urls("https://github.com/JWasm/JWasm.git")
-    add_versions("2022.12.25", "7218960b65d69216693a655d928eb4c2fb6b505c")
+    add_versions("2025.01.10", "a5c4ea03cc0545a15d81a354251b5f534bef7a1b")
 
-    on_install("@windows", "@linux", "@macosx", "@bsd", "@mingw", "@msys", function (package)
+    on_install(function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("jwasm")
                 set_kind("binary")
-                add_files("*.c")
+                add_files("*.c|trmem.c")
                 add_includedirs("H")
+                add_defines("DEBUG_OUT")
+                if is_plat("windows", "mingw") then
+                    add_defines("__NT__")
+                else
+                    add_defines("__UNIX__")
+                end
         ]])
         import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
         -- os.vrun("jwasm -h") -- return 1
-        assert(os.isexec(package:installdir("bin/jwasm") .. (is_host("windows") and ".exe" or "")))
+        assert(os.isexec(path.join(package:installdir("bin"), "jwasm" .. (is_host("windows") and ".exe" or ""))))
     end)
