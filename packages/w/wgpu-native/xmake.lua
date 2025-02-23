2c1f8019a6ba9ea8ceb9cb1a27344a2d5f900d0d
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-02-23 09:43:56 +0100

    adding windows aarch64 download

--- packages/w/wgpu-native/xmake.lua
+++ packages/w/wgpu-native/xmake.lua
@@ -3,7 +3,7 @@ package("wgpu-native")
     set_description("Native WebGPU implementation based on wgpu-core")
     set_license("Apache-2.0")
 
-    if is_plat("windows") and is_arch("x64") then
+    if is_plat("windows") and is_arch("x86_64") then
         add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version).zip", {version = function(version)
             local build = version:ge("v24.0.0+1") and "-msvc" or ""
             return version:gsub("%+", ".") .. "/wgpu-windows-x86_64" .. build .. "-release"
@@ -12,19 +12,26 @@ package("wgpu-native")
         add_versions("v0.19.4+1", "9e1591d60c2d2ee20d6d4a63bc01c7c5eecf7734761673160aa639e550a1ba4d")
         add_versions("v0.17.0+2", "1b8ae05bb7626e037ab7088f9f11fc8bb8341a32800d33857c09ff2fb1b3893f")
     elseif is_plat("windows") and is_arch("x86") then
-        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-windows-i686-release.zip", {version = function(version) return version:gsub("%+", ".") end})
-        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-windows-i686-msvc-release.zip", {version = function(version) return version:gsub("%+", ".") end})
+        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version).zip", {version = function(version)
+            local build = version:ge("v24.0.0+1") and "-msvc" or ""
+            return version:gsub("%+", ".") .. "/wgpu-windows-i686" .. build .. "-release"
+        end})
         add_versions("v24.0.0+1", "88c82813fa5a737dc9c84a3ce7b8e848bf305b6d6e3679f059aa9694e364c761")
         add_versions("v0.19.4+1", "6bd7d57d132282adf46150e7fb176d86fe6ffd10aa833ad2e70d9dfbe17700df")
         add_versions("v0.17.0+2", "098037ca18c1a3fbf25f061f822762d5eab1cd4ecf8e7d039f9ccbd357322a54")
+    elseif is_plat("windows") and is_arch("arm64") then
+        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-windows-aarch64-msvc-release.zip", {version = function(version) return version:gsub("%+", ".") end})
+        add_versions("v24.0.0+1", "0cfc9023dbc2f0ff3ec2b219907b1dd7b46ad83c9a2089e5300ae925ad40664d")
     elseif is_plat("linux") and is_arch("x86_64") then
         add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-linux-x86_64-release.zip", {version = function(version) return version:gsub("%+", ".") end})
         add_versions("v24.0.0+1", "b2169ee4587431f7b1754100115b419795f6155d6163a5de9d0659840fed306b")
         add_versions("v0.19.4+1", "7d73bd7af2be60b632e5ab814996acb381d1b459975d6629f91c468049c8866a")
         add_versions("v0.17.0+2", "2bfebb48072cafee316fcec452d49d02aa46d7096325097e637c3c2e784eca5b")
-    elseif is_plat("linux") and is_arch("arm64-v8a") then
-        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-linux-x86_64-release.zip", {version = function(version) return version:gsub("%+", ".") end})
-        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-linux-aarch64-release.zip", {version = function(version) return version:gsub("%+", ".") end})
+    elseif is_plat("linux") and is_arch("arm64") then
+        add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version).zip", {version = function(version)
+            local arch = version:ge("v24.0.0+1") and "aarch64" or "x86_64"
+            return version:gsub("%+", ".") .. "/wgpu-windows-" .. arch .. "-release"
+        end})
         add_versions("v24.0.0+1", "430007015d6d6560bebf676236a315ccd46a740e628e6b4d777c349edcfdd329")
         add_versions("v0.19.4+1", "6e53aa3f0aec4b2b65cb0d7635000cf39bddd672bcb6138a593bf8cb8134f621")
     elseif is_plat("macosx") and is_arch("x86_64") then
