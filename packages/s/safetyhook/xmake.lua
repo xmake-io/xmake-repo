8900e3fc81f53d7909423b6b6c59a04a6ccba365
Saikari <lin@sz.cn.eu.org>
2025-02-24 19:51:18 +0300

    limit plats

--- packages/s/safetyhook/xmake.lua
+++ packages/s/safetyhook/xmake.lua
@@ -8,10 +8,19 @@ package("safetyhook")
 
     add_versions("v0.6.4", "57c2a7e23e9e0857eb0f5c6322d97d75147b579ae2b8831c821e6dbf6da04298")
 
-    add_deps()
     add_deps("cmake", "zycore-c v1.5.0", "zydis v4.1.0")
 
-    on_install(function (package)
+    on_check("windows", function (package)
+        import("core.base.semver")
+
+        local msvc = package:toolchain("msvc")
+        if msvc then
+            local vs_toolset = package:toolchain("msvc"):config("vs_toolset")
+            assert(vs_toolset and semver.new(vs_toolset):minor() >= 30, "package(safetyhook): need vs_toolset >= v143")
+        end
+    end)
+
+    on_install("windows|!arm*", "mingw", function (package)
         local configs = {"-DSAFETYHOOK_FETCH_ZYDIS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
