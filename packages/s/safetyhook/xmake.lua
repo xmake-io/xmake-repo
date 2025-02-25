955f5e6994f8777ebce3533369ab6b2a29169ff9
Saikari <lin@sz.cn.eu.org>
2025-02-25 11:46:52 +0300

    Removed v prefix, use on_check for x86 plat

--- packages/s/safetyhook/xmake.lua
+++ packages/s/safetyhook/xmake.lua
@@ -8,7 +8,7 @@ package("safetyhook")
 
     add_versions("v0.6.4", "57c2a7e23e9e0857eb0f5c6322d97d75147b579ae2b8831c821e6dbf6da04298")
 
-    add_deps("cmake", "zydis v4.1.0")
+    add_deps("cmake", "zydis 4.1.0")
 
     on_check("windows", function (package)
         import("core.base.semver")
@@ -18,9 +18,13 @@ package("safetyhook")
             local vs_toolset = package:toolchain("msvc"):config("vs_toolset")
             assert(vs_toolset and semver.new(vs_toolset):minor() >= 30, "package(safetyhook): need vs_toolset >= v143")
         end
+
+        if not is_arch("x64", "x86", "x86_64") then 
+            raise("package(safetyhook) only support x86 arch")
+        end
     end)
 
-    on_install("windows|!arm*", "linux|!arm*", "mingw|!arm*", "msys|!arm*", function (package)
+    on_install("windows", "linux", "mingw", "msys", function (package)
         local configs = {"-DSAFETYHOOK_FETCH_ZYDIS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
