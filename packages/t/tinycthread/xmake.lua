eaa1d0e3c47d980c0952324083b83a925ecb2efe
Jérôme Leclercq <lynix680@gmail.com>
2025-02-27 10:35:36 +0100

    Update xmake.lua

--- packages/t/tinycthread/xmake.lua
+++ packages/t/tinycthread/xmake.lua
@@ -5,11 +5,13 @@ package("tinycthread")
 
     add_urls("https://github.com/tinycthread/tinycthread.git")
 
-	on_install("windows", "linux", "macos", function (package)
+    add_versions("2016.09.30", "6957fc8383d6c7db25b60b8c849b29caab1caaee")
+
+	on_install(function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("tinycthread")
-                set_kind("static")
+                set_kind("$(kind)")
                 add_files("source/*.c")
                 add_headerfiles("source/*.h")
         ]])
@@ -18,4 +20,17 @@ package("tinycthread")
 
     on_test(function (package)
         assert(package:has_ctypes("thrd_t", {configs = {languages = "c11"}, includes = "tinycthread.h"}))
+        assert(package:check_csnippets({test = [[
+            int thread_entrypoint(void* arg) {
+                (void) arg;
+                return 0;
+            }
+                    
+            void test() {
+                thrd_t t;
+                if (thrd_create(&t, HelloThread, (void*)0) == thrd_success) {
+                    thrd_join(t, NULL);
+                }
+            }
+        ]]}, {includes = {"tinycthread.h"}}))
     end)
