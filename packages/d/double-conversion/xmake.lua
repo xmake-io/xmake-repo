24bc1cb2c8b4916c0b494bc9a6f790e802db7440
star9029 <hengxings783@gmail.com>
2025-02-28 09:11:52 +0800

    Update xmake.lua

--- packages/d/double-conversion/xmake.lua
+++ packages/d/double-conversion/xmake.lua
@@ -13,7 +13,7 @@ package("double-conversion")
     add_deps("cmake")
 
     on_install(function (package)
-        local configs = {"-DBUILD_TESTING=OFF"}
+        local configs = {"-DBUILD_TESTING=OFF", "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
