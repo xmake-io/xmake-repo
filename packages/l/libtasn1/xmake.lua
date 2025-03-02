1938273e102b517b65c17dcbebf751e97c87a98a
Doekin <105162544+Doekin@users.noreply.github.com>
2025-03-02 18:38:57 +0800

    p11-kit: add package (#6490)
    * libtasn1: add support for additional platforms
    
    * p11-kit: add package
    
    * p11-kit: add license
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/l/libtasn1/xmake.lua
+++ packages/l/libtasn1/xmake.lua
@@ -9,8 +9,10 @@ package("libtasn1")
     add_versions("4.15.0", "dd77509fe8f5304deafbca654dc7f0ea57f5841f41ba530cff9a5bf71382739e")
     add_versions("4.19.0", "1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a")
 
-    on_install("macosx", "linux", function (package)
-        package:addenv("PATH", "bin")
+    on_install("!windows and !wasm", function (package)
+        if not package:is_cross() then
+            package:addenv("PATH", "bin")
+        end
         local configs = {"--disable-doc", "--disable-dependency-tracking"}
         if package:config("shared") then
             table.insert(configs, "--enable-shared")
@@ -18,6 +20,9 @@ package("libtasn1")
         else
             table.insert(configs, "--disable-shared")
             table.insert(configs, "--enable-static")
+        end
+        if is_host("windows") then
+            io.replace("configure", "LIBTOOL='$(SHELL) $(top_builddir)/libtool'", "LIBTOOL='\"$(SHELL)\" $(top_builddir)/libtool'", {plain = true})
         end
         import("package.tools.autoconf").install(package, configs)
     end)
