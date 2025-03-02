6670e9ad7068ddbb0802e10aefea837100174169
Saikari <lin@sz.cn.eu.org>
2025-03-02 18:57:21 +0300

    Limit to !android and !iphoneos and !cross and !wasm

--- packages/p/portmidi/xmake.lua
+++ packages/p/portmidi/xmake.lua
@@ -7,8 +7,12 @@ package("portmidi")
 
     add_versions("v2.0.4", "64893e823ae146cabd3ad7f9a9a9c5332746abe7847c557b99b2577afa8a607c")
 
+    if is_plat("linux", "bsd", "macosx") then
+        add_configs("sndio", {description = "Use sndio", default = false, type = "boolean"})
+    end
+
     if is_plat("windows", "mingw") then
-        add_syslinks("winmm")
+        add_syslinks("winmm", "advapi32")
     elseif is_plat("linux", "bsd") then
         add_syslinks("pthread")
     elseif is_plat("macosx") then
@@ -21,13 +25,18 @@ package("portmidi")
         add_deps("alsa-lib")    
     end
 
-    on_install(function (package)
+    on_load("linux", "bsd", "macosx", function (package)
+        package:add("deps", "libsndio")
+    end)
+
+    on_install("!android and !iphoneos and !cross and !wasm", function (package)
         io.replace("pm_common/CMakeLists.txt", [[MSVC_RUNTIME_LIBRARY]], "", {plain = true})
         io.replace("pm_common/CMakeLists.txt", [["MultiThreaded$<$<CONFIG:Debug>:Debug>${MSVCRT_DLL}"]], "", {plain = true})
         io.replace("pm_win/pmwinmm.c", "midi->fill_offset_ptr = &(hdr->dwBytesRecorded);", "midi->fill_offset_ptr = (uint32_t *) &(hdr->dwBytesRecorded);", {plain = true})
         local configs = {"-DBUILD_PORTMIDI_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_SNDIO=" .. (package:config("sndio") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
