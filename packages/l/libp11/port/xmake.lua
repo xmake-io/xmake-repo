0d7b20265e0538eb5cf3b2e922881b58ccb6b63b
さくらみこ <lin@sz.cn.eu.org>
2025-03-08 16:50:03 +0300

    lower to 199506

--- packages/l/libp11/port/xmake.lua
+++ packages/l/libp11/port/xmake.lua
@@ -59,7 +59,7 @@ target("libp11")
             add_defines("USE_CYGWIN")
         end
     elseif is_plat("linux", "bsd", "android") then
-        add_defines("HAVE_PTHREAD", "_POSIX_C_SOURCE=200809L")
+        add_defines("HAVE_PTHREAD", "_POSIX_C_SOURCE=199506L")
         add_syslinks("pthread", "dl")
     end
 
