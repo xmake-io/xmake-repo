40fb5f64ddbb60587acfac76e002a3b07fe65d29
さくらみこ <lin@sz.cn.eu.org>
2025-03-08 16:03:03 +0300

    fixup

--- packages/l/libp11/xmake.lua
+++ packages/l/libp11/xmake.lua
@@ -13,6 +13,7 @@ package("libp11")
     on_install("!wasm" , function (package)
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         os.cp(path.join(package:scriptdir(), "port", "config.h.in"), "src/config.h.in")
+        io.gsub("src/config.h.in", "# ?undef (.-)\n", "${define %1}\n")
         import("package.tools.xmake").install(package)
     end)
 
