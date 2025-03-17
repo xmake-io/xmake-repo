aedfd47c6ff8bcdfe098a7b4c523d13f03c392a5
star9029 <hengxings783@gmail.com>
2025-03-17 16:43:19 +0800

    cryptopp: fix build_type

--- packages/c/cryptopp/xmake.lua
+++ packages/c/cryptopp/xmake.lua
@@ -1,9 +1,8 @@
 package("cryptopp")
-
     set_homepage("https://cryptopp.com/")
     set_description("free C++ class library of cryptographic schemes")
 
-    add_urls("https://github.com/weidai11/cryptopp/archive/CRYPTOPP_$(version).tar.gz", {version = function (version) return version:gsub("%.", "_") end})
+    add_urls("https://github.com/weidai11/cryptopp/archive/refs/tags/CRYPTOPP_$(version).tar.gz", {version = function (version) return version:gsub("%.", "_") end})
     add_versions("8.9.0", "ab5174b9b5c6236588e15a1aa1aaecb6658cdbe09501c7981ac8db276a24d9ab")
     add_versions("8.7.0", "8d6a4064b8e9f34cd3e838f5a12c40067ee7b95ee37d9173ec273cb0913e7ca2")
     add_versions("8.6.0", "9304625f4767a13e0a5f26d0f019d78cf9375604a33e5391c3bf2e81399dfeb8")
@@ -15,6 +14,14 @@ package("cryptopp")
     add_resources("8.6.0", "cryptopp_cmake", "https://github.com/noloader/cryptopp-cmake/archive/CRYPTOPP_8_6_0.tar.gz", "970b20d55dbf9d6335485e72c9f8967d878bf64bbd3de6aa28436beb6799c493")
     add_resources("8.5.0", "cryptopp_cmake", "https://github.com/noloader/cryptopp-cmake/archive/CRYPTOPP_8_5_0.tar.gz", "10685209405e676993873fcf638ade5f8f99d7949afa6b2045289ce9cc6d90ac")
     add_resources("8.4.0", "cryptopp_cmake", "https://github.com/noloader/cryptopp-cmake/archive/CRYPTOPP_8_4_0.tar.gz", "b850070141f6724fce640e4e2cfde433ec5b2d99d4386d29ba9255167bc4b4f0")
+
+    if is_plat("mingw") and is_subhost("msys") then
+        add_extsources("pacman::crypto++")
+    elseif is_plat("linux") then
+        add_extsources("pacman::crypto++", "apt::libcrypto++-dev")
+    elseif is_plat("macosx") then
+        add_extsources("brew::cryptopp")
+    end
 
     add_deps("cmake")
 
@@ -37,9 +44,12 @@ package("cryptopp")
         -- fix unresolved external symbol PadLastBlock
         -- @see https://github.com/weidai11/cryptopp/issues/358
         io.replace("iterhash.h", "CRYPTOPP_NO_VTABLE", "CRYPTOPP_DLL CRYPTOPP_NO_VTABLE")
+
         local configs = {"-DBUILD_TESTING=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DBUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+
         local cxflags
         if package:is_plat("windows") and package:config("shared") then
             cxflags = "-DCRYPTOPP_EXPORTS"
