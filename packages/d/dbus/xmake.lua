f26111428248067f4b077c4b09b5d802f4a97b8c
さくらみこ <lin@sz.cn.eu.org>
2025-03-18 05:25:54 +0300

    Try proxy

--- packages/d/dbus/xmake.lua
+++ packages/d/dbus/xmake.lua
@@ -4,7 +4,8 @@ package("dbus")
     set_description("D-Bus is a message bus system, a simple way for applications to talk to one another.")
     set_license("GPL-2.0-or-later")
 
-    add_urls("https://gitlab.freedesktop.org/dbus/dbus/-/archive/dbus-$(version)/dbus-dbus-$(version).tar.gz")
+    add_urls("https://gitlab.freedesktop.org/dbus/dbus/-/archive/dbus-$(version)/dbus-dbus-$(version).tar.gz",
+             "https://fossies.org/linux/misc/dbus-$(version).tar.gz")
     add_versions("1.14.2", "9ec5aad6310f79149aa04e8c6bd9e5e2cdca47cf3acec2d23ee9fe06ac7e7a62")
     add_versions("1.14.6", "2533742eb324fa7fbb093a3ed0ff436c7eb11861fd6a31e9b857fc4878f01831")
     add_versions("1.14.8", "273718fe5150a1a44fe77abcf442dc64082f7375fdc15fcbd80b84316f897326")
