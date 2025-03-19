94f60d9cc5b02fb54f666dc60e097b6daaaecc73
さくらみこ <lin@sz.cn.eu.org>
2025-03-19 05:17:18 +0300

    Fix win static builds. Remove CRT enforcement, due rel/debug diff.

--- packages/z/zoe/xmake.lua
+++ packages/z/zoe/xmake.lua
@@ -20,15 +20,17 @@ package("zoe")
         if package:config("openssl") then
             package:add("deps", "openssl")
         end
+        if not package:config("shared") and package:is_plat("windows") then
+            package:add("defines", "ZOE_STATIC")
+        end
     end)
 
     on_install(function (package)
         io.replace("CMakeLists.txt", "set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)", "include(GNUInstallDirs)\nset(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR})", {plain = true})
         io.replace("CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)]], [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR})]], {plain = true})
         io.replace("CMakeLists.txt", [[set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)]], [[set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR})]], {plain = true})
-        local configs = {"-DZOE_BUILD_TESTS=OFF"}
+        local configs = {"-DZOE_BUILD_TESTS=OFF", "-DZOE_USE_STATIC_CRT=OFF"}
         table.insert(configs, "-DZOE_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DZOE_USE_STATIC_CRT=" .. (package:has_runtime("MT") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
