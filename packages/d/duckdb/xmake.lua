8c61807d82fd271faa9b2cfb3a0bff9e87d4d7e5
Arthur <arthur.vasseur@hotmail.fr>
2025-03-19 16:45:31 +0100

    duckdb: enhance CMake installation with configurable build options

--- packages/d/duckdb/xmake.lua
+++ packages/d/duckdb/xmake.lua
@@ -88,7 +88,12 @@ package("duckdb")
             os.trycp("*.hpp", package:installdir("include"))
             return
         end
-        import("package.tools.cmake").install(package)
+
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
