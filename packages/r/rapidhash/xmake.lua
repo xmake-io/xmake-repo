025efb23fe61cbdef7d0c468c5f60b320a848dd2
star9029 <hengxings783@gmail.com>
2025-03-21 16:23:33 +0800

    add git url

--- packages/r/rapidhash/xmake.lua
+++ packages/r/rapidhash/xmake.lua
@@ -4,7 +4,9 @@ package("rapidhash")
     set_homepage("https://github.com/Nicoshev/rapidhash")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/Nicoshev/rapidhash/archive/refs/tags/rapidhash_$(version).tar.gz")
+    add_urls("https://github.com/Nicoshev/rapidhash/archive/refs/tags/rapidhash_$(version).tar.gz",
+             "https://github.com/Nicoshev/rapidhash.git")
+
     add_versions("v1.0", "d295e66eec6745cc0e0c8c65fb8b5edf08ab3af83b0a503c54c6705144b53848")
 
     on_install(function(package)
@@ -12,5 +14,5 @@ package("rapidhash")
     end)
 
     on_test(function(package)
-        assert(package:has_cxxfuncs("rapidhash", {configs = {languages = "c++11"}, includes = "rapidhash.h"}))      
+        assert(package:has_cxxfuncs("rapidhash", {configs = {languages = "c++11"}, includes = "rapidhash.h"}))
     end)
