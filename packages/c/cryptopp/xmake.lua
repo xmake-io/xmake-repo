793da6114271c047e51894ceb1c2623ce9f9fcd8
star9029 <hengxings783@gmail.com>
2025-03-21 15:55:36 +0800

    Update xmake.lua

--- packages/c/cryptopp/xmake.lua
+++ packages/c/cryptopp/xmake.lua
@@ -48,8 +48,10 @@ package("cryptopp")
         -- fix unresolved external symbol PadLastBlock
         -- @see https://github.com/weidai11/cryptopp/issues/358
         io.replace("iterhash.h", "CRYPTOPP_NO_VTABLE", "CRYPTOPP_DLL CRYPTOPP_NO_VTABLE", {plain = true})
-        -- remove CRYPTOPP_BUILD_SHARED hardcore
-        io.replace("cryptopp/CMakeLists.txt", "set(BUILD_SHARED_LIBS ${CRYPTOPP_BUILD_SHARED})", "", {plain = true})
+
+        io.replace("cryptopp/CMakeLists.txt",
+            "set(BUILD_SHARED_LIBS ${CRYPTOPP_BUILD_SHARED})",
+            format("set(CRYPTOPP_BUILD_SHARED %s)", package:config("shared") and "ON" or "OFF"), {plain = true})
 
         local configs = {
             -- Don't auto fetch source code
