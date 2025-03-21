2f821242df2103a4e7083ed855c39589df9900a8
Jérôme Leclercq <lynix680@gmail.com>
2025-03-21 12:26:07 +0100

    update slang

--- packages/s/slang/xmake.lua
+++ packages/s/slang/xmake.lua
@@ -3,10 +3,10 @@ package("slang")
     set_description("Making it easier to work with shaders")
     set_license("MIT")
 
-    add_urls("https://github.com/shader-slang/slang.git")
+    add_urls("https://github.com/shader-slang/slang/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/shader-slang/slang.git")
 
-    add_versions("v2024.1.18", "efdbb954c57b89362e390f955d45f90e59d66878")
-    add_versions("v2024.1.17", "62b7219e715bd4c0f984bcd98c9767fb6422c78f")
+    add_versions("v2025.6.2", 
 
     add_configs("shared", { description = "Build shared library", default = true, type = "boolean", readonly = true })
     add_configs("embed_stdlib_source", { description = "Embed stdlib source in the binary", default = true, type = "boolean" })
