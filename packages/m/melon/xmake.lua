22678e6cd7491d65a3b145ee61ba2a4bb62f3d26
Saikari <lin@sz.cn.eu.org>
2025-03-21 06:48:36 +0300

    Try differ MinGW32/64 and CLang64 shells with on_check

--- packages/m/melon/xmake.lua
+++ packages/m/melon/xmake.lua
@@ -6,20 +6,23 @@ package("melon")
     add_urls("https://github.com/Water-Melon/Melon.git")
     add_versions("2025.01.18", "9df92922ab384295380d4414493e69983671dbf5")
 
-    if is_plat("mingw") then
-        add_deps("mman_win32")    
-    end
+    on_check(function (package)
+        if is_plat("mingw") and is_subhost("msys") then
+            raise("package(melon) is unsupported on MinGW64/UCRT64. Use CLANG64 Shell.")
+        end
+        if is_plat("android") then
+            import("core.tool.toolchain")
+            local ndk = toolchain.load("ndk", {plat = package:plat(), arch = package:arch()})
+            local ndk_sdkver = ndk:config("ndk_sdkver")
+            assert(ndk_sdkver and tonumber(ndk_sdkver) >= 26, "package(melon): need ndk api level >= 26 for android")
+        end
+    end)
 
     on_install(function (package)
         io.replace("include/mln_utils.h", 
         "#if defined(__APPLE__) || defined(MSVC) || defined(__wasm__) || defined(__FreeBSD__)", 
-        "#if defined(__APPLE__) || defined(MSVC) || defined(__wasm__) || defined(__FreeBSD__) || defined(__MINGW32__) || defined(__MINGW64__) || defined(__ANDROID_API__)", {plain = true})
-        if is_plat("mingw") then
-            io.replace("src/mln_bignum.c", "#include <stdlib.h>", "#include <pthread.h>\n#include <stdlib.h>", {plain = true})
+        "#if defined(__APPLE__) || defined(MSVC) || defined(__wasm__) || defined(__FreeBSD__) || defined(__ANDROID_API__)", {plain = true})
 
-            io.replace("include/mln_event.h", "#if defined(MSVC)", "#if 1", {plain = true})
-            io.replace("include/mln_connection.h", "#if defined(MSVC)", "#if 1", {plain = true})
-        end
         if is_plat("windows") then
             for _, file in ipairs(os.files("src/*.c")) do
                 io.replace(file, "!defined(MSVC)", "0", {plain = true})
@@ -36,18 +39,10 @@ package("melon")
         io.replace("src/mln_path.c", "MLN_NULL", [["]] .. path.join(home, "bin"):gsub([[\]], [[\\]]) .. [["]], {plain = true})
         io.replace("src/mln_path.c", "MLN_LANG_LIB", [["]] .. path.join(home, "lib"):gsub([[\]], [[\\]]) .. [["]], {plain = true})
         io.replace("src/mln_path.c", "MLN_LANG_DYLIB", [["]] .. path.join(home, "lib"):gsub([[\]], [[\\]]) .. [["]], {plain = true})
-        io.cat("src/mln_path.c")
 
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
-            if is_plat("mingw") then
-                add_requires("mman_win32")    
-            end
             target("melon")
-                if is_plat("mingw") then
-                    add_packages("mman_win32")
-                    add_defines("_DEFAULT_SOURCE", "_POSIX_C_SOURCE=200809L")
-                end
                 if is_plat("windows") then
                     set_languages("c11")
                 else
