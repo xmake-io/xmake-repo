06a44292edfe5e8eebfdbdf54b36052075f26a74
star9029 <hengxings783@gmail.com>
2025-03-23 00:11:25 +0800

    fix grammer

--- packages/m/msvc-wine/xmake.lua
+++ packages/m/msvc-wine/xmake.lua
@@ -5,7 +5,7 @@ package("msvc-wine")
 
     add_urls("https://github.com/mstorsjo/msvc-wine.git")
 
-    add_version("2025.03.02", "44dc13b5e62ecc2373fbe7e4727a525001f403f4")
+    add_versions("2025.03.02", "44dc13b5e62ecc2373fbe7e4727a525001f403f4")
 
     add_deps("python 3.x", {kind = "binary"})
 
