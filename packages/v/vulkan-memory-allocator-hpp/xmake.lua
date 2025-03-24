90f81e7ef5ff1ab806752a841c338c22e34b073c
Cappecasper03 <03repsac@gmail.com>
2025-03-24 10:58:51 +0100

    Update vulkam-memory-allocator-hpp

--- packages/v/vulkan-memory-allocator-hpp/xmake.lua
+++ packages/v/vulkan-memory-allocator-hpp/xmake.lua
@@ -6,6 +6,8 @@ package("vulkan-memory-allocator-hpp")
 
     add_urls("https://github.com/YaaZ/VulkanMemoryAllocator-Hpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/YaaZ/VulkanMemoryAllocator-Hpp.git")
+    add_versions("v3.2.1", '517FB3FD0D36038124B9644907784840C7E33C52F259B324156687B82FC6A878')
+    add_versions("v3.2.0", '7F24C857642ED9C030A843738892188301F6409E4405938F3F83BA12F0E3B553')
     add_versions("v3.0.0", '2f062b1631af64519d09e7b319c2ba06d7de3c9c5589fb7109a3f4e341cee2b7')
     add_versions("v3.0.1-1", '0e0c374751d5ca6123d0ae0df756693f0674412d7c758ec4a39a5a9dcc412911')
     add_versions("v3.0.1-3", '66a3d4be3bc1404c844b5a36aadeb6b366878e7cf1efe899eb0a0095f3871aae')
