806bd0383c4c38e4e32ed29f2651ebc0482a2a92
Saikari <lin@sz.cn.eu.org>
2025-03-24 21:51:53 +0300

    Init

--- packages/l/linux-syscall-support/xmake.lua
+++ packages/l/linux-syscall-support/xmake.lua
@@ -3,11 +3,9 @@ package("linux-syscall-support")
     set_description("Linux Syscall Support provides a header file that can be included into your application whenever you need to make direct system calls.")
     set_license("BSD-3-Clause")
 
-    add_urls("https://chromium.googlesource.com/linux-syscall-support/+archive/refs/tags/$(version).tar.gz",
-             "https://chromium.googlesource.com/linux-syscall-support.git")
-
-    add_versions("v2022.10.12", "44fa671ed2025304a6eb7edfdf220e722666776ed425e7e17247521eb775201a")
-    add_versions("v2024.02.01", "7067663bd165fc3a914a94385137bc8c8a70d4d613338a8bdf753787a1d22d86")
+    add_urls("https://chromium.googlesource.com/linux-syscall-support.git")
+    add_versions("v2022.10.12", "9719c1e1e676814c456b55f5f070eabad6709d31")
+    add_versions("v2024.02.01", "ed31caa60f20a4f6569883b2d752ef7522de51e0")
 
     set_kind("library", {headeronly = true})
 
