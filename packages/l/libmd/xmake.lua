7c5b2c7495f678ae57cf076b228af21f9e0a0447
Saikari <lin@sz.cn.eu.org>
2025-03-25 15:25:13 +0300

    Try windows

--- packages/l/libmd/xmake.lua
+++ packages/l/libmd/xmake.lua
@@ -8,9 +8,11 @@ package("libmd")
     add_urls("https://libbsd.freedesktop.org/releases/libmd-$(version).tar.xz")
     add_versions("1.0.4", "f51c921042e34beddeded4b75557656559cf5b1f2448033b4c1eec11c07e530f")
 
-    add_deps("autotools")
+    if not is_subhost("windows") then
+        add_deps("autotools")
+    end
 
-    on_install("linux", "android@linux,macosx", "cross", "bsd", "mingw", "wasm", function (package)
+    on_install("linux", "android", "cross", "bsd", "mingw", "wasm", function (package)
         local configs = {"--disable-dependency-tracking"}
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
