737a65c847f02c0e26c405a0701aa2b956c2ed5f
SirLynix <lynix680@gmail.com>
2025-03-26 08:04:25 +0100

    Update xmake.lua

--- packages/v/vulkan-utility-libraries/xmake.lua
+++ packages/v/vulkan-utility-libraries/xmake.lua
@@ -16,7 +16,7 @@ package("vulkan-utility-libraries")
 
     -- when adding a new sdk version, please ensure vulkan-headers, vulkan-hpp, vulkan-loader, vulkan-tools, vulkan-validationlayers, vulkan-utility-libraries, spirv-headers, spirv-reflect, spirv-tools, glslang and volk packages are updated simultaneously
     add_versions("archive:v1.4.309", "d888151924c2ec0a0a852d2c7d6c2262362f535513efc2a3a413cc2071b551d8")
-    add_versions("archive:v1.3.283", "a446616dede2b0168726f4e1b51777ba5c20ec46c475b378e2c07fd4ab4375ee")
+    add_versions("archive:v1.3.283", "ac8d5e943e2477c142245a3b835a14efb9c62d617f7ba7a3712ec21080c66df2")
     add_versions("archive:v1.3.280", "075e13f2fdeeca3bb6fb39155c8cc345cf216ab93661549b1a33368aa28a9dea")
     add_versions("archive:v1.3.275", "96d3ec7bda7b6e9fabbb471c570104a7b1cb56928d097dd0441c96129468b888")
     add_versions("archive:v1.3.268", "990de84b66094b647ae420ba13356b79d69e1c6f95532f40466457d51a9d127d")
