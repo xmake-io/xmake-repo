84945900f0a6cdd336247ab4820d269322971bd1
Saikari <lin@sz.cn.eu.org>
2025-03-26 19:54:35 +0300

    Test hpsocket

--- packages/h/hpsocket/port/xmake.lua
+++ packages/h/hpsocket/port/xmake.lua
@@ -108,16 +108,23 @@ target("hpsocket")
         elseif vs == "2019" then vs_ver = "16.0"
         elseif vs == "2022" then vs_ver = "17.0"
         end
-        if get_config("hpversion") == "v5.9.1" then
+        if get_config("hpversion") >= "v5.9.1" then
             vs_ver = (vs == "2015" and "100" or "14x")
         end
 
+        local arch = "x64"
+        if is_arch("x64", "x86_64", "arm64.*") then
+            arch = "x64"
+        else
+            arch = "x86"
+        end
+
         add_includedirs(".")
-        add_includedirs(path.join(winBuiltinDependentLibPath, "openssl", vs_ver, "$(arch)", "include"))
-        ssllinkdir = path.join(winBuiltinDependentLibPath, "openssl", vs_ver, "$(arch)", "lib")
+        add_includedirs(path.join(winBuiltinDependentLibPath, "openssl", vs_ver, arch, "include"))
+        ssllinkdir = path.join(winBuiltinDependentLibPath, "openssl", vs_ver, arch, "lib")
         add_linkdirs(ssllinkdir)
-        add_includedirs(path.join(winBuiltinDependentLibPath, "zlib", vs_ver, "$(arch)", "include"))
-        zliblinkdir = path.join(winBuiltinDependentLibPath, "zlib", vs_ver, "$(arch)", "lib")
+        add_includedirs(path.join(winBuiltinDependentLibPath, "zlib", vs_ver, arch, "include"))
+        zliblinkdir = path.join(winBuiltinDependentLibPath, "zlib", vs_ver, arch, "lib")
         add_linkdirs(zliblinkdir)
 
         if not has_config("no_ssl") then
--- packages/h/hpsocket/xmake.lua
+++ packages/h/hpsocket/xmake.lua
@@ -8,6 +8,7 @@ package("hpsocket")
     add_versions("v5.7.3", "e653f3c15ded3a4b622ab9a4a52a477c7aa40f5b86398c6b75f5a732a55496a0")
     add_versions("v5.8.4", "6fd207b84e41174c06d27c0df7244584eb07fbac0a7e49d7429103071184a451")
     add_versions("v5.9.1", "d40a3d0b4f0d2773ae61d32ed95df655aa6ccf5ae22c40ef38bfc88882b2478b")
+    add_versions("v6.0.1", "df8649a889b8eec97ddd02b00fe5274b8fb20d6da4f2c636082325a962c635f1")
 
     local configs = {{name = "udp",    package = "kcp"},
                      {name = "http",   package = "http_parser"},
