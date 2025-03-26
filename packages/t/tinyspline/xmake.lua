a38b84666edb738fa6b0bfc5c6a8539260e50ac4
Saikari <lin@sz.cn.eu.org>
2025-03-26 03:02:06 +0300

    Fixup

--- packages/t/tinyspline/xmake.lua
+++ packages/t/tinyspline/xmake.lua
@@ -12,10 +12,17 @@ package("tinyspline")
 
     on_install(function (package)
         io.replace("src/CMakeLists.txt", [["lib64"]], [["lib"]], {plain = true})
-        local configs = { 
+        local configs = {
             "-DTINYSPLINE_BUILD_EXAMPLES=OFF", "-DTINYSPLINE_BUILD_TESTS=OFF", 
             "-DTINYSPLINE_BUILD_DOCS=OFF",  "-DTINYSPLINE_WARNINGS_AS_ERRORS=OFF"
         }
+        -- Fix for mingw@macosx being recognized as Windows
+        if package:is_plat("mingw") and is_subhost("macosx") then
+            table.insert(configs, "-DCMAKE_SYSTEM_NAME=Darwin")
+        end
+        -- Fix for newer WASM/Emscripten versions
+        io.replace("src/tinysplinecxx.h", "toWireType(const std::vector<T, Allocator> &vec)", "toWireType(const std::vector<T, Allocator> &vec, rvp::default_tag)", {plain = true})
+        io.replace("src/tinysplinecxx.h", "return ValBinding::toWireType(val::array(vec))", "return ValBinding::toWireType(val::array(vec), rvp::default_tag{})", {plain = true})
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
