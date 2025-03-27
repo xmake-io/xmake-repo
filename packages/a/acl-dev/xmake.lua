79066486becb00017b7f0c5e9ea65bdae413866b
Saikari <lin@sz.cn.eu.org>
2025-03-27 17:50:38 +0300

    try to fix installpath

--- packages/a/acl-dev/xmake.lua
+++ packages/a/acl-dev/xmake.lua
@@ -12,22 +12,51 @@ package("acl-dev")
 
     add_deps("cmake")
 
+    add_links("libacl", "acl",
+              "libacl_cpp", "acl_cpp",
+              "")
+
     on_install(function (package)
-        -- Fix static lib install path
-        io.replace("lib_fiber/c/CMakeLists.txt", [[ARCHIVE_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_fiber/c/CMakeLists.txt", [[LIBRARY_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[LIBRARY_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_acl/CMakeLists.txt", [[ARCHIVE_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_acl/CMakeLists.txt", [[LIBRARY_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[LIBRARY_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_protocol/CMakeLists.txt", [[ARCHIVE_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_protocol/CMakeLists.txt", [[LIBRARY_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[LIBRARY_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_fiber/cpp/CMakeLists.txt", [[ARCHIVE_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_fiber/cpp/CMakeLists.txt", [[LIBRARY_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[LIBRARY_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_acl_cpp/CMakeLists.txt", [[ARCHIVE_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
-        io.replace("lib_acl_cpp/CMakeLists.txt", [[LIBRARY_OUTPUT_DIRECTORY ${lib_output_path}/static]], [[LIBRARY_OUTPUT_DIRECTORY "${CMAKE_INSTALL_LIBDIR}"]], {plain = true})
+        -- Fix libs install path
+        io.replace("lib_fiber/c/CMakeLists.txt", [[${lib_output_path}/static]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_fiber/c/CMakeLists.txt", [[${lib_output_path}/shared]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_fiber/c/CMakeLists.txt", [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_LIBDIR}]], [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_BINDIR}]], {plain = true})
+
+        io.replace("lib_fiber/c/CMakeLists.txt", [[set(LIBRARY_OUTPUT_PATH]], [[#set(LIBRARY_OUTPUT_PATH]], {plain = true})
+        io.replace("lib_fiber/c/CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], [[#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], {plain = true})
+
+        io.replace("lib_acl/CMakeLists.txt", [[${acl_output_path}/static]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_acl/CMakeLists.txt", [[${acl_output_path}/shared]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_acl/CMakeLists.txt", [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_LIBDIR}]], [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_BINDIR}]], {plain = true})
+
+        io.replace("lib_acl/CMakeLists.txt", [[set(LIBRARY_OUTPUT_PATH]], [[#set(LIBRARY_OUTPUT_PATH]], {plain = true})
+        io.replace("lib_acl/CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], [[#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], {plain = true})
+
+        io.replace("lib_protocol/CMakeLists.txt", [[${lib_output_path}/static]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_protocol/CMakeLists.txt", [[${lib_output_path}/shared]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_protocol/CMakeLists.txt", [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_LIBDIR}]], [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_BINDIR}]], {plain = true})
+
+        io.replace("lib_protocol/CMakeLists.txt", [[set(LIBRARY_OUTPUT_PATH]], [[#set(LIBRARY_OUTPUT_PATH]], {plain = true})
+        io.replace("lib_protocol/CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], [[#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], {plain = true})
+
+        io.replace("lib_fiber/cpp/CMakeLists.txt", [[${lib_output_path}/static]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_fiber/cpp/CMakeLists.txt", [[${lib_output_path}/shared]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_fiber/cpp/CMakeLists.txt", [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_LIBDIR}]], [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_BINDIR}]], {plain = true})
+
+        io.replace("lib_fiber/cpp/CMakeLists.txt", [[set(LIBRARY_OUTPUT_PATH]], [[#set(LIBRARY_OUTPUT_PATH]], {plain = true})
+        io.replace("lib_fiber/cpp/CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], [[#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], {plain = true})
+
+        io.replace("lib_acl_cpp/CMakeLists.txt", [[${lib_output_path}/static]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_acl_cpp/CMakeLists.txt", [[${lib_output_path}/shared]], [[${CMAKE_INSTALL_LIBDIR}]], {plain = true})
+        io.replace("lib_acl_cpp/CMakeLists.txt", [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_LIBDIR}]], [[RUNTIME_OUTPUT_DIRECTORY ${CMAKE_INSTALL_BINDIR}]], {plain = true})
+
+        io.replace("lib_acl_cpp/CMakeLists.txt", [[set(LIBRARY_OUTPUT_PATH]], [[#set(LIBRARY_OUTPUT_PATH]], {plain = true})
+        io.replace("lib_acl_cpp/CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], [[#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY]], {plain = true})
+
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DACL_BUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DACL_BUILD_SHARED=" .. (package:config("shared") and "YES" or "NO"))
         import("package.tools.cmake").install(package, configs)
     end)
 
