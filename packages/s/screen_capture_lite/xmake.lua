59f2a7b861d39a7a3a88dcc1ec8388948060c538
ruki <waruqi@gmail.com>
2025-03-27 13:21:21 +0800

    Update screen_capture_lite to 17.1.1369 (#6720)

--- packages/s/screen_capture_lite/xmake.lua
+++ packages/s/screen_capture_lite/xmake.lua
@@ -6,6 +6,7 @@ package("screen_capture_lite")
     add_urls("https://github.com/smasherprog/screen_capture_lite/archive/refs/tags/$(version).tar.gz",
              "https://github.com/smasherprog/screen_capture_lite.git")
 
+    add_versions("17.1.1369", "72abf1cd9fc538e98547841ec56396ba75a36853e861f7bf721f21c64c34453e")
     add_versions("17.1.1368", "78b5f6c2689f49672ff465e4e832377a244455ada90d271d1cd44c3c3ecef952")
 
     add_deps("cmake", "lodepng", "tinyjpeg")
