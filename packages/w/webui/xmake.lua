5030501c86e4a20b112a816a9328aa324e464257
HapiFive <1143910315@qq.com>
2025-03-27 18:21:09 +0800

    webui: add config for log

--- packages/w/webui/xmake.lua
+++ packages/w/webui/xmake.lua
@@ -9,6 +9,8 @@ package("webui")
     add_versions("2.4.2", "c51967bbab472655d04e28ce1668ee4adda1f320e05f98c14f071b2cdf61228b")
     add_versions("2.3.0", "14be57405b12cf434daade2310178534240866e3169c7213a6fa0e4a6c6f9f27")
 
+    add_configs("log", {description = "Enable log", default = true, type = "boolean"})
+
     if is_plat("windows") then
         add_syslinks("user32", "advapi32", "shell32", "ws2_32", "ole32")
     elseif is_plat("mingw") then
@@ -29,22 +31,42 @@ package("webui")
             end
         end
 
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("webui")
-                set_kind("$(kind)")
-                add_files("src/civetweb/civetweb.c", {defines = {"NO_CACHING", "NO_CGI", "NO_SSL", "USE_WEBSOCKET"}})
-                add_files("src/webui.c", {defines = "WEBUI_LOG"})
-                add_headerfiles("include/webui.h", "include/webui.hpp")
-                add_includedirs("include", "src/civetweb")
-                if is_plat("windows") then
-                    add_syslinks("user32", "advapi32", "shell32", "ws2_32", "ole32")
-                elseif is_plat("mingw") then
-                    add_syslinks("ws2_32")
-                elseif is_plat("linux") then
-                    add_syslinks("pthread", "dl")
-                end
-        ]])
+        local log = package:config("log")
+        if log then
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                target("webui")
+                    set_kind("$(kind)")
+                    add_files("src/civetweb/civetweb.c", {defines = {"NO_CACHING", "NO_CGI", "NO_SSL", "USE_WEBSOCKET"}})
+                    add_files("src/webui.c", {defines = "WEBUI_LOG"})
+                    add_headerfiles("include/webui.h", "include/webui.hpp")
+                    add_includedirs("include", "src/civetweb")
+                    if is_plat("windows") then
+                        add_syslinks("user32", "advapi32", "shell32", "ws2_32", "ole32")
+                    elseif is_plat("mingw") then
+                        add_syslinks("ws2_32")
+                    elseif is_plat("linux") then
+                        add_syslinks("pthread", "dl")
+                    end
+            ]])
+        else
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                target("webui")
+                    set_kind("$(kind)")
+                    add_files("src/civetweb/civetweb.c", {defines = {"NO_CACHING", "NO_CGI", "NO_SSL", "USE_WEBSOCKET"}})
+                    add_files("src/webui.c")
+                    add_headerfiles("include/webui.h", "include/webui.hpp")
+                    add_includedirs("include", "src/civetweb")
+                    if is_plat("windows") then
+                        add_syslinks("user32", "advapi32", "shell32", "ws2_32", "ole32")
+                    elseif is_plat("mingw") then
+                        add_syslinks("ws2_32")
+                    elseif is_plat("linux") then
+                        add_syslinks("pthread", "dl")
+                    end
+            ]])
+        end
         import("package.tools.xmake").install(package)
     end)
 
