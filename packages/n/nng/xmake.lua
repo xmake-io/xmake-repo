aaa7c6a5eb68491915801dc6a0c9f39bef578b86
star9029 <hengxings783@gmail.com>
2025-03-29 11:59:51 +0800

    nng: improve version & add license (#6736)

--- packages/n/nng/xmake.lua
+++ packages/n/nng/xmake.lua
@@ -1,15 +1,17 @@
 package("nng")
-
     set_homepage("https://github.com/nanomsg/nng")
     set_description("NNG, like its predecessors nanomsg (and to some extent ZeroMQ), is a lightweight, broker-less library, offering a simple API to solve common recurring messaging problems.")
+    set_license("MIT")
 
-    add_urls("https://github.com/nanomsg/nng/archive/v$(version).zip")
-    add_versions("1.10.1", "ce209b0830c8dc69980cbd78aa39f4590a28799e7a00fd2b938a82cf740e7d9a")
-    add_versions("1.8.0", "48effcfd1acf31f6afcb1a92ecb4aa87f7993f5c54bf6587b0f4fb2606ce96d3")
-    add_versions("1.7.3", "72b39c63ba6467f6ee70c218c291240ff5b189569c3b84e8e0647f5d4d3888ea")
-    add_versions("1.5.2", "652ff3a2dbaeae194942205c369e9259e2b5cb5985d679d744cbfb95d1c807a3")
-    add_versions("1.4.0", "43674bb15d0f3810cf3602d2662cc91b6576b914492710244125e32b29f546b8")
-    add_versions("1.3.2", "2616110016c89ed3cbd458022ba41f4f545ab17f807546d2fdd0789b55d64471")
+    add_urls("https://github.com/nanomsg/nng/archive/refs/tags/$(version).zip",
+             "https://github.com/nanomsg/nng.git")
+
+    add_versions("v1.10.1", "ce209b0830c8dc69980cbd78aa39f4590a28799e7a00fd2b938a82cf740e7d9a")
+    add_versions("v1.8.0", "48effcfd1acf31f6afcb1a92ecb4aa87f7993f5c54bf6587b0f4fb2606ce96d3")
+    add_versions("v1.7.3", "72b39c63ba6467f6ee70c218c291240ff5b189569c3b84e8e0647f5d4d3888ea")
+    add_versions("v1.5.2", "652ff3a2dbaeae194942205c369e9259e2b5cb5985d679d744cbfb95d1c807a3")
+    add_versions("v1.4.0", "43674bb15d0f3810cf3602d2662cc91b6576b914492710244125e32b29f546b8")
+    add_versions("v1.3.2", "2616110016c89ed3cbd458022ba41f4f545ab17f807546d2fdd0789b55d64471")
 
     -- default is false
     add_configs("NNG_ELIDE_DEPRECATED", {description = "Elide deprecated functionality.", default = false, type = "boolean"})
