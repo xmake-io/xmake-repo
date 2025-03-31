067fd4682bd4a67518f2aa33be284709915717cb
Redbeanw44602 <redbeana44945@gmail.com>
2025-03-31 22:36:47 +0800

    update.

--- packages/f/funchook/xmake.lua
+++ packages/f/funchook/xmake.lua
@@ -8,7 +8,7 @@ package("funchook")
 
     add_versions("v1.1.3", "4b0195e70524237e222dc34c53ac25e12677bb936e64eefe33189931688444c4")
 
-    -- TODO: This patch cannot be used with the latest commit.
+    -- Warning: This patch cannot be used with the latest commit.
     add_patches("*", "patches/fix-build-system-deps.patch", "fe01ce372df7cb4c5c2420ed2be3486772f723007ff254fc277103a5eaf1bddc")
     add_patches("*", "patches/fix-function-visibility.patch", "5b505ad24332320f3970a6cb56b5f550b01b9c80aa14cea0fea74ac77f1fc8f3")
 
@@ -35,7 +35,7 @@ package("funchook")
             end
         else
             if package:config("disasm") == "zydis" then
-                -- The latest commit updates to 4.x, but you must use 3.x for the current version.
+                -- The latest commit updated to 4.x, but we must use 3.x for the current version.
                 package:add("deps", "zydis 3.2.1")
             else
                 package:add("deps", package:config("disasm"))
