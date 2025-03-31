f177ef728225e46ff8e229a350784c91946e044c
RedbeanW <redbeana44945@gmail.com>
2025-03-31 15:25:10 +0800

    Update xmake.lua

--- packages/a/aklomp-base64/xmake.lua
+++ packages/a/aklomp-base64/xmake.lua
@@ -3,9 +3,10 @@ package("aklomp-base64")
     set_description("Fast Base64 stream encoder/decoder in C99, with SIMD acceleration.")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/aklomp/base64.git")
+    add_urls("https://github.com/aklomp/base64/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/aklomp/base64.git")
 
-    add_versions("0.5.2", "8bdda2d47caf8b066999c5bd01069e55bcd0d396")
+    add_versions("v0.5.2", "723a0f9f4cf44cf79e97bcc315ec8f85e52eb104c8882942c3f2fba95acc080d")
 
     add_deps("cmake")
     on_load(function (package)
@@ -18,7 +19,6 @@ package("aklomp-base64")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE="  .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-
         import("package.tools.cmake").install(package, configs)
     end)
 
