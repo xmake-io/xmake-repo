678b3de3704e10387cd025d49e416ad1ca219f11
star9029 <hengxings783@gmail.com>
2025-04-04 22:07:16 +0800

    limit plat

--- packages/d/dlfcn-win32/xmake.lua
+++ packages/d/dlfcn-win32/xmake.lua
@@ -11,14 +11,15 @@ package("dlfcn-win32")
 
     add_deps("cmake")
 
-    on_install(function (package)
+    on_install("windows", "mingw", function (package)
+        if package:config("shared") then
+            package:add("defines", "DLFCN_WIN32_SHARED")
+        end
+
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
-        if package:config("shared") then
-            package:add("defines", "DLFCN_WIN32_SHARED")
-        end
     end)
 
     on_test(function (package)
