e9234c0b82884e72d421bd9daf8f50f753dbd020
star9029 <hengxings783@gmail.com>
2025-04-06 05:43:38 +0800

    msgpack23: add package

---
+++ packages/m/msgpack23/xmake.lua
@@ -0,0 +1,26 @@
+package("msgpack23")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/rwindegger/msgpack23")
+    set_description("A modern, header-only C++ library for MessagePack serialization and deserialization. msgpack.org[c++23]")
+    set_license("MIT")
+
+    add_urls("https://github.com/rwindegger/msgpack23/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/rwindegger/msgpack23.git")
+
+    add_versions("v2.1", "9ce1e294518aa76cac50f778a359aed17a0daa0d8dc4c1f94cd4f12438b3606c")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {"-DBUILD_TESTING=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                msgpack23::Packer packer;
+            }
+        ]]}, {configs = {languages = "c++23"}, includes = "msgpack23/msgpack23.h"}))
+    end)
