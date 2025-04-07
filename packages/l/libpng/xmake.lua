c986473f54491337c20bb76a7262f59b7acb2173
Elite-stay <344233108@qq.com>
2025-04-07 10:11:50 +0800

    libpng: add older version

--- packages/l/libpng/xmake.lua
+++ packages/l/libpng/xmake.lua
@@ -16,6 +16,7 @@ package("libpng")
     add_versions("v1.6.36", "5bef5a850a9255365a2dc344671b7e9ef810de491bd479c2506ac3c337e2d84f")
     add_versions("v1.6.35", "6d59d6a154ccbb772ec11772cb8f8beb0d382b61e7ccc62435bf7311c9f4b210")
     add_versions("v1.6.34", "e45ce5f68b1d80e2cb9a2b601605b374bdf51e1798ef1c2c2bd62131dfcf9eef")
+    add_versions("v1.2.56", "dadb07d7d96c20f2b53b923b582b078821423f356c0bd52d9a7c14e5e2eafb87")
 
     add_deps("zlib")
 
@@ -32,44 +33,61 @@ package("libpng")
     end
 
     on_install(function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            add_requires("zlib")
-            target("png")
-                set_kind("$(kind)")
-                add_files("*.c|example.c|pngtest.c")
-                if is_arch("x86", "x64", "i386", "x86_64") then
-                    add_files("intel/*.c")
-                    add_defines("PNG_INTEL_SSE_OPT=1")
-                    add_vectorexts("sse", "sse2")
-                elseif is_arch("arm.*") then
-                    add_files("arm/*.c")
-                    if is_plat("windows") then
-                        add_defines("PNG_ARM_NEON_OPT=1")
-                        add_defines("PNG_ARM_NEON_IMPLEMENTATION=1")
-                    else
-                        add_files("arm/*.S")
-                        add_defines("PNG_ARM_NEON_OPT=2")
+        if package:version():le("v1.2.56") then
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                add_requires("zlib")
+                target("png")
+                    set_kind("$(kind)")
+                    add_files("*.c|example.c|pngtest.c")
+                    add_defines("LIBPNG_NO_MMX")
+                    add_defines("PNG_NO_MMX_CODE")
+                    add_headerfiles("*.h")
+                    add_packages("zlib")
+                    if is_kind("shared") and is_plat("windows") then
+                        add_defines("PNG_BUILD_DLL")
                     end
-                elseif is_arch("mips.*") then
-                    add_files("mips/*.c")
-                    add_defines("PNG_MIPS_MSA_OPT=2")
-                elseif is_arch("ppc.*") then
-                    add_files("powerpc/*.c")
-                    add_defines("PNG_POWERPC_VSX_OPT=2")
-                end
-                add_headerfiles("*.h")
-                add_packages("zlib")
-                if is_kind("shared") and is_plat("windows") then
-                    add_defines("PNG_BUILD_DLL")
-                end
-        ]])
-        local configs = {}
-        if package:is_plat("android") and package:is_arch("armeabi-v7a") then
-            io.replace("arm/filter_neon.S", ".func", ".hidden", {plain = true})
-            io.replace("arm/filter_neon.S", ".endfunc", "", {plain = true})
+            ]])
+        else
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                add_requires("zlib")
+                target("png")
+                    set_kind("$(kind)")
+                    add_files("*.c|example.c|pngtest.c")
+                    if is_arch("x86", "x64", "i386", "x86_64") then
+                        add_files("intel/*.c")
+                        add_defines("PNG_INTEL_SSE_OPT=1")
+                        add_vectorexts("sse", "sse2")
+                    elseif is_arch("arm.*") then
+                        add_files("arm/*.c")
+                        if is_plat("windows") then
+                            add_defines("PNG_ARM_NEON_OPT=1")
+                            add_defines("PNG_ARM_NEON_IMPLEMENTATION=1")
+                        else
+                            add_files("arm/*.S")
+                            add_defines("PNG_ARM_NEON_OPT=2")
+                        end
+                    elseif is_arch("mips.*") then
+                        add_files("mips/*.c")
+                        add_defines("PNG_MIPS_MSA_OPT=2")
+                    elseif is_arch("ppc.*") then
+                        add_files("powerpc/*.c")
+                        add_defines("PNG_POWERPC_VSX_OPT=2")
+                    end
+                    add_headerfiles("*.h")
+                    add_packages("zlib")
+                    if is_kind("shared") and is_plat("windows") then
+                        add_defines("PNG_BUILD_DLL")
+                    end
+            ]])
+            if package:is_plat("android") and package:is_arch("armeabi-v7a") then
+                io.replace("arm/filter_neon.S", ".func", ".hidden", {plain = true})
+                io.replace("arm/filter_neon.S", ".endfunc", "", {plain = true})
+            end
+            os.cp("scripts/pnglibconf.h.prebuilt", "pnglibconf.h")
         end
-        os.cp("scripts/pnglibconf.h.prebuilt", "pnglibconf.h")
+        local configs = {}
         import("package.tools.xmake").install(package, configs)
     end)
 
