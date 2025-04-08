a8c95fcb89b73668720adcb89ea5a5511e3f43e0
Louis Poirier <contact@louispoirier.io>
2025-04-08 19:00:56 +0200

    v8: fix and update package for V8 library
    - support latest stable version 13.5.212.10
    - tested on Windows only
    - fix syntax to configure "runtimes"
    - only build static monolith flavor
    - patch to fix build configuration of a third party
    - erase previous version 10.0.58

--- packages/v/v8/xmake.lua
+++ packages/v/v8/xmake.lua
@@ -1,9 +1,11 @@
 package("v8")
-    set_homepage("https://chromium.googlesource.com/v8/v8.git")
+    set_homepage("https://v8.dev")
     set_description("V8 JavaScript Engine")
 
     add_urls("https://github.com/v8/v8.git")
-    add_versions("10.0.58", "d75903764c8547b6fc35c7a3fe4991320be03135")
+    add_versions("13.3.415.19", "fe051262efbbd92479a08436f733eba9f756e008")
+    add_versions("13.4.114.19", "a57459aad9ec3ed5f78d9ded700d52e31029efd2")
+    add_versions("13.5.212.10", "e2591684c45463aa1e46ebefc3fd35deee63f37c")
 
     add_deps("depot_tools")
 
@@ -11,24 +13,11 @@ package("v8")
         add_syslinks("pthread", "dl")
     elseif is_plat("windows") then
         add_syslinks("user32", "winmm", "advapi32", "dbghelp", "shlwapi")
-        add_configs("vs_runtime", {description = "Set vs runtime.", default = "MT", readonly = true})
+        add_configs("runtimes", {description = "Set runtime.", default = "MT", readonly = true})
     end
 
-    add_links("v8_monolith",
-              "v8_initializers",
-              "v8_init",
-              "v8_compiler",
-              "v8_compiler_opt",
-              "v8_cppgc_shared",
-              "v8_bigint",
-              "v8_snapshot",
-              "v8_base_without_compiler",
-              "v8_libplatform",
-              "v8_libbase",
-              "torque_base",
-              "torque_generated_definitions",
-              "cppgc_base",
-              "torque_ls_base")
+    add_includedirs("include", {public = true})
+    add_links("v8_monolith")
 
     on_install("linux", "macosx", "windows", function (package)
         import("core.base.global")
@@ -49,30 +38,67 @@ package("v8")
     "managed": False,
     "custom_deps": {},
   }]]=])
+
         if package:is_plat("windows") then
-            envs.DEPOT_TOOLS_WIN_TOOLCAHIN = "0"
+            envs.DEPOT_TOOLS_WIN_TOOLCHAIN = "0"
+            envs.GYP_MSVS_VERSION = "2022"
         end
+
         local gclient = is_host("windows") and "gclient.bat" or "gclient"
-        os.vrunv(gclient, {"sync", "-v"}, {envs = envs})
+
+        -- Prevent long path issue on Windows
+        os.vrunv("git", {"config", "--local", "core.longpaths", "true"})
+
+        -- Update repository and dependencies
+        -- Clean any local changes to apply patches
+        try {
+            function()
+                os.vrunv(gclient, {"sync", "-v", "--reset", "--delete_unversioned_trees"}, {envs = envs})
+            end,
+
+            finally {
+                function()
+                    os.vrunv("git", {"config", "--local", "--unset", "core.longpaths"})
+                end
+            }
+        }
+
+        -- Fix GN issue
+        local version = package:version()
+        local patch_path = path.join(package:scriptdir(), "patches", version, "fix.patch")
+
+        if os.exists(patch_path) then
+            local source_path = path.join(package:cachedir(), "source", "v8")
+            local fix_path = path.join(source_path, "third_party", "partition_alloc")
+
+            os.execv("git", {"apply", patch_path}, {curdir = source_path})
+            os.execv("git", {"add", path.join("src", "partition_alloc", "BUILD.gn")}, {curdir = fix_path})
+            os.execv("git", {"commit", "-m", "xmake-patch-01"}, {curdir = fix_path})
+        end
+
+        -- Setup args.gn
         local configs = {
             is_official_build = false,
             is_component_build = false,
             is_debug = package:debug(),
-            is_shared_library = package:config("shared"),
-            symbol_level = 0,
+            symbol_level = package:debug() and 2 or 0,
+            strip_debug_info = not package:debug(),
             treat_warnings_as_errors = false,
             use_custom_libcxx = false,
-            v8_static_library = not package:config("shared"),
             v8_monolithic = true,
-            v8_use_external_startup_data = false,
+            v8_enable_sandbox = false,
+            v8_enable_pointer_compression = false,
+            v8_enable_webassembly = false,
+            v8_enable_gdbjit = package:debug(),
+            v8_enable_i18n_support = false,
             v8_enable_test_features = false,
-            v8_enable_i18n_support = false}
+            v8_use_external_startup_data = false
+        }
 
-        if package:is_plat("windows") then
-            configs.extra_cflags = {(package:config("vs_runtime"):startswith("MT") and "/MT" or "/MD")}
-            configs.is_clang = false 
-        end
-        import("package.tools.gn").build(package, configs, {buildir = "out.gn"})
+        -- Build V8 library
+        import("package.tools.gn").build(package, configs, {buildir = "out.gn", target = {"v8_monolith"}})
+
+        -- Install headers and library files
         os.cp("include", package:installdir())
         os.trycp("out.gn/obj/*.a", package:installdir("lib"))
         os.trycp("out.gn/obj/*.lib", package:installdir("lib"))
@@ -80,5 +106,11 @@ package("v8")
     end)
 
     on_test(function (package)
-        assert(package:has_cxxfuncs("v8::V8::InitializePlatform(0)", {configs = {languages = "c++17"}, includes = "v8.h"}))
+        assert(package:has_cxxfuncs("v8::V8::InitializePlatform(0)", {
+            configs = {
+                languages = "c++20",
+                cxxflags = "/Zc:__cplusplus"
+            },
+            includes = "v8.h"
+        }))
     end)
