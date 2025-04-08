0c1b74f8ff1aec6e05f42a098c6dad9d6ac48203
Saikari <lin@sz.cn.eu.org>
2025-04-08 17:41:29 +0300

    Validate websocketpp on win/linux/mac

--- packages/w/websocketpp/xmake.lua
+++ packages/w/websocketpp/xmake.lua
@@ -7,10 +7,13 @@ package("websocketpp")
              "https://github.com/zaphoyd/websocketpp.git")
     add_versions("0.8.2", "6ce889d85ecdc2d8fa07408d6787e7352510750daa66b5ad44aacb47bea76755")
 
-    add_deps("cmake")
-    add_deps("boost")
+    -- Compatibility fixes for Boost 1.87 Reference to PR https://github.com/zaphoyd/websocketpp/pull/1164
+    --add_patches("0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "1d50646a00452155443f401a4b88b8b16047f3218cbe0a33a6947e92d094a13c")
 
-    on_install("linux", "macosx", "windows", function (package)
+    add_deps("cmake")
+    add_deps("boost") --, {configs = {system = true, asio = true, regex = true}}
+
+    on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
