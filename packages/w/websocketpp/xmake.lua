14a2936185a2a0d42985988ac7fc73f546c8b1ba
Saikari <lin@sz.cn.eu.org>
2025-04-09 10:31:28 +0300

    Update xmake.lua

--- packages/w/websocketpp/xmake.lua
+++ packages/w/websocketpp/xmake.lua
@@ -8,7 +8,7 @@ package("websocketpp")
     add_versions("0.8.2", "6ce889d85ecdc2d8fa07408d6787e7352510750daa66b5ad44aacb47bea76755")
 
     -- Compatibility fixes for Boost 1.87 Reference to PR https://github.com/zaphoyd/websocketpp/pull/1164
-    add_patches("0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "1d50646a00452155443f401a4b88b8b16047f3218cbe0a33a6947e92d094a13c")
+    add_patches("0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "5396d10ebe593f031580b3c3683f205a8d4c57f2d0942d48c5a4b74e64365f97")
 
     add_deps("cmake")
     add_deps("boost", {configs = {system = true, asio = true, regex = true, thread = true}})
