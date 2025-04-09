8dd22f4819a22b2ea7c49b19d735ee73215b59ba
Saikari <lin@sz.cn.eu.org>
2025-04-09 13:28:27 +0300

    websocketpp: adapt to new versions of boost (#6853)
    * Validate websocketpp on win/linux/mac
    
    * pass missing `boost` components
    
    * fixup
    
    * pass `thread`
    
    * support boost 1.87 via patch
    
    * Update xmake.lua
    
    * Update xmake.lua

--- packages/w/websocketpp/xmake.lua
+++ packages/w/websocketpp/xmake.lua
@@ -7,11 +7,14 @@ package("websocketpp")
              "https://github.com/zaphoyd/websocketpp.git")
     add_versions("0.8.2", "6ce889d85ecdc2d8fa07408d6787e7352510750daa66b5ad44aacb47bea76755")
 
-    add_deps("cmake")
-    add_deps("boost")
+    -- Compatibility fixes for Boost 1.87 Reference to PR https://github.com/zaphoyd/websocketpp/pull/1164
+    add_patches("0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "5396d10ebe593f031580b3c3683f205a8d4c57f2d0942d48c5a4b74e64365f97")
 
-    on_install("linux", "macosx", "windows", function (package)
-        local configs = {}
+    add_deps("cmake")
+    add_deps("boost", {configs = {system = true, asio = true, regex = true, thread = true}})
+
+    on_install("!wasm", function (package)
+        local configs = {"-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
