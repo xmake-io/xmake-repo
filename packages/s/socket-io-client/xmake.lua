f09a0c6cfadf48acd04a36a23b4c55818d088898
Saikari <lin@sz.cn.eu.org>
2025-04-11 12:05:21 +0300

    websocketpp: add `standalone_asio` option, socket-io-client: use `asio::asio`

--- packages/s/socket-io-client/xmake.lua
+++ packages/s/socket-io-client/xmake.lua
@@ -12,16 +12,13 @@ package("socket-io-client")
     end})
 
     add_versions("2024.07.17", "da779141a7379cc30c870d48295033bc16a23c66")
-    -- Synchronize with 03.06.2024
-    -- add_patches("3.1.0", [[https://github.com/socketio/socket.io-client-cpp/compare/3.1.0%2E%2E%2.patch]], "31f3e6be5f618c8e3d670351d47169edacedd24c8c59396769d0d693ab08b673")
 
     if is_plat("linux", "bsd") then
         add_syslinks("pthread")
     end
 
     add_deps("cmake", "websocketpp", "rapidjson", "openssl")
-    add_deps("boost", {configs = {context = true, asio = true}}) 
-    -- add_deps("asio")
+    add_deps("asio 1.32.0")
 
     if is_host("windows") then
         add_deps("pkgconf")
@@ -30,48 +27,35 @@ package("socket-io-client")
     end
 
     on_install(function (package)
-        io.replace("CMakeLists.txt", "-DASIO_STANDALONE", "", {plain = true})
-        io.replace("CMakeLists.txt", "BUILD_TESTING", "0", {plain = true})
+        io.replace("CMakeLists.txt", "if((CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME AND BUILD_TESTING) OR BUILD_UNIT_TESTS)", "if(0)", {plain = true})
 
-        -- io.replace("CMakeLists.txt", "find_package(asio CONFIG REQUIRED)",
--- [[find_package(PkgConfig)
--- pkg_check_modules(asio REQUIRED IMPORTED_TARGET asio)]], {plain = true})
-        -- io.replace("CMakeLists.txt", "asio::asio", "PkgConfig::asio", {plain = true})
-
-        io.replace("CMakeLists.txt", "find_package(asio CONFIG REQUIRED)", "find_package(Boost REQUIRED COMPONENTS asio)", {plain = true})
-        io.replace("CMakeLists.txt", "asio::asio", "Boost::asio", {plain = true})
-
+        io.replace("CMakeLists.txt", "find_package(asio CONFIG REQUIRED)",
+[[find_package(PkgConfig)
+pkg_check_modules(asio REQUIRED IMPORTED_TARGET asio)]], {plain = true})
+        io.replace("CMakeLists.txt", "asio::asio", "PkgConfig::asio", {plain = true})
 
         io.replace("CMakeLists.txt", "find_package(RapidJSON CONFIG REQUIRED)", 
-[[find_package(PkgConfig)
-pkg_check_modules(rapidjson REQUIRED IMPORTED_TARGET RapidJSON)]], {plain = true})
+[[pkg_check_modules(rapidjson REQUIRED IMPORTED_TARGET RapidJSON)]], {plain = true})
         io.replace("CMakeLists.txt", "rapidjson)", "PkgConfig::rapidjson)", {plain = true})
-
-        -- io.replace("CMakeLists.txt", "${OPENSSL_INCLUDE_DIR}", "${OPENSSL_INCLUDE_DIR} PkgConfig::rapidjson")
-
-        for _, file in ipairs(table.join(os.files("src/**.cpp"), os.files("src/**.h"))) do
-            io.replace(file, [[#include <asio/]], [[#include <boost/asio/]], {plain = true})
-            io.replace(file, [[boost/asio/error_code.hpp]], [[boost/asio/error.hpp]], {plain = true})
-        end
 
         local configs = {"-DUSE_SUBMODULES=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs) --, {packagedeps = {"websocketpp", "boost", "rapidjson"}}
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
+            #include <string>
             using namespace sio;
+            using namespace std;
             void test() {
             socket::ptr current_socket;
-            current_socket->on("new message", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
+            current_socket->on("new message", sio::socket::event_listener_aux(
+                [&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
                 {
-                    _lock.lock();
                     string user = data->get_map()["username"]->get_string();
                     string message = data->get_map()["message"]->get_string();
-                    EM(user<<":"<<message);
-                    _lock.unlock();
                 }));
             }
         ]]}, {configs = {languages = "c++11"}, includes = "sio_client.h"}))
--- packages/w/websocketpp/xmake.lua
+++ packages/w/websocketpp/xmake.lua
@@ -6,12 +6,22 @@ package("websocketpp")
     add_urls("https://github.com/zaphoyd/websocketpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/zaphoyd/websocketpp.git")
     add_versions("0.8.2", "6ce889d85ecdc2d8fa07408d6787e7352510750daa66b5ad44aacb47bea76755")
-
-    -- Compatibility fixes for Boost 1.87 Reference to PR https://github.com/zaphoyd/websocketpp/pull/1164
-    add_patches("0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "5396d10ebe593f031580b3c3683f205a8d4c57f2d0942d48c5a4b74e64365f97")
+    
+    add_configs("asio_standalone", {description = "Use standalone asio", default = true, type = "boolean"})
 
     add_deps("cmake")
-    add_deps("boost", {configs = {system = true, asio = true, regex = true, thread = true}})
+
+    on_load(function (package)
+        if package:config("asio_standalone") then
+            package:add("defines", "ASIO_STANDALONE", "_WEBSOCKETPP_CPP11_STL_")
+            package:add("deps", "asio 1.32.0")
+            package:add("deps", "boost", {configs = {system = true, regex = true, thread = true, type_traits = true}})
+        else
+            package:add("deps", "boost", {configs = {system = true, asio = true, regex = true, thread = true}})
+            -- Compatibility fixes for Boost 1.87 Reference to PR https://github.com/zaphoyd/websocketpp/pull/1164
+            package:add("patches", "0.8.2", [[https://github.com/zaphoyd/websocketpp/compare/0.8.2%2E%2E%2Eamini-allight%3Awebsocketpp%3Adevelop.diff]], "5396d10ebe593f031580b3c3683f205a8d4c57f2d0942d48c5a4b74e64365f97")
+        end
+    end)
 
     on_install("!wasm", function (package)
         local configs = {"-DCMAKE_POLICY_DEFAULT_CMP0057=NEW"}
