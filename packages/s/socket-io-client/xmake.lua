36d7105356cc45f5e0174698a2f2651724a6d41a
star9029 <hengxings783@gmail.com>
2025-04-11 20:00:21 +0800

    Update xmake.lua

--- packages/s/socket-io-client/xmake.lua
+++ packages/s/socket-io-client/xmake.lua
@@ -3,38 +3,28 @@ package("socket-io-client")
     set_description("C++11 implementation of Socket.IO client")
     set_license("MIT")
 
-    add_urls("https://github.com/socketio/socket.io-client-cpp.git")
+    add_urls("https://github.com/socketio/socket.io-client-cpp.git", {submodules = false})
     add_versions("2024.07.17", "da779141a7379cc30c870d48295033bc16a23c66")
 
     if is_plat("linux", "bsd") then
         add_syslinks("pthread")
     end
 
-    add_deps("cmake", "websocketpp", "rapidjson", "openssl")
-    add_deps("asio <=1.32.0")
-
-    if is_host("windows") then
-        add_deps("pkgconf")
-    else
-        add_deps("pkg-config")
-    end
+    add_deps("cmake")
+    add_deps("websocketpp", "rapidjson", "openssl", "asio <=1.32.0")
 
     on_install("!wasm", function (package)
-        io.replace("CMakeLists.txt", "if((CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME AND BUILD_TESTING) OR BUILD_UNIT_TESTS)", "if(0)", {plain = true})
+        io.replace("CMakeLists.txt", "find_package(asio CONFIG REQUIRED)", "", {plain = true})
+        io.replace("CMakeLists.txt", "asio::asio", "", {plain = true})
+        io.replace("CMakeLists.txt", " asio ", "", {plain = true})
 
-        io.replace("CMakeLists.txt", "find_package(asio CONFIG REQUIRED)",
-[[find_package(PkgConfig)
-pkg_check_modules(asio REQUIRED IMPORTED_TARGET asio)]], {plain = true})
-        io.replace("CMakeLists.txt", "asio::asio", "PkgConfig::asio", {plain = true})
-
-        io.replace("CMakeLists.txt", "find_package(RapidJSON CONFIG REQUIRED)", 
-[[pkg_check_modules(rapidjson REQUIRED IMPORTED_TARGET RapidJSON)]], {plain = true})
-        io.replace("CMakeLists.txt", "rapidjson)", "PkgConfig::rapidjson)", {plain = true})
-
-        local configs = {"-DUSE_SUBMODULES=OFF"}
+        local configs = {"-DUSE_SUBMODULES=OFF", "-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = {"asio"}})
+        if package:config("shared") and package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+        end
+        import("package.tools.cmake").install(package, configs, {packagedeps = {"asio", "openssl"}})
     end)
 
     on_test(function (package)
