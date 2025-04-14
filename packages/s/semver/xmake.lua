f29a20a103ffb62d71964f1adcc153a41d767e4f
star9029 <hengxings783@gmail.com>
2025-04-14 20:06:41 +0800

    semver: add v1.0.0-rc

--- packages/s/semver/xmake.lua
+++ packages/s/semver/xmake.lua
@@ -4,20 +4,32 @@ package("semver")
     set_description("Semantic Versioning for modern C++")
     set_license("MIT")
 
-    add_urls("https://github.com/Neargye/semver.git")
+    add_urls("https://github.com/Neargye/semver/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/Neargye/semver.git")
 
-    add_versions("2023.10.04", "92df4b229d0f43382f5934ed50d6489d44e4a9c4")
-    add_versions("2023.05.27", "492ba1beb1da6a4f150b8ca45985290f152396ed")
+    add_versions("v1.0.0-rc", "343a667ecf619ead05ba75ccd6bc500e7a809a450b2a79fe3ee92238f2ecf814")
+
+    add_deps("cmake")
 
     on_install(function (package)
-        os.cp("include", package:installdir())
+        local configs = {
+            "-DSEMVER_OPT_BUILD_EXAMPLES=OFF",
+            "-DSEMVER_OPT_BUILD_TESTS=OFF",
+            "-DSEMVER_OPT_INSTALL=ON",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             #include <semver.hpp>
             void test() {
-                constexpr semver::version v1{1, 4, 3};
+                semver::version v1;
+                if (semver::parse("1.4.3", v1)) {
+                    const int patch = v1.patch(); // 3
+                    // do something...
+                }
             }
         ]]}, {configs = {languages = "c++17"}}))
     end)
