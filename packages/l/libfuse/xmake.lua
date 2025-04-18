8e6a139fc7fa517b7ceb3f923f218887d8e1e8a8
star9029 <hengxings783@gmail.com>
2025-04-18 13:55:26 +0800

    libfuse: add 3.17.1 (#6928)
    * libfuse: add 3.17.1
    
    * Update xmake.lua

--- packages/l/libfuse/xmake.lua
+++ packages/l/libfuse/xmake.lua
@@ -1,18 +1,22 @@
 package("libfuse")
-
     set_homepage("https://github.com/libfuse/libfuse")
     set_description("FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel.")
     set_license("GPL-2.0")
 
+    add_urls("https://github.com/libfuse/libfuse/releases/download/fuse-$(version)/fuse-$(version).tar.gz",
+             "https://github.com/libfuse/libfuse.git")
+
     add_urls("https://github.com/libfuse/libfuse/releases/download/fuse-$(version)/fuse-$(version).tar.xz")
+
+    add_versions("3.17.1", "2d8ae87a4525fbfa1db5e5eb010ff6f38140627a7004554ed88411c1843d51b2")
     add_versions("3.10.4", "9365b74fd8471caecdb3cc5adf25a821f70a931317ee9103d15bd39089e3590d")
 
-    if is_plat("linux") then
-        add_extsources("apt::fuse3", "pacman::fuse3")
-        add_syslinks("pthread", "dl", "rt")
-    end
+    add_extsources("apt::fuse3", "pacman::fuse3")
 
-    add_deps("meson")
+    add_syslinks("pthread", "dl", "rt")
+
+    add_deps("meson", "ninja")
+
     on_install("linux", function (package)
         local configs = {"-Dtests=false", "-Dexamples=false", "-Dutils=false"}
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
