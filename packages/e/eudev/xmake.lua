3893a8815edff8b6a0a9a6520de90261aaa7af07
star9029 <hengxings783@gmail.com>
2025-04-19 09:12:23 +0800

    remove doc

--- packages/e/eudev/xmake.lua
+++ packages/e/eudev/xmake.lua
@@ -6,23 +6,36 @@ package("eudev")
     add_urls("https://github.com/eudev-project/eudev/archive/refs/tags/$(version).tar.gz",
              "https://github.com/eudev-project/eudev.git")
 
+    add_versions("v3.2.14", "c340e6c51dfc5531ac0c0fa84a34b72162acf525f9023eb9cf4931b782c8f177")
     add_versions("v3.2.9", "7d281276b480da3935d1acb239748c2c9db01a8043aad7e918ce57a223d8cd24")
+
+    add_configs("kmod", {description = "Enable loadable modules support", default = false, type = "boolean"})
+    add_configs("selinux", {description = "Enable optional SELINUX support", default = false, type = "boolean", readonly = true})
+    add_configs("tools", {description = "Build tools", default = true, type = "boolean"})
 
     add_deps("autotools", "pkg-config", "gperf")
 
-    on_install("linux", "cross", function (package)
-        local configs = {}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
-        else
-            table.insert(configs, "--enable-shared=no")
+    on_load(function (package)
+        if package:config("kmod") then
+            package:add("deps", "libkmod")
         end
-        if package:config("pic") then
-            table.insert(configs, "--with-pic")
-        end
-        import("package.tools.autoconf").install(package, configs)
     end)
 
+    on_install("linux", "cross", function (package)
+        io.replace("autogen.sh", "./make.sh", "", {plain = true}) --remove doc build
+        if package:config("kmod") then
+            io.replace("configure.ac", "libkmod >= 15", "libkmod >= v15", {plain = true})
+            io.replace("src/udev/udev-builtin-kmod.c", "#include <libkmod.h>", "#include <libkmod/libkmod.h>", {plain = true})
+        end
+
+        local configs = {"--disable-manpages"}
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+        table.insert(configs, "--enable-kmod=" .. (package:config("kmod") and "yes" or "no"))
+        table.insert(configs, "--enable-selinux=" .. (package:config("selinux") and "yes" or "no"))
+        table.insert(configs, "--enable-programs=" .. (package:config("tools") and "yes" or "no"))
+        import("package.tools.autoconf").install(package, configs)
+    end)
 
     on_test(function (package)
         assert(package:has_cfuncs("udev_new", {includes = "libudev.h"}))
