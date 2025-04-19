b5a895904ff8f84b83724ca7e29630db1e312bb7
star9029 <hengxings783@gmail.com>
2025-04-19 08:21:58 +0800

    remove v3.2.14

--- packages/e/eudev/xmake.lua
+++ packages/e/eudev/xmake.lua
@@ -6,7 +6,6 @@ package("eudev")
     add_urls("https://github.com/eudev-project/eudev/archive/refs/tags/$(version).tar.gz",
              "https://github.com/eudev-project/eudev.git")
 
-    add_versions("v3.2.14", "c340e6c51dfc5531ac0c0fa84a34b72162acf525f9023eb9cf4931b782c8f177")
     add_versions("v3.2.9", "7d281276b480da3935d1acb239748c2c9db01a8043aad7e918ce57a223d8cd24")
 
     add_deps("autotools", "pkg-config", "gperf")
