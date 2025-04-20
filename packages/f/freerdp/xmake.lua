32eaa912be0364314d5243ffbd9681d227d16666
Saikari <lin@sz.cn.eu.org>
2025-04-20 17:58:51 +0300

    Update xmake.lua

--- packages/f/freerdp/xmake.lua
+++ packages/f/freerdp/xmake.lua
@@ -26,9 +26,7 @@ package("freerdp")
     add_configs("openh264", {description = "Build openh264", default = false, type = "boolean"})
     add_configs("krb5", {description = "Compile support for kerberos authentication.", default = false, type = "boolean"})
     -- Try resolve emmintrin.h:740:1: error: inlining failed in call to 'always_inline' '_mm_storeu_si128': target specific option mismatch
-    if not (is_arch("i386") and is_plat("mingw") and is_subhost("macosx")) then
-        add_configs("simd", {description = "Build with simd", default = not is_plat("wasm"), type = "boolean"})
-    end
+    add_configs("simd", {description = "Build with simd", default = not is_plat("wasm") and not (is_arch("i386") and is_plat("mingw") and is_subhost("macosx")), type = "boolean"})
 
     -- winpr
     add_configs("unicode_builtin", {description = "Build builtin unicode", default = true, type = "boolean"})
@@ -125,6 +123,7 @@ package("freerdp")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=" .. (package:config("lto") and "ON" or "OFF"))
+        table.insert(configs, "-DWITH_SIMD=" .. (package:config("simd") and "ON" or "OFF"))
 
         local dep = package:config("json")
         table.insert(configs, "-DWITH_JSON_DISABLED=" .. (dep and "OFF" or "ON"))
