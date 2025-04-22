51248225acec23d97ff3b7aa7e4cfe537956f74a
jj683 <jj683@protonmail.com>
2025-04-22 15:34:16 +0200

    add strong_type v15

---
+++ packages/s/strong_type/xmake.lua
@@ -0,0 +1,20 @@
+package("strong_type")
+
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/rollbear/strong_type")
+    set_description("Implementation of strong types in C++.")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/rollbear/strong_type/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/rollbear/strong_type.git")
+
+    add_versions("v15", "8a2b339474904887cbb9820c48cbe28d8d9af538")
+
+    add_deps("cmake")
+    on_install(function (package)
+        import("package.tools.cmake").install(package, {"-DSTRONG_TYPE_UNIT_TEST=OFF"})
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets("using myint = strong::type<int, struct my_int_>;", {configs = {languages = "c++14"}, includes = "strong_type/strong_type.hpp"}))
+    end)
