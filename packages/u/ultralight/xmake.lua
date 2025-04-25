bb64b2df26e1b1723c659871fc45c82d62eda4c0
さくらみこ <lin@sz.cn.eu.org>
2025-04-25 23:55:05 +0300

    Try fix `Archlinux / build (ubuntu-latest, shared) (pull_request)`, semver fixup

--- packages/u/ultralight/xmake.lua
+++ packages/u/ultralight/xmake.lua
@@ -3,24 +3,32 @@ package("ultralight")
     set_description("Ultralight makes it easy for C/C++ developers to seamlessly integrate web-content into games and desktop apps.")
     set_license("LGPL")
 
+    local versions = {
+        ["2023.07.24"] = "208d653"
+    }
+
     if is_plat("windows") then
         if is_arch("x86_64", "x64") then
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-win-x64.7z", {alias = "release"})
-            add_versions("release:208d653", "4fa7aadd1e4ba4a7dc04d17b1d82b37b141c6e4e7196501150486fa6ac1635c5") -- 2023-07-24T22:00:29.614Z
+            add_versions("release:2023.07.24", "4fa7aadd1e4ba4a7dc04d17b1d82b37b141c6e4e7196501150486fa6ac1635c5", {version = function (version) return versions[tostring(version)] end})
         end
     elseif is_plat("linux") then
         if is_arch("x86_64", "x64") then
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-linux-x64.7z", {alias = "release"})
-            add_versions("release:208d653", "1de6298b5ed3c5e0c22ac27e0e30fcb0ba6d195467a58ee44ef4e13dd1a6d352") -- 2023-07-24T22:00:06.347Z
+            add_versions("release:2023.07.24", "1de6298b5ed3c5e0c22ac27e0e30fcb0ba6d195467a58ee44ef4e13dd1a6d352", {version = function (version) return versions[tostring(version)] end})
         end
     elseif is_plat("macosx") then
         if is_arch("x86_64", "x64") then
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-mac-x64.7z", {alias = "release"})
-            add_versions("release:208d653", "bbf81ed456a617a60a19e9a76946e4479d5bac877f859005c50f66e9ec3c77a2") -- 2023-07-24T21:59:44.717Z
+            add_versions("release:2023.07.24", "bbf81ed456a617a60a19e9a76946e4479d5bac877f859005c50f66e9ec3c77a2", {version = function (version) return versions[tostring(version)] end})
         end
     end
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
+
+    if is_plat("linux") then
+        add_deps("fontconfig")
+    end
 
     on_install("windows|x64", "linux|x86_64", "macosx|x86_64", function (package)
         os.cp("include", package:installdir())
