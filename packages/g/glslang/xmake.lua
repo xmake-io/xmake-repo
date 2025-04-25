0e9910124f87530f4b66f5997c1e915f89adcefd
ruki <waruqi@gmail.com>
2025-04-25 23:06:06 +0800

    Update xmake.lua

--- packages/g/glslang/xmake.lua
+++ packages/g/glslang/xmake.lua
@@ -91,8 +91,8 @@ package("glslang")
             package:add("links", "glslang", "glslang-default-resource-limits")
         end
 
-        os.cp("glslang/MachineIndependent/**.h", path.join(package:installdir("include"), "glslang", "MachineIndependent"))
-        os.cp("glslang/Include/**.h", path.join(package:installdir("include"), "glslang", "Include"))
+        os.cp("glslang/MachineIndependent/**.h", package:installdir("include", "glslang", "MachineIndependent"))
+        os.cp("glslang/Include/**.h", package:installdir("include", "glslang", "Include"))
 
         -- https://github.com/KhronosGroup/glslang/releases/tag/12.3.0
         local bindir = package:installdir("bin")
