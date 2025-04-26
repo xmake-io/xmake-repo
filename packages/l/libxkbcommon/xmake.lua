9147bf9612eaeca4824fddf6bbe7176f35c4881f
Cappecasper03 <112723892+Cappecasper03@users.noreply.github.com>
2025-04-26 13:21:12 +0200

    Update packages/l/libxkbcommon/xmake.lua
    Co-authored-by: Jérôme Leclercq <lynix680@gmail.com>

--- packages/l/libxkbcommon/xmake.lua
+++ packages/l/libxkbcommon/xmake.lua
@@ -43,7 +43,7 @@ package("libxkbcommon")
             "-Dx-locale-root=/usr/share/X11/locale",
             format("-Denable-x11=%s", package:config("x11")),
             format("-Denable-wayland=%s", package:config("wayland")),
-            format("-Dbash-completion-path=%s", package:installdir() .. "share/bash-completion/completions")
+            format("-Dbash-completion-path=%s", path.join(package:installdir(), "share", "bash-completion", "completions"))
         }
 
         import("package.tools.meson").install(package, configs)
