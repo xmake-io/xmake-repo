9b098fd711a699f74c0493af9cc6683f9f01638c
Saikari <lin@sz.cn.eu.org>
2025-04-27 18:44:25 +0300

    Update xmake.lua

--- packages/u/ultralight/xmake.lua
+++ packages/u/ultralight/xmake.lua
@@ -3,9 +3,7 @@ package("ultralight")
     set_description("Ultralight makes it easy for C/C++ developers to seamlessly integrate web-content into games and desktop apps.")
     set_license("LGPL")
 
-    local versions = {
-        ["1.3.0"] = "208d653",
-    }
+    local versions = { ["1.3.0"] = "208d653" }
 
     if is_plat("windows") then
         if is_arch("x86_64", "x64") then
