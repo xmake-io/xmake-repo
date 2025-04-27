da34b84a6d39180ddf24999edf7b5d165dc6bc6e
Saikari <lin@sz.cn.eu.org>
2025-04-27 15:30:29 +0300

    Update xmake.lua

--- packages/r/resip/xmake.lua
+++ packages/r/resip/xmake.lua
@@ -78,7 +78,7 @@ package("resip")
             table.insert(configs, "--enable-debug")
         end
         if package:is_plat("bsd", "android") then
-            opt.cxflags = "-D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR"
+            table.insert(configs, "CXXFLAGS=-D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR")
         end
         import("package.tools.autoconf").install(package, configs)
     end)
