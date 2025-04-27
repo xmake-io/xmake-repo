2920f3d46a5043f328f04cafa976590c2a435613
Saikari <lin@sz.cn.eu.org>
2025-04-27 12:31:32 +0300

    Update xmake.lua

--- packages/u/ultralight/xmake.lua
+++ packages/u/ultralight/xmake.lua
@@ -4,7 +4,7 @@ package("ultralight")
     set_license("LGPL")
 
     local versions = {
-        ["2023.07.24"] = "208d653",
+        ["1.3.0"] = "208d653",
     }
 
     if is_plat("windows") then
@@ -12,21 +12,21 @@ package("ultralight")
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-win-x64.7z", {alias = "release", version = function (version)
                 return versions[tostring(version)]
             end})
-            add_versions("release:2023.07.24", "4fa7aadd1e4ba4a7dc04d17b1d82b37b141c6e4e7196501150486fa6ac1635c5")
+            add_versions("release:1.3.0", "4fa7aadd1e4ba4a7dc04d17b1d82b37b141c6e4e7196501150486fa6ac1635c5")
         end
     elseif is_plat("linux") then
         if is_arch("x86_64", "x64") then
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-linux-x64.7z", {alias = "release", version = function (version)
                 return versions[tostring(version)]
             end})
-            add_versions("release:2023.07.24", "1de6298b5ed3c5e0c22ac27e0e30fcb0ba6d195467a58ee44ef4e13dd1a6d352")
+            add_versions("release:1.3.0", "1de6298b5ed3c5e0c22ac27e0e30fcb0ba6d195467a58ee44ef4e13dd1a6d352")
         end
     elseif is_plat("macosx") then
         if is_arch("x86_64", "x64") then
             add_urls("https://ultralight-sdk.sfo2.cdn.digitaloceanspaces.com/ultralight-sdk-$(version)-mac-x64.7z", {alias = "release", version = function (version)
                 return versions[tostring(version)]
             end})
-            add_versions("release:2023.07.24", "bbf81ed456a617a60a19e9a76946e4479d5bac877f859005c50f66e9ec3c77a2")
+            add_versions("release:1.3.0", "bbf81ed456a617a60a19e9a76946e4479d5bac877f859005c50f66e9ec3c77a2")
         end
     end
 
