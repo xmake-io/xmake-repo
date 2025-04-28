7af3e986cd1cd066c1bb890ab85b08e902ce48d7
さくらみこ <lin@sz.cn.eu.org>
2025-04-29 02:45:58 +0300

    try make

--- packages/l/libbpf/xmake.lua
+++ packages/l/libbpf/xmake.lua
@@ -9,27 +9,41 @@ package("libbpf")
     add_versions("v0.3", "c168d84a75b541f753ceb49015d9eb886e3fb5cca87cdd9aabce7e10ad3a1efc")
     add_versions("v1.3.4", "236f404707977c4856ad53c58182862cf79671bc244b906ee1137cfd3c7d9688")
 
+    add_configs("make", {description = "Use make buildsystem", default = true, type = "boolean"})
+
     add_deps("libelf", "zlib")
 
     add_includedirs("include", "include/uapi")
 
+    on_load(function (package)
+        if package:config("make") then
+            package:add("deps", "autotools")
+        end
+    end)
+
     on_install("linux", "android", function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            add_requires("libelf", "zlib")
-            target("bpf")
-                set_kind("$(kind)")
-                add_files("src/*.c")
-                add_includedirs("include", "include/uapi")
-                add_packages("libelf", "zlib")
-                add_headerfiles("src/(*.h)", {prefixdir = "bpf"})
-                add_headerfiles("include/(uapi/**.h)")
-                if is_plat("android") then
-                    add_defines("__user=", "__force=", "__poll_t=uint32_t")
-                end
-        ]])
-        local configs = {}
-        import("package.tools.xmake").install(package, configs)
+        if package:config("make") then
+            local configs = {}
+            table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+            table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+            import("package.tools.autoconf").install(package, configs)
+        else
+            io.writefile("xmake.lua", [[
+                add_rules("mode.debug", "mode.release")
+                add_requires("libelf", "zlib")
+                target("bpf")
+                    set_kind("$(kind)")
+                    add_files("src/*.c")
+                    add_includedirs("include", "include/uapi")
+                    add_packages("libelf", "zlib")
+                    add_headerfiles("src/(*.h)", {prefixdir = "bpf"})
+                    add_headerfiles("include/(uapi/**.h)")
+                    if is_plat("android") then
+                        add_defines("__user=", "__force=", "__poll_t=uint32_t")
+                    end
+            ]])
+            import("package.tools.xmake").install(package)
+        end
     end)
 
     on_test(function (package)
