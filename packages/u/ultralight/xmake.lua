fd83d2c65c67a66100507787e13a2e184542cec9
Saikari <lin@sz.cn.eu.org>
2025-04-29 10:01:29 +0300

    ultralight: update to 1.4.0-beta (#7008)
    * ultralight: update to 1.4.0-beta
    
    * cl
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Fixup
    
    * Update xmake.lua
    
    * Limit distros
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua

--- packages/u/ultralight/xmake.lua
+++ packages/u/ultralight/xmake.lua
@@ -4,7 +4,7 @@ package("ultralight")
     set_license("LGPL")
 
     local versions = {
-        ["1.3.0"] = "208d653",
+        ["1.3.0"] = "208d653"
     }
 
     if is_plat("windows") then
@@ -13,6 +13,11 @@ package("ultralight")
                 return versions[tostring(version)]
             end})
             add_versions("release:1.3.0", "4fa7aadd1e4ba4a7dc04d17b1d82b37b141c6e4e7196501150486fa6ac1635c5")
+            add_urls("https://github.com/xmake-mirror/Ultralight/releases/download/$(version)", {alias = "mirror", version = function (version)
+                local beta_version = version:gsub("-beta$", "b")
+                return version .. "/ultralight-sdk-" .. beta_version .. "-win-x64.7z"
+            end})
+            add_versions("mirror:1.4.0-beta", "6749c3d1aef49ba1c4ca783a453fe2f68b827b5935534751b68623b4b0eb91f1")
         end
     elseif is_plat("linux") then
         if is_arch("x86_64", "x64") then
@@ -20,6 +25,17 @@ package("ultralight")
                 return versions[tostring(version)]
             end})
             add_versions("release:1.3.0", "1de6298b5ed3c5e0c22ac27e0e30fcb0ba6d195467a58ee44ef4e13dd1a6d352")
+            add_urls("https://github.com/xmake-mirror/Ultralight/releases/download/$(version)", {alias = "mirror", version = function (version)
+                local beta_version = version:gsub("-beta$", "b")
+                return version .. "/ultralight-sdk-" .. beta_version .. "-linux-x64.7z"
+            end})
+            add_versions("mirror:1.4.0-beta", "1d5092bfd7d96417547872a5c5b5950207f495ea299d713fa105314f4185c760")
+        else
+            add_urls("https://github.com/xmake-mirror/Ultralight/releases/download/$(version)", {alias = "mirror", version = function (version)
+                local beta_version = version:gsub("-beta$", "b")
+                return version .. "/ultralight-sdk-" .. beta_version .. "-linux-arm64.7z"
+            end})
+            add_versions("mirror:1.4.0-beta", "efa8f6c8b351daa42570f11bcb162f280cca2ce8e167f854a5e0687db854f268")
         end
     elseif is_plat("macosx") then
         if is_arch("x86_64", "x64") then
@@ -27,6 +43,17 @@ package("ultralight")
                 return versions[tostring(version)]
             end})
             add_versions("release:1.3.0", "bbf81ed456a617a60a19e9a76946e4479d5bac877f859005c50f66e9ec3c77a2")
+            add_urls("https://github.com/xmake-mirror/Ultralight/releases/download/$(version)", {alias = "mirror", version = function (version)
+                local beta_version = version:gsub("-beta$", "b")
+                return version .. "/ultralight-sdk-" .. beta_version .. "-mac-x64.7z"
+            end})
+            add_versions("mirror:1.4.0-beta", "ac2abd395a5080d35d36a482b7c8e2f4e7bb89bfb6705d35ec07d9dcb4528fa7")
+        else
+            add_urls("https://github.com/xmake-mirror/Ultralight/releases/download/$(version)", {alias = "mirror", version = function (version)
+                local beta_version = version:gsub("-beta$", "b")
+                return version .. "/ultralight-sdk-" .. beta_version .. "-mac-arm64.7z"
+            end})
+            add_versions("mirror:1.4.0-beta", "3b8c71cf8e403738dcdb12cacc233838c168d48322c31d40ec1c6fcaa761a016")
         end
     end
 
@@ -36,7 +63,20 @@ package("ultralight")
         add_deps("fontconfig")
     end
 
-    on_install("windows|x64", "linux|x86_64", "macosx|x86_64", function (package)
+    on_check(function (package)
+        if package:version():gt("1.3.0") then
+            if package:is_plat("linux") then
+                local distrib = linuxos.name()
+                assert(distrib ~= "archlinux", "package(ultralight): Archlinux is not supported.")
+                assert(distrib ~= "fedora", "package(ultralight): Fedora is not supported.")
+            end
+        else
+            assert(not package:is_arch("arm.*"), "package(ultralight): version older than 1.4.0 does not support arm.")
+        end
+        assert(not package:is_arch("x86", "i386", "i686"), "package(ultralight): does not support x86/i386/i686.")
+    end)
+
+    on_install("windows|!arm*", "linux", "macosx", function (package)
         os.cp("include", package:installdir())
         os.trycp("bin/*.dll", package:installdir("bin"))
         os.trycp("lib/*.lib", package:installdir("lib"))
