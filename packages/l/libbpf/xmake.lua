f8a2c9a552e2ccf50ad4648397666e042eb2811c
さくらみこ <lin@sz.cn.eu.org>
2025-04-29 03:31:14 +0300

    try fixup

--- packages/l/libbpf/xmake.lua
+++ packages/l/libbpf/xmake.lua
@@ -29,10 +29,8 @@ package("libbpf")
             -- Fix installdir for .so / .a to expected *lib*
             io.replace("Makefile", [[LIBSUBDIR := lib64]], [[LIBSUBDIR := lib]], {plain = true})
             -- Wrap BUILD_STATIC_ONLY to install only .so or .a
-            io.replace("Makefile", [[STATIC_LIBS := $(OBJDIR)/libbpf.a]], "ifdef BUILD_STATIC_ONLY\nSTATIC_LIBS := $(OBJDIR)/libbpf.a", {plain = true})
-            io.replace("Makefile", [[ifndef BUILD_STATIC_ONLY]], [[else]], {plain = true})
             local configs = {}
-            if package:config("shared") then
+            if not package:config("shared") then
                 table.insert(configs, "BUILD_STATIC_ONLY=y")
             end
             import("package.tools.make").install(package, configs)
