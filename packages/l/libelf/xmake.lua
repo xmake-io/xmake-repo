31821494702c48d5003f06e3cedca48eb18d5e27
さくらみこ <lin@sz.cn.eu.org>
2025-05-01 05:13:30 +0300

    test

--- packages/l/libelf/xmake.lua
+++ packages/l/libelf/xmake.lua
@@ -1,17 +1,18 @@
 package("libelf")
-
     set_homepage("https://web.archive.org/web/20181111033959/www.mr511.de/software/english.html")
     set_description("ELF object file access library")
 
     set_urls("https://github.com/xmake-mirror/libelf/releases/download/$(version)/libelf-$(version).tar.gz",
              "https://github.com/xmake-mirror/libelf.git")
+
     add_versions("0.8.13", "591a9b4ec81c1f2042a97aa60564e0cb79d041c52faa7416acb38bc95bd2c76d")
+
+    add_resources("0.8.13", "guess", "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=00b15927496058d23e6258a28d8996f87cf1f191", "e3d148130e9151735f8b9a8e69a70d06890ece51468a9762eb7ac0feddddcc2f")
+    add_resources("0.8.13", "sub", "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=00b15927496058d23e6258a28d8996f87cf1f191", "11c54f55c3ac99e5d2c3dc2bb0bcccbf69f8223cc68f6b2438daa806cf0d16d8")
 
     add_includedirs("include", "include/libelf")
 
-    add_deps("autotools")
-
-    on_install("cross", "linux", "android@linux,macosx", function (package)
+    on_install("cross", "linux", "android", function (package)
         local configs = {"--disable-dependency-tracking",
                          "--disable-compat"}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
@@ -31,18 +32,8 @@ package("libelf")
             package:add("defines", "__libelf_u64_t=uint64_t")
             package:add("defines", "__libelf_i64_t=int64_t")
         end
-        os.rm("configure", "config.guess", "config.sub")
-        local automake = package:dep("automake")
-        if automake and not automake:is_system() then
-            local automake_dir = path.join(automake:installdir(), "share", "automake-*")
-            os.cp(path.join(automake_dir, "config.guess"), ".")
-            os.cp(path.join(automake_dir, "config.sub"), ".")
-        else
-            import("net.http")
-            import("core.base.global")
-            http.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD", path.join(package:buildir(), "config.guess"), {insecure = global.get("insecure-ssl")})
-            http.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD", path.join(package:buildir(), "config.sub"), {insecure = global.get("insecure-ssl")})
-        end
+        os.cp(path.join(package:resourcedir("guess"), "../config.sub"), "config.guess")
+        os.cp(path.join(package:resourcedir("sub"), "../config.sub"), "config.sub")
         import("package.tools.autoconf").install(package, configs, {cxflags = cxflags})
     end)
 
