43597a552376d14fd7308d68096e91e13b044735
さくらみこ <lin@sz.cn.eu.org>
2025-05-07 21:58:35 +0300

    fixup

--- packages/d/diligentcore/xmake.lua
+++ packages/d/diligentcore/xmake.lua
@@ -9,9 +9,9 @@ package("diligentcore")
     add_versions("v2.5.6", "abc190c05ee7e5ef2bba52fcbc5fdfe2256cce3435efba9cfe263a386653f671")
 
     add_patches("v2.5.6", "patches/v2.5.6/debundle-thirdparty.diff", "3d276a78e9ae47516668229dcb644f328a3602d389a3d73784eeb52d8a53108d")
-    add_patches("v2.5.6", "patches/v2.5.6/enforce-static-lib-type-for-platform-libraries.diff", "43acdb7048b92e61969a90f34f83fdc8cd878f1d6d780ef30bb08e993d0b435f")
+    add_patches("v2.5.6", "patches/v2.5.6/enforce-static-lib-type-for-platform-libraries.diff", "ee88a2a04348dcc9de7960a87ff5dc1fb1b534caf1f6224bb2d0c88d37a4bc53")
     add_patches("v2.5.6", "patches/v2.5.6/fix-build-deps-pkgconf.diff", "9998204546cf551a48301e972b582eeeaf002607a4f474086d7d80f4762451ee")
-    add_patches("v2.5.6", "patches/v2.5.6/fix-install-path.diff", "f91db2886c7c8fdde956d57c1fade82ff898f16f3aaf87e59bd8398c7afa25ae")
+    add_patches("v2.5.6", "patches/v2.5.6/fix-install-path.diff", "0fa50da9946dff8e57713edc4a224b8b54c548242e1c556d3dc11354eb69b2c1")
     add_patches("v2.5.6", "patches/v2.5.6/fix-spirv-cross-namespace.diff", "d5a866d4b5bce061a3597dcc026cb88c4b9f92af352ef4071750d355b8b924f0")
 
     add_includedirs("include", "include/DiligentCore")
