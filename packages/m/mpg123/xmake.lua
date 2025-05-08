c04d32bec0232f46046b6dd0e4ed4140ac278c18
さくらみこ <lin@sz.cn.eu.org>
2025-05-08 06:17:50 +0300

    windows@android fallback to MINGW32/64

--- packages/m/mpg123/xmake.lua
+++ packages/m/mpg123/xmake.lua
@@ -7,19 +7,26 @@ package("mpg123")
     add_versions("1.32.10", "87b2c17fe0c979d3ef38eeceff6362b35b28ac8589fbf1854b5be75c9ab6557c")
     add_versions("1.30.2", "c7ea863756bb79daed7cba2942ad3b267a410f26d2dfbd9aaf84451ff28a05d7")
 
-    add_deps("autoconf", "automake", "libtool")
+    if not is_subhost("windows") then
+        add_deps("autoconf", "automake", "libtool")
+    end
+    
     if is_plat("linux") then
         add_syslinks("m")
     end
+
+    on_load(function(package)
+        if is_subhost("windows") then
+            local msystem = "MINGW" .. (package:is_arch64() and "64" or "32")
+            package:add("deps", "msys2", {configs = {msystem = msystem, base_devel = true, gcc = true, make = true}})
+        end
+    end)
 
     on_install("linux", "macosx", "android", "iphoneos", "bsd", "cross", function (package)
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         if package:debug() then
             table.insert(configs, "--enable-debug")
-        end
-        if package:is_plat("linux") and package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
         end
         import("package.tools.autoconf").install(package, configs)
     end)
