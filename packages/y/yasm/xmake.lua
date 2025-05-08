e4a1a9553549d665d888a7d5acd5253a6b175d22
さくらみこ <lin@sz.cn.eu.org>
2025-05-08 11:11:18 +0300

    try fix version for cmake

--- packages/y/yasm/xmake.lua
+++ packages/y/yasm/xmake.lua
@@ -8,12 +8,13 @@ package("yasm")
              "https://ftp.openbsd.org/pub/OpenBSD/distfiles/yasm-$(version).tar.gz")
     add_versions("1.3.0", "3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f")
     add_patches("1.3.0", path.join(os.scriptdir(), "patches", "fix-bool-gcc15.diff"), "bc58964451aec9495e1bd4ebb8f6a035dc7b01be6a041cba3fcf90decba7d6e1")
+    add_patches("1.3.0", path.join(os.scriptdir(), "patches", "upd-min-cmake.diff"), "5c234e51f03502970104027f1580b733d89109144b85ee0b3b5366febab28623")
 
     add_deps("cmake")
     add_deps("python 3.x", {kind = "binary"})
 
     on_install("@windows", "@linux", "@macosx", "@mingw", "@msys", function (package)
-        local configs = {}
+        local configs = {"-DYASM_BUILD_TESTS=OFF", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("mingw") then
