dd62d15d9fb18c9423a9c5bd5e56ff3c99f0c389
さくらみこ <lin@sz.cn.eu.org>
2025-05-09 06:00:49 +0300

    fixup

--- packages/y/yasm/xmake.lua
+++ packages/y/yasm/xmake.lua
@@ -10,22 +10,21 @@ package("yasm")
     add_patches("1.3.0", path.join(os.scriptdir(), "patches", "fix-bool-gcc15.diff"), "bc58964451aec9495e1bd4ebb8f6a035dc7b01be6a041cba3fcf90decba7d6e1")
     add_patches("1.3.0", path.join(os.scriptdir(), "patches", "upd-min-cmake.diff"), "5c234e51f03502970104027f1580b733d89109144b85ee0b3b5366febab28623")
 
-    add_deps("cmake")
-    add_deps("python 3.x", {kind = "binary"})
+    add_deps("cmake", "python 3.x", {kind = "binary"})
+
+    on_install("mingw", "msys", function (package)
+        local configs = {"--disable-python"}
+        if package:debug() then
+            table.insert(configs, "--enable-debug")
+        end
+        import("package.tools.autoconf").install(package)
+    end)
 
     on_install("@windows", "@linux", "@macosx", function (package)
         local configs = {"-DYASM_BUILD_TESTS=OFF", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
-    end)
-
-    on_install("@mingw", "@msys", function (package)
-        local configs = {"--disable-python"}
-        if package:debug() then
-            table.insert(configs, "--enable-debug")
-        end
-        import("package.tools.autoconf").install(package)
     end)
 
     on_test(function (package)
