fb47a580e183ad4d8fcde13046288f6e661d67a6
Jérôme Leclercq <lynix680@gmail.com>
2025-05-10 10:16:40 +0200

    Update xmake.lua

--- packages/r/rust/xmake.lua
+++ packages/r/rust/xmake.lua
@@ -12,9 +12,6 @@ package("rust")
     add_configs("target_plat", {description = "Target platform (for cross-compilation)", default = nil, type = "string"})
     add_configs("target_arch", {description = "Target arch (for cross-compilation)", default = nil, type = "string"})
 
-    on_check("mingw", function (package)
-    end)
-
     on_load(function (package)
         if package:config("target_plat") == nil then
             package:config_set("target_plat", package:plat())
@@ -24,29 +21,66 @@ package("rust")
         end
     end)
 
-    on_install(function (package)
-        import("private.tools.rust.target_triple")
+    on_check("mingw|i386", function (package)
+        -- MinGW 32bits exception model must match rustc LLVM exception model (dwarf2)
+        local mingw = package:toolchain("mingw")
+        if not mingw then
+            return
+        end
 
-        local rustup = assert(os.getenv("RUSTUP_HOME"), "cannot find rustup home!")
-        local version = package:version():shortstr()
+        local compiler, toolname = mingw:tool("cc")
+        if toolname ~= "gcc" then
+            return
+        end
+
+        local output, errdata = os.iorunv(compiler, {"-v"})
+        -- for some reason the output is in stderr
+        if #output:trim() == 0 then
+            output = errdata
+        end
+        assert(output:find("--with-dwarf2", 1, true), "rustc is only compatible with dwarf2 exception model in 32bits mode, please use dwarf2 MinGW")
+    end)
+
+    on_install(function (package)
+        import("core.tools.rustc.target_triple", {try = true}) -- introduced in xmake 3.0.0
 
         local plat = package:config("target_plat")
         local arch = package:config("target_arch")
 
-        local host_target = assert(target_triple(package:plat(), package:arch()), "failed to build target triple for plat %s and arch %s, if you think this is a bug please create an issue", package:plat(), package:arch())
-        local toolchain_name = version .. "-" .. host_target
-        os.vrunv("rustup", {"install", "--no-self-update", toolchain_name})
-
-        local target = assert(target_triple(plat, arch), "failed to build target triple for target_plat %s and target_arch %s, if you think this is a bug please create an issue", plat, arch)
-        if target ~= host_target then
-            os.vrunv("rustup", {"target", "add", target})
+        if not target_triple and (plat ~= package:plat() or arch ~= package:arch()) then
+            os.raise("rust cross-compilation requires xmake dev or xmake 3.0.0")
         end
 
-        os.vmv(path.join(rustup, "toolchains", toolchain_name, "*"), package:installdir())
+        local rustup = assert(os.getenv("RUSTUP_HOME"), "cannot find rustup home!")
+        local version = package:version():shortstr()
+
+        local toolchain_name = version
+        local toolchain_dir
+
+        if target_triple then
+            local host_target = assert(target_triple(package:plat(), package:arch()), "failed to build target triple for plat/arch, if you think this is a bug please create an issue")
+            toolchain_name = toolchain_name .. "-" .. host_target
+            toolchain_dir = toolchain_name
+        elseif package:is_plat("msys", "mingw") and is_host("windows") then
+            toolchain_name = toolchain_name .. (package:is_arch("i386") and "-x86_64-pc-windows-gnu" or "-i686-pc-windows-gnu")
+            toolchain_dir = toolchain_name
+        else
+            toolchain_dir = toolchain_name .. "-*"
+        end
+        os.vrunv("rustup", {"install", "--no-self-update", toolchain_name})
+
+        if target_triple then
+            local target = assert(target_triple(plat, arch), "failed to build target triple for target_plat/target_arch, if you think this is a bug please create an issue")
+            if target ~= host_target then
+                os.vrunv("rustup", {"target", "add", target})
+            end
+        end
+
+        os.vmv(path.join(rustup, "toolchains", toolchain_dir, "*"), package:installdir())
 
         -- cleanup to prevent rustup to think the toolchain is still installed
-        os.vrm(path.join(rustup, "toolchains", toolchain_name))
-        os.vrm(path.join(rustup, "update-hashes", toolchain_name))
+        os.vrm(path.join(rustup, "toolchains", toolchain_dir))
+        os.vrm(path.join(rustup, "update-hashes", toolchain_dir))
 
         package:addenv("RC", "bin/rustc" .. (is_host("windows") and ".exe" or ""))
         package:mark_as_pathenv("RC")
