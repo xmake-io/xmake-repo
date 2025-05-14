47f8aebf1f017dc84c0372f4d5e2713128511e20
ruki <waruqi@gmail.com>
2025-05-14 01:23:35 +0000

    Update libcpuid to v0.8.0

--- packages/l/libcpuid/xmake.lua
+++ packages/l/libcpuid/xmake.lua
@@ -4,6 +4,7 @@ package("libcpuid")
 
     add_urls("https://github.com/anrieff/libcpuid/archive/refs/tags/$(version).tar.gz",
              "https://github.com/anrieff/libcpuid.git")
+    add_versions("v0.8.0", "a5fe37d79bda121cbdf385ae3f6fa621da6a3102aa609400a718a4b8b82ed8aa")
     add_versions("v0.7.1", "c54879ea33b68a2e752c20fb0e3cd04439a9177eab23371f709f15a45df43644")
     add_versions("v0.7.0", "cfd9e6bcda5da3f602273e55f983bdd747cb93dde0b9ec06560e074939314210")
     add_versions("v0.6.5", "4d106d66d211f2bfaf876eb62c84d4b54664e1c2b47eb6138161d3c608c0bc5e")
