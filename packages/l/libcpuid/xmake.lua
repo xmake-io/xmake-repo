451fd90345125b77b5d4292be1c666cd1e10f643
star9029 <hengxings783@gmail.com>
2025-05-15 10:32:09 +0800

    Update xmake.lua

--- packages/l/libcpuid/xmake.lua
+++ packages/l/libcpuid/xmake.lua
@@ -14,11 +14,11 @@ package("libcpuid")
     add_versions("v0.6.3", "da570fdeb450634d84208f203487b2e00633eac505feda5845f6921e811644fc")
     add_versions("v0.5.1", "36d62842ef43c749c0ba82237b10ede05b298d79a0e39ef5fd1115ba1ff8e126")
 
-    add_configs("drivers", {description = "Enable building kernel drivers", default = false})
+    add_configs("drivers", {description = "Enable building kernel drivers", default = false, type = "boolean"})
 
     add_deps("cmake")
 
-    on_install("windows", "macosx", "linux", "bsd", "mingw", "msys", function (package)
+    on_install("windows|!arm64", "macosx", "linux", "bsd", "mingw", "msys", function (package)
         io.replace("CMakeLists.txt", "add_subdirectory(tests)", "", {plain = true})
 
         local configs = {}
