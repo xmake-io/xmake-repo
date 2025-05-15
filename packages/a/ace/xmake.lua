a1186fa60ab0617450395098c414edc354b65329
さくらみこ <lin@sz.cn.eu.org>
2025-05-15 18:32:03 +0300

    fixup

--- packages/a/ace/xmake.lua
+++ packages/a/ace/xmake.lua
@@ -29,7 +29,7 @@ package("ace")
         end
         if not package:config("shared") then
             io.replace("ACE_vs2022.vcxproj", "DynamicLibrary", "StaticLibrary", {plain = true})
-            io.replace("ACE_vs2022.vcxproj", "ACE_COMPRESSION_BUILD_DLL", "ACE_AS_STATIC_LIBS", {plain = true})
+            io.replace("ACE_vs2022.vcxproj", "ACE_BUILD_DLL", "ACE_AS_STATIC_LIBS", {plain = true})
         end
         -- Allow use another Win SDK
         io.replace("ACE_vs2022.vcxproj", "<WindowsTargetPlatformVersion>10.0</WindowsTargetPlatformVersion>", "", {plain = true})
