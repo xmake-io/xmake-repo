34f23e9aeec1b61be7fc72c4c434f03e57c19144
Saikari <lin@sz.cn.eu.org>
2025-05-15 05:33:39 +0300

    Follow fix

--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -5,13 +5,13 @@ package("mbedtls")
 
     add_urls("https://github.com/Mbed-TLS/mbedtls/releases/download/$(version).tar.bz2", {version = function (version)
         if version:lt("3.6.1") then
-            return string.format("%s/mbedtls-%s", version, tostring(version):sub(2))
+            return string.format("%s/mbedtls-%s", version, version:sub(2))
         else
-            return string.format("mbedtls-%s/mbedtls-%s", tostring(version):sub(2), tostring(version):sub(2))
+            return string.format("mbedtls-%s/mbedtls-%s", version:sub(2), version:sub(2))
         end
     end})
     add_urls("https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/$(version).zip", {version = function (version)
-        return version:ge("v2.23.0") and version or ("mbedtls-" .. tostring(version):sub(2))
+        return version:ge("v2.23.0") and version or ("mbedtls-" .. version:sub(2))
     end})
     add_urls("https://github.com/Mbed-TLS/mbedtls.git")
 
