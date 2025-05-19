08d503209f239d2eda58dab9ee74d92bd835a11c
さくらみこ <lin@sz.cn.eu.org>
2025-05-19 17:21:26 +0300

    omniorb: add package

--- packages/o/omniorb/xmake.lua
+++ packages/o/omniorb/xmake.lua
@@ -1,26 +1,38 @@
 package("omniorb")
     set_kind("binary")
-    set_homepage("https://sourceforge.net/projects/omniorb/")
+    set_homepage("https://omniorb.sourceforge.io/omni41/omniidl.html")
     set_description("omniORB is a CORBA object request broker for C++ and Python. It is very fast, robust, and standards-compliant.")
     set_license("GPL-2.0")
+
     add_urls("https://sourceforge.net/projects/omniorb/files/omniORB/omniORB-$(version)/omniORB-$(version).tar.bz2")
+
     add_versions("4.3.3", "accd25e2cb70c4e33ed227b0d93e9669e38c46019637887c771398870ed45e7a")
+
     add_deps("python 3.x", {kind = "binary"})
 
     on_load("windows", function(package)
-        -- if is_subhost("windows") then
+        package:addenv("PATH", "bin")
         local msystem = "MINGW" .. (package:is_arch64() and "64" or "32")
         package:add("deps", "msys2", {configs = {msystem = msystem, base_devel = true, gcc = true, make = true}})
-        -- end
     end)
 
     on_install("windows", function (package)
         import("package.tools.make")
+        local python = package:dep("python")
+        local python_dir = python:installdir()
+        os.setenv("PYTHONHOME", python_dir)
+        os.setenv("PYTHONPATH", path.join(python_dir, "Lib") .. ";" .. path.join(python_dir, "Lib", "site-packages"))
 
-        -- os.mkdir("bin/x86_win32")
-        -- os.mkdir("lib")
-        -- os.mkdir("lib/x86_win32")
+        package:addenv("PYTHONHOME", python_dir)
+        package:addenv("PYTHONPATH", path.join(python_dir, "Lib") .. ";" .. path.join(python_dir, "Lib", "site-packages"))
 
+        -- Fix windows manifest flag
+        io.replace("mk/win32.mk", "/outputresource:", "-outputresource:", {plain = true})
+        io.replace("mk/win32.mk", "/manifest", "-manifest", {plain = true})
+        -- Do not build libs & applications & services, build only tool
+        io.replace("src/dir.mk", "SUBDIRS += lib", "", {plain = true})
+        io.replace("src/dir.mk", "SUBDIRS += appl services", "", {plain = true})
+        -- Specify vs toolset
         local msvc = import("core.tool.toolchain").load("msvc")
         local vs = msvc:config("vs")
         local platform
@@ -29,57 +41,30 @@ package("omniorb")
         elseif vs == "2019" then platform = "x86_win32_vs_16"
         elseif vs == "2022" then platform = "x86_win32_vs_17"
         end
-        -- Specify vs toolset
         io.replace("config/config.mk", "#platform = " .. platform, "platform = " .. platform, {plain = true})
+        -- Specify python.exe file path
         local python = path.cygwin_path(package:dep("python"):installdir("bin") .. "/python")
-        print("python => " .. python)
-        -- Specify python 3 bin directory
         io.replace("mk/platforms/" .. platform .. ".mk", "#PYTHON = /cygdrive/c/Python27/python", "PYTHON = " .. python, {plain = true})
-
-        local msys2 = package:dep("msys2")
-        local msys2_bin = msys2:installdir("usr/bin")
-        print("msys2_bin => " .. msys2_bin)
-
-        
-        -- os.setenv("PATH", msys2_bin .. ";" .. os.getenv("PATH"))
-
-        local msvc = import("core.tool.toolchain").load("msvc")
-
+        -- Wrap missing env variables in order MSVC (cl/link/rc) -> MinGW (make/sh/mkdir/basename/install)
         local envs = make.buildenvs(package)
         envs.PATH = os.getenv("PATH")
+        local msvc = import("core.tool.toolchain").load("msvc")
         envs = os.joinenvs(msvc:runenvs(), envs)
-        -- envs.PATH = msvc:tool("ld") .. ";" .. envs.PATH
-        
-        -- envs.link = [["]] .. path.unix(msvc:tool("ld")) .. [["]]
-        -- print("MSVC LD => " .. msvc:tool("ld"))
-
-
-        -- print(envs)
-        -- envs.PATH = os.getenv("PATH")
-        -- local envs = msvc:runenvs()
-        -- envs.PATH = os.getenv("PATH")
-
-        -- envs.PATH = msys2_bin .. ";" .. envs.PATH
-
-
-        -- os.setenvs(envs)
-
-        -- local cl_path = [["]] .. path.cygwin_path(msvc:tool("cc")) .. [["]]
-        -- print(cl_path)
-        -- io.replace("src/tool/win32/dir.mk", [[CL.EXE $< advapi32.lib]], cl_path .. [[ $< advapi32.lib]], {plain = true})
-
-
+        -- Build tool
         os.cd("src")
-        -- make export
         make.make(package, { "export" }, {envs = envs})
         os.cd("..")
+        -- Install tool
         os.cp("**.lib", package:installdir("lib"))
         if package:config("shared") then
             os.cp("**.dll", package:installdir("bin"))
         end
         os.cp("**.exe", package:installdir("bin"))
+        -- omniidl: Python files for IDL compiler omniidl & omniidl_be python modules
+        os.cp("lib/python/omniidl_be", path.join(python_dir, "Lib", "site-packages"))
+        os.cp("lib/python/omniidl", path.join(python_dir, "Lib", "site-packages"))
     end)
 
-    -- on_test(function (package)
-    
-    -- end)
+    on_test(function (package)
+        os.vrun("omniidl -u")
+    end)
