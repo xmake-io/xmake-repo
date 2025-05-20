5e705dd470ce7c14c3bcc30eea25d91d6ca1c0bb
Saikari <lin@sz.cn.eu.org>
2025-05-20 14:04:15 +0300

    Fix semver & NDK

--- packages/u/usrsctp/xmake.lua
+++ packages/u/usrsctp/xmake.lua
@@ -2,10 +2,12 @@ package("usrsctp")
     set_homepage("https://github.com/sctplab/usrsctp")
     set_description("A portable SCTP userland stack")
 
-    add_urls("https://github.com/sctplab/usrsctp/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/sctplab/usrsctp.git")
+    add_urls("https://github.com/sctplab/usrsctp/archive/refs/tags/$(version).tar.gz", {version = function (version)
+        return version:gsub("%+", ".")
+    end})
+    add_urls("https://github.com/sctplab/usrsctp.git")
 
-    add_versions("0.9.5.0", "260107caf318650a57a8caa593550e39bca6943e93f970c80d6c17e59d62cd92")
+    add_versions("0.9.5+0", "260107caf318650a57a8caa593550e39bca6943e93f970c80d6c17e59d62cd92")
     
     add_configs("invariants", {description = "Add runtime checks", default = false, type = "boolean"})
     add_configs("inet", {description = "Support IPv4", default = true, type = "boolean"})
@@ -23,10 +25,17 @@ package("usrsctp")
         add_syslinks("pthread")
     end
 
-    on_install("windows", "linux", "macosx", "android@linux,macosx", "cross", "bsd", "mingw", function (package)
-        io.replace("CMakeLists.txt", "cmake_minimum_required(VERSION 3.0)", "cmake_minimum_required(VERSION 3.5)", {plain = true})
+   if on_check then
+        on_check("android", function (package)
+            local ndk = package:toolchain("ndk")
+            local ndk_sdkver = ndk:config("ndk_sdkver")
+            assert(ndk_sdkver and tonumber(ndk_sdkver) > 23, "package(usrsctp): need ndk api level > 23")
+        end)
+    end
+
+    on_install("windows", "linux", "macosx", "android", "cross", "bsd", "mingw", function (package)
         local configs ={"-Dsctp_build_programs=0"}
-        if package:debug() then
+        if package:is_debug() then
             table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
             table.insert(configs, "-Dsctp_debug=1")
         else
@@ -49,4 +58,3 @@ package("usrsctp")
     on_test(function (package)
         assert(package:has_cfuncs("usrsctp_init", {includes = "usrsctp.h"}))
     end)
-
