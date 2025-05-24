9fe6d8507e0d76883cc632862aa53a5d1da83054
star9029 <hengxings783@gmail.com>
2025-05-24 17:32:56 +0800

    use cmake

--- packages/c/cpp-jwt/xmake.lua
+++ packages/c/cpp-jwt/xmake.lua
@@ -7,12 +7,18 @@ package("cpp-jwt")
     add_urls("https://github.com/arun11299/cpp-jwt/archive/refs/tags/$(version).tar.gz",
              "https://github.com/arun11299/cpp-jwt.git")
 
+    add_versions("v1.5", "44a59d619b0a82cae6334bb7d430d27b7fc7595e872c9f20d46aa96d2301edb2")
     add_versions("v1.4", "1cb8039ee15bf9bf735c26082d7ff50c23d2886d65015dd6b0668c65e17dd20f")
 
-    add_deps("nlohmann_json", "openssl3")
+    add_deps("cmake")
+    add_deps("openssl3")
+    add_deps("nlohmann_json", {configs = {cmake = true}})
 
     on_install(function (package)
-        os.cp("include/jwt", package:installdir("include"))
+        local configs = {"-DCPP_JWT_BUILD_EXAMPLES=OFF", "-DCPP_JWT_BUILD_TESTS=OFF", "-DCPP_JWT_USE_VENDORED_NLOHMANN_JSON=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
