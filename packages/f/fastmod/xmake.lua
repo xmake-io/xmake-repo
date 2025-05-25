d1edcc1680c814a7f872abc933ae5527e582cac4
jj683 <jj683@protonmail.com>
2025-05-25 23:10:28 +0200

    Test unsupported 32 bit check

--- packages/f/fastmod/xmake.lua
+++ packages/f/fastmod/xmake.lua
@@ -11,14 +11,17 @@ package("fastmod")
     add_versions("v0.1.0", "b80e759208d6b4318caa14b1851352c69e8c7a1f7895639e3af1d86944d2973b")
 
     add_deps("cmake")
+
+    on_check(function (package)
+        if package:check_sizeof("void*") == "4" then
+            raise("package(fastmod) unsupported 32-bit")
+        end
+    end)
+
     on_install(function (package)
         import("package.tools.cmake").install(package)
     end)
 
     on_test(function (package)
-        if package:check_sizeof("void*") == "4" then
-            raise("package(fastmod) unsupported 32-bit")
-        end
-
         assert(package:has_cfuncs("fastmod_u32", {includes = "fastmod.h"}))
     end)
