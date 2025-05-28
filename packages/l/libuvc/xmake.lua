cd3a14bb87be2f83170f8d0676eb55d532e8e8c6
Saikari <lin@sz.cn.eu.org>
2025-05-28 12:58:29 +0300

    Update xmake.lua

--- packages/l/libuvc/xmake.lua
+++ packages/l/libuvc/xmake.lua
@@ -1,10 +1,10 @@
 package("libuvc")
     set_homepage("https://github.com/libuvc/libuvc")
-    set_description("A cross-platform  video device oynchronous I/O.")
+    set_description("A cross-platform video device oynchronous I/O.")
     set_license("BSD-3-Clause")
 
     add_urls("https://github.com/libuvc/libuvc.git")
-    add_versions("v0.0.7", "047920bcdfb1dac42424c90de5cc77dfc9fba04d")
+    add_versions("2024.03.05", "047920bcdfb1dac42424c90de5cc77dfc9fba04d")
 
     add_configs("jpeg", {description = "Enable jpeg support.", default = true, type = "boolean"})
 
@@ -15,18 +15,17 @@ package("libuvc")
         add_deps("pkgconf", "pthreads4w")
     end
 
-    on_load("windows|x86", "windows|x64", function (package)
+    on_load(function (package)
         if package:config("jpeg") then
             package:add("deps", "libjpeg")
         end
     end)
 
-    on_install("windows|x86", "windows|x64", function (package)
+    on_install("windows", function (package)
         local configs = {}
         table.insert(configs, "-DBUILD_EXAMPLE=OFF")
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DCMAKE_BUILD_TARGET=" .. (package:config("shared") and "Shared" or "Static"))
-
         import("package.tools.cmake").install(package, configs)
     end)
 
