3cba040fbb313528840302b4fdef58b0cdb57d04
さくらみこ <lin@sz.cn.eu.org>
2025-05-29 16:09:38 +0300

    re

--- packages/u/uasm/xmake.lua
+++ packages/u/uasm/xmake.lua
@@ -23,7 +23,7 @@ package("uasm")
     end)
 
     on_install("msys", "mingw@macosx,linux,windows,msys", function (package)
-        local configs = { "-f", "Makefile-DOS-GCC.mak", "CC=cc -c -IH -std=gnu11 -funsigned-char -fcommon -Wno-implicit-function-declaration -Wno-incompatible-pointer-types"}
+        local configs = { "-f", "Makefile-DOS-GCC.mak", "CC=gcc -c -IH -std=gnu11 -funsigned-char -fcommon -Wno-implicit-function-declaration -Wno-incompatible-pointer-types"}
         import("package.tools.make").install(package, configs)
         os.cp("*/hjwasm.exe", path.join(package:installdir("bin"), "uasm.exe"))
     end)
