fa8939ec0c378fb18aee048dd02921ff362185e8
さくらみこ <lin@sz.cn.eu.org>
2025-05-29 12:41:41 +0300

    check mingw

--- packages/u/uasm/xmake.lua
+++ packages/u/uasm/xmake.lua
@@ -8,6 +8,19 @@ package("uasm")
 
     add_versions("v2.57r", "09fa69445f2af47551e82819d024e6b4b629fcfd47af4a22ccffbf37714230e5")
 
+    if not is_plat("windows") then
+        add_patches("v2.57r", "patches/fix-bool.diff", "91a6b4634fab3fb3991a372c845247bd895ea3d2de104d3687eb6e2c61e44e39")
+        add_patches("v2.57r", "patches/fix-build.diff", "840a36bf7200941cd98331245a5b7b71f9ee56a912b4c10c255498c6f7c89d45")
+        add_patches("v2.57r", "patches/fix-esc-seq.diff", "4011194c59f87b5be798b4e85dca869e3a3f7ddf2b0fc4f8b3b26190df1abca3")
+        add_patches("v2.57r", "patches/uint64-fix.diff", "ea6bab7192894fc673b55b9ead67cf1eb0046cd34c425f33781e167097988662")
+    end
+
+    on_install("msys", "mingw", function (package)
+        local configs = { "-f", "Makefile-DOS-GCC.mak", "CC=cc.exe -c -IH -std=gnu11 -funsigned-char -fcommon -Wno-implicit-function-declaration -Wno-incompatible-pointer-types"}
+        import("package.tools.make").install(package, configs)
+        os.cp("DJGPPr/hjwasm.exe", path.join(package:installdir("bin"), "uasm.exe"))
+    end)
+
     on_install("windows", function (package)
         import("package.tools.msbuild")
         local configs = {}
