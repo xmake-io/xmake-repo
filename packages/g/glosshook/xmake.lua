30818eea47d0c5d4cb8a8c823342b45185dc918c
さくらみこ <lin@sz.cn.eu.org>
2025-05-31 01:59:28 +0300

    `.so` is supplied only in .zip release

--- packages/g/glosshook/xmake.lua
+++ packages/g/glosshook/xmake.lua
@@ -3,29 +3,29 @@ package("glosshook")
     set_description("A simple android NativeHook library.")
     set_license("MIT")
 
-    add_urls("https://github.com/XMDS/GlossHook/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/XMDS/GlossHook.git")
+    add_urls("https://github.com/XMDS/GlossHook/releases/download/$(version)", {version = function (version)
+        return version .. "/GlossHook." .. version:major() .. "." .. version:minor() .. "." .. version:patch() .. ".zip"
+    end})
 
-    add_versions("v1.9.5", "a80995c625a99bf5cd67539838d1fd487366c89e8a61c5838165da11c46de8f1")
+    add_versions("v1.9.5", "3e964dc5d3dc8f49647a3fc2415ac8d0978fde508579db573df9d4b0a4e923bf")
 
     add_deps("xdl")
 
     on_install("android", function (package)
-        os.cp("GlossHook/include", package:installdir())
         if package:config("shared") then
             if package:check_sizeof("void*") == "4" then
-                os.cp("GlossHook/lib/ARM/libGlossHook.so", package:installdir("lib"))
+                os.cp("lib/ARM/libGlossHook.so", package:installdir("lib"))
             else
-                os.cp("GlossHook/lib/ARM64/libGlossHook.so", package:installdir("lib"))
+                os.cp("lib/ARM64/libGlossHook.so", package:installdir("lib"))
             end
         else
             if package:check_sizeof("void*") == "4" then
-                os.cp("GlossHook/lib/ARM/libGlossHook.a", package:installdir("lib"))
+                os.cp("lib/ARM/libGlossHook.a", package:installdir("lib"))
             else
-                os.cp("GlossHook/lib/ARM64/libGlossHook.a", package:installdir("lib"))
+                os.cp("lib/ARM64/libGlossHook.a", package:installdir("lib"))
             end
-        end        
-        os.cp("GlossHook/include", package:installdir())
+        end
+        os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
