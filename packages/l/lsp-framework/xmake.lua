1bb4776c20208cd0030173eecdfadf0d4b3163b7
さくらみこ <lin@sz.cn.eu.org>
2025-05-31 00:52:42 +0300

    fixup

--- packages/l/lsp-framework/xmake.lua
+++ packages/l/lsp-framework/xmake.lua
@@ -9,14 +9,26 @@ package("lsp-framework")
     add_versions("1.0.1", "07f924d851896a2d424d554d20820483f8458aa1ff907bb68657b0d2d0bd0d13")
 
     add_patches("1.0.1", "patches/1.0.1/fix-install.diff", "bb5e4436091ba1846144ffa80fb8afd4d0213760bce45dd6fd31662905cb4bc3")
+    add_patches("1.0.1", "patches/1.0.1/fix-missing-ios.diff", "8447605c2ed14cfbf394b29ffe2348e2179424504100aa01cd88b3d054e5ceb1")
 
     add_deps("cmake")
 
-    if is_plat("windows", "mingw") then
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    elseif is_plat("windows", "mingw") then
         add_syslinks("ws2_32")
     end
 
-    on_install(function (package)
+    if on_check then
+        on_check("windows|arm64", function (package)
+            import("core.base.semver")
+            local vs = package:toolchain("msvc"):config("vs")
+            assert(tonumber(vs) >= 2022, "lsp-framework requires Visual Studio 2022 and later for arm64 targets")
+            assert(os.arch() == "arm64", "package(lsp-framework): requires host arch to be arm64.")
+        end)
+    end
+
+    on_install("windows", "linux", "macosx", "mingw@windows", "bsd", function (package)
         local configs = {}
         table.insert(configs, "-DLSP_USE_SANITIZERS=" .. (package:config("asan") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
