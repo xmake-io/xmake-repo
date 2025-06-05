3ab6db11ce3dd7d205b939575103922ed2fe02ea
apocelipes <seve3r@outlook.com>
2025-06-05 11:02:34 +0800

    fix deps

--- packages/x/xmlto/xmake.lua
+++ packages/x/xmlto/xmake.lua
@@ -11,6 +11,10 @@ package("xmlto")
     add_versions("0.0.29", "6000d8e8f0f9040426c4f85d7ad86789bc88d4aeaef585c4d4110adb0b214f21")
 
     add_deps("util-linux")
+    if not is_plat("windows") then
+        add_deps("autoconf", "automake", "libtool")
+        add_deps("libxslt", {kind = "binary"})
+    end
 
     on_install("macosx", "linux", function (package)
         import("package.tools.autoconf")
