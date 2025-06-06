68debc918c51a9e3b5f4735947572ad761fa8f6a
Maxime Trimolet <maxime.trimolet@gmail.com>
2025-06-06 17:28:26 +0200

    ftxui: Add modules support (#7301)

--- packages/f/ftxui/xmake.lua
+++ packages/f/ftxui/xmake.lua
@@ -14,19 +14,50 @@ package("ftxui")
 
     add_deps("cmake")
 
+    add_configs("modules", { default = false, type = "boolean" })
+    add_configs("microsoft_fallback_terminal", { default = true, description = "On windows, assume the \
+terminal used will be one of Microsoft and use a set of reasonnable fallback \
+to counteract its implementations problems.", type = "boolean" })
+
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     elseif is_plat("linux", "bsd") then
         add_syslinks("pthread")
     end
 
-    add_links("ftxui-component", "ftxui-dom", "ftxui-screen")
+    add_components("screen", "dom", "component")
+
+    on_load(function(package)
+        if package:config("modules") then
+            assert(package:gitref() or (package:version() and package:version():gt("1.0")), "modules support is not compatible with ftxui <= 1.0")
+        end
+    end)
+
+    on_component("screen", function(_, component)
+        component:add("links","ftxui-screen")
+    end)
+
+    on_component("dom", function(_, component)
+        component:add("links", "ftxui-dom")
+        component:add("deps", "screen")
+    end)
+
+    on_component("component", function(_, component)
+        component:add("links", "ftxui-component")
+        component:add("deps", "dom")
+    end)
 
     on_install("linux", "windows", "macosx", "bsd", "mingw", "cross", function (package)
-        local configs = {"-DFTXUI_BUILD_DOCS=OFF", "-DFTXUI_BUILD_EXAMPLES=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
+        if package:config("modules") then
+            os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), ".")
+            import("package.tools.xmake").install(package, {modules = package:config("modules"), microsoft_fallback_terminal = package:config("microsoft_fallback_terminal")})
+        else
+            local configs = {"-DFTXUI_BUILD_DOCS=OFF", "-DFTXUI_BUILD_EXAMPLES=OFF"}
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+            table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+            table.insert(configs, "-DFTXUI_MICROSOFT_TERMINAL_FALLBACK=" .. (package:config("microsoft_fallback_terminal") and "ON" or "OFF"))
+            import("package.tools.cmake").install(package, configs)
+        end
     end)
 
     on_test(function (package)
