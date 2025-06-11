33cce50bf892f23b8a9aa5be1e31675b527b0cec
さくらみこ <lin@sz.cn.eu.org>
2025-06-11 05:55:15 +0300

    glib: update to 2.85.0

--- packages/g/glib/xmake.lua
+++ packages/g/glib/xmake.lua
@@ -1,5 +1,4 @@
 package("glib")
-
     set_homepage("https://docs.gtk.org/glib/")
     set_description("Low-level core library that forms the basis for projects such as GTK+ and GNOME.")
     set_license("LGPL-2.1")
@@ -11,6 +10,7 @@ package("glib")
     add_urls("https://gitlab.gnome.org/GNOME/glib.git")
     add_versions("home:2.71.0", "926816526f6e4bba9af726970ff87be7dac0b70d5805050c6207b7bb17ea4fca")
     add_versions("home:2.78.1", "915bc3d0f8507d650ead3832e2f8fb670fce59aac4d7754a7dab6f1e6fed78b2")
+    add_versions("home:2.85.0", "c7e1f7d0562c98edb501b7d586ee7f901558ccd0e641f3390c126a207abadd86")
     add_patches("2.71.0", path.join(os.scriptdir(), "patches", "2.71.0", "macosx.patch"), "a0c928643e40f3a3dfdce52950486c7f5e6f6e9cfbd76b20c7c5b43de51d6399")
 
     if is_plat("mingw") and is_subhost("msys") then
@@ -78,7 +78,7 @@ package("glib")
         end
     end)
 
-    on_install("windows", "macosx", "linux", "cross", function (package)
+    on_install("windows", "macosx", "linux", "cross", "mingw", function (package)
         local configs = {"-Dbsymbolic_functions=false",
                          "-Ddtrace=false",
                          "-Dman=false",
@@ -87,7 +87,8 @@ package("glib")
                          "-Dinstalled_tests=false",
                          "-Dsystemtap=false",
                          "-Dselinux=disabled",
-                         "-Dlibmount=disabled"}
+                         "-Dlibmount=disabled",
+                         "-Dsysprof=disabled"}
         if package:is_plat("macosx") and package:version():le("2.61.0") then
             table.insert(configs, "-Diconv=native")
         elseif package:is_plat("windows") and package:version():le("2.74.0") then
