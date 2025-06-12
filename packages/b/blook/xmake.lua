c404071faa47a23ae8e698b4acb961fe3a2618a4
Saikari <lin@sz.cn.eu.org>
2025-06-12 09:39:54 +0300

    Update xmake.lua

--- packages/b/blook/xmake.lua
+++ packages/b/blook/xmake.lua
@@ -4,7 +4,7 @@ package("blook")
 
     add_urls("https://github.com/std-microblock/blook.git")
 
-    add_versions("2025", "997b6a288fb2e8cd9b9e2dec0c15c050a52139d3")
+    add_versions("2025.04.04", "997b6a288fb2e8cd9b9e2dec0c15c050a52139d3")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
@@ -14,6 +14,6 @@ package("blook")
 
     add_deps("zasm 916f28f882801c048eaececc2466c8fdc17653fa")
 
-    on_install("windows", function (package)
+    on_install("windows|!arm*", function (package)
         import("package.tools.xmake").install(package, {}, {target = "blook"})
     end)
