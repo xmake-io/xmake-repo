ed2ae5d5f38aed3534caea112195d40443d4833d
Saikari <lin@sz.cn.eu.org>
2025-06-12 04:15:30 +0300

    expresscpp: fix build (#7345)
    * expresscpp: fix build
    
    * fix MinGW & limit wasm/windows
    
    * re-check MinGW
    
    * re

--- packages/e/expresscpp/xmake.lua
+++ packages/e/expresscpp/xmake.lua
@@ -1,5 +1,4 @@
 package("expresscpp")
-
     set_homepage("https://github.com/expresscpp/expresscpp.git")
     set_description("Fast, unopinionated, minimalist web framework for C++ Perfect for building REST APIs.")
     set_license("MIT")
@@ -9,18 +8,22 @@ package("expresscpp")
 
     add_versions("v0.20.0", "55f10531e4ba162ec768cf9c745ccc7b5a0930c7ad9974b268ad40246276baa8")
 
+    add_patches("v0.20.0", "patches/v0.20.0/fix-build.diff", "09bc180fd8b52f0323d16f1f96bb3a07e2b2fdbe2a48a7a6731637a940d13157")
+
     add_deps("cmake")
     add_deps("nlohmann_json", "fmt", {configs = {cmake = true}})
-    add_deps("boost", {configs = {system = true}})
+    add_deps("boost", {configs = {asio = true}})
 
-    if is_plat("linux") then
+    if is_plat("linux", "bsd") then
         add_syslinks("pthread")
+    elseif is_plat("windows", "mingw") then
+        add_syslinks("ws2_32", "mswsock")
     end
 
-    on_install("linux", "macosx", function (package)
+    on_install("!wasm and !windows", function (package)
         local configs = {}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
 
