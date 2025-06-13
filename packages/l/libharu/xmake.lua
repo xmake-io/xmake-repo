d0ff04692dee475c5d3632d6595208ce6c58314e
さくらみこ <lin@sz.cn.eu.org>
2025-06-13 09:36:25 +0300

    libharu: update to 2.4.5

--- packages/l/libharu/xmake.lua
+++ packages/l/libharu/xmake.lua
@@ -4,8 +4,15 @@ package("libharu")
     set_description("libHaru is a free, cross platform, open source library for generating PDF files.")
     set_license("zlib")
 
-    add_urls("https://github.com/libharu/libharu/archive/refs/tags/RELEASE_$(version).tar.gz", {version = function (version) return version:gsub("%.", "_") end})
+    add_urls("https://github.com/libharu/libharu/archive/refs/tags/RELEASE_$(version).tar.gz", {version = function (version)
+        if version:lt("2.4.0")
+            return version:gsub("%.", "_")
+        else
+            return format("%d_%d_%d", version:major(), version:minor(), version:patch())
+    end})
     add_urls("https://github.com/libharu/libharu.git")
+
+    add_versions("v2.4.5", "0ed3eacf3ceee18e40b6adffbc433f1afbe3c93500291cd95f1477bffe6f24fc")
     add_versions("2.3.0", "8f9e68cc5d5f7d53d1bc61a1ed876add1faf4f91070dbc360d8b259f46d9a4d2")
 
     add_deps("cmake", "zlib", "libpng")
@@ -15,7 +22,7 @@ package("libharu")
         end
     end)
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install(function (package)
         io.replace("src/CMakeLists.txt", "install(FILES ${addlib}", "#", {plain = true})
         local configs = {"-DLIBHPDF_EXAMPLES=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
