6f23e68c51431ea6005362a79801121e91800632
さくらみこ <lin@sz.cn.eu.org>
2025-06-13 09:56:19 +0300

    re

--- packages/t/taocpp-json/xmake.lua
+++ packages/t/taocpp-json/xmake.lua
@@ -8,9 +8,16 @@ package("taocpp-json")
     add_versions("2025.03.11", "11a31e12eda35c1322f9cf6ebb4cca0653d579dc")
 
     add_deps("cmake")
+    add_deps("pkgconf")
     add_deps("pegtl 54a2e32bf4593ed86782b4882702286cc8d621f9")
 
     on_install(function (package)
+        io.replace("CMakeLists.txt",
+            [[find_package(pegtl ${TAOCPP_JSON_PEGTL_MIN_VERSION} QUIET CONFIG)]], [[include(FindPkgConfig)
+pkg_check_modules(pegtl REQUIRED pegtl)]], {plain = true})
+        io.replace("CMakeLists.txt",
+            [[target_link_libraries(taocpp-json INTERFACE taocpp::pegtl)]],
+            [[target_include_directories(${pegtl_INCLUDE_DIRS})]], {plain = true})
         local configs = {
             "-DTAOCPP_JSON_BUILD_TESTS=OFF",
             "-DTAOCPP_JSON_BUILD_EXAMPLES=OFF",
