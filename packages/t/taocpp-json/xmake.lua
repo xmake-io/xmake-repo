09d73f2dddd6ac7f56da6d4db3314ccd4ba4680e
Saikari <lin@sz.cn.eu.org>
2025-06-13 09:40:09 +0300

    `, {submodules = false})`

--- packages/t/taocpp-json/xmake.lua
+++ packages/t/taocpp-json/xmake.lua
@@ -4,7 +4,7 @@ package("taocpp-json")
     set_description("C++ header-only JSON library")
     set_license("MIT")
 
-    add_urls("https://github.com/taocpp/json.git")
+    add_urls("https://github.com/taocpp/json.git", {submodules = false})
     add_versions("2025.03.11", "11a31e12eda35c1322f9cf6ebb4cca0653d579dc")
 
     add_deps("cmake")
