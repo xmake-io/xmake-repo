c668f7a8ae27ee8bd5ae8a2c023da6977c2b7b14
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-06-13 15:17:13 +0800

    upstream fixed MT issue

--- packages/b/breeze-js/xmake.lua
+++ packages/b/breeze-js/xmake.lua
@@ -4,12 +4,11 @@ package("breeze-js")
 
     add_urls("https://github.com/breeze-shell/breeze-js.git")
 
-    add_versions("2025.06.11", "ad906133ef4a940bed72909ddc2da9314c1c8846") -- use master temporarily to make fixing issues easier
+    add_versions("2025.06.11", "61c847bb5455873b65e9b82fe31a74e0676cc8b7") -- use master temporarily to make fixing issues easier
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
     on_install("windows", function (package)
-        io.replace("xmake.lua", [[set_runtimes("MT")]], [[]], {plain = true})
         io.replace("xmake.lua", [[set_languages("c89", "c++20")]], [[set_languages("c11", "c++20")]], {plain = true})
         io.replace("xmake.lua", [[set_kind("binary")]], [[set_kind("binary")
 set_enabled(false)]], {plain = true})
