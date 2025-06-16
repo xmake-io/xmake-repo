64a42da41edd2f69e6229919431e54d5c3481606
さくらみこ <lin@sz.cn.eu.org>
2025-06-16 07:18:28 +0300

    try to build on windows using MSYS2

--- packages/l/lunzip/xmake.lua
+++ packages/l/lunzip/xmake.lua
@@ -10,7 +10,16 @@ package("lunzip")
 
     add_versions("archive:1.15",  "fdb930b87672a238a54c4b86d63df1c86038ff577d512adbc8e2c754c046d8f2")
 
-    add_deps("autotools")
+    if not is_subhost("windows") then
+        add_deps("autotools")
+    end
+
+    on_load(function(package)
+        if is_subhost("windows") then
+            local msystem = "MINGW" .. (package:is_arch64() and "64" or "32")
+            package:add("deps", "msys2", {configs = {msystem = msystem, base_devel = true, gcc = true, make = true}})
+        end
+    end)
 
     on_install(function (package)
         import("package.tools.autoconf").install(package)
