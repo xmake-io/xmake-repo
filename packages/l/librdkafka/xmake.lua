17f202884b18bcdffce7e5420eff57805d6e2e4d
ruki <waruqi@gmail.com>
2025-06-19 01:56:31 +0000

    Update librdkafka to v2.10.1

--- packages/l/librdkafka/xmake.lua
+++ packages/l/librdkafka/xmake.lua
@@ -4,6 +4,7 @@ package("librdkafka")
 
     add_urls("https://github.com/edenhill/librdkafka/archive/refs/tags/$(version).tar.gz",
              "https://github.com/edenhill/librdkafka.git")
+    add_versions("v2.10.1", "75f59a2d948276504afb25bcb5713a943785a413b84f9099d324d26b2021f758")
     add_versions("v1.6.2", "b9be26c632265a7db2fdd5ab439f2583d14be08ab44dc2e33138323af60c39db")
     add_versions("v1.8.2-POST2", "d556d07cb88ea689e28c8e058ec3265ab333c9fc5e8f4ac0b7509bb5ae0e9f25")
 
