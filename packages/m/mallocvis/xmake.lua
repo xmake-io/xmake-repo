63978b4d75033a6bef356d5aab0b8b5243c4887d
さくらみこ <lin@sz.cn.eu.org>
2025-06-20 16:14:38 +0300

    strip export surface

--- packages/m/mallocvis/xmake.lua
+++ packages/m/mallocvis/xmake.lua
@@ -5,7 +5,7 @@ package("mallocvis")
     add_urls("https://github.com/archibate/mallocvis.git")
     
     add_versions("2024.07.17", "371e8dc21fec00adf2b45d7c7bb1b7cce8ac75ff")
-    add_patches("2024.07.17", "patches/2024.07.17/fix-build-mingw-bsd.diff", "9b380a5d9a1e7e439d0ff5b6a4725853a43aef087513c45668ccddd1fa125695")
+    add_patches("2024.07.17", "patches/2024.07.17/fix-build-mingw-bsd.diff", "4bab1555046df8bf2e8fef47f8fbbea4c540652e398d7c6944821b4be9644db7")
 
     if is_plat("windows", "mingw") then
         add_syslinks("dbghelp")
