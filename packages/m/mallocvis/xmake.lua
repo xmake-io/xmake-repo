28016c3cdc707433735dc3c6ef2631b11ddbc4e0
さくらみこ <lin@sz.cn.eu.org>
2025-06-20 17:04:40 +0300

    fixup

--- packages/m/mallocvis/xmake.lua
+++ packages/m/mallocvis/xmake.lua
@@ -5,7 +5,7 @@ package("mallocvis")
     add_urls("https://github.com/archibate/mallocvis.git")
     
     add_versions("2024.07.17", "371e8dc21fec00adf2b45d7c7bb1b7cce8ac75ff")
-    add_patches("2024.07.17", "patches/2024.07.17/fix-build-mingw-bsd.diff", "a62d9a7bd6e30afc7cd1659a251487b781440656533cbc004db9c6b201e2bbb0")
+    add_patches("2024.07.17", "patches/2024.07.17/fix-build-mingw-bsd.diff", "a26f43134c8efe139a6b4df215f1633cbe2cd21cc0281f86dea246f2ee25f1c6")
 
     if is_plat("windows", "mingw") then
         add_syslinks("dbghelp")
