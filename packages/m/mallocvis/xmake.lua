aa9391ac5dbec636727c1035c76c5d65f409f8a4
さくらみこ <lin@sz.cn.eu.org>
2025-06-20 12:59:20 +0300

    try freebsd

--- packages/m/mallocvis/xmake.lua
+++ packages/m/mallocvis/xmake.lua
@@ -13,6 +13,10 @@ package("mallocvis")
 
     on_install("!macosx and !iphoneos", function (package)
         io.replace("malloc_hook.cpp", "#if __GNUC__", "#if __GNUC__ && !_WIN32", {plain = true})
+        io.replace("malloc_hook.cpp", [[# include <sys/mman.h>]], [[# include <sys/mman.h>
+# if defined(__FreeBSD__)
+#  include <sys/types.h>
+# endif]], {plain = true})
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         import("package.tools.xmake").install(package)
     end)
