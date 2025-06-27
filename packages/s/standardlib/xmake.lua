e93f7ea410c3f54a96ec9981ee1484eb7d2bc9b8
Saikari <lin@sz.cn.eu.org>
2025-06-28 02:42:09 +0300

    rise std to c++17

--- packages/s/standardlib/xmake.lua
+++ packages/s/standardlib/xmake.lua
@@ -30,5 +30,5 @@ package("standardlib")
                 const char *suffix = "text";
                 printf("Does the string end with \"%s\"? %s\n", suffix, ends_with(text, suffix) ? "Yes" : "No");
             }
-        ]]}))
+        ]]}, {configs = {languages = "c++17"}}))
     end)
