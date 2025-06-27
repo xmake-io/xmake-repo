b5b30445f18359572dfaf1f487515034d45acb8b
Saikari <lin@sz.cn.eu.org>
2025-06-28 01:55:01 +0300

    standardlib: update to 2024.03.25

--- packages/s/standardlib/xmake.lua
+++ packages/s/standardlib/xmake.lua
@@ -1,13 +1,13 @@
 package("standardlib")
-
-    set_kind("library", {headeronly = true})
+    set_kind("library", {headeronly = true})    
     set_homepage("https://github.com/gregoryc/standardlib")
-    set_description("an actually usable and maximally efficient C standard library to make C as easy (or easier) than other languages")
+    set_description("A complete standardlib for c for once")
 
     add_urls("https://github.com/gregoryc/standardlib.git")
+    add_versions("2024.03.25", "d27a1293ccfe7ef04a961806754c5d1272614b72")
     add_versions("2023.12.5", "4fb308a5716927e5622a0488d7aa104660c96841")
 
-    on_install("linux", function (package)
+    on_install(function (package)
         os.cp("standardlib.h", package:installdir("include"))
     end)
 
