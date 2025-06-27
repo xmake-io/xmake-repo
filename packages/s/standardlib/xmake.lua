b3994dc43781f2e4866cf8ade95a24e66ebf80ac
Saikari <lin@sz.cn.eu.org>
2025-06-28 02:50:31 +0300

    limit platforms

--- packages/s/standardlib/xmake.lua
+++ packages/s/standardlib/xmake.lua
@@ -4,7 +4,7 @@ package("standardlib")
     set_description("A complete standardlib for c for once")
     set_license("Public Domain")
 
-    add_urls("https://github.com/gregoryc/standardlib.git", {includes = {"foundationallib.h"}})
+    add_urls("https://github.com/gregoryc/standardlib.git")
     add_versions("2024.03.25", "d27a1293ccfe7ef04a961806754c5d1272614b72")
 
     if is_plat("linux", "bsd") then
@@ -12,17 +12,17 @@ package("standardlib")
     end
 
     on_load(function (package)
-        if not package:is_plat("linux", "bsd", "cross") then
+        if not package:has_cxxincludes("threads.h", {configs = {languages = "c11"}}) then
             package:add("defines", "FOUNDATIONAL_LIB_THREAD_FUNCTIONS_ENABLED=0")
         end
     end)
 
-    on_install(function (package)
+    on_install("linux", "bsd", "cross", "mingw", function (package)
         os.cp("foundationallib.h", package:installdir("include"))
     end)
 
     on_test(function (package)
-        assert(package:check_cxxsnippets({test = [[
+        assert(package:check_csnippets({test = [[
             #include <foundationallib.h>
             #include <stdio.h>
             void test() {
@@ -30,5 +30,5 @@ package("standardlib")
                 const char *suffix = "text";
                 printf("Does the string end with \"%s\"? %s\n", suffix, ends_with(text, suffix) ? "Yes" : "No");
             }
-        ]]}, {configs = {languages = "c++17"}}))
+        ]]}, {configs = {languages = "c11"}}))
     end)
