92003b49865f0dc4633037f5845ea54c73fd1597
star9029 <hengxings783@gmail.com>
2025-06-29 10:36:56 +0800

    Update xmake.lua

--- packages/c/cmocka/xmake.lua
+++ packages/c/cmocka/xmake.lua
@@ -13,6 +13,7 @@ package("cmocka")
     on_install("windows", "linux", "macosx", "mingw", "msys", function (package)
         local configs = {"-DUNIT_TESTING=OFF", "-DWITH_EXAMPLES=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
