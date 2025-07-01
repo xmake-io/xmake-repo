aeb2610e61b4396affed6e2021e46bbfdd38d020
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2025-07-01 00:38:30 -0300

    libcoro: added version v0.14.1

--- packages/l/libcoro/xmake.lua
+++ packages/l/libcoro/xmake.lua
@@ -6,10 +6,12 @@ package("libcoro")
     add_urls("https://github.com/jbaldwin/libcoro/archive/refs/tags/$(version).tar.gz",
              "https://github.com/jbaldwin/libcoro.git", {submodules = false})
 
+    add_versions("v0.14.1", "0a18058fe17826237a868e3d266960e839db8c7aeeb2beba9b596c84124afe0e")
     add_versions("v0.14.0", "baf4b1535dee94bf47d3901b7e4842cedead5828ce7583e9a30ff8c5a8e0eb6e")
     add_versions("v0.13.0", "aea5e4f4c04ef01269cc4e40ce9e693f71e324574ea0a933d908783ef385f9f5")
     add_versions("v0.12.1", "2cb6f45fc73dad6008cc930d92939785684835e03b12df422b98fcab9e393add")
 
+    add_patches("v0.14.1", path.join(os.scriptdir(), "patches", "v0.14.1.patch"), "bd5892560831ec322409ed9af82466ae523d967c1c80ca77c66bc9b64a4b54c7")
     add_patches("v0.14.0", path.join(os.scriptdir(), "patches", "v0.14.0.patch"), "bd5892560831ec322409ed9af82466ae523d967c1c80ca77c66bc9b64a4b54c7")
     add_patches("v0.13.0", path.join(os.scriptdir(), "patches", "v0.13.0.patch"), "bd5892560831ec322409ed9af82466ae523d967c1c80ca77c66bc9b64a4b54c7")
 
