b2311a9c0828908465bcaf63cd2907796b277468
choyy <68107073+choyy@users.noreply.github.com>
2025-07-11 22:05:17 +0800

    fix gklib

--- packages/g/gklib/port/xmake.lua
+++ packages/g/gklib/port/xmake.lua
@@ -20,6 +20,7 @@ end
 includes("@builtin/check")
 
 configvar_check_cincludes("HAVE_EXECINFO_H", "execinfo.h")
+configvar_check_cincludes("HAVE_STDBOOL_H", "stdbool.h")
 configvar_check_cfuncs("HAVE_GETLINE", "getline")
 
 target("gklib")
