8776ab6589da84580a93fc9de08d8cb39606e28d
Saikari <lin@sz.cn.eu.org>
2025-07-15 19:14:22 +0300

    libbacktrace: update to 2025.04.10

--- packages/l/libbacktrace/xmake.lua
+++ packages/l/libbacktrace/xmake.lua
@@ -1,20 +1,19 @@
 package("libbacktrace")
-
     set_homepage("https://github.com/ianlancetaylor/libbacktrace")
     set_description("A C library that may be linked into a C/C++ program to produce symbolic backtraces")
 
     add_urls("https://github.com/ianlancetaylor/libbacktrace.git")
-    add_versions("v1.0", "d0f5e95a87a4d3e0a1ed6c069b5dae7cbab3ed2a")
+    add_versions("2025.04.10", "793921876c981ce49759114d7bb89bb89b2d3a2d")
 
-    on_install("linux", "macosx", function (package)
+    add_deps("autotools")
+
+    on_install("!windows", function (package)
         local configs = {}
-        local cxflags = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-        if package:config("pic") ~= false then
-            table.insert(cxflags, "-fPIC")
+        if package:is_debug() then
+            table.insert(configs, "--enable-debug")
         end
-        import("package.tools.autoconf").install(package, configs, {cxflags = cxflags})
+        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_test(function (package)
