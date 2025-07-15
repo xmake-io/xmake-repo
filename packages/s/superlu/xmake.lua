df3b77bf52f9387d3d30cc858990a9a938ed9485
Willaaaaaaa <willaaaaaaa936@gmail.com>
2025-07-15 14:20:16 +0800

    update xmake.lua

--- packages/s/superlu/xmake.lua
+++ packages/s/superlu/xmake.lua
@@ -14,12 +14,7 @@ package("superlu")
     add_configs("blas", {description = "Choose BLAS library to use.", default = "openblas", type = "string", values = {"mkl", "openblas"}})
 
     on_load("windows|!arm64", "linux", "macosx", function (package)
-        local blas_lib = package:config("blas")
-        if blas_lib == "openblas" and is_plat("macosx") then
-                package:add("deps", "openblas", {configs = {fortran = true}})
-        else
-            package:add("deps", blas_lib)
-        end
+        package:add("deps", package:config("blas"))
     end)
 
     on_install("windows|!arm64", "linux", "macosx", function (package)
