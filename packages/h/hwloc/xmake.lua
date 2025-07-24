6d5275e1ad17763b03835f019f54027c271a2ec2
Willaaaaaaa <willaaaaaaa936@gmail.com>
2025-07-24 18:27:31 +0800

    update: 2.12.1

--- packages/h/hwloc/xmake.lua
+++ packages/h/hwloc/xmake.lua
@@ -1,5 +1,4 @@
 package("hwloc")
-
     set_homepage("https://www.open-mpi.org/software/hwloc/")
     set_description("Portable Hardware Locality (hwloc)")
     set_license("BSD-3-Clause")
@@ -11,12 +10,14 @@ package("hwloc")
             end})
             add_versions("2.5.0", "0ff33ef99b727a96fcca8fd510e41f73444c5e9ea2b6c475a64a2d9a294f2973")
             add_versions("2.7.1", "217d508f715d42932c6d52e5cf5eb3559d9691d6bb77c34f00b3dcb6517c58e5")
+            add_versions("2.12.1", "8c293adbfbeb9ad1295f2d58e8231a7e07938ae7175b4f81ceda78d47274e55f")
         elseif is_arch("x64") then
             add_urls("https://download.open-mpi.org/release/hwloc/v$(version).zip", {version = function (version)
                 return format("%d.%d/hwloc-win64-build-%s", version:major(), version:minor(), version)
             end})
             add_versions("2.5.0", "b64f5ebe534d1ad57cdd4b18ab4035389b68802a97464c1295005043075309ea")
             add_versions("2.7.1", "31031eb09f7d8bfaaa069e537ec26374269dddd5b1f1a368c1ed6593849be5b1")
+            add_versions("2.12.1", "b48e5407def209bc0b6891becb9f30bb8d1a0c09790085ef56f280e3d164dc4b")
         end
     else
         add_urls("https://download.open-mpi.org/release/hwloc/v$(version).tar.gz", {version = function (version)
@@ -24,6 +25,7 @@ package("hwloc")
         end})
         add_versions("2.5.0", "38aa8102faec302791f6b4f0d23960a3ffa25af3af6af006c64dbecac23f852c")
         add_versions("2.7.1", "4cb0a781ed980b03ad8c48beb57407aa67c4b908e45722954b9730379bc7f6d5")
+        add_versions("2.12.1", "ffa02c3a308275a9339fbe92add054fac8e9a00cb8fe8c53340094012cb7c633")
     end
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
