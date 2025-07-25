d79ffc7e13a3fb1872e23a2ec9396494cab91444
star9029 <hengxings783@gmail.com>
2025-07-25 14:42:58 +0800

    Update xmake.lua

--- packages/m/marisa/xmake.lua
+++ packages/m/marisa/xmake.lua
@@ -12,23 +12,29 @@ package("marisa")
     add_patches("v0.3.0", "patches/v0.3.0/support-debug-install.diff", "a3d02bf6881d233bf8cfadded33edfcde167bee719d47538b869e0e90d8bf7ce")
     add_patches("v0.3.0", "https://github.com/s-yata/marisa-trie/pull/119.diff", "f02211699465b55cd2ab93ef20bafcd69aa573da1fd796cb9366697075074093")
 
+    add_configs("tools", {description = "Build tools", default = false, type = "boolean"})
+
     add_deps("cmake")
 
     on_install(function (package)
         if package:version() and package:version():lt("v0.3.0") then
             os.cp(path.join(package:scriptdir(), "port", "CMakeLists.txt"), "CMakeLists.txt")
         end
+        -- fix install for debug build type
+        io.replace("CMakeLists.txt", "CONFIGURATIONS Release", "", {plain = true})
+
         local configs = {
             "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
             "-DENABLE_TESTS=OFF",
             "-DBUILD_TESTING=OFF",
-            "-DENABLE_TOOLS=OFF"
         }
         if package:config("shared") and package:is_plat("windows") then
             table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
         end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_TOOLS=" .. (package:config("tools") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
