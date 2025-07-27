9eafa9648dd50683a536418a1a4b2496122a1f54
star9029 <hengxings783@gmail.com>
2025-07-27 19:21:17 +0800

    clean

--- packages/l/libpciaccess/xmake.lua
+++ packages/l/libpciaccess/xmake.lua
@@ -3,8 +3,9 @@ package("libpciaccess")
     set_description("Generic PCI access library")
     set_license("MIT")
 
-    add_urls("https://gitlab.freedesktop.org/xorg/lib/libpciaccess/-/archive/libpciaccess-$(version)/libpciaccess-libpciaccess-$(version).tar.gz")
-    add_urls("https://gitlab.freedesktop.org/xorg/lib/libpciaccess.git")
+    add_urls("https://gitlab.freedesktop.org/xorg/lib/libpciaccess/-/archive/libpciaccess-$(version)/libpciaccess-libpciaccess-$(version).tar.gz",
+             "https://gitlab.freedesktop.org/xorg/lib/libpciaccess.git")
+
     add_versions("0.18.1", "a395317730e0e8d5e71419d4d1256a89e32c2fa793607b63c4d0fb497ae34602")
 
     add_configs("zlib", {description = "Enable zlib support to read gzip compressed pci.ids.", default = false, type = "boolean"})
@@ -13,7 +14,7 @@ package("libpciaccess")
 
     add_deps("meson", "ninja")
 
-    on_load("linux", "bsd", function (package)
+    on_load(function (package)
         if package:config("zlib") then
             package:add("deps", "zlib")
         end
@@ -21,19 +22,12 @@ package("libpciaccess")
 
     on_install("linux", "bsd", function (package)
         local configs = {}
-        if package:is_debug() then
-            table.insert(configs, "-Dbuildtype=debug")
-        else
-            table.insert(configs, "-Dbuildtype=release")
-        end
-
         table.insert(configs, "-Dzlib=" .. (package:config("zlib") and "enabled" or "disabled"))
         table.insert(configs, "-Dlinux-rom-fallback=" .. (package:config("linux_rom_fallback") and "true" or "false"))
         table.insert(configs, "-Dpci-ids=" .. package:config("pci_ids_path"))
-
         import("package.tools.meson").install(package, configs)
     end)
 
-    on_test("linux", "bsd", function (package)
+    on_test(function (package)
         assert(package:has_cfuncs("pci_system_init", {includes = "pciaccess.h"}))
     end)
