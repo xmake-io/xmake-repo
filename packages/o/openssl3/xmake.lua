a15cbe8eebedbeaed7ea5314c439a3384f4f438b
Redbeanw44602 <redbeana44945@gmail.com>
2025-08-01 20:12:38 +0800

    fix.

--- packages/o/openssl3/xmake.lua
+++ packages/o/openssl3/xmake.lua
@@ -101,6 +101,9 @@ package("openssl3")
 
         io.replace("Configurations/10-main.conf", "/Fd", "/Fd\\\"" .. package:installdir("bin"):gsub("\\", "/") .. "/", {plain = true})
         io.replace("Configurations/10-main.conf", ".pdb\"", ".pdb\\\"\"", {plain = true})
+        io.replace("Configurations/windows-makefile.tmpl", [[	@if not "$(INSTALL_PROGRAMS)"=="" \
+	 "$(PERL)" "$(SRCDIR)\util\copy.pl" $(INSTALL_PROGRAMPDBS) \
+                                        "$(INSTALLTOP)\bin"]], "", {plain = true})
 
         os.vrunv("perl", configs)
 
