3fbc97d63be16cbaa1b8b06e41e5576870f7992f
star9029 <hengxings783@gmail.com>
2025-08-01 11:07:46 +0800

    nativefiledialog-extended: add mingw support

--- packages/n/nativefiledialog-extended/xmake.lua
+++ packages/n/nativefiledialog-extended/xmake.lua
@@ -1,10 +1,11 @@
 package("nativefiledialog-extended")
-
     set_homepage("https://github.com/btzy/nativefiledialog-extended")
     set_description("Cross platform (Windows, Mac, Linux) native file dialog library with C and C++ bindings, based on mlabbe/nativefiledialog.")
-    
+    set_license("zlib")
+
     add_urls("https://github.com/btzy/nativefiledialog-extended/archive/refs/tags/$(version).zip",
              "https://github.com/btzy/nativefiledialog-extended.git")
+
     add_versions("v1.2.1", "fc359b212e56011931b90bf4241057eddec45308bb4d8b9aab4dfb2f70e3b211")
     add_versions("v1.2.0", "27dc13320816392d0d9905c60645aa684784c7c2559d656b504021edd40f07ed")
     add_versions("v1.1.1", "7003001d36235db2c2062cd992e61c59c77a5ad3ca5e5ed8175e56502513886e")
@@ -17,11 +18,13 @@ package("nativefiledialog-extended")
     end
 
     add_deps("cmake")
-    if is_plat("windows") then
-        add_syslinks("shell32", "ole32")
+
+    if is_plat("windows", "mingw", "msys") then
+        add_syslinks("shell32", "ole32", "uuid")
     elseif is_plat("macosx") then
         add_frameworks("AppKit", "UniformTypeIdentifiers")
     end
+
     on_load("linux", function (package)
         if package:config("portal") then
             package:add("deps", "dbus")
@@ -30,9 +33,9 @@ package("nativefiledialog-extended")
         end
     end)
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install("windows", "macosx", "linux", "mingw", "msys", function (package)
         local configs = {"-DNFD_BUILD_TESTS=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DNFD_PORTAL=" .. (package:config("portal") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
