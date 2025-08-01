2708fe010539534d63fab63f214059c9abbefe91
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-08-01 18:26:38 +0200

    glfw3webgpu: add defines on package loading

--- packages/g/glfw3webgpu/xmake.lua
+++ packages/g/glfw3webgpu/xmake.lua
@@ -2,7 +2,7 @@ package("glfw3webgpu")
     set_description("An extension for the GLFW library for using WebGPU native.")
     set_homepage("https://github.com/eliemichel/glfw3webgpu")
     set_license("MIT")
-    
+
     add_urls("https://github.com/eliemichel/glfw3webgpu/archive/refs/tags/$(version).tar.gz",
              "https://github.com/eliemichel/glfw3webgpu.git")
 
@@ -11,11 +11,23 @@ package("glfw3webgpu")
     add_versions("v1.1.0", "307ba86a724adc84a875e8bd2374baad0fabc77797f20f9a1779eef8d9ffe95a")
     add_versions("v1.0.1", "b98c63f1905f0e4cf99229de8b7e5c2693fdf3b8d684b5a43d60f21f67d33e6b")
 
-    add_deps("wgpu-native", "glfw")
-
     if is_plat("macosx", "iphoneos") then
         add_frameworks("Metal", "Foundation", "QuartzCore")
     end
+
+    on_load(function (package)
+        if package:is_plat("windows") then
+            package:add("defines", "GLFW_EXPOSE_NATIVE_WIN32")
+        elseif package:is_plat("macosx", "iphoneos") then
+            package:add("defines", "GLFW_EXPOSE_NATIVE_COCOA")
+        end
+
+        if package:config("x11") then
+            package:add("defines", "GLFW_EXPOSE_NATIVE_X11")
+        elseif package:config("wayland") then
+            package:add("defines", "GLFW_EXPOSE_NATIVE_WAYLAND")
+        end
+    end)
 
     on_install("windows|x64", "windows|x86", "linux|x86_64", "macosx|x86_64", "macosx|arm64", function (package)
         if package:is_plat("macosx", "iphoneos") then
@@ -45,19 +57,19 @@ package("glfw3webgpu")
                 set_kind("$(kind)")
                 set_languages("c11")
                 add_headerfiles("glfw3webgpu.h")
-                
+
                 add_mxflags("-fno-objc-arc")
-                
+
                 add_packages("wgpu-native")
                 add_packages("glfw")
-                
+
                 if is_plat("iphoneos", "macosx") then
                     add_frameworks("Metal", "Foundation", "QuartzCore")
                     add_files("glfw3webgpu.m")
                 else
                     add_files("glfw3webgpu.c")
                 end
-                
+
                 if is_plat("windows") and is_kind("shared") then
                     add_rules("utils.symbols.export_all")
                 end
