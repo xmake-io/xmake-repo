2742a38aabd3a300ab1a8bd882593b3f92db2d6e
Redbeanw44602 <redbeana44945@gmail.com>
2025-08-06 00:20:46 +0800

    mpich: fix build on archlinux.

--- packages/m/mpich/xmake.lua
+++ packages/m/mpich/xmake.lua
@@ -23,13 +23,16 @@ package("mpich")
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {"--disable-fortran",
-                         "--without-slurm",
-                         "--without-xpmem",
-                         "--without-hcoll",
-                         "--without-blcr",
-                         "--without-papi",
-                         "--without-pmix"}
+        local configs = {
+            "--disable-dependency-tracking",
+            "--disable-fortran",
+            "--without-slurm",
+            "--without-xpmem",
+            "--without-hcoll",
+            "--without-blcr",
+            "--without-papi",
+            "--without-pmix"
+        }
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
         table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
         if package:config("pic") ~= false then
@@ -38,11 +41,19 @@ package("mpich")
         table.insert(configs, "--with-device=ch4:" .. package:config("device"))
         table.insert(configs, "--with-hwloc-prefix=" .. package:dep("hwloc"):installdir())
         table.insert(configs, "--with-x=" .. (package:config("x11") and "yes" or "no"))
-        import("package.tools.autoconf").install(package, configs)
+
+        local opt = {}
+        if package:is_plat("linux") then
+            opt.cxflags = "-lm"
+        end
+
+        import("package.tools.autoconf").install(package, configs, opt)
         package:addenv("PATH", "bin")
     end)
 
     on_test(function (package)
-        os.vrun("mpicc --version")
+        if not package:is_cross() then
+            os.vrun("mpicc --version")
+        end
         assert(package:has_cfuncs("MPI_Init", {includes = "mpi.h"}))
     end)
