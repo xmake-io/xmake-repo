c973f68dd9d8bd68ef62f7ded9eaee4a244727fe
Saikari <lin@sz.cn.eu.org>
2025-08-06 12:05:31 +0300

    re

--- packages/a/aui/xmake.lua
+++ packages/a/aui/xmake.lua
@@ -8,7 +8,7 @@ package("aui")
 
     add_versions("v7.1.2", "a4cf965c50d75e20a319c9c8b231ad9c13c25a06ad303e1eb65d1ff141b1f85c")
 
-    add_patches("v7.1.2", "patches/v7.1.2/debundle-audio.diff", "8ad59e8cb6519be3eb3a2d4e94f3605ae5a2792b7a3c894990f28ba55bca6fc9")
+    add_patches("v7.1.2", "patches/v7.1.2/debundle-audio.diff", "6f3ae1823a19837ab6569fd6267105912588c70a6d0fe26c084b9b542dc64e28")
     add_patches("v7.1.2", "patches/v7.1.2/debundle-build.diff", "92bfd68e28a703518c12cf51b898a6b75cacae1fec9384328562c47b003e9577")
     add_patches("v7.1.2", "patches/v7.1.2/debundle-core.diff", "0b3cf2140767e2f5232a3769301f1ca8af8c5e07ded55a8f8a98e627a597fd64")
     add_patches("v7.1.2", "patches/v7.1.2/debundle-crypt.diff", "58045d168a8c7f2658554e0a3010579ec53b54e2c51f524a4fb61e5e4d6fc0a7")
