e696273a36f665273ab531b737b171940afd46bb
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-12 11:25:25 +0200

    update libpq to version 17.5, use meson instead of autoconf, added windows build

--- packages/l/libpq/xmake.lua
+++ packages/l/libpq/xmake.lua
@@ -6,27 +6,23 @@ package("libpq")
     add_urls("https://github.com/postgres/postgres/archive/refs/tags/REL_$(version).tar.gz", {alias = "github", version = function (version)
         return version:gsub("%.", "_")
     end})
-    add_versions("14.1", "14809c9f669851ab89b344a50219e85b77f3e93d9df9e255b9781d8d60fcfbc9")
+    add_versions("17.5", "476e0522af981352177c12a05295b08d2f49b35c667aecb5bae1af4807999467")
 
-    add_deps("krb5", "openssl", "zlib")
+    add_deps("openssl", "zlib")
     if is_plat("linux") then
-        add_deps("flex", "bison")
+        add_deps("flex", "bison", "krb5")
     end
 
-    on_install("macosx", "linux", function (package)
-        local configs = {"--with-openssl", "--without-readline"}
-        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
-        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
-        if package:is_plat("macosx") then
-            table.insert(configs, "--with-gssapi")
-        end
+    on_install(function (package)
+        local configs = {"-Dssl=openssl", "-Dzlib=enabled"}
+
+        table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
+
         if package:debug() then
-            table.insert(configs, "--enable-debug")
+            table.insert(configs, "--buildtype=debug")
         end
-        if package:config("pic") ~= false then
-            table.insert(configs, "--with-pic")
-        end
-        import("package.tools.autoconf").install(package, configs, {packagedeps = {"openssl", "zlib"}})
+
+        import("package.tools.meson").install(package)
     end)
 
     on_test(function (package)
