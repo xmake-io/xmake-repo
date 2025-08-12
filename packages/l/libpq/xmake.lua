a36424aa7e0200f1912385b30dab408ab372ff5e
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-12 12:27:17 +0200

    fix: update on_install function to exclude cross-compilation

--- packages/l/libpq/xmake.lua
+++ packages/l/libpq/xmake.lua
@@ -13,7 +13,7 @@ package("libpq")
         add_deps("krb5")
     end
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install("windows", "macosx", "linux", "!cross", function (package)
         local configs = {"-Dssl=openssl", "-Dzlib=enabled"}
 
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
