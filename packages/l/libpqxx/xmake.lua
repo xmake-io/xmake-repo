6e6fab5252a2c534fe74b8616f3f470313a21fb2
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-13 11:27:08 +0200

    update libpqxx to version 7.10.1 and refine dependencies

--- packages/l/libpqxx/xmake.lua
+++ packages/l/libpqxx/xmake.lua
@@ -4,12 +4,11 @@ package("libpqxx")
 
     add_urls("https://github.com/jtv/libpqxx/archive/refs/tags/$(version).tar.gz",
              "https://github.com/jtv/libpqxx.git")
-    add_versions("7.7.0", "2d99de960aa3016915bc69326b369fcee04425e57fbe9dad48dd3fa6203879fb")
+    add_versions("7.10.1", "9bfaf9cb5a73ac23f9b7a9dfd7ef069a6e2124fb")
 
-    add_deps("cmake", "python 3.x")
-    add_deps("xmlto", "libpq")
+    add_deps("cmake", "libpq")
 
-    on_install("linux", "macosx", function (package)
+    on_install("windows|!arm64 or macosx|!arm64 or linux|!arm64 or bsd|!arm64", function (package)
         local configs = {"-DSKIP_BUILD_TEST=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
