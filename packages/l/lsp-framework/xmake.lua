4bdd588b5abecfa54bd4d5f63a3c06f686e5996a
ruki <waruqi@gmail.com>
2025-08-13 01:27:56 +0000

    Update lsp-framework to 1.1.1

--- packages/l/lsp-framework/xmake.lua
+++ packages/l/lsp-framework/xmake.lua
@@ -6,6 +6,7 @@ package("lsp-framework")
     add_urls("https://github.com/leon-bckl/lsp-framework/archive/refs/tags/$(version).tar.gz",
              "https://github.com/leon-bckl/lsp-framework.git")
 
+    add_versions("1.1.1", "bd430a3c0a8a6704c220a479790bbea24a17895c2e25720681b49cfd90081217")
     add_versions("1.0.1", "07f924d851896a2d424d554d20820483f8458aa1ff907bb68657b0d2d0bd0d13")
 
     add_patches("1.0.1", "patches/1.0.1/fix-install.diff", "bb5e4436091ba1846144ffa80fb8afd4d0213760bce45dd6fd31662905cb4bc3")
