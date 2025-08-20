1509ad094064890f36c617f735e2256387984dce
ruki <waruqi@gmail.com>
2025-08-20 11:05:41 +0800

    Update mimalloc to v3.1.5 (#7430)
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/m/mimalloc/xmake.lua
+++ packages/m/mimalloc/xmake.lua
@@ -6,6 +6,8 @@ package("mimalloc")
     set_urls("https://github.com/microsoft/mimalloc/archive/refs/tags/$(version).zip",
              "https://github.com/microsoft/mimalloc.git")
 
+    add_versions("v3.1.5", "3cf724ec469198f23505d157893331f9d062e982c38b2c92a7fb789d7ddb67d9")
+
     add_versions("v2.2.4", "664667a48c9f101d979bbe4e41ee631da49d2024e30d66b7779b6ba4279af367")
     add_versions("v2.1.7", "fa61cf01e3dd869b35275bfd8be95bfde77f0b65dfa7e34012c09a66e1ea463f")
     add_versions("v2.1.2", "86281c918921c1007945a8a31e5ad6ae9af77e510abfec20d000dd05d15123c7")
