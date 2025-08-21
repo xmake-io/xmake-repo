25159f52f553f33e04a38872bc61c7b303981cf7
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-21 18:59:45 +0200

    Update glgpuselect version to 0.1.1 and change default shared library config to true

--- packages/g/glgpuselect/xmake.lua
+++ packages/g/glgpuselect/xmake.lua
@@ -5,9 +5,9 @@ package("glgpuselect")
 
     add_urls("https://github.com/ArthurVasseur/GLGpuSelect/archive/refs/tags/$(version).zip",
              "https://github.com/ArthurVasseur/GLGpuSelect.git")
-    add_versions("0.1.0", "8f3b619c7cb33cfbf84bb2657a48ac6862b7593df85cd4ca289f58681932e738")
+    add_versions("0.1.1", "29c9f2a0e879605abdf69e72ff008716dbba18865974761df5c887647b0ad774")
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
     add_configs("profiling", {description = "Build with tracy profiler", default = false, type = "boolean"})
     add_configs("asserts", {description = "Enable asserts", default = false, type = "boolean"})
     add_configs("logging", {description = "Enable logging", default = false, type = "boolean"})
