3de9c7cbdb94a5a7337c4426bcfefc5fecfc6412
star9029 <hengxings783@gmail.com>
2025-08-24 01:21:58 +0800

    libavif: fix deps

--- packages/l/libavif/xmake.lua
+++ packages/l/libavif/xmake.lua
@@ -13,7 +13,17 @@ package("libavif")
     add_versions("v1.0.4", "dc56708c83a4b934a8af2b78f67f866ba2fb568605c7cf94312acf51ee57d146")
     add_versions("v0.9.1", "8526f3fff34a05a51d7c703cdcf1d0d38c939b5b6dd4bb7d3a3405ddad88186c")
 
+    add_configs("aom", {description = "Use the aom codec for encoding/decoding.", default = false, type = "boolean"})
     add_configs("dav1d", {description = "Use the dav1d codec for decoding.", default = false, type = "boolean"})
+    add_configs("libgav1", {description = "Use the libgav1 codec for decoding.", default = false, type = "boolean", readonly = true})
+    add_configs("rav1e", {description = "Use the rav1e codec for encoding.", default = false, type = "boolean", readonly = true})
+    add_configs("svt_av1", {description = "Use the svt-av1 codec for encoding.", default = false, type = "boolean"})
+    add_configs("libsharpyuv", {description = "Use libsharpyuv", default = false, type = "boolean", readonly = true})
+    add_configs("tools", {description = "Build tools", default = false, type = "boolean"})
+
+    if is_plat("linux", "bsd") then
+        add_syslinks("m", "pthread")
+    end
 
     add_deps("cmake")
     add_deps("libyuv")
@@ -31,17 +41,82 @@ package("libavif")
     end
 
     on_load(function (package)
-        if package:config("dav1d") then
-            package:add("deps", "dav1d")
+        local config_to_dep_name = {
+            ["svt_av1"] = "svt-av1",
+            -- ["libsharpyuv"] = "libwebp",
+        }
+        for name, enabled in table.orderpairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") and name ~= "tools" then
+                if package:config(name) then
+                    package:add("deps", config_to_dep_name[name] or name)
+                end
+            end
+        end
+
+        if package:config("tools") then
+            package:add("deps", "libpng", "libjpeg-turbo", "libxml2", {private = true})
+        end
+
+        if package:is_plat("windows") and package:config("shared") then
+            package:add("defines", "AVIF_DLL")
         end
     end)
 
     on_install("!cross and !wasm", function (package)
+        io.replace("CMakeLists.txt", "set(CMAKE_POSITION_INDEPENDENT_CODE ON)", "", {plain = true})
+        if package:config("aom") then
+            io.replace("CMakeLists.txt",
+                "check_avif_option(AVIF_CODEC_AOM TARGET aom PKG_NAME aom)",
+                "find_package(aom REQUIRED)", {plain = true})
+        end
+        if package:config("dav1d") then
+            io.replace("CMakeLists.txt",
+                "check_avif_option(AVIF_CODEC_DAV1D TARGET dav1d::dav1d PKG_NAME dav1d)",
+                "find_package(dav1d REQUIRED)", {plain = true})
+        end
+        if package:config("svt_av1") then
+            io.replace("CMakeLists.txt",
+                "check_avif_option(AVIF_CODEC_SVT TARGET SvtAv1Enc PKG_NAME svt)",
+                "find_package(svt REQUIRED)\nfind_package(cpuinfo REQUIRED)", {plain = true})
+            io.replace("CMakeLists.txt",
+                "avif_target_link_library(SvtAv1Enc)",
+                "avif_target_link_library(SvtAv1Enc)\navif_target_link_library(cpuinfo::cpuinfo)", {plain = true})
+        end
+        if package:config("tools") then
+            io.replace("CMakeLists.txt",
+                "check_avif_option(AVIF_LIBXML2 TARGET LibXml2::LibXml2 PKG_NAME LibXml2)",
+                "find_package(LibXml2 CONFIG REQUIRED)", {plain = true})
+        end
+
         local configs = {"-DAVIF_ENABLE_WERROR=OFF", "-DAVIF_BUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
 
-        table.insert(configs, "-DAVIF_CODEC_DAV1D=" .. (package:config("dav1d") and "ON" or "OFF"))
+        local config_map = {
+            ["aom"] = "AVIF_CODEC_AOM",
+            ["dav1d"] = "AVIF_CODEC_DAV1D",
+            ["libgav1"] = "AVIF_CODEC_LIBGAV1",
+            ["rav1e"] = "AVIF_CODEC_RAV1E",
+            ["svt_av1"] = "AVIF_CODEC_SVT",
+            ["libsharpyuv"] = "AVIF_LIBSHARPYUV",
+        }
+        for name, enabled in table.orderpairs(package:configs()) do
+            if not package:extraconf("configs", name, "builtin") and name ~= "tools" then
+                local key = config_map[name] or name
+                local value
+                if key:startswith("AVIF_CODEC_") then
+                    value = enabled and "ON" or "OFF"
+                    table.insert(configs, format("-D%s_ENABLED=%s", key, value))
+                else
+                    value = enabled and "SYSTEM" or "OFF"
+                end
+                table.insert(configs, format("-D%s=%s", key, value))
+            end
+        end
+        table.insert(configs, "-DAVIF_BUILD_APPS=" .. (package:config("tools") and "ON" or "OFF"))
+        table.insert(configs, "-DAVIF_ZLIBPNG=" .. (package:config("tools") and "SYSTEM" or "OFF"))
+        table.insert(configs, "-DAVIF_JPEG=" .. (package:config("tools") and "SYSTEM" or "OFF"))
+        table.insert(configs, "-DAVIF_LIBXML2=" .. (package:config("tools") and "SYSTEM" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
 
