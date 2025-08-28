c34e22d90d408fa15f5a7fa0eb71e8ca9f8e8ba6
star9029 <hengxings783@gmail.com>
2025-08-29 01:21:40 +0800

    eve: add cmake

--- packages/e/eve/xmake.lua
+++ packages/e/eve/xmake.lua
@@ -4,14 +4,22 @@ package("eve")
     set_description("Expressive Vector Engine - SIMD in C++ Goes Brrrr")
     set_license("BSL-1.0")
 
-    add_urls("https://github.com/jfalcou/eve/archive/refs/tags/v$(version).tar.gz", {excludes = {"*.paxheader", "*.data"}})
+    add_urls("https://github.com/jfalcou/eve/archive/refs/tags/$(version).tar.gz", {excludes = {"*.paxheader", "*.data"}})
     add_urls("https://github.com/jfalcou/eve.git")
 
-    add_versions("2023.02.15", "7a5fb59c0e6ef3bef3e8b36d62e138d31e7f2a9f1bdfe95a8e96512b207f84c5")
+    add_versions("v2023.02.15", "7a5fb59c0e6ef3bef3e8b36d62e138d31e7f2a9f1bdfe95a8e96512b207f84c5")
+
+    add_deps("cmake")
 
     on_install(function (package)
-        print(os.filedirs("*"))
-        os.cp("include", package:installdir())
+        io.replace("cmake/config/eve-install.cmake", [[set(MAIN_DEST     "${CMAKE_INSTALL_LIBDIR}/eve-${PROJECT_VERSION}")]], [[set(MAIN_DEST     "${CMAKE_INSTALL_LIBDIR}/eve")]], {plain = true})
+        io.replace("cmake/config/eve-install.cmake", [[set(INSTALL_DEST  "${CMAKE_INSTALL_INCLUDEDIR}/eve-${PROJECT_VERSION}")]], [[set(INSTALL_DEST  "${CMAKE_INSTALL_INCLUDEDIR}")]], {plain = true})
+        io.replace("cmake/config/eve-install.cmake", [[set(DOC_DEST      "${CMAKE_INSTALL_DOCDIR}-${PROJECT_VERSION}")]], [[set(DOC_DEST      "${CMAKE_INSTALL_DOCDIR}")]], {plain = true})
+
+        local configs = {"-DEVE_BUILD_TEST=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
