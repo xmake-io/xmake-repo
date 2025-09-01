963c04becea75aa1df5c0abb2749d0c5332a36ac
choyy <68107073+choyy@users.noreply.github.com>
2025-09-01 15:39:33 +0800

    libminc: fix cmake (#8022)

--- packages/l/libminc/xmake.lua
+++ packages/l/libminc/xmake.lua
@@ -6,7 +6,7 @@ package("libminc")
              "https://github.com/BIC-MNI/libminc.git")
     add_versions("2.4.06", "2d8b01e67322507000dbfa3e46fa38e12d5c72eb535b169a33a6c5c53202cf90")
 
-    add_patches("2.4.06", "patches/2.4.06/libminc.patch", "56205cec43d1e7d10664ff45df2fcdd50102f97b8c6ad60ca0d541957a05626f")
+    add_patches("2.4.06", "patches/2.4.06/libminc.patch", "80f00f422f9a59cd6627879c1c51d62a6b1bd379049fe3d86a2663bf7c60b0fb")
 
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
@@ -18,7 +18,6 @@ package("libminc")
     end
 
     on_install("windows", "linux", "macosx", "bsd", function (package)
-        io.replace("CMakeLists.txt", "${HDF5_LIBRARY}", "hdf5::hdf5-" .. (package:dep("hdf5"):config("shared") and "shared" or "static"), {plain = true})
         local configs = {
             "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
             "-DLIBMINC_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
