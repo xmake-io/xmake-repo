37cd8c26cd85dbbddf58f69b5a072d52b96738c6
star9029 <hengxings783@gmail.com>
2025-09-02 10:52:07 +0800

    fix

--- packages/l/libdatachannel/xmake.lua
+++ packages/l/libdatachannel/xmake.lua
@@ -1,63 +1,91 @@
 package("libdatachannel")
-    add_deps("cmake", "openssl")
-    add_urls("https://github.com/paullouisageneau/libdatachannel.git", {submodules = true})
-    add_versions("v0.23.1", "222529eb2c8ae44f96462504ae38023f62809cec")
+    set_homepage("https://libdatachannel.org/")
+    set_description("C/C++ WebRTC network library featuring Data Channels, Media Transport, and WebSockets")
+    set_license("MPL-2.0")
 
-    add_configs("use_gnutls", {description = "Use GnuTLS instead of OpenSSL", default = false, type = "boolean"})
-    add_configs("use_mbedtls", {description = "Use Mbed TLS instead of OpenSSL", default = false, type = "boolean"})
-    add_configs("use_nice", {description = "Use libnice instead of libjuice", default = false, type = "boolean"})
-    add_configs("prefer_system_lib", {description = "Prefer system libraries over submodules", default = false, type = "boolean"})
-    add_configs("use_system_srtp", {description = "Use system libSRTP", default = false, type = "boolean"})
-    add_configs("use_system_juice", {description = "Use system libjuice", default = false, type = "boolean"})
-    add_configs("use_system_usrsctp", {description = "Use system libusrsctp", default = false, type = "boolean"})
-    add_configs("use_system_plog", {description = "Use system Plog", default = false, type = "boolean"})
-    add_configs("use_system_json", {description = "Use system Nlohmann JSON", default = false, type = "boolean"})
-    add_configs("no_websocket", {description = "Disable WebSocket support", default = false, type = "boolean"})
-    add_configs("no_media", {description = "Disable media transport support", default = false, type = "boolean"})
-    add_configs("no_examples", {description = "Disable examples", default = false, type = "boolean"})
-    add_configs("no_tests", {description = "Disable tests build", default = false, type = "boolean"})
-    add_configs("warnings_as_errors", {description = "Treat warnings as errors", default = false, type = "boolean"})
+    add_urls("https://github.com/paullouisageneau/libdatachannel/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/paullouisageneau/libdatachannel.git", {submodules = false})
+
+    add_versions("v0.23.1", "63e14d619ac4d9cc310a0c7620b80e6da88abf878f27ccc78cd099f95d47b121")
+
+    add_configs("gnutls", {description = "Use GnuTLS instead of OpenSSL", default = false, type = "boolean"})
+    add_configs("mbedtls", {description = "Use Mbed TLS instead of OpenSSL", default = false, type = "boolean"})
+    add_configs("nice", {description = "Use libnice instead of libjuice", default = false, type = "boolean", readonly = true})
+    add_configs("websocket", {description = "Enable WebSocket support", default = false, type = "boolean"})
+    add_configs("media", {description = "Enable media transport support", default = false, type = "boolean"})
     add_configs("capi_stdcall", {description = "Set calling convention of C API callbacks stdcall", default = false, type = "boolean"})
     add_configs("sctp_debug", {description = "Enable SCTP debugging output to verbose log", default = false, type = "boolean"})
     add_configs("rtc_update_version_header", {description = "Enable updating the version header", default = false, type = "boolean"})
 
+    if is_plat("windows", "mingw") then
+        add_syslinks("ws2_32")
+    elseif is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
+
+    add_deps("cmake")
+    add_deps("plog", "usrsctp", "libjuice")
+    add_deps("nlohmann_json", {configs = {cmake = true}})
+
+    on_check("wasm", function (target)
+        raise("package(libdatachannel) dep(usrsctp) unsupported wasm platform")
+    end)
+
+    on_load(function (package)
+        if package:config("mbedtls") then
+            package:add("deps", "mbedtls")
+        else
+            package:add("deps", "openssl3")
+        end
+        if package:config("media") then
+            package:add("deps", "srtp")
+        else
+            package:add("defines", "RTC_ENABLE_MEDIA=0")
+        end
+
+        if package:config("capi_stdcall") then
+            package:add("defines", "CAPI_STDCALL")
+        end
+
+        package:add("defines", "RTC_ENABLE_WEBSOCKET=" .. (package:config("websocket") and "1" or "0"))
+
+        if not package:config("shared") then 
+            package:add("defines", "RTC_STATIC")
+        end 
+    end)
 
     on_install(function (package)
-         if package:is_plat("windows") and not package:config("shared") then 
-            package:add("defines", "RTC_STATIC") 
-        end 
-
-        local configs = {}
+        local configs = {
+            "-DNO_EXAMPLES=ON",
+            "-DNO_TESTS=ON",
+            "-DWARNINGS_AS_ERRORS=OFF",
+            "-DPREFER_SYSTEM_LIB=ON",
+        }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
 
-        table.insert(configs, "-DUSE_GNUTLS=" .. (package:config("use_gnutls") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_MBEDTLS=" .. (package:config("use_mbedtls") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_NICE=" .. (package:config("use_nice") and "ON" or "OFF"))
-        table.insert(configs, "-DPREFER_SYSTEM_LIB=" .. (package:config("prefer_system_lib") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_SYSTEM_SRTP=" .. (package:config("use_system_srtp") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_SYSTEM_JUICE=" .. (package:config("use_system_juice") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_SYSTEM_USRSCTP=" .. (package:config("use_system_usrsctp") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_SYSTEM_PLOG=" .. (package:config("use_system_plog") and "ON" or "OFF"))
-        table.insert(configs, "-DUSE_SYSTEM_JSON=" .. (package:config("use_system_json") and "ON" or "OFF"))
-        table.insert(configs, "-DNO_WEBSOCKET=" .. (package:config("no_websocket") and "ON" or "OFF"))
-        table.insert(configs, "-DNO_MEDIA=" .. (package:config("no_media") and "ON" or "OFF"))
-        table.insert(configs, "-DNO_EXAMPLES=" .. (package:config("no_examples") and "ON" or "OFF"))
-        table.insert(configs, "-DNO_TESTS=" .. (package:config("no_tests") and "ON" or "OFF"))
-        table.insert(configs, "-DWARNINGS_AS_ERRORS=" .. (package:config("warnings_as_errors") and "ON" or "OFF"))
+        local openssl = package:dep("openssl3")
+        if openssl and not openssl:is_system() then
+            table.insert(configs, "-DOPENSSL_USE_STATIC_LIBS=" .. (not openssl:config("shared") and "ON" or "OFF"))
+            table.insert(configs, "-DOPENSSL_ROOT_DIR=" .. openssl:installdir())
+        end
+
+        table.insert(configs, "-DUSE_GNUTLS=" .. (package:config("gnutls") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_MBEDTLS=" .. (package:config("mbedtls") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_NICE=" .. (package:config("nice") and "ON" or "OFF"))
+        table.insert(configs, "-DNO_WEBSOCKET=" .. (not package:config("websocket") and "ON" or "OFF"))
+        table.insert(configs, "-DNO_MEDIA=" .. (not package:config("media") and "ON" or "OFF"))
         table.insert(configs, "-DCAPI_STDCALL=" .. (package:config("capi_stdcall") and "ON" or "OFF"))
         table.insert(configs, "-DSCTP_DEBUG=" .. (package:config("sctp_debug") and "ON" or "OFF"))
         table.insert(configs, "-DRTC_UPDATE_VERSION_HEADER=" .. (package:config("rtc_update_version_header") and "ON" or "OFF"))
-        
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DOPENSSL_USE_STATIC_LIBS=ON")
-        table.insert(configs, "-DNO_EXAMPLES=ON")
+
         import("package.tools.cmake").install(package, configs, {
             targets = {
                 package:config("shared") and "datachannel" or "datachannel-static",
             }
         })
     end)
+
     on_test(function (package)
         assert(package:has_cfuncs("rtcSetUserPointer", {includes = "rtc/rtc.h"}))
     end)
-package_end()
