a99a332b91672263d227113d34849e4cff806cd3
Saikari <lin@sz.cn.eu.org>
2025-09-02 20:13:31 +0300

    gte: add new package

---
+++ packages/g/gte/port/xmake.lua
@@ -0,0 +1,80 @@
+add_rules("mode.debug", "mode.release")
+set_languages("c++14")
+
+target("gtgraphics")
+    add_rules("utils.install.cmake_importfiles")
+    set_kind("$(kind)")
+
+    add_includedirs("GTE", {public = true})
+    add_headerfiles("(GTE/Graphics/*.h)")
+    add_files("GTE/Graphics/*.cpp")
+
+    if is_plat("windows", "mingw") then
+        add_defines("UNICODE", "_UNICODE", {public = true})
+        set_pcxxheader("GTE/Graphics/GTGraphicsPCH.h")
+        add_defines("GTE_USE_MSWINDOWS", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_DIRECTX")
+        remove_headerfiles("GTE/Graphics/GL46/*.h")
+        add_files("GTE/Graphics/DX11/*.cpp")
+    else
+        add_defines("GTE_USE_LINUX", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_OPENGL", "GTE_DISABLE_PCH")
+        remove_headerfiles("GTE/Graphics/DX11/*.h")
+        add_files("GTE/Graphics/GL46/*.cpp")
+    end
+
+    if is_plat("windows") and is_kind("shared") then
+        add_rules("utils.symbols.export_all", {export_classes = true})
+    end
+
+target("gtapplications")
+    add_rules("utils.install.cmake_importfiles")
+    set_kind("$(kind)")
+
+    add_includedirs("GTE", {public = true})
+    add_headerfiles("(GTE/Applications/**.h)")
+    add_files("GTE/Applications/*.cpp")
+
+    if is_plat("windows", "mingw") then
+        add_defines("UNICODE", "_UNICODE", {public = true})
+        set_pcxxheader("GTE/Applications/GTApplicationsPCH.h")
+        add_defines("GTE_USE_MSWINDOWS", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_DIRECTX")
+        remove_headerfiles("GTE/Graphics/GLX/*.h")
+        add_files("GTE/Graphics/MSW/*.cpp")
+    else
+        add_defines("GTE_USE_LINUX", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_OPENGL", "GTE_DISABLE_PCH")
+        remove_headerfiles("GTE/Graphics/MSW/*.h")
+        add_files("GTE/Graphics/GLX/*.cpp")
+    end
+
+    if is_plat("windows") and is_kind("shared") then
+        add_rules("utils.symbols.export_all", {export_classes = true})
+    end
+
+target("gtmathematicsgpu")
+    add_rules("utils.install.cmake_importfiles")
+    set_kind("$(kind)")
+
+    add_includedirs("GTE", {public = true})
+    add_headerfiles("(GTE/MathematicsGPU/*.h)")
+    add_files("GTE/MathematicsGPU/*.cpp")
+
+    if is_plat("windows", "mingw") then
+        add_defines("UNICODE", "_UNICODE", {public = true})
+        add_defines("GTE_USE_MSWINDOWS", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_DIRECTX")
+        set_pcxxheader("GTE/MathematicsGPU/GTMathematicsGPUPCH.h")
+    else
+        add_defines("GTE_USE_LINUX", "GTE_USE_ROW_MAJOR", "GTE_USE_MAT_VEC", "GTE_USE_OPENGL", "GTE_DISABLE_PCH")
+    end
+
+    if is_plat("windows") and is_kind("shared") then
+        add_rules("utils.symbols.export_all", {export_classes = true})
+    end
+
+target("gtmathematics")
+    add_rules("utils.install.cmake_importfiles")
+    set_kind("headeronly")
+
+    if is_plat("windows", "mingw") then
+        add_defines("UNICODE", "_UNICODE", {public = true})
+    end    
+    add_includedirs("GTE", {public = true})
+    add_headerfiles("(GTE/Mathematics/*.h)")
---
+++ packages/g/gte/xmake.lua
@@ -0,0 +1,34 @@
+package("gte")
+    set_homepage("https://github.com/davideberly/GeometricTools")
+    set_description("A collection of source code for computing in the fields of mathematics, geometry, graphics, image analysis and physics.")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/davideberly/GeometricTools.git")
+    add_versions("2025.08.20", "1ed0582d307b8608eb1b741d067f657e09483a5e")
+
+    add_includedirs("include", "include/GTE")
+
+    on_install(function (package)
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <memory>
+            #include <GTE/MathematicsGPU/GTMathematicsGPU.h>
+            void test() {
+                std::shared_ptr<gte::GraphicsEngine> engine;
+                std::shared_ptr<gte::ProgramFactory> factory;
+
+                int32_t xSize = 128;
+                int32_t ySize = 128;
+                float dt = 0.016f;
+                float densityViscosity = 0.001f;
+                float velocityViscosity = 0.001f;
+
+                gte::GPUFluid2 fluid(engine, factory, xSize, ySize, dt, densityViscosity, velocityViscosity);
+                fluid.Initialize();
+            }
+        ]]}, {configs = {languages = "c++14"}}))
+    end)
