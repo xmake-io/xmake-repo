94044d91c9ba665f3675c7f2fa0e211d3166de8d
Saikari <lin@sz.cn.eu.org>
2025-09-04 16:15:54 +0300

    Update xmake.lua

--- packages/h/hiredis-vip/xmake.lua
+++ packages/h/hiredis-vip/xmake.lua
@@ -11,6 +11,9 @@ package("hiredis-vip")
     add_deps("cmake")
 
     on_install("linux", "macosx", "iphoneos", "cross", "bsd", "wasm", "android", function (package)
+        -- Repair FreeBSD
+        io.replace("hiutil.c", [[#include <sys/types.h>]], [[#include <sys/types.h>
+#include <sys/socket.h>]], {plain = true})
         -- GCC15 workaround
         io.replace("command.c", [[#include "hiarray.h"]], [[#include "hiarray.h"
 #include <stdlib.h>]], {plain = true})
