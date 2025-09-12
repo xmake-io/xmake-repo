b8c2b884f243ef9ba9a3d8bffe9dd8fee6e14d8e
choyy <68107073+choyy@users.noreply.github.com>
2025-09-12 21:50:15 +0800

    hdf5: fix cmake (#8100)
    * hdf5: fix cmake
    
    * update
    
    * Update cmake.patch
    
    * update hash

--- packages/h/hdf5/xmake.lua
+++ packages/h/hdf5/xmake.lua
@@ -22,6 +22,8 @@ package("hdf5")
     add_versions("github:1.14.4-3", "019ac451d9e1cf89c0482ba2a06f07a46166caf23f60fea5ef3c37724a318e03")
     add_versions("github:1.14.6", "e4defbac30f50d64e1556374aa49e574417c9e72c6b1de7a4ff88c4b1bea6e9b")
 
+    add_patches(">1.10", "patch/cmake.patch", "d919e05ca15144ddf7a3c48586080860595ce08cb78411fd51e68056d4a5d366")
+
     add_configs("zlib", {description = "Enable Zlib Filters", default = false, type = "boolean"})
     add_configs("szip", {description = "Enable Szip Filters", default = false, type = "boolean"})
     add_configs("cpplib", {description = "Build HDF5 C++ Library", default = false, type = "boolean"})
