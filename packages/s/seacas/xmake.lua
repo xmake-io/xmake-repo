7b99301f9376e67c2d971971cac138139e1c49b7
chooyy <choo-yy@qq.com>
2025-09-15 13:33:56 +0800

    semver

--- packages/s/seacas/xmake.lua
+++ packages/s/seacas/xmake.lua
@@ -3,9 +3,11 @@ package("seacas")
     set_description("The Sandia Engineering Analysis Code Access System (SEACAS) is a suite of preprocessing, postprocessing, translation, and utility applications supporting finite element analysis software using the Exodus database file format.")
     set_license("BSD-3-Clause")
 
-    add_urls("https://github.com/sandialabs/seacas/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/sandialabs/seacas.git")
-    add_versions("v2025-08-28", "29125a84859c78b6bb0b5909ce7443aa2774235f0fc75dedf467a223603e0ffd")
+    add_urls("https://github.com/sandialabs/seacas.git")
+    add_urls("https://github.com/sandialabs/seacas/archive/refs/tags/$(version).tar.gz", {version = function (version) 
+        return "v" .. version:gsub("%.", "-")
+    end})
+    add_versions("2025.08.28", "29125a84859c78b6bb0b5909ce7443aa2774235f0fc75dedf467a223603e0ffd")
 
     add_deps("cmake")
     add_deps("fmt", "hdf5", "netcdf-c")
@@ -14,7 +16,7 @@ package("seacas")
 
     on_load(function (package)
         if package:is_plat("windows") and package:config("shared") then
-            package:add("patches", "v2025-08-28", "patches/v2025-08-28/windows_shared.patch", "286681457a359a1f498087b72f221c01ec5d51f46bf13b13c1a8c0211bebe766")
+            package:add("patches", "2025.08.28", "patches/2025.08.28/windows_shared.patch", "286681457a359a1f498087b72f221c01ec5d51f46bf13b13c1a8c0211bebe766")
         end
     end)
 
