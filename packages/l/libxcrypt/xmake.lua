5411186e6fcb4d530082c06553343c74bd27b802
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-15 15:48:44 +0800

    update.

--- packages/l/libxcrypt/xmake.lua
+++ packages/l/libxcrypt/xmake.lua
@@ -7,7 +7,6 @@ package("libxcrypt")
     add_versions("4.4.38", "80304b9c306ea799327f01d9a7549bdb28317789182631f1b54f4511b4206dd6")
 
     add_configs("largefile",           {description = "Enable support for large files.", default = true, type = "boolean"})
-    add_configs("symvers",             {description = "Enable library symbol versioning. (true => auto)", default = true, type = "boolean"})
     add_configs("failure_tokens",      {description = "Make crypt and crypt_r return NULL on failure.", default = true, type = "boolean"})
     add_configs("xcrypt_compat_files", {description = "Enable installation of compatibility headers/libraries/symlinks.", default = true, type = "boolean"})
     add_configs("obsolete_api",        {description = "Enable all compatibility APIs, or only some.", default = "all", type = "string", values = {"all", "none", "alt", "glibc", "owl", "suse"}})
@@ -15,9 +14,11 @@ package("libxcrypt")
     add_configs("hashes",              {description = "Select the hashing method to enable.", default = "all", type = "string"})
     add_configs("year2038",            {description = "Enable support for timestamps after 2038.", default = true, type = "boolean"})
 
-    on_load("mingw", "msys", "cygwin", function (package)
-        package:config_set("symvers", false)
-    end)
+    if is_plat("mingw", "msys", "cygwin") then
+        add_configs("symvers", {description = "Enable library symbol versioning. (true => auto)", default = false, type = "boolean", readonly = true})
+    else
+        add_configs("symvers", {description = "Enable library symbol versioning. (true => auto)", default = true, type = "boolean"})
+    end
 
     on_install("linux", "bsd", "macosx", "mingw", "msys", "cygwin", function (package)
         local configs = {
@@ -43,10 +44,11 @@ package("libxcrypt")
         table.insert(configs, "--enable-hashes=" .. package:config("hashes"))
 
         if package:is_plat("mingw", "msys", "cygwin") then
-            io.replace("configure", "-Wpedantic", "", {plain = true})
             io.replace("Makefile.in", "libcrypt_la_LDFLAGS = -version-info", "libcrypt_la_LDFLAGS = -no-undefined -version-info", {plain = true})
-            table.insert(configs, "--enable-werror=no")
         end
+
+        io.replace("configure", "-Wpedantic", "", {plain = true})
+        table.insert(configs, "--enable-werror=no")
 
         import("package.tools.autoconf").install(package, configs)
     end)
