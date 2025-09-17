0f7052e43a69dfc17cd21708e29cdf00edb7dba1
Saikari <lin@sz.cn.eu.org>
2025-09-17 17:26:32 +0300

    fontconfig: update to 2.16.0

--- packages/f/fontconfig/xmake.lua
+++ packages/f/fontconfig/xmake.lua
@@ -1,14 +1,15 @@
 package("fontconfig")
-
     set_homepage("https://www.freedesktop.org/wiki/Software/fontconfig/")
     set_description("A library for configuring and customizing font access.")
 
     set_urls("https://www.freedesktop.org/software/fontconfig/release/fontconfig-$(version).tar.gz")
+    set_urls("https://www.freedesktop.org/software/fontconfig/release/fontconfig-$(version).tar.xz", {alias = "xz"})
     add_versions("2.13.1", "9f0d852b39d75fc655f9f53850eb32555394f36104a044bb2b2fc9e66dbbfa7f")
     add_versions("2.13.93", "0f302a18ee52dde0793fe38b266bf269dfe6e0c0ae140e30d72c6cca5dc08db5")
     add_versions("2.13.94", "246d1640a7e54fba697b28e4445f4d9eb63dda1b511d19986249368ee7191882")
     add_versions("2.14.0", "b8f607d556e8257da2f3616b4d704be30fd73bd71e367355ca78963f9a7f0434")
     add_versions("2.14.2", "3ba2dd92158718acec5caaf1a716043b5aa055c27b081d914af3ccb40dce8a55")
+    add_versions("xz:2.16.0", "6a33dc555cc9ba8b10caf7695878ef134eeb36d0af366041f639b1da9b6ed220")
 
     -- fix the build issue with --enable-static
     add_patches("2.13.1", "https://gitlab.freedesktop.org/fontconfig/fontconfig/commit/8208f99fa1676c42bfd8d74de3e9dac5366c150c.diff",
