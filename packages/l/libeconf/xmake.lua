52f0b5a5d95e4d39118ec069079303af3911cb4f
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-20 21:20:43 +0800

    libeconf: add package.

---
+++ packages/l/libeconf/xmake.lua
@@ -0,0 +1,21 @@
+package("libeconf")
+    set_homepage("https://github.com/openSUSE/libeconf")
+    set_description("Pluggable Authentication Modules for Linux")
+    set_license("MIT")
+
+    add_urls("https://github.com/openSUSE/libeconf/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/openSUSE/libeconf.git")
+
+    add_versions("v0.7.10", "e8fee300cbbae11287d2682d185d946a1ffbd23bf02b4f97d68f2df34d8de07f")
+
+    add_deps("meson", "ninja")
+    on_install("linux", function (package)
+        io.replace("meson.build", "subdir%b()", "")
+        io.replace("meson.build", "executable%b()", "")
+
+        import("package.tools.meson").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("econf_readFile", {includes = "libeconf.h"}))
+    end)
