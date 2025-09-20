219973523c1766f1c4795a4a462abb8c21727ea0
RedbeanW <redbeana44945@gmail.com>
2025-09-20 21:41:25 +0800

    fix desc, test multiplat.

--- packages/l/libeconf/xmake.lua
+++ packages/l/libeconf/xmake.lua
@@ -1,6 +1,6 @@
 package("libeconf")
     set_homepage("https://github.com/openSUSE/libeconf")
-    set_description("Pluggable Authentication Modules for Linux")
+    set_description("A highly flexible and extensible library for parsing and managing configuration files.")
     set_license("MIT")
 
     add_urls("https://github.com/openSUSE/libeconf/archive/refs/tags/$(version).tar.gz",
@@ -9,7 +9,7 @@ package("libeconf")
     add_versions("v0.7.10", "e8fee300cbbae11287d2682d185d946a1ffbd23bf02b4f97d68f2df34d8de07f")
 
     add_deps("meson", "ninja")
-    on_install("linux", function (package)
+    on_install(function (package)
         io.replace("meson.build", "subdir%b()", "")
         io.replace("meson.build", "executable%b()", "")
 
