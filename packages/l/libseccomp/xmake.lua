404ac2c9e0614c438511ad8f30a4b71b59278bb6
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-21 12:17:45 +0800

    update.

--- packages/l/libseccomp/xmake.lua
+++ packages/l/libseccomp/xmake.lua
@@ -1,12 +1,21 @@
 package("libseccomp")
+    set_homepage("https://github.com/seccomp/libseccomp")
     set_description("The libseccomp library provides an easy to use, platform independent, interface to the Linux Kernel's syscall filtering mechanism.")
+    set_license("LGPL-2.1")
 
-    add_urls("https://github.com/seccomp/libseccomp/releases/download/v$(version)/libseccomp-$(version).tar.gz")
+    add_urls("https://github.com/seccomp/libseccomp/releases/download/v$(version)/libseccomp-$(version).tar.gz",
+             "https://github.com/seccomp/libseccomp.git")
     add_versions("2.6.0", "83b6085232d1588c379dc9b9cae47bb37407cf262e6e74993c61ba72d2a784dc")
 
-    add_configs("tools", {description = "Build tools.", default = true, type = "boolean"})
+    add_configs("tools", {description = "Build tools.", default = false, type = "boolean"})
 
     add_deps("gperf")
+    on_load(function (package)
+        if package:gitref() then
+            package:add("deps", "autotools")
+        end
+    end)
+
     on_install("linux", function (package)
         local configs = {
             "--disable-dependency-tracking",
