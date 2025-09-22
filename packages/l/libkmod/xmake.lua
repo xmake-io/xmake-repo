264f7f067ef9a9a03282b4ac8162f0ef708d2b6c
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-22 12:50:47 +0800

    test autotools.

--- packages/l/libkmod/xmake.lua
+++ packages/l/libkmod/xmake.lua
@@ -3,45 +3,62 @@ package("libkmod")
     set_description("libkmod - Linux kernel module handling")
     set_license("LGPL-2.1")
 
-    add_urls("https://github.com/kmod-project/kmod/archive/refs/tags/v$(version).tar.gz", {alias = "tarball"})
-    add_urls("https://github.com/kmod-project/kmod.git", {alias = "git"})
+    add_urls("https://github.com/kmod-project/kmod/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/kmod-project/kmod.git")
 
-    add_versions("git:33", "v33")
-    add_versions("git:32", "v32")
-    add_versions("git:31", "v31")
-    add_versions("git:30", "v30")
-    add_versions("tarball:33", "c72120a2582ae240221671ddc1aa53ee522764806f50f8bf1522bbf055679985")
-    add_versions("tarball:32", "9477fa096acfcddaa56c74b988045ad94ee0bac22e0c1caa84ba1b7d408da76e")
-    add_versions("tarball:31", "16c40aaa50fc953035b4811b29ce3182f220e95f3c9e5eacb4b07b1abf85f003")
-    add_versions("tarball:30", "1fa3974abd80b992d61324bcc04fa65ea96cfe2e9e1150f48394833030c4b583")
+    add_versions("v34", "cb47be49366b596e4554eeeb7595b128feb261619c7675603e004b07c5ebbd5b")
+    add_versions("v33", "c72120a2582ae240221671ddc1aa53ee522764806f50f8bf1522bbf055679985")
+    add_versions("v32", "9477fa096acfcddaa56c74b988045ad94ee0bac22e0c1caa84ba1b7d408da76e")
+    add_versions("v31", "16c40aaa50fc953035b4811b29ce3182f220e95f3c9e5eacb4b07b1abf85f003")
+    add_versions("v30", "1fa3974abd80b992d61324bcc04fa65ea96cfe2e9e1150f48394833030c4b583")
 
-    add_patches(">=30 <33", path.join(os.scriptdir(), "patches", "31", "basename.patch"), "83d07e169882cc91f3af162912ae97cd4b62ff48876ca83b0317c40a388773ad")
+    -- add_patches(">=30 <33", path.join(os.scriptdir(), "patches", "31", "basename.patch"), "83d07e169882cc91f3af162912ae97cd4b62ff48876ca83b0317c40a388773ad")
 
-    add_configs("zstd", {description = "Enable zstd support.", default = true, type = "boolean"})
-    add_configs("zlib", {description = "Enable zlib support.", default = true, type = "boolean"})
-    add_configs("xz", {description = "Enable xz support.", default = true, type = "boolean"})
+    -- "--enable-static" is not supported by kmod
+    add_configs("shared", {description = "Build shared library", default = true, type = "boolean", readonly = true})
 
-    add_includedirs("include", "include/libkmod")
+    add_configs("tools",     {description = "Build tools.", default = false, type = "boolean"})
+    add_configs("logging",   {description = "Enable system logging.", default = true, type = "boolean"})
+
+    add_configs("zstd",    {description = "Enable Zstandard-compressed modules support.", default = false, type = "boolean"})
+    add_configs("zlib",    {description = "Enable gzipped modules support.", default = false, type = "boolean"})
+    add_configs("xz",      {description = "Enable Xz-compressed modules support.", default = false, type = "boolean"})
+    add_configs("openssl", {description = "Enable PKCS7 signatures support", default = false, values = {false, "openssl", "openssl3"}})
+
+    add_deps("autotools")
     on_load(function (package)
         for _, lib in ipairs({"zstd", "zlib", "xz"}) do
             if package:config(lib) then
                 package:add("deps", lib)
             end
         end
+        local openssl = package:config("openssl")
+        if openssl then
+            package:add("deps", openssl)
+        end
     end)
 
     on_install("linux", "android", function (package)
-        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        os.cp(path.join(package:scriptdir(), "port", "config.h"), "config.h")
-        os.cp(path.join(package:scriptdir(), "port", "endian-darwin.h"), "endian-darwin.h")
-        import("package.tools.xmake").install(package, {
-            zstd = package:config("zstd"),
-            zlib = package:config("zlib"),
-            xz = package:config("xz"),
-            ver = package:version_str()
-        })
+        local configs = {
+            "--disable-dependency-tracking",
+            "--disable-manpages",
+            "--disable-test-modules"
+        }
+
+        table.insert(configs, "--enable-tools=" .. (package:config("tools") and "yes" or "no"))
+        table.insert(configs, "--enable-logging=" .. (package:config("logging") and "yes" or "no"))
+
+        table.insert(configs, "--with-zstd=" .. (package:config("logging") and "yes" or "no"))
+        table.insert(configs, "--with-zlib=" .. (package:config("logging") and "yes" or "no"))
+        table.insert(configs, "--with-xz=" .. (package:config("logging") and "yes" or "no"))
+        table.insert(configs, "--with-openssl=" .. (package:config("logging") and "yes" or "no"))
+
+        io.replace("Makefile.am", [[dist_bashcompletion_DATA = \
+	shell-completion/bash/kmod]], "", {plain = true})
+
+        import("package.tools.autoconf").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("kmod_new", {includes = "libkmod/libkmod.h"}))
+        assert(package:has_cfuncs("kmod_new", {includes = "libkmod.h"}))
     end)
