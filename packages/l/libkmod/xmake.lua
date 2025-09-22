4cb42994a445aac684fe8cba78c58626c658c0c6
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-22 13:51:44 +0800

    disable zlib by default (pkg-config problem)

--- packages/l/libkmod/xmake.lua
+++ packages/l/libkmod/xmake.lua
@@ -6,7 +6,6 @@ package("libkmod")
     add_urls("https://github.com/kmod-project/kmod/archive/refs/tags/$(version).tar.gz",
              "https://github.com/kmod-project/kmod.git")
 
-    add_versions("v34", "cb47be49366b596e4554eeeb7595b128feb261619c7675603e004b07c5ebbd5b")
     add_versions("v33", "c72120a2582ae240221671ddc1aa53ee522764806f50f8bf1522bbf055679985")
     add_versions("v32", "9477fa096acfcddaa56c74b988045ad94ee0bac22e0c1caa84ba1b7d408da76e")
     add_versions("v31", "16c40aaa50fc953035b4811b29ce3182f220e95f3c9e5eacb4b07b1abf85f003")
@@ -19,7 +18,7 @@ package("libkmod")
     add_configs("logging",   {description = "Enable system logging.", default = true, type = "boolean"})
 
     add_configs("zstd",    {description = "Enable Zstandard-compressed modules support.", default = true, type = "boolean"})
-    add_configs("zlib",    {description = "Enable gzipped modules support.", default = true, type = "boolean"})
+    add_configs("zlib",    {description = "Enable gzipped modules support.", default = false, type = "boolean"})
     add_configs("xz",      {description = "Enable Xz-compressed modules support.", default = true, type = "boolean"})
     add_configs("openssl", {description = "Enable PKCS7 signatures support", default = "openssl3", values = {false, "openssl", "openssl3"}})
 
