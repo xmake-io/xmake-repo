0035304cf62dce9389ddcfa1257f94f5434a3b9f
star9029 <hengxings783@gmail.com>
2025-09-22 15:03:25 +0800

    remove test

--- packages/k/kenlm/xmake.lua
+++ packages/k/kenlm/xmake.lua
@@ -17,37 +17,37 @@ package("kenlm")
     end
 
     add_deps("cmake")
-    add_deps("boost", {configs = {program_options = true, system = true, thread = true, test = true}})
+    add_deps("boost", {configs = {program_options = true, system = true, thread = true}})
     add_deps("zlib", "bzip2", "xz")
+
     add_includedirs("include/kenlm")
+
     on_load(function (package)
         package:add("defines", "KENLM_MAX_ORDER=" .. tostring(package:config("max_order")))
         package:add("links", "kenlm_builder", "kenlm", "kenlm_filter", "kenlm_util")
     end)
 
     on_install("!iphoneos and !wasm", function (package)
-        local configs = {
-            "-DBUILD_TESTING=OFF"
-        }
+        io.replace("CMakeLists.txt", "unit_test_framework", "", {plain = true})
+        io.replace("CMakeLists.txt", "if (WIN32)\n    set(Boost", "if (0)\nset(Boost", {plain = true})
+        io.replace("cmake/KenLMFunctions.cmake", "function(AddExes)", "function(AddExes)\nreturn()", {plain = true})
+
+        local configs = {"-DBUILD_TESTING=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DKENLM_MAX_ORDER=" .. tostring(package:config("max_order")))
 
-        local options = { cxflags = {} }
+        local opt = { cxflags = {} }
         if package:is_plat("windows") then
-            table.insert(options.cxflags, "-DNOMINMAX")
+            table.insert(opt.cxflags, "-DNOMINMAX")
             if package:has_tool("cxx", "cl", "clang_cl") then
-                table.insert(options.cxflags, "/EHsc")
+                table.insert(opt.cxflags, "/EHsc")
             end
         end
         if not package:dep("xz"):config("shared") then
-            table.insert(options.cxflags, "-DLZMA_API_STATIC")
+            table.insert(opt.cxflags, "-DLZMA_API_STATIC")
         end
-
-        io.replace("CMakeLists.txt", "if (WIN32)\n    set(Boost", "if (0)\nset(Boost", {plain = true})
-        io.replace("cmake/KenLMFunctions.cmake", "function(AddExes)", "function(AddExes)\nreturn()", {plain = true})
-
-        import("package.tools.cmake").install(package, configs, options)
+        import("package.tools.cmake").install(package, configs, opt)
     end)
 
     on_test(function (package)
