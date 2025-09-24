a1aa63dcb9b635afad9b144673d4e825b1f86798
Casinius <749929621@qq.com>
2025-09-24 20:35:22 +0800

    merge plats & fixing cmake param

--- packages/m/manifold/xmake.lua
+++ packages/m/manifold/xmake.lua
@@ -8,89 +8,87 @@ package("manifold")
     add_versions("3.2.1","67c4e0cb836f9d6dfcb7169e9d19a7bb922c4d4bfa1a9de9ecbc5d414018d6ad")
     
     add_configs("jsbind", {description = "Enable js binding", default = false, type = "boolean"})
-    add_configs("cbind", {description = "Enable c binding", default = true, type = "boolean"})
+    add_configs("cbind", {description = "Enable c binding", default = true, type = "boolean"}) --requires no deps
     add_configs("pybind", {description = "Enable python binding", default = false, type = "boolean"})
     
-    add_configs("parellel", {description = "Enable parellel processing", default = true, type = "boolean"}) --不开启的话你为什么不去用libigl呢 ：)
+    add_configs("parallel", {description = "Enable parallel processing", default = true, type = "boolean"}) -- disable it if you don't have the cpu/soc that support simd
     add_configs("clipper2_feature", {description = "Enable 2d simple operation", default = true, type = "boolean"})
+    
+    
     add_configs("exporter", {description = "Enable exporting models", default = true, type = "boolean"})
     
-    add_configs("debug", {description = "Enable debug", default = false, type = "boolean"})
-    add_configs("assert", {description = "Enable debug assert", default = false, type = "boolean"}) --只能和DEBUG一起开！！
+
+   
     
     add_configs("test", {description = "Enable test", default = false, type = "boolean"})
-    add_configs("tracy", {description = "Enable profiling", default = false, type = "boolean"})
+    add_configs("tracy", {description = "Enable profiling", default = false, type = "boolean"}) --for profiling,should be disabled by default
     
     add_deps("cmake") --necessary for cmake project
-    
-        
-    on_install("windows", function (package)
-         local configs = {}
+    add_deps("gtest")
 
-        local cmake_args = table.join({" -DCMAKE_INSTALL_PREFIX=" .. package:installdir()},package:config("cmake_args"))
-        local cachedir = package:cachedir()
-        table.insert(configs,cmake_args)
-        table.insert(configs,"-DMANIFOLD_DEBUG=OFF") --官方建议的我也不知道为什么一定要关闭
-        
-        table.insert(configs,"-DMANIFOLD_JSBIND" .. (package:config("jsbind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_CBIND" .. (package:config("cbind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_PYBIND" .. (package:config("pybind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_PAR" .. (package:config("parellel") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_CROSS_SECTION" .. (package:config("clipper2_feature") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_EXPORT" .. (package:config("exporter") and "ON" or "OFF"))
-
-        table.insert(configs,"-DMANIFOLD_ASSERT" .. (package:config("assert") and "ON" or "OFF"))
-        
-        table.insert(configs,"-DMANIFOLD_TEST" .. (package:config("test") and "ON" or "OFF"))
-        table.insert(configs,"-DTRACY_ENABLE" .. (package:config("tracy") and "ON" or "OFF"))
-        
-        table.insert(configs,"-DMANIFOLD_USE_BUILTIN_TBB=ON")
-        table.insert(configs,"-DMANIFOLD_USE_BUILTIN_CLIPPER2=ON")  --clipper2需要依赖他的版本 :( 不然不方便
-        table.insert(configs,"-DMANIFOLD_USE_BUILTIN_NANOBIND=ON") 
-        import("package.tools.cmake").install(package, configs)
-        
-
-
-
-
-
-
-        import("package.tools.cmake").install(package, configs)
-
-        
-    end)
-
-
-    on_install("linux", "macosx", function (package)
+    on_install("linux", "macosx","windows", function (package)
         local configs = {}
 
-        local cmake_args = table.join({" -DCMAKE_INSTALL_PREFIX=" .. package:installdir()},package:config("cmake_args"))
+        table.insert(configs," -DCMAKE_INSTALL_PREFIX=" .. package:installdir())
+        
+        
+        if package:config("cmake_args") then
+            table.join2(configs, package:config("cmake_args"))
+        end --this allows user to add more option
+            
+        table.insert(configs,"-DMANIFOLD_JSBIND=" .. (package:config("jsbind") and "ON" or "OFF"))
+        table.insert(configs,"-DMANIFOLD_CBIND=" .. (package:config("cbind") and "ON" or "OFF"))
+        table.insert(configs,"-DMANIFOLD_PYBIND=" .. (package:config("pybind") and "ON" or "OFF"))
+        table.insert(configs,"-DMANIFOLD_PAR=" .. (package:config("parallel") and "ON" or "OFF"))
+        table.insert(configs,"-DMANIFOLD_CROSS_SECTION=" .. (package:config("clipper2_feature") and "ON" or "OFF"))
+        table.insert(configs,"-DMANIFOLD_EXPORT=" .. (package:config("exporter") and "ON" or "OFF"))
+        
+        
+        
 
-        table.insert(configs,"-DMANIFOLD_JSBIND" .. (package:config("jsbind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_CBIND" .. (package:config("cbind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_PYBIND" .. (package:config("pybind") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_PAR" .. (package:config("parellel") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_CROSS_SECTION" .. (package:config("clipper2_feature") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_EXPORT" .. (package:config("exporter") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_DEBUG" .. (package:config("debug") and "ON" or "OFF"))
-        table.insert(configs,"-DMANIFOLD_ASSERT" .. (package:config("assert") and "ON" or "OFF"))
+        
+        
+        table.insert(configs,"-DMANIFOLD_TEST=" .. (package:config("test") and "ON" or "OFF"))
+        table.insert(configs,"-DTRACY_ENABLE=" .. (package:config("tracy") and "ON" or "OFF"))
+        
+        if(os.host() == "windows")
+            then
+                table.insert(configs,"--DBUILD_SHARED_LIBS=OFF")   --according to author,shared lib may cause problem on windows platform 
+            end
+            
+            
+        table.insert(configs,"-DMANIFOLD_DEBUG=" .. (package:is_debug() and "ON" or "OFF"))
 
-        table.insert(configs,"-DMANIFOLD_TEST" .. (package:config("test") and "ON" or "OFF"))
-        table.insert(configs,"-DTRACY_ENABLE" .. (package:config("tracy") and "ON" or "OFF"))
+            
         
+        if(package:is_debug() == true)  --according to author,assertion may only be enabled when debug is enabled
+            then
+                table.insert(configs,"-DMANIFOLD_ASSERT=ON")
+            else
+                table.insert(configs,"-DMANIFOLD_ASSERT=OFF")
+            end
         
-        
-        table.insert(configs,cmake_args)
+        --this requires network connections
         table.insert(configs,"-DMANIFOLD_USE_BUILTIN_TBB=ON")
         table.insert(configs,"-DMANIFOLD_USE_BUILTIN_CLIPPER2=ON")
         table.insert(configs,"-DMANIFOLD_USE_BUILTIN_NANOBIND=ON")
+        
+        
+        
+        
+        
+        
         import("package.tools.cmake").install(package, configs)
 
 
     end)
 
     on_test(function (package)
-        import("lib.detect.find_file")
-        local file = find_file("libmanifold.a", {package:installdir()})
-        assert(file == nil)
+        assert(package:check_cxxsnippets({test = [[
+            #include "manifold.h"
+            void test() {
+                manifold::Manifold cube = manifold::Manifold::Cube({1, 1, 1});
+                (void)cube;
+            }
+        ]]}, {configs = {languages = "c++17"}}))
     end)
