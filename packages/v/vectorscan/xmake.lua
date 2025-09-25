ab5af60de7c15f3fbe9916d98e620485f45e16ad
Dominik Kaszewski <dakaszewski@gmail.com>
2025-09-25 20:20:16 +0200

    Fix boost dependency config

--- packages/v/vectorscan/xmake.lua
+++ packages/v/vectorscan/xmake.lua
@@ -9,9 +9,16 @@ package("vectorscan")
     add_versions("github:5.4.12", "vectorscan/5.4.12")
     add_versions("archive:5.4.12", "1ac4f3c038ac163973f107ac4423a6b246b181ffd97fdd371696b2517ec9b3ed")
 
-    add_deps("cmake", "boost", "ragel", "python")
+    add_deps("cmake", "ragel", "python")
+    add_deps("boost", {configs = {
+        exception = true,
+        container = true,
+        thread = true,
+        graph = true
+    }})
 
-    on_install("linux", "windows", function (package)
+    -- currently no ragel package for ARM Windows
+    on_install("linux", "windows|!arm", function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
