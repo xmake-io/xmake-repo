da3c0a2b1ec5d819069282ff7e4a2a00e096f90f
Dominik Kaszewski <dakaszewski@gmail.com>
2025-09-25 20:53:47 +0200

    Disable broken debug mode

--- packages/v/vectorscan/xmake.lua
+++ packages/v/vectorscan/xmake.lua
@@ -10,17 +10,13 @@ package("vectorscan")
     add_versions("archive:5.4.12", "1ac4f3c038ac163973f107ac4423a6b246b181ffd97fdd371696b2517ec9b3ed")
 
     add_deps("cmake", "ragel", "python")
-    add_deps("boost", {configs = {
-        exception = true,
-        container = true,
-        thread = true,
-        graph = true
-    }})
+    add_deps("boost", {configs = {thread = true, graph = true}})
 
-    -- currently no ragel package for ARM Windows
+    -- Currently no ragel package for ARM Windows
     on_install("linux", "windows|!arm", function (package)
         local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        -- Debug build currently fails
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
