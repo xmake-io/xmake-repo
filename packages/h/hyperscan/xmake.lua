bd8ea4fea3d5ac73aa7d0ebee17d5b569fee4c3b
star9029 <hengxings783@gmail.com>
2025-09-26 06:06:36 +0800

    fix arch

--- packages/h/hyperscan/xmake.lua
+++ packages/h/hyperscan/xmake.lua
@@ -5,14 +5,25 @@ package("hyperscan")
 
     add_urls("https://github.com/intel/hyperscan/archive/refs/tags/$(version).tar.gz",
              "https://github.com/intel/hyperscan.git")
+
     add_versions("v5.4.2", "32b0f24b3113bbc46b6bfaa05cf7cf45840b6b59333d078cc1f624e4c40b2b99")
 
-    add_deps("cmake", "ragel", "python")
+    add_deps("cmake", "ragel", "python 3.x", {kind = "binary"})
     add_deps("boost", {configs = {thread = true, graph = true}})
 
-    on_install("linux", "windows|!arm*", function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+    on_check(function (package)
+        if not package:is_arch("x64", "x86", "x86_64") then
+            raise("package(hyperscan) only support x86 arch")
+        end
+    end)
+
+    on_install(function (package)
+        if not package:config("pic") then
+            io.replace("CMakeLists.txt", "POSITION_INDEPENDENT_CODE TRUE", "POSITION_INDEPENDENT_CODE FALSE", {plain = true})
+        end
+
+        local configs = {"-DBUILD_EXAMPLES=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
