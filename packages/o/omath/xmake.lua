aef4afdd5df7c1d7558e1c744eb9b67c520b5058
Saikari <lin@sz.cn.eu.org>
2025-09-30 23:23:08 +0300

    fixup

--- packages/o/omath/xmake.lua
+++ packages/o/omath/xmake.lua
@@ -8,7 +8,7 @@ package("omath")
 
     add_versions("v3.8.1", "aaea99570c382478f825af759a2b0a214b429c74a9a5492ddd2866c836e85f4e")
 
-    add_patches("v3.8.1", "patches/v3.8.1/fix-build.patch", "060cacbe12423e87dc3195d37f3f548ed1639ceb38687ef48ae1c6c1a7152d83")
+    add_patches("v3.8.1", "patches/v3.8.1/fix-build.patch", "4ad392f63b432ba7a713fc79926d90efed0f9841186bd3df433d5610ff2b221f")
 
     if not is_arch("arm.*") then
         add_configs("avx2",  {description = "Enable AVX2", default = true, type = "boolean"})
