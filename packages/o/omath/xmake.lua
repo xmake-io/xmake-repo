434e363db89665aa7f5c44e35c7db776f045ed73
Saikari <lin@sz.cn.eu.org>
2025-09-30 15:42:41 +0300

    omath:update to 3.8.1

--- packages/o/omath/xmake.lua
+++ packages/o/omath/xmake.lua
@@ -6,17 +6,16 @@ package("omath")
     add_urls("https://github.com/orange-cpp/omath/archive/refs/tags/$(version).tar.gz",
              "https://github.com/orange-cpp/omath.git", {submodules = false})
 
-    add_versions("v3.0.3", "f72ec671eb99d83bf6d63ec5eee7436110a9f340b416eefac51464665bbda06c")
+    add_versions("v3.8.1", "aaea99570c382478f825af759a2b0a214b429c74a9a5492ddd2866c836e85f4e")
 
-    add_configs("avx2",  {description = "Enable AVX2", default = true, type = "boolean"})
+    add_patches("v3.8.1", "patches/v3.8.1/fix-build.patch", "c6baf54c5002f66e786c6ba3d86ac77eacd47a446833c17e273b811375a9202d")
+
+    if not is_arch("arm.*") then
+        add_configs("avx2",  {description = "Enable AVX2", default = true, type = "boolean"})
+    end
     add_configs("imgui", {description = "Define method to convert omath types to imgui types", default = true, type = "boolean"})
 
     add_deps("cmake")
-    if is_plat("windows") then
-        add_deps("pkgconf")
-    else
-        add_deps("pkg-config")
-    end
 
     on_load(function (package)
         if package:config("imgui") then
@@ -25,24 +24,12 @@ package("omath")
     end)
 
     on_install("!macosx and !iphoneos and !android and !bsd", function (package)
-        if package:config("imgui") then
-            local imgui = package:dep("imgui")
-            if imgui and not imgui:is_system() then
-                local imgui_fetch = imgui:fetch()
-                if imgui_fetch then
-                    for _, inc in ipairs(imgui_fetch.includedirs or imgui_fetch.sysincludedirs) do
-                        os.mkdir(inc)
-                    end
-                end
-            end
-        end
-        io.replace("CMakeLists.txt", [[find_package(imgui CONFIG REQUIRED)]], [[include(FindPkgConfig)
-pkg_search_module("imgui" REQUIRED IMPORTED_TARGET "imgui")]], {plain = true})
-        io.replace("CMakeLists.txt", [[imgui::imgui]], [[PkgConfig::imgui]], {plain = true})
-        if package:is_plat("wasm") then
-            io.replace("CMakeLists.txt", [[target_compile_options(${PROJECT_NAME} PRIVATE -mavx2 -mfma)]], [[target_compile_options(${PROJECT_NAME} PRIVATE -msimd128 -mavx2)]], {plain = true})
-        end
-        local configs = {"-DOMATH_THREAT_WARNING_AS_ERROR=OFF", "-DOMATH_BUILD_TESTS=OFF"}
+        local configs = {
+            "-DOMATH_BUILD_TESTS=OFF",
+            "-DOMATH_BUILD_BENCHMARK=OFF",
+            "-DOMATH_THREAT_WARNING_AS_ERROR=OFF",
+            "-DOMATH_BUILD_EXAMPLES=OFF",
+        }
         table.insert(configs, "-DOMATH_USE_AVX2=" .. (package:config("avx2") and "ON" or "OFF"))
         table.insert(configs, "-DOMATH_IMGUI_INTEGRATION=" .. (package:config("imgui") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
@@ -52,8 +39,13 @@ pkg_search_module("imgui" REQUIRED IMPORTED_TARGET "imgui")]], {plain = true})
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
+            #if __has_include(<omath/omath.hpp>)
+                #include <omath/omath.hpp>
+            #else
+                #include <omath/vector2.hpp>
+            #endif
             void test() {
                 omath::Vector2 w = omath::Vector2(20.0, 30.0);
             }
-        ]]}, {configs = {languages = "c++23"}, includes = "omath/vector2.hpp"}))
+        ]]}, {configs = {languages = "c++23"}}))
     end)
