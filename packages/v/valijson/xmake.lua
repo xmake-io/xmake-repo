7beef76d245a7eea733c814efcff75463441aae2
Saikari <lin@sz.cn.eu.org>
2025-10-01 01:01:19 +0300

    Update xmake.lua

--- packages/v/valijson/xmake.lua
+++ packages/v/valijson/xmake.lua
@@ -22,7 +22,7 @@ package("valijson")
     on_install(function (package)
         io.replace("include/valijson/constraints/concrete_constraints.hpp", [[#include <cmath>]], [[#include <cmath>
 #include <cstdint>]], {plain = true})
-        local configs = {"-Dvalijson_BUILD_EXAMPLES=ON", "-Dvalijson_BUILD_TESTS=OFF"}
+        local configs = {"-Dvalijson_BUILD_EXAMPLES=OFF", "-Dvalijson_BUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
