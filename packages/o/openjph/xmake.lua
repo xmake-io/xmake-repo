75bb095e14d75c08442464ff7d0792c88a86bfb5
Saikari <lin@sz.cn.eu.org>
2025-10-01 02:17:21 +0300

    Update xmake.lua

--- packages/o/openjph/xmake.lua
+++ packages/o/openjph/xmake.lua
@@ -1,30 +1,35 @@
 package("openjph")
     set_homepage("https://github.com/aous72/OpenJPH")
-    set_description("Open-source implementation of JPEG2000 Part-15 (or JPH or HTJ2K) ")
+    set_description("Open-source implementation of JPEG2000 Part-15 (or JPH or HTJ2K)")
     set_license("BSD-2-Clause")
     
-    add_urls("https://github.com/aous72/OpenJPH/archive/refs/tags/$(version).zip"
-        ,"https://github.com/aous72/OpenJPH.git")
-    add_versions("0.24.1","c9914d98c40262fb10941ff5d263bd671d133cd3572ec8d4c62151700ffa580e")
+    add_urls("https://github.com/aous72/OpenJPH/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/aous72/OpenJPH.git")
+
+    add_versions("0.24.1", "5e44a809c9ee3dad175da839feaf66746cfc114a625ec61c786de8ad3f5ab472")
+
     add_deps("cmake")
-        on_check("android",function (package)
-        -- 检查 NDK 版本
-        --local ndk_version = os.getenv("NDK_VERSION") or "unknown"
-        local config_ndk_version = package:config("ndk_version")
-        if config_ndk_version < 24 then
-            assert(false,"ndk ver too low")
-        end
-    end)
+
+    if on_check then
+        on_check("android", function (package)
+            local ndk = package:toolchain("ndk")
+            local ndk_sdkver = ndk:config("ndk_sdkver")
+            assert(ndk_sdkver and tonumber(ndk_sdkver) >= 28, "package(openjph): need ndk api level >= 28")
+        end)
+    end
+
     on_install(function (package)
 	    local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
     end)
+
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             void test() {
-                ojph::point x;
-                x.x;
+                ojph::j2c_outfile file;
+                file.open("file.txt");
             }
-        ]]}, {languages = "c++11",includes = {"openjph/ojph_base.h"}}))
+        ]]}, {languages = "c++11", includes = {"openjph/ojph_file.h"}}))
     end)
