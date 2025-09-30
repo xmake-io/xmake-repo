1b3e05eedcbfa71f076e06a2cae305d62c799e8a
Casinius <749929621@qq.com>
2025-09-30 20:53:23 +0800

    add claim configs

--- packages/o/openjph/xmake.lua
+++ packages/o/openjph/xmake.lua
@@ -7,7 +7,7 @@ package("OpenJPH")
     add_versions("0.24.1","c9914d98c40262fb10941ff5d263bd671d133cd3572ec8d4c62151700ffa580e")
     add_deps("cmake")
     on_install(function (package)
-	local configs = {}
+	    local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         import("package.tools.cmake").install(package, configs)
     end)
