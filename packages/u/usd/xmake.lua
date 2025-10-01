c75c580fbc4af81f10fa409f414314f438a82e15
star9029 <hengxings783@gmail.com>
2025-10-01 21:53:31 +0800

    Update architecture check for usd package on macOS

--- packages/u/usd/xmake.lua
+++ packages/u/usd/xmake.lua
@@ -36,8 +36,9 @@ package("usd")
 
     if on_check then
         on_check(function (package)
-            -- MacOS with Apple Silicon can use usd, so let it pass when using OSX.
-            assert(package:is_arch("x64", "x86_64") or package.is_plat("macosx"), "package(usd) only support x86 or macosx")
+            if not package:is_plat("macosx") then
+                assert(package:is_arch("x64", "x86_64"), "package(usd) only support x86")
+            end
             if package:version() and package:version():eq("25.08") and
                 package:is_plat("linux") and package:has_tool("cxx", "clang") then
 
