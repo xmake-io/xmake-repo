d309ff0c75aa6f392de942fdd7db4b68d8c16fbc
Saikari <lin@sz.cn.eu.org>
2025-10-03 18:27:52 +0300

    Update xmake.lua

--- packages/d/dbus-cxx/xmake.lua
+++ packages/d/dbus-cxx/xmake.lua
@@ -9,9 +9,9 @@ package("dbus-cxx")
     add_versions("2.6.0", "ca22380ec04a1f10154fca76d41e8ce4a8a6351ce86546b297bda5f7eefbc108")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
-    add_configs("with_glib", {description = "Build with GLib library support.", default = true, type = "boolean"})
-    add_configs("with_qt", {description = "Build with QT library support.", default = true, type = "boolean"})
-    add_configs("with_uv", {description = "Build with UV library support.", default = true, type = "boolean"})
+    add_configs("glib", {description = "Build dbus-cxx with GLib module.", default = true, type = "boolean"})
+    add_configs("libuv", {description = "Build dbus-cxx with libuv module.", default = true, type = "boolean"})
+    add_configs("qt5", {description = "Build dbus-cxx with QT5 module.", default = true, type = "boolean"})
 
     add_includedirs("include", "include/dbus-cxx-2.0")
 
@@ -23,17 +23,17 @@ package("dbus-cxx")
             package:config_set("with_qt", false)
             wprint("Disabling with_qt, since Linux arm64 AQT does not support QT5, but supports QT6 (6.7.0 -> 6.10.0).")
         end
-        if package:config("with_glib") then
+        if package:config("glib") then
             package:add("deps", "glib")
             package:add("includedirs", "include/dbus-cxx-glib-2.0")
         end
-        if package:config("with_qt") then
-            package:add("deps", "qt5core")
-            package:add("includedirs", "include/dbus-cxx-qt-2.0")
-        end
-        if package:config("with_uv") then
+        if package:config("libuv") then
             package:add("deps", "libuv")
             package:add("includedirs", "include/dbus-cxx-uv-2.0")
+        end
+        if package:config("qt5") then
+            package:add("deps", "qt5core")
+            package:add("includedirs", "include/dbus-cxx-qt-2.0")
         end
     end)
 
@@ -42,9 +42,9 @@ package("dbus-cxx")
             "-DBUILD_TESTING=OFF", "-DENABLE_CODE_COVERAGE_REPORT=OFF",
             "-DENABLE_EXAMPLES=OFF", "-DENABLE_TOOLS=OFF", "-DBUILD_SITE=OFF", "-DUV_STATIC=OFF"
         }
-        table.insert(configs, "-DENABLE_GLIB_SUPPORT=" .. (package:config("with_glib") and "ON" or "OFF"))
-        table.insert(configs, "-DENABLE_QT_SUPPORT=" .. (package:config("with_qt") and "ON" or "OFF"))
-        table.insert(configs, "-DENABLE_UV_SUPPORT=" .. (package:config("with_uv") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_GLIB_SUPPORT=" .. (package:config("glib") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_QT_SUPPORT=" .. (package:config("qt") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_UV_SUPPORT=" .. (package:config("libuv") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
