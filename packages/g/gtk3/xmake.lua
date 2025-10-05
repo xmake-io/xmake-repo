6ec5720416924c1890e57316d7be77d1b4dc0f45
Saikari <lin@sz.cn.eu.org>
2025-10-05 05:40:07 +0300

    Update xmake.lua

--- packages/g/gtk3/xmake.lua
+++ packages/g/gtk3/xmake.lua
@@ -31,15 +31,14 @@ package("gtk3")
     add_deps("meson", "ninja")
     add_deps("cairo", {configs = {glib = true}})
     add_deps("glib", "pango", "libepoxy", "graphene", "libxkbcommon", "libxext")
-    add_deps("libx11", "libxfixes", "libxcursor", "libxi", "libxcomposite", "libxrandr", "libxdamage", "libxinerama", "libiconv", "at-spi2-core")
+    add_deps("libx11", "libxfixes", "libxcursor", "libxi", "libxcomposite", "libxrandr", "libxdamage", "libxinerama", "at-spi2-core")
     add_links("gtk-3", "gdk-3", "gailutil-3", "X11", "X11-cxb", "pangocairo-1.0", "pango", "rt")
 
     on_install("linux", function (package)
         local configs = {"-Dintrospection=false", "-Ddemos=false", "-Dexamples=false", "-Dtests=false"}
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         io.replace("gdk/x11/gdkglcontext-x11.c", [[cairo/cairo-xlib.h]], [[cairo-xlib.h]], {plain = true})
-        import("package.tools.meson").install(package, configs, {packagedeps = {"libiconv",
-                                                                                "libx11", 
+        import("package.tools.meson").install(package, configs, {packagedeps = {"libx11", 
                                                                                 "libxext", 
                                                                                 "libxi",
                                                                                 "pango", 
