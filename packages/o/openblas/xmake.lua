5f278b22357688e10fcf6fa0faa3732b80ae5c70
chooyy <choo-yy@qq.com>
2025-10-21 09:22:28 +0800

    fix macosx

--- packages/o/openblas/xmake.lua
+++ packages/o/openblas/xmake.lua
@@ -54,6 +54,10 @@ package("openblas")
         add_versions("0.3.28", "f1003466ad074e9b0c8d421a204121100b0751c96fc6fcf3d1456bd12f8a00a1")
         add_versions("0.3.30", "27342cff518646afb4c2b976d809102e368957974c250a25ccc965e53063c95d")
 
+        if is_plat("macosx") and is_arch("x86_64") then
+            add_patches("0.3.30", "https://github.com/OpenMathLib/OpenBLAS/pull/5478/commits/b48a089d7526211e5521a6767470b3d38642831e.patch", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+        end
+
         add_configs("fortran", {description = "Compile with fortran enabled.", default = not is_plat("macosx"), type = "boolean"})
         add_configs("openmp",  {description = "Compile with OpenMP enabled.", default = not is_plat("macosx"), type = "boolean"})
     end
