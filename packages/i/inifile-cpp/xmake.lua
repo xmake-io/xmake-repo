876606f221b0acc9338b76885e54d4857754f757
Ehren Bendler <a5ehren@gmail.com>
2025-10-22 09:17:28 -0400

    initfile-cpp: Update to most recent commit

--- packages/i/inifile-cpp/xmake.lua
+++ packages/i/inifile-cpp/xmake.lua
@@ -6,6 +6,7 @@ package("inifile-cpp")
 
     add_urls("https://github.com/Rookfighter/inifile-cpp.git")
     add_versions("2022.06.25", "e7ba25eede111e76e176a341ea12a47e9948627c")
+    add_versions("2025.02.11", "7e49789411beba98ca2c98940c40f7ca7e4b23b5")
 
     on_install(function (package)
         os.cp("include", package:installdir())
