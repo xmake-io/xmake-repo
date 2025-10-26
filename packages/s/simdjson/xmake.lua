192178c800281b22218ca2364a9b0bba2c142aea
ruki <waruqi@gmail.com>
2025-10-26 10:26:10 +0800

    Update simdjson to v4.0.7 (#8488)

--- packages/s/simdjson/xmake.lua
+++ packages/s/simdjson/xmake.lua
@@ -6,6 +6,7 @@ package("simdjson")
 
     add_urls("https://github.com/simdjson/simdjson/archive/refs/tags/$(version).tar.gz",
              "https://github.com/simdjson/simdjson.git")
+    add_versions("v4.0.7", "d2d15490605858d3dd42e90d25e0fde31c53446b7d3cde9ef334449236927916")
     add_versions("v4.0.6", "84b90eaff91c8a4ac40feff1fffa9d13d706f914413dd41351644038a14079b6")
     add_versions("v3.12.3", "d0af071f2f4187d8b26b556e83ef832b634bd5feb4e2f537b9dabbd334d4e334")
     add_versions("v3.12.2", "8ac7c97073d5079f54ad66d04381ec75e1169c2e20bfe9b6500bc81304da3faf")
