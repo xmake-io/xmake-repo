3d7612d30d988d508bc6982ed8827d74527d2eec
star9029 <hengxings783@gmail.com>
2025-10-28 14:33:02 +0800

    Add WASM platform support

--- packages/l/libepoxy/xmake.lua
+++ packages/l/libepoxy/xmake.lua
@@ -9,13 +9,17 @@ package("libepoxy")
         alias = "gnome",
         version = function (version)
             return format("%d.%d/libepoxy-%s", version:major(), version:minor(), version)
-        end}
-    )
+        end
+    })
 
     add_versions("github:1.5.10", "a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15")
 
     add_versions("gnome:1.5.10", "072cda4b59dd098bba8c2363a6247299db1fa89411dc221c8b81b8ee8192e623")
     add_versions("gnome:1.5.9", "d168a19a6edfdd9977fef1308ccf516079856a4275cf876de688fb7927e365e4")
+
+    if is_plat("wasm") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
 
     if is_plat("linux") then
         add_extsources("apt::libepoxy-dev")
@@ -25,7 +29,7 @@ package("libepoxy")
 
     add_deps("meson", "ninja")
 
-    on_install(function (package)
+    on_install("!android and !bsd and !cross", function (package)
         if package:is_plat("windows") and not package:config("shared") then
             io.replace("include/epoxy/common.h", "__declspec(dllimport)", "", {plain = true})
         end
