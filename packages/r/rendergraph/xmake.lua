10059ae9170177d242c8d63cd8cb98026db2d941
Willaaaaaaa <willaaaaaaa936@gmail.com>
2025-10-29 11:14:29 +0800

    add version

--- packages/r/rendergraph/xmake.lua
+++ packages/r/rendergraph/xmake.lua
@@ -7,6 +7,7 @@ package("rendergraph")
          "https://github.com/DragonJoker/RenderGraph.git")
 
     add_versions("v2.1.0", "a496e0b04944edd52cb6f131877559314a9d43e6964567a5d613df1989da0cb2")
+    add_versions("v2.0.0", "9ab5bf4ef16fac2bec8633b1634843c97e3e244a556be62c942348e2462d0888")
     add_versions("v1.4.1", "7096a6384165f98ec3fab995deba10523b42a4f170f9ad9473107bc03eb50a3d")
     add_versions("v1.4.0", "0009eac85885231069f7ba644d22a801e71505cc")
     add_versions("v1.3.0", "b9c68b6949c7b60ffb49f9b9997432aac5baec69")
