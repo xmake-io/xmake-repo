84de19db2e02ac10c85874416dfe94587b11078a
Paul Reilly <2909238-paul-reilly@users.noreply.gitlab.com>
2025-11-02 23:50:56 +0000

    add filc toolchain

---
+++ packages/f/filc/xmake.lua
@@ -0,0 +1,47 @@
+package("filc")
+    set_kind("toolchain")
+    set_homepage("https://fil-c.org/")
+    set_description("A memory safe implementation of the C and C++ programming languages.")
+    set_license("LLVM")
+
+    if is_host("linux") then
+        if os.arch() == "x86_64" then
+            set_urls("https://github.com/pizlonator/fil-c/releases/download/v$(version)/filc-$(version)-linux-x86_64.tar.xz")
+            add_versions("0.674", "a8ec349f383a49dacc09a9540643164c67081245e591c1e6823609653f2c9740")
+        end
+    end
+
+    add_deps("patchelf") -- needed for setup.sh
+
+    on_install("@linux|x86_64", function (package)
+        local installdir = package:installdir()
+        os.cp(path.join(os.curdir(), "*"), installdir)
+
+        -- now we replicate the commands in setup.sh...
+        local pathx = path.join(installdir, "/pizfix/lib64") .. ":" .. path.join(installdir, "/pizfix/lib")
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib/libc.so"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib/libpizlo.so"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib/libc++.so.1.0"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib/libc++abi.so.1.0"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib_test/libpizlo.so"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib_test_gcverify/libpizlo.so"}, {shell=true})
+        os.runv("patchelf --set-rpath", {pathx, "pizfix/lib_gcverify/libpizlo.so"}, {shell=true})
+
+        -- ... but copy dirs instead of symlinking to system dirs
+        local dest = path.join(installdir, "pizfix", "os-include")
+        os.cp("/usr/include/linux", path.join(dest, "linux"))
+        if os.exists("/usr/include/x86_64-linux-gnu/asm") then
+            os.cp("/usr/include/x86_64-linux-gnu/asm", path.join(dest, "asm"))
+        else
+            os.cp("/usr/include/asm", path.join(dest, "asm"))
+        end
+        os.cp("/usr/include/asm-generic", path.join(dest, "asm-generic"))
+
+        -- we must preserve relative directory positions
+        package:addenv("PATH", "build/bin")
+    end)
+
+    on_test(function (package)
+        os.vrun("filcc --version")
+    end)
+
