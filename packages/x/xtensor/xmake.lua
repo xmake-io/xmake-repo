2a0290af68f80a553ab61d24b22b8df6d00de2f3
Willaaaaaaa <willaaaaaaa936@gmail.com>
2025-11-03 00:24:51 +0800

    add patch for 0.25.0

--- packages/x/xtensor/xmake.lua
+++ packages/x/xtensor/xmake.lua
@@ -17,6 +17,8 @@ package("xtensor")
     add_versions("0.24.0", "37738aa0865350b39f048e638735c05d78b5331073b6329693e8b8f0902df713")
     add_versions("0.23.10", "2e770a6d636962eedc868fef4930b919e26efe783cd5d8732c11e14cf72d871c")
 
+    add_patches("0.25.0", "https://patch-diff.githubusercontent.com/raw/xtensor-stack/xtensor/pull/2833.patch", "809e9009286d6ab56cd07531b8055a45d08a140066f5ba0db2e957cfee5d56a4")
+
     add_configs("simd", {description = "Enable SIMD acceleration ", default = true, type = "boolean"})
     add_configs("tbb", {description = "Enable parallelization using intel TBB", default = false, type = "boolean"})
     add_configs("openmp", {description = "Enable parallelization using OpenMP", default = false, type = "boolean"})
