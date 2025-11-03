86fbd3c499ab2637632de3c4de7d4aa96d4bd30a
ruki <waruqi@gmail.com>
2025-11-03 15:13:21 +0800

    Auto-update bdwgc to v8.2.10 (#8547)
    * Update bdwgc to v8.2.10
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/b/bdwgc/xmake.lua
+++ packages/b/bdwgc/xmake.lua
@@ -5,6 +5,7 @@ package("bdwgc")
     add_urls("https://github.com/ivmai/bdwgc/archive/refs/tags/$(version).tar.gz",
              "https://github.com/ivmai/bdwgc.git")
 
+    add_versions("v8.2.10", "5858a417ab3eaac2add0daf79cd29ddf248c9f648e6c08092b775dcbfcbe1edb")
     add_versions("v8.2.6", "3bfc2b1dd385bfb46d2dab029211a66249a309795b6893f4e00554904999e40a")
     add_versions("v8.2.4", "18e63ab1428bd52e691da107a6a56651c161210b11fbe22e2aa3c31f7fa00ca5")
 
@@ -17,6 +18,11 @@ package("bdwgc")
                 local ndk = package:toolchain("ndk")
                 local ndk_sdkver = ndk:config("ndk_sdkver")
                 assert(ndk_sdkver and tonumber(ndk_sdkver) > 21, "package(bdwgc/armeabi-v7a): need ndk api level > 21")
+            end
+        end)
+        on_check("mingw", function (target)
+            if package:version() and package:version():eq("8.2.4") then
+                raise("package(bdwgc 8.2.4) unsupported mingw")
             end
         end)
     end
