8ee62bc2c853b7e5d2b86e1b6aea3171f25859d5
Saikari <lin@sz.cn.eu.org>
2025-11-20 21:07:10 +0300

    Try resolve NDK issue

--- packages/c/cppli/xmake.lua
+++ packages/c/cppli/xmake.lua
@@ -3,6 +3,7 @@ package("cppli")
     set_description("an intuitive CLI framework for C++")
     
     add_urls("https://github.com/TheBearodactyl/cppli.git")
+
     add_versions("2025.10.22", "98c8c2e8ee65d7a5a6b160cf0b85ba1be39ffb05")
 
     add_patches("2025.10.22", "patches/2025.10.22/fix-clang.patch", "7d7c1363774e4279636455b7cfe4806138a272a03d10f6bfce6dda00b3b17d0d")
@@ -10,7 +11,16 @@ package("cppli")
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
     add_deps("cmake")
-    
+
+    on_check(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <source_location>
+            void test(std::signed_integral auto x) {
+                std::source_location loc = std::source_location::current();
+            }
+        ]]}, {configs = {languages = "c++20"}}), "package(cppli): need std::source_location from <source_location> header.")
+    end)
+
     on_install(function(package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
