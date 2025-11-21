1f96e48c22a8b3f95dd7889c79ab8f2ce89446cd
Saikari <lin@sz.cn.eu.org>
2025-11-21 23:58:46 +0300

    Update xmake.lua

--- packages/f/filament/xmake.lua
+++ packages/f/filament/xmake.lua
@@ -4,24 +4,30 @@ package("filament")
     set_license("Apache-2.0")
 
     if is_plat("windows") and is_arch("x64") then
-        add_urls("https://github.com/google/filament/releases/download/v$(version)/filament-v$(version)-windows.tgz")
+        add_urls("https://github.com/google/filament/releases/download/v$(version)/filament-v$(version)-windows.tgz",
+                 "https://github.com/google/filament/releases/download/v$(version)/filament-windows.tgz")
         add_versions("1.9.23", "8f5728dd944c052f991bfb15d18cd6fc8f678d0cbd37bb066d76bf682ab789c8")
         add_versions("1.20.3", "0a3fdd5fe8662a02117f3de51dcbea3b260cff716a7cffa407ca939727d7b634")
         add_versions("1.32.0", "6372895cd7729df722bb0529a1ceb086aee3e532268d541db88aba4a252cde4e")
         add_versions("1.67.0", "07acead9d2fc2c19058656da679fe33078f4de82d7a762cdab4af6c9e02d05b2")
+        add_versions("1.67.1", "f7d6dcd5d848835ae83ab908cdff001cb5c3c65f33bc89d036c77fba143572c7")
     elseif is_plat("macosx") and is_arch("x86_64") then
         add_urls("https://github.com/google/filament/releases/download/v$(version)/filament-v$(version)-mac.tgz")
         add_versions("1.9.23", "0adbf2359338e28a80b2ef84c70d8914b56ed1c97ef0135603fcd330ec4c34a1")
         add_versions("1.20.3", "820f2c7b5360021b9ff361f0868b45613726d6704f9112e8c8cf92d07c7c95b7")
         add_versions("1.32.0", "f8bd877227f10a9dc10513bfc6a396a5bb0c65b15d2fd771e2e5de4d39267395")
+        add_versions("1.67.0", "1f0db35a7808d944fdd65b321b99093969866869f7abf2ed5386615127880db0")
+        add_versions("1.67.1", "edd708241f146f216c3a8ef93e0eb5cc16e13f0ae95c35c83daefde4f3546f2b")
     elseif is_plat("macosx") and is_arch("arm64") then
         add_urls("https://github.com/google/filament/releases/download/v$(version)/filament-v$(version)-mac.tgz")
+        add_versions("1.67.0", "1f0db35a7808d944fdd65b321b99093969866869f7abf2ed5386615127880db0")
         add_versions("1.67.1", "edd708241f146f216c3a8ef93e0eb5cc16e13f0ae95c35c83daefde4f3546f2b")
     elseif is_plat("linux") and is_arch("x86_64") then
         add_urls("https://github.com/google/filament/releases/download/v$(version)/filament-v$(version)-linux.tgz")
         add_versions("1.9.23", "016473371753ff6beb430900eb6550f73acc71c1092cbb654f272ed0666f6210")
         add_versions("1.20.3", "f57e1c967e09fe73ef69b0db8a48a09b3dab6f9ecb21b906b3f1fbe1d3a2ce3d")
         add_versions("1.32.0", "9c3fba4ed307aeeaa412f1846774c7a1c30afff3404d7f9822a7e77338f70fe9")
+        add_versions("1.67.0", "d39fd7faec6fafefc9e3dce1f0fb7505068141bd2fb59ec42119ecf9b190114a")
         add_versions("1.67.1", "dde71072c84781b2b908735aebb9657f07dfdfd9b917bcd42a38c9e265e26bb8")
     end
 
@@ -42,16 +48,20 @@ package("filament")
 
     if on_check then
         on_check(function (package)
-            if package:is_plat("windows") and package:version():eq("1.67.1") then
+            local version = package:version()
+            if not version then
+                return
+            end
+            if package:is_plat("windows") and version:eq("1.67.1") then
                 raise("package(filament): does not support 1.67.1 version for Windows OS x64.")
             end
-            if package:is_plat("macosx") and package:is_arch("x86_64") and package:version():gt("1.32.0") then
+            if package:is_plat("macosx") and package:is_arch("x86_64") and version:gt("1.32.0") then
                 raise("package(filament): does not support versions newer than 1.32.0 for Mac OS x64.")
             end
-            if package:is_plat("macosx") and package:is_arch("arm64") and package:version():eq("1.67.0") then
+            if package:is_plat("macosx") and package:is_arch("arm64") and version:eq("1.67.0") then
                 raise("package(filament): does not support 1.67.0 version for Mac OS arm64.")
             end
-            if package:is_plat("linux") and package:is_arch("x86_64") and package:version():eq("1.67.0") then
+            if package:is_plat("linux") and package:is_arch("x86_64") and version:eq("1.67.0") then
                 raise("package(filament): does not support 1.67.0 version for Linux x64.")
             end
         end)
