e66c93cb9816413f6d0947777a17eeba85ad3a79
star9029 <hengxings783@gmail.com>
2025-11-24 13:14:19 +0800

    Add cmake config option and modify dependencies
    Added cmake configuration option and conditional dependency.

--- packages/e/emhash/xmake.lua
+++ packages/e/emhash/xmake.lua
@@ -10,10 +10,20 @@ package("emhash")
     add_versions("v1.0.1", "dbcce726c5ccce4a260a2c5ca9aa239e4d6109aacb3b5097ebfa465247708a7b")
     add_versions("v1.0.0", "9de79897a94e8c2545a401bb441ee6f6c293124e46bf9cf3023be6b1632e708b")
 
-    add_deps("cmake")
+    add_configs("cmake", {description = "Use cmake build system", default = false, type = "boolean"})
+
+    on_load(function (package)
+        if package:config("cmake") then
+            package:add("deps", "cmake")
+        end
+    end)
 
     on_install(function (package)
-        import("package.tools.cmake").install(package, {"-DWITH_BENCHMARKS=OFF"})
+        if package:config("cmake") then
+            import("package.tools.cmake").install(package, {"-DWITH_BENCHMARKS=OFF"})
+        else
+            os.cp("*.hpp", package:installdir("include"))
+        end
     end)
 
     on_test(function (package)
