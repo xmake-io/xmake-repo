1c94eeb5a790a66e9513ff1a919fdf1a171d567d
ruki <waruqi@gmail.com>
2025-11-26 23:35:57 +0800

    fix url

--- packages/a/appimage/xmake.lua
+++ packages/a/appimage/xmake.lua
@@ -6,42 +6,41 @@ package("appimage")
     set_license("MIT")
 
     if is_host("linux") then
+        local arch
         if os.arch() == "x86_64" then
-            add_urls("https://github.com/AppImage/AppImageKit/releases/download/$(version)", {version = function (version)
-                    local ver = version:gsub("%.0$", "")
-                    local prefix = (ver == "13" and "obsolete-" or "")
-                    return ver .. "/" .. prefix .. "appimagetool-x86_64.AppImage"
-                end})
-            add_versions("13.0", "df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb")
-            add_versions("12.0", "d918b4df547b388ef253f3c9e7f6529ca81a885395c31f619d9aaf7030499a13")
-            add_versions("11.0", "c13026b9ebaa20a17e7e0a4c818a901f0faba759801d8ceab3bb6007dde00372")
-            add_versions("10.0", "2f8a62f8ad1a4ad9608132bc467d6cc3f2c948ec5697a57b983e2824e98aeb0f")
-            add_versions("9.0", "b411f3737211bb3c8be7eca5084271306c69bb5cbb28b8a2725350dfbac08858")
-            add_versions("8.0", "3475d0fcf43ba08eb483fd413b44476e2f8274196d0d2b482c4bf780a2c30091")
+            arch = "x86_64"
         elseif os.arch():find("arm64.*") then
-            add_urls("https://github.com/AppImage/AppImageKit/releases/download/$(version)", {version = function (version)
-                    local ver = version:gsub("%.0$", "")
-                    local prefix = (ver == "13" and "obsolete-" or "")
-                    return ver .. "/" .. prefix .. "appimagetool-aarch64.AppImage"
-                end})
-            add_versions("13.0", "334e77beb67fc1e71856c29d5f3f324ca77b0fde7a840fdd14bd3b88c25c341f")
-            add_versions("12.0", "c9d058310a4e04b9fbbd81340fff2b5fb44943a630b31881e321719f271bd41a")
-            add_versions("11.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
-            add_versions("10.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
-            add_versions("9.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
-            add_versions("8.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
+            arch = "aarch64"
         elseif os.arch() == "i386" then
+            arch = "i686"
+        end
+
+        if arch then
             add_urls("https://github.com/AppImage/AppImageKit/releases/download/$(version)", {version = function (version)
                     local ver = version:gsub("%.0$", "")
                     local prefix = (ver == "13" and "obsolete-" or "")
-                    return ver .. "/" .. prefix .. "appimagetool-i686.AppImage"
+                    return ver .. "/" .. prefix .. "appimagetool-" .. arch .. ".AppImage"
                 end})
-            add_versions("13.0", "104978205c888cb2ad42d1799e03d4621cb9a6027cfb375d069b394a82ff15d1")
-            add_versions("12.0", "3af6839ab6d236cd62ace9fbc2f86487f0bf104f521d82da6dea4dab8d3ce4ca")
-            add_versions("11.0", "d691fac50728fe5d46ca584c7843088a306751ff6d3dcf790bf42c0784f7a213")
-            add_versions("10.0", "9b3c3f4f8a1b8d9c8009902b9f6b69fa0a6bdb31e6acffa53c48ae701e3cc671")
-            add_versions("9.0", "3ae608ee16686c3e6e495ac1f79fcd50a4ed0d60e1000cc7833b3d1c20477395")
-            add_versions("8.0", "812c505a61e493a37b726ace83156a6562d90a8dd2be5088b07ab327f6098bc3")
+
+            if arch == "x86_64" then
+                add_versions("13.0", "df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb")
+                add_versions("12.0", "d918b4df547b388ef253f3c9e7f6529ca81a885395c31f619d9aaf7030499a13")
+                add_versions("11.0", "c13026b9ebaa20a17e7e0a4c818a901f0faba759801d8ceab3bb6007dde00372")
+                add_versions("10.0", "2f8a62f8ad1a4ad9608132bc467d6cc3f2c948ec5697a57b983e2824e98aeb0f")
+                add_versions("9.0", "b411f3737211bb3c8be7eca5084271306c69bb5cbb28b8a2725350dfbac08858")
+                add_versions("8.0", "3475d0fcf43ba08eb483fd413b44476e2f8274196d0d2b482c4bf780a2c30091")
+            elseif arch == "aarch64" then
+                add_versions("13.0", "334e77beb67fc1e71856c29d5f3f324ca77b0fde7a840fdd14bd3b88c25c341f")
+                add_versions("12.0", "c9d058310a4e04b9fbbd81340fff2b5fb44943a630b31881e321719f271bd41a")
+                add_versions("11.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
+            elseif arch == "i686" then
+                add_versions("13.0", "104978205c888cb2ad42d1799e03d4621cb9a6027cfb375d069b394a82ff15d1")
+                add_versions("12.0", "3af6839ab6d236cd62ace9fbc2f86487f0bf104f521d82da6dea4dab8d3ce4ca")
+                add_versions("11.0", "d691fac50728fe5d46ca584c7843088a306751ff6d3dcf790bf42c0784f7a213")
+                add_versions("10.0", "9b3c3f4f8a1b8d9c8009902b9f6b69fa0a6bdb31e6acffa53c48ae701e3cc671")
+                add_versions("9.0", "3ae608ee16686c3e6e495ac1f79fcd50a4ed0d60e1000cc7833b3d1c20477395")
+                add_versions("8.0", "812c505a61e493a37b726ace83156a6562d90a8dd2be5088b07ab327f6098bc3")
+            end
         end
     end
 
