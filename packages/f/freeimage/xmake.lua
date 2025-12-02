51573db0143bd9a9ad921df7794f1b80ff92ad44
star9029 <hengxings783@gmail.com>
2025-12-02 15:48:31 +0800

    Update xmake.lua

--- packages/f/freeimage/xmake.lua
+++ packages/f/freeimage/xmake.lua
@@ -1,7 +1,7 @@
 package("freeimage")
     set_homepage("https://sourceforge.net/projects/freeimage/")
     set_description("FreeImage is a library project for developers who would like to support popular graphics image formats (PNG, JPEG, TIFF, BMP and others).")
-    set_license("GPL-2.0", "GPL-3.0", "FreeImage Public License")
+    set_license("FreeImage Public License")
 
     add_urls("https://sourceforge.net/projects/freeimage/files/Source%20Distribution/$(version).zip", {version = function (version)
         return version .. "/FreeImage" .. version:gsub("%.", "")
@@ -16,8 +16,16 @@ package("freeimage")
 
     add_configs("rgb", {description = "Use RGB instead of BGR.", default = false})
 
-    add_deps("jxrlib", "libjpeg", "libpng", "libraw <0.20", "libtiff", "openexr <3.0", "openjpeg", "zlib")
+    add_deps("jxrlib", "libjpeg-turbo", "libpng", "libraw <0.20", "libtiff", "openexr <3.0", "openjpeg", "zlib")
     add_deps("libwebp", {configs = {libwebpmux = true}})
+
+    on_check("windows|x86", function (package)
+        local msvc = package:toolchain("msvc")
+        local vs = msvc:config("vs")
+        if vs and tonumber(vs) < 2019 then
+            raise("package(freeimage): MSVC 2019 and earlier are not supported.")
+        end
+    end)
 
     on_load("windows", function (package)
         if not package:config("shared") then
@@ -25,31 +33,23 @@ package("freeimage")
         end
     end)
 
-    on_install("windows|x64", "windows|x86", "macosx", "linux", function (package)
-        if package:is_plat("windows") and package:is_arch("x86") then
-            local vs = import("core.tool.toolchain").load("msvc"):config("vs")
-            if tonumber(vs) < 2019 then
-                raise("Your compiler is too old to use this library.")
-            end
-        end
-        
+    on_install("windows", "mingw", "macosx", "linux", "bsd", function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             includes("@builtin/check")
-            add_requires("jxrlib", "libjpeg", "libpng", "libraw", "libtiff", "libwebp", "openexr", "openjpeg", "zlib")
+            add_requires("jxrlib", "libjpeg-turbo", "libpng", "libraw", "libtiff", "libwebp", "openexr", "openjpeg", "zlib")
             option("rgb", {default = false})
             target("freeimage")
                 set_kind("$(kind)")
                 set_languages("c++11")
-                set_symbols("hidden")
-                
+
                 add_includedirs("Source", "Source/FreeImage", "Source/FreeImageToolkit", "Source/Metadata")
                 add_headerfiles("Source/FreeImage.h", "Source/FreeImageIO.h")
                 add_files("Source/FreeImage/*.cpp", "Source/FreeImage/*.c")
                 add_files("Source/FreeImageToolkit/*.cpp", "Source/Metadata/*.cpp")
 
                 check_cincludes("Z_HAVE_UNISTD_H", "unistd.h")
-                add_packages("jxrlib", "libjpeg", "libpng", "libraw", "libtiff", "libwebp", "openexr", "openjpeg", "zlib")
+                add_packages("jxrlib", "libjpeg-turbo", "libpng", "libraw", "libtiff", "libwebp", "openexr", "openjpeg", "zlib")
                 if has_config("rgb") then
                     add_defines("FREEIMAGE_COLORORDER=1")
                 end
