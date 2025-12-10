8e157708a7f0aa6ad46d09015f88c0ae34ea1c86
ruki <waruqi@gmail.com>
2025-12-10 01:45:23 +0000

    Update snmalloc to 0.7.3

--- packages/s/snmalloc/xmake.lua
+++ packages/s/snmalloc/xmake.lua
@@ -6,6 +6,7 @@ package("snmalloc")
     add_urls("https://github.com/microsoft/snmalloc/archive/refs/tags/$(version).tar.gz",
              "https://github.com/microsoft/snmalloc.git")
 
+    add_versions("0.7.3", "a908b604a77213169b526ab96a64a79c222a03a41a87f13ac00adfeff379f0be")
     add_versions("0.7.1", "91824fdf553f03cf6ef8be57f29f1d4f79cd651667455e9fe4af8b7c09e705d3")
     add_versions("0.7.0", "9e6bd04e58d981218bd5bd3a853d93bbcb1a82dd914f912670f798e011e86746")
     add_versions("0.6.2", "e0486ccf03eac5dd8acbb66ea8ad33bec289572a51614acdf7117397e4f1af8c")
