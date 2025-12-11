5f6a66ae208bff801dbe6109065d22d2002dabac
xq114 <1140735506@qq.com>
2025-12-11 23:25:14 +0800

    add microstl

---
+++ packages/m/microstl/xmake.lua
@@ -0,0 +1,23 @@
+package("microstl")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/cry-inc/microstl")
+    set_description("Small header-only C++ library for STL mesh files.")
+    set_license("MIT")
+
+    add_urls("https://github.com/cry-inc/microstl.git")
+    add_versions("2023.02.04", "ec3868a14d8eff40f7945b39758edf623f609b6f")
+
+    on_install(function (package)
+        os.cp("include/microstl.h", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test(char fileName[]) {
+                std::filesystem::path filePath(fileName);
+                microstl::MeshReaderHandler meshHandler;
+                microstl::Result result = microstl::Reader::readStlFile(filePath, meshHandler);
+                const microstl::Mesh& mesh = meshHandler.mesh;
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "microstl.h"}))
+    end)
