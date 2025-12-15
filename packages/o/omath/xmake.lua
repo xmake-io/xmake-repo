609282df9027d64f0d72fa0501bb7a65553a5b43
Saikari <lin@sz.cn.eu.org>
2025-12-15 04:10:25 +0300

    Just message user error

--- packages/o/omath/xmake.lua
+++ packages/o/omath/xmake.lua
@@ -44,7 +44,7 @@ package("omath")
             Angle<float, 0.0f, 180.0f> a;
             return 0;
         }
-        ]]}, {configs = {languages = "c++23"}}))
+        ]]}, {configs = {languages = "c++23"}}), error "package(omath): Your compiler does not support floating-point non-type template.")
     end)
 
     on_load(function (package)
