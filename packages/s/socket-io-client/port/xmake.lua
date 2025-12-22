7e7cfa3d38e9aa42e2393eaac2b8037b49683498
ruki <waruqi@gmail.com>
2025-12-22 14:19:07 +0800

    Update xmake.lua

--- packages/s/socket-io-client/port/xmake.lua
+++ packages/s/socket-io-client/port/xmake.lua
@@ -10,9 +10,8 @@ target("sioclient")
     add_files("src/*.cpp", "src/internal/*.cpp")
     add_headerfiles("src/*.h")
     add_defines("VERSION=\"$(version)\"")
+    add_packages("rapidjson", "websocketpp", "asio", "openssl")
 
     if is_plat("windows") and is_kind("shared") then
         add_rules("utils.symbols.export_all", {export_classes = true})
     end
-
-    add_packages("rapidjson", "websocketpp", "asio", "openssl")
