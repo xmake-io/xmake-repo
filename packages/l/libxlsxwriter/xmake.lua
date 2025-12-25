3aa80adc4354994ea3b40213930676df89b1e872
star9029 <hengxings783@gmail.com>
2025-12-25 10:54:05 +0800

    libxlsxwriter: add v1.2.3 version (#8900)
    * libxlsxwriter: add v1.2.3 version
    
    * test 1.1.5 version
    
    * Revert "test 1.1.5 version"
    
    This reverts commit 7a31dd4c62a1942e1b576b52ea1327a99b269334.

--- packages/l/libxlsxwriter/xmake.lua
+++ packages/l/libxlsxwriter/xmake.lua
@@ -3,10 +3,11 @@ package("libxlsxwriter")
     set_description("A C library for creating Excel XLSX files.")
     set_license("BSD-2-Clause")
 
-    add_urls("https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/RELEASE_$(version).tar.gz",
-             "https://github.com/jmcnamara/libxlsxwriter.git")
+    add_urls("https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://github.com/jmcnamara/libxlsxwriter.git", {alias = "git"})
 
-    add_versions("1.1.5", "12843587d591cf679e6ec63ecc629245befec2951736804a837696cdb5d61946")
+    add_versions("v1.2.3", "63f070c19c97ce4d5dfcbc1fa8cc5237d4c9decf39341a31188dbdceef93b542")
+    add_versions("git:1.1.5", "RELEASE_1.1.5")
 
     add_configs("tmpfile", {description = "Use the C standard library's tmpfile()", default = false, type = "boolean"})
     add_configs("md5", {description = "Build libxlsxwriter without third party MD5 lib", default = false, type = "boolean"})
@@ -16,71 +17,65 @@ package("libxlsxwriter")
     add_configs("dtoa", {description = "Use the locale independent third party Milo Yip DTOA library", default = false, type = "boolean"})
 
     add_deps("cmake")
-    add_deps("zlib")
+    if is_subhost("windows") then
+        add_deps("pkgconf")
+    else
+        add_deps("pkg-config")
+    end
+    -- minizip cmake missing iowin32.h
+    add_deps("minizip", {configs = {cmake = not is_plat("windows")}})
 
-    on_load(function (package)
-        if package:is_plat("android") then
-            import("core.tool.toolchain")
-            local ndk = toolchain.load("ndk", {plat = package:plat(), arch = package:arch()})
+    if on_check then
+        on_check("android", function (package)
+            local ndk = package.toolchain("ndk")
             local ndk_sdkver = ndk:config("ndk_sdkver")
             assert(ndk_sdkver and tonumber(ndk_sdkver) > 21, "package(libxlsxwriter): need ndk api level > 21 for android")
-        end
+        end)
+    end
+
+    on_load(function (package)
         if package:config("openssl_md5") then
             package:add("deps", "openssl")
-        end
-        if not package:is_plat("linux", "bsd", "mingw") then
-            package:add("deps", "minizip")
         end
     end)
 
     on_install(function (package)
-        local configs = {"-DBUILD_TESTS=OFF", "-DBUILD_EXAMPLES=OFF"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        if package:is_plat("windows") then
-            table.insert(configs, "-DUSE_STATIC_MSVC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
-            if package:is_debug() then
-                io.replace("CMakeLists.txt", [[set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /Fd\"${CMAKE_BINARY_DIR}/${PROJECT_NAME}.pdb\"")]], "", {plain = true})
+        io.replace("CMakeLists.txt", "set(CMAKE_POSITION_INDEPENDENT_CODE ON)", "", {plain = true})
+
+        local version = package:version()
+        if version then
+            -- Remove `find_package(MINIZIP NAMES unofficial-minizip REQUIRED)` for windows
+            io.replace("CMakeLists.txt", "    if(MSVC)", "if(0)", {plain = true})
+            if version:ge("1.1.9") then
+                io.replace("CMakeLists.txt", "list(APPEND LXW_PRIVATE_INCLUDE_DIRS ${MINIZIP_INCLUDE_DIRS}/..)", "list(APPEND LXW_PRIVATE_INCLUDE_DIRS ${MINIZIP_INCLUDE_DIRS})", {plain = true})
             else
-                io.replace("CMakeLists.txt", [[set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /Ox /Zi /Fd\"${CMAKE_BINARY_DIR}/${PROJECT_NAME}.pdb\"")]], "", {plain = true})
+                io.replace("CMakeLists.txt", [[find_package(ZLIB REQUIRED "1.0")]], "find_package(ZLIB REQUIRED)", {plain = true})
+                io.replace("CMakeLists.txt", [[find_package(MINIZIP REQUIRED "1.0")]], "", {plain = true})
+                local file = io.open("CMakeLists.txt", "a")
+                file:write([[
+                    include(FindPkgConfig)
+                    pkg_search_module("minizip" REQUIRED IMPORTED_TARGET "minizip")
+                    target_link_libraries(${PROJECT_NAME} PUBLIC PkgConfig::minizip)
+                ]])
+                file:close()
             end
         end
+
+        local configs = {
+            "-DBUILD_TESTS=OFF",
+            "-DBUILD_EXAMPLES=OFF",
+            "-DUSE_STATIC_MSVC_RUNTIME=OFF",
+            "-DUSE_SYSTEM_MINIZIP=ON",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DUSE_STANDARD_TMPFILE=" .. (package:config("tmpfile") and "ON" or "OFF"))
         table.insert(configs, "-DUSE_NO_MD5=" .. (package:config("md5") and "OFF" or "ON"))
         table.insert(configs, "-DUSE_OPENSSL_MD5=" .. (package:config("openssl_md5") and "ON" or "OFF"))
         table.insert(configs, "-DUSE_MEM_FILE=" .. (package:config("mem_file") and "ON" or "OFF"))
         table.insert(configs, "-DIOAPI_NO_64=" .. (package:config("64") and "OFF" or "ON"))
         table.insert(configs, "-DUSE_DTOA_LIBRARY=" .. (package:config("dtoa") and "ON" or "OFF"))
-
-        io.replace("include/xlsxwriter/packager.h", "minizip/", "", {plain = true})
-        io.replace("src/packager.c", "minizip/", "", {plain = true})
-
-        local packagedeps
-        if package:is_plat("wasm") then
-            table.insert(configs, "-DUSE_SYSTEM_MINIZIP=ON")
-            if package:version():le("1.1.5") then
-                io.replace("CMakeLists.txt", [[find_package(ZLIB REQUIRED "1.0")]], "", {plain = true})
-                io.replace("CMakeLists.txt", [[find_package(MINIZIP REQUIRED "1.0")]], "", {plain = true})
-            else
-                io.replace("CMakeLists.txt", [[find_package(ZLIB "1.0" REQUIRED)]], "", {plain = true})
-                io.replace("CMakeLists.txt", [[find_package(MINIZIP "1.0" REQUIRED)]], "", {plain = true})
-            end
-            packagedeps = {"minizip", "zlib"}
-        elseif package:is_plat("linux", "bsd", "mingw") then
-            table.insert(configs, "-DUSE_SYSTEM_MINIZIP=OFF")
-            io.replace("CMakeLists.txt", [["1.0"]], "", {plain = true})
-        else
-            table.insert(configs, "-DUSE_SYSTEM_MINIZIP=ON")
-            if package:version():le("1.1.5") then
-                io.replace("CMakeLists.txt", [[find_package(ZLIB REQUIRED "1.0")]], "find_package(ZLIB REQUIRED)", {plain = true})
-                io.replace("CMakeLists.txt", [[find_package(MINIZIP REQUIRED "1.0")]], "", {plain = true})
-            else
-                io.replace("CMakeLists.txt", [[find_package(ZLIB "1.0" REQUIRED)]], "find_package(ZLIB REQUIRED)", {plain = true})
-                io.replace("CMakeLists.txt", [[find_package(MINIZIP "1.0" REQUIRED)]], "", {plain = true})
-            end
-            packagedeps = {"minizip"}
-        end
-        import("package.tools.cmake").install(package, configs, {packagedeps = packagedeps})
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
