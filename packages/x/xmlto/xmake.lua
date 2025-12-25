f6de1bf59f774d0dd21d5ec9aa60f7f1360ab089
Casinius <68890331+Casinius@users.noreply.github.com>
2025-12-25 14:11:04 +0800

    Xmlto: FIx implicit int (#8447)
    * fix implicit int
    
    * add flex
    
    ---------
    
    Co-authored-by: cyan <cyan@cyan-81b1>
    Co-authored-by: star9029 <hengxings783@gmail.com>

--- packages/x/xmlto/xmake.lua
+++ packages/x/xmlto/xmake.lua
@@ -1,5 +1,4 @@
 package("xmlto")
-
     set_kind("binary")
     set_homepage("https://pagure.io/xmlto")
     set_description("Convert XML to another format (based on XSL or other tools)")
@@ -9,10 +8,15 @@ package("xmlto")
              "https://pagure.io/xmlto.git")
     add_versions("0.0.28", "1130df3a7957eb9f6f0d29e4aa1c75732a7dfb6d639be013859b5c7ec5421276")
 
+    add_deps("flex", {kind = "binary"})
     add_deps("util-linux")
 
     on_install("macosx", "linux", function (package)
         import("package.tools.autoconf")
+
+        io.replace("xmlif/xmlif.l",[[static ifsense;]],[[static int ifsense;]],{plain = true})
+        io.replace("xmlif/xmlif.l",[[main(int argc, char *argv[])]],[[int main(int argc, char *argv[])]],{plain = true})
+
         local envs = autoconf.buildenvs(package)
         local getopt = package:dep("util-linux"):fetch()
         for _, dir in ipairs(getopt.linkdirs) do
