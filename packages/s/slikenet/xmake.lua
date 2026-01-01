4d0740e0c8b1ab1c8228f8b23ed9997a8b12aec6
Saikari <lin@sz.cn.eu.org>
2026-01-02 01:33:18 +0300

    retry

--- packages/s/slikenet/xmake.lua
+++ packages/s/slikenet/xmake.lua
@@ -9,7 +9,7 @@ package("slikenet")
     add_patches("2021.07.01", "patches/2021.07.01/fix-emscripten.patch", "c4d8ffbbdb5fe1ca95f6fd7dbdafb83976fa5de9435e80e6aab5f8c34ae74d62")
     add_patches("2021.07.01", "patches/2021.07.01/fix-install.patch", "7f1307651bbf7fdff4cedf1b0301521275d83a060361ffc896065254c9908953")
     add_patches("2021.07.01", "patches/2021.07.01/fix-mingw.patch", "904ee48e53f31cf0a4cd40cef3db50ff64d641e40089816d4f0923b10ddcff81")
-    add_patches("2021.07.01", "patches/2021.07.01/fix-dllimport.patch", "edbfbe0e5bb078b9cad7ce69df68b9ee7257aa7a5318685b4b72133f3e780eef")
+    add_patches("2021.07.01", "patches/2021.07.01/fix-dllimport.patch", "88064d4caf7782039535d917a75b28f3f83162f8b6e4949670d9755a8c8091f8")
 
     add_deps("cmake")
     add_deps("openssl3")
@@ -21,7 +21,9 @@ package("slikenet")
     end
 
     on_load("windows", "mingw", function (package)
-        package:add("defines", "_RAKNET_DLL_IMPORT")
+        if package:config("shared") then
+            package:add("defines", "_RAKNET_DLL_IMPORT")
+        end
     end)
 
     on_check("android", function (package)
