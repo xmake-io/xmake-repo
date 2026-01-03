04bd4496f8569b9cd9855c9ba4eec8e8ad2ae36f
Saikari <lin@sz.cn.eu.org>
2026-01-03 18:32:33 +0300

    gpm: add new package (#8971)
    * gmp: add new package
    
    * cleaning
    
    * fixup
    
    * Add ncurses
    
    * guard ncurses
    
    * re-check
    
    * Assume add_deps("bison") is enough for yacc
    
    * test
    
    * retry gathering info
    
    * limit to linux and cross
    
    * c11
    
    * GPL-2.0-or-later
    
    * https://www.nico.schottelius.org/software/gpm/
    
    * cls
    
    * rm etc

---
+++ packages/g/gpm/xmake.lua
@@ -0,0 +1,61 @@
+package("gpm")
+    set_homepage("https://www.nico.schottelius.org/software/gpm/")
+    set_description("general purpose mouse")
+    set_license("GPL-2.0-or-later")
+
+    add_urls("https://github.com/telmich/gpm.git")
+    add_versions("2020.06.17", "e82d1a653ca94aa4ed12441424da6ce780b1e530")
+
+    add_deps("autotools", "bison")
+    add_deps("ncurses")
+
+    on_install("linux", "cross", function (package)
+        for _, dir in ipairs({"bin", "sbin", "lib", "include"}) do
+            os.mkdir(path.join(package:installdir(), dir))
+        end
+        io.replace("src/Makefile.in",
+            [[gpm lib/libgpm.so.@abi_lev@ lib/libgpm.so @LIBGPM_A@ $(PROG)]],
+            [[gpm lib/libgpm.so.@abi_lev@ lib/libgpm.so lib/libgpm.a $(PROG)]], {plain = true})
+        local configs = {}
+        table.insert(configs, "--enable-static")
+        table.insert(configs, "--enable-shared")
+        if package:is_debug() then
+            table.insert(configs, "--enable-debug")
+        end
+        local envs = import("package.tools.autoconf").buildenvs(package)
+        envs.CFLAGS = (envs.CFLAGS or "") .. " -std=c11"
+        import("package.tools.autoconf").install(package, configs, {
+            envs = envs,
+            makeconfigs = {
+                "-C", "src",
+                "install",
+                "PREFIX=" .. path.unix(package:installdir())
+            }
+        })
+        local libdir = path.join(package:installdir(), "lib")
+        os.cd(libdir)
+        if package:config("shared") then
+            local files = os.files("libgpm.so.*")
+            if #files > 0 then
+                os.ln(path.filename(files[1]), "libgpm.so")
+            end
+            os.tryrm("libgpm.a")
+        else
+            os.tryrm("libgpm.so*")
+        end
+    end)
+
+    on_test(function (package)
+        assert(package:check_csnippets({test = [[
+            #include <gpm.h>
+            void test() {
+                Gpm_Connect conn = {
+                    .eventMask = GPM_DOWN | GPM_UP | GPM_DRAG | GPM_MOVE,
+                    .defaultMask = 0,
+                    .minMod = 0,
+                    .maxMod = (unsigned short) ~(1 << 0)
+                };
+                Gpm_Open(&conn, 0);
+            }
+        ]]}, {configs = {languages = "c11"}}))
+    end)
