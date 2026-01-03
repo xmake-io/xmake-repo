77be00723d93ebe887449365ec853254fc064348
Saikari <lin@sz.cn.eu.org>
2026-01-03 14:12:18 +0300

    floatengine: add new package

---
+++ packages/f/floatengine/xmake.lua
@@ -0,0 +1,46 @@
+package("floatengine")
+    set_homepage("https://github.com/Fls-Float/FloatEngine")
+    set_description("FloatEngine的源代码")
+    set_license("MIT")
+
+    add_urls("https://github.com/fls-float/FloatEngine.git")
+    add_versions("2025.12.20", "d3754c2b8235fe1920aea65cfd7cd9247c758408")
+
+    add_deps("minizip-ng", {configs = {bzip2 = true}})
+    add_deps("lua", "libcurl", "slikenet", "nlohmann_json", "nativefiledialog-extended", "fls-float-raylib", "sol2")
+
+    on_install("windows", "mingw", function (package)
+        io.replace("FloatEngine/F_Resource.cpp", [[#include "minizip-ng/]], [[#include "minizip/]], {plain = true})
+        io.replace("FloatEngine/F_Network.cpp", [[#include "F_NetWork.h"]], [[#include "F_Network.h"]], {plain = true})
+        io.replace("FloatEngine/F_Network.cpp", [[#include "slikenet/Peer.h"]], [[#include "slikenet/peer.h"]], {plain = true})
+        io.replace("FloatEngine/F_Network.cpp", [[#include "slikenet/Types.h"]], [[#include "slikenet/types.h"]], {plain = true})
+        io.replace("FloatEngine/FMath.cpp", "#include <numeric>", "#include <numeric>\n#include <cfloat>", {plain = true})
+        io.replace("FloatEngine/F_Network.h", "#include <memory>", "#include <memory>\n#include <cstdint>", {plain = true})
+        local configs = {}
+        io.writefile("xmake.lua", [[
+            set_languages("c11", "c++17")
+            add_rules("mode.release", "mode.debug")
+            add_requires("minizip-ng", {configs = {bzip2 = true}})
+            add_requires("lua", "libcurl", "slikenet", "nlohmann_json", "nativefiledialog-extended", "fls-float-raylib", "sol2")
+            add_packages("minizip-ng", "lua", "libcurl", "slikenet", "nlohmann_json", "nativefiledialog-extended", "fls-float-raylib", "sol2")
+            set_encodings("utf-8")
+            target("floatengine")
+                set_kind("$(kind)")
+                add_files("FloatEngine/*.cpp",
+                          "FloatEngine/*.c")
+                add_headerfiles("(FloatEngine/*.h)",
+                                "(FloatEngine/*.hpp)")
+                add_includedirs("FloatEngine",
+                                "FloatEngine/gui/include")
+        ]])
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                Sprite sprite = Sprite();
+                size_t w = sprite.FrameCount();
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "FloatEngine/Sprite.h"}))
+    end)
