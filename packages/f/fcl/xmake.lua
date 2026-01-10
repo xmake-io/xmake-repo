c5b656df3eddbf5cc5e022d220c8198fbcb8c944
Willaaaaaaa <willaaaaaaa936@gmail.com>
2026-01-10 13:38:32 +0800

    try old eigen with low c++std

--- packages/f/fcl/xmake.lua
+++ packages/f/fcl/xmake.lua
@@ -1,21 +1,21 @@
 package("fcl")
-
     set_homepage("https://github.com/flexible-collision-library/fcl")
     set_description("Flexible Collision Library")
     set_license("BSD-3-Clause")
 
     add_urls("https://github.com/flexible-collision-library/fcl/archive/refs/tags/$(version).tar.gz",
              "https://github.com/flexible-collision-library/fcl.git")
-    add_versions("v0.6.1", "c8a68de8d35a4a5cd563411e7577c0dc2c626aba1eef288cb1ca88561f8d8019")
+
     add_versions("0.7.0", "90409e940b24045987506a6b239424a4222e2daf648c86dd146cbcb692ebdcbc")
+    add_versions("v0.6.1", "c8a68de8d35a4a5cd563411e7577c0dc2c626aba1eef288cb1ca88561f8d8019")
 
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
     add_configs("octomap", {description = "Enable OctoMap library support.", default = false, type = "boolean"})
 
-    add_deps("cmake")
-    add_deps("eigen", "libccd")
+    add_deps("cmake", "eigen 3.4.1", "libccd")
+
     on_load(function (package)
         if package:config("octomap") then
             package:add("deps", "octomap")
@@ -24,7 +24,7 @@ package("fcl")
 
     on_install("windows", "macosx", "linux", function (package)
         local configs = {"-DFCL_BUILD_TESTS=OFF"} 
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DFCL_STATIC_LIBRARY=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DFCL_WITH_OCTOMAP=" .. (package:config("octomap") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
@@ -40,5 +40,5 @@ package("fcl")
                 SamplerR<double, 4> sampler(lower, upper);
                 auto sp = sampler.sample();
             }   
-        ]]}, {configs = {languages = "c++14"}, includes = "fcl/math/sampler/sampler_r.h"}))
+        ]]}, {configs = {languages = "c++11"}, includes = "fcl/math/sampler/sampler_r.h"}))
     end)
