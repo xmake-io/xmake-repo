39c354d2aa5a09f819a816d7b0a52461fe3db527
Saikari <lin@sz.cn.eu.org>
2026-01-10 11:57:52 +0300

    Test ftgl

--- packages/f/ftgl/xmake.lua
+++ packages/f/ftgl/xmake.lua
@@ -1,13 +1,14 @@
 package("ftgl")
-
     set_homepage("https://github.com/frankheckenbach/ftgl")
     set_description("FTGL is a free open source library to enable developers to use arbitrary fonts in their OpenGL applications.")
     set_license("MIT")
 
     add_urls("https://github.com/frankheckenbach/ftgl/archive/refs/tags/$(version).tar.gz")
+
     add_versions("v2.4.0", "aa97da1c3442a8fd3941037655df18016d70b5266381c81d81e8b5335f196ea8")
 
-    add_patches("v2.4.0", path.join(os.scriptdir(), "patches", "2.4.0", "cmake.patch"), "9006ccfba2632c5ffee50c76f163a98d9867a3e11c6390c14ed07ab401a85f29")
+    add_patches("v2.4.0", "patches/2.4.0/cmake.patch", "9006ccfba2632c5ffee50c76f163a98d9867a3e11c6390c14ed07ab401a85f29")
+    add_patches("v2.4.0", "patches/2.4.0/fix-type-error.patch", "6d5080d92e8d18d39d7f4ad9279bae555fa8af6f9dcfe3b437647e09ffc88312")
 
     add_deps("cmake", "freetype", "opengl")
     on_load("windows", function (package)
