8e2ff531a8503da480b58c95fae823b03d55dec6
Saikari <lin@sz.cn.eu.org>
2026-01-10 03:44:54 +0300

    tvision: Try to use `gpm` (#9029)
    * tvision: Try to use `gpm`
    
    * Update xmake.lua

--- packages/t/tvision/xmake.lua
+++ packages/t/tvision/xmake.lua
@@ -11,6 +11,8 @@ package("tvision")
 
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    elseif is_plat("linux", "cross") then
+        add_configs("gpm", {description = "Enable linking to libgpm", default = true, type = "boolean"})
     end
 
     add_deps("cmake")
@@ -23,15 +25,21 @@ package("tvision")
         add_deps("ncurses")
     end
 
+    on_load("linux", "cross", function (package)
+        if package:config("gpm") then
+            package:add("deps", "gpm")
+        end
+    end)
+
     on_install("!wasm and !iphoneos and (!android or android@!windows)", function (package)
         if not package:is_plat("windows") then
             io.replace("source/CMakeLists.txt", "add_library(${PROJECT_NAME} STATIC ${TVSOURCE})", "add_library(${PROJECT_NAME} ${TVSOURCE})", {plain = true})
         end
         local configs = {
             "-DTV_BUILD_EXAMPLES=OFF",
-            "-DTV_BUILD_TESTS=OFF",
-            "-DTV_BUILD_USING_GPM=OFF"
+            "-DTV_BUILD_TESTS=OFF"
         }
+        table.insert(configs, "-DTV_BUILD_USING_GPM=" .. (package:config("gpm") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
