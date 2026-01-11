272a344423b2d71293d57a23437201faae1f4541
Saikari <lin@sz.cn.eu.org>
2026-01-11 12:18:59 +0300

    macdylibbundler: add more platforms

--- packages/m/macdylibbundler/xmake.lua
+++ packages/m/macdylibbundler/xmake.lua
@@ -8,19 +8,35 @@ package("macdylibbundler")
 
     add_versions("1.0.5", "d48138fd6766c70097b702d179a657127f9aed3d083051c2d4fce145881a316e")
 
-    on_install("linux", "macosx", "android", function (package)
+    on_load(function (package)
+        if not package:is_cross() then
+            package:addenv("PATH", "bin")
+        end
+    end)
+
+    on_install(function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
+            set_languages("c++11")
             target("macdylibbundler")
                 set_kind("$(kind)")
-                set_languages("c++11")
                 add_files("src/*.cpp")
+                remove_files("src/main.cpp")
                 add_includedirs("src")
-                add_headerfiles("src/*.h")]])
+                add_headerfiles("src/*.h")
+            
+            target("macdylibbundler-cli")
+                set_basename("macdylibbundler")
+                set_kind("binary")
+                add_files("src/main.cpp")
+                add_deps("macdylibbundler")]])
         import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
+        if not package:is_cross() then
+            os.vrun("macdylibbundler --help")
+        end
         assert(package:check_cxxsnippets({test = [[
             void test() {
                 collectSubDependencies();
