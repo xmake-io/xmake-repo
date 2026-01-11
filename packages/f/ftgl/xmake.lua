4b0b468c06cfe13b8136a5cd229b3d84f0880e42
star9029 <hengxings783@gmail.com>
2026-01-11 23:03:42 +0800

    use pkgconf

--- packages/f/ftgl/xmake.lua
+++ packages/f/ftgl/xmake.lua
@@ -9,10 +9,16 @@ package("ftgl")
     add_versions("v2.4.0", "aa97da1c3442a8fd3941037655df18016d70b5266381c81d81e8b5335f196ea8")
 
     add_patches("v2.4.0", "patches/2.4.0/cmake.patch", "9006ccfba2632c5ffee50c76f163a98d9867a3e11c6390c14ed07ab401a85f29")
+    add_patches("v2.4.0", "patches/2.4.0/cmake-freetype.patch", "644ff180e230e6ff49139fe18f34e10c648e4993324f8faf48b12d9f19d61f6f")
     add_patches("v2.4.0", "patches/2.4.0/fix-type-error.patch", "6d5080d92e8d18d39d7f4ad9279bae555fa8af6f9dcfe3b437647e09ffc88312")
 
     add_deps("cmake")
-    add_deps("freetype")
+    if is_subhost("windows") then
+        add_deps("pkgconf")
+    else
+        add_deps("pkg-config")
+    end
+    add_deps("freetype", {configs = {zlib = true}})
     add_deps("opengl", {optional = true})
 
     on_load("windows", "mingw", function (package)
@@ -25,7 +31,7 @@ package("ftgl")
         local configs = {"-DBUILD_TESTS=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs, {packagedeps = "freetype"})
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
