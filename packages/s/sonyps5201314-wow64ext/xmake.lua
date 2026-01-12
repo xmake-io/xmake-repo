f6b9a924fe331f53a28e2b9ea4d97607fbea11d1
Saikari <lin@sz.cn.eu.org>
2026-01-12 20:25:40 +0300

    sigh try zezula

--- packages/s/sonyps5201314-wow64ext/xmake.lua
+++ packages/s/sonyps5201314-wow64ext/xmake.lua
@@ -4,17 +4,20 @@ package("sonyps5201314-wow64ext")
     set_license("LGPL-3.0-or-later")
 
     add_urls("https://github.com/sonyps5201314/wow64ext.git", {alias = "git"})
-    
-    add_versions("git:2026.01.01", "e51083dd7b60c68a103791d3d258547022d55ac8")
-    add_resources("2026.01.01", "ntdll", "https://github.com/sonyps5201314/ntdll.git", "549726c4348118f5e74a68b337366e5158c83e5c")
 
-    on_install("windows", "mingw", function (package)
+    add_versions("git:2026.01.01", "e51083dd7b60c68a103791d3d258547022d55ac8")
+    add_resources("2026.01.01", "ntdll", "https://github.com/ladislav-zezula/Aaa.git", "3c8ff5d8648f569bfde536751981b0466942c222")
+
+    on_install("windows", function (package)
+        io.replace("wow64ext/wow64ext.h", [[#ifndef _WIN64]], [[]], {plain = true})
+        io.replace("wow64ext/wow64ext.h", [[typedef XSAVE_FORMAT XMM_SAVE_AREA32, * PXMM_SAVE_AREA32;
+#endif]], [[typedef XSAVE_FORMAT XMM_SAVE_AREA32, * PXMM_SAVE_AREA32;]], {plain = true})
         local ntdlldir = package:resourcefile("ntdll")
-        os.cp(path.join(ntdlldir, "include/ntdll.h"), "wow64ext/ntdll.h")
+        os.cp(path.join(ntdlldir, "inc/ntdll.h"), "wow64ext/ntdll.h")
         if package:check_sizeof("void*") == "8" then
-            os.cp(path.join(ntdlldir, "lib/amd64/ntdll.lib"), "wow64ext/ntdll.lib")
+            os.cp(path.join(ntdlldir, "lib64/Ntdll.lib"), "wow64ext/ntdll.lib")
         else
-            os.cp(path.join(ntdlldir, "lib/ntdll.lib"), "wow64ext/ntdll.lib")
+            os.cp(path.join(ntdlldir, "lib32/Ntdll.lib"), "wow64ext/ntdll.lib")
         end
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
