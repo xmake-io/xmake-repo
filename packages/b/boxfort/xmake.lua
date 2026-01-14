2c2c879ca4f2f23a5b7c7f72aa290b984fb0c7e4
Saikari <lin@sz.cn.eu.org>
2026-01-14 20:18:57 +0300

    resolve

--- packages/b/boxfort/xmake.lua
+++ packages/b/boxfort/xmake.lua
@@ -7,7 +7,7 @@ package("boxfort")
 
     add_versions("2024.05.02", "1018a44e63b57e9ffc3b8b68b6c8a8aa8f342d2a")
 
-    add_patches("2024.05.02", "patches/android-shm.patch", "91f600e5f51386b2f92baf345ef74d916c2b0fe632bd883b157dadf6298f714c")
+    add_patches("2024.05.02", "patches/android-shm.patch", "619a3e56cd6bb040d522acbf610603bf22e68a435742aadc1746793c32d44e99")
     add_patches("2024.05.02", "patches/arm64-windows-setjmp.patch", "ba4c26eee443e60d9592d78f8aca6c1458b0df4a8c37896f475d8c05a1982767")
     add_patches("2024.05.02", "patches/arm64-windows-trampoline.patch", "fb03e5484e4f7b0b8945708483937c79da704c38e7cf0d1acaffb821f5338c60")
     add_patches("2024.05.02", "patches/arm64-windows-use-armas64.patch", "0fbdb9baf62c63311cef7497cf9be1c47c2d21166f7acbe2390a5ab14ae4b059")
