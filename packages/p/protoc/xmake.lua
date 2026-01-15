dcf40b64d18e099d48e171acee7486dc1500b2b5
Saikari <lin@sz.cn.eu.org>
2026-01-15 19:48:53 +0300

    Update dependencies to include tools configuration

--- packages/p/protoc/xmake.lua
+++ packages/p/protoc/xmake.lua
@@ -3,7 +3,7 @@ package("protoc")
     set_homepage("https://developers.google.com/protocol-buffers/")
     set_description("Google's data interchange format compiler")
 
-    add_deps("protobuf-cpp")
+    add_deps("protobuf-cpp", {configs = {tools = true}})
 
     on_install("@windows|x86", "@windows|x64", "@windows|arm64*", "@msys", "@cygwin", "@macosx", "@bsd", "@linux", function (package) end)
 
