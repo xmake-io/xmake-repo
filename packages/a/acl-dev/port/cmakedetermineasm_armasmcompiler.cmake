7a06d7029b559a4bd2013b18fa6678a8b36c5fab
Saikari <lin@sz.cn.eu.org>
2026-01-17 00:50:31 +0300

    re

--- packages/a/acl-dev/port/cmakedetermineasm_armasmcompiler.cmake
+++ packages/a/acl-dev/port/cmakedetermineasm_armasmcompiler.cmake
@@ -1,11 +1,14 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
 
-# Find the armasm assembler, armasm or armasm64
 
-set(ASM_DIALECT "_ARMASM")
+# Find the MS ARM assembler (marmasm or marmasm64)
+
+set(ASM_DIALECT "_MARMASM")
 
 # if we are using the 64bit cl compiler, assume we also want the 64bit assembler
-if(";${CMAKE_VS_PLATFORM_NAME};${MSVC_C_ARCHITECTURE_ID};${MSVC_CXX_ARCHITECTURE_ID};"
-  MATCHES ";ARM64;")
+if(";${CMAKE_VS_PLATFORM_NAME};${CMAKE_C_COMPILER_ARCHITECTURE_ID};${CMAKE_CXX_COMPILER_ARCHITECTURE_ID};"
+    MATCHES ";(ARM64);")
   set(CMAKE_ASM${ASM_DIALECT}_COMPILER_INIT armasm64)
 else()
   set(CMAKE_ASM${ASM_DIALECT}_COMPILER_INIT armasm)
