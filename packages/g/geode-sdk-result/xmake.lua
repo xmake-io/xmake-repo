38fc270244cfa49b9cf1148a78723e9a0a7c791a
Saikari <lin@sz.cn.eu.org>
2026-01-18 00:56:55 +0300

    sigh

--- packages/g/geode-sdk-result/xmake.lua
+++ packages/g/geode-sdk-result/xmake.lua
@@ -9,6 +9,18 @@ package("geode-sdk-result")
 
     add_versions("v1.3.5", "62bd9cc3abe98640d673c95571eb9978434a22351dfff4efef6cfdc3f94af36a")
 
+    if on_check then
+        on_check(function (package)
+            assert(package:check_cxxsnippets({test = [[
+                #include <concepts>
+                void test() {
+                    static_assert(std::constructible_from<int, int>);
+                    static_assert(std::constructible_from<double, float>);
+                }
+            ]]}, {configs = {languages = "c++20"}}), "package(geode-sdk-result) Require at least C++20 (supports std::constructible_from).")
+        end)
+    end
+
     on_install(function (package)
         os.cp("include", package:installdir())
     end)
