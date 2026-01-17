f6764718d35f1df3aa34692803a1cc86e66420f1
Jérôme Leclercq <lynix680@gmail.com>
2026-01-17 11:25:16 +0100

    Update xmake.lua

--- packages/c/csfml/xmake.lua
+++ packages/c/csfml/xmake.lua
@@ -92,7 +92,7 @@ package("csfml")
         end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.cmake").install(package, configs, {packagedeps = "zlib"})
     end)
 
     on_test(function (package)
