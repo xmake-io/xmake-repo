41d87d49417f98c4b3c523a15cc143ee9f47fbc1
apocelipes <seve3r@outlook.com>
2026-01-19 16:46:00 +0800

    awk: clean up & add more platforms

--- packages/a/awk/xmake.lua
+++ packages/a/awk/xmake.lua
@@ -2,13 +2,15 @@ package("awk")
     set_kind("binary")
     set_homepage("https://github.com/onetrueawk/awk")
     set_description("One true awk")
+    set_license("MIT-Lucent")
 
-    add_urls("https://github.com/onetrueawk/awk.git")
-    add_versions("2021.12.26", "b9c01f51224fd302519e8a35bd06effc06f6d3d1")
+    add_urls("https://github.com/onetrueawk/awk/archive/refs/tags/$(version).tar.gz", 
+            "https://github.com/onetrueawk/awk.git")
+    add_versions("20251225", "626d7d19f8e4ceae70f60e2e662291789e0f54ab86945317a3d5693c30f847a2")
 
     add_deps("bison")
 
-    on_install("macosx", "linux", function (package)
+    on_install("macosx", "linux", "bsd", function (package)
         local configs = {}
         import("package.tools.make").make(package, configs)
         os.cp("a.out", path.join(package:installdir("bin"), "awk"))
