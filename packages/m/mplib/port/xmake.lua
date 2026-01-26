89a80029e87810634440db1759f1fe62f9944574
star9029 <hengxings783@gmail.com>
2026-01-26 11:30:29 +0800

    fix windows

--- packages/m/mplib/port/xmake.lua
+++ packages/m/mplib/port/xmake.lua
@@ -13,6 +13,7 @@ target("mp")
     set_kind("$(kind)")
     add_files("src/**.cpp")
     add_includedirs("include", {public = true})
+    add_cxflags("/permissive-", "/bigobj", {tools = "cl"})
     add_headerfiles("include/(**.h)")
 
     add_packages("ompl", "fcl", "pinocchio", "assimp", "orocos-kdl", "urdfdom", {public = true})
--- packages/m/mplib/xmake.lua
+++ packages/m/mplib/xmake.lua
@@ -9,6 +9,9 @@ package("mplib")
     add_versions("v0.2.1", "ea5b549965994ab7794c73f9c95f49590e473e76513f48e6feb4b37c53e5c4da")
 
     add_configs("python", {description = "Build Python bindings.", default = false, type = "boolean"})
+    if is_plat("windows") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
 
     add_deps("ompl", "assimp", "orocos-kdl", "urdfdom")
     add_deps("pinocchio v2.7.1", {configs = {urdf = true}})
