c479076d8134346c841d8217fb8fa2a7e426c767
Saikari <lin@sz.cn.eu.org>
2026-01-29 10:20:08 +0300

    update avisynthplus package version and add wasm support

--- packages/a/avisynthplus/xmake.lua
+++ packages/a/avisynthplus/xmake.lua
@@ -1,17 +1,21 @@
 package("avisynthplus")
     set_homepage("http://avs-plus.net")
     set_description("AviSynth with improvements")
+    set_license("GPL-2.0")
 
     add_urls("https://github.com/AviSynth/AviSynthPlus/archive/refs/tags/$(version).tar.gz",
              "https://github.com/AviSynth/AviSynthPlus.git")
 
-    add_versions("v3.7.5", "2533fafe5b5a8eb9f14d84d89541252a5efd0839ef62b8ae98f40b9f34b3f3d5")
+    add_versions("2026.01.01", "f2f4e318a5ba161d4945185f11c6a27e7aafa92b")
 
     add_deps("cmake")
     add_deps("ghc_filesystem")
 
     on_install(function (package)
         local configs = {"-DENABLE_PLUGINS=OFF"}
+        if package:is_plat("wasm") then
+            io.replace("CMakeLists.txt", "-msse2", "-msimd128 -msse2", {plain = true})
+        end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
