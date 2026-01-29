3bf4915cbc7c4a0d6f33154e22a064c422f52977
apocelipes <seve3r@outlook.com>
2026-01-29 20:38:00 +0800

    libxext: update to v1.3.7

--- packages/l/libxext/xmake.lua
+++ packages/l/libxext/xmake.lua
@@ -1,10 +1,12 @@
 package("libxext")
     set_homepage("https://www.x.org/")
     set_description("X.Org: Library for common extensions to the X11 protocol")
+    set_license("X11")
 
     set_urls("https://www.x.org/archive/individual/lib/libXext-$(version).tar.gz")
     add_versions("1.3.5", "1a3dcda154f803be0285b46c9338515804b874b5ccc7a2b769ab7fd76f1035bd")
     add_versions("1.3.6", "1a0ac5cd792a55d5d465ced8dbf403ed016c8e6d14380c0ea3646c4415496e3d")
+    add_versions("1.3.7", "6564608dc3b816b0cfddf0c7ddc62bc579055dd70b2f28113a618df2acb64189")
 
     if is_plat("linux") then
         add_extsources("apt::libxext-dev")
