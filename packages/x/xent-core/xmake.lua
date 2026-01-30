375a46cf66a3954173e3ca6fb3ea42e1c865d6bd
Saikari <lin@sz.cn.eu.org>
2026-01-30 17:38:27 +0300

    Enhance xent-core package with WASM support and C++20 checks

--- packages/x/xent-core/xmake.lua
+++ packages/x/xent-core/xmake.lua
@@ -6,7 +6,24 @@ package("xent-core")
     add_urls("https://github.com/Project-Xent/xent-core.git")
     add_versions("2026.01.30", "cb485c8dc62c33fbdcf188385222193f39be3f1f")
 
+    if is_plat("wasm") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
     add_deps("yoga")
+
+    if on_check then
+        on_check(function (package)
+            assert(package:check_cxxsnippets({test = [[
+                #include <bit>
+                #include <cstdint>
+                void test() {
+                    constexpr double f64v = 19880124.0; 
+                    constexpr auto u64v = std::bit_cast<std::uint64_t>(f64v);
+                }
+            ]]}, {configs = {languages = "c++20"}}), "package(xent-core) Require at least C++20.")
+        end)
+    end
 
     on_install(function (package)
         io.writefile("xmake.lua", [[
@@ -19,6 +36,9 @@ package("xent-core")
                 add_files("src/*.cpp")
                 add_headerfiles("include/xent/(**.hpp)", {prefixdir = "xent"})
                 add_packages("yoga")
+                if is_plat("windows") and is_kind("shared") then
+                    add_rules("utils.symbols.export_all", {export_classes = true})
+                end
         ]])
         import("package.tools.xmake").install(package)
     end)
