f82777127b18aaf97a79290895323fcf99a8b7ad
Saikari <lin@sz.cn.eu.org>
2026-01-30 18:12:35 +0300

    add avisynthplus package (#9215)
    * add avisynthplus package
    
    * update avisynthplus package version and add wasm support
    
    * refactor avisynthplus package installation logic and add platform checks
    
    * Update xmake.lua
    
    * Update on_install condition to exclude android and cross
    
    * Add conditional linking for Windows platform
    
    * simplify test aas .def file is not friendly
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Add MinGW check for 64-bit support in avisynthplus package

---
+++ packages/a/avisynthplus/xmake.lua
@@ -0,0 +1,41 @@
+package("avisynthplus")
+    set_homepage("http://avs-plus.net")
+    set_description("AviSynth with improvements")
+    set_license("GPL-2.0")
+
+    add_urls("https://github.com/AviSynth/AviSynthPlus.git")
+
+    add_versions("2026.01.01", "f2f4e318a5ba161d4945185f11c6a27e7aafa92b")
+
+    add_deps("cmake")
+    add_deps("ghc_filesystem")
+
+    if on_check then
+        on_check("mingw", function (package)
+            assert(package:check_sizeof("void*") == "8", "package(avisynthplus) MinGW requires to be 64-bit, 32-bit is not supported.")
+        end)
+    end
+
+    on_load("windows", "mingw", function (package)
+        if not package:config("shared") then
+            package:add("defines", "AVS_STATIC_LIB")
+        end
+    end)
+
+    on_install("!wasm and !android and !cross", function (package)
+        io.replace("avs_core/CMakeLists.txt", "if (MINGW)", "if (0)", {plain = true})
+        local configs = {
+            "-DENABLE_PLUGINS=OFF"
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                AvisynthError("test");
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "avisynth/avisynth.h"}))
+    end)
