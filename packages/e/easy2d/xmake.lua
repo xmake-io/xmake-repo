71c94c42d15d149d266f1d8281bfd12c93c81162
ChestnutYueyue <952134128@qq.com>
2026-02-03 21:00:31 +0800

    Refactor(easy2d): Optimize xmake build configuration and test logic
    - Simplify the build configuration logic and merge conditional judgments
    - Add UTF-8 encoding support and additional compilation warning options
    - Update test cases and replace simple header file checks with code snippet tests

--- packages/e/easy2d/xmake.lua
+++ packages/e/easy2d/xmake.lua
@@ -5,45 +5,36 @@ package("easy2d")
 
     add_urls("https://github.com/ChestnutYueyue/Easy2D/archive/refs/tags/$(version).tar.gz",
              "https://github.com/ChestnutYueyue/Easy2D.git")
-    add_versions("v2.1.27", "787B792C80C9BCD091D5E7B36A3E8707D7230D7EE183A318190E33A71B328622")
+    add_versions("v2.1.27", "787b792c80c9bcd091d5e7b36a3e8707d7230d7ee183a318190e33a71b328622")
 
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean"})
 
     add_syslinks("user32", "gdi32", "shell32", "winmm", "imm32", "version", "ole32", "comdlg32", "dinput8", "d2d1", "dwrite", "dxguid")
 
-    on_load("windows", "mingw", function(package)
-        if package:config("shared") then
-            package:add("defines", "EASY2D_SHARED")
-        end
-    end)
-
     on_install("windows", "mingw", function(package)
-        local configs = {}
-        configs.mode = package:is_debug() and "debug" or "release"
-        if package:config("shared") then
-            configs.kind = "shared"
-        else
-            configs.kind = "static"
-        end
-
-        local libcxxflags = {}
-        if package:is_plat("windows") then
-            table.insert(libcxxflags, "/std:c++17")
-            if not package:is_debug() then
-                table.insert(libcxxflags, "/O2")
-            end
-        else
-            if not package:is_debug() then
-                table.insert(libcxxflags, "-O2")
-            end
-        end
-
-        import("package.tools.xmake").install(package, configs, {cxxflags = libcxxflags})
+        local configs = {
+            mode = package:is_debug() and "debug" or "release",
+            kind = package:config("shared") and "shared" or "static",
+            cxxflags = package:is_plat("windows") and "/utf-8" or "-finput-charset=UTF-8 -fexec-charset=UTF-8",
+            flags = {
+                "-Wall",
+                "-Wextra",
+                "-Wpedantic",
+            },
+            definesflags = {
+                "-D_EASY2D_SHARED",
+            },
+        }
+        import("package.tools.xmake").install(package, configs)
 
         os.cp("Easy2D/include/easy2d", package:installdir("include"))
         os.cp("Easy2D/include/spdlog", package:installdir("include"))
     end)
 
-    on_test(function(package)
-        assert(package:has_cxxincludes("easy2d/easy2d.h", {configs = {languages = "c++17", cxxflags = package:is_plat("windows") and "/utf-8" or "-finput-charset=UTF-8 -fexec-charset=UTF-8"}}))
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                EASY_FUNCTION();
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = {"easy2d/easy2d.h"}}))
     end)
