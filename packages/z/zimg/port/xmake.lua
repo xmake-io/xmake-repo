859c128bcbf997c7bd7e59d36fb23a16bbbe439f
star9029 <hengxings783@gmail.com>
2026-02-03 10:10:13 +0800

    clean

--- packages/z/zimg/port/xmake.lua
+++ packages/z/zimg/port/xmake.lua
@@ -4,15 +4,10 @@ add_rules("mode.release", "mode.debug")
 
 set_languages("c++17")
 
-local dirs = os.dirs("src/zimg/*")
-
 target("zimg")
     set_kind("$(kind)")
     add_includedirs("src/zimg")
-    for _, dir in ipairs(dirs) do
-        add_files(path.join(dir, "*.cpp"))
-    end
-
+    add_files("src/zimg/*/*.cpp")
     add_headerfiles(
         "src/zimg/api/*.h",
         "src/zimg/api/*.hpp"
@@ -23,15 +18,9 @@ target("zimg")
     if has_config("simd") then
         if is_arch("x86", "x64", "x86_64", "i386") then
             add_defines("ZIMG_X86")
-
-            for _, dir in ipairs(dirs) do
-                add_files(path.join(dir, "x86/*.cpp"), {vectorexts = "all"})
-            end
+            add_files("src/zimg/*/x86/*.cpp", {vectorexts = "all"})
         elseif is_arch("arm64", "arm64-v8a") then
             add_defines("ZIMG_ARM")
-
-            for _, dir in ipairs(dirs) do
-                add_files(path.join(dir, "arm/*.cpp"), {vectorexts = "all"})
-            end
+            add_files("src/zimg/*/arm/*.cpp", {vectorexts = "all"})
         end
     end
