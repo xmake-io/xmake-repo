8d51d1b91fab10ce59269e369dced478535b9c8a
apocelipes <seve3r@outlook.com>
2026-02-05 14:27:09 +0800

    fix build

--- packages/t/teascript/xmake.lua
+++ packages/t/teascript/xmake.lua
@@ -12,9 +12,28 @@ package("teascript")
     add_versions("v0.13.0", "7c8cc05a8775ee2c857278b5e353670bf02442b2fa3a411343e82b2b85eedced")
 
     add_patches("0.14.0", "patches/0.14.0/macosx.patch", "cae068739506806679f63e316ca4368f5750954d3083e525ae457abf973d672b")
+    add_patches("0.16.0", "patches/0.16.0/macosx.patch", "bc0c37cbdbf1d6d2c4b59483cc8e9cde2690014680995020d9e3c7a9850aea78")
 
     add_configs("fmt", {description = "Use fmt for printing.", default = true, type = "boolean"})
     add_configs("toml++", {description = "Enable toml support.", default = true, type = "boolean"})
+
+    if on_check then
+        on_check(function (package)
+            assert(package:check_cxxsnippets({test = [[
+                #include <thread>
+
+                int main() {
+                    std::stop_source ss;
+                    std::thread t([](std::stop_token st) {
+                        while (!st.stop_requested()) {}
+                    }, ss.get_token());
+                    ss.request_stop();
+                    t.join();
+                    return 0;
+                }
+            ]]}, {configs = {languages = "c++20"}}), "package(teascript) Require std::stop_token support.")
+        end)
+    end
 
     on_load(function (package)
         if package:config("fmt") then
