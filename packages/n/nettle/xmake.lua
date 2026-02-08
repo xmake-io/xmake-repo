63713018a591aded3020645f18297c56403f2785
apocelipes <seve3r@outlook.com>
2026-02-08 08:18:51 +0800

    nettle: update to v4.0 (#9291)
    * nettle: update to v4.0
    
    * fix the check code

--- packages/n/nettle/xmake.lua
+++ packages/n/nettle/xmake.lua
@@ -1,5 +1,4 @@
 package("nettle")
-
     set_homepage("https://www.lysator.liu.se/~nisse/nettle/")
     set_description("Nettle is a cryptographic library that is designed to fit easily in more or less any context.")
     set_license("LGPL-3.0")
@@ -9,6 +8,7 @@ package("nettle")
     add_versions("3.6", "d24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1")
     add_versions("3.9.1", "ccfeff981b0ca71bbd6fbcb054f407c60ffb644389a5be80d6716d5b550c6ce3")
     add_versions("3.10.1", "b0fcdd7fc0cdea6e80dcf1dd85ba794af0d5b4a57e26397eee3bc193272d9132")
+    add_versions("4.0", "3addbc00da01846b232fb3bc453538ea5468da43033f21bb345cb1e9073f5094")
 
     add_deps("m4")
     add_deps("gmp")
@@ -40,14 +40,18 @@ package("nettle")
     on_test(function (package)
         assert(package:check_csnippets([[
             void sha1_test(void) {
-                struct sha1_ctx ctx;
-                sha1_init(&ctx);
+                struct sha256_ctx ctx;
+                sha256_init(&ctx);
                 uint8_t const buffer[] = "test";
-                sha1_update(&ctx, sizeof(buffer), buffer);
-                uint8_t digest[SHA1_DIGEST_SIZE];
-                sha1_digest(&ctx, SHA1_DIGEST_SIZE, digest);
+                sha256_update(&ctx, sizeof(buffer), buffer);
+                uint8_t digest[SHA256_DIGEST_SIZE];
+                #if defined(NETTLE_VERSION_MAJOR) && (NETTLE_VERSION_MAJOR <= 3)
+                    sha256_digest(&ctx, SHA256_DIGEST_SIZE, digest);
+                #else
+                    sha256_digest(&ctx, digest);
+                #endif
             }
-        ]], {includes = "nettle/sha1.h"}))
+        ]], {includes = {"nettle/sha2.h", "nettle/version.h"}}))
         assert(package:check_csnippets([[
             void rsa_test(void) {
                 struct rsa_public_key pub;
