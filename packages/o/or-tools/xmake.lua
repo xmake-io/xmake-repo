033d970b873a1953159290384b160902e84b750a
star9029 <hengxings783@gmail.com>
2026-02-09 16:45:09 +0800

    fix find zlib

--- packages/o/or-tools/xmake.lua
+++ packages/o/or-tools/xmake.lua
@@ -4,12 +4,9 @@ package("or-tools")
     set_license("Apache-2.0")
 
     add_urls("https://github.com/google/or-tools/archive/refs/tags/$(version).tar.gz",
-                "https://github.com/google/or-tools.git")
+             "https://github.com/google/or-tools.git")
 
     add_versions("v9.15", "6395a00a97ff30af878ee8d7fd5ad0ab1c7844f7219182c6d71acbee1b5f3026")
-
-    add_deps("cmake")
-    add_deps("zlib", "bzip2", "abseil", "protobuf-cpp", "re2", "eigen")
 
     add_configs("glpk", {description = "Enable GLPK support", default = false, type = "boolean"})
     add_configs("highs", {description = "Enable HiGHS support", default = false, type = "boolean"})
@@ -18,22 +15,13 @@ package("or-tools")
     add_configs("coin-or", {description = "Enable Coin-OR support", default = false, type = "boolean", readonly = true})
     add_configs("scip", {description = "Enable SCIP support", default = false, type = "boolean", readonly = true})
     add_configs("cplex", {description = "Enable CPLEX support", default = false, type = "boolean", readonly = true})
+    if is_plat("windows") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
 
-    on_install("linux", "macosx", "bsd", function (package)
-        local configs = {
-            "-DBUILD_CXX=ON",
-            "-DBUILD_SAMPLES=OFF",
-            "-DBUILD_TESTING=OFF",
-            "-DUSE_COINOR=" .. (package:config("coin-or") and "ON" or "OFF"),
-            "-DUSE_GLPK=" .. (package:config("glpk") and "ON" or "OFF"),
-            "-DUSE_HIGHS=" .. (package:config("highs") and "ON" or "OFF"),
-            "-DUSE_SCIP=" .. (package:config("scip") and "ON" or "OFF"),
-            "-DUSE_CPLEX=" .. (package:config("cplex") and "ON" or "OFF"),
-            "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
-            "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF")
-        }
-        import("package.tools.cmake").install(package, configs)
-    end)
+    add_deps("cmake")
+    add_deps("zlib", "bzip2", "eigen", "re2")
+    add_deps("protobuf-cpp", {configs = {zlib = true}})
 
     on_load(function (package)
         if package:config("coin-or") then
@@ -45,11 +33,35 @@ package("or-tools")
         if package:config("highs") then
             package:add("deps", "highs")
         end
+        if package:is_plat("windows") and package:config("shared") then
+            package:add("defines", "OR_BUILD_DLL")
+        end
+    end)
+
+    on_install("windows", "linux", "macosx", "bsd", function (package)
+        -- Fix for RHEL/CentOS/Fedora system zlib
+        io.replace("cmake/system_deps.cmake", "find_package(ZLIB REQUIRED)", "find_package(ZLIB REQUIRED MODULE)", {plain = true})
+
+        local configs = {
+            "-DBUILD_TESTING=OFF",
+            "-DBUILD_EXAMPLES=OFF",
+            "-DBUILD_SAMPLES=OFF",
+            "-DBUILD_DEPS=OFF",
+            "-DUSE_BOP=OFF",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_COINOR=" .. (package:config("coin-or") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_GLPK=" .. (package:config("glpk") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_HIGHS=" .. (package:config("highs") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_SCIP=" .. (package:config("scip") and "ON" or "OFF"))
+        table.insert(configs, "-DUSE_CPLEX=" .. (package:config("cplex") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            int main(int argc, char** argv) {
+            void test(int argc, char** argv) {
                 InitGoogle(argv[0], &argc, &argv, true);
             }
         ]]
