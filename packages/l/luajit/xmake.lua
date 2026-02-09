5f30c14da67f24d97ec47a5df7728fa579d7fb5f
Saikari <lin@sz.cn.eu.org>
2026-02-09 04:22:50 +0300

    LuaJIT: try to update and support arm (#9282)
    * LuaJIT: try to update and support arm
    
    * fixup
    
    * try get more reasonable results
    
    * assume we can solve macosx
    
    * re
    
    * try simplify
    
    * fixup
    
    * try fix win arm64
    
    * try ENDIAN_LE
    
    * test
    
    * test
    
    * Try fix arm64 win
    
    * retry
    
    * retry
    
    * test
    
    * Resolve review
    
    * re
    
    * test
    
    * validate that android armeabi-v7a does not work
    
    * retry for android armeabi-v7
    
    * just drop armeabi-v7a
    
    * try armeabi
    
    * retry for armeabi
    
    * Assume we need multilib?
    
    * give up with windows armeabi-v7a for now
    
    * just use fence
    
    * gc
    
    * fixup
    
    * fixup
    
    * fixup
    
    * re
    
    * retry for ios/ndk
    
    * fix ios by dropping jit
    
    * try build armeabi-v7a on windows host
    
    * fixup
    
    * retry
    
    * fixup
    
    * drop armeabi-v7a build on windows os
    
    * cls
    
    * retry building
    
    * cls
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Refactor defines insertion using table.join2
    
    * Update xmake.lua
    
    * Try build armeaabi win
    
    * fixup
    
    * drop armeabi on win
    
    * just add old code
    
    * fixup
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>

--- packages/l/luajit/xmake.lua
+++ packages/l/luajit/xmake.lua
@@ -3,24 +3,34 @@ package("luajit")
     set_description("A Just-In-Time Compiler (JIT) for the Lua programming language.")
     set_license("MIT")
 
-    set_urls("https://github.com/LuaJIT/LuaJIT.git",
-             "http://repo.or.cz/luajit-2.0.git")
+    set_urls("https://github.com/LuaJIT/LuaJIT.git")
 
+    add_versions("v2.1.0-beta4.20260109", "707c12bf00dafdfd3899b1a6c36435dbbf6c7022")
     add_versions("v2.1.0-beta3", "8271c643c21d1b2f344e339f559f2de6f3663191")
+
+    add_patches("v2.1.0-beta4.20260109", "patches/v2.1.0-beta4.20260109/fix-bsd.patch", "e60b6f1ddeaaf503123c025433d1906b3bd3cefbd0237c9170f33a3d535ffb05")
 
     add_configs("nojit", { description = "Disable JIT.", default = false, type = "boolean"})
     add_configs("fpu",   { description = "Enable FPU.", default = true, type = "boolean"})
     add_configs("gc64",  { description = "Enable GC64.", default = false, type = "boolean"})
 
-    add_includedirs("include/luajit")
+    add_includedirs("include", "include/luajit")
     if not is_plat("windows") then
         add_syslinks("dl")
     end
 
     if on_check then
         on_check(function (package)
-            if package:is_arch("arm.*") then
-                raise("package(luajit/arm64) unsupported arch")
+            if package:version() then
+                if package:version():eq("v2.1.0-beta3") then
+                    if package:is_arch("arm.*") then
+                        raise("package(luajit/arm64) unsupported arch")
+                    end
+                else
+                    if package:is_plat("android") and package:is_arch("arm.*") and package:check_sizeof("void*") == "4" and is_host("windows") then
+                        raise("package(luajit/armeabi-v7a): Windows OS host is unsupported for android cross-compilation")
+                    end
+                end
             end
         end)
     end
@@ -37,13 +47,20 @@ package("luajit")
         configs.fpu     = package:config("fpu")
         configs.nojit   = package:config("nojit")
         configs.gc64    = package:config("gc64")
-        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        if package:is_plat("macosx") and not package:is_arch("arm.*") then
+            configs.gc64 = true
+        end
+        if package:version():eq("v2.1.0-beta3") then
+            os.cp(path.join(package:scriptdir(), "port", "v2.1.0-beta3", "xmake.lua"), "xmake.lua")
+        else
+            os.cp(path.join(package:scriptdir(), "port", "v2.1.0-beta4.20260109", "xmake.lua"), "xmake.lua")
+        end
         import("package.tools.xmake").install(package, configs)
     end)
 
     on_test(function (package)
-        if package:is_plat(os.host()) then
+        if package:is_plat(os.host()) and not package:is_cross() then
             os.vrun("luajit -e \"print('hello xmake!')\"")
         end
-        assert(package:has_cfuncs("lua_pcall", {includes = "luajit.h"}))
+        assert(package:check_csnippets("int main() { lua_State* L = luaL_newstate(); return 0; }", {includes = {"lua.h", "lauxlib.h", "lualib.h"}}))
     end)
