14e9665a3fe4e6af3e18eaba159260d7e2a89295
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-02-12 11:55:02 +0800

    Add xmake.lua for ia32-doc package

---
+++ packages/i/ia32-doc/xmake.lua
@@ -0,0 +1,37 @@
+package("ia32-doc")
+    set_homepage("https://github.com/ia32-doc/ia32-doc")
+    set_description("C/C++ headers for Intel Architecture Software Developer's Manual")
+    set_license("MIT")
+
+    add_urls("https://github.com/ia32-doc/ia32-doc.git")
+
+    add_configs("header_type", {
+        description = "The header file type to use",
+        default = "hpp",
+        values = {"h", "hpp", "compact", "defines_only"}
+    })
+
+    on_install(function (package)
+        local header_map = {
+            h            = "ia32.h",
+            hpp          = "ia32.hpp",
+            compact      = "ia32_compact.h",
+            defines_only = "ia32_defines_only.h"
+        }
+
+        local selected_file = header_map[package:config("header_type")]
+        
+        import("lib.detect.find_file")
+        local src_file = "out/" .. selected_file
+        
+        os.cp(src_file, package:installdir("include/ia32.h"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <ia32.h>
+            void test() {
+                CR3 reg;
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
