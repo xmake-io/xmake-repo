116c273ba3f99dcb4cd5bc11e5bb7537868770c0
star9029 <hengxings783@gmail.com>
2026-02-20 23:26:36 +0800

    fix midi

--- packages/l/libsdl2_mixer_x/xmake.lua
+++ packages/l/libsdl2_mixer_x/xmake.lua
@@ -8,16 +8,32 @@ package("libsdl2_mixer_x")
     add_versions("2.7.0", "28d1fd54e616cc285839936cc209d874504a2725")
     add_versions("2.6.0", "9e97247483ef7102442dcb836309b204ccefed503d1bc38996093c79ab284b1e")
 
+    add_configs("midi", {description = "Build with MIDI support", default = true, type = "boolean"})
     add_configs("lgpl", {description = "Use libflac to playEnable components with LGPL license", default = false, type = "boolean"})
     add_configs("gpl", {description = "Enable using of components with GPL and LGPL licenses", default = false, type = "boolean"})
 
     add_deps("cmake")
     add_deps("libsdl2")
 
+    on_load(function (package)
+        if package:config("midi") then
+            if package:is_plat("windows") then
+                package:add("syslinks", "winmm")
+            end
+        end
+    end)
+
     on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("bsd") then
+            table.insert(configs, "-DCMAKE_CXX_STANDARD=11")
+        elseif package:is_plat("iphoneos") then
+            table.insert(configs, "-DUSE_MIDI_NATIVE=OFF")
+        end
+
+        table.insert(configs, "-DUSE_MIDI=" .. (package:config("midi") and "ON" or "OFF"))
         table.insert(configs, "-DMIXERX_ENABLE_LGPL=" .. (package:config("lgpl") and "ON" or "OFF"))
         table.insert(configs, "-DMIXERX_ENABLE_GPL=" .. (package:config("gpl") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
