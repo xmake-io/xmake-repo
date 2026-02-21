8ef8d023d469888bdf2c8209d56d9693a6d53080
ruki <waruqi@gmail.com>
2026-02-21 01:54:33 +0000

    Update xproperty to 0.13.0

--- packages/x/xproperty/xmake.lua
+++ packages/x/xproperty/xmake.lua
@@ -7,6 +7,7 @@ package("xproperty")
     add_urls("https://github.com/jupyter-xeus/xproperty/archive/refs/tags/$(version).tar.gz",
              "https://github.com/jupyter-xeus/xproperty.git")
 
+    add_versions("0.13.0", "41fa0e2b292e8e6d8ab98ec618a1e22ca6ebf1ea4bb6e51d3637a4b3c1360eaf")
     add_versions("0.12.1", "e8fd89e8b4bfd1631189654156dc9da4f668e011f8ccf8bc3fdd723479922b18")
     add_versions("0.12.0", "27cbc8e441dcc515a1ebbf11bad5ef240748d32f5e1adf84deed87a1dc57a440")
 
