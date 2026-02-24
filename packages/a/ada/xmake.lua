c528bc948e7993a4cce87f9d77303ec7d6c7008e
apocelipes <seve3r@outlook.com>
2026-02-24 15:49:30 +0800

    Update ada to v3.4.3

--- packages/a/ada/xmake.lua
+++ packages/a/ada/xmake.lua
@@ -6,6 +6,7 @@ package("ada")
     set_urls("https://github.com/ada-url/ada/archive/refs/tags/$(version).tar.gz",
              "https://github.com/ada-url/ada.git")
 
+    add_versions("v3.4.3", "c7a6450019a326c6a10b144249b03ebea662afb2451d74f06963389fe7707460")
     add_versions("v3.4.2", "3aceb6028eb0787ea77c8f3035a5aaa15108ab11d0fe24f23fe850cf94816523")
     add_versions("v3.4.1", "befb20175cd05fd10f345bbfd4202af31ad6bb25732beaacac69793eeefa8d4f")
     add_versions("v3.3.0", "75565e2d4cc8e3ce2dd7927f5c75cc5ebbd3b620468cb0226501dae68d8fe1cd")
