ff07dee378163e01c1383cb2291e03a3abec77cd
MasterLaplace <guillaume.papineau@epitech.eu>
2026-02-24 22:03:20 +0100

    feat: add xmake.lua files for the brainflow and liblsl packages

---
+++ packages/b/brainflow/xmake.lua
@@ -0,0 +1,63 @@
+package("brainflow")
+    set_homepage("https://github.com/brainflow-dev/brainflow")
+    set_description("BrainFlow SDK for EEG and biosensors")
+    set_license("MIT")
+
+    add_urls("https://github.com/brainflow-dev/brainflow/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/brainflow-dev/brainflow.git")
+    add_versions("5.20.1", "0f01f00025c67ce52745be26ebc049d1ba68d398c3fe5a6df6c766c3bf6d40ce")
+
+    add_deps("cmake")
+
+    on_load(function (package)
+        package:add("includedirs", "inc")
+
+        local is_windows = package:is_plat("windows")
+        local is_32bit = is_windows and package:is_arch("x86", "i386")
+        local suffix = is_32bit and "32" or ""
+
+        package:add("links",
+            "Brainflow" .. suffix,
+            "BoardController" .. suffix,
+            "DataHandler" .. suffix,
+            "MLModule" .. suffix)
+
+        if package:is_plat("linux", "bsd") then
+            package:add("syslinks", "pthread", "dl")
+        elseif is_windows then
+            package:add("syslinks", "ws2_32", "iphlpapi")
+        end
+    end)
+
+    on_install("linux", "macosx", "windows", "bsd", function (package)
+        local configs = {
+            "-DBUILD_TESTS=OFF",
+            "-DBUILD_BLUETOOTH=OFF",
+            "-DBUILD_BLE=OFF",
+            "-DBUILD_ONNX=OFF",
+            "-DBUILD_PERIPHERY=OFF",
+            "-DBUILD_OYMOTION_SDK=OFF",
+            "-DBUILD_SYNCHRONI_SDK=OFF",
+            "-DUSE_LIBFTDI=OFF",
+            "-DUSE_OPENMP=OFF",
+            "-DWARNINGS_AS_ERRORS=OFF"
+        }
+
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        local version = package:version()
+        if version then
+            table.insert(configs, "-DBRAINFLOW_VERSION=" .. tostring(version))
+        end
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "board_shim.h"
+            int main() {
+                auto version = BoardShim::get_version();
+                return version.empty();
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
---
+++ packages/l/liblsl/xmake.lua
@@ -0,0 +1,43 @@
+package("liblsl")
+    set_homepage("https://github.com/sccn/liblsl")
+    set_description("Lab Streaming Layer C/C++ library")
+    set_license("MIT")
+
+    add_urls("https://github.com/sccn/liblsl/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/sccn/liblsl.git")
+    add_versions("v1.17.5", "6f1f5a3fc4c4a162c86ced19c75d13a81d8ebe1f819c50caf257b1ee0b401d1a")
+
+    add_deps("cmake")
+
+    on_load(function (package)
+        if package:is_plat("linux", "bsd") then
+            package:add("syslinks", "pthread")
+        elseif package:is_plat("windows") then
+            package:add("syslinks", "ws2_32")
+        end
+
+        if not package:config("shared") then
+            package:add("defines", "LIBLSL_STATIC")
+        end
+    end)
+
+    on_install("linux", "macosx", "windows", "bsd", function (package)
+        local configs = {
+            "-DLSL_UNITTESTS=OFF",
+            "-DLSL_TOOLS=OFF",
+            "-DLSL_INSTALL=ON",
+            "-DLSL_BUNDLED_BOOST=ON",
+            "-DLSL_BUNDLED_PUGIXML=ON",
+            "-DLSL_FRAMEWORK=OFF",
+            "-DLSL_BUILD_STATIC=" .. (package:config("shared") and "OFF" or "ON")
+        }
+
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("lsl_local_clock", {includes = "lsl_c.h"}))
+    end)
