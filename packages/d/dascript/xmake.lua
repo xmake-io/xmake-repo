9057235d0aeb10b548b3582dabac57c36945aa8c
Saikari <lin@sz.cn.eu.org>
2026-03-02 01:37:37 +0300

    Update xmake.lua

--- packages/d/dascript/xmake.lua
+++ packages/d/dascript/xmake.lua
@@ -4,26 +4,26 @@ package("dascript")
     set_license("BSD-3-Clause")
 
     if is_host("windows") then
-        if is_arch("x64", "x86_64") then
+        if os.arch() == "x86_64" then
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-windows-x86_64.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "9ea32c7f0aee2c84fe5d652b627964c74772422f1e0fde235536d72936669f60")
-        elseif is_arch("x86", "i386") then
+        else
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-windows-x86.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "f6545b310c73463334b31625adc3587cf4930ef71fcee1caf0fe0b35df1df313")
         end
     elseif is_host("macosx") then
-        if is_arch("arm64") then
+        if os.arch() == "arm64" then
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-darwin26-arm64.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "b8ba4943cf4aa0e0042918ae2f1511935029a149488d4835b56679f390747faa")
-        elseif is_arch("x86_64") then
+        elseif os.arch() == "x86_64" then
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-darwin15-x86_64.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "5130b9867cd9cc7b24fc3baf6218c563ffe01bfea5e95170748a95a44379d431")
         end
     elseif is_host("linux") then
-        if is_arch("arm64", "aarch64") then
+        if os.arch() == "arm64" then
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-linux-arm64.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "23595d99a62279d4f2b9e50c7b47f9c46f6a7bd02dd2ce2a7e01e422021b1775")
-        elseif is_arch("x86_64", "x64") then
+        elseif os.arch() == "x86_64" then
             add_urls("https://github.com/GaijinEntertainment/daScript/releases/download/$(version)/daslang-bundle-linux-x86_64.zip", {version = function (version) return version:gsub("%+", ".") end})
             add_versions("v0.5.9+2.1", "7ca9c700d72097e14678aa9ae33bb15ebc98abf14c8d1fd9e9e89692d5513898")
         end
