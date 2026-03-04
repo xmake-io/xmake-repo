1f66e636c26e954a0a542c726a3447b4f198d929
ruki <waruqi@gmail.com>
2026-03-04 15:46:43 +0800

    add more templates (#9441)
    * add more templates
    
    * add raylib and sdl templates
    
    * improve linux driver

---
+++ templates/c/linux/driver/src/main.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("The Xmake Open Source Community");
+MODULE_DESCRIPTION("A Simple Hello World module");
+
+static int __init hello_init(void) {
+    printk(KERN_INFO "Hello world!\n");
+    return 0;
+}
+
+static void __exit hello_cleanup(void) {
+    printk(KERN_INFO "Goodbye world!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
---
+++ templates/c/raylib/src/main.c
@@ -0,0 +1,15 @@
+#include "raylib.h"
+
+int main(void) {
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
