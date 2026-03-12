b5b31ccdde2f5f45f2a8f7b8996fba313af2c494
ruki <waruqi@gmail.com>
2026-03-12 02:01:34 +0000

    Update minizip to v1.3.2

--- packages/m/minizip/xmake.lua
+++ packages/m/minizip/xmake.lua
@@ -6,6 +6,7 @@ package("minizip")
     add_urls("https://github.com/madler/zlib/archive/refs/tags/$(version).tar.gz",
              "https://github.com/madler/zlib.git")
 
+    add_versions("v1.3.2", "b99a0b86c0ba9360ec7e78c4f1e43b1cbdf1e6936c8fa0f6835c0cd694a495a1")
     add_versions("v1.3.1", "17e88863f3600672ab49182f217281b6fc4d3c762bde361935e436a95214d05c")
     add_versions("v1.2.10", "42cd7b2bdaf1c4570e0877e61f2fdc0bce8019492431d054d3d86925e5058dc5")
     add_versions("v1.2.11", "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff")
