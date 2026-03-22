aff1c1c59f280beef7a5f7691fc5997124eece98
ruki <waruqi@gmail.com>
2026-03-22 22:24:04 +0800

    Update xmake.lua

--- packages/o/openai-cpp/xmake.lua
+++ packages/o/openai-cpp/xmake.lua
@@ -1,16 +1,23 @@
 package("openai-cpp")
+    set_kind("library", {headeronly = true})
     set_homepage("https://github.com/olrea/openai-cpp")
     set_description("OpenAI C++ is a community-maintained library for the Open AI API")
     set_license("MIT")
 
-    add_urls("https://github.com/olrea/openai-cpp/archive/refs/tags/v$(version).tar.gz")
+    add_urls("https://github.com/olrea/openai-cpp/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/olrea/openai-cpp.git")
     add_versions("0.1.3", "e73510ea7470d1a7e313bf1fcaeba1d2d67f31eeacea5e1efb3176fcb3040f42")
 
     add_deps("nlohmann_json", "libcurl")
-    set_kind("library", {headeronly = true})
 
     on_install(function (package)
-        os.mkdir(package:installdir("include/openai"))
-        os.cp("include/openai/*.hpp", package:installdir("include/openai/"))
-        package:add("includedirs", "include")
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                openai::OpenAI oai;
+            }
+        ]]}, {configs = {languages = "c++11"}, includes = "openai/openai.hpp"}))
     end)
