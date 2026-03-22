479ff5794c802197cc6e1b112e94c5217b4f0357
ruki <waruqi@gmail.com>
2026-03-22 22:25:08 +0800

    Update versioning format in xmake.lua

--- packages/o/openai-cpp/xmake.lua
+++ packages/o/openai-cpp/xmake.lua
@@ -4,9 +4,9 @@ package("openai-cpp")
     set_description("OpenAI C++ is a community-maintained library for the Open AI API")
     set_license("MIT")
 
-    add_urls("https://github.com/olrea/openai-cpp/archive/refs/tags/v$(version).tar.gz",
+    add_urls("https://github.com/olrea/openai-cpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/olrea/openai-cpp.git")
-    add_versions("0.1.3", "e73510ea7470d1a7e313bf1fcaeba1d2d67f31eeacea5e1efb3176fcb3040f42")
+    add_versions("v0.1.3", "e73510ea7470d1a7e313bf1fcaeba1d2d67f31eeacea5e1efb3176fcb3040f42")
 
     add_deps("nlohmann_json", "libcurl")
 
