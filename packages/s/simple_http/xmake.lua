0538f047a76b8c9cc7eadbdfb30165d7dc92b44a
fantasy-peak <82742316+fantasy-peak@users.noreply.github.com>
2026-03-27 15:41:07 +0800

    Update simple_http to 0.6.4 (#9595)

--- packages/s/simple_http/xmake.lua
+++ packages/s/simple_http/xmake.lua
@@ -1,12 +1,13 @@
 package("simple_http")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/fantasy-peak/simple_http")
-    set_description("A header-only HTTP library that supports both HTTP/2 and HTTP/1, based on Beast, nghttp2, and Asio.")
+    set_description("A c++20 header-only HTTP library that supports both HTTP/2 and HTTP/1, based on Beast, nghttp2, and Asio.")
     set_license("MIT")
 
     add_urls("https://github.com/fantasy-peak/simple_http/archive/refs/tags/$(version).tar.gz",
              "https://github.com/fantasy-peak/simple_http.git")
 
+    add_versions("v0.6.4", "3e25e23ba6473a4cd51357abc4995b68722b3deb3c043897c509913810e1818b")
     add_versions("v0.6.3", "9b80b0329dbb6e042b46844f1ab6255c1bb3108f0b1e2082704f853b50bc75bc")
     add_versions("v0.6.2", "ffe93846c583a9951c209550101486e2437c123b095c8b00dbed3b9bb594abc9")
     add_versions("v0.6.1", "1772bf750adb04e430b4275385b6d65230830181b93574dba178a854526247ff")
