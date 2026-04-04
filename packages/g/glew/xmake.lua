92ca0a813a445bea0cb0e16d5044fb6081575781
Saikari <lin@sz.cn.eu.org>
2026-04-04 18:34:41 +0300

    fixup

--- packages/g/glew/xmake.lua
+++ packages/g/glew/xmake.lua
@@ -7,18 +7,17 @@ package("glew")
     add_versions("2.1.0", "2700383d4de2455f06114fbaf872684f15529d4bdc5cdea69b5fb0e9aa7763f1")
     add_versions("2.2.0", "a9046a913774395a095edcc0b0ac2d81c3aacca61787b39839b941e9be14e0d4")
 
-    if is_plat("windows") or is_plat("mingw") then
-        add_syslinks("opengl32")
-    elseif is_plat("linux") then
-        add_syslinks("GL")
-        add_deps("libx11", "xorgproto")
-    end
     add_defines("GLEW_NO_GLU")
 
     on_load("windows", "mingw", function (package)
+        package:add("syslinks", "opengl32")
         if not package:config("shared") then
             package:add("defines", "GLEW_STATIC")
         end
+    end)
+    on_load("linux", function (package)
+        package:add("syslinks", "GL")
+        package:add("deps", "libx11", "xorgproto")
     end)
 
     on_install("linux", "macosx", "mingw", "windows", function (package)
