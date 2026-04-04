7800dad65eeb49e2ce389f03c1e97c8589234af7
Saikari <lin@sz.cn.eu.org>
2026-04-04 13:55:03 +0300

    Test .pc

--- packages/g/glew/port/xmake.lua
+++ packages/g/glew/port/xmake.lua
@@ -33,6 +33,34 @@ target("glew")
     add_files("src/glew.c")
     add_includedirs("include", {public = true})
     add_headerfiles("include/(GL/*.h)")
+    -- pkgconfig_importfiles names the .pc after target:basename() which on Windows is
+    -- "glew32" or "glew32s", not "glew". Generate glew.pc manually so pkg_check_modules
+    -- can find it, and include -DGLEW_STATIC in Cflags for static builds.
+    if is_plat("windows", "mingw") then
+        after_install(function (target)
+            local installdir = path.unix(target:installdir())
+            local pcdir = path.join(target:installdir(), "lib", "pkgconfig")
+            os.mkdir(pcdir)
+            local file = io.open(path.join(pcdir, "glew.pc"), "w")
+            if file then
+                file:print("prefix=%s", installdir)
+                file:print("exec_prefix=${prefix}")
+                file:print("libdir=${prefix}/lib")
+                file:print("includedir=${prefix}/include")
+                file:print("")
+                file:print("Name: glew")
+                file:print("Description: The OpenGL Extension Wrangler Library")
+                file:print("Version: %s", target:get("version") or "")
+                file:print("Libs: -L${libdir} -l%s", target:basename())
+                local cflags = "-I${includedir}"
+                if not target:is_shared() then
+                    cflags = cflags .. " -DGLEW_STATIC"
+                end
+                file:print("Cflags: %s", cflags)
+                file:close()
+            end
+        end)
+    end
 
 target("glewinfo")
     set_kind("binary")
