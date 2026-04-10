b4209b428baf08a929e44825b9ab86399a00fbc5
rempas <60424977+rempas@users.noreply.github.com>
2026-04-10 22:10:31 +0300

    ODE: update to 0.16.6
    Fixed the bug with the windows arm64 build.

--- packages/o/ode/xmake.lua
+++ packages/o/ode/xmake.lua
@@ -3,9 +3,17 @@ package("ode")
     set_description("ODE is an open source, high performance library for simulating rigid body dynamics.")
     set_license("BSD-3-Clause")
 
-    add_urls("https://bitbucket.org/odedevs/ode/get/$(version).zip")
-    add_versions("0.16.6", "a6845f79fb401995de1fa1882d067e6803c0cc3a755a5fb0a28874a5182d89d6")
-    add_versions("0.16.2", "000a5cdd0a81811cade2b0409ec06911a95e3c4c0d72a4cce3af6131115d0350")
+    -- Windows arm64 will only be able to get the master version
+    -- until a new release with a working windows arm64 build is
+    -- pushed!
+    if is_plat("windows") and is_arch("arm64") then
+        set_urls("https://github.com/odedevs/ode.git")
+        add_versions("master", "master")
+    else
+        add_versions("0.16.6", "a6845f79fb401995de1fa1882d067e6803c0cc3a755a5fb0a28874a5182d89d6")
+        add_versions("0.16.2", "000a5cdd0a81811cade2b0409ec06911a95e3c4c0d72a4cce3af6131115d0350")
+        add_urls("https://bitbucket.org/odedevs/ode/get/$(version).zip")
+    end
 
     add_configs("libccd", {description = "Build with libccd.", default = false, type = "boolean"})
 
