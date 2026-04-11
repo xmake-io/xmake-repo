f80dd3261a5ce5518e919f2620d6d4458fbaaacd
rempas <60424977+rempas@users.noreply.github.com>
2026-04-11 10:07:51 +0300

    openal-soft: update to 1.25.1
    Added openal-soft versions 1.25.0 and 1.25.1. I also added conditional code to check for minimal c++ compiler support which didn't existed before.

--- packages/o/openal-soft/xmake.lua
+++ packages/o/openal-soft/xmake.lua
@@ -9,6 +9,8 @@ package("openal-soft")
     end})
     add_urls("https://github.com/kcat/openal-soft.git")
 
+    add_versions("1.25.1", "5f8efe8dfba5e9307a50251ba615ace857c7fa9dddfe34130b83e213d7f7cf24")
+    add_versions("1.25.0", "c07424e16cc53632a58f7ccaf7f4cd1cf2efde7fe4d2cdca1edbf618ea9470d1")
     add_versions("1.24.3", "7e1fecdeb45e7f78722b776c5cf30bd33934b961d7fd2a11e0494e064cc631ce")
     add_versions("1.23.1", "dfddf3a1f61059853c625b7bb03de8433b455f2f79f89548cbcbd5edca3d4a4a")
     add_versions("1.22.2", "3e58f3d4458f5ee850039b1a6b4dac2343b3a5985a6a2e7ae2d143369c5b8135")
@@ -65,5 +67,16 @@ package("openal-soft")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("alGetProcAddress", {includes = "AL/al.h"}))
+        if package:version():ge("v1.25.0") then
+            required_cpp_ver = "c++20"
+        elseif  package:version():ge("v1.24.0") then
+            required_cpp_ver = "c++17"
+        else
+            required_cpp_ver = "c++11"
+        end
+
+        assert(package:has_cfuncs("alGetProcAddress",
+            {configs = { languages = required_cpp_ver}},
+            {includes = "AL/al.h"}
+        ))
     end)
