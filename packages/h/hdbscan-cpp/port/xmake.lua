081187f3784fc9ab4986bf9959aaeb4edf3f6d6b
RC1844 <1844766234@qq.com>
2026-04-14 19:43:03 +0800

    Update xmake build configuration
    Remove unnecessary do-end block structures and simplify the syntax for defining target and package.
    Remove the configuration that mandates the inclusion of the cstdio header file.
    Update the test function to use has_cxxfuncs instead of has_cxxtypes for functional verification.

--- packages/h/hdbscan-cpp/port/xmake.lua
+++ packages/h/hdbscan-cpp/port/xmake.lua
@@ -1,11 +1,8 @@
 add_rules("mode.release", "mode.debug")
 
 target("hdbscan-cpp")
-do
     set_kind("$(kind)")
     set_languages("cxx11")
     add_includedirs("HDBSCAN-CPP")
-    add_forceincludes("cstdint")
     add_headerfiles("HDBSCAN-CPP/(**/*.hpp)")
     add_files("HDBSCAN-CPP/**/*.cpp")
-end
--- packages/h/hdbscan-cpp/xmake.lua
+++ packages/h/hdbscan-cpp/xmake.lua
@@ -1,24 +1,16 @@
 package("hdbscan-cpp")
-do
     set_kind("library")
     set_homepage("https://github.com/rohanmohapatra/hdbscan-cpp")
     set_description("A C++ implementation of HDBSCAN clustering algorithm")
     set_license("MIT")
 
-    add_urls("https://github.com/rohanmohapatra/hdbscan-cpp.git", {
-        alias = "git"
-    })
+    add_urls("https://github.com/rohanmohapatra/hdbscan-cpp.git", { alias = "git" })
 
     add_versions("git:1.0.0", "1.0.0")
     add_versions("git:latest", "master")
 
     if is_plat("windows") then
-        add_configs("shared", {
-            description = "Build shared library.",
-            default = false,
-            type = "boolean",
-            readonly = true
-        })
+        add_configs("shared", { description = "Build shared library.", default = false, type = "boolean", readonly = true })
     end
 
     on_install("macosx", "android", "linux", "windows", "mingw", function(package)
@@ -27,8 +19,5 @@ do
     end)
 
     on_test(function(package)
-        assert(package:has_cxxtypes("Hdbscan", {
-            includes = "Hdbscan/hdbscan.hpp"
-        }))
+        assert(package:has_cxxfuncs("Hdbscan(std::string())", { includes = "Hdbscan/hdbscan.hpp"}))
     end)
-end
