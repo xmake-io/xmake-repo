d561d8bb22526f40b6902a6a2c2baead9e8dca3c
ruki <waruqi@gmail.com>
2026-04-14 02:29:13 +0000

    Update jemalloc to 5.3.1

--- packages/j/jemalloc/xmake.lua
+++ packages/j/jemalloc/xmake.lua
@@ -5,6 +5,7 @@ package("jemalloc")
     add_urls("https://github.com/jemalloc/jemalloc/releases/download/$(version)/jemalloc-$(version).tar.bz2",
              "https://github.com/jemalloc/jemalloc.git")
 
+    add_versions("5.3.1", "3826bc80232f22ed5c4662f3034f799ca316e819103bdc7bb99018a421706f92")
     add_versions("5.3.0", "2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aecfeaa")
     add_versions("5.2.1", "34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6")
 
