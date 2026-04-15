cbd310af854d864057fc39147d5bcfbdfc505a81
ruki <waruqi@gmail.com>
2026-04-15 19:36:03 +0800

    Auto-update sailormoon_flags to 1.2 (#9752)
    * Update sailormoon_flags to 1.2
    
    * fix build
    
    * fix android
    
    ---------
    
    Co-authored-by: apocelipes <seve3r@outlook.com>

--- packages/s/sailormoon_flags/xmake.lua
+++ packages/s/sailormoon_flags/xmake.lua
@@ -5,13 +5,28 @@ package("sailormoon_flags")
     set_license("MIT")
 
     add_urls("https://github.com/sailormoon/flags/archive/refs/tags/v$(version).tar.gz")
+    add_versions("1.2", "2fb981e00d5f97753fa2b685819c4cab8aea7a3f62939a9a0549fb8406b37500")
     add_versions("1.1", "f6626c97ba7a45c473557db2e4b68df4d9cda18a8a97c89a5d8d4e5c53dde904")
+
+    if on_check then
+        on_check("android", function (package)
+            local ndk = package:toolchain("ndk")
+            local ndkver = ndk:config("ndkver")
+            if package:version() and package:version():ge("1.2") then
+                assert(ndkver and tonumber(ndkver) >= 27, "package(sailormoon_flags >= 1.2): need ndk version >= 27")
+            end
+        end)
+    end
 
     on_install(function (package)
         os.cp("include", package:installdir())
     end)
 
     on_test(function (package)
+        local languages = "c++17"
+        if package:version() and package:version():ge("1.2") then
+            languages = "c++20"
+        end
         assert(package:check_cxxsnippets({test = [[
             #include "flags.h"
             void test() {
@@ -19,5 +34,5 @@ package("sailormoon_flags")
                 char **argv = NULL;
                 const flags::args args(argc, argv);
             }
-        ]]}, {configs = {languages = "c++17"}}))
+        ]]}, {configs = {languages = languages}}))
     end)
