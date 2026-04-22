9364c288588de6ee1e2c5d8877b07928c5cc6de4
Saikari <lin@sz.cn.eu.org>
2026-04-22 23:29:02 +0300

    Change set_urls to add_urls for libxdmcp

--- packages/l/libxdmcp/xmake.lua
+++ packages/l/libxdmcp/xmake.lua
@@ -2,8 +2,8 @@ package("libxdmcp")
     set_homepage("https://www.x.org/")
     set_description("X.Org: X Display Manager Control Protocol library")
 
-    set_urls("https://www.x.org/archive/individual/lib/libXdmcp-$(version).tar.gz", {alias = "xorg"})
-    set_urls("https://www.rro.rs/gentoo-distfiles/distfiles/25/libXdmcp-$(version).tar.xz", {alias = "mirror"})
+    add_urls("https://www.x.org/archive/individual/lib/libXdmcp-$(version).tar.gz", {alias = "xorg"})
+    add_urls("https://www.rro.rs/gentoo-distfiles/distfiles/25/libXdmcp-$(version).tar.xz", {alias = "mirror"})
 
     add_versions("xorg:1.1.3", "2ef9653d32e09d1bf1b837d0e0311024979653fe755ad3aaada8db1aa6ea180c")
     add_versions("xorg:1.1.4", "55041a8ff8992ab02777478c4b19c249c0f8399f05a752cb4a1a868a9a0ccb9a")
