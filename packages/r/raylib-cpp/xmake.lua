5b1c09f2a4e3b3344d257f57f14bb7ec26c126e0
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-04-23 21:21:38 +0100

    make sha lowercase

--- packages/r/raylib-cpp/xmake.lua
+++ packages/r/raylib-cpp/xmake.lua
@@ -7,7 +7,7 @@ package("raylib-cpp")
     add_urls("https://github.com/RobLoach/raylib-cpp/archive/refs/tags/$(version).tar.gz", 
              "https://github.com/RobLoach/raylib-cpp.git")
 
-    add_versions("v6.0.0", "42E1378A56A8440B6CA048640E75763B64AFD51A8A0CA3A5BAFF032761338300")
+    add_versions("v6.0.0", "42e1378a56a8440b6ca048640e75763b64afd51a8a0ca3a5baff032761338300")
     add_versions("v5.5.1", "3426a957c50b2f79a877f75881e588a1aab25da4815172715b472b2afb549557")
     add_versions("v5.5.0", "bcb4a4e241a95376e8562aa77c29976ed0921235a9f5326822130a7bcf4860a5")
     add_versions("v5.0.2", "d3a718170882bc873c973a19a824d7fa4bfd9d0087b4778057231409a240920d")
