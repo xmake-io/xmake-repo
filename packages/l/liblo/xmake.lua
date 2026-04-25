5e67d92d2f0321827ff3af6f4a5cfc89af3e8e0d
EZ4Stephen <ez4stephen@tutamail.com>
2026-04-25 23:44:45 +0400

    Add liblo 0.34 to xrepo
    This PR supports liblo for windows, macos, linux and bsd. However, the liblo repo seems to have support for Anrdoid too. I couldn't figure out how to add that, so I'm leaving that for anyone else to add.

---
+++ packages/l/liblo/xmake.lua
@@ -0,0 +1,28 @@
+package("liblo")
+    set_homepage("https://github.com/radarsat1/liblo")
+    set_description("An implementation of the Open Sound Control protocol for POSIX systems")
+    set_license("LGPL-2.1-or-later")
+
+    add_urls("https://github.com/radarsat1/liblo/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/radarsat1/liblo.git")
+    add_versions("0.34", "e9a294c7613e1bec2abcf26f2010604643d605ed6852e16b51837400729fcbee")
+
+    add_deps("cmake")
+
+    if is_plat("linux", "cross", "bsd") then
+        add_syslinks("pthread")
+    end
+
+    on_install("windows", "macosx", "linux", "bsd", "mingw", function (package)
+        os.cd("cmake")
+        local configs = {
+            "-DWITH_STATIC=ON",
+            "-DWITH_TESTS=OFF",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("lo_address_new", {includes = "lo/lo.h"}))
+    end)
