114c726b83dce99ebd674da338f4e2a532110276
xq114 <1140735506@qq.com>
2026-04-26 14:20:05 +0800

    feat: openimageio 3.x

--- packages/o/openimageio/xmake.lua
+++ packages/o/openimageio/xmake.lua
@@ -4,79 +4,93 @@ package("openimageio")
     set_description("OpenImageIO is a library for reading and writing images, and a bunch of related classes, utilities, and applications.")
     set_license("BSD-3-Clause")
 
-    add_urls("https://github.com/OpenImageIO/oiio/archive/refs/tags/v$(version).zip", {version = function (version) return version:gsub("%+", ".") end})
-    add_versions("2.2.19+0", "30a494ccfc2ee951fabbdf308d1b59eddb1a21aa5276887fe6a6fc874454794e")
-    add_versions("2.3.10+1", "48ce9b9c3e815885f5634438fed3e240ad186fd9e30d392e17564b5e9eebb089")
-    add_versions("2.3.13+0", "4b42e10c93e9954fc89287b9a187c78539c49d16c2947fc140b1452860a54c26")
+    add_urls("https://github.com/AcademySoftwareFoundation/OpenImageIO/releases/download/v$(version)/OpenImageIO-$(version).tar.gz", {version = function (version) return version:gsub("%+", ".") end})
+    add_versions("2.5.19+1", "8e3d7473d7a42e527426c38c35187519488ea2a34e994782bd3e6504a209719f")
+    add_versions("3.1.12+0", "704511376faf32767cdcd9aa9a6d0be2b03b91f849ad9008227dc9f0e14bc265")
 
     add_deps("cmake")
-    add_deps("boost", {configs = {filesystem = true, system = true, thread = true}})
-    add_deps("zlib", "libtiff", "openexr", "libjpeg-turbo", "pugixml", "libpng", "robin-map", "libsquish", "fmt")
-    local configdeps = {bzip2    = "BZip2",
-                        freetype = "Freetype",
-                        hdf5     = "HDF5",
-                        opencv   = "OpenCV",
-                        tbb      = "TBB",
-                        giflib   = "GIF",
-                        libheif  = "Libheif",
-                        openjpeg = "OpenJPEG",
-                        openvdb  = "OpenVDB",
-                        ptex     = "Ptex",
-                        libwebp  = "WebP",
-                        libraw   = "LibRaw",
-                        field3d  = "Field3D",
-                        dcmtk    = "DCMTK",
-                        ffmpeg   = "FFmpeg"}
+    add_deps("boost", {configs = {filesystem = true, system = true, thread = true, stacktrace = true}})
+    add_deps("opencolorio", "zlib", "libtiff", "openexr", "libjpeg-turbo", "pugixml", "libpng", "robin-map", "fmt")
+    local configdeps = {bzip2       = "BZip2",
+                        freetype    = "Freetype",
+                        opencv      = "OpenCV",
+                        tbb         = "TBB",
+                        giflib      = "GIF",
+                        libheif     = "Libheif",
+                        openjpeg    = "OpenJPEG",
+                        openvdb     = "OpenVDB",
+                        ptex        = "Ptex",
+                        libwebp     = "WebP",
+                        libraw      = "LibRaw",
+                        libultrahdr = "libuhdr",
+                        dcmtk       = "DCMTK",
+                        ffmpeg      = "FFmpeg"}
     for conf, dep in pairs(configdeps) do
-        add_configs(conf, {description = "Build with " .. conf .. " support.", default = (conf == "libwebp"), type = "boolean"})
+        add_configs(conf, {description = "Build with " .. conf .. " support.", default = false, type = "boolean"})
     end
+    add_configs("tools",  {description = "Build OpenImageIO tools.", default = false, type = "boolean"})
     add_configs("python", {description = "Build python bindings.", default = false, type = "boolean"})
 
     if is_plat("windows") then
         add_syslinks("shell32")
     end
-    add_links("OpenImageIO", "OpenImageIO_Util")
 
-    on_load("windows", "macosx", "linux", function (package)
+    on_load("windows|!arm64", "macosx", "linux", function (package)
         if package:is_plat("windows") and not package:config("shared") then
             package:add("defines", "OIIO_STATIC_DEFINE")
         end
         for conf, dep in pairs(configdeps) do
             if package:config(conf) then
-                package:add("deps", conf)
+                if (conf == "libwebp") then
+                    package:add("deps", conf, {configs = {libwebpmux = true}})
+                else
+                    package:add("deps", conf)
+                end
             end
         end
         if package:config("python") then
             package:add("deps", "python 3.x")
             package:add("deps", "pybind11")
         end
+        if package:config("debug") then
+            package:add("links", "OpenImageIO_d", "OpenImageIO_Util_d")
+        else
+            package:add("links", "OpenImageIO", "OpenImageIO_Util")
+        end
     end)
 
-    on_install("windows", "macosx", "linux", function (package)
+    on_install("windows|!arm64", "macosx", "linux", function (package)
         io.replace("CMakeLists.txt", "NOT ${PROJECT_NAME}_IS_SUBPROJECT", "TRUE", {plain = true})
+        io.replace("src/png.imageio/png_pvt.h", "#include <libpng16/png.h>", "#include <png.h>", {plain = true})
         local configs = {"-DBUILD_DOCS=OFF",
                          "-DINSTALL_DOCS=OFF",
                          "-DOIIO_BUILD_TESTS=OFF",
-                         "-DOIIO_BUILD_TOOLS=OFF",
                          "-DUSE_QT=OFF",
                          "-DUSE_EXTERNAL_PUGIXML=ON",
                          "-DLINKSTATIC=ON",
                          "-DBoost_USE_STATIC_LIBS=ON",
+                         "-DCMAKE_FIND_FRAMEWORK=LAST",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_Nuke=ON",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_R3DSDK=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
-            table.insert(configs, "-DBoost_USE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+            table.insert(configs, "-DBoost_USE_STATIC_RUNTIME=" .. (package:has_runtime("MT", "MTd") and "ON" or "OFF"))
         end
         for conf, dep in pairs(configdeps) do
+            table.insert(configs, "-DUSE_" .. dep .. "=" .. (package:config(conf) and "ON" or "OFF"))
             table.insert(configs, "-DCMAKE_DISABLE_FIND_PACKAGE_" .. dep .. "=" .. (package:config(conf) and "OFF" or "ON"))
         end
+        table.insert(configs, "-DOIIO_BUILD_TOOLS=" .. (package:config("tools") and "ON" or "OFF"))
         table.insert(configs, "-DUSE_PYTHON=" .. (package:config("python") and "ON" or "OFF"))
         if package:config("python") then
             os.vrunv("python", {"-m", "pip", "install", "numpy"})
         end
-        import("package.tools.cmake").install(package, configs)
+        ext = {packagedeps = {"minizip-ng", "openssl"}}
+        if package:is_plat("windows") then
+            ext.cxflags = "/utf-8"
+        end
+        import("package.tools.cmake").install(package, configs, ext)
     end)
 
     on_test(function (package)
