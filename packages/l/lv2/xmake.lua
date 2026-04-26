e61c12e9195e23b58def6439b72599317b24ba20
EZ4Stephen <ez4stephen@tutamail.com>
2026-04-26 21:50:38 +0400

    Fix the versioning formatting

--- packages/l/lv2/xmake.lua
+++ packages/l/lv2/xmake.lua
@@ -4,10 +4,10 @@ package("lv2")
     set_description("The LV2 audio plugin specification")
     set_license("0BSD", "Apache-2.0", "CC-BY-1.0", "CC-BY-4.0", "ISC", "MIT", "W3C-19980720", "W3C-20150513")
 
-    add_urls("https://gitlab.com/lv2/lv2/-/archive/$(version).tar.gz",
+    add_urls("https://gitlab.com/lv2/lv2/-/archive/v$(version).tar.gz",
              "https://gitlab.com/lv2/lv2.git")
 
-    add_versions("v1.18.10", "15038fabd0313f281a5611f41502e3649274082b74c879bcc4a4bc5a2e79e85b")
+    add_versions("1.18.10", "15038fabd0313f281a5611f41502e3649274082b74c879bcc4a4bc5a2e79e85b")
 
     add_deps("meson", "ninja")
 	
