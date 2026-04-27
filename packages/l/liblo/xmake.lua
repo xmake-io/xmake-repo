3decfa261df2472ab368732a0f2109014cc9f054
EZ4Stephen <ez4stephen@tutamail.com>
2026-04-27 19:21:30 +0400

    Add liblo 0.34 to xrepo (#9860)
    * Add liblo 0.34 to xrepo
    
    This PR supports liblo for windows, macos, linux and bsd. However, the liblo repo seems to have support for Anrdoid too. I couldn't figure out how to add that, so I'm leaving that for anyone else to add.
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Add shared library configuration to xmake.lua
    
    * Update xmake.lua
    
    * Modify CMakeLists.txt handling for shared libraries
    
    * Add 'm' library to Linux syslinks and update Windows links
    
    * Fix CMakeLists.txt target properties handling
    
    * Add shared library config for wasm platform
    
    Add support for shared library configuration on wasm platform
    
    * Implement NDK API version requirement for Android
    
    Add Android NDK version check for liblo package.
    
    * Modify xmake.lua to handle wasm platform
    
    Exclude shared library from all targets when building for wasm.
    
    * Refactor liblo build configuration for CMake
    
    Removed WASM shared library configuration and adjusted CMakeLists.txt handling.
    
    * Modify CMakeLists.txt handling for shared libraries
    
    * Modify CMakeLists.txt handling for shared libraries
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: Saikari <lin@sz.cn.eu.org>

---
+++ packages/l/liblo/xmake.lua
@@ -0,0 +1,51 @@
+package("liblo")
+    set_homepage("https://github.com/radarsat1/liblo")
+    set_description("An implementation of the Open Sound Control protocol for POSIX systems")
+    set_license("LGPL-2.1-or-later")
+
+    add_urls("https://github.com/radarsat1/liblo/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/radarsat1/liblo.git")
+    add_versions("0.34", "e9a294c7613e1bec2abcf26f2010604643d605ed6852e16b51837400729fcbee")
+
+    add_deps("cmake")
+
+    if is_plat("linux", "cross", "bsd") then
+        add_syslinks("pthread", "m")
+    elseif is_plat("windows", "mingw") then
+        add_syslinks("wsock32", "ws2_32", "iphlpapi")
+    end
+
+    on_check("android", function (package)
+        local ndk_sdkver = package:toolchain("ndk"):config("ndk_sdkver")
+        if ndk_sdkver and tonumber(ndk_sdkver) < 24 then
+            raise("package(liblo): require ndk api >= 24")
+        end
+    end)
+
+    on_install("!wasm", function (package)
+        os.cd("cmake")
+        local configs = {
+            "-DWITH_STATIC=ON",
+            "-DWITH_TESTS=OFF",
+            "-DWITH_EXAMPLES=OFF",
+            "-DWITH_CPP_TESTS=OFF"
+        }
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        if package:config("shared") then
+            io.replace("CMakeLists.txt", [[TARGETS ${LIBRARY_STATIC} ${LIBRARY_SHARED}]], [[TARGETS ${LIBRARY_SHARED}]], {plain = true})
+        else
+            io.replace("CMakeLists.txt", [[TARGETS ${LIBRARY_STATIC} ${LIBRARY_SHARED}]], [[TARGETS ${LIBRARY_STATIC}]], {plain = true})
+        end
+        io.replace("CMakeLists.txt", [[# Tools]], [[# Tools
+    if (BUILD_SHARED_LIBS)
+        set_target_properties(${LIBRARY_STATIC} PROPERTIES EXCLUDE_FROM_ALL 1)
+    else()
+        set_target_properties(${LIBRARY_SHARED} PROPERTIES EXCLUDE_FROM_ALL 1)
+    endif()]], {plain = true})
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("lo_address_new", {includes = "lo/lo.h"}))
+    end)
