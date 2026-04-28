361e32914ca239404bf552e578d1d1c172a15890
EZ4Stephen <ez4stephen@tutamail.com>
2026-04-28 15:54:43 +0400

    Edit add_urls and add_version to match existing packages
    Changes made from comparing against asap's xmake file.

--- packages/v/vamp-plugin-sdk/xmake.lua
+++ packages/v/vamp-plugin-sdk/xmake.lua
@@ -3,11 +3,9 @@ package("vamp-plugin-sdk")
     set_description("The SDK for Vamp plugins, an API for audio analysis and feature extraction plugins.")
     set_license("BSD-3-Clause AND MIT")
 
-    add_urls("https://github.com/vamp-plugins/vamp-plugin-sdk/archive/refs/tags/vamp-plugin-sdk-v$(version).tar.gz",
-             "https://github.com/vamp-plugins/vamp-plugin-sdk.git")
+    add_urls("https://github.com/vamp-plugins/vamp-plugin-sdk.git")
 
-    --No new release tag since the addition of CMake build system, so use commit instead
-    add_versions("2.10+cmake", "44c2487763eb248a933e9eff9169cfadee375009")
+    add_versions("2024.11.20", "44c2487763eb248a933e9eff9169cfadee375009")
 
     add_deps("cmake")
 
