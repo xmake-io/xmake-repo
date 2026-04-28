dbb742e58834e903aaf5b304866899513f71ddfd
24bit-xjkp <63223769+24bit-xjkp@users.noreply.github.com>
2026-04-28 10:10:44 +0800

    Update verilator to v5.048 (#9877)

--- packages/v/verilator/xmake.lua
+++ packages/v/verilator/xmake.lua
@@ -7,6 +7,7 @@ package("verilator")
     add_urls("https://github.com/verilator/verilator/archive/refs/tags/$(version).tar.gz",
              "https://github.com/verilator/verilator.git")
 
+    add_versions("v5.048", "02d934b3f972c6d9b792350634d81eadfc9e61f347e3f3bdcaad40960b9fcb53")
     add_versions("v5.046", "002bc6d92b203eb8b4612e1d198d8108517d4ec9859e131ef328015352fe6d0c")
     add_versions("v5.044", "ded2a4a96e3b836ddc9fd5d01127999d981adee4d19133ff819b7129897d801a")
     add_versions("v5.042", "bec14f17de724851b110b698f3bd25e22effaaced7265b26d2bc13075dbfb4bf")
