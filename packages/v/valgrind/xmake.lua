4f8d9e6911174eae31f439c6ce43db1864a1a862
Akylzhan <akyl.sauranbai@gmail.com>
2026-05-12 00:16:44 +0500

    init

---
+++ packages/v/valgrind/xmake.lua
@@ -0,0 +1,24 @@
+package("valgrind")
+    set_homepage("https://valgrind.org")
+    set_description("Valgrind is a GPL'd system for debugging and profiling Linux programs.")
+    set_license("GPL-3.0")
+
+    set_kind("binary")
+
+    add_urls("https://sourceware.org/pub/valgrind/valgrind-$(version).tar.bz2", {alias = "home"})
+    add_urls("https://sourceware.org/git/valgrind.git", {alias = "git"})
+
+    add_versions("home:3.27.0", "5b5937de8257ee8f51698ea71b9711adce98061aa07daa4a685efc3af9215bef")
+    add_versions("git:3.27.0", "VALGRIND_3_27_0")
+
+    add_deps("autoconf", "automake")
+
+    add_extsources("apt::valgrind", "pacman::valgrind")
+
+    on_install("linux", function (package)
+        import("package.tools.autoconf").install(package)
+    end)
+
+    on_test(function (package)
+        os.vrun("valgrind --version")
+    end)
