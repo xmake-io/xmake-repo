bc5d8182432d21aa076cb6faeb62d73f09426e2e
EZ4Stephen <ez4stephen@tutamail.com>
2026-05-14 14:13:41 +0400

    Remove pkgconf/pkg-config as a dependency

--- packages/c/cairomm/xmake.lua
+++ packages/c/cairomm/xmake.lua
@@ -13,11 +13,6 @@ package("cairomm")
     add_configs("exceptions_api", {description = "Build exceptions API and include it in the library", default = true, type = "boolean"})
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean"})
 
-    if is_subhost("windows") then
-        add_deps("pkgconf")
-    else
-        add_deps("pkg-config")
-    end
     add_deps("meson", "ninja")
 
     on_load(function (package)
