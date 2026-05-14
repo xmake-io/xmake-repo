64df2c3db4408302261a2d07b0d615f25e976705
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2026-05-14 20:03:48 +0500

    Add TooManyCooks (#9929)
    * init
    
    * fix suggestions
    
    * specify c++20
    
    * specify desktop OS
    
    * linux only because macos,windows compilers are weird
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>

---
+++ packages/t/toomanycooks/xmake.lua
@@ -0,0 +1,43 @@
+package("toomanycooks")
+    set_homepage("https://github.com/tzcnt/TooManyCooks/")
+    set_description("C++20 concurrency framework with no compromises. Excellent performance, powerful features, and simple syntax.")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/tzcnt/TooManyCooks/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/tzcnt/TooManyCooks.git")
+
+    add_versions("1.4.0", "5c847cfd73231409301732f3e158c52b694a2bd90d336e90f558811d59ef7f69")
+
+    add_configs("priority_count", {default = 0, type = "number", description = "Allows you to specify the number of priority levels at compile time. 0 = specified at runtime"})
+    add_configs("more_threads", {default = false, type = "boolean", description = "Unlimited threads in cpu executor, otherwise fixed to 64 threads"})
+    add_configs("hwloc", {default = false, type = "boolean", description = "Build with hwloc"})
+
+    on_load(function (package)
+        if package:config("hwloc") then
+            package:add("deps", "hwloc")
+        end
+    end)
+
+    on_install("linux", function (package)
+        io.writefile("lib.cpp", [[
+            #define TMC_IMPL
+            #include "tmc/all_headers.hpp"
+        ]])
+
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+
+        import("package.tools.xmake").install(package, {
+            more_threads = package:config("more_threads"),
+            priority_count = package:config("priority_count"),
+            hwloc = package:config("hwloc")
+        })
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({ test = [[
+            #include <tmc/all_headers.hpp>
+            int main(int argc, char **argv) {
+                return tmc::async_main([]() -> tmc::task<int> { co_return 0; }());
+            }
+        ]]}, {configs = {languages = "c++20"}}))
+    end)
