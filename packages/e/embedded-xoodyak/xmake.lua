d9d4e3d1ed3a161f6cfc93af8da1302de865cfe8
Sarun Rattanasiri <midnight_w@gmx.tw>
2026-05-20 14:12:22 +0700

    Add embedded-xoodyak package

---
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -0,0 +1,21 @@
+package("embedded-xoodyak")
+    set_homepage("https://github.com/midnight-wonderer/embedded-xoodyak")
+    set_description("A portable, lightweight, and zero-allocation C library implementing the Xoodyak cryptographic scheme")
+    set_license("CC0-1.0")
+
+    add_urls("https://github.com/midnight-wonderer/embedded-xoodyak/archive/refs/tags/v$(version).tar.gz",
+        "https://github.com/midnight-wonderer/embedded-xoodyak.git")
+
+    add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("Xoodyak_Initialize", {includes = "Xoodyak.h"}))
+    end)
