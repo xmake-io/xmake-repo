74c7248aa9883e7ffda52a1e99e0ba5e414f6463
ruki <waruqi@gmail.com>
2026-05-24 03:29:51 +0000

    Update google-cloud-cpp to v3.5.0

--- packages/g/google-cloud-cpp/xmake.lua
+++ packages/g/google-cloud-cpp/xmake.lua
@@ -6,6 +6,7 @@ package("google-cloud-cpp")
     add_urls("https://github.com/googleapis/google-cloud-cpp/archive/refs/tags/$(version).tar.gz",
              "https://github.com/googleapis/google-cloud-cpp.git")
 
+    add_versions("v3.5.0", "92869fc9930ee12b1ee95c388be91cdab04669404921436ca1b1f2cf890e3bd7")
     add_versions("v2.46.0", "492734e092e5150d8395797f0d269f3d1e49ba3a959db4a332d15a1f382ff7ee")
     add_versions("v2.45.0", "3d1b5eb696832f9071bf7ef0b3f0c9fd27c1a39d5edcb8a9976c65193319fd01")
     add_versions("v2.43.0", "2aea914128db8a550bd926e1e08c155fae1caff8a451e1b644602952dd6d8b5c")
