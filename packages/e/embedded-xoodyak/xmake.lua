6779936661442fd3ec9059d9d80012095c6f1d76
ruki <waruqi@gmail.com>
2026-05-27 09:31:42 +0800

    Update xmake.lua

--- packages/e/embedded-xoodyak/xmake.lua
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -10,9 +10,6 @@ package("embedded-xoodyak")
 
     on_install(function (package)
         local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
-        end
         import("package.tools.xmake").install(package, configs)
     end)
 
