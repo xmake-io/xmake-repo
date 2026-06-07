83f3de57b62cb6fc80eefa734b0f965fc2ab68b7
AkarinATCP <akarin@icatcp.work>
2026-06-07 21:37:34 +0800

    Add stv v2.2.0 (set languages c99)

--- packages/s/stv/xmake.lua
+++ packages/s/stv/xmake.lua
@@ -3,9 +3,11 @@ package("stv")
     set_description("A Lightweight C String-View Library")
     set_homepage("https://github.com/AkarinATCP/stv")
     set_license("MIT")
-    
+
     set_urls("https://github.com/AkarinATCP/stv/archive/refs/tags/v$(version).tar.gz")
     add_versions("2.2.0", "5beed1ca3fa388266ad26130175b31811c8e4eb6ee7e1613dc7e761291e77da5")
+
+    set_languages("c99")
 
     on_install(function (package)
         os.cp("include/stv.h", package:installdir("include"))
