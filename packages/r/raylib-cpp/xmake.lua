3e038364e43437cf3521221d35d58a0095854ddd
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-06-07 16:16:25 +0100

    Update raylib-cpp to v6.0.1 (#10133)

--- packages/r/raylib-cpp/xmake.lua
+++ packages/r/raylib-cpp/xmake.lua
@@ -7,6 +7,7 @@ package("raylib-cpp")
     add_urls("https://github.com/RobLoach/raylib-cpp/archive/refs/tags/$(version).tar.gz", 
              "https://github.com/RobLoach/raylib-cpp.git")
 
+    add_versions("v6.0.1", "ebfa63c4a9a25ac81efb54189f03622249bdfcd736da28cfdc7c15340608de8d")
     add_versions("v6.0.0", "42e1378a56a8440b6ca048640e75763b64afd51a8a0ca3a5baff032761338300")
     add_versions("v5.5.1", "3426a957c50b2f79a877f75881e588a1aab25da4815172715b472b2afb549557")
     add_versions("v5.5.0", "bcb4a4e241a95376e8562aa77c29976ed0921235a9f5326822130a7bcf4860a5")
