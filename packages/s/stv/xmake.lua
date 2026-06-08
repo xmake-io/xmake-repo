f4def51fee1ad34af3078ac3c41cc94c30ce3aca
AkarinATCP <akarin@icatcp.work>
2026-06-08 13:21:58 +0800

    Add stv v2.2.0 (set languages c99 in on_test)

--- packages/s/stv/xmake.lua
+++ packages/s/stv/xmake.lua
@@ -7,12 +7,11 @@ package("stv")
     set_urls("https://github.com/AkarinATCP/stv/archive/refs/tags/v$(version).tar.gz")
     add_versions("2.2.0", "5beed1ca3fa388266ad26130175b31811c8e4eb6ee7e1613dc7e761291e77da5")
 
-    set_languages("c99")
-
     on_install(function (package)
         os.cp("include/stv.h", package:installdir("include"))
     end)
 
     on_test(function (package)
+        package:set("languages", "c99")
         assert(package:has_cincludes("stv.h"), "stv.h not found")
     end)
