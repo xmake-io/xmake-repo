ff3a4aa9ddf9ce5747d8d4124eabe17544772dc0
apocelipes <seve3r@outlook.com>
2026-06-15 18:12:01 +0800

    fix build

--- packages/o/ois/xmake.lua
+++ packages/o/ois/xmake.lua
@@ -19,7 +19,7 @@ package("ois")
         add_syslinks("dinput8", "dxguid", "ole32", "oleaut32", "user32", "uuid", "xinput", "winmm")
     end
 
-    on_install("windows", "linux", "macosx", function (package)
+    on_install("windows|!arm64", "linux", "macosx", function (package)
         local configs = {
             "-DOIS_BUILD_DEMOS=OFF", 
             "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW",
