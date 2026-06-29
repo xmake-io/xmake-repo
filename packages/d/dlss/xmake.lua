1474bc25e76909adaf16156ba6620cc76cdfda2f
ruki <waruqi@gmail.com>
2026-06-29 03:44:38 +0000

    Update dlss to v310.7.0

--- packages/d/dlss/xmake.lua
+++ packages/d/dlss/xmake.lua
@@ -6,6 +6,7 @@ package("dlss")
     add_urls("https://github.com/NVIDIA/DLSS/archive/refs/tags/$(version).tar.gz",
              "https://github.com/NVIDIA/DLSS.git", {submodules = false})
 
+    add_versions("v310.7.0", "eba34a073b2cf8dbb30991a6c4ec1bd463be533597a5e5489322fe8473ebed75")
     add_versions("v310.6.0", "4c10c3093f9ae6c434c8dc374bc685be43c24e79d65119886f2d0e02e2a18791")
     add_versions("v310.5.3", "6b54a684b5b31e819a51742ad534abb4e8cdada76572f061a5d3149c7432a0a1")
     add_versions("v310.5.0", "9effc97025eb8c61f85e41b97e4ccaaed18313e245c7f0b739525c6f26a5d4e6")
