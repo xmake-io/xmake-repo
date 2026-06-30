00feb2fd82d6a38c084fd44deff7058c1006111c
Saikari <lin@sz.cn.eu.org>
2026-06-30 09:28:55 +0300

    try wrap in on_load

--- packages/n/node_crunch2/xmake.lua
+++ packages/n/node_crunch2/xmake.lua
@@ -12,7 +12,7 @@ package("node_crunch2")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
 
-    add_deps("spdlog", "asio", "lz4", "openssl", "taocpp-json")
+    add_deps("spdlog", "asio", "lz4", "taocpp-json")
 
     if on_check then
         on_check(function (package)
@@ -40,6 +40,10 @@ package("node_crunch2")
             "package(node_crunch2) requires a compiler supporting parenthesized aggregate initialization.")
         end)
     end
+
+    on_load(function (package)
+        package:add("deps", "openssl")
+    end)
 
     on_install("!wasm", function (package)
         import("package.tools.xmake").install(package)
