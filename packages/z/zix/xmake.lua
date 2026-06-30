e37d473d4c5c221361efdf7b2eace4ba4fe2690c
Saikari <lin@sz.cn.eu.org>
2026-06-30 18:29:34 +0300

    zix: Fix FreeBSD pkgconfig discovery issue
    Added logic to copy .pc files for FreeBSD package discovery.

--- packages/z/zix/xmake.lua
+++ packages/z/zix/xmake.lua
@@ -34,6 +34,15 @@ package("zix")
             "-Dtests_cpp=disabled",
         }
         import("package.tools.meson").install(package, configs)
+        -- Copying .pc files from libdata/pkgconfig to lib/pkgconfig after install fixes the FreeBSD package discovery issue.
+        if package:is_plat("bsd") then
+            local srcdir = path.join(package:installdir(), "libdata", "pkgconfig")
+            local dstdir = path.join(package:installdir(), "lib", "pkgconfig")
+            if os.isdir(srcdir) then
+                os.mkdir(dstdir)
+                os.cp(path.join(srcdir, "*.pc"), dstdir)
+            end
+        end
     end)
 
     on_test(function (package)		
