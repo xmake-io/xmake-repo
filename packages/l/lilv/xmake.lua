9af311820b5feeff2213180afdd8ff8b5b74f64e
EZ4Stephen <ez4stephen@tutamail.com>
2026-07-01 10:17:11 +0400

    Add bsd .pc patch for lilv
    Won't be tested in CI until something depending on lilv gets added, but better to add the patch early.

--- packages/l/lilv/xmake.lua
+++ packages/l/lilv/xmake.lua
@@ -51,6 +51,15 @@ package("lilv")
         table.insert(configs, "-Dbindings_cpp=" .. (package:config("bindings_cpp") and "enabled" or "disabled"))
         table.insert(configs, "-Dbindings_py=" .. (package:config("bindings_py") and "enabled" or "disabled"))
         import("package.tools.meson").install(package, configs)
+        -- Copying .pc files from libdata/pkgconfig to lib/pkgconfig after install fixes the FreeBSD package discovery issue.
+        if package:is_plat("bsd") then
+            local srcdir = path.join(package:installdir(), "libdata", "pkgconfig")
+            local dstdir = path.join(package:installdir(), "lib", "pkgconfig")
+            if os.isdir(srcdir) then
+                os.mkdir(dstdir)
+                os.cp(path.join(srcdir, "*.pc"), dstdir)
+            end
+        end
     end)
 
     on_test(function (package)
