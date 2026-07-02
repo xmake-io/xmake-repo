b4f4a3770d026e94052a7f56f1e466738f5c08da
Jean Letessier <jean.letessier@protonmail.com>
2026-07-02 07:46:49 +0200

    Simplify test in box3d.

--- packages/b/box3d/xmake.lua
+++ packages/b/box3d/xmake.lua
@@ -40,24 +40,6 @@ package("box3d")
             void test(int argc, char** argv) {
                 b3WorldDef worldDef = b3DefaultWorldDef();
                 b3WorldId worldId = b3CreateWorld(&worldDef);
-                b3BodyDef groundBodyDef = b3DefaultBodyDef();
-                groundBodyDef.position = (b3Vec3){ 0.0f, -10.0f, 0.0f };
-                b3BodyId groundId = b3CreateBody(worldId, &groundBodyDef);
-                b3BoxHull groundBox = b3MakeBoxHull(50.0f, 10.0f, 50.0f);
-                b3ShapeDef groundShapeDef = b3DefaultShapeDef();
-                b3CreateHullShape(groundId, &groundShapeDef, &groundBox.base);
-                b3BodyDef bodyDef = b3DefaultBodyDef();
-                bodyDef.type = b3_dynamicBody;
-                bodyDef.position = (b3Vec3){ 0.0f, 4.0f, 0.0f };
-                b3BodyId bodyId = b3CreateBody(worldId, &bodyDef);
-                b3BoxHull dynamicBox = b3MakeCubeHull(1.0f);
-                b3ShapeDef shapeDef = b3DefaultShapeDef();
-                shapeDef.density = 1.0f;
-                shapeDef.baseMaterial.friction = 0.3f;
-                b3CreateHullShape(bodyId, &shapeDef, &dynamicBox.base);
-                b3World_Step(worldId, 1.0f / 60.0f, 4);
-                b3Vec3 position = b3Body_GetPosition(bodyId);
-                b3Quat rotation = b3Body_GetRotation(bodyId);
                 b3DestroyWorld(worldId);
             }
         ]]}, {configs = {languages = "c17"}, includes = "box3d/box3d.h" }))
