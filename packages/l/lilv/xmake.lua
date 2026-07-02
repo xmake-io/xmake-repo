61a3b50334911ea0c0a833746eb2910688ae35fa
EZ4Stephen <ez4stephen@tutamail.com>
2026-07-02 06:30:57 +0400

    Add lilv (#10311)
    * Add lilv
    
    * Set bindings_py default to false
    
    * Add pkgconf/pkg-config for zix
    
    Hoping to fix the error for freebsd of zix-0 not getting found.
    
    * zix: Fix FreeBSD pkgconfig discovery issue
    
    Added logic to copy .pc files for FreeBSD package discovery.
    
    * Add bsd .pc patch for serd
    
    * Add bsd .pc patch for sord
    
    * Add bsd .pc patch for sratom
    
    * Add bsd .pc patch for lilv
    
    Won't be tested in CI until something depending on lilv gets added, but better to add the patch early.
    
    ---------
    
    Co-authored-by: Saikari <lin@sz.cn.eu.org>

--- packages/l/lilv/xmake.lua
+++ packages/l/lilv/xmake.lua
@@ -1,13 +1,13 @@
-package("sord")
-    set_description("Lightweight C library for storing RDF statements in memory.")
+package("lilv")
+    set_description("LV2 host library.")
     set_license("ISC")
 
-    add_urls("https://gitlab.com/drobilla/sord/-/archive/v$(version)/sord-v$(version).tar.gz",
-             "https://gitlab.com/drobilla/sord.git")
+    add_urls("https://gitlab.com/lv2/lilv/-/archive/v$(version)/lilv-v$(version).tar.gz",
+             "https://gitlab.com/lv2/lilv.git")
 
-    add_versions("0.16.22", "040fb3f369dd49a7717eb28ca0a66766352e25e760729903fc8a01e117122901")
+    add_versions("0.28.0", "006065dcb59ccaad5463e6bb4598160e41dd6474a959838e74820f60a849bfdb")
 
-    add_deps("meson", "ninja", "zix", "serd")
+    add_deps("meson", "ninja", "zix", "lv2", "serd", "sord", "sratom")
 
     if is_plat("wasm") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
@@ -20,30 +20,36 @@ package("sord")
     else
         add_deps("pkg-config", {host = true})
     end
-
     add_configs("tools", { description = "Build command line utilities", default = false, type = "boolean"})
-    add_configs("bindings_cpp", { description = "Build C++ bindings", default = true, type = "boolean"})
+    add_configs("dynmanifest", {description = "Build dynamic manifest support", default = false, type = "boolean"})
+    add_configs("bindings_cpp", {description = "Build C++ bindings", default = true, type = "boolean"})
+    add_configs("bindings_py", {description = "Build Python bindings", default = false, type = "boolean"})
 
     on_load(function (package)
-        if package:config("tools") then
-            package:add("deps", "pcre2")
-        end
-
         if not package:config("shared") then
-            package:add("defines", "SORD_STATIC")
+            package:add("defines", "LILV_STATIC")
         end
 
-        package:add("includedirs", "include/sord-0")
+        if package:config("tools") then
+            package:add("deps", "libsndfile")
+        end
+
+        if package:config("bindings_py") then
+            package:add("deps", "python")
+        end
+
+        package:add("includedirs", "include/lilv-0")
     end)
 
-    on_install(function (package)
+    on_install("!android", function (package)
         local configs = {
             "-Ddocs=disabled",
-            "-Dman=disabled",
             "-Dtests=disabled",
         }
-        table.insert(configs, "-Dbindings_cpp=" .. (package:config("bindings_cpp") and "enabled" or "disabled"))
         table.insert(configs, "-Dtools=" .. (package:config("tools") and "enabled" or "disabled"))
+        table.insert(configs, "-Ddynmanifest=" .. (package:config("dynmanifest") and "enabled" or "disabled"))
+        table.insert(configs, "-Dbindings_cpp=" .. (package:config("bindings_cpp") and "enabled" or "disabled"))
+        table.insert(configs, "-Dbindings_py=" .. (package:config("bindings_py") and "enabled" or "disabled"))
         import("package.tools.meson").install(package, configs)
         -- Copying .pc files from libdata/pkgconfig to lib/pkgconfig after install fixes the FreeBSD package discovery issue.
         if package:is_plat("bsd") then
@@ -57,5 +63,5 @@ package("sord")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("sord_new", {includes = "sord/sord.h"}))
+        assert(package:has_cfuncs("lilv_world_new", {includes = "lilv/lilv.h"}))
     end)
