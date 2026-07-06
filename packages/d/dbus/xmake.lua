cd7e17a9b91ab8b1268ed744d02696c8ae673924
Saikari <lin@sz.cn.eu.org>
2026-07-06 05:17:25 +0300

    dbus: update version to 1.16.2 and refine dependency handling

--- packages/d/dbus/xmake.lua
+++ packages/d/dbus/xmake.lua
@@ -1,5 +1,4 @@
 package("dbus")
-
     set_homepage("https://www.freedesktop.org/wiki/Software/dbus/")
     set_description("D-Bus is a message bus system, a simple way for applications to talk to one another.")
     set_license("GPL-2.0-or-later")
@@ -8,6 +7,7 @@ package("dbus")
     add_versions("1.14.2", "9ec5aad6310f79149aa04e8c6bd9e5e2cdca47cf3acec2d23ee9fe06ac7e7a62")
     add_versions("1.14.6", "2533742eb324fa7fbb093a3ed0ff436c7eb11861fd6a31e9b857fc4878f01831")
     add_versions("1.14.8", "273718fe5150a1a44fe77abcf442dc64082f7375fdc15fcbd80b84316f897326")
+    add_versions("1.16.2", "d77cc71acd93e85f2bd2a6fe3a40e5bd023519e3e9fa9b5361e7109f42b74060")
 
     if is_plat("mingw") and is_subhost("msys") then
         add_extsources("pacman::dbus")
@@ -17,29 +17,43 @@ package("dbus")
         add_extsources("brew::dbus")
     end
 
-    add_deps("expat", "cmake")
+    add_deps("cmake")
+    if is_plat("bsd") then
+        add_deps("expat", {system = false})
+    else
+        add_deps("expat")
+    end
     add_includedirs("include/dbus-1.0", "lib/dbus-1.0/include")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
     add_configs("system_bus_address", {description = "D-Bus system bus address.", type = "string"})
 
-    on_install("windows", "linux", "macosx", "cross", function (package)
+    on_install(function (package)
         local configs = {"-DDBUS_BUILD_TESTS=OFF", "-DDBUS_ENABLE_DOXYGEN_DOCS=OFF", "-DDBUS_ENABLE_XML_DOCS=OFF"}
         table.insert(configs, "-DDBUS_SESSION_SOCKET_DIR=" .. package:installdir("socket"))
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         local system_bus_address = package:config("system_bus_address")
         if system_bus_address then
             table.insert(configs, "-DDBUS_SYSTEM_BUS_DEFAULT_ADDRESS=" .. system_bus_address)
         end
-
-        local packagedeps
         if package:is_plat("windows") then
-            packagedeps = "expat"
             io.replace("CMakeLists.txt", "find_package(EXPAT)", "", {plain = true})
             io.replace("CMakeLists.txt", "NOT EXPAT_FOUND", "FALSE", {plain = true})
+        else
+            local expat = package:dep("expat")
+            local fetchinfo = expat and expat:fetch()
+            if fetchinfo then
+                local incdir = table.wrap(fetchinfo.includedirs or fetchinfo.sysincludedirs)[1]
+                local libfile = table.wrap(fetchinfo.libfiles)[1]
+                if incdir then
+                    table.insert(configs, "-DEXPAT_INCLUDE_DIR=" .. incdir)
+                end
+                if libfile then
+                    table.insert(configs, "-DEXPAT_LIBRARY=" .. libfile)
+                end
+            end
         end
-        import("package.tools.cmake").install(package, configs, {packagedeps = packagedeps})
+        import("package.tools.cmake").install(package, configs, {packagedeps = "expat"})
     end)
 
     on_test(function (package)
