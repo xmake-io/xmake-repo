8d8ece25f427532c0b1462536b81b7785afe9338
Master_Laplace <60772783+MasterLaplace@users.noreply.github.com>
2026-07-07 17:06:28 +0200

    Add liblsl and brainflow (#10370)
    * feat: add xmake.lua files for the brainflow and liblsl packages
    
    * feat: update package descriptions for brainflow and liblsl
    
    * Update liblsl to v1.17.7
    
    * Update brainflow to 5.22.2
    
    * fix: disable shared library build for brainflow package
    
    * fix: add Windows-specific flags and disable link optimizations for liblsl package
    
    * fix: update namespace in DSPFilters for Windows compatibility
    
    * fix: remove Windows-specific handling in brainflow package
    
    * fix: add missing Windows-specific syslinks for liblsl package
    
    * fix: remove BSD platform support from brainflow package
    
    * fix: refactor package loading for brainflow and liblsl to simplify syslink handling

---
+++ packages/b/brainflow/xmake.lua
@@ -0,0 +1,54 @@
+package("brainflow")
+    set_homepage("https://github.com/brainflow-dev/brainflow")
+    set_description("BrainFlow is a library intended to obtain, parse and analyze EEG, EMG, ECG and other kinds of data from biosensors.")
+    set_license("MIT")
+
+    add_urls("https://github.com/brainflow-dev/brainflow/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/brainflow-dev/brainflow.git")
+    add_versions("5.22.2", "a7e21b1b9d086370496282a5198c904cb25199cacd9c6a809b9adce544fcbe36")
+    add_versions("5.20.1", "0f01f00025c67ce52745be26ebc049d1ba68d398c3fe5a6df6c766c3bf6d40ce")
+
+    add_deps("cmake")
+
+    add_includedirs("inc")
+    add_links("Brainflow", "BoardController", "DataHandler", "MLModule")
+    if is_plat("linux") then
+        add_syslinks("pthread", "dl")
+    end
+
+    -- Windows (MSVC) is intentionally omitted: BrainFlow only supports an MSVC
+    -- build with the Windows 8.1 SDK (https://github.com/brainflow-dev/brainflow/blob/master/.github/workflows/run_windows.yml)
+    -- and does not target Windows on ARM at all, so it does not compile against a recent MSVC/SDK.
+    on_install("linux", "macosx", function (package)
+        local configs = {
+            "-DBUILD_TESTS=OFF",
+            "-DBUILD_BLUETOOTH=OFF",
+            "-DBUILD_BLE=OFF",
+            "-DBUILD_ONNX=OFF",
+            "-DBUILD_PERIPHERY=OFF",
+            "-DBUILD_OYMOTION_SDK=OFF",
+            "-DBUILD_SYNCHRONI_SDK=OFF",
+            "-DUSE_LIBFTDI=OFF",
+            "-DUSE_OPENMP=OFF",
+            "-DWARNINGS_AS_ERRORS=OFF",
+            "-DBUILD_SHARED_LIBS=OFF"
+        }
+
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        local version = package:version()
+        if version then
+            table.insert(configs, "-DBRAINFLOW_VERSION=" .. tostring(version))
+        end
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "board_shim.h"
+            int main() {
+                auto version = BoardShim::get_version();
+                return version.empty();
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
