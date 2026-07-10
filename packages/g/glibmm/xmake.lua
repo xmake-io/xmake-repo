58efc6c70fd4fa4f95cbcfe22fb71dff19925166
EZ4Stephen <ez4stephen@tutamail.com>
2026-07-10 06:39:36 +0400

    libsigcplusplus, glibmm: Add bsd support (#10368)
    * libsigcplusplus: Add bsd platform
    
    * glibmm: Add bsd support
    
    * Fix for glibmm bsd
    
    * Update xmake.lua
    
    * Use opt for the bsd fix
    
    * Update opts
    
    * Try using meson's b_lundef flag for bsd

--- packages/g/glibmm/xmake.lua
+++ packages/g/glibmm/xmake.lua
@@ -41,15 +41,22 @@ package("glibmm")
             "lib/giomm-" .. abi .. "/include")
     end)
 
-    on_install("!android and !wasm and !iphoneos and !bsd", function (package)
+    on_install("!android and !wasm and !iphoneos", function (package)
         -- Prevent building generate_defs_glib, generate_defs_gio. Otherwise when being built,
         -- errors arise on mingw builds. Doesn't hurt to filter for all platforms.
         io.replace("tools/extra_defs_gen/meson.build", "%s*executable%b()", "", {pattern = true, multiline = true})
+
         local configs = {"-Dbuild-documentation=false",
                          "-Dbuild-examples=false",
                          "-Dmsvc14x-parallel-installable=false"}
         table.insert(configs, "-Dbuild-deprecated-api=" .. (package:config("deprecated_api") and "true" or "false"))
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
+
+        -- FreeBSD: environ is provided by the main executable, not libc.
+        -- Allow undefined symbols in the shared library; resolved at runtime.
+        if package:is_plat("bsd") then
+            table.insert(configs, "-Db_lundef=false")
+        end
         import("package.tools.meson").install(package, configs)
     end)
 
