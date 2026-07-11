f9a1cf0a87a723a762702d8a2190ef954c9e84bb
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-07-11 09:44:18 +0100

    new package

---
+++ packages/q/qjswrapper/xmake.lua
@@ -0,0 +1,25 @@
+package("qjswrapper")
+    set_homepage("https://github.com/burdockcascade/qjswrapper")
+    set_description("A modern, high-level C++ wrapper library designed to integrate and interact with the QuickJS engine.")
+    set_license("MIT")
+
+    add_urls("https://github.com/burdockcascade/qjswrapper/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/burdockcascade/qjswrapper.git")
+
+    add_versions("v0.1.0", "e38144b0c01cf1e484a0417ef97e7fa8cf307fb8515a3514512cd32605d7c6f4")
+
+    add_deps("quickjs-ng")
+
+    on_install(function (package)
+        os.cp("include/qjswrapper.hpp", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({
+            test = [[
+                void test() {
+                    qjs::Engine engine;
+                }
+            ]]
+        }, {configs = {languages = "cxx23"}, includes = "qjswrapper.hpp"}))
+    end)
