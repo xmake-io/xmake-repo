b5bf83275a475b3368c03e7f6bd57f325a33e60e
Maltose <30581989+Maltose0118@users.noreply.github.com>
2026-07-14 20:20:24 +0800

    Add structural_interface package

---
+++ packages/s/structural_interface/xmake.lua
@@ -0,0 +1,39 @@
+package("structural_interface")
+    set_policy("check.auto_ignore_flags", false)
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/Maltose0118/structural_interface")
+    set_description("Header-only C++26 library for structural interfaces and type erasure")
+    set_license("MIT")
+
+    add_urls("https://github.com/Maltose0118/structural_interface/archive/refs/tags/v$(version).tar.gz")
+    add_versions("0.1.0", "a71b07193d9fdfc1bf74b5898a057331ead982ee8896567ea51761af4ba184e1")
+
+    on_load(function(package)
+        package:add("cxxflags", "-std=c++26", "-freflection", {
+            tools = "gcc",
+            force = true
+        })
+    end)
+
+    on_install(function(package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function(package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <structural_interface.hpp>
+
+            void test() {
+                struct Interface {
+                    void draw() const;
+                };
+
+                static_assert(si::satisfies<Interface, Interface>);
+            }
+        ]]}, {
+            configs = {
+                languages = "c++26",
+                cxflags = {"-freflection"}
+            }
+        }))
+    end)
