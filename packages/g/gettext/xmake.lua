a3ffb3b03da092ad7efbacbfd2c743471558a621
apocelipes <seve3r@outlook.com>
2026-07-14 18:23:43 +0800

    gettext: support windows (#10427)
    * gettext: support windows
    
    * fix build

--- packages/g/gettext/xmake.lua
+++ packages/g/gettext/xmake.lua
@@ -19,7 +19,20 @@ package("gettext")
     if is_plat("macosx") then
         add_frameworks("CoreFoundation")
     end
+    if is_plat("windows") then
+        add_deps("libintl")
+    end
     add_deps("libiconv")
+
+    if on_check then
+        on_check("android", function (package)
+            local ndk = package:toolchain("ndk")
+            local ndkver = ndk and ndk:config("ndkver")
+            if package:version() and package:version():ge("1.0") and ndkver and tonumber(ndkver) < 27 then
+                raise("package(gettext >= 1.0) does not support NDK versions earlier than r27")
+            end
+        end)
+    end
     
     on_load(function(package)
         if is_subhost("windows") then
@@ -29,7 +42,7 @@ package("gettext")
         end
     end)
 
-    on_install("macosx", "linux", "android", function (package)
+    on_install("macosx", "linux", "android", "windows|x64", function (package)
         local configs = {"--disable-dependency-tracking",
                          "--disable-silent-rules",
                          "--with-included-glib",
