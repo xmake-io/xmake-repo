9bdc76d2fdc35e38f647f4b1536e3c36b663d6ee
ruki <waruqi@gmail.com>
2026-07-15 12:37:09 +0800

    Update mongoose to 7.22 (#10435)

--- packages/m/mongoose/xmake.lua
+++ packages/m/mongoose/xmake.lua
@@ -8,6 +8,7 @@ package("mongoose")
     add_urls("https://github.com/cesanta/mongoose/archive/refs/tags/$(version).tar.gz",
              "https://github.com/cesanta/mongoose.git")
 
+    add_versions("7.22", "87727cd2c240ff559b16e9710d44b61ba3513dbee50428bd8ee1596d7c58460a")
     add_versions("7.21", "d4ddbd12c12f223abefcc0a74417a638ae5c118d7cf10ba546553c6b0e0b5ada")
     add_versions("7.20", "ede2371ff4e41e95fd7b2de83a7df727388307a64d3706046ead320defe35e7e")
     add_versions("7.19", "7ae38c9d673559d1d9c1bd72c0bdd4a98a2cae995e87cae98b4604d5951762b8")
