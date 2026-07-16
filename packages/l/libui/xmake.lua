ba964f2d5cf0b3e9224eead99ff2f4c3dfd7c553
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-07-16 17:38:20 +0200

    libui: update to 2025.3.15
    and make it buildable again

--- packages/l/libui/xmake.lua
+++ packages/l/libui/xmake.lua
@@ -4,20 +4,23 @@ package("libui")
 
     set_urls("https://github.com/libui-ng/libui-ng.git")
     add_versions("2022.12.3", "8c82e737eea2f8ab3667e227142abd2fd221f038")
+    add_versions("2025.3.15", "43ba1ef553c8993a43a67f1ce6e35983a2660d8c")
 
     add_deps("meson", "ninja")
 
     if is_plat("macosx") then
         add_frameworks("CoreGraphics", "CoreText", "Foundation", "AppKit")
     elseif is_plat("windows") then
-        add_syslinks("user32", "ole32", "gdi32", "d2d1", "dwrite", "comctl32", "windowscodecs")
+        -- the windows meson build file links all of these with a todo to prune the list
+        add_syslinks("user32", "kernel32", "gdi32", "comctl32", "uxtheme", "msimg32", "comdlg32", "d2d1", "dwrite", "ole32", "oleaut32", "oleacc", "uuid", "windowscodecs")
     elseif is_plat("linux") then
-        add_deps("gtk+3", "glib")
+        add_deps("gtk3", "glib")
     end
 
     on_install("linux", "macosx", "windows", function (package)
         local configs = {"-Dexamples=false", "-Dtests=false"}
-        table.insert(configs, "--default-library=" .. (package:config("shared") and "shared" or "static"))
+        -- this causes an error because both `--default-library` and `-Ddefault_library` being set
+        --table.insert(configs, "--default-library=" .. (package:config("shared") and "shared" or "static"))
         import("package.tools.meson").install(package, configs)
     end)
 
