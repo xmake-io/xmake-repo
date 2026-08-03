9a18e8b3cda70750022a73d40c02a083b3f47e23
Saikari <lin@sz.cn.eu.org>
2026-08-03 16:48:17 +0300

    Update EUI-NEO package definition and build options

--- packages/e/eui-neo/xmake.lua
+++ packages/e/eui-neo/xmake.lua
@@ -1,32 +1,19 @@
--- xrepo package definition for EUI-NEO
--- https://github.com/sudoevolve/EUI-NEO
---
--- After submitting this to the xrepo-repo, users can do:
---   add_requires("eui-neo")
---   target("myapp")
---       add_packages("eui-neo")
---
--- Configurable options:
---   add_requires("eui-neo", {configs = {render_backend = "vulkan", window_backend = "glfw"}})
---   add_requires("eui-neo", {configs = {shared = true}})
-
 package("eui-neo")
     set_homepage("https://github.com/sudoevolve/EUI-NEO")
     set_description("Cross-platform, high-performance, low-overhead C++17 GPUI framework")
     set_license("Apache-2.0")
 
-    set_urls("https://github.com/lilyco-42/EUI-NEO.git")
+    add_urls("https://github.com/sudoevolve/EUI-NEO/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/sudoevolve/EUI-NEO.git")
 
-    add_versions("v0.5.5", "df2399495a139a21290bf8a9288f5efba2c52bde")
+    add_versions("v0.5.5", "cf0da91d7544fe406b704922137fd4d55ed080b3e647501e0ca5303abb00eb98")
 
-    -- Build configuration options (mirror the xmake.lua options)
     add_configs("window_backend", {description = "Window backend", default = "glfw", values = {"glfw", "sdl2"}})
     add_configs("render_backend", {description = "Render backend", default = "opengl", values = {"auto", "opengl", "vulkan"}})
-    add_configs("shared", {description = "Build eui_neo as a shared library", default = false, type = "boolean"})
     add_configs("markdown", {description = "Enable MD4C Markdown parsing support", default = true, type = "boolean"})
     add_configs("vulkan_low_latency", {description = "Prefer low-latency Vulkan presentation", default = false, type = "boolean"})
 
-    -- External dependencies resolved by xrepo
+    add_deps("cmake")
     add_deps("freetype", "libpng", "zlib")
 
     on_load(function(package)
@@ -39,28 +26,31 @@ package("eui-neo")
         if package:config("render_backend") == "vulkan" then
             package:add("deps", "vulkan")
         end
-        if not package:is_plat("windows") then
+        if not package:is_plat("windows", "mingw") then
             package:add("deps", "curl")
         end
     end)
 
     on_install(function(package)
-        -- Build EUI-NEO using its own xmake.lua
         local configs = {}
-        configs.window_backend = package:config("window_backend")
-        configs.render_backend = package:config("render_backend")
-        configs.shared = package:config("shared")
-        configs.markdown = package:config("markdown")
-        configs.vulkan_low_latency = package:config("vulkan_low_latency")
-        -- Only build the core library (skip examples/user apps/modules)
-        configs.apps = false
-        configs.user_apps = false
-        import("package.tools.xmake").install(package, configs)
+        table.insert(configs, "-DEUI_VULKAN_LOW_LATENCY_PRESENT=" .. (package:config("vulkan_low_latency") and "ON" or "OFF"))
+        table.insert(configs, "-DEUI_RENDER_BACKEND=" .. package:config("render_backend"))
+        table.insert(configs, "-DEUI_WINDOW_BACKEND=" .. package:config("window_backend"))
+        table.insert(configs, "-DEUI_ENABLE_MARKDOWN=" .. (package:config("markdown") and "ON" or "OFF"))
+        table.insert(configs, "-DEUI_BUILD_APPS=OFF")
+        table.insert(configs, "-DEUI_BUILD_USER_APPS=OFF")
+        table.insert(configs, "-DEUI_ENABLE_INSTALL=ON")
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DEUI_BUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
-    on_test(function(package)
-        assert(os.isfile(path.join(package:installdir(), "lib", "eui_neo.lib"))
-            or os.isfile(path.join(package:installdir(), "lib", "libeui_neo.a"))
-            or os.isfile(path.join(package:installdir(), "lib", "libeui_neo.so")))
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                eui::Ui ui;
+                ui.stack("root");
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "eui_neo.h"}))
     end)
-package_end()
