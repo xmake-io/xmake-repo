95dc77fc9a9bb14e64656b841949a8afc682a1f6
Simon Pribylski <66266021+Persson-dev@users.noreply.github.com>
2026-08-04 03:39:43 +0200

    add libbcrypt package (#10563)
    * add libbcrypt package
    
    * don't build for windows

---
+++ packages/l/libbcrypt/xmake.lua
@@ -0,0 +1,31 @@
+package("libbcrypt")
+    set_homepage("https://github.com/trusch/libbcrypt")
+    set_description("A c++ wrapper around bcrypt password hashing")
+    set_license("MIT")
+
+    add_urls("https://github.com/trusch/libbcrypt.git")
+
+    add_versions("2021.06.22", "d6523c370de6e724ce4ec703e2449b5b028ea3b1")
+
+    add_deps("cmake")
+
+    on_install("!windows and !mingw", function (package)
+        local configs = {}
+
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                std::string password = "test";
+                std::string hash = BCrypt::generateHash(password);
+
+                BCrypt::validatePassword(password,hash);
+                BCrypt::validatePassword("test1",hash);
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "bcrypt/BCrypt.hpp"}))
+    end)
