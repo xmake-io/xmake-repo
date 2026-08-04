1e10c6158e63e9dc9dc4c1b2c763e45396640cb9
ruki <waruqi@gmail.com>
2026-08-04 02:27:31 +0000

    Update tinygltf to v3.0.1

--- packages/t/tinygltf/xmake.lua
+++ packages/t/tinygltf/xmake.lua
@@ -7,6 +7,7 @@ package("tinygltf")
     add_urls("https://github.com/syoyo/tinygltf/archive/refs/tags/$(version).tar.gz",
              "https://github.com/syoyo/tinygltf.git")
 
+    add_versions("v3.0.1", "b7e953f13a30d7b6fd677e484de35febde954143a265da15dacd92ed171c73e6")
     add_versions("v3.0.0", "806b0f1ba8007837fcd531e23872286f8a8870ee23275e1eb5304cdb48e4cb30")
     add_versions("v2.9.7", "9d31cf7f22e81febaf1ad587d7722582c154f7d9125673ee46c0c594765e8f35")
     add_versions("v2.9.6", "ba2c47a095136bfc8a5d085421e60eb8e8df3bca4ae36eb395084c1b264c6927")
