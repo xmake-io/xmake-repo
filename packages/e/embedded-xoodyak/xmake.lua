a79f6b5fff73e1dc0a96d3c4a69aad555a4f18c0
Saikari <lin@sz.cn.eu.org>
2026-08-05 20:25:10 +0300

    Update xmake.lua

--- packages/e/embedded-xoodyak/xmake.lua
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -8,14 +8,10 @@ package("embedded-xoodyak")
 
     add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
 
-    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "2011966e92938663d5c6df833e5b89096c8a2e861ced040cf164e04b62d36f09")
-
-    if is_plat("windows", "mingw") then
-        add_configs("shared", {description = "Build shared binaries.", default = false, type = "boolean", readonly = true})
-    end
+    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "b3695a9fc3efae3242f219bf19adce2faf02a42c6adab6b5dcb35e55b9633e53")
 
     on_install(function (package)
-        import("package.tools.xmake").install(package)
+        import("package.tools.xmake").install(package, {}, {target = "xoodyak"})
     end)
 
     on_test(function (package)
