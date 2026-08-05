00ac5780d14d84116add85c2b06a5ba1c4f15682
Saikari <lin@sz.cn.eu.org>
2026-08-05 18:42:57 +0300

    Update patch checksum for fix-msvc-asm.patch

--- packages/e/embedded-xoodyak/xmake.lua
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -8,7 +8,7 @@ package("embedded-xoodyak")
 
     add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
 
-    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "0dae945231b4f141868387e0508ef213ea1f5771b0116a9736a614f87375ef61")
+    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "a19489a70ae12f792bc4dad975e93e9c9a706b19dd4300971e48650589ed4498")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
