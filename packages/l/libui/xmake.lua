f9b2bfe1e4fe62f87052fc21c1bacd7e0e2defa4
Saikari <lin@sz.cn.eu.org>
2026-08-05 23:06:50 +0300

    assume add_links for linux os

--- packages/l/libui/xmake.lua
+++ packages/l/libui/xmake.lua
@@ -10,6 +10,8 @@ package("libui")
 
     if is_plat("macosx") then
         add_frameworks("CoreGraphics", "CoreText", "Foundation", "AppKit")
+    elseif is_plat("linux") then
+        add_links("ui", "pango-1.0", "pangoxft-1.0", "pangocairo-1.0", "pangoft2-1.0")
     elseif is_plat("windows") then
         -- the windows meson build file links all of these with a todo to prune the list
         add_syslinks("user32", "kernel32", "gdi32", "comctl32", "uxtheme", "msimg32", "comdlg32", "d2d1", "dwrite", "ole32", "oleaut32", "oleacc", "uuid", "windowscodecs")
