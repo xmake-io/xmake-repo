3e4678444db332750c6d5424b69cdbbbb6484635
Saikari <lin@sz.cn.eu.org>
2026-08-05 18:23:36 +0300

    Add MSVC assembly patch for installation
    Added a patch for MSVC assembly during installation.

--- packages/e/embedded-xoodyak/xmake.lua
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -8,7 +8,9 @@ package("embedded-xoodyak")
 
     add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
 
-    on_install("!windows", function (package)
+    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "0dae945231b4f141868387e0508ef213ea1f5771b0116a9736a614f87375ef61")
+
+    on_install(function (package)
         import("package.tools.xmake").install(package)
     end)
 
