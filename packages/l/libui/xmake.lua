5a4933431dbe41d0712fcb3c46b60e464985b192
Saikari <lin@sz.cn.eu.org>
2026-08-05 21:55:28 +0300

    Try to supply link order
    Removed Linux-specific library links and added logic to ensure link order during installation.

--- packages/l/libui/xmake.lua
+++ packages/l/libui/xmake.lua
@@ -13,8 +13,6 @@ package("libui")
     elseif is_plat("windows") then
         -- the windows meson build file links all of these with a todo to prune the list
         add_syslinks("user32", "kernel32", "gdi32", "comctl32", "uxtheme", "msimg32", "comdlg32", "d2d1", "dwrite", "ole32", "oleaut32", "oleacc", "uuid", "windowscodecs")
-    elseif is_plat("linux") then
-        add_links("pango-1.0", "pangoxft-1.0", "pangocairo-1.0", "pangoft2-1.0")
     end
 
     on_load("linux", function (package)
@@ -30,6 +28,25 @@ package("libui")
         local configs = {"-Dexamples=false", "-Dtests=false"}
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         import("package.tools.meson").install(package, configs)
+        -- get links and ensure link order
+        import("core.base.graph")
+        local dag = graph.new(true)
+        local pkgconfigdir = package:installdir("lib", "pkgconfig")
+        for _, pcfile in ipairs(os.files(path.join(pkgconfigdir, "*.pc"))) do
+            local link = path.basename(pcfile)
+            local content = io.readfile(pcfile)
+            for _, line in ipairs(content:split("\n")) do
+                if line:startswith("Requires: ") then
+                    local requires = line:sub(10):split(",")
+                    for _, dep in ipairs(requires) do
+                        dep = dep:split("=")[1]:trim()
+                        dag:add_edge(link, dep)
+                    end
+                end
+            end
+        end
+        local links = dag:topological_sort()
+        package:add("links", links)
     end)
 
     on_test(function (package)
