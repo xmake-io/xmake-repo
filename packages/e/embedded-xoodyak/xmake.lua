dbdb3637e193ea14868d1363535a1f120f890824
Saikari <lin@sz.cn.eu.org>
2026-08-05 19:57:32 +0300

    Update patch hash for fix-msvc-asm.patch

--- packages/e/embedded-xoodyak/xmake.lua
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -8,7 +8,7 @@ package("embedded-xoodyak")
 
     add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
 
-    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "9f6a313b32a1dfea3dd0f2ada3dfd256432220edfc2745b6e24000615d44208c")
+    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "2011966e92938663d5c6df833e5b89096c8a2e861ced040cf164e04b62d36f09")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
