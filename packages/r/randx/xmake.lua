59aa955dc2292ae508a7e07dfb9e6eabfb8a9d7f
历代星辰 <115717776+lidaixingchen@users.noreply.github.com>
2026-08-05 18:15:02 +0800

    Add package randx 1.4.2 (#10481)
    * Add package randx 1.3.1
    
    * Update randx to 1.4.0
    
    * refactor(randx): simplify package recipe per review feedback
    
    * fix(randx): correct SHA hashes, use Security framework, test with C++17
    
    * Update randx to 1.4.2
    
    * Fix SHA256 for randx 1.4.2 tarball
    
    * Fix header-only link check: create empty archive, remove macOS-incompatible 1.3.1
    
    * fix(randx): remove redundant archive file creation and clean version list
    
    * Update xmake.lua
    
    * Fix checksum for version 1.3.1 in xmake.lua
    
    Corrected the checksum for version 1.3.1.
    
    * Remove version 1.3.1 from xmake.lua
    
    Removed version 1.3.1 from the list of available versions.
    
    * Update C++ standard and header in test function
    
    Updated the C++ standard version in the test configuration and changed the included header from RandX.hpp to RandX_Cpp17.hpp.
    
    * Enhance RandX_Cpp17.hpp for Android support
    
    Add Android compatibility for getrandom syscall in RandX_Cpp17.hpp
    
    * Update include directives for Android in RandX_Cpp17.hpp
    
    Replace include directive for Android with unistd.h and syscall.h.
    
    * Update RandX_Cpp17.hpp for Linux and Android compatibility
    
    * Correct pragma once handling in xmake.lua
    
    Fix pragma once replacement in xmake.lua during installation.
    
    ---------
    
    Co-authored-by: Saikari <lin@sz.cn.eu.org>

---
+++ packages/r/randx/xmake.lua
@@ -0,0 +1,39 @@
+package("randx")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/lidaixingchen/RandX")
+    set_description("Modern, fast, with header-only C++ pseudo-random number generator and distribution library (C++17/C++23).")
+    set_license("MIT")
+
+    add_urls("https://github.com/lidaixingchen/RandX/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("1.4.3", "2227db348490f72e0aa8aaff923b24b773dec268656ef60bec3fe997e2fbac61")
+    add_versions("1.4.2", "25badb73e98b2e83456bea63bb60b1f335091576ec1d1c06ec2649ed92fc84bf")
+    add_versions("1.4.0", "ecf611c6f340986df2abf3191def2222a3287f5e20ec4280c88996770a95eec7")
+
+    if is_plat("windows", "mingw") then
+        add_syslinks("bcrypt")
+    elseif is_plat("macosx", "iphoneos") then
+        add_frameworks("Security")
+    end
+
+    on_install(function (package)
+        io.replace("RandX_Cpp17.hpp", [[# pragma once]], [[# pragma once
+#if defined(__ANDROID__) && __ANDROID_API__ < 28
+#  include <unistd.h>
+#  include <sys/syscall.h>
+#  define getrandom(buf, buflen, flags) syscall(SYS_getrandom, buf, buflen, flags)
+#endif]], {plain = true})
+        os.cp("RandX.hpp", package:installdir("include"))
+        os.cp("RandX_Cpp17.hpp", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <RandX_Cpp17.hpp>
+            #include <cstdint>
+            static void test() {
+                std::uint64_t v = RandX::RandInt<std::uint64_t>(0, 1000);
+                (void)v;
+            }
+        ]]}, {configs = {languages = "c++17"}}))
+    end)
