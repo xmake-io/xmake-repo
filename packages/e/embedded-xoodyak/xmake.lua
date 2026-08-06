64438e1f6b1fc0748485bf46514cbc76a1aabc4c
Sarun Rattanasiri <7634596+midnight-wonderer@users.noreply.github.com>
2026-08-06 14:14:02 +0700

    Add embedded-xoodyak package (#10021)
    * Add embedded-xoodyak package
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Refactor on_install function for non-Windows systems
    
    * Fix assembly file inclusion for Windows platform
    
    Modify xmake.lua to conditionally include assembly files based on platform.
    
    * Add MSVC assembly patch for installation
    
    Added a patch for MSVC assembly during installation.
    
    * Update patch checksum for fix-msvc-asm.patch
    
    * Fix MSVC assembly file inclusion in xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua for MSVC compatibility
    
    Fix MSVC assembly file inclusion and language settings.
    
    * Update patch hash for fix-msvc-asm.patch
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua
    
    * Update xmake.lua for MSVC assembly compatibility
    
    Fix MSVC assembly file handling in xmake.lua.
    
    ---------
    
    Co-authored-by: ruki <waruqi@gmail.com>
    Co-authored-by: Saikari <lin@sz.cn.eu.org>

---
+++ packages/e/embedded-xoodyak/xmake.lua
@@ -0,0 +1,19 @@
+package("embedded-xoodyak")
+    set_homepage("https://github.com/midnight-wonderer/embedded-xoodyak")
+    set_description("A portable, lightweight, and zero-allocation C library implementing the Xoodyak cryptographic scheme")
+    set_license("CC0-1.0")
+
+    add_urls("https://github.com/midnight-wonderer/embedded-xoodyak/archive/refs/tags/v$(version).tar.gz",
+             "https://github.com/midnight-wonderer/embedded-xoodyak.git")
+
+    add_versions("0.1.0", "37b977a4233efbb86270a67a02aaf6a897548ce4b95e624d532c1296d113e756")
+
+    add_patches("0.1.0", "patches/0.1.0/fix-msvc-asm.patch", "b3695a9fc3efae3242f219bf19adce2faf02a42c6adab6b5dcb35e55b9633e53")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package, {}, {target = "xoodyak"})
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("Xoodyak_Initialize", {includes = "Xoodyak.h"}))
+    end)
