5ce4e493987a2552a680ed401f0f7786b020d8cb
Northn <erolerol3760@gmail.com>
2026-08-07 14:48:35 +0400

    mbedtls: add v3.6.7

--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -15,6 +15,7 @@ package("mbedtls")
     end})
     add_urls("https://github.com/Mbed-TLS/mbedtls.git")
 
+    add_versions("v3.6.7", "a7e8bcbec0e6f761b4af24f25677626b35f762f68eef79c08677a363212d11f6")
     add_versions("v3.6.1", "fc8bef0991b43629b7e5319de6f34f13359011105e08e3e16eed3a9fe6ffd3a3")
     add_versions("v3.6.0", "3ecf94fcfdaacafb757786a01b7538a61750ebd85c4b024f56ff8ba1490fcd38")
     add_versions("v3.5.1", "959a492721ba036afc21f04d1836d874f93ac124cf47cf62c9bcd3a753e49bdb")
