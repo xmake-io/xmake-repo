fb9992d2926b03e58f9ef3f2e6a497e81dc4d43f
ruki <waruqi@gmail.com>
2026-08-15 18:51:54 +0800

    add addons

---
+++ addons/s/serial-tools/xmake.lua
@@ -0,0 +1,19 @@
+package("serial-tools")
+    set_kind("addon")
+    set_homepage("https://github.com/xmake-addons/serial-tools")
+    set_description("The serial port toolkit, it provides the `xmake monitor` command and the serial module.")
+
+    add_urls("https://github.com/xmake-addons/serial-tools/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/xmake-addons/serial-tools.git")
+    add_versions("v1.0.3", "0504b8fdeaaef31583ea6edb525e84efa06850ee5ce592dbb8368c099af8b4e8")
+
+    on_test(function (package)
+        assert(package:has_addon({plugins = "monitor", modules = "serial"}))
+
+        os.vrun("xmake monitor --help")
+        os.vrunv("xmake", {"lua", "-c", [[
+            import("@addon.serial-tools.serial")
+            assert(type(serial.ports()) == "table")
+            assert(serial.monitor and serial.resolve_port)
+        ]]})
+    end)
--- scripts/test.lua
+++ scripts/test.lua
@@ -1,11 +1,46 @@
 -- imports
+import("test_addons")
 import("test_templates")
 import("test_packages")
+
+-- get the arguments of the addon tests
+--
+-- @note we are called with the whole command line, but most of the options are only for
+-- the package tests, e.g. `-a x86_64`, `--runtimes=MT`, so we must not pass them through
+--
+function _addon_argv(argv)
+    local result = {}
+    local with_addons = table.contains(argv, "--addon")
+    local drop_value = false
+    for _, arg in ipairs(argv) do
+        if arg == "--addon" or arg == "-v" or arg == "--verbose" or arg == "-D" or arg == "--diagnosis"
+            or arg == "-vD" or arg == "-Dv" then
+            table.insert(result, arg)
+            drop_value = false
+        elseif arg:startswith("-") then
+            -- an option of the package tests, it may take a value, e.g. `-a x86_64`
+            drop_value = not arg:find("=", 1, true)
+        elseif drop_value then
+            drop_value = false
+        elseif with_addons then
+            -- the addon names, they are only given with `--addon`
+            table.insert(result, arg)
+        end
+    end
+    return result
+end
 
 function main(...)
     local argv = table.pack(...)
     local run_packages = false
     local run_templates = false
+    local run_addons = false
+
+    -- only test the addons? e.g. xmake l scripts/test.lua --addon esp32-devel
+    if table.contains(argv, "--addon") then
+        test_addons(table.unpack(_addon_argv(argv)))
+        return
+    end
 
     -- get modified files
     local diff = try {function () return os.iorun("git --no-pager diff --name-only HEAD^") end}
@@ -16,6 +51,8 @@ function main(...)
                 run_packages = true
             elseif file:startswith("templates") then
                 run_templates = true
+            elseif file:startswith("addons") then
+                run_addons = true
             end
         end
     else
@@ -23,9 +60,15 @@ function main(...)
         run_packages = true
     end
 
-    -- if no changes detected in packages or templates, run package tests by default (e.g. tbox dev)
-    if not run_packages and not run_templates then
+    -- if no changes detected in packages, templates or addons, run package tests by default (e.g. tbox dev)
+    if not run_packages and not run_templates and not run_addons then
         run_packages = true
+    end
+
+    -- run addon tests
+    if run_addons then
+        print("Running addon tests...")
+        test_addons(table.unpack(_addon_argv(argv)))
     end
 
     -- run template tests
