88438cc5561e35cdd853f990cd1c638a55d8daba
Casinius <749929621@qq.com>
2026-08-24 17:33:33 +0800

    disable asan support

--- packages/s/systemc/xmake.lua
+++ packages/s/systemc/xmake.lua
@@ -44,6 +44,7 @@ package("systemc")
             "-DCMAKE_CXX_STANDARD=17",
             "-DCMAKE_CXX_STANDARD_REQUIRED=ON",
             "-DCMAKE_CXX_EXTENSIONS=OFF",
+            "-DENABLE_ASAN=OFF"
         }
         table.insert(configs, "-DCMAKE_INSTALL_PREFIX=" .. package:installdir())
 
