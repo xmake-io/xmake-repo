212d09484b4f417669101ed51f64f700e51dbe57
Casinius <749929621@qq.com>
2026-08-24 19:46:07 +0800

    use static libs for windows cause no symbol export , use pthread for macosx to avoid asan

--- packages/s/systemc/xmake.lua
+++ packages/s/systemc/xmake.lua
@@ -31,7 +31,6 @@ package("systemc")
         end
 
         local configs = {
-            "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
             "-DENABLE_ASSERTIONS=" .. (package:config("assertions") and "ON" or "OFF"),
             "-DBUILD_SOURCE_DOCUMENTATION=OFF",
             "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"),
@@ -40,11 +39,24 @@ package("systemc")
             "-DCMAKE_CXX_STANDARD_REQUIRED=ON",
             "-DCMAKE_CXX_EXTENSIONS=OFF",
         }
+
+        if package:is_plat("windows") then
+            table.insert(configs,"-DBUILD_SHARED_LIBS=OFF")
+        else
+            table.insert(configs,"-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        end
+
+
         if package:is_plat("macosx") then
             table.insert(configs, "-DENABLE_PTHREADS=ON")
-        else
-            table.insert(configs, "-DENABLE_PTHREADS=" .. (package:config("pthreads") and "ON" or "OFF"))
         end
+        if not package:is_plat("windows") then
+            table.insert(configs, "-DENABLE_PTHREADS=" .. (package:config("pthreads") and "ON" or "OFF"))
+        else
+            table.insert(configs, "-DENABLE_PTHREADS=OFF")
+        end
+
+
         import("package.tools.cmake").install(package, configs)
     end)
 
