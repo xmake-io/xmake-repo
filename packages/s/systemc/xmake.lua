dbd68f7a9a1f63a610c3187d081e046b90a76982
ruki <waruqi@gmail.com>
2026-08-24 22:11:48 +0800

    Update xmake.lua

--- packages/s/systemc/xmake.lua
+++ packages/s/systemc/xmake.lua
@@ -3,18 +3,27 @@ package("systemc")
     set_description("SystemC: A modeling language for system-level design")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/accellera-official/systemc.git")
+    add_urls("https://github.com/accellera-official/systemc/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/accellera-official/systemc.git")
 
-    add_versions("3.0.2", "5b191bb6500712243eb152e155c1c6039066cf38")
-    add_versions("3.0.1","e598d4afbef12e7e7002719ce3c7a77c4a227a47")
-    add_versions("3.0.0","cfbb862974d239a4105789b7644b24c0557763fc")
+    add_versions("3.0.2", "9b3693ed286aab958b9e5d79bb0ad3bc523bbc46931100553275352038f4a0c4")
+
+    add_configs("pthreads",   {description = "Use POSIX threads", type = "boolean", default = is_plat("macosx")})
+    add_configs("assertions", {description = "Enable assertions", type = "boolean", default = true})
+
+    if is_plat("windows", "mingw") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
     add_deps("cmake")
 
-    add_configs("pthreads",  {description = "Use POSIX threads",    type = "boolean", default = false})
-    add_configs("assertions",{description = "Enable assertions",    type = "boolean", default = true})
+    on_load(function (package)
+        if package:config("pthreads") and package:is_plat("macosx", "linux", "bsd") then
+            package:add("syslinks", "pthread")
+        end
+    end)
 
-
-    on_install("linux", "windows", "mingw@windows", "mingw@linux", "bsd",function(package)
+    on_install("linux", "windows", "mingw@windows", "mingw@linux", "bsd", function (package)
         import("package.tools.cmake")
 
         if package:is_plat("windows") then
@@ -29,30 +38,13 @@ package("systemc")
         local configs = {
             "-DENABLE_ASSERTIONS=" .. (package:config("assertions") and "ON" or "OFF"),
             "-DBUILD_SOURCE_DOCUMENTATION=OFF",
-            "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"),
-            "-DCMAKE_INSTALL_PREFIX=" .. package:installdir(),
+            "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
             "-DCMAKE_CXX_STANDARD=17",
             "-DCMAKE_CXX_STANDARD_REQUIRED=ON",
             "-DCMAKE_CXX_EXTENSIONS=OFF",
+            "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
+            "-DENABLE_PTHREADS=" .. (package:config("pthreads") and "ON" or "OFF")
         }
-
-        if package:is_plat("windows") or package:is_plat("mingw") then
-            table.insert(configs,"-DBUILD_SHARED_LIBS=OFF")
-        else
-            table.insert(configs,"-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        end
-
-
-        if package:is_plat("macosx") then
-            table.insert(configs, "-DENABLE_PTHREADS=ON")
-        end
-        if not package:is_plat("windows") then
-            table.insert(configs, "-DENABLE_PTHREADS=" .. (package:config("pthreads") and "ON" or "OFF"))
-        else
-            table.insert(configs, "-DENABLE_PTHREADS=OFF")
-        end
-
-
         import("package.tools.cmake").install(package, configs)
     end)
 
