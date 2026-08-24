e399fd9043262900bafd034e505fe7ae198e9b75
Casinius <749929621@qq.com>
2026-08-24 18:20:19 +0800

    fix source dir again

--- packages/s/systemc/xmake.lua
+++ packages/s/systemc/xmake.lua
@@ -1,57 +1,49 @@
 package("systemc")
-    set_homepage("https://systemc.org/overview/systemc")
-    set_description("Fork of the popular zip manipulation library found in the zlib distribution.")
+    set_homepage("https://accellera.org")
+    set_description("SystemC: A modeling language for system-level design")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/accellera-official/systemc.git")
+    add_urls("https://github.com/accellera-official/systemc/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/accellera-official/systemc.git")
 
-    add_versions("3.0.2", "5b191bb6500712243eb152e155c1c6039066cf38")
-    add_versions("3.0.1","e598d4afbef12e7e7002719ce3c7a77c4a227a47")
-    add_versions("3.0.0","cfbb862974d239a4105789b7644b24c0557763fc")
-    add_versions("2.3.4","e8b9e51917abab02b2223cb2f497a1a55450cc64")
-    add_versions("2.3.3","739f1f6ef6d50eaed4102b95cd48a91c5be6a2cf")
-    add_versions("2.3.2","032c018cbee2fca005001088fbfba3f2bd0ab1af")
+    add_versions("3.0.1", "5b191bb6500712243eb152e155c1c6039066cf38")
 
     add_deps("cmake")
 
-    add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
-    add_configs("pthreads", {description = "Use POSIX threads", default = false, type = "boolean"})
-    add_configs("assertions", {description = "Enable assertions", default = true, type = "boolean"})
-    add_configs("docs", {description = "Build source documentation", default = false, type = "boolean"})
-    on_check(function(package)
+    add_configs("shared",    {description = "Build shared library", type = "boolean"})
+    add_configs("pthreads",  {description = "Use POSIX threads",    type = "boolean", default = false})
+    add_configs("assertions",{description = "Enable assertions",    type = "boolean", default = true})
+    add_configs("docs",      {description = "Build source documentation", type = "boolean", default = false})
 
-        local supported_plats = {"linux", "macosx", "windows", "mingw", "cygwin", "bsd"}
-        if not table.contains(supported_plats, package:plat()) then
-            raise("systemc does not support platform %s, supported: %s",
-                package:plat(), table.concat(supported_plats, ", "))
+    on_check(function(package)
+        local supported = {"linux", "macosx", "windows", "mingw", "cygwin", "bsd"}
+        if not table.contains(supported, package:plat()) then
+            raise("systemc: unsupported platform '%s' (supported: %s)",
+                  package:plat(), table.concat(supported, ", "))
         end
     end)
-    on_load(function (package)
+
+    on_load(function(package)
         if package:config("shared") == nil then
-            -- if windows use static
             local default_shared = not package:is_plat("windows")
             package:set_config("shared", default_shared)
         end
     end)
 
     on_install(function(package)
+        import("package.tools.cmake")
+
+        -- 设置临时目录
         local tmpdir = os.tmpdir()
         os.setenv("TMPDIR", tmpdir)
         os.setenv("TEMP", tmpdir)
 
-        if package:is_plat("windows") then
-            local cxxflags = os.getenv("CXXFLAGS") or ""
-            os.setenv("CXXFLAGS", cxxflags .. " -DSC_USE_OLD_OSTREAM")
-        end
+        -- 构建目录
+        local builddir = package:builddir()
+        os.mkdir(builddir)
 
-
-        local generator = "Unix Makefiles"
-        if package:is_plat("windows") or package:is_plat("mingw") then
-            generator = "Ninja"
-        end
-
-
-        local sourcedir = package:source_dir()
+        -- 源码目录：xmake 会将源码 clone 到 builddir/source/ 下
+        local sourcedir = builddir .. "/source/systemc"
 
         -- Windows 源码修补：在配置之前替换 osfx/opfx
         if package:is_plat("windows") then
@@ -64,7 +56,13 @@ package("systemc")
             end
         end
 
+        -- 生成器选择
+        local generator = "Unix Makefiles"
+        if package:is_plat("windows") or package:is_plat("mingw") then
+            generator = "Ninja"
+        end
 
+        -- CMake 参数
         local configs = {
             "-G", generator,
             "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
@@ -76,20 +74,21 @@ package("systemc")
             "-DCMAKE_CXX_STANDARD=17",
             "-DCMAKE_CXX_STANDARD_REQUIRED=ON",
             "-DCMAKE_CXX_EXTENSIONS=OFF",
-            "-DENABLE_ASAN=OFF",
-            "-DENABLE_UBSAN=OFF",
         }
 
-        --disable all the sanitizer for macosx
+        -- macOS 禁用 sanitizer
         if package:is_plat("macosx") then
             table.insert(configs, "-DCMAKE_CXX_FLAGS=-fno-sanitize=address -fno-sanitize=undefined")
             table.insert(configs, "-DCMAKE_C_FLAGS=-fno-sanitize=address -fno-sanitize=undefined")
         end
 
-        cmake.configure(package, {configs = configs, buildir = builddir})
+        -- 1. CMake 配置
+        cmake.configure(package, {configs = configs, buildir = builddir, sourcedir = sourcedir})
 
+        -- 2. 构建
         cmake.build(package, {buildir = builddir})
 
+        -- 3. 安装
         cmake.install(package, {buildir = builddir})
     end)
 
@@ -106,6 +105,5 @@ package("systemc")
             links = "systemc",
         }
         assert(package:check_cxxsnippets({test = snippet}, opts),
-            "SystemC test program failed to compile/link")
+               "SystemC test program failed to compile/link")
     end)
-
