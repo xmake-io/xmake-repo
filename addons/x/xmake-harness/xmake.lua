717f2c47d6fbb486791c4c194623eadd77acb784
ruki <waruqi@gmail.com>
2026-08-25 23:23:29 +0800

    update xmake ai version

--- addons/x/xmake-harness/xmake.lua
+++ addons/x/xmake-harness/xmake.lua
@@ -8,6 +8,7 @@ package("xmake-harness")
              "https://github.com/xmake-addons/xmake-harness.git")
     add_versions("v1.0.0", "44a0b3369ed6d2fc4458b0741ebc264c3bfe62de6749200c3498944811212daa")
     add_versions("v1.0.1", "aefa5d431efc73ae946480cb7e61fa22a342dd1055c05120a4dac40f044550c9")
+    add_versions("v1.0.2", "a22f6a2aad36dbfe2baf0d83a16ae46df551ab5c92a79d12ae57705449e817bf")
 
     on_test(function (package)
         assert(package:has_addon({plugins = "ai", modules = "harness"}))
