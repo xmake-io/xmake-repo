e45ea4d69500848e5f6a14beb2d6923ccc1146a8
Casinius <749929621@qq.com>
2026-09-04 21:33:50 +0800

    add some blank and del set_kind

--- packages/a/adaptive_cpp/xmake.lua
+++ packages/a/adaptive_cpp/xmake.lua
@@ -1,16 +1,22 @@
 package("adaptive_cpp")
-    set_kind("library")
     set_homepage("https://github.com/AdaptiveCpp/AdaptiveCpp")
+    
     set_description("AdaptiveCpp (formerly hipSYCL) - an independent, multi-target implementation of SYCL and C++ standard parallelism for CPUs and GPUs")
+
     set_license("BSD-2-Clause")
+    
     add_urls("https://github.com/AdaptiveCpp/AdaptiveCpp/archive/refs/tags/v$(version).tar.gz")
+
     add_versions("25.10.0", "334b16ebff373bd2841f83332c2ae9a45ec192f2cf964d5fdfe94e1140776059")
     add_patches("25.10.0", "patches/25.10.0/use-xrepo-opencl-headers.patch", "c7a646f4bd8d8b740f8f06dc9f09a296850183e71c2d11195dd1f80209ce36bc")
+
     add_deps("llvm 21.1.0", {host = true, kind = "library", private = true, configs = {clang = true, lld = true, openmp = true, ["compiler-rt"] = false, libcxx = false, libcxxabi = false, libunwind = false}})
     add_deps("libffi", {kind = "library"})
     add_deps("python 3.x", {kind = "binary"})
+
     add_configs("cuda",   {description = "Enable the CUDA backend. Requires a CUDA toolkit already installed on the system.", default = false, type = "boolean"})
     add_configs("rocm",   {description = "Enable the ROCm/HIP backend. Requires a ROCm installation on the system.", default = false, type = "boolean"})
+
     on_load(function (package)
         if package:config("opencl") then
             package:add("deps", "opencl", "opencl-headers", "opencl-clhpp")
@@ -20,11 +26,14 @@ package("adaptive_cpp")
         package:add("links", "acpp-rt", "acpp-common")
         package:addenv("PATH", "bin")
     end)
+    
     on_install("linux", function (package)
         import("package.tools.cmake")
         local llvm = package:dep("llvm")
         local llvm_prefix = llvm:installdir()
+
         assert(os.isfile(path.join(llvm_prefix, "bin", "clang++")), "llvm dependency does not contain clang++!")
+
         local configs = {
             "-DACPP_COMPILER_FEATURE_PROFILE=full",
             "-DACPP_EXPERIMENTAL_LLVM=ON",
@@ -35,6 +44,7 @@ package("adaptive_cpp")
             "-DACPP_CONFIG_FILE_GLOBAL_INSTALLATION=OFF",
             "-DLLVM_DIR=" .. path.join(llvm_prefix, "lib", "cmake", "llvm")
         }
+
         if package:config("opencl") then
             table.insert(configs, "-DOpenCL_ROOT=" .. package:dep("opencl"):installdir())
             table.insert(configs, "-DACPP_OCL_HEADERS_INCLUDE_DIR=" .. package:dep("opencl-headers"):installdir("include"))
@@ -43,17 +53,22 @@ package("adaptive_cpp")
         if package:config("rocm") then
             table.insert(configs, "-DROCM_PATH=" .. (os.getenv("ROCM_PATH") or "/opt/rocm"))
         end
+
         cmake.install(package, configs)
+
         local clangxx = path.join(llvm_prefix, "bin", "clang++")
-        local llvm_libdirs = {path.join(llvm_prefix, "lib")}
+		local llvm_libdirs = {path.join(llvm_prefix, "lib")}
         for _, libomp in ipairs(os.files(path.join(llvm_prefix, "lib", "*", "libomp.so"))) do
             table.insert(llvm_libdirs, 1, path.directory(libomp))
         end
+
         local rpaths = {}
         for _, dir in ipairs(llvm_libdirs) do
             table.insert(rpaths, "-Wl,-rpath," .. dir)
         end
+
         rpaths = table.concat(rpaths, " ")
+
         local corecfg = path.join(package:installdir("etc", "AdaptiveCpp"), "acpp-core.json")
         if os.isfile(corecfg) then
             io.gsub(corecfg, '"default%-cpu%-cxx"%s*:%s*"[^"]*"', string.format('"default-cpu-cxx" : "%s"', clangxx))
@@ -61,6 +76,7 @@ package("adaptive_cpp")
             io.gsub(corecfg, '"default%-sequential%-link%-line"%s*:%s*"[^"]*"', string.format('"default-sequential-link-line" : "%s"', rpaths))
         end
     end)
+    
     on_test(function (package)
         local acpp = path.join(package:installdir("bin"), "acpp")
         assert(os.isfile(acpp), "acpp driver not installed!")
