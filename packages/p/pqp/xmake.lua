2b27e7d52ea48e9501fd49fa0928b40291fe098b
Saikari <lin@sz.cn.eu.org>
2026-09-07 01:33:16 +0300

    Improve pqp for #10763

--- packages/p/pqp/xmake.lua
+++ packages/p/pqp/xmake.lua
@@ -1,25 +1,23 @@
 package("pqp")
-
-    set_homepage("http://gamma.cs.unc.edu/SSV/")
+    set_homepage("https://gamma.cs.unc.edu/SSV/")
     set_description("A Proximity Query Package")
 
     set_urls("http://gamma.cs.unc.edu/software/downloads/SSV/pqp-$(version).tar.gz",
              "https://github.com/GammaUNC/PQP.git")
 
     add_versions("1.3", "00bdcb380206393349ed7ee9a773f894bf796059c66cec0d97c0a93649eb012f")
-    add_patches("1.3", path.join(os.scriptdir(), "patches", "1.3", "fix.patch"), "9160f8bc35d23dd5a84af0891410fafa07b8fd9329389724627a68e96b4c270f")
+    add_patches("1.3", "patches/1.3/fix.patch", "9160f8bc35d23dd5a84af0891410fafa07b8fd9329389724627a68e96b4c270f")
 
     on_install(function (package)
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("pqp")
                 set_kind("$(kind)")
+                set_languages("c++11")
                 add_files("**/src/*.cpp")
                 add_headerfiles("**/src/*.h")
         ]])
-        local configs = {}
-        configs.kind = package:config("shared") and "shared" or "static"
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
