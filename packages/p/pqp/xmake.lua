d8253685326f0252f65bed6504f971605e416e99
Saikari <lin@sz.cn.eu.org>
2026-09-07 02:20:03 +0300

    Try add export symbols

--- packages/p/pqp/xmake.lua
+++ packages/p/pqp/xmake.lua
@@ -16,6 +16,9 @@ package("pqp")
                 set_languages("c++11")
                 add_files("**/src/*.cpp")
                 add_headerfiles("**/src/*.h")
+                if is_plat("windows") and is_kind("shared") then
+                    add_rules("utils.symbols.export_all", {export_classes = true})
+                end
         ]])
         import("package.tools.xmake").install(package)
     end)
