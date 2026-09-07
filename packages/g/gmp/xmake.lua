ebb6d1c7ce946de3dfe0dcad969a782264ba6970
Saikari <lin@sz.cn.eu.org>
2026-09-07 23:45:04 +0300

    fixup

--- packages/g/gmp/xmake.lua
+++ packages/g/gmp/xmake.lua
@@ -3,26 +3,20 @@ package("gmp")
     set_description("GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers.")
     set_license("LGPL-3.0")
 
-    add_urls("https://ftpmirror.gnu.org/gnu/gmp/gmp-$(version).tar.xz")
     add_urls("https://ftp.gnu.org/gnu/gmp/gmp-$(version).tar.xz")
     add_urls("https://gmplib.org/download/gmp/gmp-$(version).tar.xz")
 
     add_versions("6.3.0", "a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898")
-    add_versions("6.2.1", "fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2")
-    add_versions("6.1.2", "87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912")
 
     add_patches("6.3.0", "patches/6.3.0/c23.patch", "24eb6ad75fb2552db247d3c5c522d30f221cca23a0fdc925b2684af44d51b7b3")
 
     add_configs("cpp_api", {description = "Enable C++ support", default = false, type = "boolean"})
-    if is_plat("windows") and is_arch("arm64") then
+    if is_plat("windows") then
         add_configs("assembly", {description = "Enable the use of assembly loops", default = false, type = "boolean"})
     else
         add_configs("assembly", {description = "Enable the use of assembly loops", default = true, type = "boolean"})
     end
     add_configs("fat", {description = "Build fat libraries on systems that support it", default = false, type = "boolean"})
-    if is_plat("windows") then
-        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-    end
 
     if is_plat("mingw") and is_subhost("msys") then
         add_extsources("pacman::gmp")
@@ -40,14 +34,14 @@ package("gmp")
     if on_check then
         on_check(function (package)
             if package:is_plat("windows") then
-                if package:config("shared") then
-                    raise("package(gmp) cannot be built as a shared library on Windows")
-                end
                 if package:has_tool("cxx", "clang_cl") then
                     raise("package(gmp) unsupported clang-cl toolchain now, you can use clang toolchain\nadd_requires(\"gmp\", {configs = {toolchains = \"clang\"}}))")
                 end
             end
             if package:config("fat") then
+                if package:is_plat("windows") then
+                    raise("package(gmp) config(fat) is not supported on Windows MSVC")
+                end
                 if not package:is_arch("x86", "x86_64", "i386") then
                     raise("package(gmp) config(fat) is only supported on x86 and x86_64 architectures")
                 end
@@ -83,18 +77,17 @@ package("gmp")
             package:add("deps", "msys2", {configs = {msystem = msystem, base_devel = true}})
         end
         if package:is_plat("windows") then
-            -- msvc toolchain require other tool to build asm
-            -- x86, x64 -> yasm, arm64 -> clang
-            if package:is_arch("x64", "x86") and package:config("assembly") ~= false then
-                package:add("deps", "yasm")
+            if package:config("shared") then
+                package:add("defines", "__GMP_LIBGMP_DLL")
             end
 
             if package:is_arch("x64", "arm64") then
                 package:add("defines", "_LONG_LONG_LIMB") -- mp_limb_t type
             end
-            -- if package:config("shared") then
-            --     package:add("defines", "__GMP_LIBGMP_DLL")
-            -- end
+
+            if package:config("assembly") and package:is_arch("x86") and not package:has_tool("cxx", "clang") then
+                package:add("deps", "yasm")
+            end
         end
     end)
 
@@ -118,6 +111,16 @@ package("gmp")
         if package:is_plat("windows") then
             -- Fix MSVC nextprime.c memset conflict with <string.h>
             io.replace("nextprime.c", "#include <string.h>", "#ifndef _MSC_VER\n#include <string.h>\n#endif", {plain = true})
+            -- MSVC symbol & inline fix (from vcpkg msvc_symbol.patch)
+            io.replace("gmp-h.in", "#define __GMP_EXTERN_INLINE  __inline", "#define __GMP_EXTERN_INLINE  static __inline", {plain = true})
+            -- Remove GNU ld specific flags from configure for MSVC
+            io.replace("configure", 'GMP_LDFLAGS="$GMP_LDFLAGS -no-undefined -Wl,--export-all-symbols"', 'GMP_LDFLAGS="$GMP_LDFLAGS -no-undefined"', {plain = true})
+            io.replace("configure", 'LIBGMP_LDFLAGS="$LIBGMP_LDFLAGS -Wl,--output-def,.libs/libgmp-3.dll.def"', '', {plain = true})
+            io.replace("configure", 'LIBGMPXX_LDFLAGS="$LIBGMP_LDFLAGS -Wl,--output-def,.libs/libgmpxx-3.dll.def"', '', {plain = true})
+            if package:config("shared") then
+                -- export symbol macro
+                io.replace("gmp-h.in", "#define __GMP_LIBGMP_DLL  @LIBGMP_DLL@", "#define __GMP_LIBGMP_DLL  1", {plain = true})
+            end
             -- Let asm code use windows abi
             io.replace("configure", "*-*-mingw* | *-*-msys | *-*-cygwin)", "*-*-msvc)", {plain = true})
             local obj_file_suffix = package:has_tool("cxx", "cl") and ".obj" or ".o"
@@ -127,12 +130,6 @@ package("gmp")
             if package:has_tool("ld", "link") then
                 -- `lib /out: xxx` -> `lib /out:xxx`
                 io.replace("configure", "$AR $AR_FLAGS ", "$AR $AR_FLAGS", {plain = true})
-            end
-            if package:config("shared") then
-                -- error: duplicate symbol: __gmpn_add
-                io.replace("gmp-h.in", "#define __GMP_EXTERN_INLINE  __inline", "#define __GMP_EXTERN_INLINE  static __inline", {plain = true})
-                -- export symbol macro
-                io.replace("gmp-h.in", "#define __GMP_LIBGMP_DLL  @LIBGMP_DLL@", "#define __GMP_LIBGMP_DLL  1", {plain = true})
             end
         end
 
@@ -180,6 +177,8 @@ package("gmp")
                 table.insert(configs, "gmp_cv_asm_w32=.word") -- fix detect
                 table.insert(configs, "ac_cv_c_restrict=restrict")
                 table.insert(configs, "gmp_cv_asm_label_suffix=:")
+                table.insert(configs, "ac_cv_func_memset=yes")
+                table.insert(configs, "gmp_cv_check_libm_for_build=no")
                 opt.envs.CXXFLAGS = (opt.envs.CXXFLAGS or "") .. " -EHsc"
                 opt.envs.CFLAGS = (opt.envs.CFLAGS or "") .. " -FS"
                 opt.envs.CXXFLAGS = opt.envs.CXXFLAGS .. " -FS"
@@ -214,27 +213,24 @@ package("gmp")
                 ["arm64"] = "aarch64",
             }
             local target = clang_archs[package:arch()] .. "-windows-msvc"
-            if package:is_arch("x64", "x86") then
-                local yasm_machine = {
-                    ["x86"] = "x86",
-                    ["x64"] = "amd64",
-                }
-                local yasm_format = {
-                    ["x86"] = "win32",
-                    ["x64"] = "win64",
-                }
-                opt.envs.CCAS = "yasm"
-                opt.envs.CCASFLAGS = format("-a x86 -m %s -p gas -r raw -f %s -g null -X gnu", yasm_machine[package:arch()], yasm_format[package:arch()])
-            elseif package:is_arch("arm64") then
-                if package:has_tool("cxx", "clang") then
-                    opt.envs.CCAS = opt.envs.CC
+            if package:config("assembly") then
+                local clang = package:has_tool("cxx", "clang") and opt.envs.CC or find_tool("clang")
+                if clang then
+                    local clang_prog = type(clang) == "table" and clang.program or clang
+                    opt.envs.CCAS = path.unix(clang_prog)
+                    opt.envs.CCASFLAGS = table.concat({"--target=" .. target, "-c"}, " ")
+                    if package:is_arch("arm64") and not package:has_tool("cxx", "clang") then
+                        local llvm_nm = find_tool("llvm-nm")
+                        if llvm_nm then
+                            opt.envs.NM = path.unix(llvm_nm.program)
+                        end
+                    end
+                elseif package:is_arch("x86") then
+                    opt.envs.CCAS = "yasm"
+                    opt.envs.CCASFLAGS = "-a x86 -m x86 -p gas -r raw -f win32 -g null -X gnu"
                 else
-                    local llvm_nm = assert(find_tool("llvm-nm"), "windows arm64 require llvm-nm to detect")
-                    local clang = assert(find_tool("clang"), "windows arm64 require clang to build asm")
-                    opt.envs.CCAS = path.unix(clang.program)
-                    opt.envs.NM = path.unix(llvm_nm.program)
+                    table.insert(configs, "--enable-assembly=no")
                 end
-                opt.envs.CCASFLAGS = table.concat({"--target=" .. target, "-c"}, " ")
             end
             table.insert(configs, "--host=" .. target)
         end
