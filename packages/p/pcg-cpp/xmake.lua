684cac6bbf81ad3843d43e7448126ef0e10e7dca
Saikari <lin@sz.cn.eu.org>
2026-09-07 14:45:50 +0300

    pcg-cpp: fix windows arm64 build (#10776)
    * pcg-cpp: fix windows arm64 build
    
    * Homepage use HTTPS

--- packages/p/pcg-cpp/xmake.lua
+++ packages/p/pcg-cpp/xmake.lua
@@ -1,11 +1,12 @@
 package("pcg-cpp")
     set_kind("library", {headeronly = true})
-    set_homepage("http://www.pcg-random.org")
+    set_homepage("https://www.pcg-random.org/")
     set_description("PCG — C++ Implementation")
     set_license("Apache-2.0")
 
     add_urls("https://github.com/imneme/pcg-cpp.git")
     add_versions("2022.04.09", "428802d1a5634f96bcd0705fab379ff0113bcf13")
+    add_patches("2022.04.09", "patches/2022.04.09/msvc-arm64.patch", "6923b80da69c531d0f4e3c0f736dced7e096311d5f3c00681a55ec63b2bb3591")
 
     on_install(function (package)
         os.cp("include", package:installdir())
