95df36a07a9066fda66a9e059c8f996780aabc86
Saikari <lin@sz.cn.eu.org>
2026-09-08 05:40:05 +0300

    Improve vapoursynth for #10763 (#10768)
    * Improve vapoursynth for #10763
    
    * Fix meson
    
    * Remove redundant if
    
    * Try use venv
    
    * Improve python 3.x check
    
    * Refactor vapoursynth installation logic to prioritize python configuration and adjust meson.build handling for version compatibility
    
    * Try fix build for windows arm64
    
    * Improve wrap python logic
    
    * Guard io.replace
    
    * Try fix cross build by upstream patch
    
    * Try fix iOS via libintl inspiration
    
    * Try fix macosx
    
    * Try fix NDK
    
    * Try fix NDK and FreeBSD
    
    * fixup

--- packages/v/vapoursynth/xmake.lua
+++ packages/v/vapoursynth/xmake.lua
@@ -1,18 +1,21 @@
 package("vapoursynth")
-    set_homepage("http://www.vapoursynth.com/")
+    set_homepage("https://www.vapoursynth.com/")
     set_description("A video processing framework with simplicity in mind")
     set_license("LGPL-2.1")
 
     add_urls("https://github.com/vapoursynth/vapoursynth/archive/refs/tags/R$(version).tar.gz")
     add_urls("https://github.com/vapoursynth/vapoursynth.git", {alias = "git"})
 
+    add_versions("79", "cb7ea3c75431176f8ce1f466e1c1fff7ffdacdd2d397be8fabc2d467194ab5a6")
     add_versions("73", "1bb8ffe31348eaf46d8f541b138f0136d10edaef0c130c1e5a13aa4a4b057280")
 
+    add_versions("git:79", "R79")
     add_versions("git:73", "R73")
 
-    add_configs("vsscript", {description = "Build VSScript. Requires Python 3", default = false, type = "boolean"})
-    add_configs("vspipe", {description = "Build vspipe. Requires VSScript", default = false, type = "boolean"})
-    add_configs("python", {description = "Build the Python module. Requires Python and Cython", default = false, type = "boolean"})
+    add_patches(">=75", "patches/79/meson.patch", "1b5b6035e6047e94175393f50930e6f2b6e3f30215e9cde9d9e502205baaf3ae")
+    add_patches(">=79", "patches/79/fix-arm.patch", "322990a2480bf082c36ebafcd468d5a8882ded53cd5fc65b08dbc1df3d34629a")
+    add_patches(">=79", "patches/79/fix-freebsd.patch", "6ac2375d41db918da3f29e2838fd7fe5c9ac237d97182c162100319a8b8b1c1d")
+
     if is_plat("wasm") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
@@ -36,7 +39,17 @@ package("vapoursynth")
         assert(ndk and tonumber(ndk) > 22, "package(vapoursynth) require ndk version > 22")
     end)
 
-    on_load("@windows", function (package)
+    on_load(function (package)
+        if package:version() and package:version():le("74") then
+            package:add("configs", "vsscript", {description = "Build VSScript. Requires Python 3", default = false, type = "boolean"})
+            package:add("configs", "vspipe", {description = "Build vspipe. Requires VSScript", default = false, type = "boolean"})
+            package:add("configs", "python", {description = "Build the Python module. Requires Python and Cython", default = false, type = "boolean"})
+            if package:config("vsscript") or package:config("python") then
+                package:add("deps", "python 3.x", {kind = "binary"})
+            end
+        else
+            package:add("deps", "python 3.x", {kind = "binary"})
+        end
         local has_cat = try { function()
             os.vrun("cat --version")
             os.vrun("grep --version")
@@ -49,24 +62,71 @@ package("vapoursynth")
     end)
 
     on_install(function (package)
+        if package:config("python") then
+            local pytool = package:find_tool("python") or package:find_tool("python3")
+            local venv_dir = path.join(os.curdir(), ".venv")
+            os.vrunv(pytool.program, {"-m", "venv", venv_dir})
+            local venv_bin = is_host("windows") and path.join(venv_dir, "Scripts") or path.join(venv_dir, "bin")
+            local venv_python = is_host("windows") and path.join(venv_bin, "python.exe") or path.join(venv_bin, "python")
+            if not os.isfile(venv_python) and is_host("windows") then
+                venv_python = path.join(venv_dir, "python.exe")
+                venv_bin = venv_dir
+            end
+            os.vrunv(venv_python, {"-m", "pip", "install", "cython"})
+            os.addenv("PATH", venv_bin)
+        elseif package:version() and package:version():ge("75") then
+            io.replace("meson.build", "project('VapourSynth', 'c', 'cpp', 'cython',", "project('VapourSynth', 'c', 'cpp',", {plain = true})
+            io.replace("meson.build", "py.extension_module('vapoursynth',", "if false\npy.extension_module('vapoursynth',", {plain = true})
+            io.replace("meson.build", "filters_sources = files(", "endif\nfilters_sources = files(", {plain = true})
+        else
+            io.replace("meson.build", "['c', 'cpp', 'cython']", "['c', 'cpp']", {plain = true})
+            io.replace("meson.build", ", 'cython'", "", {plain = true})
+        end
         if package:has_tool("cxx", "cl") then
+            io.replace("meson.build", "'-Wno-ignored-attributes',", "", {plain = true})
             io.replace("meson.build", "-Wno-ignored-attributes", "", {plain = true})
             io.replace("meson.build", "add_project_arguments(['-fno-math-errno', '-fno-trapping-math'], language: lang)", "", {plain = true})
         end
-        if not package:config("python") then
-            io.replace("meson.build", ", 'cython'", "", {plain = true})
+        if package:is_plat("android") and package:check_sizeof("void*") == "4" then
+            io.replace("meson.build", "add_project_arguments('-D_FILE_OFFSET_BITS=64', language: ['c', 'cpp'])", "", {plain = true})
+        end
+        if package:is_plat("iphoneos") then
+            local ldflags = table.wrap(package:build_getenv("ldflags"))
+            local new_ldflags = {}
+            for _, flag in ipairs(ldflags) do
+                if flag ~= "-ObjC" then
+                    table.insert(new_ldflags, flag)
+                end
+            end
+            package:build_setenv("ldflags", new_ldflags)
+
+            local shflags = table.wrap(package:build_getenv("shflags"))
+            local new_shflags = {}
+            for _, flag in ipairs(shflags) do
+                if flag ~= "-ObjC" then
+                    table.insert(new_shflags, flag)
+                end
+            end
+            package:build_setenv("shflags", new_shflags)
         end
         if not package:config("shared") and package:is_plat("windows", "mingw") then
-            io.replace("include/VapourSynth.h", "__declspec(dllexport)", "", {plain = true})
-            io.replace("include/VapourSynth4.h", "__declspec(dllexport)", "", {plain = true})
+            if os.isfile("include/VapourSynth.h") then
+                io.replace("include/VapourSynth.h", "__declspec(dllexport)", "", {plain = true})
+            end
+            if os.isfile("include/VapourSynth4.h") then
+                io.replace("include/VapourSynth4.h", "__declspec(dllexport)", "", {plain = true})
+            end
         end
-
         local configs = {}
         table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
         table.insert(configs, "-Db_lto=" .. (package:config("lto") and "true" or "false"))
-        table.insert(configs, "-Denable_vsscript=" .. (package:config("vsscript") and "true" or "false"))
-        table.insert(configs, "-Denable_vspipe=" .. (package:config("vspipe") and "true" or "false"))
-        table.insert(configs, "-Denable_python_module=" .. (package:config("python") and "true" or "false"))
+        if package:version() and package:version():le("74") then
+            table.insert(configs, "-Denable_vsscript=" .. (package:config("vsscript") and "true" or "false"))
+            table.insert(configs, "-Denable_vspipe=" .. (package:config("vspipe") and "true" or "false"))
+            table.insert(configs, "-Denable_python_module=" .. (package:config("python") and "true" or "false"))
+        elseif package:version() and package:version():ge("79") and package:has_tool("cxx", "cl") then
+            table.insert(configs, "-Denable_arm_asm=false")
+        end
 
         local opt = {}
         if package:is_plat("windows") then
@@ -76,12 +136,20 @@ package("vapoursynth")
         elseif package:is_plat("linux", "bsd") then
             opt.cxflags = "-pthread"
             opt.shflags = "-pthread"
+        elseif package:is_plat("android") and package:check_sizeof("void*") == "4" then
+            opt.cxflags = "-U_FILE_OFFSET_BITS"
         end
         import("package.tools.meson").install(package, configs, opt)
 
         if not package:config("shared") and package:is_plat("windows", "mingw") then
-            io.replace(path.join(package:installdir("include"), "vapoursynth/VapourSynth.h"), "__declspec(dllimport)", "", {plain = true})
-            io.replace(path.join(package:installdir("include"), "vapoursynth/VapourSynth4.h"), "__declspec(dllimport)", "", {plain = true})
+            local header_v3 = path.join(package:installdir("include"), "vapoursynth/VapourSynth.h")
+            local header_v4 = path.join(package:installdir("include"), "vapoursynth/VapourSynth4.h")
+            if os.isfile(header_v3) then
+                io.replace(header_v3, "__declspec(dllimport)", "", {plain = true})
+            end
+            if os.isfile(header_v4) then
+                io.replace(header_v4, "__declspec(dllimport)", "", {plain = true})
+            end
         end
     end)
 
