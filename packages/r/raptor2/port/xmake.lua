dcfab197eb396289e5481c9bf50ce671f7a75494
EZ4Stephen <ez4stephen@tutamail.com>
2026-09-10 15:20:08 +0400

    Update xmake.lua

--- packages/r/raptor2/port/xmake.lua
+++ packages/r/raptor2/port/xmake.lua
@@ -135,9 +135,8 @@ target("raptor2")
             elseif v == false then return "0"
             else return tostring(v) end
         end)
-        local cfg = io.readfile("src/raptor_config.h")
-        cfg = cfg .. "\n#define HAVE_STRINGS_H 1\n"
-        io.writefile("src/raptor_config.h", cfg)
+        template = template .. "\n#define HAVE_STRINGS_H 1\n"
+        io.writefile("src/raptor_config.h", template)
 
         -- Windows: add <io.h> and fix access() macro for MSVC
         if is_plat("windows") then
