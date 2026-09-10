e1b2bea1040b0b3c45e266ab1afc3ae9aa5edf2c
Saikari <lin@sz.cn.eu.org>
2026-09-10 18:38:43 +0300

    libfixmath: update homepage

--- packages/l/libfixmath/xmake.lua
+++ packages/l/libfixmath/xmake.lua
@@ -1,10 +1,11 @@
 package("libfixmath")
-    set_homepage("https://code.google.com/p/libfixmath/")
+    set_homepage("https://github.com/PetteriAimonen/libfixmath")
     set_description("Cross Platform Fixed Point Maths Library")
     set_license("MIT")
 
     add_urls("https://github.com/PetteriAimonen/libfixmath.git")
 
+    add_versions("2026.07.14", "ed9391c33a56078c126e3e7a811a883c8c87daa2")
     add_versions("2023.08.06", "d308e466e1a09118d03f677c52e5fbf402f6fdd0")
 
     on_install(function (package)
