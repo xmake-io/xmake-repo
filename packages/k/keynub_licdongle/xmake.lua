c29100f4f25208402d8240413745817edf18fc15
Saikari <lin@sz.cn.eu.org>
2026-09-14 00:28:51 +0300

    Refactor installation logic for keynub_licdongle
    Updated installation process to copy all relevant library files based on platform, simplifying the code structure.

--- packages/k/keynub_licdongle/xmake.lua
+++ packages/k/keynub_licdongle/xmake.lua
@@ -7,49 +7,35 @@ package("keynub_licdongle")
              "https://github.com/AB-KeyNub/KeyNub-SDK.git")
     add_versions("1.1.1", "3a8242fc9a19cd6c04f1abc9e23c17374ba48712089f49c4d9f6e1830321513a")
 
-    -- The library is prebuilt and shared; there is nothing to build here.
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
 
     on_load(function (package)
         package:add("links", "keynub_licdongle", "keynub_licdongle_flat")
     end)
 
-    on_install("windows|x64", "windows|x86", "windows|arm64",
-               "linux|x86_64", "linux|arm64-v8a", "linux|aarch64",
-               "macosx|x86_64", "macosx|arm64", function (package)
+    on_install("windows", "linux", "macosx", function (package)
         local os_name = package:is_plat("windows") and "win" or (package:is_plat("linux") and "linux" or "osx")
-        local arch = package:arch()
         local arch_name
-        if arch == "x64" or arch == "x86_64" then
+        if package:is_arch("x64") then
             arch_name = "x64"
-        elseif arch == "x86" then
+        elseif package:is_arch("x86", "i386") then
             arch_name = "x86"
         else
             arch_name = "arm64"
         end
         local natives = path.join("natives", os_name .. "-" .. arch_name)
-        assert(os.isdir(natives), "no prebuilt keynub_licdongle library for %s-%s", os_name, arch_name)
-
         os.cp("include/licdongle.h", package:installdir("include"))
         os.cp("bindings/cpp/licdongle.hpp", package:installdir("include"))
         os.cp("bindings/flat/licd_flat.h", package:installdir("include"))
         os.cp("bindings/labview/licd_labview.h", package:installdir("include"))
         if package:is_plat("windows") then
-            os.cp(path.join(natives, "keynub_licdongle.dll"), package:installdir("bin"))
-            os.cp(path.join(natives, "keynub_licdongle_flat.dll"), package:installdir("bin"))
-            os.cp(path.join(natives, "keynub_licdongle.lib"), package:installdir("lib"))
-            os.cp(path.join(natives, "keynub_licdongle_flat.lib"), package:installdir("lib"))
+            os.cp(path.join(natives, "*.dll"), package:installdir("bin"))
+            os.cp(path.join(natives, "*.lib"), package:installdir("lib"))
         elseif package:is_plat("linux") then
-            os.cp(path.join(natives, "libkeynub_licdongle.so"), package:installdir("lib"))
-            os.cp(path.join(natives, "libkeynub_licdongle_flat.so"), package:installdir("lib"))
+            os.cp(path.join(natives, "*.so"), package:installdir("lib"))
         else
-            os.cp(path.join(natives, "libkeynub_licdongle.dylib"), package:installdir("lib"))
-            os.cp(path.join(natives, "libkeynub_licdongle_flat.dylib"), package:installdir("lib"))
+            os.cp(path.join(natives, "*.dylib"), package:installdir("lib"))
         end
-        os.cp("LICENSE", package:installdir())
-        os.cp("BINARY-LICENSE.txt", package:installdir())
-        os.cp("NOTICE", package:installdir())
-        os.cp("THIRD-PARTY-NOTICES.txt", package:installdir())
     end)
 
     on_test(function (package)
