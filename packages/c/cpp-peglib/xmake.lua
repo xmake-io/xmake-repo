934742ca56e3b34e5d25f0d303b11fb9b983a2bb
ruki <waruqi@gmail.com>
2026-09-16 03:24:18 +0000

    Update cpp-peglib to 1.17.0

--- packages/c/cpp-peglib/xmake.lua
+++ packages/c/cpp-peglib/xmake.lua
@@ -8,6 +8,7 @@ package("cpp-peglib")
     set_urls("https://github.com/yhirose/cpp-peglib/archive/refs/tags/v$(version).tar.gz",
              "https://github.com/yhirose/cpp-peglib.git")
 
+    add_versions("1.17.0", "48145b73d080d4cad950ca6f89d5ccd3373ba4017ab488733f532a0d74afc67a")
     add_versions("1.10.3", "af654d345788715754cee3757433837620aed38a2efc30a3e94ee709bf407ba0")
     add_versions("1.10.2", "a20d79c32b91ed08f845b91a138c5958b3eb819d2127afcc64714ec1a6ad451b")
     add_versions("1.10.1", "3ba50bdc1be5521affc507e9fa589526372f6d7396ec490f706255a2b30d9635")
