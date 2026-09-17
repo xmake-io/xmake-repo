69b8c215d867853be73a09615a3ed8787d7734c9
ruki <waruqi@gmail.com>
2026-09-17 03:30:14 +0000

    Update expat to 2.8.4

--- packages/e/expat/xmake.lua
+++ packages/e/expat/xmake.lua
@@ -4,6 +4,7 @@ package("expat")
     set_license("MIT")
 
     set_urls("https://github.com/libexpat/libexpat/releases/download/R_$(version).tar.bz2", {version = function (version) return version:gsub("%.", "_") .. "/expat-" .. version end})
+    add_versions("2.8.4", "963250a823c16a498582b4ad82ad0f88926be0769675d3b6956be4d769a1cd8f")
     add_versions("2.8.1", "f5833dd2e1cd7739ec9182804a1a29c4f0cc7c2f26b633d3a2188b7766a88ecb")
     add_versions("2.7.4", "e6af11b01e32e5ef64906a5cca8809eabc4beb7ff2f9a0e6aabbd42e825135d0")
     add_versions("2.7.3", "59c31441fec9a66205307749eccfee551055f2d792f329f18d97099e919a3b2f")
