1944a5dece6dac2e28e13e18719684bfd5d244da
Saikari <lin@sz.cn.eu.org>
2026-09-22 07:50:39 +0300

    Refactor program variable assignment in on_test

--- packages/g/ghostscript/xmake.lua
+++ packages/g/ghostscript/xmake.lua
@@ -56,9 +56,11 @@ package("ghostscript")
     end)
 
     on_test(function (package)
-        local program = "gsc"
+        local program
         if package:is_plat("windows") then
             program = package:is_arch("x64") and "gswin64c" or "gswin32c"
+        else
+            program = "gsc"
         end
         os.vrunv(program, {"--version"})
         assert(package:has_cxxfuncs("gsapi_new_instance", {includes = "iapi.h"}))
