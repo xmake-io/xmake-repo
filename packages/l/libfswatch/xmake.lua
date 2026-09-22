ef2a1792c20dd04846a83267ae5ce7889797ff5e
ruki <waruqi@gmail.com>
2026-09-22 14:56:10 +0800

    Update libfswatch to 1.22.0 (#10888)

--- packages/l/libfswatch/xmake.lua
+++ packages/l/libfswatch/xmake.lua
@@ -6,6 +6,7 @@ package("libfswatch")
     add_urls("https://github.com/emcrisostomo/fswatch/archive/refs/tags/$(version).tar.gz",
              "https://github.com/emcrisostomo/fswatch.git")
 
+    add_versions("1.22.0", "469bab7acb0405e1cead8a59576f8daa6fc720832db4baea20f1fa43edd66d38")
     add_versions("1.21.0", "1a184152a768b8ce7bb5049f768071ab07c5e9b4d0fd70db91c79d069b01c0b5")
     add_versions("1.19.0", "0701283dd0eae6d21eb4c996ef4caf49921538c11f1c68ae2a5f7b3a2f1e36e7")
     add_versions("1.18.3", "07f9fec15138fe25ea92d869117811c7c4fba2e6c6231682d49b159bae2ebd53")
