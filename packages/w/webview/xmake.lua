6ed314b1e2ee0a36152432d8b5a9d08765e4933c
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-09-23 09:56:50 +0400

    add webview package

---
+++ packages/w/webview/xmake.lua
@@ -0,0 +1,14 @@
+package("webview")
+
+set_kind("library", {headeronly = true})
+
+set_homepage("https://github.com/webview/webview")
+set_description("A tiny cross-platform webview library for C/C++ to build modern cross-platform GUIs.")
+set_license("MIT")
+
+add_urls(...)
+add_versions(...)
+
+on_test(function(package)
+    ...
+end)
