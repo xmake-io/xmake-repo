810c2e2ea03d71ee086a322febc81d8efbda6537
ruki <waruqi@gmail.com>
2026-09-24 16:46:08 +0800

    Update xmake.lua

--- packages/c/citor/xmake.lua
+++ packages/c/citor/xmake.lua
@@ -3,13 +3,18 @@ package("citor")
     set_homepage("https://github.com/Lallapallooza/citor")
     set_description("Header-only C++20 thread pool with sub-microsecond dispatch, decentralized work-stealing, and per-CCD arenas.")
     set_license("MIT")
-    add_urls("https://github.com/Lallapallooza/citor/archive/refs/tags/$(version).tar.gz","https://github.com/Lallapallooza/citor.git")
-    add_versions("v0.6.1","717f59a3e443858973d1e143d0cfeb061ba0dd5d")
-    add_versions("v0.6.0","ffc33c285f22d2a0d7dc5feb4662c08fe06b537f")
-    add_versions("v0.5.0","77bd994439b2500d7691e75ab1132a27f9eb4d8b")
-    on_install("!*|arm64", "!*|i386", "!*|i686", "!*|arm",function (package)
+
+    add_urls("https://github.com/Lallapallooza/citor/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/Lallapallooza/citor.git")
+
+    add_versions("v0.6.1", "1a0f930881d064dbc0bde7d605b9c9ef970e07709dc95cdad858a1d6013fc39e")
+    add_versions("v0.6.0", "db080e18e10f920d3e4097c031f2c2b152b1a784e300a8439d0dda09221b7829")
+    add_versions("v0.5.0", "9a80af1eaf655066fabbe131bf5815a024efc7ea1d728f57243d2a6850fb854d")
+
+    on_install("!*|arm64", "!*|i386", "!*|i686", "!*|arm", function (package)
         os.cp("include/*", package:installdir("include"))
     end)
+
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
             #include <citor/coro.h>
